294bebcb8f247bf0a01c784990f16e186b8f4520
Jordan Liu <jordan.zliu@gmail.com>
2018-08-11 17:13:38 -0700

    fix test compilation

--- src/thunderbots/software/test/geom/util.cpp
+++ src/thunderbots/software/test/geom/util.cpp
@@ -22,27 +22,27 @@ TEST(GeomUtilTest, dist_line_vector2)
     double calculated_val, expected_val;
 
     // case 1
-    Line test1line(Vector2(0, 1), Vector2(0, 0));
-    Vector2 test1p(2, 0);
+    Line test1line(Point(0, 1), Point(0, 0));
+    Point test1p(2, 0);
     calculated_val = dist(test1line, test1p);
     expected_val   = 2;
     EXPECT_EQ(expected_val, calculated_val);
 
     // case 2
-    Line test2line(Vector2(2, 0), Vector2(0, 2));
-    Vector2 test2p(0, 0);
+    Line test2line(Point(2, 0), Point(0, 2));
+    Point test2p(0, 0);
     calculated_val = dist(test2line, test2p);
     expected_val   = sqrt(2);
     EXPECT_DOUBLE_EQ(expected_val, calculated_val);
 
     // case 3
-    Line test3line(Vector2(0, 0), Vector2(0, 0));
+    Line test3line(Point(0, 0), Point(0, 0));
     Point test3p(1, 0);
     calculated_val = dist(test3line, test3p);
     expected_val   = 1;
     EXPECT_EQ(expected_val, calculated_val);
 
-    Line line(Vector2(1, -1), Vector2(5, -2));
+    Line line(Point(1, -1), Point(5, -2));
     Point p(2, -3);
 
     EXPECT_NEAR(1.69775, dist(line, p), 1e-5);
