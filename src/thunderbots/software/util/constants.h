14e3c775a034df33e9c5dde3106c78021ae4dfc4
Mathew <mmacd97@gmail.com>
2018-08-13 19:40:52 -0700

    Fixed launchfile issues (#30)
    * renamed thunderbots pacakge to 'thunderbots', and move launch files to be with the package
    
    * fixed incorrect topic and publisher names

--- src/thunderbots/software/util/constants.h
+++ src/thunderbots/software/util/constants.h
@@ -12,7 +12,7 @@ static const std::string BACKEND_INPUT_BALL_TOPIC          = "backend/ball";
 static const std::string BACKEND_INPUT_FIELD_TOPIC         = "backend/field";
 static const std::string BACKEND_INPUT_FRIENDLY_TEAM_TOPIC = "backend/friendly_team";
 static const std::string BACKEND_INPUT_ENEMY_TEAM_TOPIC    = "backend/enemy_team";
-static const std::string AI_PRIMITIVES_TOPIC               = "backend/enemy_team";
+static const std::string AI_PRIMITIVES_TOPIC               = "backend/primitives";
 
 // TODO: Make this a tuneable parameter
 const TeamColour FRIENDLY_TEAM_COLOUR = YELLOW;
