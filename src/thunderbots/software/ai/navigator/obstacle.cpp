b42c74a6a4edab61516204706fee856fea521bdd
Henry Liu <henryymliu@gmail.com>
2018-08-25 09:25:21 -0700

    another commit, waiting on geom changes

---
+++ src/thunderbots/software/ai/navigator/obstacle.cpp
@@ -0,0 +1,9 @@
+#include "obstacle.h"
+
+std::vector<RobotObstacle> process_friendly(Team& friendly_team, double avoid_dist)
+{
+    for (Robot r : friendly_team.getAllRobots())
+    {
+        
+    }
+}
--- src/thunderbots/software/ai/navigator/obstacle.h
+++ src/thunderbots/software/ai/navigator/obstacle.h
@@ -2,27 +2,53 @@
 
 #include "ai/world/team.h"
 #include "ai/geom/point.h"
-// Requires geom classes.
+
 /**
- * This defines the interface for obstacle processing
+ * This defines the interface for obstacle processing.
  */
 
+constexpr double DEFAULT_AVOID_DIST = 0.15; // meters
 
+typedef struct RobotObstacle
+{
+    /**
+     * Represents the physical footprint of the robot, with
+     * an additional avoid distance.
+     */
+    Geom::Circle robot;
+
+    /**
+     * Segment extending from robot in direction of robot travel with length
+     * proportional to current velocity. Used for collision avoidance.
+     */
+    Geom::Seg velocity;
+
+} RobotObstacle;
 
 /**
- * Creates circles around the friendly team robots.
+ * Since all field 'obstacles' (ex. external boundary and defense areas) are rectangles,
+ * we can model these as rectangles.
+ */
+typedef Geom::Rect FieldBoundary;
+
+/**
+ * Creates RobotObstacles based on the friendly team robots.
  * 
  * @param friendly_team The team of friendly robots.
- * @param avoid_dist Avoid distance around robots.
+ * @param avoid_dist Avoidance distance around robots in meters.
  */
-std::vector<Geom::Circle> process_friendly(Team& friendly_team, double avoid_dist);
+std::vector<RobotObstacle> process_friendly(Team& friendly_team, double avoid_dist);
 
 /**
- * Creates circles around the enemy team robots.
+ * Creates RobotObstacles around the enemy team robots.
  * 
  * @param enemy_team The team of enemy robots.
- * @param avoid_dist Avoid distance around robots.
+ * @param avoid_dist Avoidance distance around robots in meters.
  */
-std::vector<Geom::Circle> process_enemy(Team& enemy_team, double avoid_dist);
+std::vector<RobotObstacle> process_enemy(Team& enemy_team, double avoid_dist);
+
+/**
+ * Creates FieldBoundaries for the field 'obstacles' (ex. defense areas, outer boundaries).
+ * 
 
 bool path_valid(std::vector<Point>);
