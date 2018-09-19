fba431cd71f0a0a31f09bcd5e280cadab681e104
Mathew MacDougall <mmacd97@gmail.com>
2018-09-18 23:51:11 -0700

    updated ball class to use std::chrono for timestamps, and added new updateState function

--- src/thunderbots/software/ai/world/ball.cpp
+++ src/thunderbots/software/ai/world/ball.cpp
@@ -1,18 +1,45 @@
 #include "ball.h"
 
-Ball::Ball(Point position, Vector velocity) : position_(position), velocity_(velocity)
+Ball::Ball(Point position, Vector velocity, std::chrono::time_point<std::chrono::steady_clock> last_update_timestamp) : position_(position), velocity_(velocity), last_update_timestamp(last_update_timestamp)
 {
 }
 
 void Ball::update(const Ball &new_ball_data)
 {
-    update(new_ball_data.position(), new_ball_data.velocity());
+    update(new_ball_data.position(), new_ball_data.velocity(), new_ball_data.lastUpdateTimestamp());
 }
 
-void Ball::update(const Point &new_position, const Vector &new_velocity)
+void Ball::update(const Point &new_position, const Vector &new_velocity, std::chrono::time_point<std::chrono::steady_clock> timestamp)
 {
+    if (timestamp < last_update_timestamp) {
+        // TODO: Error. We should never be updating with times from the past
+        // TODO: Throw a proper exception here
+        // https://github.com/UBC-Thunderbots/Software/issues/16
+        exit(1);
+    }
+
     position_ = new_position;
     velocity_ = new_velocity;
+    last_update_timestamp = timestamp;
+}
+
+void Ball::updateState(std::chrono::time_point<std::chrono::steady_clock> timestamp) {
+    if (timestamp < last_update_timestamp) {
+        // TODO: Error. We should never be updating with times from the past
+        // TODO: Throw a proper exception here
+        // https://github.com/UBC-Thunderbots/Software/issues/16
+        exit(1);
+    }
+
+    auto milliseconds_in_future = std::chrono::duration_cast<std::chrono::milliseconds>(timestamp - last_update_timestamp);
+    Point new_position = estimatePositionAtFutureTime(milliseconds_in_future);
+    Point new_velocity = estimateVelocityAtFutureTime(milliseconds_in_future);
+
+    update(new_position, new_velocity, timestamp);
+}
+
+std::chrono::time_point<std::chrono::steady_clock> Ball::lastUpdateTimestamp() const {
+    return last_update_timestamp;
 }
 
 Point Ball::position() const
@@ -20,12 +47,20 @@ Point Ball::position() const
     return position_;
 }
 
-Point Ball::estimatePositionAtFutureTime(double time_delta) const
+Point Ball::estimatePositionAtFutureTime(const std::chrono::milliseconds& milliseconds_in_future) const
 {
+    if (milliseconds_in_future < std::chrono::milliseconds(0)) {
+        // TODO: Error. We should never be updating with times from the past
+        // TODO: Throw a proper exception here
+        // https://github.com/UBC-Thunderbots/Software/issues/16
+    }
+
     // TODO: This is a simple linear implementation that does not necessarily reflect
     // real-world behavior. Position prediction should be improved as outlined in
     // https://github.com/UBC-Thunderbots/Software/issues/47
-    return position_ + (velocity_.norm(time_delta * velocity_.len()));
+    typedef std::chrono::duration<double> double_seconds;
+    double seconds_in_future = std::chrono::duration_cast<double_seconds>(milliseconds_in_future).count();
+    return position_ + (velocity_.norm(seconds_in_future * velocity_.len()));
 }
 
 Vector Ball::velocity() const
@@ -33,12 +68,20 @@ Vector Ball::velocity() const
     return velocity_;
 }
 
-Vector Ball::estimateVelocityAtFutureTime(double time_delta) const
+Vector Ball::estimateVelocityAtFutureTime(const std::chrono::milliseconds& milliseconds_in_future) const
 {
+    if (milliseconds_in_future < std::chrono::milliseconds(0)) {
+        // TODO: Error. We should never be updating with times from the past
+        // TODO: Throw a proper exception here
+        // https://github.com/UBC-Thunderbots/Software/issues/16
+    }
+
     // TODO: This is a implementation with an empirically determined time constant that
     // does not necessarily reflect real-world behavior. Velocity prediction should be
     // improved as outlined in https://github.com/UBC-Thunderbots/Software/issues/47
-    return velocity_ * exp(-0.1 * time_delta);
+    typedef std::chrono::duration<double> double_seconds;
+    double seconds_in_future = std::chrono::duration_cast<double_seconds>(milliseconds_in_future).count();
+    return velocity_ * exp(-0.1 * seconds_in_future);
 }
 
 bool Ball::operator==(const Ball &other) const
--- src/thunderbots/software/ai/world/ball.h
+++ src/thunderbots/software/ai/world/ball.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "geom/point.h"
+#include <chrono>
 
 class Ball final
 {
@@ -11,8 +12,10 @@ class Ball final
      * @param position The position of the ball, with coordinates in metres.
      * Default is (0, 0)
      * @param velocity The velocity of the ball, in metres per second. Default is (0, 0)
+     * @paragraph last_update_timestamp A timestamp of when this ball's information was
+     * last updated. Default is the current time.
      */
-    explicit Ball(Point position = Point(), Vector velocity = Vector());
+    explicit Ball(Point position = Point(), Vector velocity = Vector(), std::chrono::time_point<std::chrono::steady_clock> last_update_timestamp = std::chrono::steady_clock::now());
 
     /**
      * Updates the ball with new data, updating the current data as well as the predictive
@@ -20,8 +23,10 @@ class Ball final
      *
      * @param new_position the new position of the ball, defined in metres
      * @param new_velocity the new velocity of the ball, defined in metres per second
+     * @param timestamp The timestamp for the time at which this update is taking place.
+     * The timestamp must be >= the ball's latest update timestamp
      */
-    void update(const Point& new_position, const Vector& new_velocity);
+    void update(const Point& new_position, const Vector& new_velocity, std::chrono::time_point<std::chrono::steady_clock> timestamp);
 
     /**
      * Updates the ball with new data, updating the current data as well as the predictive
@@ -32,6 +37,21 @@ class Ball final
     void update(const Ball& new_ball_data);
 
     /**
+     * Updates the ball's state to match what it would be at the given timestamp. The
+     * timestamp must be >= the ball's last update timestamp.
+     *
+     * @param timestamp The timestamp at which to update the ball's state to
+     */
+    void updateState(std::chrono::time_point<std::chrono::steady_clock> timestamp);
+
+    /**
+     * Returns the timestamp for when this ball's data was last updated
+     *
+     * @return the timestamp for when this ball's data was last updated
+     */
+    std::chrono::time_point<std::chrono::steady_clock> lastUpdateTimestamp() const;
+
+    /**
      * Returns the current position of the ball
      *
      * @return the current position of the ball
@@ -39,17 +59,18 @@ class Ball final
     Point position() const;
 
     /**
-     * Returns the estimated position of the ball at a future time, relative to the
-     * current time
+     * Returns the estimated position of the ball at a future time, relative to when the
+     * ball was last updated.
      *
-     * @param time_delta The relative amount of time in the future (in seconds) at which
-     * to predict the ball's position. For example, a value of 1.5 would return the
-     * estimated position of the ball 1.5s in the future.
+     * @param milliseconds_in_future The relative amount of time in the future
+     * (in milliseconds) at which to predict the ball's position. Value must be >= 0.
+     * For example, a value of 1500 would return the estimated position of the ball
+     * 1.5 seconds in the future.
      *
-     * @return the estimated position of the ball at a future time.
-     * Coordinates are in metres.
+     * @return the estimated position of the ball at the given number of milliseconds
+     * in the future. Coordinates are in metres.
      */
-    Point estimatePositionAtFutureTime(double time_delta = 0.0) const;
+    Point estimatePositionAtFutureTime(const std::chrono::milliseconds& milliseconds_in_future) const;
 
     /**
      * Returns the current velocity of the ball
@@ -59,17 +80,18 @@ class Ball final
     Vector velocity() const;
 
     /**
-     * Returns the estimated velocity of the ball at a future time, relative to the
-     * current time
+     * Returns the estimated velocity of the ball at a future time, relative to when the
+     * ball was last updated
      *
-     * @param time_delta The relative amount of time in the future (in seconds) at which
-     * to predict the ball's velocity. For example, a value of 1.5 would return the
-     * estimated velocity of the ball 1.5s in the future.
+     * @param milliseconds_in_future The relative amount of time in the future
+     * (in milliseconds) at which to predict the ball's velocity. Value must be >= 0.
+     * For example, a value of 1500 would return the estimated velocity of the ball
+     * 1.5 seconds in the future.
      *
-     * @return the estimated velocity of the ball at a future time.
-     * Coordinates are in metres.
+     * @return the estimated velocity of the ball at the given number of milliseconds
+     * in the future. Coordinates are in metres.
      */
-    Vector estimateVelocityAtFutureTime(double time_delta = 0.0) const;
+    Vector estimateVelocityAtFutureTime(const std::chrono::milliseconds& milliseconds_in_future) const;
 
     /**
      * Defines the equality operator for a Ball. Balls are equal if their positions and
@@ -93,4 +115,6 @@ class Ball final
     Point position_;
     // The current velocity of the ball, in metres per second
     Vector velocity_;
+    // The timestamp for when this ball was last updated
+    std::chrono::time_point<std::chrono::steady_clock> last_update_timestamp;
 };
