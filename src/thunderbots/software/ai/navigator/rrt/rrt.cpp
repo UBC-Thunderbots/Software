449f9bdc8aae1049e5d03f8741e9c9d0e68ab464
Jonathan Lew <lewkewarm@outlook.com>
2018-09-27 17:42:48 -0700

    Simplest navigator, moves toward destination

--- src/thunderbots/software/ai/navigator/rrt/rrt.cpp
+++ src/thunderbots/software/ai/navigator/rrt/rrt.cpp
@@ -36,7 +36,7 @@ std::vector<std::unique_ptr<Primitive>> RRTNav::getAssignedPrimitives(
             MoveIntent move_intent = dynamic_cast<MoveIntent &>(*intent);
 
 			std::optional<Robot> r = world.friendly_team().getRobotById(move_intent.getRobotId());
-			assert(r!=std::nullopt && "the world is messed up");
+			assert(r!=std::nullopt && "The world is messed up!");
 			Point currPos = r->position();
 			Point destPos = move_intent.getDestination();
 			double angleToDest = atan2(currPos.y(), currPos.x());
@@ -44,20 +44,11 @@ std::vector<std::unique_ptr<Primitive>> RRTNav::getAssignedPrimitives(
 				continue;
 			}
 
-			Point initPoint(stepSize * cos(angleToDest), stepSize * sin(angleToDest));
-			std::optional<Point> stepPoint = findFreePoint(initPoint, angleToDest, all_obsts);
+			Point stepPoint(stepSize * cos(angleToDest), stepSize * sin(angleToDest));
 
-			std::unique_ptr<Primitive> move_prim;
-			if (stepPoint!=std::nullopt){
-				 move_prim = std::make_unique<MovePrimitive>(
-				    move_intent.getRobotId(), *stepPoint,
+			std::unique_ptr<Primitive> move_prim = std::make_unique<MovePrimitive>(
+				    move_intent.getRobotId(), stepPoint,
 				    move_intent.getFinalAngle(), move_intent.getFinalSpeed());
-			}else{
-				//no path forward
-				move_prim = std::make_unique<MovePrimitive>(
-				    move_intent.getRobotId(), currPos,
-				    move_intent.getFinalAngle(), 0);
-			}
 
 			assigned_primitives.emplace_back(std::move(move_prim));
         }
@@ -73,42 +64,3 @@ std::vector<std::unique_ptr<Primitive>> RRTNav::getAssignedPrimitives(
     return assigned_primitives;
 }
 
-std::optional<Point> RRTNav::findFreePoint(Point initPoint, double angleToDest, std::vector<RobotObstacle> obsts) const {
-	//Use simple bug pathfinding algorithm and no collision detection
-	//counter clockwise bias
-	Point stepPoint = initPoint;
-
-	//TODO: refactor into function
-	bool goodPoint = true;
-	bool cc = true;
-	double angleShift = 0;
-	Point nextStepPoint = stepPoint;
-	do{
-		goodPoint = true;
-		stepPoint = nextStepPoint;
-		//TODO: define all_obsts from friendly and enemy obsts
-		for (const auto ro: obsts){
-			if(ro.getViolationDistance(stepPoint)!=0.0){
-				goodPoint = false;
-				break;
-			}
-		}
-		angleShift += angleStep;
-		if (angleShift>180){
-			break;
-		}
-		if (cc){
-			nextStepPoint.set(stepSize * cos(angleToDest + angleShift), stepSize * sin(angleToDest + angleShift));
-			cc = false;
-		}else{
-			nextStepPoint.set(stepSize * cos(angleToDest - angleShift), stepSize * sin(angleToDest - angleShift));
-			cc = true;
-		}
-	} while (!goodPoint);
-
-	if (angleShift>180){
-		return std::nullopt;
-	}else{
-		return std::make_optional(stepPoint);
-	}
-}
