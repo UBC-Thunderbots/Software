0ebc4570508cbe99243a13d21cdb2b3c3214ad44
akhilv <akhil.veeraghanta@outlook.com>
2018-10-03 00:02:45 -0700

    #11 Dynamic Reconf: fixed namespace and callback, params register correctly

--- src/thunderbots/software/param_server/src/param_server.cpp
+++ src/thunderbots/software/param_server/src/param_server.cpp
@@ -6,16 +6,27 @@
 #include <util/parameter/parameter.h>
 #include <thunderbots/ParamsConfig.h>
 
+
+void callback(param_server::ParamsConfig &config, uint32_t level) {
+	std::cerr<<"GETTING HERE"<<std::endl;
+	//save the file here
+}
+
+
 int main(int argc, char **argv) {
     //init node
     ros::init(argc, argv, "dynamic_param");
     ros::NodeHandle node_handle;
 
-    
     //setup dynamic reconfigure server
     dynamic_reconfigure::Server<param_server::ParamsConfig> server;
-    ros::service::waitForService("/param_server/set_parameters", 1);
-    ros::ServiceClient client = node_handle.serviceClient<dynamic_reconfigure::Reconfigure>("/param_server/set_parameters");
+    dynamic_reconfigure::Server<param_server::ParamsConfig>::CallbackType f;
+
+    f = boost::bind(&callback, _1, _2);
+    server.setCallback(f);
+
+    ros::service::waitForService("/parameters/set_parameters", 1);
+    ros::ServiceClient client = node_handle.serviceClient<dynamic_reconfigure::Reconfigure>("/parameters/set_parameters");
 
     //create configuration message
     dynamic_reconfigure::Reconfigure srv;
@@ -26,14 +37,17 @@ int main(int argc, char **argv) {
     srv.request.config.doubles = Parameter<double>::getConfigStruct().doubles;
     srv.request.config.bools = Parameter<bool>::getConfigStruct().bools;
 
+    std::cerr<<srv.request.config<<std::endl;
+
     //call the service to set params
-    if(client.call(srv)){
+    ROS_INFO("Spinning node");
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+    if(client.call(srv)) {
         ROS_INFO("all parameters have been configured");
     } else {
         ROS_FATAL("paramtrs have not been configured, showing cached values");
     }
-
-    ROS_INFO("Spinning node");
-    ros::spin();
+    ros::waitForShutdown();
     return 0;
 }
--- src/thunderbots/software/util/parameter/dynamic_parameters.cpp
+++ src/thunderbots/software/util/parameter/dynamic_parameters.cpp
@@ -6,45 +6,35 @@ namespace DynamicParameters
 
     void updateAllParametersFromROSParameterServer()
     {
-	//currently supported
         Parameter<bool>::updateAllParametersFromROSParameterServer();
         Parameter<int32_t>::updateAllParametersFromROSParameterServer();
 	Parameter<double>::updateAllParametersFromROSParameterServer();
         Parameter<std::string>::updateAllParametersFromROSParameterServer();
 
-	//currently not supported
-        Parameter<std::vector<bool>>::updateAllParametersFromROSParameterServer();
-        Parameter<std::vector<int32_t>>::updateAllParametersFromROSParameterServer();
-        Parameter<std::vector<double>>::updateAllParametersFromROSParameterServer();
-        Parameter<std::vector<std::string>>::updateAllParametersFromROSParameterServer();
+	//currently not supported (possible to implement through custom dynamic reconf)
+        //Parameter<std::vector<bool>>::updateAllParametersFromROSParameterServer();
+        //Parameter<std::vector<int32_t>>::updateAllParametersFromROSParameterServer();
+        //Parameter<std::vector<double>>::updateAllParametersFromROSParameterServer();
+        //Parameter<std::vector<std::string>>::updateAllParametersFromROSParameterServer();
     }
 
     namespace Navigator
     {
         // Default avoid distance around robots.
-        Parameter<double> default_avoid_dist(kParamNs + "default_avoid_dist",
-                                             0.15);
+        Parameter<double> default_avoid_dist("default_avoid_dist", 0.15);
 
         // Scaling factor for collision avoidance.
         // TODO this is arbitrary for now; could be determined as part of
         // #23: https://github.com/UBC-Thunderbots/Software/issues/23
-        Parameter<double> collision_avoid_velocity_scale(kParamNs + "collision_avoid_velocity_scale", 
-							2.0);
+        Parameter<double> collision_avoid_velocity_scale("collision_avoid_velocity_scale", 2.0);
     }
 
     namespace Example
     {
-        
-        Parameter<bool> bl(kParamNs + "bool_example", false); //bool
-        Parameter<std::string> strng(kParamNs + "string_example", "example"); //string
-        Parameter<int> it(kParamNs + "int_example", -1); //int
-        Parameter<double> dbl(kParamNs + "double_example",12.0); //double
-
-        // Scaling factor for collision avoidance.
-        // TODO this is arbitrary for now; could be determined as part of
-        // #23: https://github.com/UBC-Thunderbots/Software/issues/23
-        Parameter<double> collision_avoid_velocity_scale(kParamNs + "collision_avoid_velocity_scale", 
-							2.0);
+        Parameter<bool> bl("bool_example", false); //bool
+        Parameter<std::string> strng("string_example", "example"); //string
+        Parameter<int> it("int_example", -1); //int
+        Parameter<double> dbl("double_example",12.0); //double
     }
 }
 
