9fce4f70b235c7d2ee1251b7999e2814a4478793
Gareth Ellis <garethellis0@users.noreply.github.com>
2018-10-11 21:23:06 -0700

    Added Javascript Formatting Checking via Clang Format (#84)

--- clang_format/fix_formatting.sh
+++ clang_format/fix_formatting.sh
@@ -37,8 +37,17 @@ while test $# -gt 0; do
             
             # Find all the files that we want to format, and pass them to
             # clang-format as arguments
-            find $CURR_DIR/../src/ -iname *.h -o -iname *.cpp -o -iname *.c \
-                -o -iname *.hpp -o -iname *.tpp \
+            find $CURR_DIR/../src/ \
+                -path "*node_modules*" -prune \
+                -o -iname *.h \
+                -o -iname *.cpp \
+                -o -iname *.c \
+                -o -iname *.hpp \
+                -o -iname *.tpp \
+                -o -iname *.tpp \
+                -o -iname *.ts \
+                -o -iname *.js \
+                -o -iname *.tsx \
                 | xargs $CURR_DIR/clang-format-$CLANG_VERSION -i -style=file
 
             shift
