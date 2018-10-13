a5794f6a5dc0670579bd3f5d38498f9ad4f5156e
Mathew MacDougall <mmacd97@gmail.com>
2018-10-12 22:50:23 -0700

    Rewrote ssl_vision_client with boost::asio. Non-blocking, so now able to shutdown the backend_input node properly

--- src/thunderbots/software/util/constants.h
+++ src/thunderbots/software/util/constants.h
@@ -17,6 +17,10 @@ namespace UTIL
         static const std::string AI_PRIMITIVES_TOPIC            = "backend/primitives";
 
         // TODO: Make this a tuneable parameter
-        const TeamColour FRIENDLY_TEAM_COLOUR = YELLOW;
+        static const TeamColour FRIENDLY_TEAM_COLOUR = YELLOW;
+
+        // Networking and vision
+        static const std::string SSL_VISION_MULTICAST_ADDRESS = "224.5.23.2";
+        static const unsigned short SSL_VISION_MULTICAST_PORT = 10020;
     }  // namespace CONSTANTS
 }  // namespace UTIL
