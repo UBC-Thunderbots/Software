4cab750554f22c6ac09d67412d0619c8d8c6949e
Evan <evan@localhost.localdomain>
2018-10-18 17:36:28 -0600

    Resolved javadoc comments for getters in move_primitive.h and removed motion controller file from backend unit test

--- src/thunderbots/software/ai/primitive/move_primitive.h
+++ src/thunderbots/software/ai/primitive/move_primitive.h
@@ -28,18 +28,53 @@ class MovePrimitive : public Primitive
      */
     explicit MovePrimitive(const thunderbots_msgs::Primitive &primtive_msg);
 
+    /**
+     * Gets the primitive name
+     *
+     * @return The name of the primitive as a string
+     */
     std::string getPrimitiveName() const;
 
+    /**
+     * Gets the robot ID
+     *
+     * @return The robot ID as an unsigned integer
+     */
     unsigned int getRobotId() const override;
 
+    /**
+     * gets the robots destination
+     *
+     * @return The robots destination as a Point(X,Y)
+     */
     Point getDestination() const;
 
+    /**
+     * Gets the robots destination orientation
+     *
+     * @return The robots final orientation as an Angle
+     */
     Angle getFinalAngle() const;
 
+    /**
+     * Gets the robots final speed in m/s
+     *
+     * @return The robots speed in m/s
+     */
     double getFinalSpeed() const;
 
+    /**
+     * Gets the parameter array of the primitive
+     *
+     * @return The parameter array of the primitive as a vector<double>
+     */
     std::vector<double> getParameterArray() const override;
 
+    /**
+     * gets the extra bit array of the primitive
+     *
+     * @return The extra bit array as a vector<bool>
+     */
     std::vector<bool> getExtraBitArray() const override;
 
    private:
