459101c12bd30b8381043d28de3cec24cf0ff15d
Quentin Golsteyn <qgolsteyn@gmail.com>
2018-10-28 10:41:18 -0700

    Add comment for checksum comparison script

--- src/corner_kick/scripts/compare_src.sh
+++ src/corner_kick/scripts/compare_src.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
+#
+# @author Quentin Golsteyn
+#
+# This script checks if the src folder has been modified by comparing the current
+# MD5 checksum of the directory with the one stored from the last run of this script.
+#
+# If it has changed, or there are no previous value, the script returns with an error,
+# allowing you to act on it. The script will also store the new value.
+
+
+# Location to save the checksum value
 CACHE=".checksum"
 
+# Location of the src directory
+SRC="src"
+
+# File to include in the md5 checksum generation.
+# By default, we include all files.
+FILETYPE="*.*"
+
+# Check if checksum file exists.
 if [ ! -f $CACHE ]; then
-    find src -type f -name "*.*" -exec md5sum {} + | awk '{print $1}' | sort | md5sum > $CACHE
+    # File does not exist, assume src is dirty and save the checksum.
+    find "$SRC" -type f -name "$FILETYPE" -exec md5sum {} + | awk '{print $1}' | sort | md5sum > $CACHE
     exit 1
 else 
+    # File exists, get the old value and generate the new value.
     OLDCHECKSUM=$(<$CACHE)
-    NEWCHECKSUM=`find src -type f -name "*.*" -exec md5sum {} + | awk '{print $1}' | sort | md5sum`:0:31
+    NEWCHECKSUM=$(find "$SRC" -type f -name "$FILETYPE" -exec md5sum {} + | awk '{print $1}' | sort | md5sum)
 
+    # The actual checksum is 32 characters long, 
+    # so only get that portion of the string.
     OLDCHECKSUM=${OLDCHECKSUM:0:31}
     NEWCHECKSUM=${NEWCHECKSUM:0:31}
 
+    # Compare the two checksum
     if [ "$OLDCHECKSUM" == "$NEWCHECKSUM" ]
     then
-    exit 0
+        # They are the same! No need to do anything.
+        exit 0
     else
-    echo $NEWCHECKSUM > $CACHE
-    exit 1
+        # They are different, save the new checksum and return an error.
+        echo $NEWCHECKSUM > $CACHE
+        exit 1
     fi
 fi
