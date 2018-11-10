93bc3cf6a2d036040a30121302ae4c332e61da7c
Evan <evan@localhost.localdomain>
2018-11-10 16:34:23 -0700

    Fixed typo

--- src/thunderbots/software/ai/primitive/move_primitive.h
+++ src/thunderbots/software/ai/primitive/move_primitive.h
@@ -31,7 +31,7 @@ class MovePrimitive : public Primitive
      *
      * @param primtiive_msg The message from which to create the Move Primitive
      */
-    explicit MovePrimitive(const thunderbots_msgs::Primitive &primtive_msg);
+    explicit MovePrimitive(const thunderbots_msgs::Primitive &primitive_msg);
 
     std::string getPrimitiveName() const override;
 
