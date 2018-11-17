499b052cabb43e5d22a9cca351b836da4aaf6091
Matthew Berends <matthewberends97@gmail.com>
2018-11-17 12:53:32 -0800

    Clarify use of margin parameter is catch primitive.

--- src/thunderbots/software/ai/primitive/catch_primative.h
+++ src/thunderbots/software/ai/primitive/catch_primative.h
@@ -19,8 +19,9 @@ class CatchPrimitive : public Primitive
      * @param velocity Velocity to move robot forwards/backwards
      * to catch the ball without it bouncing off the dribbler; units are m/s
      * @param dribbler_speed Speed to rotate dribbler at. Units are RPM.
-     * @param margin The margin of allowed error in the robot catch velocity.
-     * The exact use of this parameter needs to be confirmed.
+     * @param margin A scaling factor for how far in front of the ball to make the point
+     * of intercept. It scales based on the difference in velocity between the
+     * robot and the ball.
      */
     explicit CatchPrimitive(unsigned int robot_id, double velocity, double dribbler_speed,
                             double margin);
