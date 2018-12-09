eb27f4840ba2a549c769f4de2adcd840ef031888
Quentin Golsteyn <qgolsteyn@gmail.com>
2018-12-08 16:21:53 -0800

    Visualizer/Support for Field (#110)
    * Begin development on the visualizer
    
    This is the first commit for the visualizer. It adds the foundation for
    the application, namely the electron app and an updated environment
    setups script. At this point, the electron app can connect to the ROS
    bridge.
    
    * Add visualizer component
    
    * Implement working example for visualization
    
    * Add logger
    
    * Finish first MVP submission
    
    * Add catkin_make build system and roslaunch capability
    
    * Add npm as rosdep dependency
    
    * Add node and yarn to setup script
    
    * Save window position
    
    * Add React compatible persist
    
    * Fix build and add field topic visualization
    
    * Add linting and code styling options
    
    * Rename to corner_kick
    
    * Lock dependency versions
    
    * Move .gitignore file
    
    * Add checksum to speed up build
    
    * Add comment for checksum comparison script
    
    * Implement code review suggestions
    
    * Remove rosdep explanation in README
    
    * Revert "Added Javascript Formatting Checking via Clang Format (#84)"
    
    This reverts commit 9fce4f70b235c7d2ee1251b7999e2814a4478793.
    
    * Bug fix: incorrect rendering of Field
    
    * Improve README styling
    
    * Disable clang Javascript support
    
    * Remove clang formating for unrecognized files
    
    * Add .clang-format-ignore
    
    * Modify extensions for git_clang_format
    
    * Merge branch 'master' of https://github.com/qgolsteyn/Software
    
    * Trying to fix clang-format extensions
    
    * Revert "Add .clang-format-ignore"
    
    This reverts commit 1f3946b2fd4f740958da0c12908d513cfd305a92.
    
    * Revert "Remove clang formating for unrecognized files"
    
    This reverts commit 2316e314dd1d483d2663b320af7636c6c409a1d1.
    
    * Apply code review fixes
    
    * Add common extension list to fix_formatting script
    
    * Add comments to fix_formatting.sh

---
+++ src/corner_kick/module.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @fileoverview This definition file defines third-party libraries that
+ * do not include any typescript definitions.
+ */
+declare module 'roslib';
+declare module 'react-panelgroup';
