2833d72298cd412d4ccdfd07adc2bfc9aafc0e5a
Gareth Ellis <gareth.ellis0@gmail.com>
2018-12-19 01:03:40 -0700

    Resolved Rebase Conflicts

--- src/thunderbots/software/ai/primitive/primitive.cpp
+++ src/thunderbots/software/ai/primitive/primitive.cpp
@@ -1,10 +1,9 @@
-#include "ai/primitive/primitive.h"
-
 #include "ai/primitive/catch_primitive.h"
 #include "ai/primitive/chip_primitive.h"
 #include "ai/primitive/direct_velocity_primitive.h"
 #include "ai/primitive/kick_primitive.h"
 #include "ai/primitive/move_primitive.h"
+#include "ai/primitive/primitive.h"
 
 thunderbots_msgs::Primitive Primitive::createMsg() const
 {
