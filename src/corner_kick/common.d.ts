88537447e418c940c5c4d9864e6eddf145200cd4
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-01-12 10:07:57 -0800

    Add build folder clean when compiling

--- src/corner_kick/common.d.ts
+++ src/corner_kick/common.d.ts
@@ -1,1 +1,8 @@
+/**
+ * This file defines supported file extensions
+ */
+
+declare module '*.css';
+declare module '*.png';
+declare module '*.jpg';
 declare module '*.svg';
--- src/corner_kick/config/webpack.prod.js
+++ src/corner_kick/config/webpack.prod.js
@@ -5,9 +5,20 @@
  * @see {@link ./webpack.config.js}
  */
 
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 const webpackConfig = require('./webpack.base.config.js');
 
 module.exports = {
     ...webpackConfig[0],
     mode: 'production',
+    plugins: [
+        ...webpackConfig[0].plugins,
+        // Removes the build folder
+        new CleanWebpackPlugin(path.resolve(__dirname, '../build'), {
+            allowExternal: true,
+            verbose: false,
+        }),
+    ],
 };
