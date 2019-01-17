1f59d7192b3c94c3b2b738a04971251c65cf2441
Muchen He <i@muchen.ca>
2019-01-16 16:53:54 -0800

    Minor cleanup

--- src/thunderbots/software/ai/primitive/primitive.cpp
+++ src/thunderbots/software/ai/primitive/primitive.cpp
@@ -14,17 +14,9 @@ thunderbots_msgs::Primitive Primitive::createMsg() const
     primitive_msg.primitive_name = getPrimitiveName();
     primitive_msg.robot_id       = getRobotId();
     primitive_msg.parameters     = getParameters();
-
-
-    // Boolean arrays can't be directly assigned, so we need
-    // to use a loop
-    // for (auto data : getExtraBits())
-    // {
-    //     primitive_msg.extra_bits.emplace_back(data);
-    // }
     primitive_msg.extra_bits = getExtraBits();
 
-        return primitive_msg;
+    return primitive_msg;
 }
 
 std::unique_ptr<Primitive> Primitive::createPrimitive(
