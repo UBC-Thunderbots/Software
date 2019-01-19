33cfb7362f42c0630933662027657ec9afbb8782
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-01-18 17:33:17 -0800

    Modify redux action

--- src/corner_kick/src/store/actions/ros.ts
+++ src/corner_kick/src/store/actions/ros.ts
@@ -7,27 +7,27 @@ import { createAction } from 'typesafe-actions';
 /**
  * Connect to ROS
  */
-export const start = createAction('ros/START');
+export const start = createAction('ros_START');
 
 /**
  * Disconnect from ROS
  */
-export const stop = createAction('ros/STOP');
+export const stop = createAction('ros_STOP');
 
 /**
  * Sent when we are connected to ROS
  */
-export const connected = createAction('ros/CONNECTED');
+export const connected = createAction('ros_CONNECTED');
 
 /**
  * Sent when are disconnected from ROS
  */
-export const disconnected = createAction('ros/DISCONNECTED');
+export const disconnected = createAction('ros_DISCONNECTED');
 
 /**
  * Sent when there is an error connecting to ROS
  */
-export const error = createAction('ros/ERROR', (resolve) => {
+export const error = createAction('ros_ERROR', (resolve) => {
     return (errorMessage: string) => resolve({ errorMessage });
 });
 
@@ -35,48 +35,48 @@ export const error = createAction('ros/ERROR', (resolve) => {
  * Subscribe to a particular topic. Messages will be available through the
  * `ros/NEW_MESSAGE` action.
  */
-export const subscribeTopic = createAction('ros/SUBSCRIBE_TOPIC', (resolve) => {
+export const subscribeTopic = createAction('ros_SUBSCRIBE_TOPIC', (resolve) => {
     return (topic: string, messageType: string) => resolve({ topic, messageType });
 });
 
 /**
  * Unsubscribes from a particular topic.
  */
-export const unsubscribeTopic = createAction('ros/UNSUBSCRIBE_TOPIC', (resolve) => {
+export const unsubscribeTopic = createAction('ros_UNSUBSCRIBE_TOPIC', (resolve) => {
     return (topic: string, messageType: string) => resolve({ topic, messageType });
 });
 
 /**
  * Sent when a new message is received from a particular topic
  */
-export const newMessage = createAction('ros/NEW_MESSAGE', (resolve) => {
+export const newMessage = createAction('ros_NEW_MESSAGE', (resolve) => {
     return (topic: string, message: any) => resolve({ topic, message });
 });
 
 /**
  * Sent when we receive the nodes available in ROS
  */
-export const setNodes = createAction('ros/SET_NODES', (resolve) => {
+export const setNodes = createAction('ros_SET_NODES', (resolve) => {
     return (nodes: string[]) => resolve({ nodes });
 });
 
 /**
  * Sent when we receive the topics available in ROS
  */
-export const setTopics = createAction('ros/SET_TOPICS', (resolve) => {
+export const setTopics = createAction('ros_SET_TOPICS', (resolve) => {
     return (topics: string[]) => resolve({ topics });
 });
 
 /**
  * Sent when we receive the services available in ROS
  */
-export const setServices = createAction('ros/SET_SERVICES', (resolve) => {
+export const setServices = createAction('ros_SET_SERVICES', (resolve) => {
     return (services: string[]) => resolve({ services });
 });
 
 /**
  * Sent when we receive the params available in ROS
  */
-export const setParams = createAction('ros/SET_PARAMS', (resolve) => {
+export const setParams = createAction('ros_SET_PARAMS', (resolve) => {
     return (params: string[]) => resolve({ params });
 });
--- src/corner_kick/src/store/actions/settings.ts
+++ src/corner_kick/src/store/actions/settings.ts
@@ -9,13 +9,13 @@ import { ISettingsState } from 'SRC/types';
 /**
  * Adds settings from local storage to the state
  */
-export const hydrateSettings = createAction('settings/HYDRATE', (resolve) => {
+export const hydrateSettings = createAction('settings_HYDRATE', (resolve) => {
     return (settings: ISettingsState) => resolve({ settings });
 });
 
 /**
  * Updates a settings entry
  */
-export const set = createAction('settings/SET', (resolve) => {
+export const set = createAction('settings_SET', (resolve) => {
     return (id: string, value: string) => resolve({ id, value });
 });
