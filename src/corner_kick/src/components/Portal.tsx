19d86159b8b1333ed55a132ebed4d3e1bc9c26c9
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-01-22 15:53:58 -0800

    Documenting Portal

--- src/corner_kick/src/components/Portal.tsx
+++ src/corner_kick/src/components/Portal.tsx
@@ -1,4 +1,4 @@
-/**
+/*
  * This file defines a React component which adds other elements
  * in the main area of the application
  */
@@ -7,6 +7,20 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 interface IPortalProps {
+    /**
+     * The area where we wish to portal content to
+     *
+     * These areas include:
+     * -   **main:** Contains the primary focus of the page
+     * -   **sidebarTitle:** Contains the page title and additional options
+     *     specific to the page
+     * -   **sidebar:** Contains additional controls or information relevant
+     *     to the current page
+     * -   **sidebarControl:** Contains navigation controls to switch between pages
+     * -   **console:** Contains views visible across the application
+     * -   **footer(Left|Right):** Contains simple breadcrumbs of information
+     *     relevant to the application as a whole or to the current page
+     */
     to:
         | 'main'
         | 'sidebar'
@@ -16,6 +30,10 @@ interface IPortalProps {
         | 'mainTitle'
         | 'footerRight'
         | 'footerLeft';
+
+    /**
+     * The content to portal
+     */
     children: React.ReactNode | React.ReactNodeArray;
 }
 
