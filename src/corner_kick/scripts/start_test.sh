3165dadd0d22e40913d8216cf9d579d0f252f6eb
kelvinkoon <kkoon@shaw.ca>
2019-01-26 13:08:08 -0800

    Fix merge conflict

--- src/corner_kick/scripts/start_test.sh
+++ src/corner_kick/scripts/start_test.sh
@@ -7,8 +7,4 @@ DIRECTORY=`dirname $0`
 # Move to this directory and start the test suite of
 # Corner Kick.
 cd $DIRECTORY
-<<<<<<< HEAD
 yarn test:coverage
-=======
-yarn test
->>>>>>> 02caab567bd108dc95226d495103476411087fbe
