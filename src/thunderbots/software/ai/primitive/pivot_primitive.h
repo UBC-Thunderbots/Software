d2b5ca82e3460f99bc5b32d9d0de1d0977c2b009
bryanlogue <43693387+bryanlogue@users.noreply.github.com>
2019-01-25 22:32:39 -0800

    Correct Pivot Primitive to include pivot_radius instead of robot_orientation (#247)
    * Corrected Pivot primitive to match firmware; robot_orientation param changed to pivot_radius. Builds but tests not updated.
    
    * Updated tests after updating pivot primitive; tests are passing.
    
    * ran formatting script.

--- src/thunderbots/software/ai/primitive/pivot_primitive.h
+++ src/thunderbots/software/ai/primitive/pivot_primitive.h
@@ -14,17 +14,16 @@ class PivotPrimitive : public Primitive
      * Pivots the robot around the specified point, maintaining a constant
      * distance from this point.
      * 	   The robot will pivot in the direction of the shortest path
-     * 	   The robot will always face the point around which it pivots
+     * 	   Assume robot always faces the point around which it pivots
      *
      *
-     * @param robot_id          The id of the robot to run this primitive
-     * @param pivot_point       The point around which the robot will pivot
-     * @param final_angle       Global angle from rotation point to robot (in radians)
-     * @param robot_orientation The orientation of robot (facing direction)
-     *                          during pivot (radians; Not used)
+     * @param robot_id      The id of the robot to run this primitive
+     * @param pivot_point   The point around which the robot will pivot
+     * @param final_angle   Global angle from rotation point to robot (in radians)
+     * @param pivot_radius  The distance from robot to pivot_point during movement
      */
     explicit PivotPrimitive(unsigned int robot_id, const Point &pivot_point,
-                            const Angle &final_angle, const Angle &robot_orientation);
+                            const Angle &final_angle, const double pivot_radius);
 
     /**
      * Create a new Pivot Primitive from a Primitive message
@@ -54,16 +53,16 @@ class PivotPrimitive : public Primitive
     /**
      * Get the robot's final orientation
      *
-     * @return the robot's orientation after a pivot as an Angle
+     * @return the radius the robot maintains during pivot (as double)
      */
-    Angle getRobotOrientation() const;
+    double getPivotRadius() const;
 
     /**
      * Returns the generic vector of parameters for this Primitive
      *
      * @return A vector of the form {pivot_point.x(), pivot_point.y(),
      *                               final_angle.toRadians(),
-     *                               robot_orientation.toRadians()}
+     *                               pivot_radius}
      */
     std::vector<double> getParameters() const override;
 
@@ -80,5 +79,5 @@ class PivotPrimitive : public Primitive
     unsigned int robot_id;
     Point pivot_point;
     Angle final_angle;
-    Angle robot_orientation;
+    double pivot_radius;
 };
