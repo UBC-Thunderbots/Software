f6a4e873d6989194faa0b51c26c6cf9f2057d3a9
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-01-26 23:26:11 -0800

    Fix

--- src/corner_kick/scripts/start_test.sh
+++ src/corner_kick/scripts/start_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Start test script for Corner Kick.
 
+PERCY_TOKEN=369660795fa79afd24f25e21a4f8803e9d56beae94a31fbd4024ad34cf1c8cd1
+
 # Get the current directory of the script.
 DIRECTORY=`dirname $0`
 
