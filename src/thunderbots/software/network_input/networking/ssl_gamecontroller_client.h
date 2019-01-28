d73827de575cbb3d941577394a731c0b3921d1f7
Jordan Liu <jordan.zliu@gmail.com>
2019-01-28 12:22:18 -0800

    fix comments in ssl_gamecontroller_client.h

--- src/thunderbots/software/network_input/networking/ssl_gamecontroller_client.h
+++ src/thunderbots/software/network_input/networking/ssl_gamecontroller_client.h
@@ -10,20 +10,20 @@ class SSLGameControllerClient
 {
    public:
     /**
-     * Creates an SSLVisionClient that will listen for data packets from SSL Vision
+     * Creates an SSLGameControllerClient that will listen for data packets from SSL Game Controller
      * on the given address and port
      *
      * @param ip_address The ip address of the multicast group on which to listen for
      * SSL Vision packets
-     * @param port The port on which to listen for SSL Vision packets
+     * @param port The port on which to listen for SSL Game Controller packets
      */
     SSLGameControllerClient(std::string ip_address, unsigned short port);
 
     /**
-     * Returns a unique_ptr to the latest SSL Vision WrapperPacket. If no new data was
+     * Returns a unique_ptr to the latest SSL GameController Referee packet. If no new data was
      * received since the last time this function was called, the unique_ptr will be empty
      *
-     * @return A unique_ptr to an SSL_WrapperPacket if data was received since the last
+     * @return A unique_ptr to an Referee packet if data was received since the last
      * time this function was called, otherwise returns an empty unique_ptr
      */
     const std::unique_ptr<Referee> getGameControllerPacket();
@@ -43,9 +43,7 @@ class SSLGameControllerClient
     boost::asio::ip::udp::socket socket_;
     boost::asio::ip::udp::endpoint sender_endpoint_;
 
-    // The actual length (in bytes) of the ssl_wrapper packets is around 263 bytes
-    // (determined empirically by printing the size of the received packet). We set the
-    // max buffer length to be the largest possible size of a UDP datagram. This way
+    // We set the max buffer length to be the largest possible size of a UDP datagram. This way
     // we don't need to worry about buffer overflow, or about our data getting truncated
     // to fit into the buffer
     static constexpr unsigned int max_buffer_length = 65535;
