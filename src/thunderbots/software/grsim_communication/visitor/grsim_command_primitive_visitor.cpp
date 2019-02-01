1d4e0dd5dc5c97440d1617cca35713a6a3ee6de9
Bryan <logue.bryan@gmail.com>
2019-01-31 17:39:46 -0800

    modified condition for updating robot expected destination; still testing

--- src/thunderbots/software/grsim_communication/visitor/grsim_command_primitive_visitor.cpp
+++ src/thunderbots/software/grsim_communication/visitor/grsim_command_primitive_visitor.cpp
@@ -113,7 +113,7 @@ void GrsimCommandPrimitiveVisitor::visit(const PivotPrimitive &pivot_primitive)
     double expected_distance =
         assumed_t_step_seconds *
                     (robot_speed < 0.05 * ROBOT_MAX_SPEED_METERS_PER_SECOND) &&
-                (rotation_remaining > stop_angle)
+                (rotation_remaining.abs() > stop_angle/10) // todo: make this a variable
             ? robot_speed + assumed_t_step_seconds *
                                 ROBOT_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED
             : robot_speed;
