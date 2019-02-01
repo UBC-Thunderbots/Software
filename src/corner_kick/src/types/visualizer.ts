397be6a591871da3904373dbcab8dd87b6cd0e93
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-01-31 22:32:16 -0800

    Misc changes

--- src/corner_kick/src/types/visualizer.ts
+++ src/corner_kick/src/types/visualizer.ts
@@ -1,9 +1,22 @@
-/**
+/*
  * This file describes a layer datatype
  */
 
-export interface ILayer {
-    id: string;
+export interface ILayerMessage {
     name: string;
+    shapes: IShape[];
+}
+
+export interface ILayer extends ILayerMessage {
     visible: boolean;
+}
+
+export interface IShape {
+    type: string;
+    data: number[];
+    fill?: string;
+    stroke?: string;
+    stroke_weight?: number;
+    transform_rotation?: number;
+    transform_scale?: number;
 }
