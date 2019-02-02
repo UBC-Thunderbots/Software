a9a7630f7bf9cf7c141c2cf2133a13434d393601
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-02-01 23:15:12 -0800

    Update console types

--- src/corner_kick/src/store/reducers/__tests__/console.test.ts
+++ src/corner_kick/src/store/reducers/__tests__/console.test.ts
@@ -7,21 +7,31 @@ import { IMessagesState } from 'SRC/types';
 import * as ros from '../../actions/ros';
 import consoleReducer from '../console';
 
+const mockMessage = {
+    file: 'protocol.py',
+    function: 'RosbridgeProtocol.log',
+    level: 2,
+    line: 388,
+    msg: '[Client 2] Subscribed to /rosout',
+    name: '/rosbridge_websocket',
+    topics: ['/rosout'],
+};
+
 describe('console reducer', () => {
     describe('when we receive action ros_NEW_MESSAGE', () => {
         it('should push messages to the state', () => {
-            const mockAction = ros.newMessage('/rosout', 'testMessage');
+            const mockAction = ros.newMessage('/rosout', mockMessage);
 
             const state = consoleReducer(undefined, mockAction);
 
-            expect(state.messages).toEqual(['testMessage']);
+            expect(state.messages).toEqual([mockMessage]);
         });
     });
     describe('when we receive other actions', () => {
         it('should return state from action payload', () => {
             const mockAction = ros.connected();
             const mockState: IMessagesState = {
-                messages: ['testMessage1', 'testMessage2'],
+                messages: [mockMessage],
             };
 
             const state = consoleReducer(mockState, mockAction);
