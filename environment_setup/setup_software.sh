ec6bbcf6d959790031cc65433388c6fa6ca4db17
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-02-02 17:32:17 -0800

    Test

--- environment_setup/setup_software.sh
+++ environment_setup/setup_software.sh
@@ -205,6 +205,7 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update
 
 host_software_packages=(
+    g++-7
     python-rosinstall
     protobuf-compiler
     libprotobuf-dev
