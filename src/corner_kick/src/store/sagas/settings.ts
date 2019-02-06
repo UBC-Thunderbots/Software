174bbb0ea927fd5b5cce2e19730ee3f8f63a9a3a
kelvinkoon <kkoon@shaw.ca>
2019-02-05 23:21:18 -0800

    Begin implementing settings saga

--- src/corner_kick/src/store/sagas/settings.ts
+++ src/corner_kick/src/store/sagas/settings.ts
@@ -1,8 +1,7 @@
 /*
  * This file specifies the saga for Settings
  */
-import { put, spawn, takeLatest } from 'redux-saga/effects';
-import { getType } from 'typesafe-actions';
+import { put, spawn, takeEvery } from 'redux-saga/effects';
 
 import * as settings from '../actions/settings';
 
@@ -11,12 +10,24 @@ export default function* init() {
 }
 
 /**
- * We hydrate settings upon starting
+ * Hydrate settings upon starting
  */
-function startSettings() {
-    // Use localStorage.getItem
+function* startSettings() {
+    // Retrieve settings from local storage
+    const initializedSettingsBuffer = localStorage.getItem('settings');
+    if (initializedSettingsBuffer != null) {
+        const initializedSettings = JSON.parse(initializedSettingsBuffer);
+        // Put hydrateSettings action from item
+        yield put(settings.hydrateSettings(initializedSettings));
+    }
+    // Listen for updateSettings
+    yield takeEvery('UPDATE_SETTINGS', updateSettings);
 }
 
+/**
+ * Update settings by putting into local storage
+ */
 function updateSettings() {
     // Use localStorage.setItem
+    // localStorage.setItem('settings', JSON.stringify(RETRIEVED_SETTINGS))
 }
