3d47d8cd928fcf7342d8c5903bf03e166ad53312
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-02-09 12:55:54 -0800

    Update action tests

--- src/corner_kick/src/store/actions/__tests__/console.test.ts
+++ src/corner_kick/src/store/actions/__tests__/console.test.ts
@@ -3,20 +3,11 @@
  * Each test case has a description of what it tests
  */
 
-import configureMockStore from 'redux-mock-store';
-
 import { IRosoutMessage } from 'SRC/types';
 
 import * as consoleActions from '../console';
 
-const mockStore = configureMockStore();
-const store = mockStore();
-
 describe('consoleActions', () => {
-    beforeEach(() => {
-        store.clearActions();
-    });
-
     describe('when we receive a new /rosout message', () => {
         test('Dispatches the new /rosout message', () => {
             const mockMessage: IRosoutMessage = {
@@ -28,19 +19,7 @@ describe('consoleActions', () => {
                 name: 'Test',
                 topics: ['testTopic'],
             };
-
-            const expectedActions = [
-                {
-                    meta: undefined,
-                    payload: {
-                        message: mockMessage,
-                    },
-                    type: 'console_NEW_ROSOUT',
-                },
-            ];
-
-            store.dispatch(consoleActions.newRosoutMessage(mockMessage));
-            expect(store.getActions()).toEqual(expectedActions);
+            expect(consoleActions.newRosoutMessage(mockMessage)).toMatchSnapshot();
         });
     });
 });
--- src/corner_kick/src/store/actions/__tests__/ros.test.ts
+++ src/corner_kick/src/store/actions/__tests__/ros.test.ts
@@ -3,86 +3,36 @@
  * Each test case has a description of what it tests
  */
 
-import configureMockStore from 'redux-mock-store';
 import * as rosActions from '../ros';
 
-const mockStore = configureMockStore();
-const store = mockStore();
-
 describe('rosActions', () => {
-    beforeEach(() => {
-        store.clearActions();
-    });
-
     describe('when we connect to ROS', () => {
-        test('Dispatches the correct start action and payload', () => {
-            const expectedActions = [
-                {
-                    payload: undefined,
-                    type: 'ros_START',
-                },
-            ];
-
-            store.dispatch(rosActions.start());
-            expect(store.getActions()).toEqual(expectedActions);
+        it('dispatches the correct start action and payload', () => {
+            expect(rosActions.start()).toMatchSnapshot();
         });
     });
 
     describe('when we disconnect to ROS', () => {
-        test('Dispatches the correct stop action and payload', () => {
-            const expectedActions = [
-                {
-                    payload: undefined,
-                    type: 'ros_STOP',
-                },
-            ];
-
-            store.dispatch(rosActions.stop());
-            expect(store.getActions()).toEqual(expectedActions);
+        it('dispatches the correct stop action and payload', () => {
+            expect(rosActions.stop()).toMatchSnapshot();
         });
     });
 
     describe('when we are connected to ROS', () => {
-        test('Dispatches the correct connect action and payload', () => {
-            const expectedActions = [
-                {
-                    payload: undefined,
-                    type: 'ros_CONNECTED',
-                },
-            ];
-
-            store.dispatch(rosActions.connected());
-            expect(store.getActions()).toEqual(expectedActions);
+        it('dispatches the correct connect action and payload', () => {
+            expect(rosActions.connected()).toMatchSnapshot();
         });
     });
 
     describe('when we are disconnected to ROS', () => {
-        test('Dispatches the correct disconnect action and payload', () => {
-            const expectedActions = [
-                {
-                    payload: undefined,
-                    type: 'ros_DISCONNECTED',
-                },
-            ];
-
-            store.dispatch(rosActions.disconnected());
-            expect(store.getActions()).toEqual(expectedActions);
+        it('dispatches the correct disconnect action and payload', () => {
+            expect(rosActions.disconnected()).toMatchSnapshot();
         });
     });
 
     describe('when there is an error connecting to ROS', () => {
-        test('Dispatches the correct error action and payload', () => {
-            const expectedActions = [
-                {
-                    payload: {
-                        errorMessage: 'Error getting pan-tilt limit',
-                    },
-                    type: 'ros_ERROR',
-                },
-            ];
-
-            store.dispatch(rosActions.error('Error getting pan-tilt limit'));
-            expect(store.getActions()).toEqual(expectedActions);
+        it('dispatches the correct error action and payload', () => {
+            expect(rosActions.error('Error getting pan-tilt limit')).toMatchSnapshot();
         });
     });
 });
