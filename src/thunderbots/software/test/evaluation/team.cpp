b72e0f454615448af5d961a343e7cc60949d715d
Matthew Berends <matthewberends97@gmail.com>
2019-02-15 14:36:20 -0800

    Add tests for nearest friendly evaluation function

--- src/thunderbots/software/test/evaluation/team.cpp
+++ src/thunderbots/software/test/evaluation/team.cpp
@@ -23,13 +23,84 @@ TEST_F(EvaluationTeamTest, one_robot)
     Robot robot_0 = Robot(0, Point(0, 1), Vector(), Angle::half(),
                           AngularVelocity::threeQuarter(), current_time);
 
-    Robot robot_1 = Robot(1, Point(3, -1), Vector(), Angle::zero(),
+
+    team.updateRobots({robot_0});
+
+    EXPECT_EQ(robot_0, Evaluation::nearest_friendly(team, Point(0,0)));
+}
+
+TEST_F(EvaluationTeamTest, multiple_robots)
+{
+    Team team = Team(Duration::fromMilliseconds(1000));
+
+    Robot robot_0 = Robot(0, Point(3, -1), Vector(), Angle::half(),
+                          AngularVelocity::threeQuarter(), current_time);
+
+    Robot robot_1 = Robot(1, Point(1, 0), Vector(), Angle::zero(),
                           AngularVelocity::zero(), current_time);
 
-    Robot robot_2 = Robot(2, Point(), Vector(-0.5, 4), Angle::quarter(),
-                          AngularVelocity::half(), current_time);
+    Robot robot_2 = Robot(2, Point(4, 6), Vector(), Angle::zero(),
+                          AngularVelocity::zero(), current_time);
 
-    team.updateRobots({robot_0, robot_1});
+
+    team.updateRobots({robot_0, robot_1, robot_2});
+
+    EXPECT_EQ(robot_1, Evaluation::nearest_friendly(team, Point(0,0)));
+}
+
+TEST_F(EvaluationTeamTest, multiple_robots_closest_is_moving)
+{
+    Team team = Team(Duration::fromMilliseconds(1000));
+
+    Robot robot_0 = Robot(0, Point(3, -1), Vector(), Angle::half(),
+                          AngularVelocity::threeQuarter(), current_time);
+
+    Robot robot_1 = Robot(1, Point(1, 0), Vector(3, 3), Angle::zero(),
+                          AngularVelocity::zero(), current_time);
+
+    Robot robot_2 = Robot(2, Point(4, 6), Vector(), Angle::zero(),
+                          AngularVelocity::zero(), current_time);
+
+
+    team.updateRobots({robot_0, robot_1, robot_2});
+
+    EXPECT_EQ(robot_1, Evaluation::nearest_friendly(team, Point(0,0)));
+}
+
+TEST_F(EvaluationTeamTest, multiple_robots_all_moving)
+{
+    Team team = Team(Duration::fromMilliseconds(1000));
+
+    Robot robot_0 = Robot(0, Point(3, -1), Vector(3, 3), Angle::half(),
+                          AngularVelocity::threeQuarter(), current_time);
+
+    Robot robot_1 = Robot(1, Point(4, 6), Vector(3, 3), Angle::zero(),
+                          AngularVelocity::zero(), current_time);
+
+    Robot robot_2 = Robot(2, Point(0, 1), Vector(3, 3), Angle::zero(),
+                          AngularVelocity::zero(), current_time);
+
+
+    team.updateRobots({robot_0, robot_1, robot_2});
+
+    EXPECT_EQ(robot_2, Evaluation::nearest_friendly(team, Point(0,0)));
+}
+
+TEST_F(EvaluationTeamTest, one_robot_on_ball)
+{
+    Team team = Team(Duration::fromMilliseconds(1000));
+
+    Robot robot_0 = Robot(0, Point(0, 0), Vector(), Angle::half(),
+                          AngularVelocity::threeQuarter(), current_time);
+
+    Robot robot_1 = Robot(1, Point(1, 0), Vector(), Angle::zero(),
+                          AngularVelocity::zero(), current_time);
+
+    Robot robot_2 = Robot(2, Point(4, 6), Vector(), Angle::zero(),
+                          AngularVelocity::zero(), current_time);
+
+
+    team.updateRobots({robot_0, robot_1, robot_2});
 
     EXPECT_EQ(robot_0, Evaluation::nearest_friendly(team, Point(0,0)));
 }
