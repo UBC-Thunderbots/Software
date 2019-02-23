c5c7ca0b86accfcfe63a0d36b673ba3ff8ac09b2
Jessica He <jessicahe21@hotmail.com>
2019-02-23 12:08:36 -0800

    What is going on

--- src/thunderbots/software/test/ai/evaluation/indirect_chip.cpp
+++ src/thunderbots/software/test/ai/evaluation/indirect_chip.cpp
@@ -28,13 +28,12 @@ TEST(GetTriangleCenterAndAreaTest, get_triangle_center_and_area_test)
 
 TEST(GetLargestTriangleTest, get_largest_triangle_test)
 {
-    Triangle triangle1 = {Point(-0.5, -0.5), Point(0, 0.5), Point(0.5, -0.5)};
-    Triangle triangle2 = {Point(-1, -1), Point(0, sqrt(0.75)), Point(1, -1)};
-
     std::vector<Triangle> allTriangles;
 
-    allTriangles.push_back(triangle1);
-    allTriangles.push_back(triangle2);
+    Triangle triangle1 = {Point(-0.5, -0.5), Point(0, 0.5), Point(0.5, -0.5)};
+    Triangle triangle2 = {Point(-1, -1), Point(0, sqrt(0.75)), Point(1, -1)};
+    allTriangles.emplace_back(triangle1);
+    allTriangles.emplace_back(triangle2);
 
     Triangle largest = triangle2;
     bool valid       = true;
@@ -43,6 +42,74 @@ TEST(GetLargestTriangleTest, get_largest_triangle_test)
     EXPECT_EQ(test_pair, Evaluation::get_largest_triangle(allTriangles, 0, 0, 0));
 }
 
+TEST(FilterOpenTrianglesTest, filter_open_triangles_test)
+{
+    /*Robot friendly_robot = Robot(0, Point(0, 0), Vector(0, 0), Angle::zero(),
+                                 AngularVelocity::zero(), Timestamp::fromMilliseconds(0));
+    Robot enemy_robot    = Robot(0, Point(-3, 0), Vector(0, 0), Angle::zero(),
+                              AngularVelocity::zero(), Timestamp::fromMilliseconds(0));
+    std::vector<Robot> friendly_robots;
+    std::vector<Robot> enemy_robots;
+    friendly_robots.emplace_back(friendly_robot);
+    enemy_robots.emplace_back(enemy_robot);*/
+
+    std::vector<Triangle> triangles;
+
+    Triangle triangle1 = {Point(-0.5, -0.5), Point(0, 0.5), Point(0.5, -0.5)};
+    Triangle triangle2 = {Point(-1, -1), Point(0, sqrt(0.75)), Point(1, -1)};
+    triangles.emplace_back(triangle1);
+    triangles.emplace_back(triangle2);
+
+    std::vector<Point> enemy_players;
+
+    Point enemy_player1 = Point(-0.7, -0.7);
+    enemy_players.emplace_back(enemy_player1);
+
+    std::vector<Triangle> filtered_triangles = triangles;
+    filtered_triangles.push_back(triangle2);
+
+    EXPECT_EQ(filtered_triangles, Evaluation::filter_open_triangles(triangles,enemy_players));
+}
+
+/*std::vector<Triangle> Evaluation::filter_open_triangles(std::vector<Triangle> triangles,
+                                                        std::vector<Point> enemy_players)
+{
+    std::vector<Triangle> filtered_triangles;
+    bool containsEnemy = false;
+
+    for (unsigned int i = 0; i < triangles.size(); i++)
+    {
+        // Create a slightly smaller triangle so the robots making up the vertices
+        // are not counted in the triangle
+        Point p1 = triangles[i][0] +
+                   ((get_triangle_center_and_area(triangles[i]).first) - triangles[i][0])
+                       .norm(2.5 * ROBOT_MAX_RADIUS_METERS);
+        Point p2 = triangles[i][1] +
+                   ((get_triangle_center_and_area(triangles[i]).first) - triangles[i][1])
+                       .norm(2.5 * ROBOT_MAX_RADIUS_METERS);
+        Point p3 = triangles[i][2] +
+                   ((get_triangle_center_and_area(triangles[i]).first) - triangles[i][2])
+                       .norm(2.5 * ROBOT_MAX_RADIUS_METERS);
+        Triangle t    = triangle(p1, p2, p3);
+        containsEnemy = false;
+
+        for (unsigned int k = 0; k < enemy_players.size(); k++)
+        {
+            if (contains(t, enemy_players[k]) == true)
+            {
+                containsEnemy = true;
+                break;
+            }
+        }
+
+        if (containsEnemy == false)
+        {
+            filtered_triangles.push_back(triangles[i]);
+        }
+    }
+
+    return filtered_triangles;
+}*/
 int main(int argc, char **argv)
 {
     std::cout << argv[0] << std::endl;
--- src/thunderbots/software/util/parameter/dynamic_parameters.cpp
+++ src/thunderbots/software/util/parameter/dynamic_parameters.cpp
@@ -34,51 +34,40 @@ namespace Util::DynamicParameters
                                                          2.0);
     }  // namespace Navigator
 
+    namespace Indirect_Chip_Evaluation
+    {
+        // Adjusts how far between ball and target the robot will chip
+        Parameter<double> chip_target_fraction("chip_target_fraction", 5.0 / 10.0);
+
+        // Maximum fraction of distance between chipper and target the first bounce
+        // should be, so ball is rolling when it reaches the target
+        Parameter<double> chip_power_bounce_threshold("chip_power_bounce_threshold",
+                                                      7.5 / 10.0);
+
+        // Maximum power the robot can chip the ball at without malfunctions
+        Parameter<double> max_chip_power("max_chip_power", 8.0);
+
+        // Closest distance to edge of field that the robot could chip and chase to
+        Parameter<double> chip_target_area_inset("chip_target_area_inset", 0.3);
+
+        // Minimum area of chip target triangle
+        Parameter<double> min_chip_tri_area("min_chip_tri_area", 0.5);
+
+        // Minimum edge length of chip target triangle
+        Parameter<double> min_chip_tri_edge_len("min_chip_tri_edge_len", 0.8);
+
+        // Minimum angle in degrees between chip triangle edges
+        Parameter<double> min_chip_tri_edge_angle("min_chip_tri_edge_angle", 20);
+
+        // Percentage of distance to center of triangle to return as target
+        Parameter<double> chip_cherry_power_downscale("chip_cherry_power_downscale",
+                                                      0.85);
+    }  // namespace Indirect_Chip_Evaluation
+
     namespace AI
     {
         namespace Passing
         {
-            // Default avoid distance around robots.
-            Parameter<double> default_avoid_dist("default_avoid_dist", 0.15);
-
-            // Scaling factor for collision avoidance.
-            // TODO this is arbitrary for now; could be determined as part of
-            // #23: https://github.com/UBC-Thunderbots/Software/issues/23
-            Parameter<double> collision_avoid_velocity_scale(
-                "collision_avoid_velocity_scale", 2.0);
-        }  // namespace Navigator
-
-        namespace Indirect_Chip_Evaluation
-        {
-            // Adjusts how far between ball and target the robot will chip
-            Parameter<double> chip_target_fraction("chip_target_fraction", 5.0 / 10.0);
-
-            // Maximum fraction of distance between chipper and target the first bounce
-            // should be, so ball is rolling when it reaches the target
-            Parameter<double> chip_power_bounce_threshold("chip_power_bounce_threshold",
-                                                          7.5 / 10.0);
-
-            // Maximum power the robot can chip the ball at without malfunctions
-            Parameter<double> max_chip_power("max_chip_power", 8.0);
-
-            // Closest distance to edge of field that the robot could chip and chase to
-            Parameter<double> chip_target_area_inset("chip_target_area_inset", 0.3);
-
-            // Minimum area of chip target triangle
-            Parameter<double> min_chip_tri_area("min_chip_tri_area", 0.5);
-
-            // Minimum edge length of chip target triangle
-            Parameter<double> min_chip_tri_edge_len("min_chip_tri_edge_len", 0.8);
-
-            // Minimum angle in degrees between chip triangle edges
-            Parameter<double> min_chip_tri_edge_angle("min_chip_tri_edge_angle", 20);
-
-            // Percentage of distance to center of triangle to return as target
-            Parameter<double> chip_cherry_power_downscale("chip_cherry_power_downscale",
-                                                          0.85);
-        }  // namespace Indirect_Chip_Evaluation
-    }      // namespace DynamicParameters
-}  // namespace Util
             Parameter<double> static_field_position_quality_x_offset(
                 "static_field_position_quality_x_offset", 0.3);
             Parameter<double> static_field_position_quality_y_offset(
@@ -87,6 +76,5 @@ namespace Util::DynamicParameters
                 "static_field_position_quality_friendly_goal_distance_weight", 0.3);
         }  // namespace Passing
     }      // namespace AI
-
-
+    
 }  // namespace Util::DynamicParameters
