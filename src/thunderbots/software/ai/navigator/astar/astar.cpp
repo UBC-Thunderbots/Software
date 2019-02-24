cfd1f61f4dd951b6c3070438d99c80632209f3b8
Jordan Liu <jordan.zliu@gmail.com>
2019-02-23 21:19:16 -0800

    generate a grid graph, and create utility functions for going to/from grid points to field points, and destroy compile times

--- src/thunderbots/software/ai/navigator/astar/astar.cpp
+++ src/thunderbots/software/ai/navigator/astar/astar.cpp
@@ -6,10 +6,43 @@
 #include "ai/navigator/RobotObstacle.h"
 #include "ai/primitive/move_primitive.h"
 
-AStarNav::AStarNav() {}
+AStarNav::AStarNav(const Field& field):
+    field_width(field.totalWidth()),
+    field_length(field.totalLength())
+{
+    // TODO: allow configuring the field graph number of nodes with
+    // dynamic parameters
+    boost::array<size_t, 2> dims = {FIELD_NUM_LENGTH_NODES, FIELD_NUM_WIDTH_NODES};
+    field_graph = std::make_unique<graph_t>(dims);
+    x_step_size = field_length / FIELD_NUM_LENGTH_NODES;
+    y_step_size = field_width / FIELD_NUM_WIDTH_NODES;
+    min_x = - (field_length / 2);
+    min_y = - (field_width / 2);
+    graph_t::vertex_iterator v, vbegin, vend;
+    for (boost::tie(v, vend) = vertices(*field_graph); v != vend; v++) {
+        Point p = gridPointToPoint(*v);
+        grid_points_list.emplace_back({p, *v});
+    }
+}
 
 std::vector<std::unique_ptr<Primitive>> AStarNav::getAssignedPrimitives(
     const World &world, const std::vector<std::unique_ptr<Intent>> &assignedIntents) const
 {
     return std::vector<std::unique_ptr<Primitive>>();
+}
+
+Point AStarNav::gridPointToPoint(const AStarNav::grid_point &grid_point) {
+    Point p(min_x + grid_point[0] * x_step_size, min_y + grid_point[1] * y_step_size);
+    return p;
+}
+
+AStarNav::grid_point AStarNav::nearestGridPoint(const Point &point) {
+    // this is unfortunately O(n) w.r.t. number of vertices
+    // try not to call this too much during the actual search
+    auto closest_point_it = grid_points_list.begin();
+    std::nth_element(grid_points_list.begin(), closest_point_it, grid_points_list.end(),
+                    [point](const auto& a, const auto& b){
+                        return dist(a.first, point) < dist(b.first, point);
+                    });
+    return closest_point_it->second;
 }
--- src/thunderbots/software/ai/navigator/astar/astar.h
+++ src/thunderbots/software/ai/navigator/astar/astar.h
@@ -1,17 +1,49 @@
 #pragma once
 
+// sincere apologies for quadrupling compile times
+#include <boost/graph/grid_graph.hpp>
+
 #include "ai/navigator/navigator.h"
 
-class AStarNav : public Navigator
-{
-   public:
+// TODO: move the actual A* implementation into its own class so that it can
+// be tested
+
+class AStarNav : public Navigator {
+public:
     /**
-     * Creates a new Navigator that uses RRT (Rapidly Expanding Random Trees)
+     * Creates a new Navigator that uses A*
      * to generate paths
      */
-    AStarNav();
+    AStarNav() = delete;
+    // we need field dimensions to have consistent sized nodes
+    explicit AStarNav(const Field& field);
 
     std::vector<std::unique_ptr<Primitive>> getAssignedPrimitives(
-        const World &world,
-        const std::vector<std::unique_ptr<Intent>> &assignedIntents) const override;
+            const World &world,
+            const std::vector<std::unique_ptr<Intent>> &assignedIntents) const override;
+
+private:
+    // TODO: these should be dynamic parameters
+    static constexpr unsigned long FIELD_NUM_LENGTH_NODES = 960;
+    static constexpr unsigned long FIELD_NUM_WIDTH_NODES = 660;
+    typedef double cost_t;
+    typedef boost::grid_graph<2> graph_t;
+    typedef graph_t::vertex_descriptor grid_point;
+
+    std::unique_ptr<graph_t> field_graph;
+    // this map is used to convert from the grid points that A* will
+    // return, into Points that can be used for navigation
+    std::vector<std::pair<Point, grid_point>> grid_points_list;
+
+    double field_width, field_length;
+    double min_x, min_y;
+    // node length/width
+    // this is a member field because we don't want to recalculate it
+    // every time gridPointToPoint is called
+    double x_step_size, y_step_size;
+
+    // TODO: create functions to go from grid point to field point, and vice versa
+    std::vector<Point> findPath(const Point& start, const Point& dest);
+    Point gridPointToPoint(const grid_point &grid_point);
+    grid_point nearestGridPoint(const Point& point);
 };
