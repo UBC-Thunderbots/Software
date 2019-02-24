18c72a3ce6e35ef255cf6b080fdd33ae4fe909fb
Gareth Ellis <garethellis0@users.noreply.github.com>
2019-02-24 11:51:43 -0800

    Implement PassGenerator Class (#379)
    * Created A Constructor For An Empty World
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * It works in space
    
    * wip
    
    * Created A Generic Gradient Descent Class
    
    * Created A Generic Gradient Descent Class
    
    * Added start-of-file Comments
    
    * Fixed Formatting
    
    * Resolved TODO
    
    * Improved a Comment In GradientDescentOptimizer Tests
    
    * Fixed formatting..... again
    
    * Fixed Issues Caused By Merging
    
    * Refactored Passing
    
    * Cleaned Up Some Comments
    
    * Refactored PassingGradientDescent Into PassGenerator
    
    * Made PassGenerator Run Mostly On Another Thread
    
    * Few more tidy-ups
    
    * Resolved Some Thread Locks
    
    * Tracking down sort bug
    
    * Stopped Pass Returning Values By Reference
    
    * less broken
    
    * wip
    
    * wip
    
    * SWEET LORD IT WORKS
    
    * misc. cleanup
    
    * wip
    
    * Refactored and tested basic evaluation functions
    
    * Integrated refactored evaluation functions into ratePass
    
    * Made changes based on feedback from Mat
    
    * Added Negative Static Weight For Enemy Defense Area
    
    * Resolved some TODO's, Added Simple Test
    
    * Finished Adding Parameters
    
    * minor fixes
    
    * Fixed Formatting
    
    * Resolved all TODO's
    
    * Fixed borked tests
    
    * Removed Message Changes
    
    * Implemented Pass Merging And Merged All Passing Tests
    
    * Fixed Formatting
    
    * Small Changes To Fix Build
    
    * Made requested changes
    
    * Fixed Pass Merging Logic
    
    * Added Warning About Sharing World Objects Between Threads
    
    * Fixed Formatting

--- src/thunderbots/software/test/ai/passing/pass.cpp
+++ src/thunderbots/software/test/ai/passing/pass.cpp
@@ -28,9 +28,3 @@ TEST(PassTest, test_stream_operator)
               out.str());
 }
 
-int main(int argc, char **argv)
-{
-    std::cout << argv[0] << std::endl;
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
