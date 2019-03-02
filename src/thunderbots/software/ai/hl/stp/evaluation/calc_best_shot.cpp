5cedf62f71a71a6101b26bdd6ae1c02eee436d40
HaomiaoZ <zhm7991@gmail.com>
2019-03-02 11:50:45 -0800

    add comment

--- src/thunderbots/software/ai/hl/stp/evaluation/calc_best_shot.cpp
+++ src/thunderbots/software/ai/hl/stp/evaluation/calc_best_shot.cpp
@@ -1,76 +1,88 @@
 #include "ai/hl/stp/evaluation/calc_best_shot.h"
+
 #include "geom/util.h"
-#include"shared/constants.h"
+#include "shared/constants.h"
 
-namespace Evaluation {
-
-    std::pair<Point, Angle> calc_best_shot(
-            const Field &f, const std::vector<Point> &obstacles,
-            const Point &p, const double radius) {
+namespace Evaluation
+{
+    std::pair<Point, Angle> calc_best_shot(const Field& f,
+                                           const std::vector<Point>& obstacles,
+                                           const Point& p, const double radius)
+    {
         // whether that the goal is always on positive x- axis at (f.length()/2,0)?
         // player and robot are all robot type?
-        // waht is mutable reference? guss it is just not const reference
+        // what is mutable reference? guss it is just not const reference
+
+        // Calculate the location of goalpost then use angleSweepCircle function to get
+        // the pair
         const Point p1 = Point(f.length() / 2.0, -f.goalWidth() / 2.0);
         const Point p2 = Point(f.length() / 2.0, f.goalWidth() / 2.0);
-        return angleSweepCircles(
-                p, p1, p2, obstacles, radius * ROBOT_MAX_RADIUS_METERS);
+        return angleSweepCircles(p, p1, p2, obstacles, radius * ROBOT_MAX_RADIUS_METERS);
     }
 
     std::vector<std::pair<Point, Angle>> calc_best_shot_all(
-            const Field &f, const std::vector<Point> &obstacles,
-            const Point &p, const double radius) {
-
+        const Field& f, const std::vector<Point>& obstacles, const Point& p,
+        const double radius)
+    {
         const Point p1 = Point(f.length() / 2.0, -f.goalWidth() / 2.0);
         const Point p2 = Point(f.length() / 2.0, f.goalWidth() / 2.0);
-        return angleSweepCirclesAll(
-                p, p1, p2, obstacles, radius * ROBOT_MAX_RADIUS_METERS);
-
+        return angleSweepCirclesAll(p, p1, p2, obstacles,
+                                    radius * ROBOT_MAX_RADIUS_METERS);
     }
 
     std::pair<Point, Angle> calc_best_shot(
-            World world, Robot robot, double radius)// why not using refernce as world
+        const World& world, const Robot& robot,
+        double radius)  // why not using refernce as world
     {
         std::vector<Point> obstacles;
-        Team enemy = world.enemyTeam();
-        Team friendly = world.friendlyTeam();
+        const Team& enemy    = world.enemyTeam();
+        const Team& friendly = world.friendlyTeam();
         obstacles.reserve(enemy.numRobots() + friendly.numRobots());
-        for (const Robot i : enemy.getAllRobots()) {
-            obstacles.push_back(i.position());
+        for (const Robot& i : enemy.getAllRobots())
+        {
+            obstacles.emplace_back(i.position());  // push back and emplace back
         }
-        for (const Robot fpl : friendly.getAllRobots()) {
-            if (fpl == robot) {
+        for (const Robot& fpl : friendly.getAllRobots())
+        {
+            if (fpl == robot)  // does this still works for const reference
+            {
                 continue;
             }
-            obstacles.push_back(fpl.position());
+            obstacles.emplace_back(fpl.position());
         }
         std::pair<Point, Angle> best_shot =
-                calc_best_shot(world.field(), obstacles, robot.position(), radius);
+            calc_best_shot(world.field(), obstacles, robot.position(), radius);
         // if there is no good shot at least make the
         // target within the goal area
-        if (best_shot.second <= Angle::zero()) {
-            Point temp = Point(world.field().length() / 2.0, 0.0);
+        if (best_shot.second <= Angle::zero())
+        {
+            Point temp      = Point(world.field().length() / 2.0, 0.0);
             best_shot.first = temp;
         }
         return best_shot;
     }
 
-    std::vector<std::pair<Point, Angle>> calc_best_shot_all(
-            World world, Robot robot, double radius) {
+    std::vector<std::pair<Point, Angle>> calc_best_shot_all(const World& world,
+                                                            const Robot& robot,
+                                                            double radius)
+    {
         std::vector<Point> obstacles;
-        Team enemy = world.enemyTeam();
-        Team friendly = world.friendlyTeam();
+        const Team& enemy    = world.enemyTeam();
+        const Team& friendly = world.friendlyTeam();
         obstacles.reserve(enemy.numRobots() + friendly.numRobots());
-        for (const Robot i : enemy.getAllRobots()) {
+        for (const Robot& i : enemy.getAllRobots())
+        {
             obstacles.push_back(i.position());
         }
-        for (const Robot fpl : friendly.getAllRobots()) {
-            if (fpl == robot) {
+        for (const Robot& fpl : friendly.getAllRobots())
+        {
+            if (fpl == robot)
+            {
                 continue;
             }
             obstacles.push_back(fpl.position());
         }
-        return calc_best_shot_all(
-                world.field(), obstacles, robot.position(), radius);
+        return calc_best_shot_all(world.field(), obstacles, robot.position(), radius);
     }
 
-}
+}  // namespace Evaluation
--- src/thunderbots/software/ai/hl/stp/evaluation/calc_best_shot.h
+++ src/thunderbots/software/ai/hl/stp/evaluation/calc_best_shot.h
@@ -1,62 +1,97 @@
 #ifndef PROJECT_CALC_BEST_SHOT_H
 #define PROJECT_CALC_BEST_SHOT_H
 
-#include "geom/point.h"
 #include "ai/world/field.h"
-#include "ai/world/world.h"
 #include "ai/world/robot.h"
+#include "ai/world/world.h"
+#include "geom/point.h"
 
 namespace Evaluation
 {
     /**
- * Finds the length of the largest continuous interval (angle-wise) of the enemy
- * goal that can be seen from a point.
- * Having a vector of points enables one to add imaginary threats.
- * This version accepts vector of obstacles, so that you can add imaginary
- * robots.
- *
- * \param[in] f field is needed to calculate length etc
- *
- * \param[in] radius the multiplier to the radius of the robot,
- * you can decrease the radius to make it easier to shoot.
- *
- * \param[in] obstacles is a list of all the obstacles in the way between the
- * robot and the net
- *
- * \param[in] p the player that the shot is being calculated from
- *
- * \return the point and the score (angle),
- * where the score will be 0 if the point is invalid.
- */
-    std::pair<Point, Angle> calc_best_shot(
-            const Field &f, const std::vector<Point> &obstacles,
-            const Point &p, double radius = 1.0);
+     * Finds the length of the largest continuous interval (angle-wise) of the
+     * goal that can be seen from a point.
+     *
+     * @param f-field is needed to calculate the location of the goalposts based on field
+     * length and goal width
+     *
+     * @param obstacles-obstacles is a list of all the obstacles in the way between the
+     * point and the net
+     *
+     * @param p-the point that the shot is being calculated from
+     *
+     * @param radius-the multiplier to the radius of the robot,
+     * you can decrease the multiplier to make it easier to shoot.
+     *
+     * @return the point and the score (angle),
+     * where the score will be 0 if the point is invalid.
+     */
+    std::pair<Point, Angle> calc_best_shot(const Field& f,
+                                           const std::vector<Point>& obstacles,
+                                           const Point& p, double radius = 1.0);
+
+    /**
+     * Finds the length of the all continuous interval (angle-wise) of the
+     * goal that can be seen from a point.
+     *
+     * @param f-field is needed to calculate the location of the goalposts based on field
+     * length and goal width
+     *
+     * @param obstacles-obstacles is a list of all the obstacles in the way between the
+     * point and the net
+     *
+     * @param p-the point that the shot is being calculated from
+     *
+     * @param radius-the multiplier to the radius of the robot,
+     * you can decrease the multiplier to make it easier to shoot.
+     *
+     * @return a set of points and the corresponding score (angle),
+     * where the score will be 0 if the point is invalid.
+     */
+
 
     std::vector<std::pair<Point, Angle>> calc_best_shot_all(
-            const Field &f, const std::vector<Point> &obstacles,
-            const Point &p, double radius = 1.0);
+        const Field& f, const std::vector<Point>& obstacles, const Point& p,
+        double radius = 1.0);
 
-/**
- * Finds the length of the largest continuous interval (angle-wise) of the enemy
- * goal that can be seen from a point.
- * To add imaginary threats, please use the other version.
- *
- * \param[in] world with field information
- *
- * \param[in] player player that the shot is being calculated from
- *
- * \param[in] radius the multiplier to the radius of the robot,
- * you can decrease the radius to make it easier to shoot.
- *
- * \return the point as and the score (angle),
- * where the score will be 0 if the point is invalid,
- */
-    std::pair<Point, Angle> calc_best_shot(
-            World world, Robot robot, double radius = 1.0);
+    /**
+     * Finds the length of the largest continuous interval (angle-wise) of the
+     * goal that can be seen from a point.
+     * To add imaginary threats or obstacles, please use the above calc_best_shot.
+     *
+     * @param world-world with field information
+     *
+     * @param robot-robot that the shot is being calculated from
+     *
+     * @param radius-the multiplier to the radius of the robot,
+     * you can decrease the radius to make it easier to shoot.
+     *
+     * @return the point as and the score (angle),
+     * where the score will be 0 if the point is invalid.
+     */
+    std::pair<Point, Angle> calc_best_shot(const World& world, const Robot& robot,
+                                           double radius = 1.0);
 
-    std::vector<std::pair<Point, Angle>> calc_best_shot_all(
-            World world, Robot robot, double radius = 1.0);
 
-}
+    /**
+     * Finds the list of length of the largest continuous interval (angle-wise) of the
+     * goal that can be seen from a point.
+     * To add imaginary threats or obstacles, please use the above calc_best_shot_all.
+     *
+     * @param world-world with field information
+     *
+     * @param robot-robot that the shot is being calculated from
+     *
+     * @param radius-the multiplier to the radius of the robot,
+     * you can decrease the radius to make it easier to shoot.
+     *
+     * @return the point as and the score (angle),
+     * where the score will be 0 if the point is invalid.
+     */
+
+    std::vector<std::pair<Point, Angle>> calc_best_shot_all(const World& world,
+                                                            const Robot& robot,
+                                                            double radius = 1.0);
+
+}  // namespace Evaluation
 #endif
-
