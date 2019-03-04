11787f9d049ec71ca7b85427abb9da16cfc04f11
Evan <evan@localhost.localdomain>
2019-03-03 19:21:16 -0700

    Minor changes and added unit test file

---
+++ src/thunderbots/software/ai/hl/stp/evaluation/shoot.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "geom/angle.h"
+#include "ai/world/field.h"
+#include "ai/world/team.h"
+#include "ai/world/robot.h"
+
+namespace Evaluation {
+
+    typedef struct ShotDirAndScore {
+        double score;
+        Vector shot_direction;
+    };
+
+  ShotDirAndScore getFriendlyRobotBestShotGoal(Robot shooter, std::vector<Point> all_obstacles, Field field);
+  ShotDirAndScore getEnemyRobotBestShotGoal(Robot shooter, std::vector<Point> all_obstacles, Field field);
+
+};
+
