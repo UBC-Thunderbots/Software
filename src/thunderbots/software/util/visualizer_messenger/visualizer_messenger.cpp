5be31c6eaa527e94bd51433ae60e300cb1cca0de
Muchen He <mansurhe@outlook.com>
2019-03-05 21:56:05 -0800

    Initial websocket merge

--- src/thunderbots/software/util/visualizer_messenger/visualizer_messenger.cpp
+++ src/thunderbots/software/util/visualizer_messenger/visualizer_messenger.cpp
@@ -1,7 +1,16 @@
 #include "visualizer_messenger.h"
 
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+
 #include "util/constants.h"
 #include "util/logger/init.h"
+
+
+using tcp           = boost::asio::ip::tcp;     // from <boost/asio/ip/tcp.hpp>
+namespace websocket = boost::beast::websocket;  // from <boost/beast/websocket.hpp>
 
 namespace Util
 {
@@ -11,15 +20,64 @@ namespace Util
         return vm;
     }
 
-    void VisualizerMessenger::initializePublisher(ros::NodeHandle node_handle)
+    void VisualizerMessenger::onConnection()
     {
-        if (this->publisher)
-        {
-            LOG(WARNING) << "Re-initializing visualizer messenger singleton publisher";
-        }
+        // The io_context is required for all I/O
+        boost::asio::io_service ioc{1};
 
-        this->publisher = node_handle.advertise<LayerMsg>(
-            Util::Constants::VISUALIZER_DRAW_LAYER_TOPIC, 8);
+        // The acceptor receives incoming connections
+        auto const address = boost::asio::ip::address::from_string("127.0.0.1");
+        auto const port    = static_cast<unsigned short>(std::atoi("9091"));
+        tcp::acceptor acceptor{ioc, {address, port}};
+        for (;;)
+        {
+            // This will receive the new connection
+            tcp::socket socket{ioc};
+
+            // Block until we get a connection
+            acceptor.accept(socket);
+
+            // Move the connection to a websocket stream
+            websocket::stream<tcp::socket> websocket(std::move(socket));
+
+            // Accept the websocket handshake
+            websocket.accept();
+
+            // Set the websocket to talk in binary
+            websocket.binary(true);
+
+            std::cout << "GOT A CONNECTION" << std::endl;
+
+            // Lock the current list of sockets
+            ws_mutex.lock();
+
+            // Save this new websocket connection
+            ws_connections.emplace_back(std::move(websocket));
+
+            // Unlock the current list of sockets
+            ws_mutex.unlock();
+
+            std::cout << "ADDED THE CONNECTION" << std::endl;
+
+            // TODO: this is disgusting. We should not tie up all the cpu
+            // Yield so that other things can run
+            std::this_thread::yield();
+        }
+    }
+
+    void VisualizerMessenger::initializeWebsocket()
+    {
+        // TODO: can we nuke this?
+        // if (this->publisher)
+        //{
+        //    LOG(WARNING) << "Re-initializing visualizer messenger singleton publisher";
+        //}
+
+        // this->publisher = node_handle.advertise<LayerMsg>(
+        //    Util::Constants::VISUALIZER_DRAW_LAYER_TOPIC, 8);
+
+        // Assign handler to a thread
+        ws_thread = std::thread([this]() { return onConnection(); });
     }
 
     const LayerMsgMap& VisualizerMessenger::getLayerMap() const
@@ -29,6 +87,8 @@ namespace Util
 
     void VisualizerMessenger::publishAndClearLayers()
     {
+        std::cout << "PUBLISHING" << std::endl;
+
         // Limit rate of the message publishing
         // Get the time right now
         const time_point now     = std::chrono::system_clock::now();
@@ -42,23 +102,52 @@ namespace Util
         if (elapsed_ms < DESIRED_PERIOD_MS)
             return;
 
-        // Check if publisher is initialized before publishing messages
-        if (!this->publisher)
+        // Lock the list of current websockets
+        ws_mutex.lock();
+
+        // Send all the shapes to all the websocket connections we have
+        for (websocket::stream<tcp::socket>& websocket : ws_connections)
         {
-            LOG(WARNING)
-                << "Publishing requested when visualizer messenger publisher is not initialized";
-        }
-        else
-        {
-            // Send layer messages
+            std::vector<int32_t> message_contents;
             for (const std::pair<std::string, LayerMsg>& layer_msg_pair : getLayerMap())
             {
-                if (!layer_msg_pair.second.shapes.empty())
+                for (auto shape : layer_msg_pair.second)
                 {
-                    this->publisher.publish(layer_msg_pair.second);
+                    message_contents.insert(message_contents.end(), shape.begin(),
+                                            shape.end());
                 }
             }
+            try
+            {
+                websocket.write(boost::asio::buffer(message_contents));
+            }
+            catch (std::exception const& e)
+            {
+                std::cerr << "Error: " << e.what() << std::endl;
+            }
         }
+
+        // UnLock the list of current websockets
+        ws_mutex.unlock();
+
+        //// Check if publisher is initialized before publishing messages
+        // if (!this->publisher)
+        //{
+        //    LOG(WARNING)
+        //        << "Publishing requested when visualizer messenger publisher is not
+        //        initialized";
+        //}
+        // else
+        //{
+        //    // Send layer messages
+        //    for (const std::pair<std::string, LayerMsg>& layer_msg_pair : getLayerMap())
+        //    {
+        //        if (!layer_msg_pair.second.shapes.empty())
+        //        {
+        //            this->publisher.publish(layer_msg_pair.second);
+        //        }
+        //    }
+        //}
 
         // Clear shapes in layers of current frame/tick
         clearLayers();
@@ -72,139 +161,153 @@ namespace Util
         // Clears all shape vector in all the layers
         for (auto& layer : this->layers_name_to_msg_map)
         {
-            layer.second.shapes.clear();
+            layer.second.clear();
         }
     }
 
-    void VisualizerMessenger::drawEllipse(const std::string& layer, double cx, double cy,
-                                          double r1, double r2, DrawStyle draw_style,
-                                          DrawTransform draw_transform)
-    {
-        ShapeMsg new_shape;
-        new_shape.type = "ellipse";
-        new_shape.data.clear();
-        new_shape.data.push_back(cx);
-        new_shape.data.push_back(cy);
-        new_shape.data.push_back(r1);
-        new_shape.data.push_back(r2);
-
-        applyDrawStyleToMsg(new_shape, draw_style);
-        applyDrawTransformToMsg(new_shape, draw_transform);
-        addShapeToLayer(layer, new_shape);
-    }
+    //    void VisualizerMessenger::drawEllipse(const std::string& layer, double cx,
+    //    double cy,
+    //                                          double r1, double r2, DrawStyle
+    //                                          draw_style, DrawTransform draw_transform)
+    //    {
+    //        ShapeMsg new_shape;
+    //        new_shape.type = "ellipse";
+    //        new_shape.data.clear();
+    //        new_shape.data.push_back(cx);
+    //        new_shape.data.push_back(cy);
+    //        new_shape.data.push_back(r1);
+    //        new_shape.data.push_back(r2);
+    //
+    //        applyDrawStyleToMsg(new_shape, draw_style);
+    //        applyDrawTransformToMsg(new_shape, draw_transform);
+    //        addShapeToLayer(layer, new_shape);
+    //    }
 
     void VisualizerMessenger::drawRect(const std::string& layer, double x, double y,
                                        double w, double h, DrawStyle draw_style,
                                        DrawTransform draw_transform)
     {
-        ShapeMsg new_shape;
-        new_shape.type = "rect";
-        new_shape.data.clear();
-        new_shape.data.push_back(x);
-        new_shape.data.push_back(y);
-        new_shape.data.push_back(w);
-        new_shape.data.push_back(h);
+        std::vector<int32_t> raw = {
+            2, int(x), int(y), int(w), int(h), draw_transform.rotation, 0xFF, 0xFF, 0xFF};
+        addShapeToLayer(layer, raw);
+        // ShapeMsg new_shape;
+        // new_shape.type = "rect";
+        // new_shape.data.clear();
+        // new_shape.data.push_back(x);
+        // new_shape.data.push_back(y);
+        // new_shape.data.push_back(w);
+        // new_shape.data.push_back(h);
 
-        applyDrawStyleToMsg(new_shape, draw_style);
-        applyDrawTransformToMsg(new_shape, draw_transform);
-        addShapeToLayer(layer, new_shape);
+        // applyDrawStyleToMsg(new_shape, draw_style);
+        // applyDrawTransformToMsg(new_shape, draw_transform);
+        // addShapeToLayer(layer, new_shape);
     }
 
-    void VisualizerMessenger::drawPoly(const std::string& layer,
-                                       std::vector<Point>& vertices, DrawStyle draw_style,
-                                       DrawTransform draw_transform)
-    {
-        ShapeMsg new_shape;
-        new_shape.type = "poly";
-        new_shape.data.clear();
+    //    void VisualizerMessenger::drawPoly(const std::string& layer,
+    //                                       std::vector<Point>& vertices,
+    //                                       DrawStyle draw_style, DrawTransform
+    //                                       draw_transform)
+    //    {
+    //        ShapeMsg new_shape;
+    //        new_shape.type = "poly";
+    //        new_shape.data.clear();
+    //
+    //        for (auto vertexIter = vertices.begin(); vertexIter != vertices.end();
+    //             vertexIter++)
+    //        {
+    //            new_shape.data.push_back((*vertexIter).x);
+    //            new_shape.data.push_back((*vertexIter).y);
+    //        }
+    //
+    //        applyDrawStyleToMsg(new_shape, draw_style);
+    //        applyDrawTransformToMsg(new_shape, draw_transform);
+    //        addShapeToLayer(layer, new_shape);
+    //    }
 
-        for (auto vertexIter = vertices.begin(); vertexIter != vertices.end();
-             vertexIter++)
-        {
-            new_shape.data.push_back((*vertexIter).x());
-            new_shape.data.push_back((*vertexIter).y());
-        }
+    //    void VisualizerMessenger::drawArc(const std::string& layer, double cx, double
+    //    cy,
+    //                                      double radius, double theta_start, double
+    //                                      theta_end, DrawStyle draw_style, DrawTransform
+    //                                      draw_transform)
+    //    {
+    //        ShapeMsg new_shape;
+    //        new_shape.type = "arc";
+    //        new_shape.data.clear();
+    //        new_shape.data.push_back(cx);
+    //        new_shape.data.push_back(cy);
+    //        new_shape.data.push_back(radius);
+    //        new_shape.data.push_back(theta_start);
+    //        new_shape.data.push_back(theta_end);
+    //
+    //        applyDrawStyleToMsg(new_shape, draw_style);
+    //        applyDrawTransformToMsg(new_shape, draw_transform);
+    //        addShapeToLayer(layer, new_shape);
+    //    }
 
-        applyDrawStyleToMsg(new_shape, draw_style);
-        applyDrawTransformToMsg(new_shape, draw_transform);
-        addShapeToLayer(layer, new_shape);
-    }
+    //    void VisualizerMessenger::drawLine(const std::string& layer, double x1, double
+    //    y1,
+    //                                       double x2, double y2, DrawStyle draw_style,
+    //                                       DrawTransform draw_transform)
+    //    {
+    //        ShapeMsg new_shape;
+    //        new_shape.type = "line";
+    //        new_shape.data.clear();
+    //        new_shape.data.push_back(x1);
+    //        new_shape.data.push_back(y1);
+    //        new_shape.data.push_back(x2);
+    //        new_shape.data.push_back(y2);
+    //
+    //        applyDrawStyleToMsg(new_shape, draw_style);
+    //        applyDrawTransformToMsg(new_shape, draw_transform);
+    //        addShapeToLayer(layer, new_shape);
+    //    }
 
-    void VisualizerMessenger::drawArc(const std::string& layer, double cx, double cy,
-                                      double radius, double theta_start, double theta_end,
-                                      DrawStyle draw_style, DrawTransform draw_transform)
-    {
-        ShapeMsg new_shape;
-        new_shape.type = "arc";
-        new_shape.data.clear();
-        new_shape.data.push_back(cx);
-        new_shape.data.push_back(cy);
-        new_shape.data.push_back(radius);
-        new_shape.data.push_back(theta_start);
-        new_shape.data.push_back(theta_end);
+    //    void VisualizerMessenger::buildLayers()
+    //    {
+    //        // TODO: #268 list these existing layer names elsewhere where it's more
+    //        obvious std::vector<std::string> layer_names = std::vector<std::string>(
+    //            {"field", "ball", "ball_vel", "friendly_robot", "friendly_robot_vel",
+    //             "enemy_robot", "enemy_robot_vel", "nav", "rule", "passing", "test",
+    //             "misc"});
+    //
+    //        for (std::string layer_name : layer_names)
+    //        {
+    //            LayerMsg new_layer_msg;
+    //            new_layer_msg.layer_name = layer_name;
+    //            this->layers_name_to_msg_map.insert(
+    //                std::pair<std::string, LayerMsg>(layer_name, new_layer_msg));
+    //        }
+    //    }
 
-        applyDrawStyleToMsg(new_shape, draw_style);
-        applyDrawTransformToMsg(new_shape, draw_transform);
-        addShapeToLayer(layer, new_shape);
-    }
+    //    void VisualizerMessenger::applyDrawStyleToMsg(ShapeMsg& shape_msg, DrawStyle&
+    //    style)
+    //    {
+    //        shape_msg.fill          = style.fill;
+    //        shape_msg.stroke        = style.stroke;
+    //        shape_msg.stroke_weight = style.stroke_weight;
+    //    }
+    //
+    //    void VisualizerMessenger::applyDrawTransformToMsg(ShapeMsg& shape_msg,
+    //                                                      DrawTransform& transform)
+    //    {
+    //        shape_msg.transform_rotation = transform.rotation;
+    //        shape_msg.transform_scale    = transform.scale;
+    //    }
 
-    void VisualizerMessenger::drawLine(const std::string& layer, double x1, double y1,
-                                       double x2, double y2, DrawStyle draw_style,
-                                       DrawTransform draw_transform)
-    {
-        ShapeMsg new_shape;
-        new_shape.type = "line";
-        new_shape.data.clear();
-        new_shape.data.push_back(x1);
-        new_shape.data.push_back(y1);
-        new_shape.data.push_back(x2);
-        new_shape.data.push_back(y2);
-
-        applyDrawStyleToMsg(new_shape, draw_style);
-        applyDrawTransformToMsg(new_shape, draw_transform);
-        addShapeToLayer(layer, new_shape);
-    }
-
-    void VisualizerMessenger::buildLayers()
-    {
-        // TODO: #268 list these existing layer names elsewhere where it's more obvious
-        std::vector<std::string> layer_names = std::vector<std::string>(
-            {"field", "ball", "ball_vel", "friendly_robot", "friendly_robot_vel",
-             "enemy_robot", "enemy_robot_vel", "nav", "rule", "passing", "test", "misc"});
-
-        for (std::string layer_name : layer_names)
-        {
-            LayerMsg new_layer_msg;
-            new_layer_msg.layer_name = layer_name;
-            this->layers_name_to_msg_map.insert(
-                std::pair<std::string, LayerMsg>(layer_name, new_layer_msg));
-        }
-    }
-
-    void VisualizerMessenger::applyDrawStyleToMsg(ShapeMsg& shape_msg, DrawStyle& style)
-    {
-        shape_msg.fill          = style.fill;
-        shape_msg.stroke        = style.stroke;
-        shape_msg.stroke_weight = style.stroke_weight;
-    }
-
-    void VisualizerMessenger::applyDrawTransformToMsg(ShapeMsg& shape_msg,
-                                                      DrawTransform& transform)
-    {
-        shape_msg.transform_rotation = transform.rotation;
-        shape_msg.transform_scale    = transform.scale;
-    }
-
-    void VisualizerMessenger::addShapeToLayer(const std::string& layer, ShapeMsg& shape)
+    // TODO: this is a hack, fix up
+    void VisualizerMessenger::addShapeToLayer(const std::string& layer,
+                                              std::vector<int32_t>& shape)
     {
         if (this->layers_name_to_msg_map.find(layer) !=
             this->layers_name_to_msg_map.end())
         {
-            this->layers_name_to_msg_map[layer].shapes.emplace_back(shape);
+            this->layers_name_to_msg_map[layer].emplace_back(shape);
         }
         else
         {
             LOG(WARNING) << "Referenced layer (" << layer << ") is undefined\n";
+            this->layers_name_to_msg_map[layer] = {};
+            this->layers_name_to_msg_map[layer].emplace_back(shape);
         }
     }
 
