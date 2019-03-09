2e76523c99fece453ddf419dd1f01619c07245dc
Mathew <mmacd97@gmail.com>
2019-03-09 08:53:06 -0800

    Implement STP play selection (#416)
    <!---
    This file outlines a list of common things that should be addressed when opening a PR. It's built from previous issues we've seen in a lot of pull requests. If you notice something that's being noted in a lot of PR's, it should probably be added here to help save people time in the future.
    -->
    
    ## Please fill out the following before requesting review on this PR
    
    ### Description
    
    <!--
        Give a high-level description of the changes in this PR
    -->
    Implemented the logic that assigns and switches between STP Plays. (previously known as the `PlayExecutor`). The tactic assignment portion will be done separately to keep the PRs reasonably sized.
    
    ### Testing Done
    
    <!--
        Outline any testing that was done for these changes. This could be unit tests, integration tests,etc.
    -->
    Unit tests added for various Play assignment cases. Please make sure I haven't missed any.
    
    Added test implementations of Plays and Tactics to facilitate this testing and keep the test code separate from our actual Plays and Tactics
    
    ### Resolved Issues
    
    <!--
        Link any issues that this PR resolved. Eg `resolves #1, #2, and #5` (note that they MUST be specified like this so Github can automatically close them then this PR merges)
    
        Please connect this PR to the issue in Zenhub by going to the bottom of this page and clicking "Connect With Issue" (so that this link is tracked in zenhub!)
    -->
    partially resolves #396
    
    ### Length Justification
    
    <!-- If this pull request is longer then **500** lines (additions + deletions), please justify here why we *cannot* break this up into multiple pull requests. -->
    Adding the test Plays and Tactics took a lot of lines.
    
    ### Review Checklist
    
    <!--
        (Please check every item to indicate your code complies with it (by changing `[ ]`->`[x]`). This will hopefully save both you and the reviewer(s) a lot of time!)
    -->
    
    **_It is the reviewers responsibility to also make sure every item here has been covered_**
    
    - [x] **Start of document comments**: each `.cpp` and `.h` file should have a comment at the start of it. See files in the `thunderbots/software/geom` folder for examples.
    - [x] **Function comments**: All function definitions (usually in the `.h` file) should have a javadoc style comment at the start of them. For examples, see the classes defined in `thunderbots/software/geom`
    - [x] **Remove all commented out code**
    - [x] **Remove extra print statements**: for example, those just used for testing
    - [x] **Resolve all TODO's**: All `TODO` (or similar) statements should either be completed or associated with a github issue
    - [x] **Justify drops in code coverage**: If this PR results in a non-trivial drop in code coverage (a bot should post a coverage diagram as a comment), please justify why we can't test the code that's not covered.
    
    <!--
        Feel free to make additions of things that we should be checking to this file if you think there's something missing!!!!
    -->

---
+++ src/thunderbots/software/test/ai/hl/stp/test_plays/move_test_play.cpp
@@ -0,0 +1,57 @@
+#include "ai/hl/stp/play/play.h"
+#include "ai/hl/stp/play/play_factory.h"
+#include "test/ai/hl/stp/test_tactics/move_test_tactic.h"
+
+/**
+ * A test Play that moves a robot to the friendly goal, a robot to the enemy goal, and
+ * a robot to the center of the field.
+ *
+ * The Play is done when the robot reaches the center of the field (within 5cm)
+ *
+ * This play is applicable when the ball's x and y coordinates are >= 0
+ * This play's invariant holds while the ball's x coordinate is >= 0
+ *
+ * We use the Ball's position to control the 'isApplicable' and 'invariantHolds' values
+ * because it is easy to change during tests
+ */
+class MoveTestPlay : public Play
+{
+   public:
+    MoveTestPlay() : Play(){};
+
+    std::string name() override
+    {
+        return "Move Test Play";
+    }
+
+    bool isApplicable(const World &world) override
+    {
+        return world.ball().position().x() >= 0;
+    }
+
+    bool invariantHolds(const World &world) override
+    {
+        return world.ball().position().x() >= 0;
+    }
+
+    std::vector<std::shared_ptr<Tactic>> getNextTactics(TacticCoroutine::push_type &yield,
+                                                        const World &world) override
+    {
+        auto move_test_tactic_friendly_goal = std::make_shared<MoveTestTactic>();
+        auto move_test_tactic_enemy_goal    = std::make_shared<MoveTestTactic>();
+        auto move_test_tactic_center_field  = std::make_shared<MoveTestTactic>();
+
+        do
+        {
+            move_test_tactic_friendly_goal->updateParams(world.field().friendlyGoal());
+            move_test_tactic_enemy_goal->updateParams(world.field().enemyGoal());
+            move_test_tactic_center_field->updateParams(Point(0, 0));
+
+            yield({move_test_tactic_center_field, move_test_tactic_friendly_goal,
+                   move_test_tactic_enemy_goal});
+        } while (!move_test_tactic_center_field->done());
+    }
+};
+
+// Register this play in the PlayFactory
+static TPlayFactory<MoveTestPlay> factory;
