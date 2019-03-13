d26394f9af4ee5d4c862e5b179f8e71224e41583
akhil <akhil@komputer.hitronhub.home>
2019-03-12 21:18:19 -0700

    yaml-gen: ran fix-formatting

--- src/thunderbots/software/network_input/main.cpp
+++ src/thunderbots/software/network_input/main.cpp
@@ -13,8 +13,8 @@
 #include "thunderbots_msgs/World.h"
 #include "util/constants.h"
 #include "util/logger/init.h"
-#include "util/parameter/dynamic_parameters.h"
 #include "util/parameter/dynamic_parameter_utils.h"
+#include "util/parameter/dynamic_parameters.h"
 #include "util/ros_messages.h"
 #include "util/time/timestamp.h"
 
