6dd29eb5086d5397450b2da1d84041e01a902e2f
Jessica He <jessicahe21@hotmail.com>
2019-03-13 22:31:34 -0700

    All dynamic param files are still there, false alarm

--- src/thunderbots/software/util/parameter/dynamic_parameter_utils.cpp
+++ src/thunderbots/software/util/parameter/dynamic_parameter_utils.cpp
@@ -31,21 +31,4 @@ namespace Util::DynamicParameters
         Parameter<double>::updateAllParametersFromROSParameterServer();
         Parameter<std::string>::updateAllParametersFromROSParameterServer();
     }
-    
-    namespace Evaluation
-    {
-        namespace Indirect_Chip
-        {
-            Parameter<double> chip_target_fraction("chip_target_fraction", 5.0 / 10.0);
-            Parameter<double> chip_power_bounce_threshold("chip_power_bounce_threshold",
-                                                          7.5 / 10.0);
-            Parameter<double> max_chip_power("max_chip_power", 8.0);
-            Parameter<double> chip_target_area_inset("chip_target_area_inset", 0.3);
-            Parameter<double> min_chip_tri_area("min_chip_tri_area", 0.5);
-            Parameter<double> min_chip_tri_edge_len("min_chip_tri_edge_len", 0.8);
-            Parameter<double> min_chip_tri_edge_angle("min_chip_tri_edge_angle", 20);
-            Parameter<double> chip_cherry_power_downscale("chip_cherry_power_downscale",
-                                                          0.85);
-        }  // namespace Indirect_Chip
-    }      // namespace Evaluation
 }  // namespace Util::DynamicParameters
