2c820ea3fea5bde325eeaeaafcbbc6ada4a25ce1
Jordan Liu <jordan.zliu@gmail.com>
2019-03-23 15:55:51 -0700

    add a trivial straight line path planner

---
+++ src/thunderbots/software/ai/navigator/path_planner/straight_line_path_planner.cpp
@@ -0,0 +1,12 @@
+//
+// Created by jordan on 3/23/19.
+//
+
+#include "straight_line_path_planner.h"
+
+std::optional<std::vector<Point>>
+StraightLinePathPlanner::findPath(const Point &start, const Point &dest, const std::vector<Obstacle> &obstacles,
+                                  const ViolationFunction &violation_function) {
+    return std::make_optional<std::vector<Point>>({start, dest});
+}
+
