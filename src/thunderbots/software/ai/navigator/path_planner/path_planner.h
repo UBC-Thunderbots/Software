ae62cd24d83d1e29be2c7fb34a45681eaf30ae19
Jordan Liu <jordan.zliu@gmail.com>
2019-03-23 15:48:01 -0700

    comments and formatting

--- src/thunderbots/software/ai/navigator/path_planner/path_planner.h
+++ src/thunderbots/software/ai/navigator/path_planner/path_planner.h
@@ -1,15 +1,28 @@
 #pragma once
 #include <optional>
 #include <vector>
-#include "geom/point.h"
+
 #include "ai/navigator/obstacle/obstacle.h"
 #include "ai/navigator/path_planner/violation_function.h"
+#include "geom/point.h"
 
-class PathPlanner {
-public:
-    virtual std::optional <std::vector<Point>> findPath(
-            const Point &start, const Point &dest,
-            const std::vector <Obstacle> &obstacles,
-            const ViolationFunction &violation_function) = 0;
-    virtual ~PathPlanner() = default;
+class PathPlanner
+{
+   public:
+    /**
+     * Returns a path from start to dest given obstacles and a
+     * violation, otherwise returns std::nullopt if a path is
+     * not found.
+     *
+     * @param start start point
+     * @param dest destination point
+     * @param obstacles a vector of obstacles
+     * @param violation_function a function that returns the distance
+     *                          that a point is violating a boundary by
+     * @return
+     */
+    virtual std::optional<std::vector<Point>> findPath(
+        const Point &start, const Point &dest, const std::vector<Obstacle> &obstacles,
+        const ViolationFunction &violation_function) = 0;
+    virtual ~PathPlanner()                           = default;
 };
--- src/thunderbots/software/ai/navigator/path_planner/violation_function.h
+++ src/thunderbots/software/ai/navigator/path_planner/violation_function.h
@@ -2,7 +2,15 @@
 
 #include "geom/point.h"
 
-class ViolationFunction {
+class ViolationFunction
+{
+    /**
+     * Returns the distance that a point is violating a boundary
+     * by, in metres.
+     * @param point a Point
+     * @return the distance that a point is violating a boundary
+     * by, in metres.
+     */
     virtual double getViolationForPoint(const Point& point) = 0;
-    virtual ~ViolationFunction() = default;
+    virtual ~ViolationFunction()                            = default;
 };
