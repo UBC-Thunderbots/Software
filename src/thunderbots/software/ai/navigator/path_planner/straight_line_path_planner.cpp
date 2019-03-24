456ae18a0b83eab8d2e09f10db2bc7611ad4cdd5
Jordan Liu <jordan.zliu@gmail.com>
2019-03-24 15:56:23 -0700

    minor pr thing

--- src/thunderbots/software/ai/navigator/path_planner/straight_line_path_planner.cpp
+++ src/thunderbots/software/ai/navigator/path_planner/straight_line_path_planner.cpp
@@ -1,8 +1,4 @@
-//
-// Created by jordan on 3/23/19.
-//
-
-#include "straight_line_path_planner.h"
+#include "ai/navigator/path_planner/straight_line_path_planner.h"
 
 std::optional<std::vector<Point>> StraightLinePathPlanner::findPath(
     const Point &start, const Point &dest, const std::vector<Obstacle> &obstacles,
