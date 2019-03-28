a60bb0efc3ae4e02b92b4a8727f87c16f481a48f
HaomiaoZ <zhm7991@gmail.com>
2019-03-27 22:31:27 -0700

    intial partial implementation

--- src/thunderbots/software/network_input/filter/robot_filter.cpp
+++ src/thunderbots/software/network_input/filter/robot_filter.cpp
@@ -6,14 +6,47 @@ FilteredRobotData RobotFilter::getFilteredData(
     const std::vector<SSLRobotDetection> &new_robot_data)
 {
     FilteredRobotData filtered_data;
+
+    //std::vector<SSLRobotDetection> this_robot_data;
+
+    int data_num =0;
+    
     filtered_data.id               = 0;
-    filtered_data.position         = Point();
-    filtered_data.velocity         = Vector();
+    filtered_data.position         = Point(0,0);
+    filtered_data.velocity         = Vector(0,0);
     filtered_data.orientation      = Angle::zero();
     filtered_data.angular_velocity = AngularVelocity::zero();
-    filtered_data.timestamp        = Timestamp::fromSeconds(0);
+    filtered_data.timestamp        = Timestamp::fromMilliseconds(0);
 
-    return filtered_data;
+    for(const SSLRobotDetection &robot_data: new_robot_data)
+    {
+        //add all data points up and then average it
+        if(robot_data.id==this->getRobotId())
+        {
+
+            filtered_data.position         += robot_data.position;
+            filtered_data.orientation      += robot_data.orientation;
+            filtered_data.timestamp =  robot_data.timestamp;
+            data_num++;
+        }
+    }
+
+
+
+    //if no new data and timestamp difference is long enough, return filtered data as a indication that
+    //the robot is off the field? if no new data and timestamp difference is small enough, then return current robot position
+    if(data_num==0)
+    {
+
+    }
+    else
+    {
+        filtered_data.position         /= data_num;
+        filtered_data.orientation      /= data_num;
+
+        return filtered_data;
+    }
+
 }
 
 unsigned int RobotFilter::getRobotId() const
