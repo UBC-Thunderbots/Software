7ab170d8b143cd1ab4e034710284c1949612b3fe
roark <roarkzhang42@gmail.com>
2019-03-30 15:58:30 -0700

    initial commit

--- src/thunderbots/software/ai/navigator/trespass.cpp
+++ src/thunderbots/software/ai/navigator/trespass.cpp
@@ -1,8 +1,27 @@
 #include "ai/navigator/trespass.h"
 
+#include "util/math_functions.h"
+
 namespace Navigator
 {
     namespace Trespass
     {
-    }
+        double calcLinearTresspassScore(Rectangle r, Point p)
+        {
+            if (!r.containsPoint(p))
+                return 0.0;
+            else
+            {
+                double width_w  = p.x() - r.seCorner().x();
+                double width_e  = r.nwCorner().x() - p.x();
+                double height_s = p.y() - r.seCorner().y();
+                double height_n = r.nwCorner().y() - p.y();
+                return fmax(Util::linear(fmin(width_w, width_e), r.width() / 4.0,
+                                         r.width() / 2.0),
+                            Util::linear(fmin(height_s, height_n), r.height() / 4.0,
+                                         r.height() / 2.0));
+            }
+        }
+
+    }  // namespace Trespass
 }  // namespace Navigator
--- src/thunderbots/software/ai/navigator/trespass.h
+++ src/thunderbots/software/ai/navigator/trespass.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include "geom/rectangle.h"
+
 namespace Navigator
 {
     namespace Trespass
     {
-    }
+        /**
+         * Returns a tresspass score in the range [0, 1] for a rectangle
+         * If the point is outside the rectangle, returns 0
+         * If the point is at the centre of the rectangle, returns 1
+         *
+         * @param
+         */
+        double calcLinearTresspassScore(Rectangle r, Point p);
+
+    }  // namespace Trespass
 }  // namespace Navigator
