23e0c680476f88c7c8e19940c6c6bd0f0ced1e11
roark <roarkzhang42@gmail.com>
2019-03-30 12:06:21 -0700

    edited param description in linear util function

--- src/thunderbots/software/util/math_functions.h
+++ src/thunderbots/software/util/math_functions.h
@@ -10,7 +10,7 @@ namespace Util
      * Linearly maps an input value to an output value in the range [0,1]
      *
      * @param value The input value
-     * @param offset The value by which to offset the input value
+     * @param offset The input value at which the linear function will return 0.5
      * @param linear_width the total length required for the output value to go from 0 to
      * 1
      */
