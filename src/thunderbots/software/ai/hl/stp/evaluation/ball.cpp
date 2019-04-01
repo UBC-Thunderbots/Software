0e4646dfa2618fa6e075c6cb09fc4b9cd24fa36b
Jessica He <jessicahe21@hotmail.com>
2019-04-01 03:19:07 -0700

    Added skeleton for both functions

---
+++ src/thunderbots/software/ai/hl/stp/evaluation/ball.cpp
@@ -0,0 +1,13 @@
+#include "ball.h"
+#include "ai/world/world.h"
+#include "ai/world/ball.h"
+#include "ai/world/field.h"
+#include "geom/rectangle.h"
+
+bool Evaluation::ballInFriendlyHalf(const Field &field, const Ball &ball){
+
+}
+
+bool Evaluation::ballInEnemyHalf(const Field &field, const Ball &ball){
+
+}
