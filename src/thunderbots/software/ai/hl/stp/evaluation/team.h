2c44ed40ac1fa3faef1a8d3af4b4ed5e9ad6fde1
Mathew MacDougall <mmacd97@gmail.com>
2019-04-04 22:06:59 -0700

    Added overloaded version of nearestEnemy evaluation

--- src/thunderbots/software/ai/hl/stp/evaluation/team.h
+++ src/thunderbots/software/ai/hl/stp/evaluation/team.h
@@ -8,10 +8,21 @@ namespace Evaluation
     /**
      * Given a team, finds the robot on that team that is closest to a reference point.
      *
-     * @param team
+     * @param team the team of robots
      * @param ref_point The point where the distance to each robot will be measured.
      * @return Robot that is closest to the reference point.
      */
-    std::optional<Robot> nearest_robot(const Team team, const Point ref_point);
+    std::optional<Robot> nearestRobot(const Team &team, const Point &ref_point);
+
+    /**
+     * Given a list of robots, finds the robot on that team that is closest to a
+     * reference point.
+     *
+     * @param robots the list of robots
+     * @param ref_point The point where the distance to each robot will be measured.
+     * @return Robot that is closest to the reference point.
+     */
+    std::optional<Robot> nearestRobot(const std::vector<Robot> &robots,
+                                      const Point &ref_point);
 
 };  // namespace Evaluation
