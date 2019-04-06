1eb6f0e766f46f5585ecc8743243ceaf2783d950
Jonathan Lew <jonathan.lew@alumni.ubc.ca>
2019-04-05 22:38:41 -0700

    improved architecture of obstacles and added tentative implementation

--- src/thunderbots/software/ai/navigator/obstacle/obstacle.cpp
+++ src/thunderbots/software/ai/navigator/obstacle/obstacle.cpp
@@ -1,17 +1,119 @@
 
 #include "obstacle.h"
 
-const Polygon &Obstacle::getBoundaryPolygon() {
+const Polygon& Obstacle::getBoundaryPolygon()
+{
     return _polygon;
 }
 
-Obstacle::Obstacle():_polygon{
-        Point(0, 0),
-        Point(0, 1),
-        Point(1, 1),
-        Point(1, 0)
-} {}
+Obstacle::Obstacle() : _polygon{Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0)} {}
 
-Obstacle Obstacle::createPlaceholderObstacle() {
+Obstacle::Obstacle(Polygon polygon) : _polygon(polygon) {}
+
+Obstacle Obstacle::createPlaceholderObstacle()
+{
     return Obstacle();
+}
+
+static Obstacle Obstacle::createRobotObstacle(const Robot& robot,
+                                              bool enable_velocity_cushion)
+{
+    // TODO: figure out how to get tick length
+    double tick_length = 1.0;
+
+    // TODO: figure out actual factor (not 2) needed to clear two robot based on hexagon
+    // implementation
+    double radius_cushion = ROBOT_MAX_RADIUS_METERS * 2;
+
+    // vector in the direction of the velocity and with the scaled size of the velocity
+    Vector velocity_cushion_vector =
+        robot.velocity().norm(robot.velocity().len() * tick_length);
+
+    return createRobotObstacleFromPositionAndRadiusAndVelocity(
+        robot.position(), radius_cushion, velocity_cushion_vector,
+        enable_velocity_cushion && velocity_cushion.len() > radius);
+}
+
+static Obstacle Obstacle::createRobotObstacleWithScalingParams(
+    const Robot& robot, double radius_cushion_scaling, double velocity_cushion_scaling,
+    bool enable_velocity_cushion)
+{
+    // TODO: figure out how to get tick length
+    double tick_length = 1.0;
+
+    // TODO: figure out actual factor (not 2) needed to clear two robot based on hexagon
+    // implementation
+    double radius_cushion = ROBOT_MAX_RADIUS_METERS * 2 * robot_radius_scaling;
+
+    // vector in the direction of the velocity and with the scaled size of the velocity
+    Vector velocity_cushion_vector = robot.velocity().norm(
+        robot.velocity().len() * tick_length * velocity_cushion_scaling);
+
+    return createRobotObstacleFromPositionAndRadiusAndVelocity(
+        robot.position(), radius_cushion, velocity_cushion_vector,
+        enable_velocity_cushion && velocity_cushion.len() > radius);
+}
+
+static Obstacle Obstacle::createRobotObstacleWithBufferParams(
+    const Robot& robot, double radius_cushion_buffer, double velocity_cushion_buffer,
+    bool enable_velocity_cushion)
+{
+    // TODO: figure out how to get tick length
+    double tick_length = 1.0;
+
+    // TODO: figure out actual factor (not 2) needed to clear two robot based on hexagon
+    // implementation
+    double radius_cushion = ROBOT_MAX_RADIUS_METERS * 2 + radius_cushion_buffer;
+
+    // vector in the direction of the velocity and with the scaled size of the velocity
+    Vector velocity_cushion_vector = robot.velocity().norm(
+        robot.velocity().len() * tick_length + velocity_cushion_buffer);
+
+    return createRobotObstacleFromPositionAndRadiusAndVelocity(
+        robot.position(), radius_cushion, velocity_cushion_vector,
+        enable_velocity_cushion && velocity_cushion.len() > radius);
+}
+
+
+static Obstacle Obstacle::createRobotObstacleFromPositionAndRadiusAndVelocity(
+    Point position, double radius_cushion, Vector velocity_cushion_vector,
+    bool enable_velocity_cushion)
+{
+    if (enable_velocity_cushion)
+    {
+        Vector velocity_direction_norm_radius = velocity_cushion.norm(radius_cushion);
+        return Obstacle(Polygon(
+            {// left side of robot
+             position + velocity_direction_norm_radius.rotate(Angle::quarter()),
+             // back left of robot
+             position + velocity_direction_norm_radius.rotate(Angle::ofDegrees(150)),
+             // back right of robot
+             position + velocity_direction_norm_radius.rotate(Angle::ofDegrees(210)),
+             // right side of robot
+             position + velocity_direction_norm_radius.rotate(Angle::threeQuarter()),
+             // right side velocity cushions
+             position + velocity_direction_norm_radius.rotate(Angle::threeQuarter()) +
+                 velocity_cushion,
+             // left side velocity cushions
+             position + velocity_direction_norm_radius.rotate(Angle::quarter()) +
+                 velocity_cushion}));
+    }
+    else
+    {
+        // force the robot to face in +x direction
+        Vector facing_direction_norm_radius = Point(1, 0).norm(radius);
+        return Obstacle(Polygon(
+            {// left side of robot
+             position + facing_direction_norm_radius.rotate(Angle::quarter()),
+             // back left of robot
+             position + facing_direction_norm_radius.rotate(Angle::ofDegrees(150)),
+             // back right of robot
+             position + facing_direction_norm_radius.rotate(Angle::ofDegrees(210)),
+             // right side of robot
+             position + facing_direction_norm_radius.rotate(Angle::threeQuarter()),
+             // front right velocity cushions
+             position + facing_direction_norm_radius.rotate(Angle::ofDegrees(330)),
+             // front left velocity cushions
+             position + facing_direction_norm_radius.rotate(Angle::ofDegrees(30))}));
+    }
 }
--- src/thunderbots/software/ai/navigator/obstacle/obstacle.h
+++ src/thunderbots/software/ai/navigator/obstacle/obstacle.h
@@ -8,16 +8,74 @@
 #include "geom/point.h"
 #include "geom/polygon.h"
 #include "geom/util.h"
+#include "shared/constants.h"
 
 class Obstacle
 {
-public:
+   public:
     static Obstacle createPlaceholderObstacle();
-    static Obstacle createRobotObstacle(const Robot& robot);
+
+    static Obstacle createRobotObstacle(const Robot& robot, bool enable_velocity_cushion);
+
+    /*
+     * Gets the boundary polygon around the given robot obstacle that other robots
+     * should not enter with a buffer scaled by radius_scaling and
+     * velocity_cushion_scaling
+     *
+     * @param robot robot to create obstacle boundary polygon around
+     * @param radius_scaling multiplicatively scales the radius of the obstacle
+     * @param velocity_cushion_scaling multiplicatively scales the cushion of
+     * velocity of the robot
+     * @param enable_velocity_cushion if true, enables the cushion of the velocity
+     * of the robot as component of the obstacle
+     *
+     * @note the radius is by default the minimum distance needed for two robots to clear
+     * each other
+     * @note the velocity cushion is by default the distance that can be travelled in
+     * one tick
+     *
+     * @return a six-sided Polygon to represent the boundary around the obstacle
+     */
+    static Obstacle createRobotObstacleWithScalingParams(const Robot& robot,
+                                                         double radius_cushion_scaling,
+                                                         double velocity_cushion_scaling,
+                                                         bool enable_velocity_cushion);
+
+    /*
+     * Gets the boundary polygon around the given robot obstacle that other robots
+     * should not enter with a buffer scaled by radius_scaling and
+     * velocity_cushion_scaling
+     *
+     * @param robot robot to create obstacle boundary polygon around
+     * @param radius_buffer additively increases the radius of the obstacle by specified
+     * amount
+     * @param velocity_cushion_buffer additively increases the cushion of velocity
+     * of the robot
+     * @param enable_velocity_cushion if true, enables the cushion of the velocity
+     * of the robot as component of the obstacle
+     *
+     * @note the radius is by default the minimum distance needed for two robots to clear
+     * each other
+     * @note the velocity cushion is by default the distance that can be travelled in
+     * one tick
+     *
+     * @return a six-sided Polygon to represent the boundary around the obstacle
+     */
+    static Obstacle createRobotObstacleWithBufferParams(const Robot& robot,
+                                                        double radial_cushion_buffer,
+                                                        double velocity_cushion_buffer,
+                                                        bool enable_velocity_cushion);
+
     static Obstacle createBallObstacle(const Ball& ball);
-    const Polygon &getBoundaryPolygon() const;
-private:
+
+    const Polygon& getBoundaryPolygon() const;
+
+   private:
     // placeholder obstacle is a 1x1 square
     Obstacle();
+    Obstacle(Polygon polygon);
+    static Obstacle createRobotObstacleFromPositionAndRadiusAndVelocity(
+        Point position, double radius_cushion, Vector velocity_cushion_vector,
+        bool enable_velocity_cushion);
     Polygon _polygon;
 };
