97b51718f351a0a22c81e8e1374c76bdf72d006d
Gareth Ellis <gareth.ellis0@gmail.com>
2019-04-06 20:52:55 -0700

    Implemented Passer Tactic

---
+++ src/thunderbots/software/ai/hl/stp/tactic/passer_tactic.cpp
@@ -0,0 +1,45 @@
+#include "ai/hl/stp/tactic/passer_tactic.h"
+#include "ai/hl/stp/action/kick_action.h"
+
+#include "geom/util.h"
+#include "shared/constants.h"
+#include "util/logger/init.h"
+
+using namespace AI::Passing;
+
+PasserTactic::PasserTactic(Pass pass, bool loop_forever)
+    : pass(std::move(pass)), Tactic(loop_forever)
+{
+}
+
+std::string PasserTactic::getName() const
+{
+    return "Passer Tactic";
+}
+
+void PasserTactic::updateParams(const Pass& updated_pass)
+{
+    this->pass = updated_pass;
+}
+
+double PasserTactic::calculateRobotCost(const Robot& robot, const World& world)
+{
+    // Prefer robots closer to the pass start position
+    // We normalize with the total field length so that robots that are within the field
+    // have a cost less than 1
+    double cost = (robot.position() - pass.passerPoint()).len() / world.field().totalLength();
+    return std::clamp<double>(cost, 0, 1);
+}
+
+std::unique_ptr<Intent> PasserTactic::calculateNextIntent(
+    intent_coroutine::push_type& yield)
+{
+    KickAction kick_action = KickAction();
+    do
+    {
+        // We want the robot to move to the starting position for the shot and also
+        // rotate to the correct orientation to face the shot
+        yield(kick_action.updateStateAndGetNextIntent(*robot, pass.passerPoint(),
+                                                      pass.passerOrientation(), pass.speed()));
+    } while (!kick_action.done());
+}
---
+++ src/thunderbots/software/ai/hl/stp/tactic/passer_tactic.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "ai/hl/stp/tactic/tactic.h"
+#include "ai/passing/pass.h"
+
+/**
+ * This tactic is for a robot performing a pass. It should be used in conjunction with
+ * the `ReceiverTactic` in order to complete the pass.
+ *
+ * Note that this tactic does not take into account the time the pass should occur at,
+ * it simply tries to move to the best position to take the pass as fast as possible
+ */
+class PasserTactic : public Tactic
+{
+   public:
+    /**
+     * Creates a new PasserTactic
+     *
+     * @param pass The pass this tactic should try to execute
+     * @param loop_forever Whether or not this Tactic should never complete. If true, the
+     * tactic will be restarted every time it completes
+     */
+    explicit PasserTactic(AI::Passing::Pass pass, bool loop_forever);
+
+    std::string getName() const override;
+
+    /**
+     * Updates the parameters for this PasserTactic.
+     *
+     * @param pass The pass to perform
+     */
+    void updateParams(const AI::Passing::Pass& updated_pass);
+
+    /**
+     * Calculates the cost of assigning the given robot to this Tactic. Prefers robots
+     * closer to the block destination
+     *
+     * @param robot The robot to evaluate the cost for
+     * @param world The state of the world with which to perform the evaluation
+     * @return A cost in the range [0,1] indicating the cost of assigning the given robot
+     * to this tactic. Lower cost values indicate a more preferred robot.
+     */
+    double calculateRobotCost(const Robot& robot, const World& world) override;
+
+   private:
+    std::unique_ptr<Intent> calculateNextIntent(
+        intent_coroutine::push_type& yield) override;
+
+    // Tactic parameters
+    // The pass this tactic is executing
+    AI::Passing::Pass pass;
+};
