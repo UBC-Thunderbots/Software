d3c55343ed093f27206213fae28c01cc1b19bd1a
akhil <akhil@komputer.hitronhub.home>
2019-04-08 21:23:53 -0700

    added another test

--- src/thunderbots/software/test/ai/hl/stp/action/pivot_action.cpp
+++ src/thunderbots/software/test/ai/hl/stp/action/pivot_action.cpp
@@ -5,9 +5,6 @@
 #include "ai/intent/pivot_intent.h"
 #include "ai/intent/move_intent.h"
 
-class PivotActionTest
-    : public ::testing::TestWithParam<Point>
-{ };
 
 // PivotAction should be yeilding move_intents as the robot is too far away from orbit
 TEST(PivotActionTest, robot_too_far_from_orbit)
@@ -28,6 +25,29 @@ TEST(PivotActionTest, robot_too_far_from_orbit)
         EXPECT_EQ(0, move_intent.getRobotId());
     } catch(...){
         ADD_FAILURE() << "Move intent not returned by pivot action, even though robot was far away!";
+    }
+
+}
+
+// PivotAction should be yeilding pivot_intents as the robot is in orbit
+TEST(PivotActionTest, robot_in_robit)
+{
+    Robot robot       = Robot(0, Point(10, 10), Vector(), Angle::zero(),
+                        AngularVelocity::zero(), Timestamp::fromSeconds(0));
+    PivotAction action = PivotAction();
+
+    auto intent_ptr =
+        action.updateStateAndGetNextIntent(robot, Point(0, 0), Angle::zero(), 1.0);
+
+    // Check an intent was returned (the pointer is not null)
+    EXPECT_TRUE(intent_ptr);
+    EXPECT_FALSE(action.done());
+
+    try{
+        PivotIntent move_intent = dynamic_cast<PivotIntent &>(*intent_ptr);
+        EXPECT_EQ(0, move_intent.getRobotId());
+    } catch(...){
+        ADD_FAILURE() << "Pivot intent not returned by pivot action, even though robot was in orbit!";
     }
 
 }
