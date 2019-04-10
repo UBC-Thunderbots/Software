3e848d5dec5dad383776505c8cd1dd9aff5c82aa
Muchen He <i@muchen.ca>
2019-04-09 21:33:17 -0700

    CalcLinearTresspass Impl

--- src/thunderbots/software/ai/navigator/trespass.cpp
+++ src/thunderbots/software/ai/navigator/trespass.cpp
@@ -1,8 +1,24 @@
 #include "ai/navigator/trespass.h"
 
+#include "geom/circle.h"
+#include "geom/point.h"
+
 namespace Navigator
 {
     namespace Trespass
     {
+        unsigned int calcLinearTrespassScore(Circle circle, Point point)
+        {
+            if (point.isnan())
+                return 0;
+            
+            // The point is in the circle if the distance to 
+            // the center of the circle is less than the radius
+            const double dx = point.x() - circle.getOrigin().x();
+            const double dy = point.y() - circle.getOrigin().y();
+            const double r  = circle.getRadius();
+
+            return dx * dx + dy * dy > r * r;
+        }
     }
 }  // namespace Navigator
--- src/thunderbots/software/ai/navigator/trespass.h
+++ src/thunderbots/software/ai/navigator/trespass.h
@@ -1,8 +1,21 @@
 #pragma once
 
+class Circle;
+class Point;
+
 namespace Navigator
 {
     namespace Trespass
     {
+
+        /**
+         * Return 0 or 1 to indicate if a point is in a circle
+         * 
+         * @param[in] Circle circle: the reference ccircle
+         * @param[in] Point point: the point of interest
+         * 
+         * @return 1 if the point is in the circle and 0 otherwise
+         */
+        unsigned int calcLinearTrespassScore(Circle circle, Point point);
     }
 }  // namespace Navigator
