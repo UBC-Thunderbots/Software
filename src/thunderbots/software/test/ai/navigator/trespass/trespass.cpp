9cc6d74f1f682c79f35594f5c24b92aefa551959
roark <roarkzhang42@gmail.com>
2019-04-20 11:59:50 -0700

    fixed bug in rectangle trespass function, added missing file in cmakelists

--- src/thunderbots/software/test/ai/navigator/trespass/trespass.cpp
+++ src/thunderbots/software/test/ai/navigator/trespass/trespass.cpp
@@ -14,21 +14,21 @@ TEST(TrespassTest, calcLinearTrespassScore_rectangle_test)
     Rectangle rect   = Rectangle(Point(0, 0), 6, 2);
     Point outside    = Point(-1, -2);
     Point corner     = Point(0, 0);
-    Point edge       = Point(6, 1);
+    Point edge_e     = Point(6, 1);
+    Point edge_s     = Point(3, 0);
     Point centre     = Point(3, 1);
-    Point quarter_x  = Point(1.5, 1);
-    Point quarter_y  = Point(3, 0.5);
     Point quarter    = Point(1.5, 0.5);
-    Point two_thirds = Point(4, 1);
+    Point quarter_y  = Point(3, 0.5);
+    Point two_thirds = Point(4, 0.2);
 
     EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, outside), 0);
     EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, corner), 0);
-    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, edge), 0);
-    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, centre), 0);
-    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, quarter_x), 0);
-    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, quarter_y), 0);
-    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, quarter), 0);
-    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, two_thirds), 0);
+    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, edge_e), 0);
+    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, edge_s), 0);
+    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, centre), 1);
+    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, quarter), 0.5);
+    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, quarter_y), 1);
+    EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, two_thirds), 2.0 / 3.0);
 }
 
 int main(int argc, char **argv)
