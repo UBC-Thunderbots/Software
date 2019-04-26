06d0fba4a2686584cc3031cbd9c5286ac120ad9a
Gareth Ellis <gareth.ellis0@gmail.com>
2019-04-26 00:18:07 -0700

    wip

--- src/thunderbots/software/util/canvas_messenger/canvas_messenger.cpp
+++ src/thunderbots/software/util/canvas_messenger/canvas_messenger.cpp
@@ -2,138 +2,201 @@
 
 #include "util/constants.h"
 
-namespace Util
+using namespace Util;
+
+std::shared_ptr<CanvasMessenger> CanvasMessenger::getInstance()
 {
-    std::shared_ptr<CanvasMessenger> CanvasMessenger::getInstance()
+    static std::shared_ptr<CanvasMessenger> canvas_messenger(new CanvasMessenger);
+    return canvas_messenger;
+}
+
+void CanvasMessenger::initializePublisher(ros::NodeHandle node_handle)
+{
+    this->publisher = node_handle.advertise<thunderbots_msgs::CanvasLayer>(
+        Util::Constants::VISUALIZER_DRAW_LAYER_TOPIC, BUFFER_SIZE);
+}
+
+void CanvasMessenger::publishAndClearLayers()
+{
+    // Take ownership of the layers for the duration of this function
+    std::lock_guard<std::mutex> best_known_pass_lock(layers_lock);
+
+    // Limit rate of the message publishing
+    // Get the time right now
+    const std::chrono::time_point<std::chrono::system_clock> now =
+        std::chrono::system_clock::now();
+    const int64_t elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                                   now - time_last_published)
+                                   .count();
+    const double elapsed_ms = elapsed_ns / 1.0e6;
+
+    // Do not do anything if the time passed hasn't been
+    // long enough
+    if (elapsed_ms < DESIRED_PERIOD_MS)
+        return;
+
+    // Send a payload per layer of messages
+    for (const auto& layer_pair : this->layers_map)
     {
-        static std::shared_ptr<CanvasMessenger> canvas_messenger(new CanvasMessenger);
-        return canvas_messenger;
+        // First is the layer number
+        const uint8_t layer_number = layer_pair.first;
+
+        // Second is the vector that contains the sprites
+        const std::vector<Sprite>& sprites = layer_pair.second;
+
+        // Only send the non-empty layers
+//            if (!layer_pair.second.empty())
+//            {
+            this->publishPayload(layer_number, sprites);
+//            }
     }
 
-    void CanvasMessenger::initializePublisher(ros::NodeHandle node_handle)
+    // Clear shapes in layers of current frame/tick
+    this->clearLayers();
+
+    // Update last published time
+    time_last_published = now;
+}
+
+void CanvasMessenger::publishPayload(uint8_t layer,
+                                     std::vector<Sprite> sprites)
+{
+    std::vector<uint8_t> payload;
+
+    // Add the layer number at the begining of the binary message
+    payload.insert(payload.end(), layer);
+
+    // Convert each sprite into a binary message
+    for (Sprite& sprite : sprites)
     {
-        this->publisher = node_handle.advertise<thunderbots_msgs::CanvasLayer>(
-            Util::Constants::VISUALIZER_DRAW_LAYER_TOPIC, BUFFER_SIZE);
+        std::vector<uint8_t> sprite_payload = sprite.serialize();
+        payload.insert(payload.end(), sprite_payload.begin(), sprite_payload.end());
     }
 
-    void CanvasMessenger::publishAndClearLayers()
+    // Create a new layer message, add the binary data
+    thunderbots_msgs::CanvasLayer new_layer;
+    new_layer.data = payload;
+
+    // and publish
+    this->publisher.publish(new_layer);
+}
+
+void CanvasMessenger::clearLayers()
+{
+    // Clears all sprite vector in all the layers
+    for (auto& layer : this->layers_map)
     {
-        // Limit rate of the message publishing
-        // Get the time right now
-        const std::chrono::time_point<std::chrono::system_clock> now =
-            std::chrono::system_clock::now();
-        const int64_t elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                                       now - time_last_published)
-                                       .count();
-        const double elapsed_ms = elapsed_ns / 1.0e6;
+        layer.second.clear();
+//            Util::CanvasMessenger::getInstance()->drawPoint(Point(), 0.5, 255, 0, 0);
+    }
+}
 
-        // Do not do anything if the time passed hasn't been
-        // long enough
-        if (elapsed_ms < DESIRED_PERIOD_MS)
-            return;
 
-        // Send a payload per layer of messages
-        for (const std::pair<uint8_t, std::vector<Sprite>>& layer_pair : this->layers_map)
-        {
-            // First is the layer number
-            const uint8_t layer_number = layer_pair.first;
+void CanvasMessenger::drawSprite(Layer layer, Sprite sprite)
+{
+    // We simply add the sprite to the specified layer
+    this->addSpriteToLayer(layer, sprite);
+}
 
-            // Second is the vector that contains the sprites
-            const std::vector<Sprite>& sprites = layer_pair.second;
+void CanvasMessenger::addSpriteToLayer(Layer layer, Sprite &sprite)
+{
+    // Take ownership of the layers for the duration of this function
+    std::lock_guard<std::mutex> best_known_pass_lock(layers_lock);
 
-            // Only send the non-empty layers
-            if (!layer_pair.second.empty())
-            {
-                this->publishPayload(layer_number, sprites);
-            }
-        }
-
-        // Clear shapes in layers of current frame/tick
-        this->clearLayers();
-
-        // Update last published time
-        time_last_published = now;
+    // We look if the layer exists
+    if (this->layers_map.find(layer) == this->layers_map.end())
+    {
+        // If not, we initialize new key to empty vector pair
+        this->layers_map[layer];
     }
 
-    void CanvasMessenger::publishPayload(uint8_t layer,
-                                         const std::vector<Sprite>& sprites)
-    {
-        std::vector<uint8_t> payload;
+    // and add the sprite to the layer vector
+    this->layers_map[layer].emplace_back(sprite);
+}
 
-        // Add the layer number at the begining of the binary message
-        payload.insert(payload.end(), layer);
+void CanvasMessenger::drawRectangle(Layer layer, Rectangle rectangle, Angle orientation, Color color) {
+    Sprite rectangle_sprite(0, rectangle.centre(), orientation, rectangle.width(), rectangle.height(), color);
 
-        // Convert each sprite into a binary message
-        for (const Sprite& sprite : sprites)
-        {
-            payload.insert(payload.end(), sprite.texture);
+    drawSprite(layer, rectangle_sprite);
+}
 
-            // These are all 16 bits, we need to split them
-            Int16OrTwoInt8 x;
-            x.base = sprite.x;
-            payload.insert(payload.end(), x.result[1]);
-            payload.insert(payload.end(), x.result[0]);
+//void CanvasMessenger::drawPoint(Point p, double radius, int r, int g, int b,
+//                                int opacity) {
+//    Sprite sprite;
+//
+//    sprite.x = std::round(p.x() * PIXELS_PER_METER - (radius*PIXELS_PER_METER/2));
+//    sprite.y =  std::round(-p.y() * PIXELS_PER_METER - (radius*PIXELS_PER_METER/2));
+//
+//    sprite.width = radius * PIXELS_PER_METER;
+//    sprite.height = radius * PIXELS_PER_METER;
+//
+////        sprite.red = r;
+////        sprite.green = g;
+////        sprite.blue = b;
+////        sprite.opacity = opacity;
+//
+//    // 1 is a circle
+//    sprite.texture = 0;
+//
+//    drawSprite(1, sprite);
+//}
 
-            Int16OrTwoInt8 y;
-            y.base = sprite.y;
-            payload.insert(payload.end(), y.result[1]);
-            payload.insert(payload.end(), y.result[0]);
+//void CanvasMessenger::drawField(const Field &field) {
+//    Sprite field_sprite;
+//    field_sprite.width = field.length() * PIXELS_PER_METER;
+//    field_sprite.height = field.width() * PIXELS_PER_METER;
+//    field_sprite.x = std::round(-field.length() * PIXELS_PER_METER/2);
+//    field_sprite.y = std::round(-field.width() * PIXELS_PER_METER/2);
+//
+//    drawSprite(0, field_sprite);
+//}
 
-            Int16OrTwoInt8 width;
-            width.base = sprite.width;
-            payload.insert(payload.end(), width.result[1]);
-            payload.insert(payload.end(), width.result[0]);
 
-            Int16OrTwoInt8 height;
-            height.base = sprite.height;
-            payload.insert(payload.end(), height.result[1]);
-            payload.insert(payload.end(), height.result[0]);
 
-            Int16OrTwoInt8 rotation;
-            rotation.base = sprite.rotation;
-            payload.insert(payload.end(), rotation.result[1]);
-            payload.insert(payload.end(), rotation.result[0]);
 
-            payload.insert(payload.end(), sprite.opacity);
-            payload.insert(payload.end(), sprite.red);
-            payload.insert(payload.end(), sprite.green);
-            payload.insert(payload.end(), sprite.blue);
-        }
 
-        // Create a new layer message, add the binary data
-        thunderbots_msgs::CanvasLayer new_layer;
-        new_layer.data = payload;
 
-        // and publish
-        this->publisher.publish(new_layer);
-    }
 
-    void CanvasMessenger::clearLayers()
-    {
-        // Clears all sprite vector in all the layers
-        for (auto& layer : this->layers_map)
-        {
-            layer.second.clear();
-        }
-    }
+Point CanvasMessenger::Sprite::getTopLeftCorner() {
+    return _center + Vector(_width, _height).rotate(_orientation);
+}
 
-    void CanvasMessenger::drawSprite(uint8_t layer, Sprite sprite)
-    {
-        // We simply add the sprite to the specified layer
-        this->addSpriteToLayer(layer, sprite);
-    }
 
-    void CanvasMessenger::addSpriteToLayer(uint8_t layer, Sprite& sprite)
-    {
-        // We look if the layer exists
-        if (this->layers_map.find(layer) == this->layers_map.end())
-        {
-            // If not, we initialize new key to empty vector pair
-            this->layers_map[layer];
-        }
+std::vector<uint8_t> CanvasMessenger::Sprite::serialize() {
+    std::vector<uint8_t> payload;
 
-        // and add the sprite to the layer vector
-        this->layers_map[layer].emplace_back(sprite);
-    }
+    Point top_left_corner = getTopLeftCorner();
 
-}  // namespace Util
+    // These are all 16 bits, we need to split them
+    Int16OrTwoInt8 x;
+    x.base = top_left_corner.x();
+    payload.insert(payload.end(), x.result[1]);
+    payload.insert(payload.end(), x.result[0]);
+
+    Int16OrTwoInt8 y;
+    y.base = top_left_corner.y();
+    payload.insert(payload.end(), y.result[1]);
+    payload.insert(payload.end(), y.result[0]);
+
+    Int16OrTwoInt8 width;
+    width.base = _width;
+    payload.insert(payload.end(), width.result[1]);
+    payload.insert(payload.end(), width.result[0]);
+
+    Int16OrTwoInt8 height;
+    height.base = _height;
+    payload.insert(payload.end(), height.result[1]);
+    payload.insert(payload.end(), height.result[0]);
+
+    Int16OrTwoInt8 orientation;
+    orientation.base = _orientation.toDegrees();
+    payload.insert(payload.end(), orientation.result[1]);
+    payload.insert(payload.end(), orientation.result[0]);
+
+    payload.insert(payload.end(), _color.a);
+    payload.insert(payload.end(), _color.r);
+    payload.insert(payload.end(), _color.g);
+    payload.insert(payload.end(), _color.b);
+
+    return payload;
+}
--- src/thunderbots/software/util/canvas_messenger/canvas_messenger.h
+++ src/thunderbots/software/util/canvas_messenger/canvas_messenger.h
@@ -16,49 +16,88 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <mutex>
 
 #include "thunderbots_msgs/CanvasLayer.h"
 #include "util/constants.h"
 
+#include "ai/world/field.h"
+
 namespace Util
 {
+
     class CanvasMessenger
     {
        public:
+        enum class Layer{
+            STATIC_FEATURES,
+            ROBOTS_AND_BALL
+        };
+
+        struct Color {
+            // Red value of the color
+            uint8_t r;
+            // Green value of the color
+            uint8_t g;
+            // Blue value of the color
+            uint8_t b;
+            // Alpha (transparency) value of the color
+            uint8_t a;
+        };
+
+        // TODO: Update this comment
         /**
          * Sprite is a struct that contains all the information
          * necessary to create a sprite. The default constructor
          * creates a 100x100 white rectangle sprite.
          */
-        typedef struct Sprite
-        {
-            Sprite()
-                : texture(0),
-                  x(0.0),
-                  y(0.0),
-                  width(100.0),
-                  height(100.0),
-                  rotation(0.0),
-                  opacity(255),
-                  red(255),
-                  green(255),
-                  blue(255)
-            {
-            }
+        class Sprite {
+        public:
+            // delete the default constructor
+            Sprite() = delete;
 
-            uint8_t texture;
-            int16_t x;
-            int16_t y;
-            int16_t width;
-            int16_t height;
-            int16_t rotation;
-            uint8_t opacity;
-            uint8_t red;
-            uint8_t green;
-            uint8_t blue;
-        } Sprite;
+            /**
+             * Create a Sprite
+             * @param texture The texture id. On the visualizer side this will determine
+             *        what sprite this is (circle, rectangle, robot, etc.)
+             * @param center The center of the sprite
+             * @param orientation The orientation of the sprite
+             * @param width The width of the sprite (in meters, will be translated to pixels)
+             * @param height The height of the sprite (in meters, will be translated to pixels)
+             * @param color The color of the sprite
+             */
+            Sprite(uint8_t texture, const Point& center, const Angle& orientation, double width, double height, Color color)
+                    : _texture(texture),
+                      _center(center),
+                      _orientation(orientation),
+                      _color(color){}
 
-       public:
+                      /**
+                       * Get the top left corner of the sprite
+                       *
+                       * @return the top left corner of the sprite
+                       */
+                      Point getTopLeftCorner();
+
+            /**
+             * Get the serialized form of this sprite
+             *
+             * This is the form that is streamed over to the web interface
+             *
+             * @return the serialized form of this sprite
+             */
+            std::vector<uint8_t> serialize();
+
+        private:
+            // TODO: Comment these
+            uint8_t _texture;
+            Point _center;
+            double _width;
+            double _height;
+            Angle _orientation;
+            Color _color;
+        };
+
         /**
          * Getter of the singleton object.
          *
@@ -80,14 +119,31 @@ namespace Util
         void clearLayers();
 
         /**
-         * Draw a sprite onto a specific layer.
+         * Draw a rectangle on the given layer
          *
-         * @param layer: The layer number this shape is being drawn to
-         * @param sprite: the sprite data to draw
+         * @param layer The layer to draw the rectangle on
+         * @param rectangle The rectangle to draw (units are in meters)
+         * @param color The color the rectangle should be
          */
-        void drawSprite(uint8_t layer, Sprite sprite);
+        void drawRectangle(Layer layer, Rectangle rectangle, Angle orientation, Color color);
+
+        /**
+         * Draw a point at a given location with a given radius
+         *
+         * @param p The point to draw
+         * // TODO: Units for the radius???
+         * @param radius The radius to draw the point with
+         */
+        void drawPoint(Point p, double radius, int r, int g, int b, int opacity);
+
+        /**
+         * Draw the given field
+         * @param field
+         */
+        void drawField(const Field& field);
 
        private:
+
         /**
          * Union used to convert a int16_t into two uint8_t
          */
@@ -96,24 +152,34 @@ namespace Util
             uint8_t result[2];
         };
 
-       private:
+        // The number of pixels per meter
+        static const int PIXELS_PER_METER = 100;
+
         /**
          * Constructor; initializes an empty layers map then populates it
          */
         explicit CanvasMessenger() : layers_map(), publisher() {}
 
-        void publishPayload(uint8_t layer, const std::vector<Sprite>& shapes);
+        void publishPayload(uint8_t layer, std::vector<Sprite> shapes);
 
         /**
          * Add sprite to layer
          * @param layer: The layer which the sprite is to be added to
-         * @param sprite_data: The sprite data
+         * @param sprite: The sprite data
          */
-        void addSpriteToLayer(uint8_t layer, Sprite& sprite_data);
+        void addSpriteToLayer(Layer layer, Sprite &sprite);
 
-       private:
+        /**
+         * Draw a sprite onto a specific layer.
+         *
+         * @param layer: The layer number this shape is being drawn to
+         * @param sprite: the sprite data to draw
+         */
+        void drawSprite(Layer layer, Sprite sprite);
+
+
         // layer to sprite data map
-        std::map<uint8_t, std::vector<Sprite>> layers_map;
+        std::map<Layer, std::vector<Sprite>> layers_map;
         ros::Publisher publisher;
 
         // Period in nanoseconds
@@ -125,5 +191,8 @@ namespace Util
 
         // Time point
         std::chrono::time_point<std::chrono::system_clock> time_last_published;
+
+        // The mutex for the layers
+        std::mutex layers_lock;
     };
 }  // namespace Util
