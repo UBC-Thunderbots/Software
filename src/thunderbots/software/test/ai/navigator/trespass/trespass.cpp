3a18790c7acf3d01a4a433c5a99a6a57d7b3eed8
roark <roarkzhang42@gmail.com>
2019-04-27 11:59:13 -0700

    added comments describing the tests in the unit tests

--- src/thunderbots/software/test/ai/navigator/trespass/trespass.cpp
+++ src/thunderbots/software/test/ai/navigator/trespass/trespass.cpp
@@ -21,13 +21,28 @@ TEST(TrespassTest, calcLinearTrespassScore_rectangle_test)
     Point quarter_y  = Point(3, 0.5);
     Point two_thirds = Point(4, 0.2);
 
+    // Test point outside the rectangle. Expect the function to return 0.
     EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, outside), 0);
+
+    // Test point on the corner of the rectangle.
     EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, corner), 0);
+
+    // Test point halfway along the right edge of the rectangle.
     EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, edge_e), 0);
+
+    // Test point halfway along the bottom edge of the rectangle.
     EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, edge_s), 0);
+
+    // Test point at the centre of the rectangle.
     EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, centre), 1);
+
+    // Test point at a quarter of the width and height of the rectangle.
     EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, quarter), 0.5);
+
+    // Test point at a quarter of the height but at half the width of the rectangle.
     EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, quarter_y), 1);
+
+    // Test point at two-thirds the width of the rectangle and one tenth the height.
     EXPECT_EQ(Navigator::Trespass::calcLinearTrespassScore(rect, two_thirds), 2.0 / 3.0);
 }
 
