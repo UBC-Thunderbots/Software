810a748a2ebf31ec7f93c4940fdee46608a25179
Akhil Veeraghanta <31548846+akhilveeraghanta@users.noreply.github.com>
2019-04-28 10:09:00 -0700

    Ball in friendly/enemy corner (#567)
    * Ball in friendly/enemy corner
    
    * fixed failing test
    
    * Removed commented code
    
    * added check for on/off field and updated tests

--- src/thunderbots/software/ai/hl/stp/evaluation/ball.h
+++ src/thunderbots/software/ai/hl/stp/evaluation/ball.h
@@ -20,4 +20,26 @@ namespace Evaluation
      * @return true if the ball is in the enemy's half of the field, and false otherwise
      */
     bool ballInEnemyHalf(const Field &field, const Ball &ball);
+
+    /**
+     * Returns true if the ball is in withing the provided radius in one of the friendly
+     * corner.
+     *
+     * @param field The field
+     * @param ball The ball
+     * @param the radius from the corner, to decide whether or not the ball is in the
+     * field.
+     */
+    bool ballInFriendlyCorner(const Field &field, const Ball &ball, double radius);
+
+    /**
+     * Returns true if the ball is in withing the provided radius in one of the enemy
+     * corner.
+     *
+     * @param field The field
+     * @param ball The ball
+     * @param the radius from the corner, to decide whether or not the ball is in the
+     * field.
+     */
+    bool ballInEnemyCorner(const Field &field, const Ball &ball, double radius);
 }  // namespace Evaluation
