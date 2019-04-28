a75c466225ceea2fbe56a1d4ae1326d151bfa3a3
akhil <akhil.veeraghanta@outlook.com>
2019-04-28 09:25:12 -0700

    added check for on/off field and updated tests

--- src/thunderbots/software/ai/hl/stp/evaluation/ball.cpp
+++ src/thunderbots/software/ai/hl/stp/evaluation/ball.cpp
@@ -16,13 +16,15 @@ namespace Evaluation
 
     bool ballInFriendlyCorner(const Field &field, const Ball &ball, double radius)
     {
-        return ball.position().isClose(field.friendlyCornerPos(), radius) ||
-               ball.position().isClose(field.friendlyCornerNeg(), radius);
+        return (ball.position().isClose(field.friendlyCornerPos(), radius) ||
+                ball.position().isClose(field.friendlyCornerNeg(), radius)) &&
+               field.pointInFieldLines(ball.position());
     }
 
     bool ballInEnemyCorner(const Field &field, const Ball &ball, double radius)
     {
-        return ball.position().isClose(field.enemyCornerPos(), radius) ||
-               ball.position().isClose(field.enemyCornerNeg(), radius);
+        return (ball.position().isClose(field.enemyCornerPos(), radius) ||
+                ball.position().isClose(field.enemyCornerNeg(), radius)) &&
+               field.pointInFieldLines(ball.position());
     }
 }  // namespace Evaluation
