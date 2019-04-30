d3424b82cf09084aa0634c2d7a60d5ac7f7f6383
Mathew MacDougall <mmacd97@gmail.com>
2019-04-30 16:33:07 -0700

    pseudocode

--- src/thunderbots/software/network_input/filter/ball_filter.cpp
+++ src/thunderbots/software/network_input/filter/ball_filter.cpp
@@ -13,6 +13,28 @@ BallFilter::BallFilter() : previous_ball_readings(5) {}
 Ball BallFilter::getFilteredData(const Ball& current_ball_state,
                                  const std::vector<SSLBallDetection>& new_ball_detections)
 {
+    // Pseudocode
+    // Create a big buffer of ball detection data
+    // add data to the buffer (should be split by timestamp?)
+    // if the new data is very far from the line, reject it and shrink the buffer by 1
+    // if the last X positions in the buffer are within distance D of one another (aka the velocity is small)
+    //     average the data in the buffer to get the position and then calculate the velocity based on that
+    // else
+    //     do very robust regression on the position data in the buffer
+    //     This gives a line. Using the timestamps on the balls in the buffer, find out
+    //     which way the ball is moving along the line to get the velocity
+    //     Then, project the latest datapoint onto the line to get the position
+    //          WHAT IF THIS LATEST POINT IS NOISE?
+    //
+    //
+    //
+    //
+
+
+
+
+
+
     if (new_ball_detections.empty())
     {
         return current_ball_state;
