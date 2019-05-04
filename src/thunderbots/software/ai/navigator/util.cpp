5ddedf0e752cec8fa348e3894f389d36e7a03801
Jared <jaredpaul9820@gmail.com>
2019-05-04 17:34:34 -0400

    Implementation of converting path of points to path of moveprimitives Fixed Formatting

--- src/thunderbots/software/ai/navigator/util.cpp
+++ src/thunderbots/software/ai/navigator/util.cpp
@@ -5,3 +5,19 @@ double calculateTransitionSpeedBetweenSegments(const Point &p1, const Point &p2,
 {
     return final_speed * (p2 - p1).norm().project((p3 - p2).norm()).len();
 }
+
+std::vector<MovePrimitive> convertToMovePrimitives(unsigned int robot_id,
+                                                   const std::vector<Point> &points)
+{
+    std::vector<MovePrimitive> movePrimitives;
+    movePrimitives.reserve(points.size());
+
+    for (const Point &point : points)
+    {
+        MovePrimitive movePrimitive =
+            MovePrimitive(robot_id, point, point.orientation(), 0, false, false);
+        movePrimitives.emplace_back(movePrimitive);
+    }
+
+    return movePrimitives;
+}
