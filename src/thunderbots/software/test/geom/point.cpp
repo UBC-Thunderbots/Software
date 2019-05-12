2bf77f2ab4a109626e2eef22dd4d04bb74ce3332
Jessica He <jessicahe21@hotmail.com>
2019-05-12 12:51:34 -0700

    Fixed += and -= operators and added tests

--- src/thunderbots/software/test/geom/point.cpp
+++ src/thunderbots/software/test/geom/point.cpp
@@ -7,6 +7,26 @@ TEST(AngleTest, accesors)
     EXPECT_DOUBLE_EQ(0, Point().x());
 }
 
+TEST(PlusEqualOperatorTest, plus_equal_operator_test)
+{
+    Point p = Point(1, 2);
+    Point q = Point(3, 4);
+
+    p += q;
+
+    EXPECT_EQ(Point(4, 6), p);
+}
+
+TEST(MinusEqualOperatorTest, minus_equal_operator_test)
+{
+    Point p = Point(4, 6);
+    Point q = Point(3, 4);
+
+    p -= q;
+    
+    EXPECT_EQ(Point(1, 2), p);
+}
+
 int main(int argc, char **argv)
 {
     std::cout << argv[0] << std::endl;
