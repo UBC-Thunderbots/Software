1de77e7db76406360ba181084641effd03a3a164
jessicahe21 <38922275+jessicahe21@users.noreply.github.com>
2019-05-20 14:43:07 -0700

    Implement getPointTrespass Function for Navigator (#599)
    * Init commit, added function and 3 unit tests
    
    * Changed return value and added one more test

--- src/thunderbots/software/ai/navigator/util.cpp
+++ src/thunderbots/software/ai/navigator/util.cpp
@@ -1,7 +1,22 @@
 #include "ai/navigator/util.h"
 
+#include "geom/point.h"
+#include "geom/util.h"
+
 double calculateTransitionSpeedBetweenSegments(const Point &p1, const Point &p2,
                                                const Point &p3, double final_speed)
 {
     return final_speed * (p2 - p1).norm().project((p3 - p2).norm()).len();
+}
+
+double getPointTrespass(const Point &p1, const Point &p2, double trespass_threshold)
+{
+    double dist_trespass = trespass_threshold - (p1 - p2).len();
+
+    if (dist_trespass < 0)
+    {
+        dist_trespass = 0;
+    }
+
+    return dist_trespass;
 }
