d80724268c005df7342226906ace2217d281328e
Evan <evan@localhost.localdomain>
2019-05-28 20:29:06 -0600

    Removed more generated files

--- src/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/evan/roboRoss/Software/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/evan/roboRoss/Software/devel;/opt/ros/melodic".split(';') if "/home/evan/roboRoss/Software/devel;/opt/ros/melodic" != "" else []
--- src/corner_kick/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/evan/roboRoss/Software/src/corner_kick
-# Build directory: /home/evan/roboRoss/Software/src/corner_kick
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
