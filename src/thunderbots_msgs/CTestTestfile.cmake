3a4577447e8b670049bd287b0a03f4e2aab86b56
EvanMorcom <evans.morcom@alumni.ubc.ca>
2019-05-28 20:36:03 -0600

    Delete CTestTestfile.cmake

--- src/thunderbots_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/evan/roboRoss/Software/src/thunderbots_msgs
-# Build directory: /home/evan/roboRoss/Software/src/thunderbots_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
