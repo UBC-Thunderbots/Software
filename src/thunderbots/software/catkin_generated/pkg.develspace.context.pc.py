5eeeed955dc95a025c9f780838bb8d761aaed021
Evan <evan@localhost.localdomain>
2019-05-28 20:35:03 -0600

    Removed more generated files

--- src/thunderbots/software/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/evan/roboRoss/Software/src/devel/include".split(';') if "/home/evan/roboRoss/Software/src/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;dynamic_reconfigure;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lthunderbots".split(';') if "-lthunderbots" != "" else []
-PROJECT_NAME = "thunderbots"
-PROJECT_SPACE_DIR = "/home/evan/roboRoss/Software/src/devel"
-PROJECT_VERSION = "0.0.0"
