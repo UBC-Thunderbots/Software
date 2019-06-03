27ad1294d8f92390a0d044c25a014a9efbb867ab
Mathew MacDougall <mmacd97@gmail.com>
2019-06-02 21:13:05 -0700

    Fixed Stop Action

--- src/thunderbots/software/ai/hl/stp/action/stop_action.cpp
+++ src/thunderbots/software/ai/hl/stp/action/stop_action.cpp
@@ -1,27 +1,25 @@
 #include "ai/hl/stp/action/stop_action.h"
 
 #include "ai/intent/stop_intent.h"
-#include "geom/angle.h"
-#include "geom/util.h"
-#include "shared/constants.h"
 
-StopAction::StopAction() : Action() {}
-
-std::unique_ptr<Intent> StopAction::updateStateAndGetNextIntent(const Robot& robot,
-                                                                bool coast)
+StopAction::StopAction(double stopped_speed_threshold, bool loop_forever)
+        : Action(), stopped_speed_threshold(stopped_speed_threshold), loop_forever(loop_forever)
 {
-    // update the parameters stored by this action
+}
+
+std::unique_ptr<Intent> StopAction::updateStateAndGetNextIntent(const Robot& robot, bool coast)
+{
+    // Update the parameters stored by this action
     this->robot = robot;
     this->coast = coast;
 
     return getNextIntent();
 }
 
-std::unique_ptr<Intent> StopAction::calculateNextIntent(
-    intent_coroutine::push_type& yield)
+void StopAction::calculateNextIntent(IntentCoroutine::push_type& yield)
 {
     do
     {
         yield(std::make_unique<StopIntent>(robot->id(), coast, 0));
-    } while (true);
+    } while (loop_forever || robot->velocity().len() > stopped_speed_threshold);
 }
--- src/thunderbots/software/test/ai/hl/stp/action/stop_action.cpp
+++ src/thunderbots/software/test/ai/hl/stp/action/stop_action.cpp
@@ -5,12 +5,11 @@
 #include "ai/intent/stop_intent.h"
 
 
-// StopAction should be yeilding stop_intents without coast
-TEST(StopAction, stop_action_with_coast)
+TEST(StopActionTest, robot_stopping_without_coasting_while_already_moving)
 {
-    Robot robot       = Robot(0, Point(10, 10), Vector(), Angle::zero(),
+    Robot robot       = Robot(0, Point(10, 10), Vector(1, 3), Angle::zero(),
                         AngularVelocity::zero(), Timestamp::fromSeconds(0));
-    StopAction action = StopAction();
+    StopAction action = StopAction(0.05, false);
 
     auto intent_ptr = action.updateStateAndGetNextIntent(robot, false);
 
@@ -29,26 +28,17 @@ TEST(StopAction, stop_action_with_coast)
     }
 }
 
-// StopAction should be yeilding stop_intents with coast
-TEST(StopAction, stop_action_coast)
+TEST(StopAction, robot_stopping_while_already_stopped)
 {
     Robot robot       = Robot(0, Point(10, 10), Vector(), Angle::zero(),
                         AngularVelocity::zero(), Timestamp::fromSeconds(0));
-    StopAction action = StopAction();
+    StopAction action = StopAction(0.05, false);
 
-    auto intent_ptr = action.updateStateAndGetNextIntent(robot, true);
+    // We call the action twice. The first time the Intent will always be returned to
+    // ensure the Robot is doing the right thing. In all future calls, the action will be
+    // done and so will return a null pointer
+    auto intent_ptr = action.updateStateAndGetNextIntent(robot, false);
+    intent_ptr = action.updateStateAndGetNextIntent(robot, false);
 
-    // Check an intent was returned (the pointer is not null)
-    EXPECT_TRUE(intent_ptr);
-    EXPECT_FALSE(action.done());
-
-    try
-    {
-        StopIntent stop_intent = dynamic_cast<StopIntent &>(*intent_ptr);
-        EXPECT_EQ(0, stop_intent.getRobotId());
-    }
-    catch (...)
-    {
-        ADD_FAILURE() << "StopIntent was not returned by the StopAction!";
-    }
+    EXPECT_TRUE(action.done());
 }
