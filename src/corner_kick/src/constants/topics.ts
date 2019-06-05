0184ecf6c07324e2caa0b844a248461f341fa27c
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-06-05 19:59:13 +0200

    More changes

--- src/corner_kick/src/constants/topics.ts
+++ src/corner_kick/src/constants/topics.ts
@@ -8,4 +8,4 @@ export const TOPIC_ROSOUT_TYPE = 'rosgraph_msgs/Log';
 export const TOPIC_PLAY_INFO = '/backend/play_info';
 export const TOPIC_PLAY_INFO_TYPE = 'thunderbots_msgs/PlayInfo';
 export const TOPIC_ROBOT_STATUS = '/thunderbots/robot_status';
-export const TOPIC_ROBOT_STATUS_TYPE = 'thunderbots/robot_status';
+export const TOPIC_ROBOT_STATUS_TYPE = 'thunderbots_msgs/RobotStatus';
