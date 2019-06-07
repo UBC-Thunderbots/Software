d68ca9473edaf3b30a85b7d68409a86152bf5ddd
Henry Liu <henryymliu@gmail.com>
2019-06-07 15:49:21 -0700

    test1

--- environment_setup/setup_software.sh
+++ environment_setup/setup_software.sh
@@ -87,7 +87,7 @@ echo "================================================================"
 
 # See http://wiki.ros.org/melodic/Installation/Ubuntu for instructions
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu bionic main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+curl http://repo.ros2.org/repos.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install ros-melodic-desktop -y
 if [ $? -ne 0 ]; then
