dc6cdd98efb0535a4b7af0986e4887dafe241ea8
Henry Liu <henryymliu@gmail.com>
2019-06-15 11:48:30 -0700

    removed testing things

--- src/thunderbots/software/ai/hl/stp/play/example_play.cpp
+++ src/thunderbots/software/ai/hl/stp/play/example_play.cpp
@@ -2,7 +2,6 @@
 
 #include "ai/hl/stp/play/play_factory.h"
 #include "ai/hl/stp/tactic/move_tactic.h"
-#include "ai/hl/stp/tactic/movespin_tactic.h"
 
 const std::string ExamplePlay::name = "Example Play";
 
@@ -23,21 +22,43 @@ bool ExamplePlay::invariantHolds(const World &world) const
 
 void ExamplePlay::getNextTactics(TacticCoroutine::push_type &yield)
 {
-    // Create MoveSpinTactics that will loop forever
-    auto movespin_tactic_1 = std::make_shared<MoveSpinTactic>(true);
-    auto movespin_tactic_2 = std::make_shared<MoveSpinTactic>(true);
+    // Create MoveTactics that will loop forever
+    auto move_tactic_1 = std::make_shared<MoveTactic>(true);
+    auto move_tactic_2 = std::make_shared<MoveTactic>(true);
+    auto move_tactic_3 = std::make_shared<MoveTactic>(true);
+    auto move_tactic_4 = std::make_shared<MoveTactic>(true);
+    auto move_tactic_5 = std::make_shared<MoveTactic>(true);
+    auto move_tactic_6 = std::make_shared<MoveTactic>(true);
 
-
-    while (true)
+    do
     {
         // The angle between each robot spaced out in a circle around the ball
         Angle angle_between_robots = Angle::full() / world.friendlyTeam().numRobots();
 
         // Move the robots in a circle around the ball, facing the ball
+        move_tactic_1->updateParams(
+            world.ball().position() + Point::createFromAngle(angle_between_robots * 1),
+            (angle_between_robots * 1) + Angle::half(), 0);
+        move_tactic_2->updateParams(
+            world.ball().position() + Point::createFromAngle(angle_between_robots * 2),
+            (angle_between_robots * 2) + Angle::half(), 0);
+        move_tactic_3->updateParams(
+            world.ball().position() + Point::createFromAngle(angle_between_robots * 3),
+            (angle_between_robots * 3) + Angle::half(), 0);
+        move_tactic_4->updateParams(
+            world.ball().position() + Point::createFromAngle(angle_between_robots * 4),
+            (angle_between_robots * 4) + Angle::half(), 0);
+        move_tactic_5->updateParams(
+            world.ball().position() + Point::createFromAngle(angle_between_robots * 5),
+            (angle_between_robots * 5) + Angle::half(), 0);
+        move_tactic_6->updateParams(
+            world.ball().position() + Point::createFromAngle(angle_between_robots * 6),
+            (angle_between_robots * 6) + Angle::half(), 0);
 
         // yield the Tactics this Play wants to run, in order of priority
-        yield({movespin_tactic_1});
-    }
+        yield({move_tactic_1, move_tactic_2, move_tactic_3, move_tactic_4, move_tactic_5,
+               move_tactic_6});
+    } while (true);
 }
 
 // Register this play in the PlayFactory
