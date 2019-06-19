ad89c5a4c222bfbd017c57f9278a193d6aa80940
Gareth Ellis <gareth.ellis0@gmail.com>
2019-06-18 18:21:04 -0700

    Replaced Accidentally Removed Statement For KickSpeed In grsim_command_primitive_visitor

--- src/thunderbots/software/grsim_communication/grsim_command_primitive_visitor.cpp
+++ src/thunderbots/software/grsim_communication/grsim_command_primitive_visitor.cpp
@@ -186,6 +186,8 @@ void GrsimCommandPrimitiveVisitor::visit(const KickPrimitive &kick_primitive)
 
 void GrsimCommandPrimitiveVisitor::visit(const MovePrimitive &move_primitive)
 {
+    double kick_speed = move_primitive.isAutoKickEnabled() ? BALL_MAX_SPEED_METERS_PER_SECOND-1 : 0;
+
     motion_controller_command = MotionController::PositionCommand(
         move_primitive.getDestination(), move_primitive.getFinalAngle(),
         move_primitive.getFinalSpeed(), kick_speed, false, false);
