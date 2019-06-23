0f271732816ddd4275f838d756a1d068a4071240
Henry Liu <henryymliu@gmail.com>
2019-06-23 00:30:43 -0700

    simplified mrf_backend

--- src/thunderbots/software/radio_communication/mrf_backend.cpp
+++ src/thunderbots/software/radio_communication/mrf_backend.cpp
@@ -6,8 +6,7 @@
 
 MRFBackend::MRFBackend(unsigned int config, ros::NodeHandle &node_handle)
     : annunciator(Annunciator(node_handle)),
-      dongle(MRFDongle(config, annunciator)),
-      ball(Ball(Point(0, 0), Vector(0, 0), Timestamp::fromSeconds(1)))
+      dongle(MRFDongle(config, annunciator))
 {
 }
 
@@ -18,17 +17,7 @@ void MRFBackend::sendPrimitives(const std::vector<std::unique_ptr<Primitive>> &p
     dongle.send_drive_packet(primitives);
 }
 
-void MRFBackend::update_robots(std::vector<std::tuple<uint8_t, Point, Angle>> robots)
-{
-    this->robots = robots;
-}
-
-void MRFBackend::update_ball(Ball b)
-{
-    ball = b;
-}
-
-void MRFBackend::send_vision_packet()
+void MRFBackend::send_vision_packet(std::vector<std::tuple<uint8_t, Point, Angle>> robots, Ball ball)
 {
     uint64_t timestamp = static_cast<uint64_t>(ball.lastUpdateTimestamp().getSeconds());
     dongle.send_camera_packet(robots, ball.position() * MILLIMETERS_PER_METER, timestamp);
