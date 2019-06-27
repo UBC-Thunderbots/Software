0e4a15852e10beca3e2c53bd6679c41e7b53fe9b
akhilveeraghanta <akhil.veeraghanta@outlook.com>
2019-06-26 21:58:00 -0700

    kickoff friendly play with chip tactic

--- src/thunderbots/software/ai/hl/stp/play/kickoff_friendly_play.cpp
+++ src/thunderbots/software/ai/hl/stp/play/kickoff_friendly_play.cpp
@@ -1,6 +1,7 @@
 #include "ai/hl/stp/play/kickoff_friendly_play.h"
 
 #include "ai/hl/stp/play/play_factory.h"
+#include "ai/hl/stp/tactic/chip_tactic.h"
 #include "ai/hl/stp/tactic/move_tactic.h"
 #include "shared/constants.h"
 
@@ -50,8 +51,8 @@ void KickoffFriendlyPlay::getNextTactics(TacticCoroutine::push_type &yield)
     // 		+--------------------+--------------------+
     //
     // This is a two part play:
-    //      Part 1: Get into position for the kickoff, but don't touch the ball (ref
-    //      kickoff) Part 2: Chip the ball over the defender (ref normal start)
+    //      Part 1: Get into position, but don't touch the ball (ref kickoff)
+    //      Part 2: Chip the ball over the defender (ref normal start)
 
     // the following positions are in the same order as the positions shown above,
     // excluding the goalie for part 1 of this play
@@ -106,7 +107,7 @@ void KickoffFriendlyPlay::getNextTactics(TacticCoroutine::push_type &yield)
 
         // yield the Tactics this Play wants to run, in order of priority
         yield(result);
-    } while (world.gameState.isSetupState());
+    } while (world.gameState().isSetupState());
 
     // Part 2: ready state (chip the ball)
     do
@@ -117,9 +118,19 @@ void KickoffFriendlyPlay::getNextTactics(TacticCoroutine::push_type &yield)
             (world.ball().position() - world.field().friendlyGoal()).orientation(), 0);
 
         std::vector<std::shared_ptr<Tactic>> result = {lone_goalie_tactic_0};
+        auto chip_tactic = std::make_shared<ChipTactic>(world.ball(), true);
 
-        // assign the remaining robots to the remaining move tactics so that they stay
-        // in their current positions
+        // TODO This needs to be adjusted post field testing, ball needs to land exactly
+        // in the middle of the enemy field
+        chip_tactic->updateParams(
+            world.ball(), world.field().centerPoint(),
+            world.field().centerPoint() + Point(world.field().length() / 4, 0),
+            world.field().length() / 2);
+        ;
+        result.emplace_back(chip_tactic);
+
+        // the robot at position 0 will be closest to the ball, so positions starting from
+        // 1 will be assigned to the rest of the robots
         for (int i = 1; i < kickoff_setup_positions.size(); i++)
         {
             move_tactics.at(i)->updateParams(kickoff_setup_positions.at(i), Angle::half(),
@@ -129,7 +140,7 @@ void KickoffFriendlyPlay::getNextTactics(TacticCoroutine::push_type &yield)
 
         // yield the Tactics this Play wants to run, in order of priority
         yield(result);
-    } while (world.gameState.isReadyState());
+    } while (world.gameState().isReadyState());
 }
 
 // Register this play in the PlayFactory
