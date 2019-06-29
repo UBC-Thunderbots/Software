025731152c5f392db6b39d4ae6d59af1c41aa39d
Jonathan Lew <jonathan.lew@alumni.ubc.ca>
2019-06-29 14:14:47 -0700

    tune oscillation

--- src/thunderbots/software/ai/navigator/path_planner/theta_star_path_planner.h
+++ src/thunderbots/software/ai/navigator/path_planner/theta_star_path_planner.h
@@ -192,8 +192,13 @@ class ThetaStarPathPlanner : public PathPlanner
      */
     CellCoordinate convertPointToCell(Point p);
 
-    static constexpr double CLOSE_TO_DEST_THRESHOLD = 0.01;
-    static constexpr double RESOLUTION_FACTOR       = 100.0;
+    static constexpr double CLOSE_TO_DEST_THRESHOLD =
+        0.01;  // if close to destination then return no path
+    static constexpr int BLOCKED_DESINATION_OSCILLATION_MITIGATION =
+        4;  // increase in threshold to reduce oscillation
+    static constexpr double BLOCKED_DESTINATION_SEARCH_RESOLUTION =
+        10.0;  // resolution for searching for unblocked point around a blocked
+               // destination
 
     // only change this value
     static constexpr int GRID_DIVISION_FACTOR = 1;  // the n in the O(n^2) algorithm :p
