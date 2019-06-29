0b740b79a0ff02a5ac49bcec834d91b08916ea24
Gareth Ellis <garethellis0@users.noreply.github.com>
2019-06-29 15:39:34 -0700

    Crease Defender Tactic (#660)
    * Changed GoalieTactic To Not Use EnemyThreat
    
    * Implemented Crease Defender Tactic
    
    * Added Crease Defenders To Defense Play
    
    * Added Dynamic Params For Crease Defender Tactic
    
    * Fixed Formatting
    
    * Fixed Formatting
    
    * Fixed Formatting

--- src/thunderbots/software/ai/hl/stp/play/defense_play.cpp
+++ src/thunderbots/software/ai/hl/stp/play/defense_play.cpp
@@ -1,5 +1,7 @@
 #include "ai/hl/stp/play/defense_play.h"
 
+#include <ai/hl/stp/tactic/crease_defender_tactic.h>
+
 #include "ai/hl/stp/evaluation/enemy_threat.h"
 #include "ai/hl/stp/evaluation/possession.h"
 #include "ai/hl/stp/play/play_factory.h"
@@ -41,16 +43,19 @@ void DefensePlay::getNextTactics(TacticCoroutine::push_type &yield)
         std::make_shared<ShadowEnemyTactic>(world.field(), world.friendlyTeam(),
                                             world.enemyTeam(), true, true),
         std::make_shared<ShadowEnemyTactic>(world.field(), world.friendlyTeam(),
-                                            world.enemyTeam(), true, true),
-        std::make_shared<ShadowEnemyTactic>(world.field(), world.friendlyTeam(),
-                                            world.enemyTeam(), true, true),
-        std::make_shared<ShadowEnemyTactic>(world.field(), world.friendlyTeam(),
                                             world.enemyTeam(), true, true)};
+
+    std::vector<std::shared_ptr<CreaseDefenderTactic>> crease_defender_tactics = {
+        std::make_shared<CreaseDefenderTactic>(world.field(), world.ball(),
+                                               world.friendlyTeam(), world.enemyTeam(),
+                                               CreaseDefenderTactic::LeftOrRight::LEFT),
+        std::make_shared<CreaseDefenderTactic>(world.field(), world.ball(),
+                                               world.friendlyTeam(), world.enemyTeam(),
+                                               CreaseDefenderTactic::LeftOrRight::RIGHT),
+    };
 
     // TODO: replace with reasonable fallback tactics
     std::vector<std::shared_ptr<StopTactic>> stop_tactics = {
-        std::make_shared<StopTactic>(false, true),
-        std::make_shared<StopTactic>(false, true),
         std::make_shared<StopTactic>(false, true),
         std::make_shared<StopTactic>(false, true),
         std::make_shared<StopTactic>(false, true)};
@@ -62,15 +67,32 @@ void DefensePlay::getNextTactics(TacticCoroutine::push_type &yield)
         bool enemy_team_can_pass =
             Util::DynamicParameters::EnemyCapability::enemy_team_can_pass.value();
 
-        goalie_tactic->updateParams(
-            world.ball(), world.field(), world.friendlyTeam(), world.enemyTeam(),
-            enemy_threats.empty() ? std::nullopt
-                                  : std::make_optional(enemy_threats.at(0)));
+        // If we have any crease defenders, we don't want the goalie tactic to consider
+        // them when deciding where to block
+        Team friendly_team_for_goalie = world.friendlyTeam();
+        for (auto crease_defender_tactic : crease_defender_tactics)
+        {
+            if (crease_defender_tactic->getAssignedRobot())
+            {
+                friendly_team_for_goalie.removeRobotWithId(
+                    crease_defender_tactic->getAssignedRobot()->id());
+            }
+        }
+        goalie_tactic->updateParams(world.ball(), world.field(), friendly_team_for_goalie,
+                                    world.enemyTeam());
 
+        std::vector<std::shared_ptr<Tactic>> result = {goalie_tactic};
+
+        // Update crease defenders
+        for (auto crease_defender_tactic : crease_defender_tactics)
+        {
+            crease_defender_tactic->updateParams(world.ball(), world.field(),
+                                                 world.friendlyTeam(), world.enemyTeam());
+            result.emplace_back(crease_defender_tactic);
+        }
 
         // Assign ShadowEnemy tactics until we have every enemy covered. If there any
         // extra friendly robots, have them perform a reasonable default defensive tactic
-        std::vector<std::shared_ptr<Tactic>> result = {goalie_tactic};
         for (int i = 0; i < std::min(stop_tactics.size(), shadow_enemy_tactics.size());
              i++)
         {
