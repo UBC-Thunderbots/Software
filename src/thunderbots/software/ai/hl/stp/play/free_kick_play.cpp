f015e78adc25637ff7a0a13cd8cf4c95343496ba
Henry Liu <henryymliu@gmail.com>
2019-06-29 00:30:54 -0700

    test

--- src/thunderbots/software/ai/hl/stp/play/free_kick_play.cpp
+++ src/thunderbots/software/ai/hl/stp/play/free_kick_play.cpp
@@ -1,15 +1,16 @@
 #include "ai/hl/stp/play/free_kick_play.h"
 
-#include <ai/hl/stp/evaluation/ball.h>
-
 #include <g3log/g3log.hpp>
 
-#include "ai/hl/stp/evaluation/ball.h"
+#include "ai/hl/stp/evaluation/calc_best_shot.h"
 #include "ai/hl/stp/evaluation/possession.h"
 #include "ai/hl/stp/play/play_factory.h"
+#include "ai/hl/stp/tactic/cherry_pick_tactic.h"
 #include "ai/hl/stp/tactic/move_tactic.h"
 #include "ai/hl/stp/tactic/passer_tactic.h"
+#include "ai/hl/stp/tactic/patrol_tactic.h"
 #include "ai/hl/stp/tactic/receiver_tactic.h"
+#include "ai/hl/stp/tactic/shoot_goal_tactic.h"
 #include "ai/passing/pass_generator.h"
 #include "shared/constants.h"
 #include "util/logger/custom_logging_levels.h"
@@ -18,7 +19,11 @@ using namespace Passing;
 
 const std::string FreeKickPlay::name = "Free Kick Play";
 
-FreeKickPlay::FreeKickPlay() : MAX_TIME_TO_COMMIT_TO_PASS(Duration::fromSeconds(5)) {}
+FreeKickPlay::FreeKickPlay()
+    : MAX_TIME_TO_COMMIT_TO_PASS(Duration::fromSeconds(2.0)),
+      MIN_NET_OPEN_ANGLE_FOR_SHOT(Angle::ofDegrees(3))
+{
+}
 
 std::string FreeKickPlay::getName() const
 {
@@ -32,72 +37,153 @@ bool FreeKickPlay::isApplicable(const World &world) const
 
 bool FreeKickPlay::invariantHolds(const World &world) const
 {
-    return !Evaluation::teamHasPossession(world.enemyTeam(), world.ball());
+    return Evaluation::teamHasPossession(world.friendlyTeam(), world.ball());
 }
 
 void FreeKickPlay::getNextTactics(TacticCoroutine::push_type &yield)
 {
     /**
-     * Plan:
-     * The bot closest to the ball takes the free kick. 
-     * If no shot on net, try to pass a la corner kick
-     * Else, just chip towards the enemy goal???
-     * 
-     * Other bots should move to the other side of field to lure enemies.
+     * There are two main stages to this Play:
+     * 1. Shoot while optimizing passes
+     *  - In this stage we try our best to shoot, while also optimizing passes
+     *  - Two robots move up to cherry-pick, two stay back as defenders, one is the
+     *    shooter/potential passer
+     * 2. If we could not shoot, perform the best pass we currently know about
+     *  - In this stage the shooter should be re-assigned to be a passer, one of
+     *    the cherry-pick tactics should be re-assigned to be a receiver, and the
+     *    two defenders continue to defend
      */
 
+    // Have two robots cherry-pick on the +y and -y sides of the field
+    auto cherry_pick_tactic_pos_y = std::make_shared<CherryPickTactic>(
+        world, Rectangle(world.field().centerPoint(), world.field().enemyCornerPos()));
+    auto cherry_pick_tactic_neg_y = std::make_shared<CherryPickTactic>(
+        world, Rectangle(world.field().centerPoint(), world.field().enemyCornerNeg()));
 
-    // We want the two cherry pickers to be in rectangles on the +y and -y sides of the
-    // field in the +x half. We also further offset the rectangle from the goal line
-    // for the cherry-picker closer to where we're taking the corner kick from
-    Vector pos_y_goalline_x_offset(world.field().enemyDefenseArea().width(), 0);
-    Vector neg_y_goalline_x_offset(world.field().enemyDefenseArea().width(), 0);
-    if (kick_from_pos_corner)
+    // Have two robots patrol along a line near the sides of the field
+    // TODO: make these two patrollers into "crease defenders"
+    double half_field_width        = world.field().width() / 2;
+    double patrol_point_y_position = half_field_width - half_field_width / 4;
+    auto patrol_tactic_pos_y =
+        std::make_shared<PatrolTactic>(std::vector<Point>({
+                                           Point(0, patrol_point_y_position),
+                                           Point(1, patrol_point_y_position),
+                                       }),
+                                       AT_PATROL_POINT_TOLERANCE, SPEED_AT_PATROL_POINTS);
+    auto patrol_tactic_neg_y =
+        std::make_shared<PatrolTactic>(std::vector<Point>({
+                                           Point(0, -patrol_point_y_position),
+                                           Point(1, -patrol_point_y_position),
+                                       }),
+                                       AT_PATROL_POINT_TOLERANCE, SPEED_AT_PATROL_POINTS);
+
+    // Have a robot keep trying to take a shot
+    auto shoot_tactic = std::make_shared<ShootGoalTactic>(
+        world.field(), world.friendlyTeam(), world.enemyTeam(), world.ball(),
+        MIN_NET_OPEN_ANGLE_FOR_SHOT, std::nullopt, false);
+
+    // Start a PassGenerator that will continuously optimize passes into roughly
+    // the enemy half of the field
+    PassGenerator pass_generator(world, world.ball().position(),
+                                 PassType::RECEIVE_AND_DRIBBLE);
+    pass_generator.setTargetRegion(
+        Rectangle(Point(-(world.field().length() / 4), world.field().width() / 2),
+                  world.field().enemyCornerNeg()));
+    std::pair<Pass, double> best_pass_and_score_so_far =
+        pass_generator.getBestPassSoFar();
+
+    // Wait for a good pass by starting out only looking for "perfect" passes (with a
+    // score of 1) and decreasing this threshold over time
+    double min_pass_score_threshold        = 1.0;
+    Timestamp pass_optimization_start_time = world.getMostRecentTimestamp();
+    // This boolean indicates if we're ready to perform a pass
+    bool ready_to_pass = false;
+    // Whether or not we've set the passer robot in the PassGenerator
+    bool set_passer_robot_in_passgenerator = false;
+
+    do
     {
-        pos_y_goalline_x_offset += {world.field().enemyDefenseArea().width(), 0};
+        // TODO: Update "crease defenders" here when they're done
+        updateShootGoalTactic(shoot_tactic);
+        updateCherryPickTactics({cherry_pick_tactic_pos_y, cherry_pick_tactic_neg_y});
+        updatePassGenerator(pass_generator);
+
+        LOG(DEBUG) << "Best pass so far is: " << best_pass_and_score_so_far.first;
+        LOG(DEBUG) << "      with score of: " << best_pass_and_score_so_far.second;
+
+        yield({shoot_tactic, cherry_pick_tactic_neg_y,
+               /* cherry_pick_tactic_pos_y, patrol_tactic_pos_y, patrol_tactic_neg_y*/});
+
+        // If there is a robot assigned to shoot, we assume this is the robot
+        // that will be taking the shot
+        if (shoot_tactic->getAssignedRobot())
+        {
+            pass_generator.setPasserRobotId(shoot_tactic->getAssignedRobot()->id());
+            set_passer_robot_in_passgenerator = true;
+        }
+
+        best_pass_and_score_so_far = pass_generator.getBestPassSoFar();
+
+        // We're ready to pass if we have a robot assigned in the PassGenerator as the
+        // passer and the PassGenerator has found a pass above our current threshold
+        ready_to_pass = set_passer_robot_in_passgenerator &&
+                        best_pass_and_score_so_far.second < min_pass_score_threshold;
+
+        // If we've assigned a robot as the passer in the PassGenerator, we lower
+        // our threshold based on how long the PassGenerator as been running since
+        // we set it
+        if (set_passer_robot_in_passgenerator)
+        {
+            // TODO: change this to use the world timestamp (Issue #423)
+            Duration time_since_commit_stage_start =
+                world.ball().lastUpdateTimestamp() - pass_optimization_start_time;
+            min_pass_score_threshold =
+                1 - std::min(time_since_commit_stage_start.getSeconds() /
+                                 MAX_TIME_TO_COMMIT_TO_PASS.getSeconds(),
+                             1.0 - ABS_MIN_PASS_QUALITY);
+        }
+
+        LOG(DEBUG) << "LOOP END";
+        //    } while(!ready_to_pass || shoot_tactic->hasShotAvailable());
+    } while (!ready_to_pass || false);
+
+    // Destruct the PassGenerator and CherryPick tactics (which contain a PassGenerator
+    // each) to save a significant number of CPU cycles
+    // TODO: stop the PassGenerators here instead of destructing them (Issue #636)
+    pass_generator.~PassGenerator();
+    cherry_pick_tactic_pos_y->~CherryPickTactic();
+    cherry_pick_tactic_neg_y->~CherryPickTactic();
+
+    // If the shoot tactic has finished, we are done this play, otherwise we need to pass
+    if (!shoot_tactic->hasShotAvailable())
+    {
+        // Commit to a pass
+        Pass pass = best_pass_and_score_so_far.first;
+
+        LOG(DEBUG) << "Committing to pass: " << best_pass_and_score_so_far.first;
+        LOG(DEBUG) << "Score of pass we committed to: "
+                   << best_pass_and_score_so_far.second;
+
+        // Perform the pass and wait until the receiver is finished
+        auto passer   = std::make_shared<PasserTactic>(pass, world.ball(), false);
+        auto receiver = std::make_shared<ReceiverTactic>(
+            world.field(), world.friendlyTeam(), world.enemyTeam(), pass, world.ball(),
+            false);
+        do
+        {
+            // TODO: Update "crease defenders" here when they're done
+            passer->updateParams(pass, world.ball());
+            receiver->updateParams(world.friendlyTeam(), world.enemyTeam(), pass,
+                                   world.ball());
+            yield({passer, receiver, patrol_tactic_pos_y, patrol_tactic_neg_y});
+        } while (!receiver->done());
     }
     else
     {
-        // kick from neg corner
-        neg_y_goalline_x_offset += {world.field().enemyDefenseArea().width(), 0};
+        LOG(DEBUG) << "Took shot";
     }
-    Vector center_line_x_offset(1, 0);
-    Rectangle pos_y_cherry_pick_rectangle(
-        world.field().centerPoint() + center_line_x_offset,
-        world.field().enemyCornerPos() - pos_y_goalline_x_offset);
-    Rectangle neg_y_cherry_pick_rectangle(
-        world.field().centerPoint() + center_line_x_offset,
-        world.field().enemyCornerNeg() - neg_y_goalline_x_offset);
 
-    // This tactic will move a robot into position to initially take the free-kick
-    auto align_to_ball_tactic = std::make_shared<MoveTactic>();
-
-    // These two tactics will set robots to roam around the field, trying to put
-    // themselves into a good position to receive a pass
-    auto cherry_pick_tactic_pos_y =
-        std::make_shared<CherryPickTactic>(world, pos_y_cherry_pick_rectangle);
-    auto cherry_pick_tactic_neg_y =
-        std::make_shared<CherryPickTactic>(world, neg_y_cherry_pick_rectangle);
-
-    // Setup two bait robots on the opposite side of the field to where the corner kick
-    // is taking place to pull enemies away from the goal
-    Point opposite_corner_to_kick = kick_from_pos_corner ? world.field().enemyCornerNeg()
-                                                         : world.field().enemyCornerPos();
-    Point bait_move_tactic_1_pos =
-        opposite_corner_to_kick - Vector(world.field().enemyDefenseArea().width() * 0.5,
-                                         copysign(0.5, opposite_corner_to_kick.y()));
-    Point bait_move_tactic_2_pos =
-        opposite_corner_to_kick - Vector(world.field().enemyDefenseArea().width() * 1.5,
-                                         copysign(0.5, opposite_corner_to_kick.y()));
-    auto bait_move_tactic_1 = std::make_shared<MoveTactic>(true);
-    auto bait_move_tactic_2 = std::make_shared<MoveTactic>(true);
-    bait_move_tactic_1->updateParams(
-        bait_move_tactic_1_pos,
-        (world.field().enemyGoal() - bait_move_tactic_1_pos).orientation(), 0.0);
-    bait_move_tactic_2->updateParams(
-        bait_move_tactic_2_pos,
-        (world.field().enemyGoal() - bait_move_tactic_2_pos).orientation(), 0.0);
-
+    LOG(DEBUG) << "Finished";
 }
 
 void FreeKickPlay::updateCherryPickTactics(
@@ -112,12 +198,18 @@ void FreeKickPlay::updateCherryPickTactics(
 void FreeKickPlay::updateAlignToBallTactic(
     std::shared_ptr<MoveTactic> align_to_ball_tactic)
 {
-    Vector ball_to_center_vec = Vector(0, 0) - world.ball().position();
-    // We want the kicker to get into position behind the ball facing the center
-    // of the field
+    // We want to the robot to face the enemy net to minimize the amount of motion
+    // required to turn and shoot
+    Vector ball_to_enemy_net_vec = world.field().enemyGoal() - world.ball().position();
     align_to_ball_tactic->updateParams(
-        world.ball().position() - ball_to_center_vec.norm(ROBOT_MAX_RADIUS_METERS * 2),
-        ball_to_center_vec.orientation(), 0);
+        world.ball().position() - ball_to_enemy_net_vec.norm(ROBOT_MAX_RADIUS_METERS * 2),
+        ball_to_enemy_net_vec.orientation(), 0);
+}
+
+void FreeKickPlay::updateShootGoalTactic(std::shared_ptr<ShootGoalTactic> shoot_tactic)
+{
+    shoot_tactic->updateParams(world.field(), world.friendlyTeam(), world.enemyTeam(),
+                               world.ball());
 }
 
 void FreeKickPlay::updatePassGenerator(PassGenerator &pass_generator)
