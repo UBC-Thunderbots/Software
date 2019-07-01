31a558bd86ab9c30fe1a84eb14fd10d2e13ca3cc
akhilveeraghanta <akhil.veeraghanta@outlook.com>
2019-07-01 16:45:04 +1000

    Stop Play position improvement
    - goalie robot stays as the goalie,
    - two robots stay back and help the goalie
    - the rest of the 3 robots stay within 0.5m of the ball
            evenly spaced

--- src/thunderbots/software/ai/hl/stp/play/stop_play.cpp
+++ src/thunderbots/software/ai/hl/stp/play/stop_play.cpp
@@ -1,7 +1,9 @@
 #include "ai/hl/stp/play/stop_play.h"
 
 #include "ai/hl/stp/play/play_factory.h"
+#include "ai/hl/stp/tactic/goalie_tactic.h"
 #include "ai/hl/stp/tactic/move_tactic.h"
+#include "shared/constants.h"
 
 const std::string StopPlay::name = "Stop Play";
 
@@ -22,40 +24,102 @@ bool StopPlay::invariantHolds(const World &world) const
 
 void StopPlay::getNextTactics(TacticCoroutine::push_type &yield)
 {
-    // Create Stop Tactics that will loop forever
-    auto move_tactic_1 = std::make_shared<MoveTactic>(true);
-    auto move_tactic_2 = std::make_shared<MoveTactic>(true);
-    auto move_tactic_3 = std::make_shared<MoveTactic>(true);
-    auto move_tactic_4 = std::make_shared<MoveTactic>(true);
-    auto move_tactic_5 = std::make_shared<MoveTactic>(true);
-    auto move_tactic_6 = std::make_shared<MoveTactic>(true);
+    // Robot assignments for the Stop Play
+    //  - 1 robot will be the goalie
+    //  - 2 robots will assist the goalie in blocking the ball, they will snap
+    //      to the best fit semicircle around the defense area
+    //  - 3 robots will stay within 0.5m of the ball facing the enemy net
+    //
+    //  If x represents the ball and G represents the goalie before, the following
+    //  diagram depicts a possible outcome of this play
+    //
+    // 		+--------------------+--------------------+
+    // 		|           4        |                    |
+    // 		|           x        |                    |
+    // 		| 0       2  3       |                    |
+    // 		+--+ 1               |                 +--+
+    // 		|  |                 |                 |  |
+    // 		|G |               +-+-+               |  |
+    // 		|  |               |   |               |  |
+    // 		|  |               +-+-+               |  |
+    // 		|  |                 |                 |  |
+    // 		+--+                 |                 +--+
+    // 		|                    |                    |
+    // 		|                    |                    |
+    // 		|                    |                    |
+    // 		+--------------------+--------------------+
 
-    // Puts one robot in the goal, and lines the rest up in front of the defense area
-    std::vector<Point> stop_positions = {
-        world.field().friendlyGoal() + Vector(0.3, 0),
-        world.field().friendlyGoal() +
-            Vector(world.field().defenseAreaLength() + 0.25, -0.4),
-        world.field().friendlyGoal() +
-            Vector(world.field().defenseAreaLength() + 0.25, -0.2),
-        world.field().friendlyGoal() +
-            Vector(world.field().defenseAreaLength() + 0.25, 0.0),
-        world.field().friendlyGoal() +
-            Vector(world.field().defenseAreaLength() + 0.25, 0.2),
-        world.field().friendlyGoal() +
-            Vector(world.field().defenseAreaLength() + 0.25, 0.4)};
+
+    std::vector<std::shared_ptr<MoveTactic>> move_tactics = {
+        std::make_shared<MoveTactic>(true), std::make_shared<MoveTactic>(true),
+        std::make_shared<MoveTactic>(true), std::make_shared<MoveTactic>(true),
+        std::make_shared<MoveTactic>(true)};
+
+    auto goalie_tactic = std::make_shared<GoalieTactic>(
+        world.ball(), world.field(), world.friendlyTeam(), world.enemyTeam());
+
+    // We want to find the radius of the semicircle in which the defense area can be
+    // inscribed, the full derivation can be found in the link below
+    //
+    // http://www.stumblingrobot.com/2015/10/06/
+    // find-the-largest-rectangle-that-can-be-inscribed-in-a-semicircle/
+    float radius = sqrt(2) * world.field().friendlyDefenseArea().width();
 
     do
     {
-        move_tactic_1->updateParams(stop_positions.at(0), Angle::zero(), 0);
-        move_tactic_2->updateParams(stop_positions.at(1), Angle::zero(), 0);
-        move_tactic_3->updateParams(stop_positions.at(2), Angle::zero(), 0);
-        move_tactic_4->updateParams(stop_positions.at(3), Angle::zero(), 0);
-        move_tactic_5->updateParams(stop_positions.at(4), Angle::zero(), 0);
-        move_tactic_6->updateParams(stop_positions.at(5), Angle::zero(), 0);
+        // goalie tactic
+        auto enemy_threats = Evaluation::getAllEnemyThreats(
+            world.field(), world.friendlyTeam(), world.enemyTeam(), world.ball(), false);
 
-        // yield the Tactics this Play wants to run, in order of priority
-        yield({move_tactic_1, move_tactic_2, move_tactic_3, move_tactic_4, move_tactic_5,
-               move_tactic_6});
+        goalie_tactic->updateParams(
+            world.ball(), world.field(), world.friendlyTeam(), world.enemyTeam(),
+            enemy_threats.empty() ? std::nullopt
+                                  : std::make_optional(enemy_threats.at(0)));
+        std::vector<std::shared_ptr<Tactic>> result = {goalie_tactic};
+
+        // defense point is the point on the semicircle around the friendly defense area,
+        // that can block the direct path to the net from the ball. The robot positioning
+        // vector will be used to position the robots on the tangent line on that
+        // semicircle
+        Vector goal_to_ball_unit_vector =
+            (world.field().friendlyGoal() - world.ball().position()).norm();
+        Vector robot_positiioning_unit_vector = goal_to_ball_unit_vector.perp();
+        Point defense_point_center =
+            world.field().friendlyGoal() - radius * goal_to_ball_unit_vector;
+
+        // position robots son either side of defense point
+        Point defense_point_left = defense_point_center + robot_positiioning_unit_vector *
+                                                              2 * ROBOT_MAX_RADIUS_METERS;
+        Point defense_point_right =
+            defense_point_center -
+            robot_positiioning_unit_vector * 2 * ROBOT_MAX_RADIUS_METERS;
+
+        move_tactics.at(0)->updateParams(
+            defense_point_left,
+            (world.ball().position() - defense_point_left).orientation(), 0);
+        move_tactics.at(1)->updateParams(
+            defense_point_right,
+            (world.ball().position() - defense_point_right).orientation(), 0);
+
+        // Move the robots in a triangle around the ball, facing the ball, this behaviour
+        // is very similar to the example play when there are only 3 robots
+        Angle angle_between_robots = Angle::full() / 3;
+        move_tactics.at(2)->updateParams(
+            world.ball().position() +
+                0.5 * Point::createFromAngle(angle_between_robots * 1),
+            (angle_between_robots * 1) + Angle::half(), 0);
+        move_tactics.at(3)->updateParams(
+            world.ball().position() +
+                0.5 * Point::createFromAngle(angle_between_robots * 2),
+            (angle_between_robots * 2) + Angle::half(), 0);
+        move_tactics.at(4)->updateParams(
+            world.ball().position() +
+                0.5 * Point::createFromAngle(angle_between_robots * 3),
+            (angle_between_robots * 3) + Angle::half(), 0);
+
+        // insert all the move tactics to the result
+        result.insert(result.end(), move_tactics.begin(), move_tactics.end());
+        yield(result);
     } while (true);
 }
 
