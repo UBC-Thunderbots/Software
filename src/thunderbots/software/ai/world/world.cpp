faa180c4999db8c4daf6dc73d742ebd016937dc2
Mathew MacDougall <mmacd97@gmail.com>
2019-07-02 04:13:57 -0700

    fixed tests

--- src/thunderbots/software/ai/world/world.cpp
+++ src/thunderbots/software/ai/world/world.cpp
@@ -33,21 +33,25 @@ World::World(const Field &field, const Ball &ball, const Team &friendly_team,
 void World::updateFieldGeometry(const Field &new_field_data)
 {
     field_.updateDimensions(new_field_data);
+    updateTimestamp(getMostRecentTimestampFromMembers());
 }
 
 void World::updateBallState(const Ball &new_ball_data)
 {
     ball_.updateState(new_ball_data);
+    updateTimestamp(getMostRecentTimestampFromMembers());
 }
 
 void World::updateFriendlyTeamState(const Team &new_friendly_team_data)
 {
     friendly_team_.updateState(new_friendly_team_data);
+    updateTimestamp(getMostRecentTimestampFromMembers());
 }
 
 void World::updateEnemyTeamState(const Team &new_enemy_team_data)
 {
     enemy_team_.updateState(new_enemy_team_data);
+    updateTimestamp(getMostRecentTimestampFromMembers());
 }
 
 void World::updateTimestamp(Timestamp time_stamp)
