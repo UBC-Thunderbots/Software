0310b1f643702eb2e69e261716c992ec427b0d6a
akhilveeraghanta <akhil.veeraghanta@outlook.com>
2019-07-03 16:34:17 +1000

    GoalieTactic improvement
    - handle block cone
    - stay within defense area

--- src/thunderbots/software/ai/hl/stp/tactic/goalie_tactic.cpp
+++ src/thunderbots/software/ai/hl/stp/tactic/goalie_tactic.cpp
@@ -8,6 +8,8 @@
 #include "geom/segment.h"
 #include "geom/util.h"
 #include "shared/constants.h"
+#include "util/parameter/dynamic_parameters.h"
+
 
 GoalieTactic::GoalieTactic(const Ball &ball, const Field &field,
                            const Team &friendly_team, const Team &enemy_team)
@@ -57,69 +59,74 @@ void GoalieTactic::calculateNextIntent(IntentCoroutine::push_type &yield)
     ChipAction chip_action = ChipAction();
     do
     {
-        Ray ball_ray = Ray(ball.position(), ball.velocity());
+        // Goalie Tactic
+        //
+        // The goalie tactic is responsible for blocking as many shots as it can, to the
+        // best of its ability. The tactic consists of 3 cases
+        //
+        // Case 1: The ball is moving towards the goal and has a speed that is concerning
+        //      Goalie moves onto the closest point on the oncoming line to stop the ball
+        //
+        // Case 2: The ball is moving at a slow speed and is inside the defense area
+        //      Goalie moves to the ball and chips it out of the defense area
+        //
+        // Case 3: Any other case
+        //      Goalie blocks the cone to the net. (Cone being from the ball to either
+        //      goal post) The goalie also snaps to a semicircle inside the defense area,
+        //      to avoid leaving the defense area
+        //
+        std::unique_ptr<Intent> next_intent;
 
-        // Create a segment along the goal line, slightly shortened to account for the
-        // robot radius so as we move along the segment we don't try run into the goal
-        // posts
-        Point p1                        = Point(field.friendlyGoalpostNeg().x(),
-                         field.friendlyGoalpostNeg().y() + ROBOT_MAX_RADIUS_METERS);
-        Point p2                        = Point(field.friendlyGoalpostPos().x(),
-                         field.friendlyGoalpostPos().y() - ROBOT_MAX_RADIUS_METERS);
-        Segment goalie_movement_segment = Segment(p1, p2);
+        // compute intersection points from ball position and velocity
+        Ray ball_ray = Ray(ball.position(), ball.velocity());
         Segment full_goal_segment =
             Segment(field.friendlyGoalpostNeg(), field.friendlyGoalpostPos());
 
-        // Our default behavior is to track the ball along the goal line, and always face
-        // the ball
-        Point goalie_pos = closestPointOnSeg(ball.position(), goalie_movement_segment);
-        Angle goalie_orientation = (ball.position() - goalie_pos).orientation();
-        std::unique_ptr<Intent> next_intent;
-
         auto [intersection1, intersection2] =
             raySegmentIntersection(ball_ray, full_goal_segment);
+
+        // Case 1
         if (intersection1.has_value() &&
             ball.velocity().len() > BALL_SLOW_SPEED_THRESHOLD)
         {
-            // The ball is heading towards the net. Move to block the shot
-            goalie_pos  = closestPointOnSeg(*intersection1, goalie_movement_segment);
+            // the ball is heading towards the net. move to block the shot
+            Point goalie_pos         = closestPointOnSeg((*robot).position(),
+                                                 Segment(ball.position(), intersection1));
+            Angle goalie_orientation = (ball.position() - goalie_pos).orientation();
+
             next_intent = move_action.updateStateAndGetNextIntent(
                 *robot, goalie_pos, goalie_orientation, 0.0, false, AUTOCHIP);
         }
+        // Case 2
         else if (ball.velocity().len() < BALL_SLOW_SPEED_THRESHOLD &&
                  field.pointInFriendlyDefenseArea(ball.position()))
         {
-            // If the ball is slow or stationary inside our defense area, chip it out
+            // if the ball is slow or stationary inside our defense area, chip it out
             next_intent = chip_action.updateStateAndGetNextIntent(
                 *robot, ball, ball.position(),
                 (ball.position() - field.friendlyGoal()).orientation(), 2);
         }
+        // Case 3
         else
         {
-            // If we have been provided with enemy threat information, use it to
-            // preemptively block their best shot. Calculate the best shot the enemy could
-            // take, ignoring the goalie as an obstacle
-            auto best_shot = Evaluation::calcBestShotOnFriendlyGoal(
-                field, friendly_team, enemy_team, ball.position(),
-                ROBOT_MAX_RADIUS_METERS, {*robot});
-            if (best_shot.has_value())
-            {
-                Vector shot_vector = best_shot->first - ball.position();
-                Ray shot_ray       = Ray(ball.position(), shot_vector);
-                auto [best_shot_intersection_1, best_shot_intersection_2] =
-                    raySegmentIntersection(shot_ray, full_goal_segment);
+            // block the cone by default
+            float radius =
+                Util::DynamicParameters::GoalieTactic::block_cone_buffer.value() +
+                ROBOT_MAX_RADIUS_METERS;
+            Point goalie_pos =
+                calcBlockCone(field.friendlyGoalpostNeg(), field.friendlyGoalpostPos(),
+                              ball.position(), radius);
 
-                if (best_shot_intersection_1.has_value())
-                {
-                    // Move to block the estimated best shot the enemy could take
-                    goalie_pos = closestPointOnSeg(*best_shot_intersection_1,
-                                                   goalie_movement_segment);
-                }
-            }
+            // restrict the goalie to a semicircle inscribed inside the defense area
+            Point goalie_restricted_pos =
+                field.friendlyGoal() - (field.friendlyDefenseArea().width() *
+                                        (field.friendlyGoal() - goalie_pos).norm());
+
+            // restrict the point to be within the defense area
+            auto goalie_orientation = (ball.position() - goalie_pos).orientation();
+            next_intent             = move_action.updateStateAndGetNextIntent(
+                *robot, goalie_restricted_pos, goalie_orientation, 0.0, false, AUTOCHIP);
         }
-
-        next_intent = move_action.updateStateAndGetNextIntent(
-            *robot, goalie_pos, goalie_orientation, 0.0, false, AUTOCHIP);
 
         yield(std::move(next_intent));
 
