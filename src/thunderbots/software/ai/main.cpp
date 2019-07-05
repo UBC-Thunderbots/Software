be8a602af03d2978f29d12f6e6e8b705bfb88f81
Jordan Liu <jordan.zliu@gmail.com>
2019-07-05 18:32:18 +1000

    ignore world state update if our goalie appears to be in their crease

--- src/thunderbots/software/ai/main.cpp
+++ src/thunderbots/software/ai/main.cpp
@@ -44,6 +44,7 @@ void worldUpdateCallback(const thunderbots_msgs::World::ConstPtr &msg)
                 Util::ROSMessages::createGameStateFromROSMessage(world_msg.refbox_data.command);
         world.updateRefboxGameState(new_game_state);
         world.updateTimestamp(new_world.getMostRecentTimestamp());
+        LOG(WARNING) << "We probably got a mirrored frame from network_input, ignoring it";
     }
 
     if (Util::DynamicParameters::AI::run_ai.value())
