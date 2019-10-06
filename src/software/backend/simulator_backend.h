ca445f604be5124f286810f219800edeafc69087
Mathew MacDougall <mmacd97@gmail.com>
2019-10-06 13:21:19 -0700

    Added simulator backend

---
+++ src/software/backend/simulator_backend.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "software/ai/world/world.h"
+#include "software/backend/backend.h"
+#include <mutex>
+
+class SimulatorBackend : public Backend
+{
+   public:
+    static const std::string name;
+
+    /**
+     * Creates a new SimulatorBackend
+     *
+     * The SimulatorBackend will run a physics simulation to simulate and publish the World, and simulate
+     * the primitives given to it. The SimulatorBackend will only advance the simulation by a fixed amount
+     * each time primitives are received, rather than running at full-speed or in real-time. This is because
+     * we want the simulator to be deterministic and always provide the World at a "fixed" rate from the
+     * perspective of any consumers.
+     *
+     * @param simulation_time_step How far to step the simulation in time each time the simulation is advanced
+     * @param num_steps_per_primitive_update How many times to advance the simulation each time new primitives
+     * are received
+     */
+    explicit SimulatorBackend(const Duration& simulation_time_step = Duration::fromSeconds(1.0 / 60.0), unsigned int num_steps_per_primitive_update = 2);
+
+    /**
+     * Returns the current state of the World in the simulated backend
+     *
+     * @return the current state of the World in the simulated backend
+     */
+    World getWorld();
+
+    /**
+     * Sets the state of the World in the simulated backend. All following simulation will follow
+     * from this newly provided world.
+     *
+     * @param new_world A new state of the World to set in the simulated backend
+     */
+    void setWorld(const World& new_world);
+
+   private:
+    /**
+     * Updates the simulation.
+     *
+     * This will advance the simulation by "simulation_time_step", "num_steps_per_primitive_update" times.
+     * We take several smaller steps if necessary to maintain the accuracy of the simulation. This will
+     * update the state of the World in the backend and send the updated values to all observers.
+     */
+    void updateSimulation();
+
+    /**
+     * Sends the current state of the world in the simulation to all Observers
+     */
+    void sendWorldToObservers();
+
+    void onValueReceived(ConstPrimitiveVectorPtr primitives) override;
+
+    Duration simulation_time_step;
+    unsigned int num_steps_per_primitive_update;
+
+    ConstPrimitiveVectorPtr most_recently_received_primitives;
+    std::mutex most_recently_received_primitives_mutex;
+
+    // The current state of the world in the simulation
+    World world;
+    std::mutex world_mutex;
+};
