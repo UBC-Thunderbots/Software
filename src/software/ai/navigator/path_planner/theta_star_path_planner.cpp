065323fc63291fce5538fc93377ac393b174b52e
jonathanlew <31377876+jonathanlew@users.noreply.github.com>
2019-10-10 11:33:58 -0700

    Restructure navigator and path planner (#873)
    * Restructure navigator and path planner

--- src/software/ai/navigator/path_planner/theta_star_path_planner.cpp
+++ src/software/ai/navigator/path_planner/theta_star_path_planner.cpp
@@ -3,7 +3,7 @@
  * which returns an optimal path that avoids obstacles
  */
 
-#include "software/ai/navigator/path_planning_navigator/path_planner/theta_star_path_planner.h"
+#include "software/ai/navigator/path_planner/theta_star_path_planner.h"
 
 #include <g3log/g3log.hpp>
 
@@ -238,7 +238,7 @@ std::optional<std::vector<Point>> ThetaStarPathPlanner::findPath(const Point &st
         auto tmp_dest = findClosestUnblockedCell(dest);
         if (tmp_dest)
         {
-            dest         = *tmp_dest;
+            dest = *tmp_dest;
         }
         else
         {
@@ -305,7 +305,7 @@ std::optional<std::vector<Point>> ThetaStarPathPlanner::findPath(const Point &st
             {
                 double dist_to_neighbour =
                     std::sqrt(std::pow(x_offset, 2) + std::pow(y_offset, 2));
-                pNew = std::make_pair(i + x_offset, j + y_offset);
+                pNew      = std::make_pair(i + x_offset, j + y_offset);
                 foundDest = updateVertex(pCurr, pNew, dest, dist_to_neighbour);
                 if (foundDest)
                 {
