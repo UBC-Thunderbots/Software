17caab515b26086222834d61c7089974bd312675
Gareth Ellis <gareth.ellis0@gmail.com>
2019-10-11 18:11:52 -0700

    wip

--- src/firmware_new/debug_firmware_on_arm_board.sh
+++ src/firmware_new/debug_firmware_on_arm_board.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-arm-none-eabi-gdb -ex "directory /home/gareth/programming/thunderbots/Software/src/" $@
+# Start gdb to connect to the board via openocd
+# TODO: the directory here needs to be set relatively
+arm-none-eabi-gdb -ex "directory /home/gareth/programming/thunderbots/Software/src/" "$@"
