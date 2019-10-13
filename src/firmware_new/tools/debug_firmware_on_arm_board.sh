6159717b7dcf39b26219e32aab11b55a76058586
Gareth Ellis <gareth.ellis0@gmail.com>
2019-10-13 13:45:33 -0700

    Finished debug_firmware_on_arm_board.sh

--- src/firmware_new/tools/debug_firmware_on_arm_board.sh
+++ src/firmware_new/tools/debug_firmware_on_arm_board.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BAZEL_ROOT_DIR="$CURR_DIR/../.."
 THIS_SCRIPT_FILENAME=`basename "$0"`
 
 NUM_ARGS=$#
@@ -10,12 +11,40 @@ if [ $NUM_ARGS -ne 2 ]; then
     exit 1
 fi
 
-# Start openocd as a child process
-echo $2
-openocd -f "$2" &
+# Stop openocd if it's already running
+pkill openocd
+KILLED_OPENOCD=$?
 
-# Run gdb and connect to openocd
+# If we killed openocd, wait for a second for the port to be free
+if [ $KILLED_OPENOCD ]; then
+    echo "Had to kill instance of openocd, waiting for a second for it to stop"
+    sleep 2
+fi
+
+# Start openocd as a child process, and redirect all its output to /dev/null.
+# If we don't redirect the output it will overwrite the gdb window
+OPENOCD_RUN_CMD="openocd -f $2"
+eval "$OPENOCD_RUN_CMD > /dev/null 2>&1 &"
+
+# Check that openocd is still running after a second. If it's not, that 
+# probably means there was an issue starting it, so we exit
+sleep 1
+pgrep openocd 
+OPENOCD_RUNNING=$?
+if [ ! $OPENOCD_RUNNING ]; then
+    echo "There was an issue running openocd, the command that failed was: '$OPENOCD_RUN_CMD'"
+    exit 1
+fi
+
+# Run gdb, then:
+#   - connect to openocd
+#   - find all the source files so you can view them in gdb
+#   - load firmware onto the board
 arm-none-eabi-gdb \
+    -ex "directory $BAZEL_ROOT_DIR" \
     -ex "target extended-remote :3333" \
-    -ex "directory $CURR_DIR/../.." \
-    -tui
+    -ex "load" \
+    -tui $1
+
+# When gdb is finished, cleanup by stopping openocd
+pkill openocd
