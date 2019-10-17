ad32aa31439a2bd32e3ae95cce3248d1318f47c4
Amr <amralmoallim@gmail.com>
2019-10-17 16:04:09 -0700

    fixed indenting

--- clang_format/fix_formatting.sh
+++ clang_format/fix_formatting.sh
@@ -55,34 +55,34 @@ while test $# -gt 0; do
             ;;
         -a|--all)
             echo "WARNING: "-a" will format all the files and can be very slow! Please consider using "-b" instead!"
-
             echo "Are you sure you want to format all files (y/n)?"
             read continueFormat
+
             if [ "$continueFormat" == "n" ]; then
-		exit 0
-	    elif [ "$continueFormat" == "y" ]; then
-		echo "Formatting all files..."
+                exit 0
 
-		# Generate extension string
-		# Formatted as -iname *.EXTENSION -o
-		EXTENSION_STRING=""
-		for value in "${EXTENSIONS[@]}"
-		do
-		    EXTENSION_STRING="$EXTENSION_STRING -iname *.$value -o"
-		done
-		    
-		# Find all the files that we want to format, and pass them to
-		# clang-format as arguments
-		# We remove the last -o flag from the extension string
-		find $CURR_DIR/../src/ ${EXTENSION_STRING::-2} \
-		    | xargs $CURR_DIR/clang-format-$CLANG_VERSION -i -style=file
+            elif [ "$continueFormat" == "y" ]; then
+                echo "Formatting all files..."
 
-		shift
+                # Generate extension string
+                # Formatted as -iname *.EXTENSION -o
+                EXTENSION_STRING=""
+                for value in "${EXTENSIONS[@]}"
+                do
+                    EXTENSION_STRING="$EXTENSION_STRING -iname *.$value -o"
+                done
+    
+                # Find all the files that we want to format, and pass them to
+                # clang-format as arguments
+                # We remove the last -o flag from the extension string
+                find $CURR_DIR/../src/ ${EXTENSION_STRING::-2} \
+                    | xargs $CURR_DIR/clang-format-$CLANG_VERSION -i -style=file
+                shift
             else 
-		echo "Error: input not recognized"
+                echo "Error: input not recognized"
                 exit 1
-	   fi
-	    ;;
+            fi
+            ;;
         -b|--branch)
             # Shift the arguents to check for the argument to this flag
             shift
