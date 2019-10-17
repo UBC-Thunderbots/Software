170a98e58b4fd32d9a28b9e7ca298a766fc30ed8
Mathew MacDougall <mmacd97@gmail.com>
2019-10-16 18:10:41 -0700

    fixed formatting

--- src/software/ai/hl/stp/tactic/shadow_freekicker_tactic.cpp
+++ src/software/ai/hl/stp/tactic/shadow_freekicker_tactic.cpp
@@ -44,7 +44,9 @@ void ShadowFreekickerTactic::calculateNextIntent(IntentCoroutine::push_type &yie
     {
         std::optional<Robot> enemy_with_ball =
             Evaluation::getRobotWithEffectiveBallPossession(enemy_team, ball, field);
-        double robot_separation_scaling_factor = Util::DynamicParameters::ShadowFreekickerTactic::robot_separation_scaling_factor.value();
+        double robot_separation_scaling_factor =
+            Util::DynamicParameters::ShadowFreekickerTactic::
+                robot_separation_scaling_factor.value();
 
         if (enemy_with_ball.has_value())
         {
@@ -53,7 +55,8 @@ void ShadowFreekickerTactic::calculateNextIntent(IntentCoroutine::push_type &yie
                     .norm(FREE_KICK_MAX_PROXIMITY + ROBOT_MAX_RADIUS_METERS);
 
             Vector perpendicular_to_enemy_direction =
-                enemy_pointing_direction.perp().norm(ROBOT_MAX_RADIUS_METERS * robot_separation_scaling_factor);
+                enemy_pointing_direction.perp().norm(ROBOT_MAX_RADIUS_METERS *
+                                                     robot_separation_scaling_factor);
 
             defend_position = free_kick_shadower == FreekickShadower::RIGHT
                                   ? ball.position() + enemy_pointing_direction +
@@ -67,8 +70,8 @@ void ShadowFreekickerTactic::calculateNextIntent(IntentCoroutine::push_type &yie
                 (field.friendlyGoal() - ball.position())
                     .norm(FREE_KICK_MAX_PROXIMITY + ROBOT_MAX_RADIUS_METERS);
 
-            Vector perpendicular_to_ball_direction =
-                ball_to_net_direction.perp().norm(ROBOT_MAX_RADIUS_METERS * robot_separation_scaling_factor);
+            Vector perpendicular_to_ball_direction = ball_to_net_direction.perp().norm(
+                ROBOT_MAX_RADIUS_METERS * robot_separation_scaling_factor);
 
             defend_position = free_kick_shadower == FreekickShadower::RIGHT
                                   ? ball.position() + ball_to_net_direction +
