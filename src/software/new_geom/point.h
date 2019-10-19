e051d31c7975853b67fde8a12eef8ed051c7caf1
scveloso <samtveloso@gmail.com>
2019-10-19 15:27:24 -0700

    First pass at point and vector implementation in new geom

---
+++ src/software/new_geom/point.h
@@ -0,0 +1,441 @@
+#pragma once
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/register/point.hpp>
+#include <boost/polygon/point_concept.hpp>
+#include <cmath>
+#include <iostream>
+
+#include "software/geom/angle.h"
+#include "software/new_geom/vector.h"
+
+/**
+ * A point in 2D space.
+ */
+class Point final
+{
+public:
+    // Due to internal representation of doubles being slightly less accurate/consistent
+    // with some numbers and operations, we consider points that are very close together
+    // to be equal (since they likely are, just possibly slightly misrepresented by the
+    // system/compiler). We use this EPSILON as a threshold for comparison. 1e-15 was
+    // chosen as a value because doubles have about 16 consistent significant figures.
+    // Comparing numbers with 15 significant figures gives us a
+    // small buffer while remaining as accurate as possible.
+    // http://www.cplusplus.com/forum/beginner/95128/
+    static constexpr double EPSILON = 1e-15;
+
+    /**
+     * Creates a Point at the origin (0, 0).
+     */
+    explicit constexpr Point();
+
+    /**
+     * Creates a unit-magnitude Point from an angle.
+     *
+     * @param angle the angle
+     *
+     * @return Point the Point
+     */
+    static Point createFromAngle(Angle angle);
+
+    /**
+     * Creates a Point at arbitrary coordinates.
+     *
+     * @param x the <var>x</var> value of the Point
+     * @param y the <var>y</var> value of the Point
+     */
+    constexpr Point(double x, double y);
+
+    /**
+     * Creates a new Point that is a copy of the given Point
+     *
+     * @param the Point to duplicate
+     */
+    constexpr Point(const Point &p);
+
+    /**
+     * Creates a new Point from a Vector
+     *
+     * @param the Vector to create a Point from
+     */
+    constexpr Point(const Vector &v);
+
+    /**
+     * Returns the x coordinate of this Point
+     *
+     * @return the x coordinate of this Point
+     */
+    constexpr double x() const;
+
+    /**
+     * Returns the y coordinate of this Point
+     *
+     * @return the y coordinate of this Point
+     */
+    constexpr double y() const;
+
+    /**
+     * Sets the coordinates of this point to the new coordinates
+     *
+     * @param x the new x coordinate
+     * @param y the new y coordinate
+     */
+    void set(double x, double y);
+
+    /**
+     * Sets the x coordinate of this point
+     *
+     * @param x the new x coordinate
+     */
+    void setX(double x);
+
+    /**
+     * Sets the y coordinate of this point
+     *
+     * @param y the new y coordinate
+     */
+    void setY(double y);
+
+    /**
+     * Returns the distance between this Point and origin (0,0)
+     *
+     * @return the distance
+     */
+    double distanceFromOrigin() const;
+
+    /**
+     * Returns the distance between this Point and the given point p
+     *
+     * @return the distance
+     */
+    double distanceFromPoint(const Point &p) const;
+
+    /**
+     * Returns the unit vector in direction of this point
+     *
+     * @return A unit vector in the direction of this Point, or a
+     * zero-vector if this Point is zero
+     */
+    Vector norm() const;
+
+    /**
+     * Returns a scaled normalized vector in the same direction as this
+     * Point
+     *
+     * @param length the desired length of the resultant vector
+     *
+     * @return a vector in the same direction as this Point and with the given length,
+     * or a zero-vector if this Point is zero
+     */
+    Vector norm(double length) const;
+
+    /**
+     * Rotates this Point counterclockwise by an angle
+     *
+     * @param rot the angle to rotate the vector
+     *
+     * @return the Point rotated by rot
+     */
+    Point rotate(Angle rot) const;
+
+    /**
+     * Returns the direction of this Point
+     *
+     * @return the direction of this Point, in the range [-π, π], with 0 being
+     * the positive x direction, π/2 being up (positive y), etc. like on a standard x-y
+     * plane
+     *
+     *              +
+     *              | Y
+     *              |
+     *              |
+     *      +---------------+
+     *       -X     |      X
+     *              |
+     *              | -Y
+     *              +
+     */
+    Angle orientation() const;
+
+    /**
+     * Checks whether this Point contains NaN in either coordinate
+     *
+     * @return true if either coordinate is NaN, and false otherwise
+     */
+    constexpr bool isnan() const;
+
+    /**
+     * Checks whether this Point is close to another Point, where “close”
+     * is defined as 1.0e-9
+     *
+     * @param other the other point to check against
+     *
+     * @return true if the other point is within a distance of 1.0e-9, not inclusive
+     */
+    constexpr bool isClose(const Point &other) const;
+
+    /**
+     * Checks whether this Point is close to another Point
+     *
+     * @param other the other point to check against
+     *
+     * @param dist the distance to check against
+     *
+     * @return true if the other point is within the given distance (not inclusive) of
+     * this Point
+     */
+    constexpr bool isClose(const Point &other, double dist) const;
+
+    /**
+     * Assigns one Point to another
+     *
+     * @param other the Point whose value should be copied into this Point
+     *
+     * @return this Point
+     */
+    Point &operator=(const Point &other);
+
+private:
+    /**
+     * The X coordinate of the Point. The variable name starts with an underscore to
+     * prevent name conflicts with its accessor function.
+     */
+    double _x;
+
+    /**
+     * The Y coordinate of the Point. The variable name starts with an underscore to
+     * prevent name conflicts with its accessor function.
+     */
+    double _y;
+};
+
+/**
+ * Adds a vector to a point
+ *
+ * @param p the Point
+ * @param v the Vector
+ *
+ * @return the Point sum of the given Point and Vector
+ */
+constexpr Point operator+(const Point &p, const Vector &v);
+
+/**
+ * Adds a vector to a point and sets that point to the resulting sum
+ *
+ * @param p the Point to add the Vector to
+ * @param v the Vector to add
+ *
+ * @return the new value of Point p
+ */
+Point &operator+=(Point &p, const Vector &v);
+
+/**
+ * Negates a Point
+ *
+ * @param p the Point to negate
+ *
+ * @return the point with its coordinates negated
+ */
+constexpr Point operator-(const Point &p) __attribute__((warn_unused_result));
+
+/**
+ * Subtracts one Point from another
+ *
+ * @param p the Point to subtract from
+ * @param q the Point to subtract
+ *
+ * @return the vector-difference of the two points
+ */
+constexpr Vector operator-(const Point &p, const Point &q)
+__attribute__((warn_unused_result));
+
+/**
+ * Prints a point to a stream
+ *
+ * @param os the stream to print to
+ * @param p the Point to print
+ *
+ * @return the stream with the point printed
+ */
+inline std::ostream &operator<<(std::ostream &os, const Point &p);
+
+/**
+ * Compares two Points for equality
+ *
+ * @param p the first Point
+ * @param q the second Point
+ *
+ * @return true if the two points represent the same point, and false otherwise
+ */
+constexpr bool operator==(const Point &p, const Point &q);
+
+/**
+ * Compares two points for inequality
+ *
+ * @param p the first Point
+ * @param q the second Point
+ *
+ * @return true if the two points represent different points, and false otherwise
+ */
+constexpr bool operator!=(const Point &p, const Point &q);
+
+inline Point Point::createFromAngle(Angle angle)
+{
+    return Point(angle.cos(), angle.sin());
+}
+
+inline constexpr Point::Point() : _x(0.0), _y(0.0) {}
+
+inline constexpr Point::Point(double x, double y) : _x(x), _y(y) {}
+
+inline constexpr Point::Point(const Point &p) : _x(p.x()), _y(p.y()) {}
+
+inline constexpr double Point::x() const
+{
+    return _x;
+}
+
+inline constexpr double Point::y() const
+{
+    return _y;
+}
+
+inline void Point::set(double x, double y)
+{
+    this->_x = x;
+    this->_y = y;
+}
+
+inline void Point::setX(double x)
+{
+    this->_x = x;
+}
+
+inline void Point::setY(double y)
+{
+    this->_y = y;
+}
+
+inline double Point::distanceFromOrigin() const {
+    return std::hypot(_x, _y);
+}
+
+inline double Point::distanceFromPoint(const Point &p) const {
+    sqrt(pow((_x - p.x()), 2) + pow((_y - p.y()), 2));
+}
+
+inline Vector Point::norm() const
+{
+    return distanceFromOrigin() < 1.0e-9 ? Vector() : Vector(_x / distanceFromOrigin(), _y / distanceFromOrigin());
+}
+
+inline Vector Point::norm(double length) const
+{
+    return distanceFromOrigin() < 1.0e-9 ? Vector() : Vector(_x * length / distanceFromOrigin(), _y * length / distanceFromOrigin());
+}
+
+inline Point Point::rotate(Angle rot) const
+{
+    return Point(_x * rot.cos() - _y * rot.sin(), _x * rot.sin() + _y * rot.cos());
+}
+
+inline Angle Point::orientation() const
+{
+    return Angle::ofRadians(std::atan2(_y, _x));
+}
+
+inline constexpr bool Point::isnan() const
+{
+    return std::isnan(_x) || std::isnan(_y);
+}
+
+inline constexpr bool Point::isClose(const Point &other) const
+{
+    return distanceFromPoint(other) < 1e-9;
+}
+
+inline constexpr bool Point::isClose(const Point &other, double dist) const
+{
+    return distanceFromPoint(other) < dist * dist;
+}
+
+inline Point &Point::operator=(const Point &q)
+{
+    _x = q.x();
+    _y = q.y();
+    return *this;
+}
+
+inline constexpr Point operator+(const Point &p, const Vector &v)
+{
+    return Point(p.x() + v.x(), p.y() + v.y());
+}
+
+inline Point &operator+=(Point &p, const Vector &v)
+{
+    p.set(p.x() + v.x(), p.y() + v.y());
+    return p;
+}
+
+inline constexpr Point operator-(const Point &p)
+{
+    return Point(-p.x(), -p.y());
+}
+
+inline constexpr Vector operator-(const Point &p, const Point &q)
+{
+    return Vector(p.x() - q.x(), p.y() - q.y());
+}
+
+inline std::ostream &operator<<(std::ostream &os, const Point &p)
+{
+    os << "(" << p.x() << ", " << p.y() << ")";
+    return os;
+}
+
+inline constexpr bool operator==(const Point &p, const Point &q)
+{
+    return p.isClose(q, Point::EPSILON);
+}
+
+inline constexpr bool operator!=(const Point &p, const Point &q)
+{
+    return !(p == q);
+}
+
+// We need to define a hash function so that the Point class can be used in unordered STL
+// containers
+// like unordered_set and unordered_map
+// https://prateekvjoshi.com/2014/06/05/using-hash-function-in-c-for-user-defined-classes/
+namespace std
+{
+    template <>
+    struct hash<Point> final
+    {
+        size_t operator()(const Point &p) const
+        {
+            hash<double> h;
+            return h(p.x()) * 17 + h(p.y());
+        }
+    };
+}  // namespace std
+
+// Make our Point class "compatible" with boost. This lets us pass our Points directly
+// into boost algorithms
+BOOST_GEOMETRY_REGISTER_POINT_2D_GET_SET(Point, double, cs::cartesian, x, y, setX, setY)
+template <>
+struct boost::polygon::geometry_concept<Point>
+{
+    typedef point_concept type;
+};
+template <>
+struct boost::polygon::point_traits<Point>
+{
+    typedef int coordinate_type;
+
+    static inline coordinate_type get(const Point &point, orientation_2d orient)
+    {
+        return (orient == HORIZONTAL) ? point.x() : point.y();
+    }
+};
---
+++ src/software/new_geom/vector.h
@@ -0,0 +1,533 @@
+#pragma once
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/register/point.hpp>
+#include <boost/polygon/point_concept.hpp>
+#include <cmath>
+#include <iostream>
+
+#include "software/geom/angle.h"
+
+/**
+ * A Euclidean vector in 2D space.
+ */
+class Vector final
+{
+public:
+    // Due to internal representation of doubles being slightly less accurate/consistent
+    // with some numbers and operations, we consider points that are very close together
+    // to be equal (since they likely are, just possibly slightly misrepresented by the
+    // system/compiler). We use this EPSILON as a threshold for comparison. 1e-15 was
+    // chosen as a value because doubles have about 16 consistent significant figures.
+    // Comparing numbers with 15 significant figures gives us a
+    // small buffer while remaining as accurate as possible.
+    // http://www.cplusplus.com/forum/beginner/95128/
+    static constexpr double EPSILON = 1e-15;
+
+    /**
+     * Creates a zero-vector.
+     */
+    explicit constexpr Vector();
+
+    /**
+     * Creates a unit-magnitude Vector from an angle.
+     *
+     * @param angle the angle
+     *
+     * @return Vector the Vector
+     */
+    static Vector createFromAngle(Angle angle);
+
+    /**
+     * Creates a Vector with arbitrary x and y values.
+     *
+     * @param x the <var>x</var> value of the Vector
+     * @param y the <var>y</var> value of the Vector
+     */
+    constexpr Vector(double x, double y);
+
+    /**
+     * Creates a new Vector that is a copy of the given Vector
+     *
+     * @param the Vector to duplicate
+     */
+    constexpr Vector(const Vector &v);
+
+    /**
+     * Returns the magnitude in the x-coordinate of this Vector
+     *
+     * @return the magnitude in the x-coordinate of this Vector
+     */
+    constexpr double x() const;
+
+    /**
+     * Returns the magnitude in the y-coordinate of this Vector
+     *
+     * @return the magnitude in the y-coordinate of this Vector
+     */
+    constexpr double y() const;
+
+    /**
+     * Sets the magnitudes of this point to the new magnitudes
+     *
+     * @param x the new magnitude in the x-coordinate
+     * @param y the new magnitude in the y-coordinate
+     */
+    void set(double x, double y);
+
+    /**
+     * Sets the magnitude in the x-coordinate of this vector
+     *
+     * @param x the new x-coordinate magnitude
+     */
+    void setX(double x);
+
+    /**
+     * Sets the magnitude in the y-coordinate of this vector
+     *
+     * @param y the new y-coordinate magnitude
+     */
+    void setY(double y);
+
+    /**
+     * Returns the square of the length of the Vector
+     *
+     * @return the square of the length of the Vector
+     */
+    constexpr double lensq() const;
+
+    /**
+     * Returns the length of the Vector
+     *
+     * @return the length of the Vector
+     */
+    double len() const;
+
+    /**
+     * Returns this Vector as a unit vector in the same direction
+     *
+     * @return Vector a unit vector in the same direction as this Vector, or a
+     * zero-vector if this Vector is a zero-vector
+     */
+    Vector norm() const;
+
+    /**
+     * Returns a scaled normalized vector in the same direction as this
+     * Vector
+     *
+     * @param length the desired length of the resultant vector
+     *
+     * @return a vector in the same direction as this Vector and with the given length,
+     * or a zero-vector if this Vector is a zero-vector
+     */
+    Vector norm(double length) const;
+
+    /**
+     * Returns the vector perpendicular to this Vector
+     *
+     * @return a vector perpendicular to this Vector
+     */
+    constexpr Vector perp() const;
+
+    /**
+     * Rotates this Vector counterclockwise by an angle
+     *
+     * @param rot the angle to rotate the vector
+     *
+     * @return the Vector rotated by rot
+     */
+    Vector rotate(Angle rot) const;
+
+    /**
+     * Projects this vector onto another vector
+     *
+     * @param the vector to project onto
+     *
+     * @return the component of this Vector that is in the same direction as the given
+     * Vector
+     */
+    constexpr Vector project(const Vector &other) const;
+
+    /**
+     * Takes the dot product of two vectors
+     *
+     * @param other the Vector to dot against
+     *
+     * @return the dot product of the vectors
+     */
+    constexpr double dot(const Vector &other) const;
+
+    /**
+     * Takes the cross product of two vectors
+     *
+     * @param other the Vector to cross with
+     *
+     * @return the z component of the 3-dimensional cross product between this Vector and
+     * the other Vector
+     */
+    constexpr double cross(const Vector &other) const;
+
+    /**
+     * Returns the direction of this Vector
+     *
+     * @return the direction of this Vector, in the range [-π, π], with 0 being
+     * the positive x direction, π/2 being up (positive y), etc. like on a standard x-y
+     * plane
+     *
+     *              +
+     *              | Y
+     *              |
+     *              |
+     *      +---------------+
+     *       -X     |      X
+     *              |
+     *              | -Y
+     *              +
+     */
+    Angle orientation() const;
+
+    /**
+     * Checks whether this Vector contains NaN in either of its x or y magnitude
+     *
+     * @return true if either x or y magnitude is NaN, and false otherwise
+     */
+    constexpr bool isnan() const;
+
+    /**
+     * Assigns one Vector to another
+     *
+     * @param other the Vector whose x and y magnitudes should be copied into this Vector
+     *
+     * @return this Vector
+     */
+    Vector &operator=(const Vector &other);
+
+private:
+    /**
+     * The magnitude in the X coordinate of the Vector. The variable name starts with an underscore to
+     * prevent name conflicts with its accessor function.
+     */
+    double _x;
+
+    /**
+     * The magnitude in the Y coordinate of the Vector. The variable name starts with an underscore to
+     * prevent name conflicts with its accessor function.
+     */
+    double _y;
+};
+
+/**
+ * Adds two vectors
+ *
+ * @param p the first Vector
+ * @param q the second Vector
+ *
+ * @return the vector-sum of the two vectors
+ */
+constexpr Vector operator+(const Vector &p, const Vector &q);
+
+/**
+ * Adds a Vector to another Vector and set the former vector to the sum
+ *
+ * @param u the Vector to add the other Vector to
+ * @param v the other Vector
+ *
+ * @return the new value of Vector u
+ */
+Vector &operator+=(Vector &u, const Vector &v);
+
+/**
+ * Negates a Vector
+ *
+ * @param v the Vector to negate
+ *
+ * @return the vector with its magnitudes negated
+ */
+constexpr Vector operator-(const Vector &p) __attribute__((warn_unused_result));
+
+/**
+ * Subtracts one Vector from another
+ *
+ * @param u the Vector to subtract from
+ * @param v the Vector to subtract
+ *
+ * @return the vector-difference of the two vectors
+ */
+constexpr Vector operator-(const Vector &u, const Vector &v)
+__attribute__((warn_unused_result));
+
+/**
+ * Subtracts from a Vector another Vector and set the former vector to the difference
+ *
+ * @paramp u the Vector to substract from
+ * @param v the Vector to subtract
+ *
+ * @return the new value of Vector u
+ */
+Vector &operator-=(Vector &u, const Vector &v);
+
+/**
+ * Multiplies a vector by a scalar
+ *
+ * @param s the scaling factor
+ * @param v the vector to scale
+ *
+ * @return the scaled vector
+ */
+constexpr Vector operator*(double s, const Vector &v);
+
+/**
+ * Multiplies a vector by a scalar
+ *
+ * @param p the vector to scale
+ * @param s the scaling factor
+ *
+ * @return the scaled vector
+ */
+constexpr Vector operator*(const Vector &p, double s);
+
+/**
+ * Scales a vector by a scalar
+ *
+ * @param p the vector to scale
+ * @param s the scaling factor
+ *
+ * @return p scaled by the scaling factor
+ */
+Vector &operator*=(Vector &p, double s);
+
+/**
+ * Divides a vector by a scalar
+ *
+ * @param p the vector to scale
+ * @param s the scalar to divide by
+ *
+ * @return the scaled vector
+ */
+constexpr Vector operator/(const Vector &p, double s);
+
+/**
+ * Scales a vector by a scalar
+ *
+ * @param p the vector to scale
+ * @param s the sclaing factor
+ *
+ * @return p scaled by the scaling factor
+ */
+Vector &operator/=(Vector &p, double s);
+
+/**
+ * Prints a vector to a stream
+ *
+ * @param os the stream to print to
+ * @param v the Vector to print
+ *
+ * @return the stream with the Vector printed
+ */
+inline std::ostream &operator<<(std::ostream &os, const Vector &v);
+
+/**
+ * Compares two Vectors for equality
+ *
+ * @param u the first Vector
+ * @param v the second Vector
+ *
+ * @return true if the two vectors represent the same vector, and false otherwise
+ */
+constexpr bool operator==(const Vector &u, const Vector &v);
+
+/**
+ * Compares two vectors for inequality
+ *
+ * @param u the first Vector
+ * @param v the second Vector
+ *
+ * @return true if the two vectors represent different vectors, and false otherwise
+ */
+constexpr bool operator!=(const Vector &u, const Vector &v);
+
+inline Vector Vector::createFromAngle(Angle angle)
+{
+    return Vector(angle.cos(), angle.sin());
+}
+
+inline constexpr Vector::Vector() : _x(0.0), _y(0.0) {}
+
+inline constexpr Vector::Vector(double x, double y) : _x(x), _y(y) {}
+
+inline constexpr Vector::Vector(const Vector &v) : _x(v.x()), _y(v.y()) {}
+
+inline constexpr double Vector::x() const
+{
+    return _x;
+}
+
+inline constexpr double Vector::y() const
+{
+    return _y;
+}
+
+inline void Vector::set(double x, double y)
+{
+    this->_x = x;
+    this->_y = y;
+}
+
+inline void Vector::setX(double x)
+{
+    this->_x = x;
+}
+
+inline void Vector::setY(double y)
+{
+    this->_y = y;
+}
+
+inline constexpr double Vector::lensq() const
+{
+    return _x * _x + _y * _y;
+}
+
+inline double Vector::len() const
+{
+    return std::hypot(_x, _y);
+}
+
+inline Vector Vector::norm() const
+{
+    return len() < 1.0e-9 ? Vector() : Vector(_x / len(), _y / len());
+}
+
+inline Vector Vector::norm(double length) const
+{
+    return len() < 1.0e-9 ? Vector() : Vector(_x * length / len(), _y * length / len());
+}
+
+inline constexpr Vector Vector::perp() const
+{
+    return Vector(-_y, _x);
+}
+
+inline Vector Vector::rotate(Angle rot) const
+{
+    return Vector(_x * rot.cos() - _y * rot.sin(), _x * rot.sin() + _y * rot.cos());
+}
+
+inline constexpr Vector Vector::project(const Vector &other) const
+{
+    return dot(other) / other.lensq() * other;
+}
+
+inline constexpr double Vector::dot(const Vector &other) const
+{
+    return _x * other.x() + _y * other.y();
+}
+
+inline constexpr double Vector::cross(const Vector &other) const
+{
+    return _x * other.y() - _y * other.x();
+}
+
+inline Vector &Vector::operator=(const Vector &q)
+{
+    _x = q.x();
+    _y = q.y();
+    return *this;
+}
+
+inline Angle Vector::orientation() const
+{
+    return Angle::ofRadians(std::atan2(_y, _x));
+}
+
+inline constexpr bool Vector::isnan() const
+{
+    return std::isnan(_x) || std::isnan(_y);
+}
+
+inline constexpr Vector operator+(const Vector &u, const Vector &v)
+{
+    return Vector(u.x() + v.x(), u.y() + v.y());
+}
+
+inline Vector &operator+=(Vector &u, const Vector &v)
+{
+    u.set(u.x() + v.x(), u.y() + v.y());
+    return u;
+}
+
+inline constexpr Vector operator-(const Vector &v)
+{
+    return Vector(-v.x(), -v.y());
+}
+
+inline constexpr Vector operator-(const Vector &u, const Vector &v)
+{
+    return Vector(u.x() - v.x(), u.y() - v.y());
+}
+
+inline Vector &operator-=(Vector &u, const Vector &v)
+{
+    u.set(u.x() - v.x(), u.y() - v.y());
+    return u;
+}
+
+inline constexpr Vector operator*(double s, const Vector &v)
+{
+    return Vector(v.x() * s, v.y() * s);
+}
+
+inline constexpr Vector operator*(const Vector &v, double s)
+{
+    return Vector(v.x() * s, v.y() * s);
+}
+
+inline Vector &operator*=(Vector &v, double s)
+{
+    v.set(v.x() * s, v.y() * s);
+    return v;
+}
+
+inline constexpr Vector operator/(const Vector &v, double s)
+{
+    return Vector(v.x() / s, v.y() / s);
+}
+
+inline Vector &operator/=(Vector &v, double s)
+{
+    v.set(v.x() / s, v.y() / s);
+    return v;
+}
+
+inline std::ostream &operator<<(std::ostream &os, const Vector &v)
+{
+    os << "(" << v.x() << ", " << v.y() << ")";
+    return os;
+}
+
+inline constexpr bool operator==(const Vector &u, const Vector &v)
+{
+    return (u-v).lensq() < Vector::EPSILON;
+}
+
+inline constexpr bool operator!=(const Vector &u, const Vector &v)
+{
+    return !(u == v);
+}
+
+// We need to define a hash function so that the Vector class can be used in unordered STL
+// containers
+// like unordered_set and unordered_map
+// https://prateekvjoshi.com/2014/06/05/using-hash-function-in-c-for-user-defined-classes/
+namespace std
+{
+    template <>
+    struct hash<Vector> final
+    {
+        size_t operator()(const Vector &v) const
+        {
+            hash<double> h;
+            return h(v.x()) * 17 + h(v.y());
+        }
+    };
+}  // namespace std
