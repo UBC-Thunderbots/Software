5ac04d26199e2e944b63535254cc1d2b9160f831
akhilveeraghanta <akhil.veeraghanta@outlook.com>
2019-10-19 23:34:46 -0700

    dpv2: staged missing file

---
+++ src/software/util/parameter/dynamic_parameters.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "software/util/parameter/config.hpp"
+
+namespace Util
+{
+    // TODO remove this as part of https://github.com/UBC-Thunderbots/Software/issues/960
+    const std::shared_ptr<ThunderbotsConfig> DynamicParameters =
+        std::make_shared<ThunderbotsConfig>();
+    const std::shared_ptr<const ThunderbotsConfig> DynamicParametersMutable =
+        std::const_pointer_cast<const ThunderbotsConfig>(DynamicParameters);
+}  // namespace Util
