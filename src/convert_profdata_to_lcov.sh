9b16595d7d38bce1aa7410a3e0f14864e989e3e4
Gareth Ellis <gareth.ellis0@gmail.com>
2019-10-23 17:44:26 -0700

    Lots of Fixes To Profdata To LCOV SCRIPT

--- src/convert_profdata_to_lcov.sh
+++ src/convert_profdata_to_lcov.sh
@@ -6,37 +6,39 @@ LOG_FILE_NAME=profdata_to_lcov.log
 
 # All the folders we'll search for object files in
 declare -a OBJECT_FILES_FOLDERS=(
-    "bazel-out/k8-fastbuild/bin/software "
+    "bazel-out/k8-fastbuild/bin/software"
     "bazel-out/k8-fastbuild/bin/firmware_new"
-    "bazel-out/k8-fastbuild/bin/firmware "
+    "bazel-out/k8-fastbuild/bin/firmware"
 )
 
 # Find all object files
-OBJECT_FILES=""
+declare -a OBJECT_FILES=()
 for folder in "${OBJECT_FILES_FOLDERS[@]}"; do
-    OBJECT_FILES="$OBJECT_FILES $(find -L  $folder -iname "*.o")"
+    for file in $(find -L  $folder -iname "*.o"); do 
+         OBJECT_FILES+=( "$file" )
+    done
 done
-
-# Find all profdata files (we assume bazel was configured to output profdata
-# to all the ".dat" files, there's no easy way to check that it's not just an
-# lcov file already)
-PROFDATA_FILES=$(find -L bazel-testlogs -iname "coverage.dat")
 
 # Create a list of arguments like "-o my_file1.o -o my_other_file.o ..." that 
 # we can pass into `llvm-cov`
 OBJECT_FILES_ARG=""
-for object_file in $OBJECT_FILES; do
+for object_file in "${OBJECT_FILES[@]}"; do
     OBJECT_FILES_ARG="$OBJECT_FILES_ARG -object $object_file"
 done
 
 # Remove the log if it already exists
 rm -f $LOG_FILE_NAME
 
-# Convert all the profdata files into lcov files
-for profdata_file_name in $PROFDATA_FILES; do
+# Find all profdata files (we assume bazel was configured to output profdata
+# to all the ".dat" files, there's no easy way to check that it's not just an
+# lcov file already), and convert them into lcov files
+for profdata_file_name in $(find -L bazel-testlogs -iname "coverage.dat"); do
     output_lcov_file_name="$profdata_file_name.lcov.txt"
 
     echo "Converting profdata file $profdata_file_name to lcov file: $output_lcov_file_name"
+
+    # Note the file we're converting in the log
+    echo "==== $output_lcov_file_name =====" >> $LOG_FILE_NAME
 
     # Remove the output if it already exists
     rm -f $output_lcov_file_name
