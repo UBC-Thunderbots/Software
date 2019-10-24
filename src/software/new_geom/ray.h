0a775f4835fa6111cbc63a5a79c17df1f0b64ea2
Jason Lee <jsn0716@gmail.com>
2019-10-24 11:37:40 -0700

    Initial implementation

---
+++ src/software/new_geom/ray.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "software/geom/line.h"
+#include "software/geom/point.h"
+
+class Ray final
+{
+   public:
+    /**
+     * Creates a degenerate Ray at (0, 0)
+     */
+    inline explicit constexpr Ray() {}
+
+    /**
+     * Creates a Ray with a start point and a direction
+     *
+     * @param start the starting point of Ray
+     * @param direction the direction of Ray
+     */
+    inline explicit Ray(const Point& start, const Vector& direction)
+        : start_(start), direction_(direction)
+    {
+    }
+
+    /**
+     * Returns the start point of this Ray
+     *
+     * @return the start point of this Ray
+     */
+    inline Point getStart() const
+    {
+        return start_;
+    }
+
+    /**
+     * Returns the direction of this Ray
+     *
+     * @return the direction of this Ray
+     */
+    inline Vector getDirection() const
+    {
+        return direction_;
+    }
+
+    /**
+     * Sets the start point of this Ray
+     *
+     * @param point the new start point
+     */
+    inline void setStart(const Point& start)
+    {
+        start_ = start;
+    }
+
+    /**
+     * Sets the direction of this Ray
+     *
+     * @param direction the new direction
+     */
+    inline void setDirection(const Vector& direction)
+    {
+        direction_ = direction;
+    }
+
+   private:
+    /**
+     * The start point of the ray.
+     */
+    Point start_;
+
+    /**
+     * The direction of the ray.
+     */
+    Vector direction_;
+};
---
+++ src/software/new_geom/ray_test.cpp
@@ -0,0 +1,29 @@
+#include "software/new_geom/ray.h"
+
+#include <gtest/gtest.h>
+
+TEST(RayTest, default_constructor)
+{
+    Ray r = Ray();
+    EXPECT_EQ(Point(), r.getStart());
+    EXPECT_EQ(Vector(), r.getDirection());
+}
+
+TEST(RayTest, start_direction_constructor)
+{
+    Ray r = Ray(Point(1, 2), Vector(2, 3));
+    EXPECT_EQ(Point(1, 2), r.getStart());
+    EXPECT_EQ(Vector(2, 3), r.getDirection());
+}
+
+TEST(RayTest, setters)
+{
+    Ray r = Ray(Point(3, 2), Vector(0, 1));
+    EXPECT_EQ(Point(3, 2), r.getStart());
+    EXPECT_EQ(Vector(0, 1), r.getDirection());
+
+    r.setStart(Point(-1.5, 2.3));
+    r.setDirection(Vector(4.2, -1.2));
+    EXPECT_EQ(Point(-1.5, 2.3), r.getStart());
+    EXPECT_EQ(Vector(4.2, -1.2), r.getDirection());
+}
