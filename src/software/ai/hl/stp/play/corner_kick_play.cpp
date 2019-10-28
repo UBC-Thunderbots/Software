dbe29445c175cb903bf8416b453d2d1a36dcbf86
Jonathan Lew <jonathanlew@github.com>
2019-10-28 14:29:03 -0700

    Remove more redundant avoid areas

--- src/software/ai/hl/stp/play/corner_kick_play.cpp
+++ src/software/ai/hl/stp/play/corner_kick_play.cpp
@@ -225,7 +225,6 @@ void CornerKickPlay::getNextTactics(TacticCoroutine::push_type &yield)
         receiver->updateWorldParams(world.friendlyTeam(), world.enemyTeam(),
                                     world.ball());
         receiver->updateControlParams(pass);
-        receiver->addWhitelistedAvoidArea(AvoidArea::BALL);
         goalie_tactic->updateWorldParams(world.ball(), world.field(),
                                          world.friendlyTeam(), world.enemyTeam());
 
