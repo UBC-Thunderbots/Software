8a5589f8c54d237869203e8be6b3bb224c28dfe9
Mathew MacDougall <mmacd97@gmail.com>
2019-10-30 14:20:15 -0700

    Addressed PR comments

--- src/software/backend/simulation/physics_field.cpp
+++ src/software/backend/simulation/physics_field.cpp
@@ -20,14 +20,14 @@ void PhysicsField::setupFieldBoundary(std::shared_ptr<b2World> world, const Fiel
     field_boundary_body_def.position.Set(0, 0);
     field_boundary_body = world->CreateBody(&field_boundary_body_def);
 
-    const unsigned int shape_loop_size = 4;
-    b2Vec2 vs[shape_loop_size]         = {
+    const unsigned int num_field_boundary_vertices = 4;
+    b2Vec2 field_boundary_vertices[num_field_boundary_vertices]         = {
         createVec2(field.fieldBoundary().posXPosYCorner()),
         createVec2(field.fieldBoundary().posXNegYCorner()),
         createVec2(field.fieldBoundary().negXNegYCorner()),
         createVec2(field.fieldBoundary().negXPosYCorner()),
     };
-    field_boundary_shape.CreateLoop(vs, shape_loop_size);
+    field_boundary_shape.CreateLoop(field_boundary_vertices, num_field_boundary_vertices);
     field_boundary_fixture_def.shape = &field_boundary_shape;
     // Collisions with the field boundary are perfectly elastic and have
     // no friction
@@ -46,13 +46,13 @@ void PhysicsField::setupEnemyGoal(std::shared_ptr<b2World> world, const Field &f
     enemy_goal_body_def.position.Set(0, 0);
     enemy_goal_body = world->CreateBody(&enemy_goal_body_def);
 
-    const unsigned int shape_chain_size          = 4;
-    b2Vec2 enemy_goal_vertices[shape_chain_size] = {
+    const unsigned int num_enemy_goal_vertices          = 4;
+    b2Vec2 enemy_goal_vertices[num_enemy_goal_vertices] = {
         createVec2(field.enemyGoalpostNeg()),
         createVec2(field.enemyGoalpostNeg() + Vector(field.goalXLength(), 0)),
         createVec2(field.enemyGoalpostPos() + Vector(field.goalXLength(), 0)),
         createVec2(field.enemyGoalpostPos())};
-    enemy_goal_shape.CreateChain(enemy_goal_vertices, shape_chain_size);
+    enemy_goal_shape.CreateChain(enemy_goal_vertices, num_enemy_goal_vertices);
     enemy_goal_fixture_def.shape = &enemy_goal_shape;
     // Collisions with the enemy goal are perfectly elastic and have no friction
     enemy_goal_fixture_def.restitution = 1.0;
@@ -70,13 +70,13 @@ void PhysicsField::setupFriendlyGoal(std::shared_ptr<b2World> world, const Field
     friendly_goal_body_def.position.Set(0, 0);
     friendly_goal_body = world->CreateBody(&friendly_goal_body_def);
 
-    const unsigned int shape_chain_size             = 4;
-    b2Vec2 friendly_goal_vertices[shape_chain_size] = {
+    const unsigned int num_friendly_goal_vertices             = 4;
+    b2Vec2 friendly_goal_vertices[num_friendly_goal_vertices] = {
         createVec2(field.friendlyGoalpostNeg()),
         createVec2(field.friendlyGoalpostNeg() - Vector(field.goalXLength(), 0)),
         createVec2(field.friendlyGoalpostPos() - Vector(field.goalXLength(), 0)),
         createVec2(field.friendlyGoalpostPos())};
-    friendly_goal_shape.CreateChain(friendly_goal_vertices, shape_chain_size);
+    friendly_goal_shape.CreateChain(friendly_goal_vertices, num_friendly_goal_vertices);
     friendly_goal_fixture_def.shape = &friendly_goal_shape;
     // Collisions with the friendly goal are perfectly elastic and have no friction
     friendly_goal_fixture_def.restitution = 1.0;
