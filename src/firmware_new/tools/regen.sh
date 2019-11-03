f22ef7c56b108ffbe95e3e1f3bad7a2aa70713c1
akhil <akhil.veeraghanta@outlook.com>
2019-11-02 20:15:35 -0700

    fix pr comments

--- src/firmware_new/tools/regen.sh
+++ src/firmware_new/tools/regen.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 #
-# This script allows the user to automatically generate cubemx code into our filestructure
-
-# --- begin runfiles.bash initialization ---
-# Copy-pasted from Bazel's Bash runfiles library (tools/bash/runfiles/runfiles.bash).
-# See here for why we need this:
-# https://github.com/bazelbuild/bazel/blob/8fa6b3fe71f91aac73c222d8082e75c69d814fa7/tools/bash/runfiles/runfiles.bash#L38-L59
-set -euo pipefail
-if [[ ! -d "${RUNFILES_DIR:-/dev/null}" && ! -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
-  if [[ -f "$0.runfiles_manifest" ]]; then
-    export RUNFILES_MANIFEST_FILE="$0.runfiles_manifest"
-  elif [[ -f "$0.runfiles/MANIFEST" ]]; then
-    export RUNFILES_MANIFEST_FILE="$0.runfiles/MANIFEST"
-  elif [[ -f "$0.runfiles/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
-    export RUNFILES_DIR="$0.runfiles"
-  fi
-fi
-if [[ -f "${RUNFILES_DIR:-/dev/null}/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
-  source "${RUNFILES_DIR}/bazel_tools/tools/bash/runfiles/runfiles.bash"
-elif [[ -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
-  source "$(grep -m1 "^bazel_tools/tools/bash/runfiles/runfiles.bash " \
-    "$RUNFILES_MANIFEST_FILE" | cut -d ' ' -f 2-)"
-else
-  echo >&2 "ERROR: cannot find @bazel_tools//tools/bash/runfiles:runfiles.bash"
-  exit 1
-fi
-set +e
-# --- end runfiles.bash initialization ---
+# This script allows the user to automatically generate cubemx
+# code into our filestructure
 
 # The directory this script is in
 CURR_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
@@ -41,14 +16,12 @@ TEMP_DIR="/tmp/tbots_autogen"
 # cleanup temp dir and create Src/Inc folder, the way Cube expects this output
 mkdir -p $TEMP_DIR/Src
 mkdir -p $TEMP_DIR/Inc
-rm -rf $TEMP_DIR/Src/*
-rm -rf $TEMP_DIR/Inc/*
 
 # make sure we got an argument
 NUM_ARGS=$#
 if [ $NUM_ARGS -ne 1 ]; then
     echo "Error: Incorrect number of arguments to script"
-    echo "Usage: $THIS_SCRIPT_FILENAME firmware_new/boards/frankie_v1"
+    echo "Usage: $THIS_SCRIPT_FILENAME path/to/folder/with/.ioc/file/"
     exit 1
 fi
 
