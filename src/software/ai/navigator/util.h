08a8b3f1482d4660072ca79bc9efba5fe1076840
Jonathan Lew <jonathanlew@github.com>
2019-11-03 18:46:46 -0800

    Remove unused navigator util functions

--- src/software/ai/navigator/util.h
+++ src/software/ai/navigator/util.h
@@ -19,29 +19,3 @@
  */
 double calculateTransitionSpeedBetweenSegments(const Point &p1, const Point &p2,
                                                const Point &p3, double final_speed);
-
-/**
- * Takes a vector of points that represents a path and converts them to a vector of move
- * primitives
- *
- * @param robot_id the robot_id to assign the move primitives and path to
- * @param points the vector of points representing a path
- * @return a vector of move primitives converted from a vector of points
- */
-std::vector<MovePrimitive> convertToMovePrimitives(unsigned int robot_id,
-                                                   const std::vector<Point> &points,
-                                                   DribblerEnable enable_dribbler,
-                                                   AutokickType autokick);
-/**
- * Calculates how much a point is trespassing in another point's space.
- *
- * Returns the distance that the point has trespassed within the given threshold of the
- * trespass point. If the trespass value is out of the threshold boundary, return 0.
- *
- * @param p1 A point
- * @param p2 The trespass point
- * @param trespass_threshold The threshold of the trespass point
- * @return Distance p1 has trespassed if it is within trespass_threshold. Returns 0
- * otherwise.
- */
-double getPointTrespass(const Point &p1, const Point &p2, double trespass_threshold);
