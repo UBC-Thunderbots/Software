8220943bdad3d5963bd9d215e09ac612061b8863
licody <licody2000@gmail.com>
2019-11-16 11:09:11 -0800

    added test file

--- src/software/ai/hl/stp/tactic/tactic_update_visiter_test.cpp
+++ src/software/ai/hl/stp/tactic/tactic_update_visiter_test.cpp
@@ -1,4 +1,12 @@
-//
-// Created by cody on 2019-11-09.
-//
+#include "software/ai/hl/stp/tactic/tactic_update_visitor.h"
 
+#include <gtest/gtest.h>
+
+#include "software/geom/util.h"
+#include "software/test_util/test_util.h"
+
+TEST(TacticUpdateVisitorTest, update_cherry_pick_tactic)
+{
+    World world = ::Test::TestUtil::createBlankTestingWorld();
+    CherryPickTactic tactic = CherryPickTactic(world, rectangle);
+}
