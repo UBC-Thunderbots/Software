2db280d774af7e048926d25a096c4b5349774980
hardik-ddod <hardik.singhi@alumni.ubc.ca>
2019-11-16 16:14:29 -0800

    Converted into class

---
+++ src/software/ai/hl/stp/play_info.cpp
@@ -0,0 +1,14 @@
+#include "software/ai/hl/stp/play_info.h"
+
+PlayInfo::PlayInfo():
+    play_type(""),
+    play_name(""),
+    robot_tactic_assignment({})
+{
+}
+
+bool PlayInfo::operator==(const PlayInfo &other) const
+{
+    return play_type == other.play_type && play_name == other.play_name &&
+           robot_tactic_assignment == other.robot_tactic_assignment;
+}
--- src/software/ai/hl/stp/play_info.h
+++ src/software/ai/hl/stp/play_info.h
@@ -3,22 +3,15 @@
 #include <string>
 #include <vector>
 
-struct PlayInfo
+class PlayInfo
 {
+    public:
+    explicit PlayInfo();
+    bool operator==(const PlayInfo& other) const;
+
+    private:
     std::string play_type;
     std::string play_name;
     std::vector<std::string> robot_tactic_assignment;
-
-    PlayInfo()
-    {
-        play_name               = "";
-        play_type               = "";
-        robot_tactic_assignment = {};
-    }
-
-    bool operator==(const PlayInfo& other) const
-    {
-        return play_type == other.play_type && play_name == other.play_name &&
-               robot_tactic_assignment == other.robot_tactic_assignment;
-    }
 };
+
