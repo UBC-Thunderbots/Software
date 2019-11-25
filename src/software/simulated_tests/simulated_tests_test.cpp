305b953460284885767e5b76584637b953bf98cd
Mathew MacDougall <mmacd97@gmail.com>
2019-11-25 14:53:59 -0800

    Created very simple tests to test the simulated test pipeline

--- src/software/simulated_tests/simulated_tests_test.cpp
+++ src/software/simulated_tests/simulated_tests_test.cpp
@@ -22,3 +22,20 @@ TEST_F(SimulatedTest, test_single_validation_function_passes_before_timeout)
 
     ASSERT_TRUE(test_passed);
 }
+
+TEST_F(SimulatedTest, test_single_validation_function_fails_if_it_times_out)
+{
+    World world         = ::Test::TestUtil::createBlankTestingWorld();
+    world.mutableBall() = Ball(Point(0, 0), Vector(4, 1.5), Timestamp::fromSeconds(0));
+    std::vector<ValidationFunction> validation_functions = {
+            [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
+                while(world_ptr->ball().position().x() < 3) {
+                    yield();
+                }
+            }
+    };
+
+    bool test_passed = backend->runSimulation(validation_functions, world, Duration::fromSeconds(0.5));
+
+    ASSERT_FALSE(test_passed);
+}
