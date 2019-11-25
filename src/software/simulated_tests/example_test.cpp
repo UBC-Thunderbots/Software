0214590680bba4b76e97deabd1a04278ca031cdf
Mathew MacDougall <mmacd97@gmail.com>
2019-11-25 14:40:07 -0800

    updated example test to use validation functions

--- src/software/simulated_tests/example_test.cpp
+++ src/software/simulated_tests/example_test.cpp
@@ -3,15 +3,22 @@
 #include "software/simulated_tests/simulated_test_fixture.h"
 #include "software/test_util/test_util.h"
 #include "software/util/time/duration.h"
+#include "software/backend/simulation/validation/validation_function.h"
 #include "software/world/world.h"
 
 TEST_F(SimulatedTest, example_simulated_test)
 {
     World world         = ::Test::TestUtil::createBlankTestingWorld();
     world.mutableBall() = Ball(Point(0, 0), Vector(4, 1.5), Timestamp::fromSeconds(0));
+    std::vector<ValidationFunction> validation_functions = {
+            [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
+                while(world_ptr->ball().position().x() < 3) {
+                    yield();
+                }
+            }
+    };
 
-    bool test_succeeded = backend->runSimulation(world, Duration::fromSeconds(10));
+    bool test_succeeded = backend->runSimulation(validation_functions, world, Duration::fromSeconds(2));
 
-    // Currently the simulation always times out because validation is not implemented yet
-    ASSERT_FALSE(test_succeeded);
+    ASSERT_TRUE(test_succeeded);
 }
