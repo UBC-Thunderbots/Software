864b4717c50f772045b8aa953511098e6c805397
Gareth Ellis <gareth.ellis0@gmail.com>
2019-11-30 09:09:16 -0800

    Updated MoveTactic To Yield Actions Instead Of Intents

--- src/software/ai/hl/stp/tactic/move_tactic.cpp
+++ src/software/ai/hl/stp/tactic/move_tactic.cpp
@@ -32,14 +32,14 @@ double MoveTactic::calculateRobotCost(const Robot &robot, const World &world)
 
 void MoveTactic::calculateNextAction(ActionCoroutine::push_type &yield)
 {
-    MoveAction move_action = MoveAction(0, Angle(), false);
+    auto move_action = std::make_shared<MoveAction>(0, Angle(), false);
     do
     {
-        move_action.updateControlParams(
+        move_action->updateControlParams(
             *robot, destination, final_orientation, final_speed, DribblerEnable::OFF,
             MoveType::NORMAL, AutokickType::NONE, BallCollisionType::AVOID);
-        yield(move_action.getNextIntent());
-    } while (!move_action.done());
+        yield(move_action);
+    } while (!move_action->done());
 }
 
 void MoveTactic::accept(TacticVisitor &visitor) const
