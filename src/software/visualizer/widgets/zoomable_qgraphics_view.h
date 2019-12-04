71d5f4ed89658f479fa26702ae7d43ccdef90e8f
Mathew MacDougall <mmacd97@gmail.com>
2019-12-03 20:30:14 -0800

    The Visualizer now zooms on the mouse rather than the center of the view

--- src/software/visualizer/widgets/zoomable_qgraphics_view.h
+++ src/software/visualizer/widgets/zoomable_qgraphics_view.h
@@ -17,6 +17,5 @@ class ZoomableQGraphicsView : public QGraphicsView
     void wheelEvent(QWheelEvent *event);
 
    private:
-    const double zoom_in_scaling_factor  = 1.04;
-    const double zoom_out_scaling_factor = 0.96;
+    const double scaling_factor_base = 1.0005;
 };
