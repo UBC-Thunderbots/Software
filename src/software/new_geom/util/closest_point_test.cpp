e660e8093be96a10af2115ce713023cf9171be68
Jason Lee <jsn0716@gmail.com>
2019-12-12 13:12:47 -0800

    Add tests

--- src/software/new_geom/util/closest_point_test.cpp
+++ src/software/new_geom/util/closest_point_test.cpp
@@ -2,16 +2,22 @@
 
 #include <gtest/gtest.h>
 
-TEST(ClosestPointTest, three_points_general)
+TEST(ClosestPointTest, three_points)
 {
     Point p = closestPointOnLine(Point(0, 0), Point(2, 0), Point(0, 2));
     EXPECT_EQ(p, Point(1, 1));
 }
 
-TEST(ClosestPointTest, point_line_general)
+TEST(ClosestPointTest, point_line1)
 {
     Point p = closestPointOnLine(Point(0, 0), Line(Point(2, 0), Point(0, 2)));
     EXPECT_EQ(p, Point(1, 1));
+}
+
+TEST(ClosestPointTest, point_line2)
+{
+    Point p = closestPointOnLine(Point(2, 2), Line(Point(3, 0), Point(0, 1.5)));
+    EXPECT_EQ(p, Point(1.4, 0.8));
 }
 
 TEST(ClosestPointTest, point_on_line)
--- src/software/new_geom/util/distance_test.cpp
+++ src/software/new_geom/util/distance_test.cpp
@@ -5,40 +5,30 @@
 #include "software/new_geom/line.h"
 #include "software/new_geom/point.h"
 
-TEST(DistanceTest, dist_line_point1)
+TEST(DistanceTest, vertical_line_point)
 {
-    // TODO: break this up
-    double calculated_val, expected_val;
+    Line l(Point(0, 1), Point(0, 0));
+    Point p(2, 0);
+    EXPECT_DOUBLE_EQ(distance(l, p), 2);
+    EXPECT_DOUBLE_EQ(distance(p, l), 2);
+}
 
-    // case 1
-    Line test1line(Point(0, 1), Point(0, 0));
-    Point test1p(2, 0);
-    calculated_val = distance(test1line, test1p);
-    expected_val   = 2;
-    EXPECT_EQ(expected_val, calculated_val);
+TEST(DistanceTest, line_point1)
+{
+    Line l(Point(2, 0), Point(0, 2));
+    Point p(0, 0);
+    EXPECT_DOUBLE_EQ(distance(l, p), sqrt(2));
+}
 
-    // case 2
-    Line test2line(Point(2, 0), Point(0, 2));
-    Point test2p(0, 0);
-    calculated_val = distance(test2line, test2p);
-    expected_val   = sqrt(2);
-    EXPECT_DOUBLE_EQ(expected_val, calculated_val);
-
-    // TODO: test degenerate case
-    //    Line test3line(Point(0, 0), Point(0, 0));
-    //    Point test3p(1, 0);
-    //    calculated_val = distance(test3line, test3p);
-    //    expected_val   = 1;
-    //    EXPECT_EQ(expected_val, calculated_val);
-
-    Line line(Point(1, -1), Point(5, -2));
+TEST(DistanceTest, line_point2)
+{
+    Line l(Point(1, -1), Point(5, -2));
     Point p(2, -3);
-
-    EXPECT_NEAR(1.69775, distance(line, p), 1e-5);
+    EXPECT_NEAR(1.69775, distance(l, p), 1e-5);
 
     p = Point(2, 1);
-    EXPECT_NEAR(2.18282, distance(line, p), 1e-5);
+    EXPECT_NEAR(2.18282, distance(l, p), 1e-5);
 
     p = Point(2, 0);
-    EXPECT_NEAR(1.21268, distance(line, p), 1e-5);
+    EXPECT_NEAR(1.21268, distance(l, p), 1e-5);
 }
