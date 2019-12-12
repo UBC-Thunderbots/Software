9392c95595d17a0406bfc3a4946933a866826cbe
Jason Lee <jsn0716@gmail.com>
2019-12-12 15:05:35 -0800

    Rename tests and add more test cases

--- src/software/new_geom/util/distance_test.cpp
+++ src/software/new_geom/util/distance_test.cpp
@@ -5,30 +5,38 @@
 #include "software/new_geom/line.h"
 #include "software/new_geom/point.h"
 
-TEST(DistanceTest, vertical_line_point)
+TEST(DistanceTest, point_on_line)
+{
+    Line l(Point(2, 0), Point(0, 2));
+    Point p(1, 1);
+    double expected = 0;
+    EXPECT_DOUBLE_EQ(distance(l, p), expected);
+    EXPECT_DOUBLE_EQ(distance(p, l), expected);
+}
+
+TEST(DistanceTest, point_near_vertical_line)
 {
     Line l(Point(0, 1), Point(0, 0));
     Point p(2, 0);
-    EXPECT_DOUBLE_EQ(distance(l, p), 2);
-    EXPECT_DOUBLE_EQ(distance(p, l), 2);
+    double expected = 2;
+    EXPECT_DOUBLE_EQ(distance(l, p), expected);
+    EXPECT_DOUBLE_EQ(distance(p, l), expected);
 }
 
-TEST(DistanceTest, line_point1)
+TEST(DistanceTest, point_near_line)
 {
     Line l(Point(2, 0), Point(0, 2));
     Point p(0, 0);
-    EXPECT_DOUBLE_EQ(distance(l, p), sqrt(2));
+    double expected = sqrt(2);
+    EXPECT_DOUBLE_EQ(distance(l, p), expected);
+    EXPECT_DOUBLE_EQ(distance(p, l), expected);
 }
 
-TEST(DistanceTest, line_point2)
+TEST(DistanceTest, point_far_from_line)
 {
-    Line l(Point(1, -1), Point(5, -2));
-    Point p(2, -3);
-    EXPECT_NEAR(1.69775, distance(l, p), 1e-5);
-
-    p = Point(2, 1);
-    EXPECT_NEAR(2.18282, distance(l, p), 1e-5);
-
-    p = Point(2, 0);
-    EXPECT_NEAR(1.21268, distance(l, p), 1e-5);
+    Line l(Point(0, -4), Point(10, 2));
+    Point p(179500, -299164);
+    double expected = 348879.57175;
+    EXPECT_NEAR(expected, distance(l, p), 1e-5);
+    EXPECT_NEAR(expected, distance(p, l), 1e-5);
 }
