4c5153693b489259d4869ff2999dd8b010a16201
Gareth Ellis <garethellis0@users.noreply.github.com>
2019-12-29 22:19:19 -0400

    Added Proper Goalie Assignment in STP (#1108)
    * Added Print Operator To RobotCapabilities
    
    * Misc. Refactoring And Some Final Sanity Checks In STP
    
    * wip
    
    * Succesfully Added Goalie-Specific Handling To STP
    
    * Made Sure The Goalie Tactic Overrides The isGoalie Method
    
    * Misc. Cleanup
    
    * Fixed Formatting
    
    * More cleanup
    
    * Fixed Formatting
    
    * Addressed Mats Comment
    
    * removed robot_capabilities.cpp
    
    * Added Tests For Robot RobotCapabilities Whitelist And Blacklist

--- src/software/ai/hl/stp/stp.cpp
+++ src/software/ai/hl/stp/stp.cpp
@@ -2,6 +2,7 @@
 
 #include <munkres/munkres.h>
 
+#include <algorithm>
 #include <chrono>
 #include <exception>
 #include <g3log/g3log.hpp>
@@ -83,10 +84,11 @@ std::vector<std::unique_ptr<Intent>> STP::getIntentsFromCurrentPlay(const World&
     std::vector<std::unique_ptr<Intent>> intents;
     if (current_tactics)
     {
-        auto assigned_tactics = assignRobotsToTactics(world, *current_tactics);
+        assignRobotsToTactics(world, *current_tactics);
 
-        for (const std::shared_ptr<Tactic>& tactic : assigned_tactics)
+        for (const std::shared_ptr<Tactic>& tactic : *current_tactics)
         {
+            // Try to get an intent from the tactic
             std::shared_ptr<Action> action = tactic->getNextAction();
             std::unique_ptr<Intent> intent;
             if (action)
@@ -126,8 +128,8 @@ std::vector<std::unique_ptr<Intent>> STP::getIntents(const World& world)
     return getIntentsFromCurrentPlay(world);
 }
 
-std::vector<std::shared_ptr<Tactic>> STP::assignRobotsToTactics(
-    const World& world, std::vector<std::shared_ptr<Tactic>> tactics) const
+void STP::assignRobotsToTactics(const World& world,
+                                std::vector<std::shared_ptr<Tactic>> tactics) const
 {
     // This functions optimizes the assignment of robots to tactics by minimizing
     // the total cost of assignment using the Hungarian algorithm
@@ -137,16 +139,43 @@ std::vector<std::shared_ptr<Tactic>> STP::assignRobotsToTactics(
     // https://github.com/saebyn/munkres-cpp is the implementation of the Hungarian
     // algorithm that we use here
 
-    if (world.friendlyTeam().numRobots() < tactics.size())
+    auto friendly_team         = world.friendlyTeam();
+    auto& friendly_team_robots = friendly_team.getAllRobots();
+
+    // Special handling for the Goalie tactics, since only one robot per team is permitted
+    // to act as the goalie
+    const std::optional<Robot> goalie    = friendly_team.goalie();
+    std::vector<Robot> non_goalie_robots = friendly_team_robots;
+    auto isGoalieTactic                  = [](std::shared_ptr<Tactic> tactic) {
+        return tactic->isGoalieTactic();
+    };
+
+    auto it = tactics.begin();
+    if (goalie)
+    {
+        non_goalie_robots.erase(
+            std::find(non_goalie_robots.begin(), non_goalie_robots.end(), *goalie));
+
+        // Assign the goalie to the first goalie tactic
+        auto iter = std::find_if(tactics.begin(), tactics.end(), isGoalieTactic);
+        if (iter != tactics.end())
+        {
+            (*iter)->updateRobot(*goalie);
+        }
+    }
+
+    // Discard all goalie tactics, since we have already assigned the goalie robot (if
+    // there is one) to the first goalie tactic, and there should only ever be one goalie
+    tactics.erase(std::remove_if(tactics.begin(), tactics.end(), isGoalieTactic),
+                  tactics.end());
+
+    if (non_goalie_robots.size() < tactics.size())
     {
         // We do not have enough robots to assign all the tactics to. We "drop"
         // (aka don't assign) the tactics at the end of the vector since they are
         // considered lower priority
-        tactics.resize(world.friendlyTeam().numRobots());
+        tactics.resize(non_goalie_robots.size());
     }
-
-    auto friendly_team        = world.friendlyTeam();
-    auto friendly_team_robots = friendly_team.getAllRobots();
 
     size_t num_rows = world.friendlyTeam().numRobots();
     size_t num_cols = tactics.size();
@@ -156,7 +185,7 @@ std::vector<std::shared_ptr<Tactic>> STP::assignRobotsToTactics(
     // This represents the cases where there are either no tactics or no robots
     if (num_rows == 0 || num_cols == 0)
     {
-        return {};
+        return;
     }
 
     // The rows of the matrix are the "workers" (the robots) and the columns are the
@@ -164,23 +193,32 @@ std::vector<std::shared_ptr<Tactic>> STP::assignRobotsToTactics(
     Matrix<double> matrix(num_rows, num_cols);
 
     // Initialize the matrix with the cost of assigning each Robot to each Tactic
-    for (unsigned row = 0; row < num_rows; row++)
+    for (size_t row = 0; row < num_rows; row++)
     {
-        for (unsigned col = 0; col < num_cols; col++)
+        for (size_t col = 0; col < num_cols; col++)
         {
-            if (friendly_team_robots.at(row).getCapabilitiesBlacklist().size() > 0 &&
-                friendly_team_robots.at(row).getCapabilitiesBlacklist() <=
-                    tactics.at(col)->robotCapabilityRequirements())
+            Robot robot                     = non_goalie_robots.at(row);
+            std::shared_ptr<Tactic>& tactic = tactics.at(col);
+            double robot_cost_for_tactic    = tactic->calculateRobotCost(robot, world);
+
+            std::set<RobotCapabilities::Capability> required_capabilities =
+                tactic->robotCapabilityRequirements();
+            std::set<RobotCapabilities::Capability> robot_capabilities =
+                robot.getCapabilitiesWhitelist();
+            std::set<RobotCapabilities::Capability> missing_capabilities;
+            std::set_difference(
+                required_capabilities.begin(), required_capabilities.end(),
+                robot_capabilities.begin(), robot_capabilities.end(),
+                std::inserter(missing_capabilities, missing_capabilities.begin()));
+
+            if (missing_capabilities.size() > 0)
             {
-                // if the blacklist contains any subset of the required capabilties,
-                // set the cost to 10.0f
-                matrix(row, col) = 10.0f;
+                matrix(row, col) = robot_cost_for_tactic + 10.0f;
             }
             else
             {
-                // hardware requirements are satisfied, calculate real cost
-                matrix(row, col) = tactics.at(col)->calculateRobotCost(
-                    friendly_team_robots.at(row), world);
+                // capability requirements are satisfied, use real cost
+                matrix(row, col) = robot_cost_for_tactic;
             }
         }
     }
@@ -198,20 +236,18 @@ std::vector<std::shared_ptr<Tactic>> STP::assignRobotsToTactics(
     //        -1, 0,-1,         and            0,-1,
     //         0,-1,-1,                       -1, 0,
     //        -1,-1, 0,
-    for (unsigned row = 0; row < num_rows; row++)
+    for (size_t row = 0; row < num_rows; row++)
     {
-        for (unsigned col = 0; col < num_cols; col++)
+        for (size_t col = 0; col < num_cols; col++)
         {
             auto val = matrix(row, col);
             if (val == 0)
             {
-                tactics.at(col)->updateRobot(friendly_team_robots.at(row));
+                tactics.at(col)->updateRobot(non_goalie_robots.at(row));
                 break;
             }
         }
     }
-
-    return tactics;
 }
 
 std::unique_ptr<Play> STP::calculateNewPlay(const World& world)
