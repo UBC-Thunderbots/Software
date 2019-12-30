cee083ffef3ec1f612e54abd79139a22bc6302de
Gareth Ellis <garethellis0@users.noreply.github.com>
2019-12-29 22:19:19 -0400

    Added Proper Goalie Assignment in STP (#1108)
    * Added Print Operator To RobotCapabilities
    
    * Misc. Refactoring And Some Final Sanity Checks In STP
    
    * wip
    
    * Succesfully Added Goalie-Specific Handling To STP
    
    * Made Sure The Goalie Tactic Overrides The isGoalie Method
    
    * Misc. Cleanup
    
    * Fixed Formatting
    
    * More cleanup
    
    * Fixed Formatting
    
    * Addressed Mats Comment
    
    * removed robot_capabilities.cpp
    
    * Added Tests For Robot RobotCapabilities Whitelist And Blacklist

--- src/software/world/robot.cpp
+++ src/software/world/robot.cpp
@@ -258,6 +258,21 @@ const std::set<RobotCapabilities::Capability> &Robot::getCapabilitiesBlacklist()
     return unavailable_capabilities_;
 }
 
+std::set<RobotCapabilities::Capability> Robot::getCapabilitiesWhitelist() const
+{
+    // robot capabilities = all possible capabilities - unavailable capabilities
+
+    std::set<RobotCapabilities::Capability> all_capabilities =
+        RobotCapabilities::allCapabilities();
+    std::set<RobotCapabilities::Capability> robot_capabilities;
+    std::set_difference(all_capabilities.begin(), all_capabilities.end(),
+                        getCapabilitiesBlacklist().begin(),
+                        getCapabilitiesBlacklist().end(),
+                        std::inserter(robot_capabilities, robot_capabilities.begin()));
+
+    return robot_capabilities;
+}
+
 std::set<RobotCapabilities::Capability> &Robot::getMutableRobotCapabilities()
 {
     return unavailable_capabilities_;
