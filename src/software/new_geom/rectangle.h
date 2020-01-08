c812a28b1cecc7f196d30f7d350cb229ec0f1bd8
Sam Veloso <samtveloso@gmail.com>
2020-01-07 17:12:27 -0800

    Geom: Polygon and Rectangle (#1095)
    * Saving progress on shape hierarchy
    
    * Saving progress shape hierarchy
    
    * Finish rectangle
    
    * Fix formatting
    
    * Address review comments
    
    * Address review comments
    
    * Address more code review comments
    
    * Added area for convex polygons removed rectangle area
    
    * Fix formatting
    
    * Address code review comments
    
    * Address review
    
    * Address review
    
    * Apply suggestions from code review
    
    Adding @throws to ConvexPolygon constructor comments
    
    Co-Authored-By: Gareth Ellis <garethellis0@users.noreply.github.com>
    
    * Fix formatting
    
    * Change comparison in isConvex to use EPSILON
    
    Co-authored-by: Gareth Ellis <garethellis0@users.noreply.github.com>

---
+++ src/software/new_geom/rectangle.h
@@ -0,0 +1,95 @@
+#include "software/new_geom/convex_polygon.h"
+
+/**
+ * A rectangle that cannot be rotated.
+ */
+class Rectangle : public ConvexPolygon
+{
+   public:
+    /**
+     * Creates a new Rectangle from two corners.
+     *
+     *   *--------p1/2      p1/2--------*
+     *   |          |        |          |
+     * p1/2---------*   or   *--------p1/2
+     *
+     * @param point1 One of the rectangle's corners
+     *
+     * @param point2 The corner diagonally-opposite to point1
+     */
+    explicit Rectangle(const Point &point1, const Point &point2);
+
+    /**
+     * Returns the length along the x-axis of the rectangle
+     *
+     * @return The length along the x-axis of the rectangle
+     */
+    double xLength() const;
+
+    /**
+     * Returns the length along the y-axis of the rectangle
+     *
+     * @return The length along the y-axis of the rectangle
+     */
+    double yLength() const;
+
+    /**
+     * Returns the centre of the rectangle
+     *
+     * @return The centre of the rectangle
+     */
+    Point centre() const;
+
+    /**
+     * Returns the <+x,+y> corner of the rectangle
+     *
+     * @return The <+x,+y> corner of the rectangle
+     */
+    Point posXPosYCorner() const;
+
+    /**
+     * Returns the <-x,+y> corner of the rectangle
+     *
+     * @return The <-x,+y> corner of the rectangle
+     */
+    Point negXPosYCorner() const;
+
+    /**
+     * Returns the <-x,-y> corner of the rectangle
+     *
+     * @return The <-x,-y> corner of the rectangle
+     */
+    Point negXNegYCorner() const;
+
+    /**
+     * Returns the <+x,-y> corner of the rectangle
+     *
+     * @return The <+x,-y> corner of the rectangle
+     */
+    Point posXNegYCorner() const;
+
+    /**
+     * Determines whether the given Point is contained within this Rectangle.
+     *
+     * @return whether the Point p is contained within this Rectangle.
+     */
+    bool contains(const Point &p) const override;
+
+    /**
+     * Returns the corner point of the rectangle that is the furthest from the input
+     * point.
+     *
+     * @param p The point to test
+     * @return The corner point that is furthest from the test point
+     */
+    Point furthestCorner(const Point &p);
+
+    /**
+     * Returns a vector from negXNegY point of the rectangle to the posXPosY point
+     *
+     * @return The vector representing the diagonal of the rectangle
+     */
+    Vector diagonal() const;
+
+    bool operator==(const Rectangle &p) const;
+};
