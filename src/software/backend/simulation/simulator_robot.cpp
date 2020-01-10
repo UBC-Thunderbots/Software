ad790e650506b58de92925373674599a9a41d6aa
Mathew MacDougall <mmacd97@gmail.com>
2020-01-09 21:07:06 -0800

    added skeleton code for SimulatorRobot

---
+++ src/software/backend/simulation/simulator_robot.cpp
@@ -0,0 +1,71 @@
+#include "software/backend/simulation/simulator_robot.h"
+
+#include "app/world/chicker.h"
+#include "app/world/dribbler.h"
+#include "app/world/wheel.h"
+
+void SimulatorRobot::setRobotId(unsigned int id)
+{
+    robot_id = id;
+}
+
+FirmwareRobot_t* SimulatorRobot::createFirmwareRobot()
+{
+    Chicker_t* chicker = app_chicker_create(
+        &(SimulatorRobot::kick), &(SimulatorRobot::chip),
+        &(SimulatorRobot::enableAutokick), &(SimulatorRobot::enableAutochip),
+        &(SimulatorRobot::disableAutokick), &(SimulatorRobot::disableAutochip));
+
+    Dribbler_t* dribbler =
+        app_dribbler_create(&(SimulatorRobot::setDribblerSpeed),
+                            &(SimulatorRobot::getDribblerTemperatureDegC));
+
+    Wheel_t* front_left_wheel =
+        app_wheel_create(&(SimulatorRobot::applyWheelForceFrontLeft));
+    Wheel_t* front_right_wheel =
+        app_wheel_create(&(SimulatorRobot::applyWheelForceFrontRight));
+    Wheel_t* back_left_wheel =
+        app_wheel_create(&(SimulatorRobot::applyWheelForceBackLeft));
+    Wheel_t* back_right_wheel =
+        app_wheel_create(&(SimulatorRobot::applyWheelForceBackRight));
+
+    FirmwareRobot_t* firmware_robot =
+        app_firmware_robot_create(chicker, dribbler, &(SimulatorRobot::getPositionX),
+                                  &(SimulatorRobot::getPositionY), front_right_wheel,
+                                  front_left_wheel, back_right_wheel, back_left_wheel);
+
+    return firmware_robot;
+}
+
+float SimulatorRobot::getPositionX() {}
+
+float SimulatorRobot::getPositionY() {}
+
+void SimulatorRobot::kick(float speed_m_per_s) {}
+
+void SimulatorRobot::chip(float distance_m) {}
+
+void SimulatorRobot::enableAutokick(float speed_m_per_s) {}
+
+void SimulatorRobot::enableAutochip(float distance_m) {}
+
+void SimulatorRobot::disableAutokick() {}
+
+void SimulatorRobot::disableAutochip() {}
+
+void SimulatorRobot::setDribblerSpeed(uint32_t rpm) {}
+
+unsigned int SimulatorRobot::getDribblerTemperatureDegC()
+{
+    // Return a somewhat arbitrary "room temperature" temperature.
+    // This is an ideal simulation so the dribbler will not overheat
+    return 25;
+}
+
+void SimulatorRobot::applyWheelForceFrontLeft(float force_in_newtons) {}
+
+void SimulatorRobot::applyWheelForceBackLeft(float force_in_newtons) {}
+
+void SimulatorRobot::applyWheelForceBackRight(float force_in_newtons) {}
+
+void SimulatorRobot::applyWheelForceFrontRight(float force_in_newtons) {}
---
+++ src/software/backend/simulation/simulator_robot.h
@@ -0,0 +1,114 @@
+#include <cinttypes>
+#include "app/world/firmware_robot.h"
+
+/**
+ * This class acts as a wrapper around a PhysicsRobot so that the PhysicsRobot
+ * can provide the interface of a FirmwareRobot
+ */
+class SimulatorRobot {
+public:
+    /**
+     * Sets the ID of the robot being controlled by this class
+     *
+     * @param id The ID of the robot to control
+     */
+    static void setRobotId(unsigned int id);
+
+    /**
+     * Creates a FirmwareRobot corresponding to the current PhysicsRobot
+     *
+     * @return a FirmwareRobot corresponding to the current PhysicsRobot
+     */
+    static FirmwareRobot_t* createFirmwareRobot();
+
+    /* Position functions */
+    /**
+     * Returns the x-position of the robot, in global field coordinates
+     *
+     * @return the x-position of the robot, in global field coordinates
+     */
+    static float getPositionX();
+
+    /**
+     * Returns the y-position of the robot, in global field coordinates
+     *
+     * @return the y-position of the robot, in global field coordinates
+     */
+    static float getPositionY();
+
+    /* Chicker functions */
+    /**
+     * Fires the kicker, kicking the ball in the direction the robot is facing
+     * at the given speed if the ball is very close to the kicker
+     *
+     * @param speed_m_per_s How fast to kick the ball, in meters per second
+     */
+    static void kick(float speed_m_per_s);
+
+    /**
+     * Fires the chipper, chipping the ball in the direction the robot is facing
+     * for the given distance if the ball is very close to the chipper
+     *
+     * @param speed_m_per_s How far to chip the ball (the distance to the first bounce)
+     * in meters
+     */
+    static void chip(float distance_m);
+
+    /**
+     * Enables autokick on the robot. If the ball touches the kicker, the robot will
+     * kick the ball with the given speed.
+     *
+     * @param speed_m_per_s How fast to kick the ball in meters per second when
+     * the kicker is fired
+     */
+    static void enableAutokick(float speed_m_per_s);
+
+    /**
+     * Enables autochip on the robot. If the ball touches the chipper, the robot will
+     * chip the ball the given distance.
+     *
+     * @param speed_m_per_s How far to chip the ball (distance to the first bounce)
+     * when the chipper is fired
+     */
+    static void enableAutochip(float distance_m);
+
+    /**
+     * Disables autokick
+     */
+    static void disableAutokick();
+
+    /**
+     * Disables autochip
+     */
+    static void disableAutochip();
+
+    /* Dribbler functions */
+    /**
+     * Sets the speed of the dribbler
+     *
+     * @param rpm The rpm to set for the dribbler
+     */
+    static void setDribblerSpeed(uint32_t rpm);
+
+    /**
+     * Returns the temperature of the dribbler, in degrees C
+     *
+     * @return the temperature of the dribbler, in degrees C
+     */
+    static unsigned int getDribblerTemperatureDegC();
+
+    /* Wheel functions */
+    /**
+     * Applies the given force to the wheel
+     *
+     * @param force_in_newtons the force to apply to the wheel
+     */
+    static void applyWheelForceFrontLeft(float force_in_newtons);
+    static void applyWheelForceBackLeft(float force_in_newtons);
+    static void applyWheelForceBackRight(float force_in_newtons);
+    static void applyWheelForceFrontRight(float force_in_newtons);
+
+private:
+    // The id of the robot currently being controlled by this class
+    static unsigned int robot_id;
+};
