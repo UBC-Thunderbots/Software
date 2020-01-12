8b993b42a6e3029d16e0bf275f514a675c15c236
Sam Veloso <samtveloso@gmail.com>
2020-01-12 12:24:00 -0800

    Geom: Triangle (#1113)
    * Implemented triangle in new_geom shape hierarchy
    
    * Fix formatting
    
    * Removed accidentally commited ide file
    
    * Break up new_geom library into individual shape libraries
    
    * Fix no longer existing references to new_geom library
    
    * Fix BUILD BUILD dependencies to new_geom libraries

---
+++ src/software/new_geom/triangle.cpp
@@ -0,0 +1,22 @@
+#include "software/new_geom/triangle.h"
+
+#include "software/new_geom/point.h"
+
+Triangle::Triangle(const Point &point1, const Point &point2, const Point &point3)
+    : ConvexPolygon({point1, point2, point3})
+{
+}
+
+Point Triangle::center() const
+{
+    Point p1 = points_[0];
+    Point p2 = points_[1];
+    Point p3 = points_[2];
+
+    double center_x = (p1.x() + p2.x() + p3.x()) / 3;
+    double center_y = (p1.y() + p2.y() + p3.y()) / 3;
+
+    Point center = Point(center_x, center_y);
+
+    return center;
+}
