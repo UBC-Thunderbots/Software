dc2158923f2d2593dccb03b6f77d6ce11af9417b
jared-paul <jaredpaul9820@gmail.com>
2020-01-18 14:41:18 -0800

    initial commmit, implementation of a PhysicsWorld for simulation purposes

---
+++ src/software/backend/simulation/physics/physics_world.cpp
@@ -0,0 +1,92 @@
+#include "physics_world.h"
+
+PhysicsWorld::PhysicsWorld(const World& world)
+{
+    b2Vec2 gravity(0, 0);
+    physics_world = std::make_shared<b2World>(gravity);
+    physics_world_timestamp = Timestamp::fromSeconds(0);
+
+    initWorld(world);
+}
+
+void PhysicsWorld::initWorld(const World& world)
+{
+    physics_field = std::make_shared<PhysicsField>(physics_world, world.field());
+    physics_ball  = std::make_shared<PhysicsBall>(physics_world, world.ball());
+    friendly_physics_robots.clear();
+    for (const auto& friendly_robot : world.friendlyTeam().getAllRobots())
+    {
+        friendly_physics_robots.emplace_back(
+                std::make_shared<PhysicsRobot>(physics_world, friendly_robot));
+    }
+    enemy_physics_robots.clear();
+    for (const auto& enemy_robot : world.enemyTeam().getAllRobots())
+    {
+        enemy_physics_robots.emplace_back(
+                std::make_shared<PhysicsRobot>(physics_world, enemy_robot));
+    }
+    physics_world_timestamp = world.getMostRecentTimestamp();
+}
+
+World PhysicsWorld::getWorld() const
+{
+    World new_world;
+    new_world.updateTimestamp(physics_world_timestamp);
+    if (physics_ball)
+    {
+        new_world.mutableBall() =
+                physics_ball->getBallWithTimestamp(physics_world_timestamp);
+    }
+    if (physics_field)
+    {
+        new_world.mutableField() =
+                physics_field->getFieldWithTimestamp(physics_world_timestamp);
+    }
+
+    new_world.mutableFriendlyTeam().clearAllRobots();
+    std::vector<Robot> friendly_robots;
+    for (const auto& robot : friendly_physics_robots)
+    {
+        friendly_robots.emplace_back(
+                robot->getRobotWithTimestamp(physics_world_timestamp));
+    }
+    new_world.mutableFriendlyTeam().updateRobots(friendly_robots);
+
+    new_world.mutableEnemyTeam().clearAllRobots();
+    std::vector<Robot> enemy_robots;
+    for (const auto& robot : enemy_physics_robots)
+    {
+        enemy_robots.emplace_back(robot->getRobotWithTimestamp(physics_world_timestamp));
+    }
+    new_world.mutableEnemyTeam().updateRobots(enemy_robots);
+
+    return new_world;
+}
+
+void PhysicsWorld::step(const Duration& time_step)
+{
+    physics_world->Step(time_step.getSeconds(), velocity_iterations, position_iterations);
+    physics_world_timestamp = physics_world_timestamp + time_step;
+}
+
+
+std::vector<std::shared_ptr<PhysicsRobot>> PhysicsWorld::getFriendlyPhysicsRobots() const
+{
+    return friendly_physics_robots;
+}
+
+std::vector<std::shared_ptr<PhysicsRobot>> PhysicsWorld::getEnemyPhysicsRobots() const
+{
+    return enemy_physics_robots;
+}
+
+std::shared_ptr<PhysicsBall> PhysicsWorld::getPhysicsBall() const
+{
+    return physics_ball;
+}
+
+std::shared_ptr<PhysicsField> PhysicsWorld::getPhysicsField() const
+{
+    return physics_field;
+}
+
---
+++ src/software/backend/simulation/physics/physics_world.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <Box2D/Box2D.h>
+
+#include "software/world/world.h"
+#include "software/backend/simulation/physics/physics_field.h"
+#include "software/backend/simulation/physics/physics_ball.h"
+#include "software/backend/simulation/physics/physics_robot.h"
+
+/**
+ * This class represents a World in a Box2D physics simulation. It provides a convenient
+ * way for us to abstract and hold a lot of the world's contents. It's also used to
+ * convert to our own World class when needed.
+ */
+class PhysicsWorld
+{
+public:
+    /**
+     * Creates a new PhysicsWorld given a Box2D world.
+     * @param world
+     */
+    explicit PhysicsWorld(const World& world);
+
+    PhysicsWorld() = delete;
+
+    // Delete the copy and assignment operators because copying this class causes
+    // issues with the b2World and how it tracks bodies in the world, because as objects
+    // are copied and destroyed, they will destroy the bodies in the b2World as well
+    PhysicsWorld& operator=(const PhysicsWorld&) = delete;
+    PhysicsWorld(const PhysicsWorld&)            = delete;
+
+    /**
+     * Returns the current state of the world
+     * @return the current state of the world
+     */
+    World getWorld() const;
+
+    /**
+     * Advances the Box2D world physics by the given time step
+     * @param time_step how much to advance the world physics by
+     */
+    void step(const Duration& time_step);
+
+    /**
+     * Returns the friendly PhysicsRobots currently in the world
+     * @return the friendly PhysicsRobots in the world
+     */
+    std::vector<std::shared_ptr<PhysicsRobot>> getFriendlyPhysicsRobots() const;
+
+    /**
+     * Returns the enemy PhysicsRobots currently in the world
+     * @return the enemy PhysicsRobots in the world
+     */
+    std::vector<std::shared_ptr<PhysicsRobot>> getEnemyPhysicsRobots() const;
+
+    /**
+     * Returns the PhysicsBall currently in the world
+     * @return the PhysicsBall in the world
+     */
+    std::shared_ptr<PhysicsBall> getPhysicsBall() const;
+
+    /**
+     * Returns the PhysicsBall currently in the world
+     * @return the PhysicsBall in the world
+     */
+    std::shared_ptr<PhysicsField> getPhysicsField() const;
+
+private:
+    std::shared_ptr<b2World> physics_world;
+    // The timestamp of the simulated physics world
+    Timestamp physics_world_timestamp;
+
+    /**
+     * Sets the state of the internal world used for simulation
+     * @param world the new state of the world to start the simulation from
+     */
+    void initWorld(const World& world);
+
+    // 3 and 8 here are somewhat arbitrary values for the velocity and position
+    // iterations but are the recommended defaults from
+    // https://www.iforce2d.net/b2dtut/worlds
+    const unsigned int velocity_iterations = 8;
+    const unsigned int position_iterations = 3;
+
+    // Abstractions of objects in the world
+    std::shared_ptr<PhysicsBall> physics_ball;
+    std::shared_ptr<PhysicsField> physics_field;
+    std::vector<std::shared_ptr<PhysicsRobot>> friendly_physics_robots;
+    // Keep track of enemy physics robots as well, because although we don't
+    // necessarily control them with our firmware they can still be
+    // simulated to move given initial velocities
+    std::vector<std::shared_ptr<PhysicsRobot>> enemy_physics_robots;
+};
+
