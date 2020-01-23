637518cba06a0826f83d29ae6bfe48000d4a2d03
Gareth Ellis <gareth.ellis0@gmail.com>
2020-01-23 05:59:42 -0800

    Revised Formatting Scripts To Be Much Simpler

--- formatting_scripts/check_formatting_ci.sh
+++ formatting_scripts/check_formatting_ci.sh
@@ -1,41 +1,35 @@
+#!/bin/bash
+
 #
 # This script is used for running formatting checks in CI
 #
 
-CURR_DIR=$(dirname $0)
+# The directory this script is in
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "=========================================================="
 echo " THIS SHOULD ONLY BE RUN IN CI, PLEASE DO NOT USE LOCALLY"
 echo "=========================================================="
 
-# Determine what we should compare this branch against to figure out what
-# files were changed
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  # Not in a pull request, so compare against parent commit
-  BASE_COMMIT="HEAD^"
-  echo "Running formatting against parent commit $(git rev-parse $BASE_COMMIT)"
-  echo "=================================================="
-else
-  # In a pull request so compare against branch we're trying to merge into
-  # (ex. "master")
-  BASE_COMMIT="$TRAVIS_BRANCH"
-  # Make sure we pull the branches we're trying to merge against
-  git fetch origin $BASE_COMMIT:$BASE_COMMIT
-  echo "Running formatting against branch $BASE_COMMIT, with hash $(git rev-parse $BASE_COMMIT)"
-  echo "=================================================="
+# Run formatting
+$CURR_DIR/fix_formatting.sh
+if [[ "$?" != 0 ]]; then
+    echo "There was a problem running the formatting script, stopping now."
+    exit 1
 fi
 
-# Run formatting
-OUTPUT="$($CURR_DIR/fix_formatting.sh -b $BASE_COMMIT)"
-FORMATTING_RETURN_VAL=$?
-
-# Check if we changed any files (based on the return code of the last command)
-if [ $FORMATTING_RETURN_VAL -ne 0 ]; then
-    echo "$OUTPUT"
+# Check if there are any files changed
+GIT_CHANGES=$(git status --porcelain)
+if [[ $GIT_CHANGES ]]; then
+    # Changes
+    echo "FILES CHANGED:"
+    echo "$GIT_CHANGES"
     echo "========================================================================"
     echo "formatting check failed :( - please reformat your code via the \`fix_formatting.sh\` script and resubmit"
     exit 1
 else
+    # No changes
     echo "formatting passed, no files changed :D"
     exit 0
 fi
+
--- formatting_scripts/fix_formatting.sh
+++ formatting_scripts/fix_formatting.sh
@@ -1,150 +1,48 @@
 #!/bin/bash
 
-# This script allows us to automatically format the entire codebase using
-# clang-format, or some set of changes based on a diff via `git-clang-format`
-# It also allows us to format the bazel files
+#
+# This script is used for running formatting checks in CI
+#
 
-# Will set the `FILES_CHANGED` environment variable to 1 if we changed any
-# files, and 0 otherwise (assuming no error occured)
-
-# Will return:
-# `0` if no files formatted and no errors
-# `1` if an unexpected error occured
-# `3` if the script ran properly and we changed some files
-
-
-# The version of the clang executable to use
-export CLANG_VERSION=7.0
-
-# The current directory
+# The directory this script is in
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# The name of this script, used for help info
-SCRIPT_NAME="$0"
+# The root bazel directory
+BAZEL_ROOT_DIR="$CURR_DIR/../src"
 
-# Extensions to check formatting
-EXTENSIONS=(h cpp c hpp tpp)
+# Extensions to check formatting for clang-format
+CLANG_FORMAT_EXTENSIONS=(h cpp c hpp tpp)
 
-# Function to format all bazel files
-function format_bazel_files() {
-    cd $CURR_DIR/../src
-    bazel run //:buildifier
+# Function to run clang format
+function run_clang_format () {
+    echo "Running clang format over all files..."
+    cd $BAZEL_ROOT_DIR
+
+    # Generate extension string
+    # Formatted as -iname *.EXTENSION -o
+    EXTENSION_STRING=""
+    for value in "${CLANG_FORMAT_EXTENSIONS[@]}"
+    do
+        EXTENSION_STRING="$EXTENSION_STRING -iname *.$value -o"
+    done
+
+    # Find all the files that we want to format, and pass them to
+    # clang-format as arguments
+    # We remove the last -o flag from the extension string
+    find $CURR_DIR/../src/ ${EXTENSION_STRING::-2} \
+        | xargs bazel run @llvm_clang//:clang-format -- -i -style=file
+    shift
 }
 
-# Check that we have at least some arguments
-if (( $# == 0 )); then
-    echo "Missing arguments (\"$SCRIPT_NAME --help\" for help)"
-    exit 1
-fi
+# Function to run bazel formatting
+function run_bazel_formatting () {
+    echo "Running bazel buildifier to format all bazel files..."
+    cd $BAZEL_ROOT_DIR
+    bazel run @com_github_bazelbuild_buildtools//:buildifier
+}
 
-# This loop iterates through and handles the provided command-line arguments.
-# See: https://stackoverflow.com/questions/7069682/how-to-get-arguments-with-flags-in-bash-script/7069755?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-#
-# Arguments are handles one=by-one until there are none left. The 'shift'
-# command shifts all arguments each time one has been handled
-while test $# -gt 0; do
-    case "$1" in
-        -h|--help)
-            echo "$SCRIPT_NAME - Run clang-format and bazel buildifier on our codebase"
-            echo " "
-            echo "$SCRIPT_NAME [options]"
-            echo " "
-            echo "options:"
-            echo "-h, --help       show help"
-            echo "-a, --all        format all files in our codebase"
-            echo "-b, --branch     specify a git branch to diff against, and only format the files you have changed (NOTE: This argument will also accept a git commit hash)"
-            echo "NOTE: Both -a and -b will format all bazel buildfiles"
-            echo " "
-            echo "Examples:"
-            echo "$SCRIPT_NAME -a"
-            echo "$SCRIPT_NAME -b dev"
+# Run formatting
+run_clang_format
+run_bazel_formatting
 
-            exit 0
-            ;;
-        -a|--all)
-            echo "WARNING: "-a" will format all the files and can be very slow! Please consider using "-b" instead!"
-            echo "Are you sure you want to format all files (y/n)?"
-            read continueFormat
-
-            if [ "$continueFormat" == "n" ]; then
-                exit 0
-
-            elif [ "$continueFormat" == "y" ]; then
-                echo "Formatting all files..."
-
-                # Generate extension string
-                # Formatted as -iname *.EXTENSION -o
-                EXTENSION_STRING=""
-                for value in "${EXTENSIONS[@]}"
-                do
-                    EXTENSION_STRING="$EXTENSION_STRING -iname *.$value -o"
-                done
-    
-                # Find all the files that we want to format, and pass them to
-                # clang-format as arguments
-                # We remove the last -o flag from the extension string
-                find $CURR_DIR/../src/ ${EXTENSION_STRING::-2} \
-                    | xargs $CURR_DIR/clang-format-$CLANG_VERSION -i -style=file
-                shift
-            else 
-                echo "Error: input not recognized"
-                exit 1
-            fi
-            ;;
-
-            format_bazel_files()
-
-        -b|--branch)
-            # Shift the arguents to check for the argument to this flag
-            shift
-            # Make sure we only have 1 arguement for the branch name
-            if test $# -lt 1; then
-                echo "Error: No branch specified"
-                exit 1
-            elif test $# -gt 1; then
-                echo "Error: More than one branch specified"
-                exit 1
-            fi
-
-            # The name of the branch to diff against
-            BRANCH_NAME="$1"
-
-            # Generate extension string
-            # Formatted as EXTENSION,
-            EXTENSION_STRING=""
-            for value in "${EXTENSIONS[@]}"
-            do
-                EXTENSION_STRING="$EXTENSION_STRING$value,"
-            done
-
-            # Fix formatting on all changes between this branch and the target branch
-            # Remove the last comma from the extension string
-            OUTPUT="$($CURR_DIR/git-clang-format --extensions ${EXTENSION_STRING::-1} --binary $CURR_DIR/clang-format-$CLANG_VERSION --commit $BRANCH_NAME)"
-
-            # Check the results of clang-format
-            if [[ $OUTPUT == *"no modified files to format"* ]] || [[ $OUTPUT == *"clang-format did not modify any files"* ]] ; then
-                # No files were changed
-                echo "clang-format passed, no files changed :D"
-                exit 0
-            else
-                # We changed some files
-                # Output the results.
-                # We're using printf here so we can replace spaces with newlines to make the output readable
-                printf '%s\n' $OUTPUT
-                echo " "
-                echo "========================================================================"
-                echo "clang-format has modified the above files. Formatting complete."
-                exit 3
-            fi
-
-            shift
-            ;;
-
-            format_bazel_files()
-
-        *)
-            echo "Invalid arguments (\"$SCRIPT_NAME --help\" for help)"
-            exit 1
-            ;;
-    esac
-done
+exit 0
