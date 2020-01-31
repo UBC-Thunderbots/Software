5ad8cc3cd7fbed9d3f7a19b9c58ff882f1f8aaad
Jason Lee <jsn0716@gmail.com>
2020-01-30 23:08:45 -0800

    Add distance functions and write unit tests

--- src/software/new_geom/util/distance.h
+++ src/software/new_geom/util/distance.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <limits>
+
+#include "software/geom/util.h"
 #include "software/new_geom/line.h"
 #include "software/new_geom/point.h"
+#include "software/new_geom/polygon.h"
+#include "software/new_geom/segment.h"
 
 /**
  * Finds the shortest distance between a Line and a Point
@@ -12,3 +17,60 @@
  */
 double distance(const Line &first, const Point &second);
 double distance(const Point &first, const Line &second);
+
+/**
+ * Finds the shortest distance between two Points
+ *
+ * @param first Point
+ * @param second Point
+ * @return the shortest distance between first and second
+ */
+double distance(const Point &first, const Point &second);
+
+/**
+ * Finds the shortest distance between two Segments
+ *
+ * @param first Segment
+ * @param second Segment
+ * @return the shortest distance between first and second
+ */
+double distance(const Segment &first, const Segment &second);
+
+/**
+ * Finds the shortest distance between a Point and a Segment
+ *
+ * @param first the Point
+ * @param second the Segment
+ * @return the shortest distance between first and second
+ */
+double distance(const Point &first, const Segment &second);
+double distance(const Segment &first, const Point &second);
+
+/**
+ * Finds the shortest distance between a Point and a Polygon
+ *
+ * @param first the Point
+ * @param second the Polygon
+ * @return the shortest distance between first and second
+ */
+double distance(const Point &first, const Polygon &second);
+double distance(const Polygon &first, const Point &second);
+
+/**
+ * Finds the squared shortest distance between a Point and a Segment
+ *
+ * @param first the Point
+ * @param second the Segment
+ * @return the squared shortest distance between first and second
+ */
+double distanceSquared(const Point &first, const Segment &second);
+double distanceSquared(const Segment &first, const Point &second);
+
+/**
+ * Finds the squared shortest distance between two Points
+ *
+ * @param first Point
+ * @param second Point
+ * @return the squared shortest distance between first and second
+ */
+double distanceSquared(const Point &first, const Point &second);
