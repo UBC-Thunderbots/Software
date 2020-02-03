1948706112ada87ebe5ab50c83ce2d4421c1f951
Gareth Ellis <garethellis0@users.noreply.github.com>
2020-02-03 15:06:13 -0800

    Setup FreeRTOS and Enabled LWiP IGMP (#1152)
    * Almost got FreeRTOS Compiling
    
    * Added RTOS Support And LWiP IGMP Flags To frankie_v1
    
    * Misc. BUILD File Cleanup
    
    * Fixed Formatting

--- src/firmware_new/boards/frankie_v1/lwip.c
+++ src/firmware_new/boards/frankie_v1/lwip.c
@@ -33,9 +33,6 @@
 /* ETH Variables initialization ----------------------------------------------*/
 void Error_Handler(void);
 
-/* DHCP Variables initialization ---------------------------------------------*/
-uint32_t DHCPfineTimer   = 0;
-uint32_t DHCPcoarseTimer = 0;
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
@@ -55,16 +52,16 @@ ip4_addr_t gw;
  */
 void MX_LWIP_Init(void)
 {
-    /* Initilialize the LwIP stack without RTOS */
-    lwip_init();
+    /* Initilialize the LwIP stack with RTOS */
+    tcpip_init(NULL, NULL);
 
     /* IP addresses initialization with DHCP (IPv4) */
     ipaddr.addr  = 0;
     netmask.addr = 0;
     gw.addr      = 0;
 
-    /* add the network interface (IPv4/IPv6) without RTOS */
-    netif_add(&gnetif, &ipaddr, &netmask, &gw, NULL, &ethernetif_init, &ethernet_input);
+    /* add the network interface (IPv4/IPv6) with RTOS */
+    netif_add(&gnetif, &ipaddr, &netmask, &gw, NULL, &ethernetif_init, &tcpip_input);
 
     /* Registers the default network interface */
     netif_set_default(&gnetif);
@@ -94,32 +91,6 @@ void MX_LWIP_Init(void)
 /* USER CODE BEGIN 4 */
 /* USER CODE END 4 */
 #endif
-
-/**
- * ----------------------------------------------------------------------
- * Function given to help user to continue LwIP Initialization
- * Up to user to complete or change this function ...
- * Up to user to call this function in main.c in while (1) of main(void)
- *-----------------------------------------------------------------------
- * Read a received packet from the Ethernet buffers
- * Send it to the lwIP stack for handling
- * Handle timeouts if LWIP_TIMERS is set and without RTOS
- * Handle the llink status if LWIP_NETIF_LINK_CALLBACK is set and without RTOS
- */
-void MX_LWIP_Process(void)
-{
-    /* USER CODE BEGIN 4_1 */
-    /* USER CODE END 4_1 */
-    ethernetif_input(&gnetif);
-
-    /* USER CODE BEGIN 4_2 */
-    /* USER CODE END 4_2 */
-    /* Handle timeouts */
-    sys_check_timeouts();
-
-    /* USER CODE BEGIN 4_3 */
-    /* USER CODE END 4_3 */
-}
 
 #if defined(__CC_ARM) /* MDK ARM Compiler */
 /**
