25c838af8fa0a3de53a31c383fdf8951ed9a70b9
akhilveeraghanta <akhil.veeraghanta@outlook.com>
2020-02-12 19:49:07 -0800

    ran formatting

--- src/firmware_new/boards/frankie_v1/udp_multicast.c
+++ src/firmware_new/boards/frankie_v1/udp_multicast.c
@@ -1,29 +1,29 @@
-#include "main.h"
-#include "lwip/opt.h"
+#include "ai_communicator.h"
+#include "firmware_new/proto/control_fw.pb.h"
 #include "lwip/api.h"
-#include "lwip/sys.h"
-#include "lwip/pbuf.h"
-#include "lwip/tcp.h"
-#include "lwip/memp.h"
 #include "lwip/inet.h"
 #include "lwip/ip_addr.h"
+#include "lwip/memp.h"
+#include "lwip/opt.h"
+#include "lwip/pbuf.h"
+#include "lwip/sys.h"
+#include "lwip/tcp.h"
 #include "lwip/udp.h"
+#include "main.h"
 #include "pb.h"
 #include "pb_decode.h"
 #include "pb_encode.h"
-#include "ai_communicator.h"
-#include "firmware_new/proto/control_fw.pb.h"
 
 
 uint8_t buffer[robot_ack_size];
-int msg_count = 0;
-robot_ack ack = robot_ack_init_zero;
+int msg_count       = 0;
+robot_ack ack       = robot_ack_init_zero;
 control_msg control = control_msg_init_zero;
 
-struct netconn* udp_multicast_join_group(const char* multicast_ip, int multicast_port);
+struct netconn *udp_multicast_join_group(const char *multicast_ip, int multicast_port);
 static void udp_multicast_thread(void *arg);
 
-struct netconn* udp_multicast_join_group(const char* multicast_ip, int multicast_port)
+struct netconn *udp_multicast_join_group(const char *multicast_ip, int multicast_port)
 {
     // create a new UDP connection on the heap
     struct netconn *conn = netconn_new(NETCONN_UDP);
@@ -39,15 +39,15 @@ struct netconn* udp_multicast_join_group(const char* multicast_ip, int multicast
     // multicast group
     err_t test = netconn_bind(conn, &multicast_address, multicast_port);
 
-    err_t err = netconn_join_leave_group(
-            conn, &multicast_address,
-            IP_ADDR_ANY, NETCONN_JOIN);
+    err_t err =
+        netconn_join_leave_group(conn, &multicast_address, IP_ADDR_ANY, NETCONN_JOIN);
 
-    if(test == ERR_OK){
-
+    if (test == ERR_OK)
+    {
     }
 
-    if (err == ERR_OK) {
+    if (err == ERR_OK)
+    {
         return conn;
     }
 
@@ -56,47 +56,46 @@ struct netconn* udp_multicast_join_group(const char* multicast_ip, int multicast
 
 static void udp_multicast_thread(void *arg)
 {
-
     struct netconn *conn = (struct netconn *)arg;
     struct netbuf *buf, *tx_buf;
     err_t err;
 
-    while (1) {
-
+    while (1)
+    {
         err = netconn_recv(conn, &buf);
 
-        if (err == ERR_OK) {
-
+        if (err == ERR_OK)
+        {
             tx_buf = netbuf_new();
             netbuf_alloc(tx_buf, robot_ack_size);
 
             // Create a stream that reads from the buffer
-            pb_istream_t in_stream = pb_istream_from_buffer(
-                    (uint8_t * )buf->p->payload, buf->p->tot_len);
-                if (pb_decode(&in_stream, control_msg_fields, &control))
-                {
-                    // update proto
-                    ack.msg_count = msg_count++;
+            pb_istream_t in_stream =
+                pb_istream_from_buffer((uint8_t *)buf->p->payload, buf->p->tot_len);
+            if (pb_decode(&in_stream, control_msg_fields, &control))
+            {
+                // update proto
+                ack.msg_count = msg_count++;
 
-                    // serialize proto
-                    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-                    pb_encode(&stream, robot_ack_fields, &ack);
+                // serialize proto
+                pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+                pb_encode(&stream, robot_ack_fields, &ack);
 
-                    // package payload and send over udp
-                    tx_buf->p->payload = buffer;
-                    netconn_sendto(conn, tx_buf, (const ip_addr_t *)&(buf->addr), buf->port);
-                }
+                // package payload and send over udp
+                tx_buf->p->payload = buffer;
+                netconn_sendto(conn, tx_buf, (const ip_addr_t *)&(buf->addr), buf->port);
+            }
             netbuf_delete(tx_buf);
         }
         netbuf_delete(buf);
     }
 }
 
- void udp_multicast_init(const char* multicast_address,
-        int multicast_port)
+void udp_multicast_init(const char *multicast_address, int multicast_port)
 {
     // create a connection to the multicast group
     struct netconn *conn = udp_multicast_join_group(multicast_address, multicast_port);
 
-    sys_thread_new("multicast_thread",udp_multicast_thread, conn, 1024, (osPriority_t)osPriorityNormal);
+    sys_thread_new("multicast_thread", udp_multicast_thread, conn, 1024,
+                   (osPriority_t)osPriorityNormal);
 }
