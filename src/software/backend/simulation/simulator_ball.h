5f7cfef41ac2f3fd8181ac2d353c93d6330ff6be
Mathew <mmacd97@gmail.com>
2020-02-23 20:19:09 -0800

    Mathew/implement simulator robot (#1172)
    * Updated robot physics body, and added dribbler and chicker fixtures
    
    * Added force and impulse functions to ball
    
    * WIP of contact stuff
    
    * renamed simulator singleton files to match class names
    
    * huge WIP
    
    * added mass and test for force
    
    * simulator robot partially tested
    
    * simulator ball
    
    * updated simulator ball singleton
    
    * link physics ball with simulator ball
    
    * WIP kick testing
    
    * WIP
    
    * done PhysicsBall
    
    * WIP
    
    * things are starting to work
    
    * dribblign WIP. Need to try version with storing dribbling in simulator robot
    
    * wheels working need tests
    
    * WIP dribbler
    
    * physics wheel force done
    
    * dribbling basically working, just needs tuning
    
    * everything but motors done
    
    * physics robot finally passes tests yay
    
    * physics robot done
    
    * physics ball done
    
    * contact listener done
    
    * removed physics_simualtor, and added physics world tests
    
    * simulator ball, simulator ball singleton, and tests done
    
    * cleaned up simulator robot
    
    * simulator singletons done
    
    * possibly all done
    
    * misc fixes
    
    * fixed formatting
    
    * Update src/software/backend/simulation/simulator_robot.cpp
    
    Co-Authored-By: Gareth Ellis <garethellis0@users.noreply.github.com>
    
    * resolved most of Gareth's PR comments
    
    * Addressed Gareth PR comments
    * Added tests for directional kicking and chipping
    * Fixed the robot and ball destroyer structs
    * Misc comments and cleanup
    
    * fixed formatting
    
    * Added tests for physics object user data, and simulation contact listener
    
    * fixed formatting
    
    * fixed built and test
    
    * addressed Jared's PR comments
    
    * fixed formatting
    
    * Update src/software/backend/simulation/physics/simulation_contact_listener.h
    
    Co-Authored-By: Cody Li <licody2000@gmail.com>
    
    * Update src/software/backend/simulation/simulator_robot.cpp
    
    Co-Authored-By: Cody Li <licody2000@gmail.com>
    
    * Update src/software/backend/simulation/simulator_robot.cpp
    
    Co-Authored-By: Cody Li <licody2000@gmail.com>
    
    * Update src/software/backend/simulation/simulator_robot_singleton.cpp
    
    Co-Authored-By: Cody Li <licody2000@gmail.com>
    
    * Update src/software/backend/simulation/simulator_robot.cpp
    
    Co-Authored-By: Cody Li <licody2000@gmail.com>
    
    * Update src/software/backend/simulation/simulator_robot_test.cpp
    
    Co-Authored-By: Cody Li <licody2000@gmail.com>
    
    * Update src/software/backend/simulation/simulator_robot_singleton.cpp
    
    Co-Authored-By: Cody Li <licody2000@gmail.com>
    
    * Addressed Cody's PR comments
    
    * Addressed Jon's PR comments
    
    * Added isAutokickEnabled() functions to simualtor robot interface
    
    * fixed formatting
    
    Co-authored-by: Gareth Ellis <garethellis0@users.noreply.github.com>
    Co-authored-by: Cody Li <licody2000@gmail.com>

--- src/software/backend/simulation/simulator_ball.h
+++ src/software/backend/simulation/simulator_ball.h
@@ -1,90 +1,50 @@
 #pragma once
 
-extern "C"
-{
-#include "firmware/main/app/world/firmware_ball.h"
-}
+#include <memory>
+
 #include "software/backend/simulation/physics/physics_ball.h"
 
 /**
- * Because the FirmwareBall_t struct is defined in the .c file (rather than the .h file),
- * C++ considers it an incomplete type and is unable to use it with smart pointers
- * because it doesn't know the size of the object. Therefore we need to create our own
- * "Deleter" class we can provide to the smart pointers to handle that instead.
- *
- * See https://en.cppreference.com/w/cpp/memory/unique_ptr/unique_ptr for more info and
- * examples
+ * The SimulatorBall class acts as a wrapper for a PhysicsBall that deals with more
+ * logic-focused elements for simulation.
  */
-struct FirmwareBallDeleter
-{
-    void operator()(FirmwareBall_t* firmware_ball) const
-    {
-        app_firmware_ball_destroy(firmware_ball);
-    };
-};
-
-/**
- * This class acts as a wrapper around a PhysicsBall so that the PhysicsBall
- * can provide the interface of a FirmwareBall
- *
- * Because our firmware structs rely on C-style function pointers, we
- * cannot use C++ constructs like std::function to provide to the structs.
- * Therefore we use this class's static functions to provide the function
- * pointers, since static functions work as C-style function pointers
- * (because there is no instance associated with them).
- *
- * Whenever a caller needs to get a firmware struct or perform an operation, they
- * need to set which ball they want to control so that all the static functions
- * that have been provided to the firmware struct operate on the correct
- * instantiated object. This is our workaround to maintain and simulate multiple
- * "instances" of firmware at once.
- */
-class SimulatorBallSingleton
+class SimulatorBall
 {
    public:
     /**
-     * Sets the PhysicsBall being controlled by this class
+     * Create a new SimulatorBall given a PhysicsBall
      *
-     * @param ball The PhysicsBall to control by this class. Must not be null
+     * @param physics_ball the PhysicsBall to simulate and control
      */
-    static void setPhysicsBall(std::weak_ptr<PhysicsBall> ball);
+    explicit SimulatorBall(std::weak_ptr<PhysicsBall> physics_ball);
+    explicit SimulatorBall() = delete;
 
     /**
-     * Creates a FirmwareBall corresponding to the current PhysicsBall
+     * Returns the current position of the ball, in global field coordinates, in meters
      *
-     * @return a FirmwareBall corresponding to the current PhysicsBall
+     * @return the current position of the ball, in global field coordinates, in meters
      */
-    static std::unique_ptr<FirmwareBall_t, FirmwareBallDeleter> createFirmwareBall();
+    Point position() const;
+
+    /**
+     * Returns the current velocity of the ball, in global field coordinates, in m/s
+     *
+     * @return the current velocity of the ball, in global field coordinates, in m/s
+     */
+    Vector velocity() const;
 
    private:
     /**
-     * Returns the x-position of the current physics ball
+     * Helper functions that check if the current pointer to the physics_ball is valid
+     * before calling the given function. If the physics_ball is invalid, a warning is
+     * logged and a default value is returned;
      *
-     * @return the x-position of the current physics ball
+     * @param func The function to perform on the physics ball
      */
-    static float getBallPositionX();
+    Point checkValidAndReturnPoint(
+        std::function<Point(const std::shared_ptr<PhysicsBall>)> func) const;
+    Vector checkValidAndReturnVector(
+        std::function<Vector(const std::shared_ptr<PhysicsBall>)> func) const;
 
-    /**
-     * Returns the y-position of the current physics ball
-     *
-     * @return the y-position of the current physics ball
-     */
-    static float getBallPositionY();
-
-    /**
-     * Returns the x-velocity of the current physics ball
-     *
-     * @return the x-velocity of the current physics ball
-     */
-    static float getBallVelocityX();
-
-    /**
-     * Returns the y-velocity of the current physics ball
-     *
-     * @return the y-velocity of the current physics ball
-     */
-    static float getBallVelocityY();
-
-    // The physics ball being controlled by this class
-    static std::weak_ptr<PhysicsBall> physics_ball_weak_ptr;
+    std::weak_ptr<PhysicsBall> physics_ball;
 };
