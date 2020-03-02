58cdde898835e42eedbcb066582062570cb7851e
kelvinkoon <kkoon@shaw.ca>
2020-03-01 19:29:47 -0800

    Attempt to make wrapper change to robot_behind_ball_chipping_towards_positive_x_positive_y

--- src/software/ai/hl/stp/tactic/chip_tactic_test.cpp
+++ src/software/ai/hl/stp/tactic/chip_tactic_test.cpp
@@ -5,6 +5,12 @@
 #include "software/ai/hl/stp/action/chip_action.h"
 #include "software/test_util/test_util.h"
 
+bool compareChipActions(ChipAction *chip_action, ChipAction *expected_chip_action) {
+    EXPECT_EQ(expected_chip_action->getChipOrigin(), chip_action->getChipOrigin());
+    EXPECT_EQ(expected_chip_action->getChipDirection(), chip_action->getChipDirection());
+    EXPECT_EQ(expected_chip_action->getChipDistanceMeters(), chip_action->getChipDistanceMeters());
+}
+
 TEST(ChipTacticTest, getChipDirection)
 {
     World world = ::Test::TestUtil::createBlankTestingWorld();
@@ -85,18 +91,30 @@ TEST(ChipTacticTest, robot_behind_ball_chipping_towards_positive_x_positive_y)
 
     ChipTactic tactic = ChipTactic(world.ball(), true);
 
+    Ball ball({0, 0}, robot.position().toVector(), Timestamp::fromSeconds(0));
+
     tactic.updateRobot(robot);
     tactic.updateControlParams(Point(0, 0), Point(1, 1), 2.0);
     auto action_ptr = tactic.getNextAction();
 
     // Check an action was returned (the pointer is not null)
-    EXPECT_TRUE(action_ptr);
+    ASSERT_TRUE(action_ptr);
 
     auto chip_action = std::dynamic_pointer_cast<ChipAction>(action_ptr);
+
+    ChipAction expected_chip_action = ChipAction();
+
+    expected_chip_action.updateWorldParams(ball);
+    expected_chip_action.updateControlParams(robot, ball.position(), Angle::fromDegrees(45.0), 2.0);
+
+    // TODO implement compareChipActions
+    // TODO cast action_ptr to ChipAction type
+
     ASSERT_NE(chip_action, nullptr);
-    EXPECT_EQ(Point(0, 0), chip_action->getChipOrigin());
-    EXPECT_EQ(Angle::fromDegrees(45.0), chip_action->getChipDirection());
-    EXPECT_EQ(2.0, chip_action->getChipDistanceMeters());
+//    EXPECT_EQ(Point(0, 0), chip_action->getChipOrigin());
+//    EXPECT_EQ(Angle::fromDegrees(45.0), chip_action->getChipDirection());
+//    EXPECT_EQ(2.0, chip_action->getChipDistanceMeters());
+    EXPECT_TRUE(compareChipActions((ChipAction) &action_ptr, &expected_chip_action));
 }
 
 TEST(ChipTacticTest, robot_behind_ball_chipping_towards_negative_x_positive_y)
