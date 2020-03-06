86498cd2c9725eca89080d002c790736e1332c11
Gareth Ellis <garethellis0@users.noreply.github.com>
2020-03-06 07:42:36 -0800

    Switch From Clang To GCC (#1184)
    * add gcc targets
    
    * some fixes
    
    * fixed more issues, will now build non-linking targets
    
    * fix linker issue
    
    * fix constexpre problems
    
    * fix lm linking
    
    * use stlib flags
    
    * add no-as-needed to fix linking issue
    
    * replace magic enum with macros
    
    * Make tests pass
    
    * Remove clang
    
    * revert coverage change
    
    * fix formatting in refbox_data enum
    
    * remove misc comments
    
    * Clean up coverage files
    
    * rename enum string macro to prinable enum macro
    
    * re-enable pass generator tests
    
    * Added Check To Make Sure Shot Optional Is Not std::nullopt
    
    * rename k8_gcc to linux_gcc
    
    * Fix formatting script to work without llvm-clang
    
    * clean up
    
    * Name 7.4.0 specific files and globs more clearly
    
    * Rename wrappers to linux
    
    * clean up more files and add gcc-7 to setup_software script
    
    * Add comment for the no-as-needed flag
    
    * Update .travis.yml
    
    Co-Authored-By: Gareth Ellis <garethellis0@users.noreply.github.com>
    
    * Fix CI
    
    * fix coverage
    
    * fix buildifier
    
    * fix ci
    
    * fix ci
    
    * fix ci to only build software
    
    * add gcov wrapper
    
    * remove extraneous g++
    
    * Fixes To Allow Compilation
    
    * Fixed GCov Wrapper Script To Properly Forward Input
    
    * Simplified GCC Executable Access By Removing Wrapper Scripts
    
    * fixed segfaults in physics tests due to non-explicit order of destruction
    
    * fixed backends not being registered in factory
    
    * .travis.yml Cleanup
    
    * Specified c11 Standard In cc_toolchain_config.bzl
    
    * Misc. Firmware Fixes
    
    * Properly Added Firmware Builds To Travis
    
    * Collected Code Coverage Results Before Building Firmware
    
    * Changed linux_gcc_7_4_0 To linux_gcc Everywhere
    
    * Fixed Script cmd Escaping
    
    * Removed ifdef In shared/physics.h - Just Using C++ Compatible Version Now:
    
    * Removed Old Comment from shared/physics.h
    
    * moved linear algebra used in firmware physics to separate file, and removed unused functions
    
    * fixed formatting
    
    Co-authored-by: Jonathan Lew <jonathanlew@github.com>
    Co-authored-by: jonathanlew <31377876+jonathanlew@users.noreply.github.com>
    Co-authored-by: Mathew MacDougall <mmacd97@gmail.com>

--- src/firmware/main/io/wheels.h
+++ src/firmware/main/io/wheels.h
@@ -50,6 +50,13 @@ float wheels_get_front_right_rpm();
 float wheels_get_back_left_rpm();
 float wheels_get_back_right_rpm();
 
+// Apply wheel force to specific wheels
+// TODO: jdoc with direction
+void apply_wheel_force_front_right(float force_in_newtons);
+void apply_wheel_force_front_left(float force_in_newtons);
+void apply_wheel_force_back_right(float force_in_newtons);
+void apply_wheel_force_back_left(float force_in_newtons);
+
 /**
  * Coast each wheel
  */
