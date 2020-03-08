20a63a6579ea9a9c253fcb10ce326ee6aa3ae553
evanmorcom <evans.morcom@alumni.ubc.ca>
2020-03-07 20:33:49 -0800

    Added 2 unit tests

--- src/firmware/main/app/control/trajectory_planner_test.cpp
+++ src/firmware/main/app/control/trajectory_planner_test.cpp
@@ -11,12 +11,14 @@ extern "C"
 #include "firmware/main/math/polynomial_2d.h"
 #include "firmware/main/math/vector_2d.h"
 #include "trajectory_planner.h"
+#include "math.h"
 
-TEST(TrajectoryPlannerTest, generate_constant_arc_length_segments)
+TEST(TrajectoryPlannerTest, path_length_and_start_point_end_point_test)
 {
     Polynomial2dOrder3_t path = {
         .x = {.coefficients = {0, 0, 1, 0}},
         .y = {.coefficients = {0, 0, 1, 0}},
+
     };
 
     const double t_start = 0;
@@ -29,12 +31,6 @@ TEST(TrajectoryPlannerTest, generate_constant_arc_length_segments)
 
     TrajectoryElement_t* trajectory =
         generate_constant_arc_length_segmentation(path, t_start, t_end, num_segments, max_acc, max_speed, initial_speed, final_speed);
-
-
-    // Test by checking the length between points
-    // and the length of the total path
-    
-    //double segment_length_sum = 0;
     
     // Create the parmeterization to contain the desired number of segments
     CREATE_STATIC_ARC_LENGTH_PARAMETRIZATION(arc_length_param, __TRAJECTORY_PLANNER_MAX_NUM_SEGMENTS__);
@@ -47,16 +43,68 @@ TEST(TrajectoryPlannerTest, generate_constant_arc_length_segments)
     const double arc_segment_length =
         arc_length_param.arc_length_values[arc_length_param.num_values-1];
 
-    std::cout << "ARC LENGTH ->>>: " << arc_segment_length << std::endl;
-
-
     double segment_length_sum = 0;
 
+    // Test by checking the length between points
+    // and the length of the total path
     for (uint i = 0; i < num_segments-1; i++)
     {
         double length = sqrt( pow(trajectory[i].position.x - trajectory[i+1].position.x,2) + pow(trajectory[i].position.x - trajectory[i+1].position.x,2));
-        //if( fabs(length -)
         segment_length_sum += length;
     }
-    std::cout << "Numerical segment length: " << segment_length_sum << std::endl;
+    
+    EXPECT_NEAR(segment_length_sum, arc_segment_length, 0.01);
+    EXPECT_NEAR(trajectory[num_segments-1].position.x, shared_polynomial2d_getValueOrder3(path, t_end).x, 0.01 );
+    EXPECT_NEAR(trajectory[num_segments-1].position.x, shared_polynomial2d_getValueOrder3(path, t_end).y, 0.01 );
+
+    EXPECT_NEAR(trajectory[0].position.x, shared_polynomial2d_getValueOrder3(path, t_start).x, 0.01 );
+    EXPECT_NEAR(trajectory[0].position.x, shared_polynomial2d_getValueOrder3(path, t_start).y, 0.01 );
+}
+
+TEST(TrajectoryPlannerTest, path_length_and_start_point_end_point_test_reverse_direction)
+{
+    Polynomial2dOrder3_t path = {
+        .x = {.coefficients = {0, 0, 1, 0}},
+        .y = {.coefficients = {0, 0, 1, 0}},
+
+    };
+
+    const double t_start = 1;
+    const double t_end = 0;
+    const unsigned int num_segments = 5999;
+    const double max_acc = 3;
+    const double max_speed = 3;
+    const double initial_speed = 0;
+    const double final_speed = 0;
+
+    TrajectoryElement_t* trajectory =
+        generate_constant_arc_length_segmentation(path, t_start, t_end, num_segments, max_acc, max_speed, initial_speed, final_speed);
+    
+    // Create the parmeterization to contain the desired number of segments
+    CREATE_STATIC_ARC_LENGTH_PARAMETRIZATION(arc_length_param, __TRAJECTORY_PLANNER_MAX_NUM_SEGMENTS__);
+
+    // Get all of the points for the arc length parameterization (Not constant arc length
+    // segments)
+    shared_polynomial_getArcLengthParametrizationOrder3(path, t_start, t_end,
+                                                        arc_length_param);
+
+    const double arc_segment_length =
+        arc_length_param.arc_length_values[arc_length_param.num_values-1];
+
+    double segment_length_sum = 0;
+
+    // Test by checking the length between points
+    // and the length of the total path
+    for (uint i = 0; i < num_segments-1; i++)
+    {
+        double length = sqrt( pow(trajectory[i].position.x - trajectory[i+1].position.x,2) + pow(trajectory[i].position.x - trajectory[i+1].position.x,2));
+        segment_length_sum += length;
+    }
+    
+    EXPECT_NEAR(segment_length_sum, arc_segment_length, 0.01);
+    EXPECT_NEAR(trajectory[num_segments-1].position.x, shared_polynomial2d_getValueOrder3(path, t_start).x, 0.01 );
+    EXPECT_NEAR(trajectory[num_segments-1].position.x, shared_polynomial2d_getValueOrder3(path, t_start).y, 0.01 );
+
+    EXPECT_NEAR(trajectory[0].position.x, shared_polynomial2d_getValueOrder3(path, t_end).x, 0.01 );
+    EXPECT_NEAR(trajectory[0].position.x, shared_polynomial2d_getValueOrder3(path, t_end).y, 0.01 );
 }
