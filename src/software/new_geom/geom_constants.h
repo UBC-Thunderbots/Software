072c733804eb9f0a49e4eeb3efa2f2fdb91ae99c
Jason Lee <jsn0716@gmail.com>
2020-03-15 11:42:59 -0700

    fix

--- src/software/new_geom/geom_constants.h
+++ src/software/new_geom/geom_constants.h
@@ -6,14 +6,14 @@ namespace GeomConstants
     // does not make sense. Instead, we have to pick some fixed epsilon to compare doubles
     // to zero. We pick 1e-15, which is approximately 10 ULPs near 1.0, and should fit the
     // needs of operations near such scale. This is not meant to cover all usage case, and
-    // other epsilon values may need to be chosen for specific use cases. Further reading
-    // on floating-point numbers and their comparisons:
-    // https://bitbashing.io/comparing-floats.html
+    // other epsilon values may need to be chosen for specific use cases.
+    // Further reading: https://bitbashing.io/comparing-floats.html
     static constexpr double FIXED_EPSILON_ONE = 1e-15;
 
     // A single double arithmetic operation with accurate inputs can have a maximum error
     // of 0.5 ULP. These errors accumulate with successive operations, and the appropriate
     // max ULPs value should be chosen based on the specific use case.
+    // Further reading: https://bitbashing.io/comparing-floats.html
     static constexpr int MAX_ULPS_TEN     = 10;
     static constexpr int MAX_ULPS_HUNDRED = 100;
 }  // namespace GeomConstants
