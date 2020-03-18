0fe54ac8a0d9f1558360a7a42094f4e6a4acdf87
Gareth Ellis <gareth.ellis0@gmail.com>
2020-03-18 15:54:26 -0700

    Changed pwm_pin To Automatically Determine The Max Pulse

--- src/firmware_new/boards/frankie_v1/io/pwm_pin.c
+++ src/firmware_new/boards/frankie_v1/io/pwm_pin.c
@@ -28,7 +28,8 @@ void io_pwm_pin_setPwm(PwmPin_t* pwm_pin, float pwm_value)
     // TODO: assert pwm value in [0,1]
     // TODO: not the appropriate way to get the max value of a uint16_t
     // Rescale PWM
-    uint16_t scaled_pwm = (uint16_t)round(pwm_value * 44);
+    uint32_t pulse_max = pwm_pin->timer.Init.Period;
+    uint16_t scaled_pwm = (uint16_t)round(pwm_value * pulse_max);
 
     // TODO: better name for this
     TIM_OC_InitTypeDef timer_config;
