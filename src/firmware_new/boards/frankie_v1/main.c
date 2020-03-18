d23408c6d407c1f42e22cb86471f9075f9133cde
Gareth Ellis <gareth.ellis0@gmail.com>
2020-03-18 16:09:01 -0700

    Changed Prescaler 96->9 And Counter 45->400

--- src/firmware_new/boards/frankie_v1/main.c
+++ src/firmware_new/boards/frankie_v1/main.c
@@ -311,9 +311,9 @@ static void MX_TIM4_Init(void)
 
     /* USER CODE END TIM4_Init 1 */
     htim4.Instance               = TIM4;
-    htim4.Init.Prescaler         = 96 - 1;
+    htim4.Init.Prescaler         = 9 - 1;
     htim4.Init.CounterMode       = TIM_COUNTERMODE_UP;
-    htim4.Init.Period            = 45 - 1;
+    htim4.Init.Period            = 400 - 1;
     htim4.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;
     htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
     if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
