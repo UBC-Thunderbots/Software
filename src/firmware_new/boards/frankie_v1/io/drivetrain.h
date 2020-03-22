6eaeef365ed292650a724567ffae2306e45f940f
Gareth Ellis <gareth.ellis0@gmail.com>
2020-03-21 19:33:49 -0700

    Created Rough Drivetrain Abstraction

---
+++ src/firmware_new/boards/frankie_v1/io/drivetrain.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "firmware_new/boards/frankie_v1/io/drivetrain_unit.h"
+
+// TODO: jdoc here
+static void io_drivetrain_init(DriveTrainUnit_t* front_left_drive_unit,
+                               DriveTrainUnit_t* front_right_drive_unit,
+                               DriveTrainUnit_t* back_left_drive_unit,
+                               DriveTrainUnit_t* back_right_drive_unit);
+
+// TODO: jdoc here
+static void io_drivetrain_applyForceFrontLeftWheel(float force_newtons);
+// TODO: jdoc here
+static void io_drivetrain_applyForceFrontRightWheel(float force_newtons);
+// TODO: jdoc here
+static void io_drivetrain_applyForceBackLeftWheel(float force_newtons);
+// TODO: jdoc here
+static void io_drivetrain_applyForceBackRightWheel(float force_newtons);
---
+++ src/firmware_new/boards/frankie_v1/io/drivetrain_unit.c
@@ -0,0 +1,40 @@
+#include "firmware_new/boards/frankie_v1/io/drivetrain_unit.h"
+
+#include <stdlib.h>
+
+typedef struct DrivetrainUnit
+{
+    AllegroA3931MotorDriver_t* motor_driver;
+} DriveTrainUnit_t;
+
+DriveTrainUnit_t* io_drivetrain_unit_create(AllegroA3931MotorDriver_t* motor_driver)
+{
+    DriveTrainUnit_t* drivetrain_unit =
+        (DriveTrainUnit_t*)malloc(sizeof(DriveTrainUnit_t));
+
+    drivetrain_unit->motor_driver = motor_driver;
+
+    return drivetrain_unit;
+}
+
+void io_drivetrain_unit_applyForce(DriveTrainUnit_t* drive_train_unit,
+                                   float force_newtons)
+{
+    // TODO: proper implementation for this
+
+    float pwm_percentage = min(1.0f, abs(force_newtons) / 255.0f);
+
+    if (force_newtons > 0)
+    {
+        io_allegro_a3931_motor_driver_setDirection(drive_train_unit->motor_driver,
+                                                   CLOCKWISE);
+    }
+    else
+    {
+        io_allegro_a3931_motor_driver_setDirection(drive_train_unit->motor_driver,
+                                                   COUNTERCLOCKWISE);
+    }
+
+    io_allegro_a3931_motor_setPwmPercentage(drive_train_unit->motor_driver,
+                                            pwm_percentage);
+}
