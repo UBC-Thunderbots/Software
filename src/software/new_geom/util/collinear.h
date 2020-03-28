62449af66ecdc1409bd4a219ab1942d1263de7ca
Jason Lee <34253612+jsn0716@users.noreply.github.com>
2020-03-28 11:19:48 -0700

    Fix `collinear` and `segment::contains` (#1235)
    * Fix
    
    * Fix
    
    * Format
    
    * WIP
    
    * WIP
    
    * Fix
    
    * Format
    
    * Add a separate test case for collinear
    
    * Add epsilons as arguments to geom util functions with default arguments

--- src/software/new_geom/util/collinear.h
+++ src/software/new_geom/util/collinear.h
@@ -1,10 +1,17 @@
+#pragma once
+
+#include "software/new_geom/geom_constants.h"
 #include "software/new_geom/point.h"
 
 /**
  * Returns true if the given points are collinear, false otherwise.
  *
  * @params a, b, c the given points
+ * @param fixed_epsilon the epsilon value for near zero double comparisons
+ * @param ulps_epsilon the epsilon value for double comparisons based on ULPs distance
  *
  * @return true if the given points are collinear, false otherwise
  */
-bool collinear(const Point &a, const Point &b, const Point &c);
+bool collinear(const Point &a, const Point &b, const Point &c,
+               double fixed_epsilon = GeomConstants::FIXED_EPSILON,
+               int ulps_epsilon     = GeomConstants::ULPS_EPSILON_TEN);
