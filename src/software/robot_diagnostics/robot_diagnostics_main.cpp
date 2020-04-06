ba1393315e0a2fc24aa8f6c3b48f3e1e2682e4ae
jonathanlew <31377876+jonathanlew@users.noreply.github.com>
2020-04-06 16:27:07 -0700

    Robot Diagnostics GUI (#1227)
    * Remove all Drive modes except Per-motor and Matrix, update feedback units, Add LEDs mode
    
    * Fix build to be consistent with the visualizer
    
    * Implemented slider behaviour with precision
    
    * Chicker Modes
    
    * Introduce callbacks for when slider values change
    
    * Clean up UI elements
    
    * Use layouts
    
    * Use MAX_ROBOTS_OVER_RADIO constant
    
    * Rearrange chicker buttons
    
    * Stop and reset

---
+++ src/software/robot_diagnostics/robot_diagnostics_main.cpp
@@ -0,0 +1,10 @@
+#include "software/robot_diagnostics/widgets/main_widget.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWidget w;
+    w.show();
+
+    return a.exec();
+}
---
+++ src/software/robot_diagnostics/widgets/slider.cpp
@@ -0,0 +1,52 @@
+#include "software/robot_diagnostics/widgets/slider.h"
+
+void setupSliderLineEdit(QLineEdit *line_edit, QSlider *slider,
+                         std::function<void(double)> value_changed_callback, double min,
+                         double max, double slider_step_size)
+{
+    slider->setMinimum(min * slider_step_size);
+    slider->setMaximum(max * slider_step_size);
+    slider->setValue(0);
+    line_edit->setText(QString::number(0));
+
+    auto on_slider_changed = [line_edit, slider_step_size,
+                              value_changed_callback](const int slider_value) {
+        double value = slider_value / slider_step_size;
+        line_edit->setText(QString::number(value));
+        value_changed_callback(value);
+    };
+
+    auto on_line_edit_changed = [line_edit, slider, min, max, slider_step_size,
+                                 value_changed_callback](const QString &text) {
+        bool ok;
+        double line_edit_input = text.toDouble(&ok);
+        double value;
+
+        // handle corner case of empty input and minus sign
+        if ((text.isEmpty()) || (text == "-"))
+        {
+            slider->setValue(0);
+            value = 0;
+        }
+        else if (ok)
+        {
+            value = std::clamp(line_edit_input, min, max);
+            if (value != line_edit_input)
+            {
+                line_edit->setText(QString::number(value));
+            }
+            slider->setValue(value * slider_step_size);
+        }
+        else
+        {
+            // line_edit input is invalid so get value from slider
+            double value = slider->value() / slider_step_size;
+            line_edit->setText(QString::number(value));
+        }
+        value_changed_callback(value);
+    };
+
+    QWidget::connect(slider, &QSlider::valueChanged, on_slider_changed);
+
+    QWidget::connect(line_edit, &QLineEdit::textChanged, on_line_edit_changed);
+}
