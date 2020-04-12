eea5f4a25d86305403d294aa593be37f6c3171ae
Mathew MacDougall <mmacd97@gmail.com>
2020-04-11 23:54:13 -0700

    fixed formatting

--- src/firmware_new/tools/communication/robot_communicator.h
+++ src/firmware_new/tools/communication/robot_communicator.h
@@ -3,8 +3,8 @@
 #include "boost/asio.hpp"
 #include "boost/bind.hpp"
 #include "firmware_new/tools/communication/transfer_media/transfer_medium.h"
-#include "software/logger/logger.h"
 #include "google/protobuf/message.h"
+#include "software/logger/logger.h"
 #include "software/multithreading/thread_safe_buffer.h"
 
 using boost::asio::ip::udp;
