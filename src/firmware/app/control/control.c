cbb74cbde20840851ead1152db86bff9778e28c4
kelvinkoon <kkoon@shaw.ca>
2020-04-15 21:26:20 -0700

    Fix formatting and add global to local

--- src/firmware/app/control/control.c
+++ src/firmware/app/control/control.c
@@ -165,14 +165,24 @@ void app_control_trackVelocityInLocalFrame(FirmwareRobot_t* robot,
     float current_vx               = app_firmware_robot_getVelocityX(robot);
     float current_vy               = app_firmware_robot_getVelocityY(robot);
     float current_angular_velocity = app_firmware_robot_getVelocityAngular(robot);
+    float current_orientation      = app_firmware_robot_getOrientation(robot);
+
+    // Translate from global to local frame
+    float current_velocity[2];
+    current_velocity[0] = current_vx;
+    current_velocity[1] = current_vy;
+
+    rotate(current_velocity, current_orientation);
 
     // This is the "P" term in a PID controller. We essentially do proportional
     // control of our acceleration based on velocity error
     static const float VELOCITY_ERROR_GAIN = 10.0f;
 
     float desired_acceleration[2];
-    desired_acceleration[0] = (linear_velocity_x - current_vx) * VELOCITY_ERROR_GAIN;
-    desired_acceleration[1] = (linear_velocity_y - current_vy) * VELOCITY_ERROR_GAIN;
+    desired_acceleration[0] =
+        (linear_velocity_x - current_velocity[0]) * VELOCITY_ERROR_GAIN;
+    desired_acceleration[1] =
+        (linear_velocity_y - current_velocity[1]) * VELOCITY_ERROR_GAIN;
 
     float angular_acceleration =
         (angular_velocity - current_angular_velocity) * VELOCITY_ERROR_GAIN;
