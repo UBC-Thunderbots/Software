b3f3d44a60e096146f6d99c8d60d47582a118c2b
Mathew MacDougall <mmacd97@gmail.com>
2020-04-20 17:43:23 -0700

    added unit tests

---
+++ src/software/util/printable_enum_macro/printable_enum_macro_test.cpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include "software/util/printable_enum_macro/printable_enum_macro.h"
+
+TEST(PrintableEnumMacroTest, validate_valid_enum_args_without_trailing_comma) {
+    std::string enum_string = "foo, bar, baz";
+    bool result = isEnumArgsValid(enum_string);
+    EXPECT_TRUE(result);
+}
+
+TEST(PrintableEnumMacroTest, validate_valid_enum_args_with_trailing_comma) {
+    std::string enum_string = "foo, bar, baz,";
+    bool result = isEnumArgsValid(enum_string);
+    EXPECT_TRUE(result);
+}
+
+TEST(PrintableEnumMacroTest, validate_invalid_enum_args) {
+    std::string enum_string = "foo, bar=3, baz,";
+    bool result = isEnumArgsValid(enum_string);
+    EXPECT_FALSE(result);
+}
+
+TEST(PrintableEnumMacroTest, separate_enum_strings_empty_string) {
+    std::string enum_string = "";
+    auto result = separateEnumStrings(enum_string);
+    EXPECT_TRUE(result.empty());
+}
+
+TEST(PrintableEnumMacroTest, separate_enum_strings_without_trailing_comma) {
+    std::string enum_string = "foo, bar, baz";
+    auto result = separateEnumStrings(enum_string);
+    std::vector<std::string> expected_result = {"foo", "bar", "baz"};
+    EXPECT_EQ(expected_result, result);
+}
+
+TEST(PrintableEnumMacroTest, separate_enum_strings_with_trailing_comma) {
+    std::string enum_string = "foo, bar, baz,";
+    auto result = separateEnumStrings(enum_string);
+    std::vector<std::string> expected_result = {"foo", "bar", "baz"};
+    EXPECT_EQ(expected_result, result);
+}
