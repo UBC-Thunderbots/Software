a6331a498249f3a2ceb879389d46ab93b2d21084
Mathew <mmacd97@gmail.com>
2020-04-23 12:43:32 -0700

    MAKE_ENUM macro upgrades (#1295)

--- src/software/ai/hl/stp/stp.cpp
+++ src/software/ai/hl/stp/stp.cpp
@@ -300,7 +300,7 @@ std::optional<std::string> STP::getCurrentPlayName() const
 PlayInfo STP::getPlayInfo()
 {
     PlayInfo info;
-    std::string info_play_type = to_string(current_game_state);
+    std::string info_play_type = toString(current_game_state);
     std::string info_play_name = getCurrentPlayName() ? *getCurrentPlayName() : "No Play";
     std::unordered_set<std::string> info_robot_tactic_assignment = {};
     info = PlayInfo(info_play_type, info_play_name, info_robot_tactic_assignment);
---
+++ src/software/util/make_enum/make_enum_test.cpp
@@ -0,0 +1,63 @@
+#include "software/util/make_enum/make_enum.h"
+
+#include <gtest/gtest.h>
+
+TEST(EnumMacroTest, validate_valid_enum_args_without_trailing_comma)
+{
+    std::string enum_string = "foo, bar, baz";
+    bool result             = isEnumArgsValid(enum_string);
+    EXPECT_TRUE(result);
+}
+
+TEST(EnumMacroTest, validate_valid_enum_args_with_trailing_comma)
+{
+    std::string enum_string = "foo, bar, baz,";
+    bool result             = isEnumArgsValid(enum_string);
+    EXPECT_TRUE(result);
+}
+
+TEST(EnumMacroTest, validate_invalid_enum_args)
+{
+    std::string enum_string = "foo, bar=3, baz,";
+    bool result             = isEnumArgsValid(enum_string);
+    EXPECT_FALSE(result);
+}
+
+TEST(EnumMacroTest, separate_enum_strings_empty_string)
+{
+    std::string enum_string = "";
+    auto result             = separateEnumStrings(enum_string);
+    EXPECT_TRUE(result.empty());
+}
+
+TEST(EnumMacroTest, separate_enum_strings_without_trailing_comma)
+{
+    std::string enum_string                  = "foo, bar, baz";
+    auto result                              = separateEnumStrings(enum_string);
+    std::vector<std::string> expected_result = {"foo", "bar", "baz"};
+    EXPECT_EQ(expected_result, result);
+}
+
+TEST(EnumMacroTest, separate_enum_strings_with_extra_spaces_tabs_and_newlines)
+{
+    std::string enum_string                  = "foo   \n, \tbar, \nbaz \t ";
+    auto result                              = separateEnumStrings(enum_string);
+    std::vector<std::string> expected_result = {"foo", "bar", "baz"};
+    EXPECT_EQ(expected_result, result);
+}
+
+TEST(EnumMacroTest, separate_enum_strings_with_trailing_comma)
+{
+    std::string enum_string                  = "foo, bar, baz,";
+    auto result                              = separateEnumStrings(enum_string);
+    std::vector<std::string> expected_result = {"foo", "bar", "baz"};
+    EXPECT_EQ(expected_result, result);
+}
+
+MAKE_ENUM(TestEnum, Var1, Var2)
+TEST(EnumMacroTest, test_macro_generated_functions)
+{
+    EXPECT_EQ(2, sizeTestEnum());
+    EXPECT_EQ(std::vector<TestEnum>({TestEnum::Var1, TestEnum::Var2}),
+              allValuesTestEnum());
+}
