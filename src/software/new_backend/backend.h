0352d746d38d246fc456a77b1fade41664a57a00
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2020-05-16 16:51:01 -0700

    new backend

---
+++ src/software/new_backend/backend.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "software/multithreading/subject.h"
+#include "software/multithreading/threaded_observer.h"
+#include "software/primitive/primitive.h"
+#include "software/world/world.h"
+
+/**
+ * A Backend is an abstraction around communication with robots, vision, and refbox. It
+ * produces SensorMsgs, and consumes primitives that can be sent to the robots.
+ *
+ * This produce/consume pattern is performed by extending both "Observer" and
+ * "Subject". Please see the implementation of those classes for details.
+ */
+class Backend : public Subject<SensorMsg>,
+                public ThreadedObserver<ConstPrimitiveVectorPtr>
+{
+   public:
+    Backend() = default;
+
+    virtual ~Backend() = default;
+
+    // Delete the copy and assignment operators because this class really shouldn't need
+    // them and we don't want to risk doing anything nasty with the internal
+    // multithreading this class potentially uses
+    Backend& operator=(const Backend&) = delete;
+    Backend(const Backend&)            = delete;
+};
