51810f174bd6501f74d504c045078c6d11b46aab
evanmorcom <evans.morcom@alumni.ubc.ca>
2020-05-17 09:42:58 -0700

    Added python test file

---
+++ src/firmware/app/control/wheel_controller/difference_equation_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+This file contains the unit tests for the DifferenceEquation class
+
+"""
+
+import numpy as np
+import control as ct
+from control.matlab import *
+import matplotlib.pyplot as plt
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
