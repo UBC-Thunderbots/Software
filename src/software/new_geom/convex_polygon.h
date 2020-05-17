5c6c7c0aca55af8388935420bcf2af0c227cd8a2
jonathanlew <31377876+jonathanlew@users.noreply.github.com>
2020-05-16 22:49:28 -0700

    Update src/software/new_geom/convex_polygon.h
    Co-authored-by: Mathew <mmacd97@gmail.com>

--- src/software/new_geom/convex_polygon.h
+++ src/software/new_geom/convex_polygon.h
@@ -30,7 +30,7 @@ class ConvexPolygon : public Polygon, public ConvexShape
      * The convex polygon is split in half perpendicular to v and points on the half that
      * v is pointing are translated by v
      *
-     * @return a convex polygon expanded by v
+     * @return a convex polygon expanded in the direction of v
      */
     ConvexPolygon expand(const Vector& v) const;
 
