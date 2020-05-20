ca3cc9f73118286b06ed59a5fd22a382b3930b46
Mathew MacDougall <mmacd97@gmail.com>
2020-05-20 10:29:54 -0700

    Added ball state without timestamp information

---
+++ src/software/world/ball_state.cpp
@@ -0,0 +1,27 @@
+#include "software/world/ball_state.h"
+
+BallState::BallState(const Point& position, const Vector& velocity)
+{
+    this->position_  = position;
+    this->velocity_  = velocity;
+}
+
+Point BallState::position() const
+{
+    return position_;
+}
+
+Vector BallState::velocity() const
+{
+    return velocity_;
+}
+
+bool BallState::operator==(const BallState &other) const
+{
+    return this->position() == other.position() && this->velocity() == other.velocity();
+}
+
+bool BallState::operator!=(const BallState &other) const
+{
+    return !(*this == other);
+}
---
+++ src/software/world/ball_state.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "software/new_geom/point.h"
+#include "software/new_geom/vector.h"
+
+/**
+ * Represents the state of a ball
+ */
+class BallState
+{
+   public:
+    /**
+     * Creates a new ball state with the given position and velocity
+     *
+     * @param position The position of the ball, with coordinates in metres
+     * @param velocity The velocity of the ball, in metres per second
+     */
+    explicit BallState(const Point& position, const Vector& velocity);
+    BallState() = delete;
+
+    /**
+     * Returns the position of the ball represented by this state
+     *
+     * @return the position of the ball represented by this state
+     */
+    Point position() const;
+
+    /**
+     * Returns the velocity of the ball represented by this state
+     *
+     * @return the velocity of the ball represented by this state
+     */
+    Vector velocity() const;
+
+    /**
+     * Defines the equality operator for a BallState. BallStates are equal if their
+     * positions and velocities are the same
+     *
+     * @param other The ball state to compare against for equality
+     * @return True if the other ball state is equal to this ball state, and false
+     * otherwise
+     */
+    bool operator==(const BallState &other) const;
+
+    /**
+     * Defines the inequality operator for a BallState.
+     *
+     * @param other The ball state to compare against for inequality
+     * @return True if the other ball state is not equal to this ball state, and false
+     * otherwise
+     */
+    bool operator!=(const BallState &other) const;
+
+   protected:
+    Point position_;
+    Vector velocity_;
+};
