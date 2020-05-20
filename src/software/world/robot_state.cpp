d4bb4795fd9fdd30ee34ec0ec0629540f74f8335
Mathew MacDougall <mmacd97@gmail.com>
2020-05-20 12:24:44 -0700

    Added RobotState (without timestamp) and unit tests

---
+++ src/software/world/robot_state.cpp
@@ -0,0 +1,39 @@
+#include "software/world/robot_state.h"
+
+RobotState::RobotState(const Point &position, const Vector &velocity,
+                                                 const Angle &orientation, const AngularVelocity &angular_velocity
+                                                 ) : position_(position), velocity_(velocity), orientation_(orientation), angular_velocity_(angular_velocity)
+{
+}
+
+Point RobotState::position() const
+{
+    return position_;
+}
+
+Vector RobotState::velocity() const
+{
+    return velocity_;
+}
+
+Angle RobotState::orientation() const
+{
+    return orientation_;
+}
+
+AngularVelocity RobotState::angularVelocity() const
+{
+    return angular_velocity_;
+}
+
+bool RobotState::operator==(const RobotState &other) const
+{
+    return this->position() == other.position() && this->velocity() == other.velocity() &&
+           this->orientation() == other.orientation() &&
+           this->angularVelocity() == other.angularVelocity();
+}
+
+bool RobotState::operator!=(const RobotState &other) const
+{
+    return !(*this == other);
+}
---
+++ src/software/world/robot_state.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "software/new_geom/angle.h"
+#include "software/new_geom/angular_velocity.h"
+#include "software/new_geom/point.h"
+#include "software/new_geom/vector.h"
+
+/**
+ * This class represents the physical state of a robot
+ */
+class RobotState
+{
+   public:
+    /**
+     * Creates a new robot state with the given position, velocity, orientation, angular
+     * velocity, and timestamp
+     *
+     * @param position The position of the robot, with coordinates in metres
+     * @param velocity The velocity of the robot, in metres per second
+     * @param orientation The orientation of the robot
+     * @param angular_velocity The angular velocity of the robot
+     */
+    explicit RobotState(const Point &position, const Vector &velocity,
+                                     const Angle &orientation, const AngularVelocity &angular_velocity);
+
+    /**
+     * Returns the position of the robot represented by this state
+     *
+     * @return the position of the robot represented by this state
+     */
+    Point position() const;
+
+    /**
+     * Returns the velocity of the robot represented by this state
+     *
+     * @return the velocity of the robot represented by this state
+     */
+    Vector velocity() const;
+
+    /**
+     * Returns the orientation of the robot represented by this state
+     *
+     * @return the orientation of the robot represented by this state
+     */
+    Angle orientation() const;
+
+    /**
+     * Returns the angular velocity of the robot represented by this state
+     *
+     * @return the angular velocity of the robot represented by this state
+     */
+    AngularVelocity angularVelocity() const;
+
+    /**
+     * Defines the equality operator for a RobotState. RobotStates are equal if their
+     * positions, velocities, orientation, and angular velocity are the same
+     *
+     * @param other The robot state to compare against for equality
+     * @return True if the other robot state is equal to this robot state, and false
+     * otherwise
+     */
+    bool operator==(const RobotState &other) const;
+
+    /**
+     * Defines the inequality operator for a RobotState.
+     *
+     * @param other The robot state to compare against for inequality
+     * @return True if the other robot state is not equal to this robot state, and false
+     * otherwise
+     */
+    bool operator!=(const RobotState &other) const;
+
+   private:
+    Point position_;
+    Vector velocity_;
+    Angle orientation_;
+    AngularVelocity angular_velocity_;
+};
