8fa82910f32586cd35a9589420dce9f7ef053c87
evanmorcom <evans.morcom@alumni.ubc.ca>
2020-05-20 14:55:43 -0700

    Added 2 more unit tests including a 2nd order oscillatory response

--- src/firmware/app/control/python_controller/difference_equation.py
+++ src/firmware/app/control/python_controller/difference_equation.py
@@ -8,6 +8,9 @@ import numpy as np
 import control as ct
 from control.matlab import *
 import matplotlib.pyplot as plt
+import collections
+import _collections_abc
+import os
 
 class DifferenceEquation:
 
@@ -18,22 +21,25 @@ class DifferenceEquation:
         :list tf_numerator_coefficients: The coefficients of the numerator of the discrete transfer function
         :list tf_denominator_coefficients: The coefficients of the denominator of the discrete transfer function
         """
-        self.numerator_coefficients = tf_numerator_coefficients
-        self.denominator_coefficients = tf_denominator_coefficients
+        self.__numerator_coefficients = tf_numerator_coefficients
+        self.__denominator_coefficients = tf_denominator_coefficients
 
         # Store the order of the numerator and denominator for calculation later
-        self.controller_rank = len(self.denominator_coefficients)
-        self.input_coeffs_order = len(self.numerator_coefficients)
+        self.__controller_rank = len(self.__denominator_coefficients)
+        self.__input_coeffs_order = len(self.__numerator_coefficients)
 
         # Normalize by the coefficient of the highest order denominator term
         # This is done so that the output of the difference equation is not scaled
-        self.numerator_coefficients / self.denominator_coefficients[0]
-        self.denominator_coefficients / self.denominator_coefficients[0]
+        for i in range(0, len(self.__numerator_coefficients)):
+            self.__numerator_coefficients[i] /= self.__denominator_coefficients[0]
+            
+        for i in range(0, len(self.__denominator_coefficients)):
+            self.__denominator_coefficients[i] /= self.__denominator_coefficients[0]
 
         # We must keep track of the system state and previous states
         self.time_step_count = 0
-        self.previous_input = []
-        self.previous_output = []
+        self.__previous_input = []
+        self.__previous_output = []
 
     def tick(self, current_input):
 
@@ -46,43 +52,46 @@ class DifferenceEquation:
         effect_of_previous_input = 0
 
         # Loop through each coefficient of the numerator
-        for i in range(0, self.input_coeffs_order):
+        for i in range(0, self.__input_coeffs_order):
 
             # Calculate how many input 'steps' back in time we must use for the given numerator coefficient
-            index = self.time_step_count + (self.input_coeffs_order - self.controller_rank - i)
+            index = self.time_step_count + (self.__input_coeffs_order - self.__controller_rank - i)
 
             # If we must go more time steps backwards than are recorded, the term has no effect on the output
-            if(index < 0 or len(self.previous_input) < index+1):
+            if(index < 0 or len(self.__previous_input) < index+1):
                 effect_of_previous_input += 0
             else:
-                effect_of_previous_input += self.numerator_coefficients[i] * self.previous_input[index]
+                effect_of_previous_input += self.__numerator_coefficients[i] * self.__previous_input[index]
 
         # Generate the systems response due to previous output
         effect_of_previous_output = 0
 
         # Loop through each coefficient of the denominator other than the highest order term
         # as it represents the output of the tick
-        for i in range(1, self.controller_rank):
+        for i in range(1, self.__controller_rank):
 
             # Calculate the number of time steps backwards to use
             index = self.time_step_count - i
 
             # If we must go more time steps backwards than are recorded, the term has no effect on the output
-            if(index <  0 or len(self.previous_output) < index+1):
+            if(index <  0 or len(self.__previous_output) < index+1):
                 effect_of_previous_output += 0
             else:
-                effect_of_previous_output += self.denominator_coefficients[i] * self.previous_output[index]
+                effect_of_previous_output += self.__denominator_coefficients[i] * self.__previous_output[index]
 
         # The new output of the system in the difference between the effect of the inputs and the effect of the outputs
         new_output = effect_of_previous_input - effect_of_previous_output
 
         # Append the current input to the history of inputs
-        self.previous_input.append(current_input)
+        self.__previous_input.append(current_input)
 
         # Append the current output to the list of outputs
-        self.previous_output.append(new_output)
+        self.__previous_output.append(new_output)
 
         # Increase the count of time steps
         self.time_step_count += 1
         
         return new_output
+
+    def get_output_history(self):
+        return self.__previous_output
--- src/firmware/app/control/python_controller/difference_equation_test.py
+++ src/firmware/app/control/python_controller/difference_equation_test.py
@@ -9,12 +9,42 @@ import control as ct
 from control.matlab import *
 import matplotlib.pyplot as plt
 import unittest
-from firmware.app.control.python_controller.difference_equation import DifferenceEquation
-
+import difference_equation as de
 
 class TestDifferenceEquation(unittest.TestCase):
 
-    def test_step_function(self):
+    def test_output(self):
+        """
+        Test that the difference equation follows the correct output sequence
+        as a human-checked sequence with constant input
+        """
+        difference_equation = de.DifferenceEquation([1, 1], [1, 1, 1])
+
+        output = difference_equation.tick(1)
+
+        self.assertEqual(output, 0)
+
+        output = difference_equation.tick(1)
+        
+        self.assertEqual(output, 1)
+
+        output = difference_equation.tick(1)
+        
+        self.assertEqual(output, 1)
+
+        output = difference_equation.tick(1)
+        
+        self.assertEqual(output, 0)
+
+        output = difference_equation.tick(1)
+        
+        self.assertEqual(output, 1)
+
+    def test_step_function_first_order(self):
+        """
+        This test checks the step response of the DifferenceEquation class vs the discrete transfer function (1st order)
+        using the python control toolbox
+        """
 
         sample_time = 0.0001  # [s]
         end_time = 5
@@ -30,22 +60,60 @@ class TestDifferenceEquation(unittest.TestCase):
         continuous_tf = K/(J*s + B)
 
         discrete_tf = ct.sample_system(continuous_tf, sample_time, 'zoh' )
-        difference_equation = DifferenceEquation(discrete_tf.num[0][0], discrete_tf.den[0][0])
-
-        difference_equation_output = []
-
-        difference_equation.tick(step_input)
+        difference_equation = de.DifferenceEquation(discrete_tf.num[0][0], discrete_tf.den[0][0])
 
         num_points = int(end_time/sample_time)+1
 
-        T = np.linspace(0, 5, num_points)
+        T = np.linspace(0, end_time, num_points)
         T, yout = ct.step_response(discrete_tf, T)
 
-        for i in range(0, num_points-1):
-            difference_equation.tick(1)
+        for i in range(0, num_points):
+            difference_equation.tick(step_input)
+
+
+        system_response = difference_equation.get_output_history()
 
         for i in range(0, len(T)):
-            self.assertAlmostEqual(yout[i], difference_equation.previous_output[i], 6)
+            self.assertAlmostEqual(yout[i], system_response[i], 4)
+
+    def test_step_function_second_order(self):
+        """
+        This test checks the step response of the DifferenceEquation class vs the discrete transfer function (2nd order)
+        using the python control toolbox
+        """
+
+        sample_time = 0.0001  # [s]
+        end_time = 20
+
+        J = 0.01
+        B = 0.001
+        K = 1
+
+        s = ct.tf([1, 0], 1)
+
+        step_input = 1
+
+        continuous_tf = K/(J*s**2 + B*s + 1.5)
+
+        discrete_tf = ct.sample_system(continuous_tf, sample_time, 'zoh')
+        difference_equation = de.DifferenceEquation(discrete_tf.num[0][0], discrete_tf.den[0][0])
+
+        num_points = int(end_time/sample_time)+1
+
+        T = np.linspace(0, end_time, num_points)
+        T, yout = ct.step_response(discrete_tf, T)
+
+        for i in range(0, num_points):
+            difference_equation.tick(step_input)
+
+        system_response = difference_equation.get_output_history()
+
+        plt.plot(T, system_response)
+        plt.plot(T, yout)
+        plt.show()
+
+        for i in range(0, len(T)):
+            self.assertAlmostEqual(yout[i], system_response[i], 4)
 
 
 if __name__ == '__main__':
