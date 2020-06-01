8cc0b4eb464299c51100655066e4837fb77df996
akhil <akhil.veeraghanta@outlook.com>
2020-05-31 17:34:56 -0700

    wip backend

--- src/software/backend/wifi_backend.cpp
+++ src/software/backend/wifi_backend.cpp
@@ -12,22 +12,47 @@ WifiBackend::WifiBackend()
                     Util::Constants::SSL_GAMECONTROLLER_MULTICAST_PORT,
                     boost::bind(&WifiBackend::receiveWorld, this, _1),
                     Util::DynamicParameters->getAIControlConfig()->getRefboxConfig(),
-                    Util::DynamicParameters->getCameraConfig()),
-      vision_output(
-          Subject<RobotStatus>::sendValueToObservers(status);
-})
+                    Util::DynamicParameters->getCameraConfig())
 {
+    // connect to current channel
+    connectOnChannel(Util::DynamicParameters->getNetworkConfig()->Channel()->value());
+
+    // add callback to switch channels on param change
+    Util::DynamicParameters->getNetworkConfig()->Channel()->registerCallbackFunction(
+        boost::bind(&WifiBackend::connectOnChannel, this, _1));
 }
 
 void WifiBackend::onValueReceived(ConstPrimitiveVectorPtr primitives_ptr)
 {
-    primitive_output.sendProto(ProtobufMessageTranslator::getPrimitiveMsgFromPrimitiveVector(primitives_ptr);
+    primitive_output->sendProto(
+        ProtobufMessageTranslator::getPrimitiveMsgFromPrimitiveVector(primitives_ptr));
 }
 
 void WifiBackend::receiveWorld(World world)
 {
-    vision_output.sendProto(ProtobufMessageTranslator::getVisionMsgFromWorld(world));
+    vision_output->sendProto(ProtobufMessageTranslator::getVisionMsgFromWorld(world));
     Subject<World>::sendValueToObservers(world);
+}
+
+void WifiBackend::receiveTbotsRobotMsg(TbotsRobotMsg robot_msg)
+{
+    // TODO handle me
+}
+
+void WifiBackend::connectOnChannel(int channel)
+{
+    // when ProtoMulticastSenders and ProtoMulticastListeners are destroyed,
+    // they close the sockets before freeing memory
+    vision_output.reset(new ProtoMulticastSender<VisionMsg>(
+        io_service, MULTICAST_CHANNELS[channel], VISION_PORT));
+
+    primitive_output.reset(new ProtoMulticastSender<PrimitiveMsg>(
+        io_service, MULTICAST_CHANNELS[channel], PRIMITIVE_PORT));
+
+    robot_status_input.reset(new ProtoMulticastListener<TbotsRobotMsg>(
+        io_service, MULTICAST_CHANNELS[channel], ROBOT_STATUS_PORT,
+        boost::bind(&WifiBackend::receiveTbotsRobotMsg, this, _1));
+    );
 }
 
 // Register this backend in the genericFactory
