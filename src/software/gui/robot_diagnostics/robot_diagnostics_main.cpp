208ddfc5c1e619b6bf1884d737edad2ca9838368
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2020-06-03 21:51:31 -0700

    rename MainWidget->RobotDiagnostics

--- src/software/gui/robot_diagnostics/robot_diagnostics_main.cpp
+++ src/software/gui/robot_diagnostics/robot_diagnostics_main.cpp
@@ -1,9 +1,9 @@
-#include "software/gui/robot_diagnostics/widgets/main_widget.h"
+#include "software/gui/robot_diagnostics/widgets/robot_diagnostics.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWidget w;
+    RobotDiagnostics w;
     w.show();
 
     return a.exec();
