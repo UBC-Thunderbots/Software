d9387fd392b3c1bb02521e2b1a0e586aa56ad605
Mathew <mmacd97@gmail.com>
2020-06-06 18:20:22 -0700

    Fixed shellcheck warnings in setup scripts (#1434)

--- environment_setup/setup_udev_rules.sh
+++ environment_setup/setup_udev_rules.sh
@@ -5,7 +5,7 @@
 # helps prevent bugs and odd behaviour if this script is run through a symlink
 # or from a different directory.
 CURR_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-cd $CURR_DIR
+cd "$CURR_DIR" || exit
 
 # Set up udev rules for USB permissions. This gives the user the required permissions
 # to use the radio dongle and flash the robots via USB.
@@ -17,8 +17,8 @@ udev_rules_file="/etc/udev/rules.d/99-thunderbots.rules"
 
 # Copy the new Thunderbots udev rules to the rule folder, overwriting any
 # existing files
-sudo cp $CURR_DIR/99-thunderbots.rules $udev_rules_file
-if [ $? -ne 0 ]; then
+
+if ! sudo cp "$CURR_DIR/99-thunderbots.rules" "$udev_rules_file" ; then
     echo "##############################################################"
     echo "Error: Failed to copy udev rules"
     echo "##############################################################"
@@ -26,16 +26,16 @@ if [ $? -ne 0 ]; then
 fi
 
 # Add user to dialout group for USB comms
-sudo adduser $USER dialout
+sudo adduser "$USER" dialout
 
 # Create a new 'thunderbots' group that is used by the udev file
 # This allows the udev rules file to properly grant USB permission
 sudo groupadd thunderbots
-sudo adduser $USER thunderbots
+sudo adduser "$USER" thunderbots
 
 echo "================================================================"
 echo "Done"
 echo ""
-echo "Make sure to restart your computer for permissions changes to take effect!"
+echo "Make sure to logout or restart your computer for permissions changes to take effect!"
 echo "================================================================"
 
