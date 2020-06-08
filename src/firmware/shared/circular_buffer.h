b0bcec6074aca94ff673dff0d290e2a7e267d232
kelvinkoon <kkoon@shaw.ca>
2020-06-07 17:46:10 -0700

    Fix minor cbuffer comments

--- src/firmware/shared/circular_buffer.h
+++ src/firmware/shared/circular_buffer.h
@@ -10,7 +10,7 @@ typedef struct CircularBuffer CircularBuffer_t;
 
 /**
  * Initialize the circular_buffer struct members
- * @param  cbuff The circular_buffer
+ * @param  cbuffer The circular_buffer
  * @param  size Size of the circular_buffer to set
  */
 CircularBuffer_t *circular_buffer_create(CircularBuffer_t *cbuffer, int size);
@@ -21,14 +21,14 @@ CircularBuffer_t *circular_buffer_create(CircularBuffer_t *cbuffer, int size);
  * NOTE: This will not destroy the values pointed to by any pointers passed to the
  * `create` function
  *
- * @param  cbuff The circular_buffer to destroy
+ * @param  cbuffer The circular_buffer to destroy
  */
 void circular_buffer_destroy(CircularBuffer_t *cbuffer);
 
 /**
  * Push data into the circular_buffer
  * If full, will overwrite the oldest values
- * @param  cbuff The circular_buffer
+ * @param  cbuffer The circular_buffer
  * @param  data  Value to be inserted into the circular_buffer
  */
 void circular_buffer_push(CircularBuffer_t *cbuffer, float data);
@@ -36,7 +36,7 @@ void circular_buffer_push(CircularBuffer_t *cbuffer, float data);
 /**
  * Retrieve the most recent value in the circular_buffer
  * Ex. 1 is the most recent value, 2 is the 2nd most recent value, etc.
- * @param  cbuff The circular_buffer
+ * @param  cbuffer The circular_buffer
  * @param  index Index of most recent value
  */
 float circular_buffer_get_index(CircularBuffer_t *cbuffer, int index);
