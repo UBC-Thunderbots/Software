94fe3702192e368219a98903ab51dbabe7afc3af
Mathew MacDougall <mmacd97@gmail.com>
2020-06-11 15:22:15 -0700

    Added javadoc comments to the wrapper message translators

--- src/software/proto/message_translation/ssl_wrapper_message_translator.h
+++ src/software/proto/message_translation/ssl_wrapper_message_translator.h
@@ -6,4 +6,14 @@
 #include "software/proto/messages_robocup_ssl_geometry.pb.h"
 #include "software/proto/messages_robocup_ssl_detection.pb.h"
 
+/**
+ * Creates a WrapperPacket from the given data
+ *
+ * @param geometry_data The geometry data to add to the wrapper packet. If null,
+ * no data is added
+ * @param detection_frame The detection frame data to add to the wrapper packet.
+ * If null, no data is added
+ *
+ * @return A WrapperPacket containing the given data
+ */
 std::unique_ptr<SSL_WrapperPacket> createWrapperPacket(std::unique_ptr<SSL_GeometryData> geometry_data, std::unique_ptr<SSL_DetectionFrame> detection_frame);
