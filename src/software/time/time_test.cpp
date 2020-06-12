746c962115b9e599ba82f88a210b2b8c9cc23580
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2020-06-12 00:15:53 -0700

    fix time test

--- src/software/time/time_test.cpp
+++ src/software/time/time_test.cpp
@@ -5,6 +5,8 @@
 TEST(TimeTest, test_now)
 {
     double t1 = Time::now();
+    // Add some time in between
+    EXPECT_EQ(t1, t1);
     double t2 = Time::now();
-    EXPECT_NE(t1, t2);
+    EXPECT_TRUE(t2 > t1);
 }
