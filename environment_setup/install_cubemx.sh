56920897a4e36a6f26b1b38ac1d7f8756d6d3379
Akhil Veeraghanta <31548846+akhilveeraghanta@users.noreply.github.com>
2020-06-13 22:25:52 -0700

    STM32CubeMX upgrade to 5.6.1 from 5.4.0 (#1468)
    * upgrade script and regenerated
    
    * fix PR comments
    
    * fix formatting even more
    
    * fix typo

--- environment_setup/install_cubemx.sh
+++ environment_setup/install_cubemx.sh
@@ -3,9 +3,14 @@
 # Any failure should terminate this script
 set -e
 
-# download version 5.4.0 (change the following two variables to change versions)
-CUBE_VERSION="5.4.0"
-CUBE_LINK="https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/5f/d0/cf/79/10/fb/4e/7e/STM32CubeMX_v5-4-0/files/stm32cubemx_v5.4.0.zip/jcr:content/translations/en.stm32cubemx_v5.4.0.zip"
+# download version 5.6.1 (change the following three variables to change versions)
+CUBE_VERSION="5.6.1"
+CUBE_ZIP_FILENAME="en.stm32cubemx_v5-6-1.zip"
+
+# this link can be found here: https://www.st.com/en/development-tools/stm32cubemx.html
+# use the guest download feature (i.e do NOT log in). ST will send out an email that has
+# a link that doesn't need to be authenticated
+CUBE_LINK="https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/01/9e/e3/0c/3d/ff/41/7b/stm32cubemx_v5-6-1/files/stm32cubemx_v5-6-1.zip/jcr:content/translations/en.stm32cubemx_v5-6-1.zip"
 
 CUBEMX_TMP_DIR="/tmp/cubemx"
 CUBEMX_INSTALL_DIR="/opt/STM32CubeMX_$CUBE_VERSION"
@@ -46,9 +51,9 @@ else
     echo -n "$STM32_SCRIPT" > cuberunner.sh
     
     curl -O $CUBE_LINK
-    unzip en.stm32cubemx_v5.4.0.zip
+    unzip $CUBE_ZIP_FILENAME
     
-    sudo java -jar ./SetupSTM32CubeMX-5.4.0.exe auto-install.xml
+    sudo java -jar ./SetupSTM32CubeMX-$CUBE_VERSION.exe auto-install.xml
     sudo cp ./cuberunner.sh /opt/STM32CubeMX_$CUBE_VERSION/cuberunner.sh
 
     cd $CUBEMX_INSTALL_DIR
@@ -57,6 +62,7 @@ else
     sudo ln -sfn /opt/STM32CubeMX_$CUBE_VERSION/cuberunner.sh /usr/local/bin/STM32CubeMX 
 
     echo "================================================================"
-    echo "Done Installing CubeMX"
+    echo "Done Installing CubeMX to $CUBEMX_INSTALL_DIR"
+    echo "NOTE: if you are upgrading versions, you can delete older versions in the /opt directory"
     echo "================================================================"
 fi
