0a61c006cac1d214a1269aed783002c20ea09807
Mathew MacDougall <mmacd97@gmail.com>
2020-06-19 11:49:55 -0700

    refactored ball drawing functions to use BallState

--- src/software/gui/drawing/ball.cpp
+++ src/software/gui/drawing/ball.cpp
@@ -1,8 +1,13 @@
 #include "software/gui/drawing/ball.h"
 
 #include "shared/constants.h"
+#include "software/math/math_functions.h"
+#include "software/new_geom/segment.h"
+#include "software/gui/drawing/geom.h"
+#include "software/gui/drawing/colors.h"
+#include "software/gui/geometry_conversion/geometry_conversion.h"
 
-void drawBallVelocity(QGraphicsScene *scene, const Ball &ball, const QColor &color)
+void drawBallVelocity(QGraphicsScene *scene, const Point& position, const Vector& velocity, const QColor &color)
 {
     QPen pen(color);
     pen.setWidth(2);
@@ -11,28 +16,35 @@ void drawBallVelocity(QGraphicsScene *scene, const Ball &ball, const QColor &col
     pen.setCapStyle(Qt::PenCapStyle::RoundCap);
     pen.setCosmetic(true);
 
-    drawSegment(scene, Segment(ball.position(), ball.position() + ball.velocity()), pen);
+    drawSegment(scene, Segment(position, position + velocity), pen);
 }
 
-void drawBallPosition(QGraphicsScene *scene, const Ball &ball, const QColor &color)
+void drawBallPosition(QGraphicsScene *scene, const Point& position, const double height, const QColor &color)
 {
     QPen pen(color);
     pen.setWidth(2);
     pen.setCosmetic(true);
 
+    QColor fill_colour = color;
+    // Decrease the alpha value as the ball's height increases
+    double alpha = 1.0 - normalizeValueToRange<double>(height, 0, ROBOT_MAX_HEIGHT_METERS, 0.0, 0.5);
+    fill_colour.setAlphaF(alpha);
     QBrush brush(color);
     brush.setStyle(Qt::BrushStyle::SolidPattern);
 
-    drawCircle(scene, Circle(ball.position(), BALL_MAX_RADIUS_METERS), pen, brush);
+    // Increase the radius of the ball the higher it is.
+    // 2.0 is an estimate upper bound on how high the ball will go
+    double ball_radius = normalizeValueToRange<double>(height, 0, 2.0, BALL_MAX_RADIUS_METERS, 3 * BALL_MAX_RADIUS_METERS);
+    drawCircle(scene, Circle(position, ball_radius), pen, brush);
 }
 
-void drawBall(QGraphicsScene *scene, const Ball &ball)
+void drawBall(QGraphicsScene *scene, const BallState &ball)
 {
-    drawBallPosition(scene, ball, ball_color);
-    drawBallVelocity(scene, ball, ball_color);
+    drawBallPosition(scene, ball.position(), ball.height(), ball_color);
+    drawBallVelocity(scene, ball.position(), ball.velocity(), ball_color);
 }
 
-void drawBallConeToFriendlyNet(QGraphicsScene *scene, const Ball &ball,
+void drawBallConeToFriendlyNet(QGraphicsScene *scene, const Point& position,
                                const Field &field)
 {
     QColor ball_cone_color = ball_color;
@@ -45,8 +57,8 @@ void drawBallConeToFriendlyNet(QGraphicsScene *scene, const Ball &ball,
     pen.setCapStyle(Qt::PenCapStyle::RoundCap);
     pen.setCosmetic(true);
 
-    Segment pos_goalpost_segment(ball.position(), field.friendlyGoalpostPos());
-    Segment neg_goalpost_segment(ball.position(), field.friendlyGoalpostNeg());
+    Segment pos_goalpost_segment(position, field.friendlyGoalpostPos());
+    Segment neg_goalpost_segment(position, field.friendlyGoalpostNeg());
     drawSegment(scene, pos_goalpost_segment, pen);
     drawSegment(scene, neg_goalpost_segment, pen);
 }
