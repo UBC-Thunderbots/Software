f74db3b1a805842c6e992974588d0dc0d5898700
Mathew <mmacd97@gmail.com>
2020-06-20 12:46:26 -0700

    Move static field constructors (#1497)

--- src/software/test_util/test_util.cpp
+++ src/software/test_util/test_util.cpp
@@ -2,25 +2,9 @@
 
 namespace TestUtil
 {
-    Field createSSLDivBField()
-    {
-        // Using the dimensions of a standard Division B SSL field
-        // https://robocup-ssl.github.io/ssl-rules/sslrules.html#_field_setup
-        Field field = Field(9.0, 6.0, 1.0, 2.0, 0.18, 1.0, 0.3, 0.5);
-        return field;
-    }
-
-    Field createSSLDivAField()
-    {
-        // Using the dimensions of a standard Division A SSL field
-        // https://robocup-ssl.github.io/ssl-rules/sslrules.html#_field_setup
-        Field field = Field(12.0, 9.0, 1.8, 3.6, 0.18, 1.8, 0.3, 0.5);
-        return field;
-    }
-
     World createBlankTestingWorld()
     {
-        Field field        = createSSLDivBField();
+        Field field        = Field::createSSLDivisionBField();
         Team friendly_team = Team(Duration::fromMilliseconds(1000));
         Team enemy_team    = Team(Duration::fromMilliseconds(1000));
         Ball ball          = Ball(Point(), Vector(), Timestamp::fromSeconds(0));
