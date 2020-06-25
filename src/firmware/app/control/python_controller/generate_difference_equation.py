ad37f5eb0ae7857adddde80a12935d7e8091547e
kelvinkoon <kkoon@shaw.ca>
2020-06-24 21:41:37 -0700

    Add generated code in generate_difference_equation

--- src/firmware/app/control/python_controller/generate_difference_equation.py
+++ src/firmware/app/control/python_controller/generate_difference_equation.py
@@ -6,12 +6,12 @@ from control.matlab import *
 
 CONTROLLER_DIFFERENCE_EQUATION_COEFFICIENTS = """
 #define WHEEL_SPEED_CONTROLLER_TICK_TIME 1e7
-#define GENERATED_NUM_WHEEL_SPEED_COEFFICIENTS {numerator_coefficients};
-#define GENERATED_NUM_PREVIOUS_WHEEL_SPEED_COMMAND_COEFFICIENTS {denominator_coefficients};
+#define GENERATED_NUM_WHEEL_SPEED_COEFFICIENTS {numerator_length};
+#define GENERATED_NUM_PREVIOUS_WHEEL_SPEED_COMMAND_COEFFICIENTS {denominator_length};
 float GENERATED_WHEEL_SPEED_COEFFICIENTS[GENERATED_NUM_WHEEL_SPEED_COEFFICIENTS] =
-{{{numerator_length}}};
+{{{numerator_coefficients}}};
 float GENERATED_PREVIOUS_WHEEL_SPEED_COMMAND_COEFFICIENTS[GENERATED_NUM_PREVIOUS_WHEEL_SPEED_COMMAND_COEFFICIENTS] =
-{{{denominator_length}}};
+{{{denominator_coefficients}}};
 """
 
 
@@ -23,7 +23,7 @@ def generate(discrete_tf: ct.TransferFunction):
     numerator_length = len(numerator_coefficients)
     denominator_length = len(denominator_coefficients)
 
-    CONTROLLER_DIFFERENCE_EQUATION_COEFFICIENTS.format(
+    return CONTROLLER_DIFFERENCE_EQUATION_COEFFICIENTS.format(
         numerator_coefficients=numerator_coefficients,
         denominator_coefficients=denominator_coefficients,
         numerator_length=numerator_length,
@@ -36,10 +36,28 @@ def generate(discrete_tf: ct.TransferFunction):
 #######################################################################
 if __name__ == "__main__":
     with open(sys.argv[-1], "w") as controller_difference_coefficient_generator:
+        # TODO hard-coded ATM, need to read discrete_function somewhere
+        numerator = [1, 1]
+        denominator = [1, 1, 1]
+        interpolation_period = 0.1
+        input_value = 1
+
+        transfer_function = tf(numerator, denominator, interpolation_period)
+
+        res = generate(transfer_function)
 
         # header
-        controller_difference_coefficient_generator.write(
-            CONTROLLER_DIFFERENCE_EQUATION_COEFFICIENTS
-        )
+        controller_difference_coefficient_generator.write(res)
+
+        # Generates generated_controller_coefficients.h ATM:
+        """
+        #define WHEEL_SPEED_CONTROLLER_TICK_TIME 1e7
+        #define GENERATED_NUM_WHEEL_SPEED_COEFFICIENTS 2;
+        #define GENERATED_NUM_PREVIOUS_WHEEL_SPEED_COMMAND_COEFFICIENTS 3;
+        float GENERATED_WHEEL_SPEED_COEFFICIENTS[GENERATED_NUM_WHEEL_SPEED_COEFFICIENTS] =
+        {[1 1]};
+        float GENERATED_PREVIOUS_WHEEL_SPEED_COMMAND_COEFFICIENTS[GENERATED_NUM_PREVIOUS_WHEEL_SPEED_COMMAND_COEFFICIENTS] =
+        {[1 1 1]};
+        """
 
     print("INFO: Generated Difference Equation Controller Coefficients")
