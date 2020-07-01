a6f019dd27ac6e3994eaa23063ed268e9b978dbb
Mathew MacDougall <mmacd97@gmail.com>
2020-06-30 19:39:51 -0700

    renamed RobotDiagnosticsGUIWrapper to ThreadedRobotDiagnosticsGUI to match new naming convention

--- src/software/gui/robot_diagnostics/threaded_robot_diagnostics_gui.h
+++ src/software/gui/robot_diagnostics/threaded_robot_diagnostics_gui.h
@@ -6,7 +6,7 @@
 #include <future>
 #include <thread>
 
-#include "software/gui/robot_diagnostics/widgets/robot_diagnostics.h"
+#include "software/gui/robot_diagnostics/widgets/robot_diagnostics_gui.h"
 #include "software/multithreading/thread_safe_buffer.h"
 #include "software/multithreading/threaded_observer.h"
 #include "software/new_geom/rectangle.h"
@@ -15,53 +15,53 @@
 #include "software/world/world.h"
 
 /**
- * This class wraps our RobotDiagnostics object which is responsible for allowing users to
+ * This class wraps our RobotDiagnosticsGUI object which is responsible for allowing users to
  * interact with and debug the robot
  */
-class RobotDiagnosticsWrapper : public ThreadedObserver<SensorMsg>
+class ThreadedRobotDiagnosticsGUI : public ThreadedObserver<SensorMsg>
 {
    public:
-    RobotDiagnosticsWrapper() = delete;
+    ThreadedRobotDiagnosticsGUI() = delete;
 
     /**
-     * Create a new RobotDiagnostics wrapper. The argc and argv arguments are required
+     * Create a new ThreadedRobotDiagnosticsGUI. The argc and argv arguments are required
      * to create a QApplication
      *
      * @param argc The number of arguments being passed
      * @param argv Keyword arguments
      */
-    explicit RobotDiagnosticsWrapper(int argc, char** argv);
+    explicit ThreadedRobotDiagnosticsGUI(int argc, char** argv);
 
-    ~RobotDiagnosticsWrapper() override;
+    ~ThreadedRobotDiagnosticsGUI() override;
 
     /**
      * Returns a shared_ptr to a promise that can be waited on, and that will
-     * be notified once the RobotDiagnostics has shut down
+     * be notified once the ThreadedRobotDiagnosticsGUI has shut down
      *
      * @return a shared_ptr to a promise that can be waited on, and that will
-     * be notified once the RobotDiagnostics has been shut down
+     * be notified once the ThreadedRobotDiagnosticsGUI has been shut down
      */
     std::shared_ptr<std::promise<void>> getTerminationPromise();
 
    private:
     /**
-     * Creates a new RobotDiagnostics in a new thread and starts running it. These
+     * Creates a new RobotDiagnosticsGUI in a new thread and starts running it. These
      * objects must be created in the new thread because the QApplication must be
-     * constructed in the thread is will run in, and the RobotDiagnostics must be
+     * constructed in the thread is will run in, and the RobotDiagnosticsGUI must be
      * created in the same context as the QApplication (which in this case is the new
      * thread).
      *
      * @param argc The number of arguments being passed
-     * @param argv Keyword arguments for the RobotDiagnostics QApplication
+     * @param argv Keyword arguments for the RobotDiagnosticsGUI QApplication
      */
-    void createAndRunRobotDiagnostics(int argc, char** argv);
+    void createAndRunRobotDiagnosticsGUI(int argc, char** argv);
 
     void onValueReceived(SensorMsg sensor_msg) override;
 
     std::thread run_robot_diagnostics_thread;
     std::shared_ptr<std::promise<void>> termination_promise_ptr;
 
-    // Buffers that are shared with the instance of the RobotDiagnostics so that data can
+    // Buffers that are shared with the instance of the RobotDiagnosticsGUI so that data can
     // be passed safely
     std::shared_ptr<ThreadSafeBuffer<SensorMsg>> sensor_msg_buffer;
     std::shared_ptr<ThreadSafeBuffer<std::unique_ptr<Primitive>>> primitive_buffer;
