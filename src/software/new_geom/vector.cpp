d1ab8117bca62163b4e46aeede57084140e47de0
kelvinkoon <kkoon@shaw.ca>
2020-07-03 19:22:12 -0700

    Add normalize comment for error

--- src/software/new_geom/vector.cpp
+++ src/software/new_geom/vector.cpp
@@ -53,7 +53,7 @@ Vector Vector::normalize() const
 {
     // 2 * FIXED_EPSILON for error needed:
     // - mathcalls `hypot` from `length` function call
-    // TODO probably needed because input in test is already FIXED_EPSILON
+    // - where FIXED_EPSILON is used to initialize the vector
     return length() < 2 * FIXED_EPSILON ? Vector() : Vector(x_ / length(), y_ / length());
 }
 
