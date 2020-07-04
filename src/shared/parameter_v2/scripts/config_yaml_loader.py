bcb0621c935eafdabdf8c7a0a0aa5f81f4e1a504
akhilveeraghanta <akhil.veeraghanta@outlook.com>
2020-07-03 18:33:05 -0700

    yaml-loader done pending pytest

--- src/shared/parameter_v2/scripts/config_yaml_loader.py
+++ src/shared/parameter_v2/scripts/config_yaml_loader.py
@@ -1,16 +1,16 @@
-import sys
 import os
+import sys
 
+import networkx
 import yaml
 from colorama import Fore, init
 from jsonschema import validate
 
 from dynamic_parameter_schema import (
-    PARAM_DEF_SCHEMA,
     INCLUDE_DEF_SCHEMA,
+    PARAM_DEF_SCHEMA,
     SUPPORTED_TYPES,
 )
-
 
 #######################################################################
 #                         Config Yaml Loader                          #
@@ -22,7 +22,7 @@ class ConfigYamlLoader(object):
     def get_config_metadata(yaml_paths):
         """Loads the yamls in the root config directory and verifies that the
         requested configuration is valid. Then returns config_metadata dict
-        with a specific format captuing all the requested params and configs.
+        with a specified format captuing all the requested params and configs.
 
         Example config directory:
         .
@@ -43,15 +43,16 @@ class ConfigYamlLoader(object):
 
         - All yamls are properly formed (i.e proper syntax, YAML Version 1.2)
         - All parameter definitions abide by the dynamic parameter schema
-        - "include" statements do NOT cause cycles in configs
+        - All include statements do NOT cause cycles in configs
 
         :param yaml_paths: the path to all the config yamls
         :type yaml_paths: list of str
+
         :returns config_metadata: the config name followed by a list of
-            included configs and a list of parameters.
+            included configs and a list of defined parameters.
         {
-            "ai": {
-                "includes": ["passing", "plays"],
+            "ai.yaml": {
+                "includes": ["passing.yaml", "plays.yaml"],
                 "parameters" :
                 [
                     "int": {
@@ -61,118 +62,168 @@ class ConfigYamlLoader(object):
                 ]
             },
 
-            "plays": {
-                "includes": ["defense", "offense"],
+            "plays.yaml": {
+                "includes": ["defense.yaml", "offense.yaml"],
                 ...
             }
             ...
         }
 
-        :rtype config_metadata: dict
-
-        """
-        # load yaml into raw_configs
-        raw_configs = ConfigYamlLoader.__load_yaml_into_dict(yaml_paths)
-
-        # validate schema
-        ConfigYamlLoader.__validate_raw_loaded_config(raw_configs)
-
-        # strip yaml extensions from include statements
-        processed_configs = {}
-
-        for raw_config in raw_configs.values():
-            __import__('pprint').pprint(raw_config)
-
-    @staticmethod
-    def __load_yaml_into_dict(yaml_paths):
-        """Loads the yamls into an dictionary, after validating with a schema.
-        Any errors while validating will raise to the main thread.
-
-        Each file will be loaded into a new config entry as follows:
-
-        :param yaml_paths: the path to all the config yamls
-        :type yaml_paths: list of str
-        :returns: Dictionary representing the data to generate
         :rtype: dict
 
         """
-        raw_configs = {}
+        # load yaml into config_metadata
+        config_metadata = ConfigYamlLoader.__load_yaml_into_dict(yaml_paths)
+
+        # validate schema
+        ConfigYamlLoader.__validate_config_metadata(config_metadata)
+
+        # detect cycles
+        ConfigYamlLoader.__detect_cycles_in_config_metadata(config_metadata)
+
+        # valid config ready to be generated
+        return config_metadata
+
+    @staticmethod
+    def __load_yaml_into_dict(yaml_paths):
+        """Loads the yamls into an dictionary. Any errors while in the yaml
+        syntax will raise to the main thread.
+
+        :raises: ConfigYamlMalformed
+        :param yaml_paths: the path to all the config yamls
+        :type yaml_paths: list of str
+        :returns: config_medata dict representing the data to generate
+        :rtype: dict
+
+        """
+        raw_config_metadata = {}
 
         for filename in yaml_paths:
             with open(filename, "r") as param_yaml:
 
                 try:
                     # extract config name from filename
-                    head, tail = os.path.split(filename)
-                    config_name = tail.split(".")[0]
+                    _, tail = os.path.split(filename)
 
                     # safe load yaml into dictionary
-                    raw_configs[config_name] =\
-                        list(yaml.safe_load_all(param_yaml))
+                    raw_config_metadata[tail] = list(
+                        yaml.safe_load_all(param_yaml))
 
                 except yaml.YAMLError as ymle:
                     raise ConfigYamlMalformed(
-                        "Check malformed {} \n {}".format(filename, ymle)
+                        "Check malformed {} \n {}".format(tail, ymle)
                     )
 
-        return raw_configs
+        return raw_config_metadata
 
     @staticmethod
-    def __validate_raw_loaded_config(raw_configs):
-        """Validates the "raw config" that was loaded with against
+    def __validate_config_metadata(config_metadata):
+        """Validates the config_metadata that was loaded with against
         the INCLUDE_DEF_SCHEMA and PARAM_DEF_SCHEMA
 
-        :param raw_configs: The output of __load_yaml_into_dict 
-        :type raw_configs: dict
-        :returns: None
+        :raises: ConfigYamlMalformed
+        :param config_metadata: Metadata describing params and config includes
+        :type config_metadata: dict
 
         """
-        # validate schema
-        for raw_config in raw_configs.values():
 
-            if len(raw_config) == 1:
+        def validate_include(config_metadata, config_file, include_dict):
+            # check schema
+            validate(include_dict, INCLUDE_DEF_SCHEMA)
 
-                # includes only
-                if isinstance(raw_config[0], dict):
-                    validate(raw_config[0], INCLUDE_DEF_SCHEMA)
-
-                # parameter definitions only
-                if isinstance(raw_config[0], list):
-                    validate(raw_config[0], PARAM_DEF_SCHEMA)
-
-            elif len(raw_config) == 2:
-
-                # includes and parameter definitions only
-                validate(raw_config[0], INCLUDE_DEF_SCHEMA)
-                validate(raw_config[1], PARAM_DEF_SCHEMA)
-
-                # This is an ugly artifact of how the yaml is loaded
-                # we are extracting all the requested types to check that
-                # they are all supported. This is the one thing the schema
-                # can't catch that we would like to check
-                requested_types = [
-                    key[0] for key in [list(entry.keys()) for entry in raw_config[1]]
-                ]
-
-                for requested_type in requested_types:
-                    if requested_type not in SUPPORTED_TYPES:
-
-                        raise ConfigYamlMalformed(
-                            "{} type unsupported".format(requested_type)
-                        )
-            else:
+            # check duplicates
+            if len(include_dict["include"]) > len(set(include_dict["include"])):
                 raise ConfigYamlMalformed(
-                    "More than 2 yaml documents found in one file, check format"
+                    "Duplicate include detected in {}".format(config_file)
                 )
 
-    def __detect_cycles_in_config_medata(arg1):
-        """TODO: Docstring for __detect_cycles_in_config_medata.
+            # check that included yaml is defined elsewhere
+            for included_yaml in include_dict["include"]:
+                if included_yaml not in config_metadata.keys():
+                    raise ConfigYamlMalformed(
+                        "{} config definition could not be found".format(
+                            included_yaml)
+                    )
 
-        :param arg1: TODO
-        :returns: TODO
+        def validate_params(config_metadata, config_file, parameter_list):
+            # check schema
+            validate(parameter_list, PARAM_DEF_SCHEMA)
+
+            # get all parameter name list
+            param_names = [
+                list(param_entry.values())[0]["name"] for param_entry in parameter_list
+            ]
+
+            # check duplicates
+            if len(param_names) > len(set(param_names)):
+                raise ConfigYamlMalformed(
+                    "Duplicate parameter detected in {}".format(config_file)
+                )
+
+            # This is an ugly artifact of how the yaml is loaded
+            # we are extracting all the requested types to check that
+            # they are all supported. This is the one thing the schema
+            # can't catch that we would like to check
+            requested_types = [
+                key[0] for key in [list(entry.keys()) for entry in parameter_list]
+            ]
+
+            # check if type requested is supported
+            for requested_type in requested_types:
+                if requested_type not in SUPPORTED_TYPES:
+
+                    raise ConfigYamlMalformed(
+                        "{} type unsupported".format(requested_type)
+                    )
+
+        # validate schema
+        for config_file, metadata in config_metadata.items():
+
+            if len(metadata) == 1:
+
+                # include only
+                if isinstance(metadata[0], dict):
+                    validate_include(config_metadata, config_file, metadata[0])
+
+                # parameter definitions only
+                if isinstance(metadata[0], list):
+                    validate_params(config_metadata, config_file, metadata[0])
+
+            elif len(metadata) == 2:
+
+                # include and parameters
+                validate_include(config_metadata, config_file, metadata[0])
+                validate_params(config_metadata, config_file, metadata[1])
+
+            else:
+                raise ConfigYamlMalformed(
+                    "More than 2 yaml documents found in a file, check format"
+                )
+
+    @staticmethod
+    def __detect_cycles_in_config_metadata(config_metadata):
+        """Creates a DiGraph from all the includes and checks if there are cycles.
+        Raises to the main thread if a cycle is detected 
+
+        :raises: ConfigYamlCycleDetected
+        :param config_metadata: Metadata describing params and config includes
+        :type config_metadata: dict
 
         """
-        pass
+        edges = []
+
+        for config, metadata in config_metadata.items():
+            if "include" in metadata[0]:
+                for included_config in metadata[0]["include"]:
+                    edges.append((config, included_config))
+
+        G = networkx.DiGraph(edges)
+
+        for cycle in networkx.simple_cycles(G):
+            raise ConfigYamlCycleDetected(
+                "Cycle detected in the include statements: "
+                + " -> ".join(cycle + [cycle[0]])
+            )
 
 
 #######################################################################
@@ -181,9 +232,9 @@ class ConfigYamlLoader(object):
 
 
 class ConfigYamlException(Exception):
-    """We have custom exceptions to facilitate terminating the program
-    using the ConfigYamlLoader and print the log in red, making it more
-    visible in the build log clutter.
+    """We have custom exceptions to facilitate terminating the genrule
+    using the ConfigYamlLoader class and to print the log in red, making
+    it more visible in the build log clutter.
 
     :param message: The exception msg to print in red
     :type message: str
@@ -196,11 +247,9 @@ class ConfigYamlException(Exception):
 
 
 class ConfigYamlCycleDetected(ConfigYamlException):
-    """Indicates when there is a cycle in the included configs.
+    """Indicates when there is a cycle in the included configs
     """
 
-
 class ConfigYamlMalformed(ConfigYamlException):
-    """Indicates when there is a typo in a specific config or if the config
-    doesn't follow proper Yaml syntax.
+    """Indicates when the yaml has a typo or doesn't follow proper syntax
     """
