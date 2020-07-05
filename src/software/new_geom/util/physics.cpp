eddb7bc5060715aac808909298b02ba0aef5b0c7
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2020-07-04 18:10:08 -0700

    add physics tests

--- src/software/new_geom/util/physics.cpp
+++ src/software/new_geom/util/physics.cpp
@@ -1,23 +1,23 @@
 #include "software/new_geom/util/physics.h"
 
 Point calculateFuturePosition(const Point &initial_position,
-                              const Vector &intial_velocity, const Vector &acceleration,
+                              const Vector &initial_velocity, const Vector &acceleration,
                               double time_in_future)
 {
     // Using second equation of motion from https://physics.info/motion-equations/
     // ∆s = v0t + ½at2
-    double x1 = intial_velocity.x() * time_in_future +
+    double x1 = initial_position.x() + initial_velocity.x() * time_in_future +
                 0.5 * acceleration.x() * pow(time_in_future, 2);
-    double y1 = intial_velocity.y() * time_in_future +
+    double y1 = initial_position.y() + initial_velocity.y() * time_in_future +
                 0.5 * acceleration.y() * pow(time_in_future, 2);
 
     return Point(x1, y1);
 }
 
-Vector calculateFutureVelocity(const Vector &intial_velocity, const Vector &acceleration,
+Vector calculateFutureVelocity(const Vector &initial_velocity, const Vector &acceleration,
                                double time_in_future)
 {
-    double vx1 = intial_velocity.x() + time_in_future * acceleration.x();
-    double vy1 = intial_velocity.y() + time_in_future * acceleration.y();
+    double vx1 = initial_velocity.x() + time_in_future * acceleration.x();
+    double vy1 = initial_velocity.y() + time_in_future * acceleration.y();
     return Vector(vx1, vy1);
 }
--- src/software/new_geom/util/physics_test.cpp
+++ src/software/new_geom/util/physics_test.cpp
@@ -2,4 +2,55 @@
 
 #include <gtest/gtest.h>
 
-TEST(PhysicsUtilTest, test_calculate_future_position) {}
+#include "software/test_util/test_util.h"
+
+TEST(PhysicsUtilTest, test_opposing_acceleration)
+{
+    Point initial_position(2, 3);
+    Vector initial_velocity(-1, -1);
+    Vector acceleration(1, 1);
+
+    Point expected_p(1.5, 2.5);
+    Vector expected_v(0, 0);
+
+    EXPECT_TRUE(TestUtil::equalWithinTolerance(
+        expected_p,
+        calculateFuturePosition(initial_position, initial_velocity, acceleration, 1.0),
+        0.001));
+    EXPECT_TRUE(TestUtil::equalWithinTolerance(
+        expected_v, calculateFutureVelocity(initial_velocity, acceleration, 1.0), 0.001));
+}
+
+TEST(PhysicsUtilTest, test_supporting_acceleration)
+{
+    Point initial_position(3, 1);
+    Vector initial_velocity(1, -2);
+    Vector acceleration(1, -2);
+
+    Point expected_p(3.625, -0.25);
+    Vector expected_v(1.5, -3);
+
+    EXPECT_TRUE(TestUtil::equalWithinTolerance(
+        expected_p,
+        calculateFuturePosition(initial_position, initial_velocity, acceleration, 0.5),
+        0.001));
+    EXPECT_TRUE(TestUtil::equalWithinTolerance(
+        expected_v, calculateFutureVelocity(initial_velocity, acceleration, 0.5), 0.001));
+}
+
+TEST(PhysicsUtilTest, test_sideways_acceleration)
+{
+    Point initial_position(3, -1.5);
+    Vector initial_velocity(1.5, -2);
+    Vector acceleration(4, 2);
+
+    Point expected_p(19.25, -0.25);
+    Vector expected_v(11.5, 3);
+
+    EXPECT_TRUE(TestUtil::equalWithinTolerance(
+        expected_p,
+        calculateFuturePosition(initial_position, initial_velocity, acceleration, 2.5),
+        0.001));
+    EXPECT_TRUE(TestUtil::equalWithinTolerance(
+        expected_v, calculateFutureVelocity(initial_velocity, acceleration, 2.5), 0.001));
+}
