78f43a787bcaa90691c32074f03ce68c3dedfbde
Mathew MacDougall <mmacd97@gmail.com>
2020-07-07 11:02:00 -0700

    renamed gamestate variables to game_state

--- src/software/gui/generic_widgets/play_info/play_info_widget.cpp
+++ src/software/gui/generic_widgets/play_info/play_info_widget.cpp
@@ -4,7 +4,7 @@ PlayInfoWidget::PlayInfoWidget(QWidget* parent) : QTextEdit(parent) {}
 
 void PlayInfoWidget::updatePlayInfo(const PlayInfo& play_info)
 {
-    QString refbox_gamestate_string =
+    QString refbox_game_state_string =
         QString("Refbox Gamestate: %1\n")
             .arg(QString::fromStdString(play_info.getRefboxGameStateName()));
     QString play_name_string =
@@ -17,7 +17,7 @@ void PlayInfoWidget::updatePlayInfo(const PlayInfo& play_info)
 
     QString play_info_string =
         QString("%1\n%2\n%3")
-            .arg(refbox_gamestate_string, play_name_string, tactics_string);
+            .arg(refbox_game_state_string, play_name_string, tactics_string);
 
     setText(play_info_string);
 }
