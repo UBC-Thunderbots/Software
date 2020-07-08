84f22bf6e57e0b5eded9f603f5616fab3c87a1dd
akhilveeraghanta <akhil.veeraghanta@outlook.com>
2020-07-08 09:19:24 -0700

    wip

---
+++ src/shared/parameter_v2/scripts/c/c_config.py
@@ -0,0 +1,102 @@
+from c_parameter import CParameter
+from typing import List, Set
+
+#######################################################################
+#                              C Config                               #
+#######################################################################
+
+
+class CConfig(object):
+
+    DEFINITION = "typedef struct {name}_s {{{contents}}} {name}_t;"
+    MALLOC = "{name}_t* {name}_config = ({name}_t*)malloc(sizeof({name}_t}));"
+    INITIALIZATION = "{name}_t {name}_init = {{{contents}}};"
+    MEMCPY = "memcpy({name}_config, &{name}_init, sizeof({name_t}));"
+    FREE = "free((void*){ptr_to_instance})"
+
+    def __init__(self, config_name: str, ptr_to_instance: str):
+        """Initializes a CParameter with the given name. The corresponding
+        generation strings (definition, malloc, initialization, memcpy, free)
+        are available from read-only properties.
+
+        :param config_name: The name of the config, the filename of the yaml
+        :param ptr_to_instance: A string representation of where this config
+            is located (ex: ThunderbotsConfig->FooConfig)
+        :type param_value: str
+        :type ptr_to_instance: str
+
+        """
+        self.config_name = config_name
+        self.ptr_to_instance = ptr_to_instance
+
+        self.parameters: Set[CParameter] = set()
+        self.configs: Set[CConfig] = set()
+
+    def add_parameter(self, parameter: CParameter):
+        """Add a parameter to this config to generate.
+
+        :param parameter: The CParameter to add to this config
+        :type parameter: CParameter
+
+        """
+        self.parameters.add(parameter)
+
+    def add_config(self, config: CConfig):
+        """Add a config to this config to generate.
+
+        :param config: The CConfig to add to this CConfig
+        :type config: CConfig
+
+        """
+        self.configs.add(config)
+
+    @property
+    def definition(self):
+        """Gets the 'contents' to format the DEFINITION string.
+        Joins all the nested configs definitions and the parameter
+        definitions.
+
+        :returns: value of the property
+        :rtype: str
+
+        """
+        definition_contents =\
+            "\n".join([config.definition for config in self.configs]) +\
+            "\n".join([parameter.definition for parameter in self.configs])
+
+        return CConfig.DEFINITION.format(
+            name=self.config_name,
+            contents=definition_contents
+        )
+
+    @property
+    def initialization(self):
+        """Gets the 'contents' to format the INITIALIZATION string.
+        Joins all the nested configs initializations and the parameter
+        initializations.
+
+        :returns: value of the property
+        :rtype: str
+
+        """
+
+        initialization_contents =\
+            "\n".join([config.initialization for config in self.configs]) +\
+            "\n".join([parameter.initialization for parameter in self.configs])
+
+        return CConfig.INITIALIZATION.format(
+            name=self.config_name,
+            contents=initialization_contents
+        )
+
+    @property
+    def malloc(self):
+        return CConfig.MALLOC.format(name=self.config_name)
+
+    @property
+    def memcpy(self):
+        return CConfig.MALLOC.format(name=self.config_name)
+
+    @property
+    def free(self):
+        return CConfig.FREE.format(name=self.ptr_to_instance)
---
+++ src/shared/parameter_v2/scripts/c/c_parameter.py
@@ -0,0 +1,60 @@
+
+#######################################################################
+#                             C Parameter                             #
+#######################################################################
+
+
+class CParameter(object):
+
+    DEFINITION = "{{const {type}_t* {name}}};"
+    CONSTRUCTOR = "app_dynamic_parameters_create{type}({value});"
+    INITIALIZATION = ".{name} = {constructor},"
+    DESTRUCTOR = "app_dynamic_parameters_destroy{type}({ptr})"
+
+    def __init__(self, param_name: str, param_type: str,
+                 param_value: str, ptr_to_instance: str):
+        """Initializes a CParameter with the given type and value. The
+        corresponding generation strings (definition, constructor, destructor)
+        are available through read-only properties.
+
+        :param param_name: The name of the parameter, as defined in the yaml
+        :param param_type: The psudo-class type of the parameter (without "_t")
+        :param param_value: The constant value the parameter should hold
+        :param ptr_to_instance: A string representation of where this parameter
+            is located (ex: FooConfig->foo_int)
+        :type param_name: str
+        :type param_type: str
+        :type param_value: str
+        :type ptr_to_instance: str
+
+        """
+
+        self.param_name = param_name
+        self.param_type = param_type
+        self.param_value = param_value
+        self.ptr_to_instance = ptr_to_instance
+
+        self.definition =\
+            CParameter.DEFINITION.format(param_type, param_name)
+        self.constructor =\
+            CParameter.CONSTRUCTOR.format(param_type, param_value)
+        self.destructor =\
+            CParameter.DESTRUCTOR.format(param_type, ptr_to_instance)
+        self.initialization =\
+            CParameter.INITIALIZATION.format(param_name, self.constructor)
+
+    @property
+    def definition(self):
+        return self.definition
+
+    @property
+    def constructor(self):
+        return self.constructor
+
+    @property
+    def destructor(self):
+        return self.destructor
+
+    @property
+    def initialization(self):
+        return self.initialization
