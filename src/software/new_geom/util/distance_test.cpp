c704ffdac6e24f5113816316349474209a3ffb22
kelvinkoon <kkoon@shaw.ca>
2020-07-16 10:23:46 -0700

    Revert test name

--- src/software/new_geom/util/distance_test.cpp
+++ src/software/new_geom/util/distance_test.cpp
@@ -104,7 +104,7 @@ TEST(DistanceTest, point_off_segment_closest_to_segment_end)
     EXPECT_DOUBLE_EQ(distance(s, p), expected);
 }
 
-TEST(DistanceTest, point_off_almost_degenerate_segment)
+TEST(DistanceTest, point_off_degenerate_segment)
 {
     Point p(5, 3);
     Segment s(Point(-2, 2), Point(-2, 2));
