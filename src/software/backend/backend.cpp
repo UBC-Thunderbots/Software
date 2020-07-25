8040d4e300b216625fb9d696f6cc7549a8df86d0
Jordan Liu <jordan.zliu@gmail.com>
2020-07-24 20:02:21 -0700

    timestamp based playback + refbox in test messages

--- src/software/backend/backend.cpp
+++ src/software/backend/backend.cpp
@@ -1,9 +1,11 @@
 #include "software/backend/backend.h"
+#include "software/proto/message_translation/tbots_protobuf.h"
 
 void Backend::receiveRobotStatus(TbotsProto::RobotStatus msg)
 {
     SensorProto sensor_msg;
     *(sensor_msg.add_robot_status_msgs()) = msg;
+    (*sensor_msg.mutable_time_received()) = *createCurrentTimestamp();
     Subject<SensorProto>::sendValueToObservers(sensor_msg);
 }
 
@@ -11,6 +13,7 @@ void Backend::receiveSSLWrapperPacket(SSLProto::SSL_WrapperPacket msg)
 {
     SensorProto sensor_msg;
     *(sensor_msg.mutable_ssl_vision_msg()) = msg;
+    (*sensor_msg.mutable_time_received()) = *createCurrentTimestamp();
     Subject<SensorProto>::sendValueToObservers(sensor_msg);
 }
 
@@ -18,5 +21,6 @@ void Backend::receiveSSLReferee(SSLProto::Referee msg)
 {
     SensorProto sensor_msg;
     *(sensor_msg.mutable_ssl_referee_msg()) = msg;
+    (*sensor_msg.mutable_time_received()) = *createCurrentTimestamp();
     Subject<SensorProto>::sendValueToObservers(sensor_msg);
 }
--- src/software/backend/replay_backend.h
+++ src/software/backend/replay_backend.h
@@ -2,8 +2,8 @@
 #include "shared/proto/tbots_robot_msg.pb.h"
 #include "shared/proto/tbots_software_msgs.pb.h"
 #include "software/backend/backend.h"
-#include "software/backend/ssl_proto_client.h"
 #include "software/backend/replay_logging/replay_reader.h"
+#include "software/backend/ssl_proto_client.h"
 #include "software/networking/threaded_proto_multicast_listener.h"
 #include "software/networking/threaded_proto_multicast_sender.h"
 #include "software/parameter/dynamic_parameters.h"
@@ -11,10 +11,12 @@
 class ReplayBackend : public Backend
 {
    public:
-    explicit ReplayBackend(std::shared_ptr<const ReplayBackendConfig> replay_backend_config =
-                    DynamicParameters->getReplayBackendConfig());
+    explicit ReplayBackend(
+        std::shared_ptr<const ReplayBackendConfig> replay_backend_config =
+            DynamicParameters->getReplayBackendConfig());
 
     static const std::string name;
+
    private:
     void onValueReceived(ConstPrimitiveVectorPtr primitives) override;
     void onValueReceived(World world) override;
@@ -22,5 +24,7 @@ class ReplayBackend : public Backend
 
     ReplayReader replay_reader;
     std::thread pull_from_replay_thread;
+    std::optional<std::chrono::duration<double>> last_msg_received_time;
+    std::optional<std::chrono::time_point<std::chrono::steady_clock>>
+        last_msg_replayed_time;
 };
-
