09787a0a9af68bb0dc0ef57ebe06fc93549380f1
Mathew MacDougall <mmacd97@gmail.com>
2020-08-14 10:01:46 -0700

    Updated the comments in stp.h to describe how parameters get updated, and the overall flow of control through the STP framework

--- src/software/ai/hl/stp/stp.h
+++ src/software/ai/hl/stp/stp.h
@@ -11,6 +11,65 @@
 /**
  * The STP module is an implementation of the high-level logic Abstract class, that
  * uses the STP (Skills, Tactics, Plays) framework for its decision making.
+ *
+ * See play.h, tactic.h, and action.h for more specific documentation on
+ * each component.
+ *
+ * Explaining WorldParams and ControlParams
+ * - WorldParams are any parameters that can be derived from the World
+ *   class (eg. the ball, field) that are not related to gameplay logic.
+ *   In the case of Tactics and Actions, because these parameters are not
+ *   related to gameplay logic, we can automatically update them from STP.
+ *   This means the Plays that call Tactics, and Tactics that call Actions,
+ *   need to provide fewer parameters which overall makes the code a bit
+ *   easier to read.
+ * - ControlParams are any parameters that are related to gameplay logic.
+ *   Some examples are:
+ *   - The EnemyThreat datastructure
+ *   - A Pass datastructure
+ *   - A position on the field
+ *   - How far to chip the ball
+ *   These parameters can NOT be automatically updated because they are specific
+ *   to the Play or Tactic that is providing them. Therefore, Plays update the
+ *   ControlParams of Tactics, and Tactics update the ControlParams of Actions.
+ *   These are the parameters that actually change their behaviour.
+ * - NOTE: In the high-level overview below, the ControlParams and WorldParams
+ *   are both updated before something is run (eg. the Control and WorldParams
+ *   for the Tactics are both updated before the Tactics are run), so all
+ *   necessary information is provided before gameplay decisions are made.
+ *
+ * HIGH-LEVEL OVERVIEW
+ * STP's main job is to decide what each robot should be doing at any point
+ * in time (ie. what Intent each robot should be running). When STP is given
+ * a new World and asked for Intents, it goes through the following steps:
+ *
+ * 1. Decide what Play should be running at this time. If the currently running
+ *    Play's invariant condition is still valid, that play is chosen to keep running.
+ *    If the invariant is no longer true, the play is stopped and a new Play whose
+ *    applicable condition is true will be chosen and started.
+ *
+ *    Alltogether it's this constant switching of Plays in reaction to the current
+ *    state of the world that makes our AI "play soccer".
+ *
+ * 2. Run the current Play to get the Tactics should be run at this time.
+ *    This will update the ControlParams for the tactics.
+ *    NOTE: At this time no particular robot is assigned to run any particular tactic.
+ *    We just know WHAT tactics should be run, not what robot is running them.
+ *
+ * 3. Assign robots to tactics. Tactics assign a "cost" to each robot based on internal
+ *    criteria, and then we attempt to assign robots in such a way that minimizes the
+ *    global cost.
+ *
+ * 4. Update the WorldParams for the tactics.
+ *
+ * 5. Run each tactic to get the Action that each robot should run.
+ *    This will update the ControlParams for each Action.
+ *
+ * 6. Update the "WorldParams" for the Actions
+ *
+ * 7. Run each Action to get the Intent that each robot should run.
+ *
+ * 8. Return this list of Intents
  */
 class STP : public HL
 {
