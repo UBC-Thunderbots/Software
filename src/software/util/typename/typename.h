11d0dd35c2336ad589ec8ccbcd44ade70f82cc3c
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2020-08-15 16:20:47 -0700

    added comments

--- src/software/util/typename/typename.h
+++ src/software/util/typename/typename.h
@@ -1,4 +1,11 @@
 #include <cxxabi.h>
 
+/**
+ * MACRO to get typename of the object, i.e.
+ * ClassA object_a;
+ * TYPENAME(object_a); // returns ClassA
+ *
+ * @param object The object to get typename for
+ */
 #define TYPENAME(object)                                                                 \
     (static_cast<std::string>(abi::__cxa_demangle(typeid(object).name(), 0, 0, 0)))
