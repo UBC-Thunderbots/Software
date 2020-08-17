afb29baf5f018d92be0b2b808b7a002fc4ce1316
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2020-08-17 09:24:57 -0700

    Fixed memory leak

--- src/software/util/typename/typename.h
+++ src/software/util/typename/typename.h
@@ -1,11 +1,22 @@
 #include <cxxabi.h>
 
+#include <memory>
+
 /**
  * MACRO to get typename of the object, i.e.
+ * ```
  * ClassA object_a;
  * TYPENAME(object_a); // returns ClassA
+ * ```
  *
  * @param object The object to get typename for
+ *
+ * @return the string representation of the object
  */
-#define TYPENAME(object)                                                                 \
-    (static_cast<std::string>(abi::__cxa_demangle(typeid(object).name(), 0, 0, 0)))
+template <class T>
+std::string TYPENAME(const T& object)
+{
+    std::unique_ptr<char, void (*)(void*)> demangled_name_ptr{
+        abi::__cxa_demangle(typeid(object).name(), NULL, NULL, NULL), std::free};
+    return std::string(demangled_name_ptr.get());
+}
