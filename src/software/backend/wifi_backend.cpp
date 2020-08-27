277290dc7fe7dd816e8372504b149d620a89aded
Mathew MacDougall <mmacd97@gmail.com>
2020-08-27 11:09:24 -0700

    fixed formatting

--- src/software/backend/wifi_backend.cpp
+++ src/software/backend/wifi_backend.cpp
@@ -37,8 +37,9 @@ void WifiBackend::onValueReceived(TbotsProto::PrimitiveSet primitives)
 
     if (sensor_fusion_config->OverrideGameControllerDefendingSide()->value())
     {
-        defending_side_output->sendProto(
-            *createDefendingSide(sensor_fusion_config->DefendingPositiveSide()->value() ? FieldSide::POS_X : FieldSide::NEG_X));
+        defending_side_output->sendProto(*createDefendingSide(
+            sensor_fusion_config->DefendingPositiveSide()->value() ? FieldSide::POS_X
+                                                                   : FieldSide::NEG_X));
     }
     else
     {
