d8d52cbb618d460d7af6852f432f54db1e3d0521
Mathew <mmacd97@gmail.com>
2020-08-28 07:42:44 -0700

    Update src/software/ai/hl/stp/tactic/passer_tactic.cpp
    Co-authored-by: Cody Li <licody2000@gmail.com>

--- src/software/ai/hl/stp/tactic/passer_tactic.cpp
+++ src/software/ai/hl/stp/tactic/passer_tactic.cpp
@@ -40,7 +40,7 @@ void PasserTactic::calculateNextAction(ActionCoroutine::push_type& yield)
 {
     // If the ball is moving, we are likely already in a live game scenario and
     // so we need to collect the ball before we can pass. If the ball is not moving,
-    // we are likely in a set play and so don't need to initially collect the ball
+    // we are likely in a set play and so we don't need to initially collect the ball
     if (ball.velocity().length() > INTERCEPT_BALL_SPEED_THRESHOLD)
     {
         auto intercept_action = std::make_shared<InterceptBallAction>(field, ball, false);
