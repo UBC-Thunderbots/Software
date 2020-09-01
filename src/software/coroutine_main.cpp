a5d3a09c6ec6b13bd9a8e84a129ec2fa84b6f08c
Mathew MacDougall <mmacd97@gmail.com>
2020-09-01 16:44:35 -0700

    cleaned up test cases and revised conclusions

--- src/software/coroutine_main.cpp
+++ src/software/coroutine_main.cpp
@@ -181,7 +181,7 @@ void variablesOnHeapNoMove()
     // 8
 }
 
-void variablesOnStackWithMove()
+void moveFromStackToStack()
 {
     std::cout << std::endl << std::endl;
     std::cout << "Test on the stack with moving variables" << std::endl;
@@ -224,45 +224,7 @@ void variablesOnStackWithMove()
     // -15
 }
 
-void variablesOnStackWithMoveVector()
-{
-    std::cout << std::endl << std::endl;
-    std::cout << "Test on the stack with moving vectors" << std::endl;
-
-    auto data = std::make_shared<int>(99);
-
-    std::vector<FunctionMemberObject> fmo_vector;
-    fmo_vector.push_back(std::move(FunctionMemberObject(print_data_func, data)));
-    std::vector<DataMemberObject> dmo_vector;
-    dmo_vector.push_back(std::move(DataMemberObject(print_data_func, data)));
-    std::vector<NoMemberObject> nmo_vector;
-    nmo_vector.push_back(std::move(NoMemberObject(print_data_func, data)));
-
-    //    fmo_vector.at(0).execute();
-    //    dmo_vector.at(0).execute();
-    nmo_vector.at(0).execute();
-
-    *data = -3;
-
-    //    fmo_vector.at(0).execute();
-    //    dmo_vector.at(0).execute();
-    nmo_vector.at(0).execute();
-
-    // Output
-    // std::bad_function_call / stops all printouts
-    // 0 (seems to be default-constructed data)
-    // 99
-    // std::bad_function_call / stops all printouts
-    // 0 (seems to be default-constructed data)
-    // -3
-
-    // Why does this not work for the DataMemberObject when it works
-    // just moving variables??? In either case the variable should be
-    // moved, so why is this different? The vector calling the
-    // default constructor somewhere I'm not expecting?
-}
-
-void variablesOnHeapWithMove()
+void moveFromHeapToHeap()
 {
     std::cout << std::endl << std::endl;
     std::cout << "Test on the heap with moving variables" << std::endl;
@@ -305,77 +267,7 @@ void variablesOnHeapWithMove()
     // 4
 }
 
-void variablesOnHeapWithMoveVectors()
-{
-    std::cout << std::endl << std::endl;
-    std::cout << "Test on the heap with moving vectors" << std::endl;
-
-    auto data = std::make_shared<int>(99);
-
-    std::vector<std::unique_ptr<FunctionMemberObject>> fmo_vector;
-    fmo_vector.push_back(std::make_unique<FunctionMemberObject>(print_data_func, data));
-    std::vector<std::unique_ptr<DataMemberObject>> dmo_vector;
-    dmo_vector.push_back(std::make_unique<DataMemberObject>(print_data_func, data));
-    std::vector<std::unique_ptr<NoMemberObject>> nmo_vector;
-    nmo_vector.push_back(std::make_unique<NoMemberObject>(print_data_func, data));
-
-    fmo_vector.at(0)->execute();
-    dmo_vector.at(0)->execute();
-    nmo_vector.at(0)->execute();
-
-    *data = 1;
-
-    fmo_vector.at(0)->execute();
-    dmo_vector.at(0)->execute();
-    nmo_vector.at(0)->execute();
-
-    // Output
-    // 99
-    // 99
-    // 99
-    // 1
-    // 1
-    // 1
-}
-
-void variablesOnHeapWithMoveVectors2()
-{
-    std::cout << std::endl << std::endl;
-    std::cout << "Test on the heap with moving vectors 2" << std::endl;
-
-    auto data = std::make_shared<int>(99);
-
-    std::vector<std::unique_ptr<FunctionMemberObject>> fmo_vector;
-    auto fmo = std::make_unique<FunctionMemberObject>(print_data_func, data);
-    std::vector<std::unique_ptr<DataMemberObject>> dmo_vector;
-    auto dmo = std::make_unique<DataMemberObject>(print_data_func, data);
-    std::vector<std::unique_ptr<NoMemberObject>> nmo_vector;
-    auto nmo = std::make_unique<NoMemberObject>(print_data_func, data);
-
-    fmo->execute();
-    dmo->execute();
-    nmo->execute();
-
-    fmo_vector.push_back(std::move(fmo));
-    dmo_vector.push_back(std::move(dmo));
-    nmo_vector.push_back(std::move(nmo));
-
-    *data = 7;
-
-    fmo_vector.at(0)->execute();
-    dmo_vector.at(0)->execute();
-    nmo_vector.at(0)->execute();
-
-    // Output
-    // 99
-    // 99
-    // 99
-    // 7
-    // 7
-    // 7
-}
-
-void moveVariablesFromStackToHeap() {
+void moveFromStackToHeap() {
     std::cout << std::endl << std::endl;
     std::cout << "Test moving from stack to heap" << std::endl;
 
@@ -410,7 +302,7 @@ void moveVariablesFromStackToHeap() {
     nmo_heap->execute();
 }
 
-void moveVariablesFromHeapToStack() {
+void moveFromHeapToStack() {
     std::cout << std::endl << std::endl;
     std::cout << "Test moving from heap to stack" << std::endl;
 
@@ -420,9 +312,9 @@ void moveVariablesFromHeapToStack() {
     auto dmo_heap = std::make_unique<DataMemberObject>(print_data_func, data);
     auto nmo_heap = std::make_unique<NoMemberObject>(print_data_func, data);
 
-    FunctionMemberObject fmo(print_data_func, data);
-    DataMemberObject dmo(print_data_func, data);
-    NoMemberObject nmo(print_data_func, data);
+    std::optional<FunctionMemberObject> fmo;
+    std::optional<DataMemberObject> dmo;
+    std::optional<NoMemberObject> nmo;
 
     fmo_heap->execute();
     dmo_heap->execute();
@@ -430,19 +322,90 @@ void moveVariablesFromHeapToStack() {
 
     *data = 45;
 
-    fmo = std::move(*fmo_heap);
-    dmo = std::move(*dmo_heap);
-    nmo = std::move(*nmo_heap);
+    *fmo = std::move(*fmo_heap);
+    *dmo = std::move(*dmo_heap);
+    *nmo = std::move(*nmo_heap);
 
-    fmo.execute();
-    dmo.execute();
-    nmo.execute();
+    fmo->execute();
+    dmo->execute();
+    nmo->execute();
 
     *data = 83;
 
-    fmo.execute();
-    dmo.execute();
-    nmo.execute();
+    fmo->execute();
+    dmo->execute();
+    nmo->execute();
+}
+
+void moveStackToVector()
+{
+    std::cout << std::endl << std::endl;
+    std::cout << "Test move stack variables into vector" << std::endl;
+
+    auto data = std::make_shared<int>(99);
+
+    std::vector<FunctionMemberObject> fmo_vector;
+    fmo_vector.push_back(std::move(FunctionMemberObject(print_data_func, data)));
+    std::vector<DataMemberObject> dmo_vector;
+    dmo_vector.push_back(std::move(DataMemberObject(print_data_func, data)));
+    std::vector<NoMemberObject> nmo_vector;
+    nmo_vector.push_back(std::move(NoMemberObject(print_data_func, data)));
+
+//    fmo_vector.at(0).execute(); // BAD
+//    dmo_vector.at(0).execute(); // BAD
+    nmo_vector.at(0).execute();
+
+    *data = -3;
+
+//    fmo_vector.at(0).execute(); // BAD
+//    dmo_vector.at(0).execute(); // BAD
+    nmo_vector.at(0).execute();
+
+    // Output
+    // std::bad_function_call / stops all printouts
+    // 0 (seems to be default-constructed data)
+    // 99
+    // std::bad_function_call / stops all printouts
+    // 0 (seems to be default-constructed data)
+    // -3
+
+    // Why does this not work for the DataMemberObject when it works
+    // just moving variables??? In either case the variable should be
+    // moved, so why is this different? The vector calling the
+    // default constructor somewhere I'm not expecting?
+}
+
+void moveHeapToVector()
+{
+    std::cout << std::endl << std::endl;
+    std::cout << "Test move heap variables into vector" << std::endl;
+
+    auto data = std::make_shared<int>(99);
+
+    std::vector<std::unique_ptr<FunctionMemberObject>> fmo_vector;
+    fmo_vector.push_back(std::make_unique<FunctionMemberObject>(print_data_func, data));
+    std::vector<std::unique_ptr<DataMemberObject>> dmo_vector;
+    dmo_vector.push_back(std::make_unique<DataMemberObject>(print_data_func, data));
+    std::vector<std::unique_ptr<NoMemberObject>> nmo_vector;
+    nmo_vector.push_back(std::make_unique<NoMemberObject>(print_data_func, data));
+
+    fmo_vector.at(0)->execute();
+    dmo_vector.at(0)->execute();
+    nmo_vector.at(0)->execute();
+
+    *data = 1;
+
+    fmo_vector.at(0)->execute();
+    dmo_vector.at(0)->execute();
+    nmo_vector.at(0)->execute();
+
+    // Output
+    // 99
+    // 99
+    // 99
+    // 1
+    // 1
+    // 1
 }
 
 int main(int argc, char** argv)
@@ -475,49 +438,72 @@ int main(int argc, char** argv)
     NoMemberObject nmo_move       = std::move(nmo);
 
 
-    // ~~~~~~~~~~ Experiments and Examples ~~~~~~~~~~ //
+    // ~~~~~~~~~~ Experiments and Observations ~~~~~~~~~~ //
+    // Note: Run each of these functions independently
+    // since failures from one can affect the behaviour
+    // of later functions
+
+    /* There are no issues if no data is moved. This makes sense
+     * since this is just a basic usage of coroutines, and is a
+     * case we really expect to work.
+     */
 //    variablesOnStackNoMove();           // OK
 //    variablesOnHeapNoMove();            // OK
-//    variablesOnStackWithMove();         // OK
-//    variablesOnStackWithMoveVector();   // BAD
-//    variablesOnHeapWithMove();          // OK
-//    variablesOnHeapWithMoveVectors();   // OK
-//    variablesOnHeapWithMoveVectors2();  // OK
-//    moveVariablesFromStackToHeap(); // BAD
-moveVariablesFromHeapToStack(); // OK???
 
-    // ~~~~~~~~~~ Observations and Conclusions ~~~~~~~~~~ //
-    /* - The "coroutine stack" seems to be able to read from and interact
-     *   with the "normal stack" just fine as long as the objects do not move.
-     *   This goes for when the objects are created on the stack or heap.
+    /* Moving coroutines works fine as long as the memory
+     * is moved from the stack to the stack, of from the
+     * heap to the heap
+     */
+//    moveFromStackToStack();         // OK
+//    moveFromHeapToHeap();          // OK
+
+    /* For some reason, moving coroutines from the stack to the heap
+     * does not work in both directions. We don't have any ideas
+     * why moving from the stack to the heap causes issues, while
+     * moving from the heap to the stack does not.
+     */
+//    moveFromStackToHeap(); // BAD
+//    moveFromHeapToStack(); // OK
+
+    /* Because vectors (and other resizeable containers) actually store
+     * data on the heap, moving coroutines into vectors only works
+     * in certain cases.
      *
-     * - When objects are created on the stack, moving single variables
-     *   seems to keep everything working fine. However, moving into / out of
-     *   containers (like vectors) seems to mess things up. We don't know exactly
-     *   why containers cause things to behave differently. It could be that the
-     *   implementation of the move constructor/assignment isn't robust enough.
+     * As seen above, moving data from the heap to the heap works fine,
+     * and is also the case here when the data starts on the heap.
      *
-     * - When objects are created on the heap, moving single variables
-     *   as well as moving containers works fine. This implies that the
-     *   "coroutine stack" seems to work better when it's not closely interacting
-     *   with another "normal stack". Things generally behave better when the
-     *   object containing the coroutine is allocated on the heap.
+     * We are not sure why moving data from the stack to the vector heap
+     * does not work here, when it seems to work fine in the above test
+     * without vectors. Specifically, it fails with the FunctionMemberObject
+     * and DataMemberObject, but works fine with NoMemberObject.
+     */
+//    moveStackToVector();   // BAD
+//    moveHeapToVector();   // OK
+
+
+    // ~~~~~~~~~~ Conclusions ~~~~~~~~~~ //
+    /* - Whether a resizeable container is used or not, moving coroutines
+     *   between the stack and the heap results in inconsistent behaviour
+     *   (it works in some cases but not others, without any obviously
+     *   consistent rules). Therefore, coroutines should never be
+     *   moved between the stack and heap in any way.
      *
-     * - In all cases, avoiding storing ANY variables (other than the coroutine
-     *   object itself) on the stack (ie. the NoMemberObject) worked fine. So it
-     *   seems that as long as any variables that need to be accessed / used
-     *   "inside" the coroutine are not stored on the "normal stack"
-     *   (they can be on the heap or passed into the coroutine directly),
-     *   things will behave as expected.
+     * - In some "bad" cases, such as moveStackToVector(), the NoMemberObject
+     *   actually works fine. This seems to imply that coroutines behave better,
+     *   especially when being moved, when any data it's accessing is
+     *   passed into the coroutine when it's created so it exists on
+     *   the "coroutine stack". Therefore, prefer passing as much data
+     *   to the coroutine as possible on creation and avoid using member
+     *   variables when possible.
      *
-     *   tl;dr
-     *
-     * - Things work fine if you don't move them
-     * - Things work fine if the objects containing coroutines are
-     *   allocated on the heap
-     * - Things work fine if all values are passed directly into
-     *   the coroutine (ie. the NoMemberObject).
-     *   This is generally the best option.
+     * COROUTINES BEST PRACTICES
+     * - Avoid moving coroutines. If the absolutely must be moved,
+     *   make sure they are not moved between the stack and heap.
+     * - Avoid using coroutines with resizeable containers. If they
+     *   must be used, make sure that the coroutines are allocated
+     *   on the heap.
+     * - Pass data to the coroutine on creation as much as possible,
+     *   avoid using member variables.
      */
 
     return 0;
