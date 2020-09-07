abbc565ed032b5ff6142b8324d035ed6307084e5
licody <licody2000@gmail.com>
2020-09-07 16:08:55 -0700

    custom progress bar

--- src/software/gui/robot_diagnostics/widgets/progress_bar.cpp
+++
@@ -1,8 +0,0 @@
-#include "progress_bar.h"
-
-void updateProgressBar(QProgressBar *progress_bar, float value)
-{
-    progress_bar->setFormat(QString(progress_bar->format()).arg(value, 0, 'f', 2));
-    // we don't use this value, it's just for visuals
-    progress_bar->setValue(static_cast<int>(value));
-}
