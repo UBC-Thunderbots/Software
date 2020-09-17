83902a821bf89f418d3e355c5c71ce11605f4a69
Amr <amralmoallim@gmail.com>
2020-09-17 02:58:58 -0700

    added tests

--- src/software/ai/evaluation/detect_successful_kick_test.cpp
+++ src/software/ai/evaluation/detect_successful_kick_test.cpp
@@ -3,8 +3,9 @@
 #include <gtest/gtest.h>
 
 
-TEST(DetectSuccessfulKickTest, ball_over_speed_threshold_and_no_angle_difference_expect_success) {
+TEST(DetectSuccessfulKickTest, ball_over_speed_threshold_and_no_direction_difference_expect_success) {
     Ball ball({0, 0}, {5, 5}, Timestamp::fromSeconds(0));
+    
 
     Angle angle = angle.fromDegrees(45);
     Vector expected_direction = Vector::createFromAngle(angle);
@@ -12,20 +13,29 @@ TEST(DetectSuccessfulKickTest, ball_over_speed_threshold_and_no_angle_difference
     EXPECT_TRUE(successfulKickDetected(ball, expected_direction));
 }
 
-TEST(DetectSuccessfulKickTest, ball_over_speed_threshold_and_no_angle_difference_expect_success) {
-    Ball ball({0, 0}, {5, 5}, Timestamp::fromSeconds(0));
+TEST(DetectSuccessfulKickTest, ball_under_speed_threshold_and_no_direction_difference) {
+    Ball ball({5, 2}, {0, 0.2}, Timestamp::fromSeconds(0));
 
-    Angle angle = angle.fromDegrees(45);
+    Angle angle = angle.fromDegrees(90);
+    Vector expected_direction = Vector::createFromAngle(angle);
+
+    EXPECT_FALSE(successfulKickDetected(ball, expected_direction));
+}
+
+TEST(DetectSuccessfulKickTest, ball_over_speed_threshold_and_small_negative_direction_difference) {
+    Ball ball({0, 1}, {3, 0}, Timestamp::fromSeconds(0));
+
+    Angle angle = angle.fromDegrees(-15);
     Vector expected_direction = Vector::createFromAngle(angle);
 
     EXPECT_TRUE(successfulKickDetected(ball, expected_direction));
 }
 
-TEST(DetectSuccessfulKickTest, ball_over_speed_threshold_and_no_angle_difference_expect_success) {
-    Ball ball({0, 0}, {5, 5}, Timestamp::fromSeconds(0));
+TEST(DetectSuccessfulKickTest, ball_over_speed_threshold_and_exceeds_direction_threshold) {
+    Ball ball({-5, 2}, {-3, 0}, Timestamp::fromSeconds(0));
 
-    Angle angle = angle.fromDegrees(45);
+    Angle angle = angle.fromRadians(0);
     Vector expected_direction = Vector::createFromAngle(angle);
 
-    EXPECT_TRUE(successfulKickDetected(ball, expected_direction));
+    EXPECT_FALSE(successfulKickDetected(ball, expected_direction));
 }
