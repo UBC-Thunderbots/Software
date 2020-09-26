d1845d73ad5b1ee269a6cc6649d6e56dd88044ac
LiCody <licody2000@gmail.com>
2020-09-26 16:59:46 -0700

    add version checking

--- environment_setup/setup_software.sh
+++ environment_setup/setup_software.sh
@@ -50,8 +50,6 @@ host_software_packages=(
     protobuf-compiler # This is required for the "NanoPb" library, which does not
                       # properly manage this as a bazel dependency, so we have
                       # to manually install it ourselves
-    python-minimal # This is required for bazel, we've seen some issues where
-                   # the bazel install hasn't installed it properly
     python3       # Python 3
     python3-pip   # Required for bazel to install python dependencies for build targets
     python3-protobuf # This is required for the "NanoPb" library, which does not
@@ -62,6 +60,27 @@ host_software_packages=(
     valgrind # Checks for memory leaks
     libclang-dev # Used to verify code-generation
 )
+
+if [[ $(lsb_release -rs) == "20.04" ]]; then
+    # This is required for bazel, we've seen some issues where
+    # the bazel install hasn't installed it properly
+    host_software_packages+=(python-is-python3)
+
+    # This is to setup the toolchain for bazel to run 
+    host_software_packages+=(llvm-6.0)
+    sudo apt -y install gcc-7 g++-7
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 7
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 7
+    
+    # This adds missing c headers
+    ldconfig
+fi
+
+if [[ $(lsb_release -rs) == "18.04" ]]; then
+    # This is required for bazel, we've seen some issues where
+    # the bazel install hasn't installed it properly
+    host_software_packages+=(python-minimal)
+fi
 
 if ! sudo apt-get install "${host_software_packages[@]}" -y ; then
     echo "##############################################################"
