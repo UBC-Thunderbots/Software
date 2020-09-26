bd3507bc48f9a7eaae30274f493cb530efbf8467
Amr <amralmoallim@gmail.com>
2020-09-26 12:38:03 -0700

    moved method to evaluation/ball

--- src/software/ai/evaluation/ball.cpp
+++ src/software/ai/evaluation/ball.cpp
@@ -25,3 +25,17 @@ bool ballInEnemyCorner(const Field &field, const Ball &ball, double radius)
             (distance(ball.position(), field.enemyCornerNeg()) < radius)) &&
            field.pointInFieldLines(ball.position());
 }
+
+bool hasBallBeenKicked(const Ball& ball, const Angle& expected_kick_direction,
+                       double min_kick_speed)
+{
+    static constexpr double MAX_ANGLE_DIFFERENCE = 20;
+
+    Angle kick_orientation_difference =
+            ball.velocity().orientation().minDiff(expected_kick_direction);
+
+    return (kick_orientation_difference.abs() <
+            Angle::fromDegrees(MAX_ANGLE_DIFFERENCE) &&
+            ball.velocity().length() > min_kick_speed);
+}
+
