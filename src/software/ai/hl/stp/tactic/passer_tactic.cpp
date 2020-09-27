cb287a633033eec660b5266acd6d4cde121af12e
Amr <amralmoallim@gmail.com>
2020-09-26 20:53:26 -0700

    build

--- src/software/ai/hl/stp/tactic/passer_tactic.cpp
+++ src/software/ai/hl/stp/tactic/passer_tactic.cpp
@@ -4,6 +4,7 @@
 #include "software/ai/hl/stp/action/intercept_ball_action.h"
 #include "software/ai/hl/stp/action/kick_action.h"
 #include "software/ai/hl/stp/action/move_action.h"
+#include "software/ai/evaluation/ball.h"
 #include "software/logger/logger.h"
 
 PasserTactic::PasserTactic(Pass pass, const Ball& ball, const Field& field,
