a21f8713db74cbb5911b362dca425833e32b568d
Marco <marcoser2000@gmail.com>
2020-10-03 21:44:36 -0700

    Renamed tick functions to app_[primitive_name]_primitive_tick (#1756)

--- src/firmware/app/primitives/spinning_move_primitive.c
+++ src/firmware/app/primitives/spinning_move_primitive.c
@@ -64,7 +64,7 @@ void app_spinning_move_primitive_start(TbotsProto_SpinningMovePrimitive prim_msg
     app_dribbler_setSpeed(dribbler, (uint32_t)prim_msg.dribbler_speed_rpm);
 }
 
-static void spinning_move_tick(void *void_state_ptr, FirmwareWorld_t *world)
+static void app_spinning_move_primitive_tick(void *void_state_ptr, FirmwareWorld_t *world)
 {
     const FirmwareRobot_t *robot = app_firmware_world_getRobot(world);
     const SpinningMovePrimitiveState_t *state =
@@ -143,6 +143,6 @@ static void spinning_move_tick(void *void_state_ptr, FirmwareWorld_t *world)
  */
 const primitive_t SPINNING_MOVE_PRIMITIVE = {
     .direct        = false,
-    .tick          = &spinning_move_tick,
+    .tick          = &app_spinning_move_primitive_tick,
     .create_state  = &createSpinningMovePrimitiveState_t,
     .destroy_state = &destroySpinningMovePrimitiveState_t};
