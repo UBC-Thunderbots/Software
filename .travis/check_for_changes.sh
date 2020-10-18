a5339a93df70e2549e30e75915b0d7cb113eaa4a
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2020-10-18 14:33:28 -0700

    clean up

--- .travis/check_for_changes.sh
+++ .travis/check_for_changes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Checks against commit $1 for files changed that match $2 and recommends whether or not we should run tests.
+# exit code 1 if tests should be run, 0 if not.
 # Adapted from https://github.com/arnaskro/monorepo-travis/blob/master/.travis/build-condition.sh
 
 if [[ -z $1 ]]; then
@@ -12,4 +14,4 @@ if [[ -z $2 ]]; then
     exit 1
 fi
 
-git diff --name-only $1 | sort -u | uniq | grep $2 > /dev/null
+if git diff --name-only $1 | sort -u | uniq | grep -q $2;then echo "There are relevant changes, so we should run the tests" && exit 1; else echo "We should skip tests because there were no relevant changes" && exit 0; fi
