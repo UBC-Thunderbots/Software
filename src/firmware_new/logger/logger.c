d6e20641025e9abc02cfb6add146336846f24eda
Akhil Veeraghanta <akhil.veeraghanta@outlook.com>
2020-10-20 18:46:31 -0700

    change to static

--- src/firmware_new/logger/logger.c
+++ src/firmware_new/logger/logger.c
@@ -1,25 +1,18 @@
 #include "firmware_new/logger/logger.h"
+#include "shared/proto/robot_log_msg.nanopb.h"
 
 #include <stdlib.h>
 
-typedef struct Logger
-{
-    void (*handle_robot_log_msg)(TbotsProto_RobotLog);
-} Logger_t;
+static uint8_t robot_id_;
+static void (*handle_robot_log_msg_)(TbotsProto_RobotLog);
 
-Logger_t* app_logger_create(void (*handle_robot_log_msg)(TbotsProto_RobotLog))
+void app_logger_init(uint8_t robot_id, void (*handle_robot_log_msg)(TbotsProto_RobotLog))
 {
-    Logger_t* profile             = (Logger_t*)malloc(sizeof(Logger_t));
-    profile->handle_robot_log_msg = handle_robot_log_msg;
-    return profile;
+    robot_id_ = robot_id;
+    handle_robot_log_msg_ = handle_robot_log_msg;
 }
 
-void app_logger_log(const Logger_t* logger, TbotsProto_RobotLog log_msg)
+void app_logger_log(TbotsProto_RobotLog log_msg)
 {
-    logger->handle_robot_log_msg(log_msg);
-}
-
-void app_logger_destroy(Logger_t* logger)
-{
-    free(logger);
+    handle_robot_log_msg_(log_msg);
 }
--- src/firmware_new/logger/logger.h
+++ src/firmware_new/logger/logger.h
@@ -1,7 +1,17 @@
 #pragma once
 #include "shared/proto/robot_log_msg.nanopb.h"
 
-typedef struct Logger Logger_t;
-Logger_t* app_logger_create(void (*handle_robot_log_msg)(TbotsProto_RobotLog));
-void app_logger_log(const Logger_t* logger, TbotsProto_RobotLog log_msg);
-void app_logger_destroy(Logger_t* logger);
+/**
+ * Initializes the logger with the given robot id and the robot log proto handler
+ *
+ * @param robot_id The robot id the logs are coming from
+ * @param handle_robot_log_msg The handler for Robot Log protos 
+ */
+void app_logger_init(uint8_t robot_id, void (*handle_robot_log_msg)(TbotsProto_RobotLog));
+
+/**
+ * Log!
+ *
+ * @param TbotsProto_RobotLog the log msg
+ */
+void app_logger_log(TbotsProto_RobotLog log_msg);
