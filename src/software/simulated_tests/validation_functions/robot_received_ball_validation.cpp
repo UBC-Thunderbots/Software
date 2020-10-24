3d67fd4448dcd159bda3ab16fbea47d860679b8f
jared-paul <jaredpaul9820@gmail.com>
2020-10-24 12:45:12 -0700

    using robots isNearDribbler now

--- src/software/simulated_tests/validation_functions/robot_received_ball_validation.cpp
+++ src/software/simulated_tests/validation_functions/robot_received_ball_validation.cpp
@@ -1,6 +1,5 @@
 #include "software/simulated_tests/validation_functions/robot_received_ball_validation.h"
 
-#include "software/ai/evaluation/robot.h"
 #include "software/logger/logger.h"
 
 
@@ -8,14 +7,15 @@ void robotReceivedBall(RobotId robot_id, std::shared_ptr<World> world_ptr,
                        ValidationCoroutine::push_type& yield)
 {
     auto ball_near_dribbler = [robot_id](std::shared_ptr<World> world_ptr) {
-        std::optional<Robot> robot = world_ptr->friendlyTeam().getRobotById(robot_id);
-        if (!robot.has_value())
+        std::optional<Robot> robotOptional = world_ptr->friendlyTeam().getRobotById(robot_id);
+        if (!robotOptional.has_value())
         {
-            LOG(FATAL);
+            LOG(FATAL) << "There is no robot with ID: " + std::to_string(robot_id)                                              ;
         }
 
+        Robot robot = robotOptional.value();
         Point ball_position = world_ptr->ball().position();
-        return pointNearRobot(ball_position, robot.value());
+        return robot.isNearDribbler(ball_position);
     };
 
     while (!ball_near_dribbler(world_ptr))
