1feae538cea22d0180e70017e3bffe3332c5205a
jared-paul <jaredpaul9820@gmail.com>
2020-10-24 15:43:50 -0700

    fixed formatting

--- src/software/simulated_tests/validation_functions/robot_received_ball_validation.cpp
+++ src/software/simulated_tests/validation_functions/robot_received_ball_validation.cpp
@@ -7,13 +7,14 @@ void robotReceivedBall(RobotId robot_id, std::shared_ptr<World> world_ptr,
                        ValidationCoroutine::push_type& yield)
 {
     auto ball_near_dribbler = [robot_id](std::shared_ptr<World> world_ptr) {
-        std::optional<Robot> robotOptional = world_ptr->friendlyTeam().getRobotById(robot_id);
+        std::optional<Robot> robotOptional =
+            world_ptr->friendlyTeam().getRobotById(robot_id);
         if (!robotOptional.has_value())
         {
-            LOG(FATAL) << "There is no robot with ID: " + std::to_string(robot_id)                                              ;
+            LOG(FATAL) << "There is no robot with ID: " + std::to_string(robot_id);
         }
 
-        Robot robot = robotOptional.value();
+        Robot robot         = robotOptional.value();
         Point ball_position = world_ptr->ball().position();
         return robot.isNearDribbler(ball_position);
     };
