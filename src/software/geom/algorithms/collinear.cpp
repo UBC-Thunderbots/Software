9cad28757a125b7dae8f49dee71bb9b53ba3b78d
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2020-11-08 00:52:23 -0800

    Fix intersection ray segment corner cases

--- src/software/geom/algorithms/collinear.cpp
+++ src/software/geom/algorithms/collinear.cpp
@@ -1,31 +1,18 @@
 #include "software/geom/algorithms/collinear.h"
 
+#include "software/geom/algorithms/acute_angle.h"
 #include "software/geom/algorithms/almost_equal.h"
 
 
 bool collinear(const Point &a, const Point &b, const Point &c, double fixed_epsilon,
                int ulps_epsilon)
 {
-    if ((a - b).lengthSquared() < fixed_epsilon ||
-        (b - c).lengthSquared() < fixed_epsilon ||
-        (a - c).lengthSquared() < fixed_epsilon)
-    {
-        return true;
-    }
-
-    if ((almostEqual(a.x(), b.x(), fixed_epsilon, ulps_epsilon) &&
-         almostEqual(a.x(), c.x(), fixed_epsilon, ulps_epsilon)) ||
-        (almostEqual(a.y(), b.y(), fixed_epsilon, ulps_epsilon) &&
-         almostEqual(a.y(), c.y(), fixed_epsilon, ulps_epsilon)))
-    {
-        // Explicit check for the vectors being near vertical or horizontal to avoid near
-        // zero comparisons
-        return true;
-    }
-
-    Vector v1 = b - a;
-    Vector v2 = c - a;
-    return almostEqual(v1.x() * v2.y(), v1.y() * v2.x(), fixed_epsilon, ulps_epsilon);
+    return (almostEqual(acuteAngle(b - a, c - a).toRadians(), 0, fixed_epsilon,
+                        ulps_epsilon) ||
+            almostEqual(acuteAngle(a - b, c - b).toRadians(), 0, fixed_epsilon,
+                        ulps_epsilon) ||
+            almostEqual(acuteAngle(a - c, b - c).toRadians(), 0, fixed_epsilon,
+                        ulps_epsilon));
 }
 
 bool collinear(const Segment &segment1, const Segment &segment2)
--- src/software/geom/algorithms/contains.cpp
+++ src/software/geom/algorithms/contains.cpp
@@ -1,9 +1,11 @@
 #include "software/geom/algorithms/contains.h"
 
+#include "software/geom/algorithms/acute_angle.h"
 #include "software/geom/algorithms/almost_equal.h"
 #include "software/geom/algorithms/collinear.h"
 #include "software/geom/algorithms/distance.h"
 #include "software/geom/geom_constants.h"
+
 
 bool contains(const Circle& container, const Segment& contained)
 {
@@ -61,13 +63,15 @@ bool contains(const Polygon& container, const Point& contained)
 bool contains(const Ray& container, const Point& contained)
 {
     Point point_in_ray_direction = container.getStart() + container.toUnitVector();
+    bool point_is_ray_start      = contained == container.getStart();
 
-    bool point_is_ray_start = contained == container.getStart();
+    // We use broader values for epsilon and ulps because the default values brought
+    // warnings from simulated tests
     bool point_collinear_with_ray =
         collinear(contained, container.getStart(), point_in_ray_direction);
     bool point_is_in_ray_direction =
-        ((contained - container.getStart()).normalize() - container.toUnitVector())
-            .length() < FIXED_EPSILON;
+        acuteAngle(container.toUnitVector(), contained - container.getStart()).abs() <
+        Angle::quarter();
     return point_is_ray_start || (point_collinear_with_ray && point_is_in_ray_direction);
 }
 
