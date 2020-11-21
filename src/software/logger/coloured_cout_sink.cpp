6958c2d00779a39f9d1f5aa605513aafff395198
LiCody <licody2000@gmail.com>
2020-11-20 23:21:18 -0800

    finish adding tests

---
+++ src/software/logger/coloured_cout_sink.cpp
@@ -0,0 +1,27 @@
+#include "software/logger/coloured_cout_sink.h"
+
+ColouredCoutSink::FG_Colour ColouredCoutSink::getColour(const LEVELS level) {
+    if (level.value == WARNING.value) { return YELLOW; }
+    if (level.value == G3LOG_DEBUG.value) { return GREEN; }
+    if (g3::internal::wasFatal(level)) { return RED; }
+
+    return WHITE;
+}
+
+void ColouredCoutSink::resetColour() {
+    std::ostringstream oss;
+    oss << "\033[" << WHITE << "m" << " " << "\033[m";
+    std::cout << oss.str();
+}
+
+void ColouredCoutSink::displayColouredLog(g3::LogMessageMover log_entry)
+{
+    auto level = log_entry.get()._level;
+    auto colour = getColour(level);
+
+    std::ostringstream oss;
+    oss << "\033[" << colour << "m"
+        << log_entry.get().toString() << "\033[m" << std::endl;
+    std::cout << oss.str();
+    resetColour();
+}
--- src/software/logger/logger.h
+++ src/software/logger/logger.h
@@ -4,9 +4,11 @@
 #include <g3log/logworker.hpp>
 #include <g3log/logmessage.hpp>
 #include <g3log/loglevels.hpp>
-#include <g3log/logcapture.hpp>
 #include <g3sinks/LogRotate.h>
-#include <iostream>
+#include <g3sinks/LogRotateWithFilter.h>
+
+#include "software/logger/coloured_cout_sink.h"
+#include "software/logger/custom_logging_levels.h"
 
 /**
  * This class acts as a Singleton that's responsible for initializing the logger.
@@ -35,14 +37,21 @@ class LoggerSingleton
     LoggerSingleton()
     {
         logWorker = g3::LogWorker::createLogWorker();
-        auto sinkHandle = logWorker->addSink(std::make_unique<LogRotate>("thunderbots", "software/"), &LogRotate::save);
-        auto MakeLogFatalThrowException = []{
-            
-            throw std::runtime_error("test");
-        };
+        // robot diagnostics logs are in bazel-out/k8-fastbuild/bin/software/gui/robot_diagnostics/robot_diagnostics_main.runfiles/__main__/software
+        // full system logs are in bazel-out/k8-fastbuild/bin/software/full_system.runfiles/__main__/software
+        // Sink for outputting logs to the terminal
+        auto colour_cout_sink_handle = logWorker->addSink(std::make_unique<ColouredCoutSink>(), &ColouredCoutSink::displayColouredLog);
+        // Sink for storing a file of all logs
+        auto log_rotate_sink_handle = logWorker->addSink(std::make_unique<LogRotate>(log_name, "/software"), &LogRotate::save);
+        // Sink for storing a file of filtered logs
+        auto filtered_log_rotate_sink_handle = logWorker->addSink(std::make_unique<LogRotateWithFilter>(std::make_unique<LogRotate>(log_name + filter_suffix, "/software"), level_filter), &LogRotateWithFilter::save);
+
         g3::initializeLogging(logWorker.get());
-        g3::setFatalPreLoggingHook(MakeLogFatalThrowException);
     }
 
+    // levels is this vector are ignored in the filtered log rotate sink
+    std::vector<LEVELS> level_filter = {DEBUG, INFO, ROBOT_STATUS};
+    const std::string filter_suffix = "_filtered";
+    const std::string log_name = "thunderbots";
     std::unique_ptr<g3::LogWorker> logWorker;
 };
