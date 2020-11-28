df5b2bd1c6d7795b9bcc7df088878277761e4a96
Akhil Veeraghanta <akhil.veeraghanta@outlook.com>
2020-11-28 11:43:11 -0800

    use virtual x-server

--- .travis/check_cubemx_ci.sh
+++ .travis/check_cubemx_ci.sh
@@ -11,12 +11,6 @@ echo "=========================================================="
 echo " THIS SHOULD ONLY BE RUN IN CI, PLEASE DO NOT USE LOCALLY"
 echo "=========================================================="
 
-# In order to run cubemx in CI, we need to have an X-Server running
-# _even though_ the process is completely scripted and does not need the GUI at all
-# So we install a virutal Xserver to make cubemx happy
-sudo apt-get install xvfb
-Xvfb -shmem -screen 0 1280x1024x24
-
 cd src
 bazel run --cpu=stm32h7 //firmware_new/tools:cubemx_regen firmware_new/boards/frankie_v1
 if [[ "$?" != 0 ]]; then
