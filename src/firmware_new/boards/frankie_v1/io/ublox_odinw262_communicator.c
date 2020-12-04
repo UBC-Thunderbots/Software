ca9a3ed97d07ab01e53d3d779548d304384e6dbb
Akhil Veeraghanta <akhil.veeraghanta@outlook.com>
2020-12-03 19:57:55 -0800

    wip

--- src/firmware_new/boards/frankie_v1/io/ublox_odinw262_communicator.c
+++ src/firmware_new/boards/frankie_v1/io/ublox_odinw262_communicator.c
@@ -1,106 +1,207 @@
 #include "firmware_new/boards/frankie_v1/io/ublox_odinw262_communicator.h"
 
+#include <assert.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include "FreeRTOS.h"
 
+#include "FreeRTOS.h"
 #include "cmsis_os.h"
+#include "firmware/app/logger/logger.h"
+#include "firmware_new/boards/frankie_v1/io/gpio_pin.h"
+#include "firmware_new/boards/frankie_v1/usart.h"
 #include "main.h"
 #include "task.h"
 
-#include "firmware_new/boards/frankie_v1/io/gpio_pin.h"
-#include "firmware_new/boards/frankie_v1/usart.h"
-#include "firmware/app/logger/logger.h"
-
 #define DMA_BUFFER __attribute__((section(".dma_buffer")))
-#define RX_BUFFER_LENGTH (1024)
-#define SLEEP_FOR_GOD_KNOWS_HOW_LONG(time) for(int k =0; k<time; k++);
+#define RX_BUFFER_LENGTH_BYTES 4096
+#define UBLOX_OK_RESPONSE_LENGTH_BYTES 4
+#define UBLOX_ERROR_RESPONSE_LENGTH_BYTES 7
+#define UBLOX_RESPONSE_TIMEOUT_S 100
 
-// These buffers are stored in the R2 domain which is what the DMA
-// controller has access to write to
-DMA_BUFFER static uint8_t g_uart_receive_buffer[RX_BUFFER_LENGTH];
-
-typedef struct UbloxOdinW262Communicator
+typedef enum
 {
-    GpioPin_t* ublox_reset_pin;
-    UART_HandleTypeDef* at_interface_uart_handle;
-} UbloxOdinW262Communicator_t;
+    UBLOX_RESPONSE_UNKOWN  = 0,  // The state is unkown, and yet to be determined
+    UBLOX_RESPONSE_INVALID = 1,  // An invalid sequence of characters was detected
+    UBLOX_RESPONSE_OK      = 2,  // OK\r\n was returned at the end of the response
+    UBLOX_RESPONSE_ERROR   = 3,  // ERROR\r\n was returned at the end of the response
+} UbloxResponseStatus_t;
 
-void io_ublox_odinw262_communicator_task(void* arg)
+DMA_BUFFER static uint8_t g_uart_receive_dma_buffer[RX_BUFFER_LENGTH_BYTES] = {0};
+/*static char g_uart_receive_buffer[RX_BUFFER_LENGTH_BYTES];*/
+
+static const char* g_ublox_ok_response    = "OK\r\n";
+static const char* g_ublox_error_response = "ERROR\r\n";
+
+volatile uint32_t g_dma_counter_on_uart_idle_line;
+volatile uint32_t g_last_byte_parsed_from_dma_buffer;
+volatile UbloxResponseStatus_t g_ublox_response_status;
+
+static osSemaphoreId_t g_dma_receive_semaphore;
+static UART_HandleTypeDef* g_ublox_uart_handle;
+static GpioPin_t* g_ublox_reset_pin;
+static bool g_initialized = false;
+
+void io_ublox_odinw262_communicator_init(UART_HandleTypeDef* uart_handle,
+                                         GpioPin_t* ublox_reset)
 {
-    TLOG_DEBUG("YEEEEEEEEEEHAW");
-    UbloxOdinW262Communicator_t* communicator = (UbloxOdinW262Communicator_t*)arg;
+    assert(!g_initialized);
 
-    TLOG_DEBUG("pinging ublox odinw262");
-    io_ublox_odinw262_communicator_sendATCommand(communicator, "AT\r");
-    osDelay(4 * 1000);
+    // create a binary sempahore
+    g_dma_receive_semaphore = osSemaphoreNew(1U, 1U, NULL);
+    g_ublox_uart_handle     = uart_handle;
+    g_ublox_reset_pin       = ublox_reset;
 
-    TLOG_DEBUG("enable ethernet bridge");
-    io_ublox_odinw262_communicator_sendATCommand(communicator, "AT+UBRGC=0,1,1,3\r");
-    osDelay(4 * 1000);
-
-    TLOG_DEBUG("what mac addresses are there");
-    io_ublox_odinw262_communicator_sendATCommand(communicator, "AT+UWAPMACADDR\r");
-    osDelay(4 * 1000);
-
-    TLOG_DEBUG("activate bridge connection");
-    io_ublox_odinw262_communicator_sendATCommand(communicator, "AT+UBRGCA=0,3\r");
-    osDelay(4 * 1000);
-
-    TLOG_DEBUG("do a wifi scan");
-    io_ublox_odinw262_communicator_sendATCommand(communicator, "AT+UWSCAN\r");
-    osDelay(4 * 1000);
-}
-
-UbloxOdinW262Communicator_t* io_ublox_odinw262_communicator_create(
-    UART_HandleTypeDef* uart_handle, GpioPin_t* ublox_reset)
-
-{
-    UbloxOdinW262Communicator_t* communicator =
-        (UbloxOdinW262Communicator_t*)malloc(sizeof(UbloxOdinW262Communicator_t));
-
-    communicator->at_interface_uart_handle = uart_handle;
-    communicator->ublox_reset_pin          = ublox_reset;
-
-    if(HAL_UART_Init(communicator->at_interface_uart_handle) != HAL_OK)
+    HAL_UART_DeInit(g_ublox_uart_handle);
+    if (HAL_UART_Init(g_ublox_uart_handle) != HAL_OK)
     {
         TLOG_FATAL("Failed to initialize UART connection");
     }
 
     // We use idle line detection to know when to parse the circular buffer
-    __HAL_UART_ENABLE_IT(communicator->at_interface_uart_handle, UART_IT_IDLE);
+    __HAL_UART_ENABLE_IT(g_ublox_uart_handle, UART_IT_IDLE);
 
     // Setup DMA transfer to continually receive data over UART as the DMA
     // controller is setup in circular mode for rx/tx
-    if(HAL_UART_Receive_DMA(communicator->at_interface_uart_handle, g_uart_receive_buffer, RX_BUFFER_LENGTH) != HAL_OK)
+    if (HAL_UART_Receive_DMA(g_ublox_uart_handle, g_uart_receive_dma_buffer,
+                             RX_BUFFER_LENGTH_BYTES) != HAL_OK)
     {
         TLOG_FATAL("Failed to setup UART DMA Receive Transfer");
     }
 
-    return communicator;
+    g_initialized = true;
 }
 
-void io_ublox_odinw262_communicator_destroy(UbloxOdinW262Communicator_t* communicator)
+void io_ublox_odinw262_communicator_task(void* arg)
 {
-    HAL_UART_DMAStop(communicator->at_interface_uart_handle);
-    HAL_UART_DeInit(communicator->at_interface_uart_handle);
-    free(communicator);
+    assert(g_initialized);
+
+    for (;;)
+    {
+        // Invalidate D-cache before reception 
+        // Make sure the address is 32-byte aligned and add 32-bytes to length, in case it overlaps cacheline 
+        // https://community.st.com/s/article/FAQ-DMA-is-not-working-on-STM32H7-devices
+        SCB_InvalidateDCache_by_Addr(
+                (uint32_t*)(((uint32_t)g_uart_receive_dma_buffer) & ~(uint32_t)0x1F), RX_BUFFER_LENGTH_BYTES+32);
+
+        TLOG_DEBUG("pinging ublox odinw262");
+        io_ublox_odinw262_communicator_sendATCommand("AT\r");
+        osDelay(8 * 1000);
+
+        TLOG_DEBUG("Enable Ethernet Bridge");
+        io_ublox_odinw262_communicator_sendATCommand("AT+UBRGC=0,1,1,3\r");
+        osDelay(8 * 1000);
+
+        TLOG_DEBUG("What mac addresses are there");
+        io_ublox_odinw262_communicator_sendATCommand("AT+UWAPMACADDR\r");
+        osDelay(8 * 1000);
+
+        TLOG_DEBUG("Activate Bridge Connection");
+        io_ublox_odinw262_communicator_sendATCommand("AT+UBRGCA=0,3\r");
+        osDelay(8 * 1000);
+
+        TLOG_DEBUG("Activate Ethernet");
+        io_ublox_odinw262_communicator_sendATCommand("AT+UETHCA=3\r");
+        osDelay(8 * 1000);
+    }
 }
 
-void io_ublox_odinw262_communicator_handleIdleLineInterrupt(UART_HandleTypeDef* uart_handle)
+void io_ublox_odinw262_communicator_handleIdleLineInterrupt(
+    UART_HandleTypeDef* uart_handle)
 {
-    // TODO do something here
-    uart_handle = uart_handle;
+    g_dma_counter_on_uart_idle_line =
+        RX_BUFFER_LENGTH_BYTES - __HAL_DMA_GET_COUNTER(uart_handle->hdmarx);
+
+    g_ublox_response_status = UBLOX_RESPONSE_UNKOWN;
+    int start_pos           = (RX_BUFFER_LENGTH_BYTES + g_dma_counter_on_uart_idle_line -
+                     UBLOX_OK_RESPONSE_LENGTH_BYTES) %
+                    RX_BUFFER_LENGTH_BYTES;
+
+    for (int k = 0; k < UBLOX_OK_RESPONSE_LENGTH_BYTES; k++)
+    {
+        if (g_ublox_ok_response[k] !=
+            g_uart_receive_dma_buffer[(start_pos + k) % RX_BUFFER_LENGTH_BYTES])
+        {
+            g_ublox_response_status = UBLOX_RESPONSE_INVALID;
+            break;
+        }
+    }
+
+    if (g_ublox_response_status == UBLOX_RESPONSE_UNKOWN)
+    {
+        g_ublox_response_status = UBLOX_RESPONSE_OK;
+        goto finalize_interrupt;
+    }
+
+    g_ublox_response_status = UBLOX_RESPONSE_UNKOWN;
+    start_pos               = (RX_BUFFER_LENGTH_BYTES + g_dma_counter_on_uart_idle_line -
+                 UBLOX_ERROR_RESPONSE_LENGTH_BYTES) %
+                RX_BUFFER_LENGTH_BYTES;
+    for (int k = 0; k < UBLOX_ERROR_RESPONSE_LENGTH_BYTES; k++)
+    {
+        if (g_ublox_error_response[k] !=
+            g_uart_receive_dma_buffer[(start_pos + k) % RX_BUFFER_LENGTH_BYTES])
+        {
+            g_ublox_response_status = UBLOX_RESPONSE_INVALID;
+            break;
+        }
+    }
+
+    if (g_ublox_response_status == UBLOX_RESPONSE_UNKOWN)
+    {
+        g_ublox_response_status = UBLOX_RESPONSE_ERROR;
+    }
+
+finalize_interrupt:
+    osSemaphoreRelease(g_dma_receive_semaphore);
 }
 
-void io_ublox_odinw262_reset(UbloxOdinW262Communicator_t* interface)
+void io_ublox_odinw262_reset()
 {
-    io_gpio_pin_setActive(interface->ublox_reset_pin);
-    io_gpio_pin_setInactive(interface->ublox_reset_pin);
+    assert(g_initialized);
+    io_gpio_pin_setActive(g_ublox_reset_pin);
+    io_gpio_pin_setInactive(g_ublox_reset_pin);
 }
 
-void io_ublox_odinw262_communicator_sendATCommand(UbloxOdinW262Communicator_t* communicator, const char* at_command)
+char* io_ublox_odinw262_communicator_sendATCommand(const char* command)
 {
-    HAL_UART_Transmit(&huart4, (uint8_t*)at_command, (uint16_t)strlen(at_command), HAL_MAX_DELAY);
-    // Pend on sempahore
+    assert(g_initialized);
+    HAL_UART_Transmit(g_ublox_uart_handle, (uint8_t*)command, (uint16_t)strlen(command),
+                      HAL_MAX_DELAY);
+
+    jank:
+    {
+        osStatus_t status = osSemaphoreAcquire(g_dma_receive_semaphore, UBLOX_RESPONSE_TIMEOUT_S);
+
+        if (status == osErrorTimeout)
+        {
+            TLOG_WARNING("Ublox did not respond to %s in %d seconds", command,
+                         UBLOX_RESPONSE_TIMEOUT_S);
+            return NULL;
+        }
+    }
+
+    switch (g_ublox_response_status)
+    {
+        case UBLOX_RESPONSE_ERROR:
+        {
+            TLOG_INFO("Ublox response error");
+            break;
+        }
+        case UBLOX_RESPONSE_UNKOWN:
+        {
+            TLOG_INFO("Ublox response unkown");
+            break;
+        }
+        case UBLOX_RESPONSE_OK:
+        {
+            TLOG_INFO("Ublox response OK");
+            break;
+        }
+        case UBLOX_RESPONSE_INVALID:
+        {
+            TLOG_INFO("Ublox response invalid");
+            goto jank;
+        }
+    }
 }
--- src/firmware_new/boards/frankie_v1/io/ublox_odinw262_communicator.h
+++ src/firmware_new/boards/frankie_v1/io/ublox_odinw262_communicator.h
@@ -3,15 +3,13 @@
 #include "firmware_new/boards/frankie_v1/io/gpio_pin.h"
 #include "firmware_new/boards/frankie_v1/usart.h"
 
-typedef struct UbloxOdinW262Communicator UbloxOdinW262Communicator_t;
-
 /**
  *
  */
 void io_ublox_odinw262_communicator_task(void* arg);
 
 /**
- * Creates a UbloxOdinW262Communicator_t to communicate with the AT interface of the
+ * Initializes the
  * Odin W262. This allows us to send AT Commands according to the manual:
  *
  * https://www.u-blox.com/sites/default/files/u-connect-ATCommands-Manual_%28UBX-14044127%29_C1-Public.pdf
@@ -21,25 +19,15 @@ void io_ublox_odinw262_communicator_task(void* arg);
  *
  * @returns A Ublox OdinW262 command interface
  */
-UbloxOdinW262Communicator_t* io_ublox_odinw262_communicator_create(
-    UART_HandleTypeDef* uart_handle, GpioPin_t* ublox_reset);
-
-/**
- * Destroys the provided UbloxOdinW262Communicator_t
- *
- * @param communicator The UbloxOdinW262Communicator_t communicator to destroy
- */
-void io_ublox_odinw262_communicator_destroy(
-    UbloxOdinW262Communicator_t* communicator);
+void io_ublox_odinw262_communicator_init(UART_HandleTypeDef* uart_handle,
+                                         GpioPin_t* ublox_reset);
 
 /*
  * Reset the UBlox ODIN W262
  *
  * NOTE: This resets the chip, including the interface
- *
- * @param communicator
  */
-void io_ublox_odinw262_reset(UbloxOdinW262Communicator_t* communicator);
+void io_ublox_odinw262_reset(void);
 
 /**
  * Uart Idle Line Interrupt Service Routine
@@ -49,6 +37,7 @@ void io_ublox_odinw262_reset(UbloxOdinW262Communicator_t* communicator);
  *
  * @param uart_handle The uart_handle
  */
-void io_ublox_odinw262_communicator_handleIdleLineInterrupt(UART_HandleTypeDef* uart_handle);
+void io_ublox_odinw262_communicator_handleIdleLineInterrupt(
+    UART_HandleTypeDef* uart_handle);
 
-void io_ublox_odinw262_communicator_sendATCommand(UbloxOdinW262Communicator_t* communicator, const char* at_command);
+char* io_ublox_odinw262_communicator_sendATCommand(const char* at_command);
