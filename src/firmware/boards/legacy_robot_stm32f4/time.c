4d6a3c18d6ab721da9ab1fa36688cb3d7025931e
Gareth Ellis <gareth.ellis0@gmail.com>
2020-12-29 17:07:19 -0800

    Got Old Firmware To Compile

---
+++ src/firmware/boards/legacy_robot_stm32f4/time.c
@@ -0,0 +1,8 @@
+#include "firmware/boards/legacy_robot_stm32f4/time.h"
+
+#include <FreeRTOS.h>
+
+
+float get_current_freertos_tick_time_seconds(){
+    return ((float)xTaskGetTickCount()) * portTICK_PERIOD_MS;
+}
