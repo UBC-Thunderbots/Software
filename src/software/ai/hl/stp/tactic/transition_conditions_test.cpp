c0c79fce607888e53f47f2b6ecc936b63394666e
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-01-15 15:38:26 -0800

    Add unit tests

---
+++ src/software/ai/hl/stp/tactic/transition_conditions_test.cpp
@@ -0,0 +1,22 @@
+#include "software/ai/hl/stp/tactic/transition_conditions.h"
+
+#include <gtest/gtest.h>
+
+TEST(TransitionConditionTest, test_move_robot_done)
+{
+    Robot robot = Robot(0, Point(2, 3), Vector(), Angle::quarter(),
+                        AngularVelocity::zero(), Timestamp::fromSeconds(0));
+    EXPECT_TRUE(moveRobotDone(robot, Point(2, 3), Angle::quarter()));
+    EXPECT_FALSE(moveRobotDone(robot, Point(2, 2), Angle::quarter()));
+    EXPECT_FALSE(moveRobotDone(robot, Point(2, 3), Angle::threeQuarter()));
+}
+
+TEST(TransitionConditionTest, test_robot_stopped)
+{
+    Robot stopped_robot = Robot(0, Point(2, 3), Vector(), Angle::quarter(),
+                                AngularVelocity::zero(), Timestamp::fromSeconds(0));
+    EXPECT_TRUE(robotStopped(stopped_robot));
+    Robot moving_robot = Robot(0, Point(2, 3), Vector(3, 1), Angle::quarter(),
+                               AngularVelocity::zero(), Timestamp::fromSeconds(0));
+    EXPECT_FALSE(robotStopped(moving_robot));
+}
