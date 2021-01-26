8c6b7aa83c555345faac6b9a9f94c7711fa72e0c
tarakong01 <tarakong01@gmail.com>
2021-01-25 21:31:06 -0800

    made header and cpp files for validation functions

---
+++ src/software/simulated_tests/validation_functions/robots_slowed_down_validation.cpp
@@ -0,0 +1,11 @@
+#include "software/simulated_tests/validation_functions/friendly_scored_validation.h"
+
+#include "software/geom/algorithms/contains.h"
+
+void robotsSlowedDown(std::shared_ptr<World> world_ptr,
+                      ValidationCoroutine::push_type& yield) {
+    while (true) {
+        yield();
+    }
+
+}
---
+++ src/software/simulated_tests/validation_functions/robots_slowed_down_validation.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "software/simulated_tests/validation/validation_function.h"
+#include "software/world/world.h"
+
+/**
+ * Checks if robots have slowed down to the required speed
+ * when the Referee has issued a STOP command
+ * @param world_ptr the world pointer given by the simulator. Gets updated every tick
+ * @param yield yields control to the next routine (coroutines)
+ */
+void robotsSlowedDown(std::shared_ptr<World> world_ptr,
+                      ValidationCoroutine::push_type& yield);
