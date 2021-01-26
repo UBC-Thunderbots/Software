93887383a48cac89c559dacf3bfecb59c32fc594
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-01-26 14:08:19 -0800

    split off stop fsm

---
+++ src/software/ai/hl/stp/tactic/stop_fsm.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "software/ai/hl/stp/tactic/tactic_fsm.h"
+#include "software/ai/intent/stop_intent.h"
+
+struct StopFSM
+{
+    struct IdleState
+    {
+    };
+    struct StopState
+    {
+    };
+
+    struct Update
+    {
+        bool coast;
+        TacticUpdate common;
+    };
+
+    auto operator()()
+    {
+        using namespace boost::sml;
+
+        const auto idle_s   = state<idle_state>;
+        const auto stop_s   = state<stop_state>;
+        const auto update_e = event<Update>;
+
+        const auto update_stop = [](auto event) {
+            event.common.set_intent(
+                std::make_unique<StopIntent>(event.common.robot.id(), event.coast));
+        };
+
+        const auto stop_done = [](auto event) {
+            return robotStopped(event.common.robot);
+        };
+
+        return make_transition_table(
+            // src_state + event [guard] / action = dest state
+            *idle_s + update_e / update_stop            = stop_s,
+            stop_s + update_e[!stop_done] / update_stop = stop_s,
+            stop_s + update_e[stop_done] / update_stop  = X,
+            X + update_e[!stop_done] / update_stop      = X);
+    }
+};
---
+++ src/software/ai/hl/stp/tactic/stop_fsm_test.cpp
@@ -0,0 +1,43 @@
+#include "software/ai/hl/stp/tactic/stop_fsm.h"
+
+#include <gtest/gtest.h>
+
+#include "software/test_util/test_util.h"
+
+TEST(StopFSMTest, test_transitions)
+{
+    World world = ::TestUtil::createBlankTestingWorld();
+    Robot robot(0,
+                RobotState(Point(1, -3), Vector(2.1, 3.1), Angle::half(),
+                           AngularVelocity::zero()),
+                Timestamp::fromSeconds(123));
+
+    boost::sml::sm<StopFSM> fsm;
+    EXPECT_TRUE(fsm.is(boost::sml::state<StopFSM::idle_state>));
+    fsm.process_event(StopFSM::Update{
+        .coast  = false,
+        .common = TacticUpdate{.robot      = robot,
+                               .world      = world,
+                               .set_intent = [](std::unique_ptr<Intent>) {}}});
+    // robot is still moving
+    EXPECT_TRUE(fsm.is(boost::sml::state<StopFSM::stop_state>));
+    robot = Robot(0,
+                  RobotState(Point(1, -3), Vector(1.1, 2.1), Angle::half(),
+                             AngularVelocity::zero()),
+                  Timestamp::fromSeconds(123));
+    fsm.process_event(StopFSM::Update{
+        .coast  = false,
+        .common = TacticUpdate{.robot      = robot,
+                               .world      = world,
+                               .set_intent = [](std::unique_ptr<Intent>) {}}});
+    // robot is still moving
+    EXPECT_TRUE(fsm.is(boost::sml::state<StopFSM::stop_state>));
+    robot = TestUtil::createRobotAtPos(Point(1, -3));
+    fsm.process_event(StopFSM::Update{
+        .coast  = false,
+        .common = TacticUpdate{.robot      = robot,
+                               .world      = world,
+                               .set_intent = [](std::unique_ptr<Intent>) {}}});
+    // robot stopped
+    EXPECT_TRUE(fsm.is(boost::sml::X));
+}
