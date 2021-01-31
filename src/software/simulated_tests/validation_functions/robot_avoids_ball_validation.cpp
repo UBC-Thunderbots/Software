3ac35640e3b1338237fc58720c025a866f55d019
tarakong01 <tarakong01@gmail.com>
2021-01-30 23:01:17 -0800

    in progress

--- src/software/simulated_tests/validation_functions/robot_avoids_ball_validation.cpp
+++ src/software/simulated_tests/validation_functions/robot_avoids_ball_validation.cpp
@@ -5,7 +5,7 @@
 void robotAvoidsBall(RobotId robot_id, std::shared_ptr<World> world_ptr,
                      ValidationCoroutine::push_type& yield) {
 
-    auto robot_slowed_down = [robot_id](std::shared_ptr<World> world_ptr) {
+    auto robot_avoids_ball = [robot_id](std::shared_ptr<World> world_ptr) {
         std::optional<Robot> robotOptional =
                 world_ptr->friendlyTeam().getRobotById(robot_id);
         if (!robotOptional.has_value())
@@ -19,5 +19,13 @@ void robotAvoidsBall(RobotId robot_id, std::shared_ptr<World> world_ptr,
 
         return (distance_from_ball <= MIN_DISTANCE_FROM_BALL);
     };
+
+    if (!robot_avoids_ball(world_ptr))
+    {
+        LOG(FATAL) << "Robot " + std::to_string(robot_id) + " failed to avoid the ball";
+    }
+    else {
+        LOG(WARNING) << "Robot " + std::to_string(robot_id) + " is avoiding the ball!";
+    }
 
 }
