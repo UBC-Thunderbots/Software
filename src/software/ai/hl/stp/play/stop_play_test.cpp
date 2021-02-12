89745142a75a69bb7f8c256ecc0a6f8ac0d4482a
tarakong01 <tarakong01@gmail.com>
2021-02-11 22:49:03 -0800

    added test for ball in enemy half, robots spread out

--- src/software/ai/hl/stp/play/stop_play_test.cpp
+++ src/software/ai/hl/stp/play/stop_play_test.cpp
@@ -121,7 +121,7 @@ class StopPlayTest : public SimulatedPlayTestFixture
 }*/
 
 // Robots went out of bounds when trying to position themselves around the ball!!
-TEST_F(StopPlayTest, test_stop_play_friendly_half_corner_robots_close_together)
+/*TEST_F(StopPlayTest, test_stop_play_friendly_half_corner_robots_close_together)
 {
     setBallState(BallState(Point(-4,-2.5), Vector(0, 0)));
 
@@ -147,14 +147,67 @@ TEST_F(StopPlayTest, test_stop_play_friendly_half_corner_robots_close_together)
                     yield();
                 }
                 // TODO: #1882 implement robots slow down when responding to stop command
-                /*robotSlowsDown(0, world_ptr, yield);
+                *//*robotSlowsDown(0, world_ptr, yield);
                 robotSlowsDown(1, world_ptr, yield);
+                robotSlowsDown(2, world_ptr, yield);
+                robotSlowsDown(3, world_ptr, yield);
+                robotSlowsDown(4, world_ptr, yield);
+                robotSlowsDown(5, world_ptr, yield);*//*
+
+                robotAvoidsBall(0, world_ptr, yield);
+                robotAvoidsBall(1, world_ptr, yield);
+                robotAvoidsBall(2, world_ptr, yield);
+                robotAvoidsBall(3, world_ptr, yield);
+                robotAvoidsBall(4, world_ptr, yield);
+                robotAvoidsBall(5, world_ptr, yield);
+            }
+    };
+
+    runTest(terminating_validation_functions, non_terminating_validation_functions,
+            Duration::fromSeconds(10));
+}*/
+
+TEST_F(StopPlayTest, test_stop_play_enemy_half_robots_spread_out)
+{
+    setBallState(BallState(Point(2, 0), Vector(0, 0)));
+
+    Point near_friendly_goal = Point(-4,0);
+    Point close_goal_side = Point(1.7, 0);
+    Point close_offence_side = Point(2.3, 0);
+    Point close_beside_ball = Point(2,0.3);
+    Point far_friendly_side = Point(-3,-1.5);
+    Point far_enemy_side = Point(3, -3);
+
+    addFriendlyRobots(TestUtil::createStationaryRobotStatesWithId(
+            {near_friendly_goal, close_goal_side, close_offence_side, close_beside_ball, far_friendly_side,
+             far_enemy_side}));
+    setFriendlyGoalie(0);
+    addEnemyRobots(TestUtil::createStationaryRobotStatesWithId(
+            {Point(1, 0), Point(1, 2.5), Point(1, -2.5), field().enemyGoalCenter(),
+             field().enemyDefenseArea().negXNegYCorner(),
+             field().enemyDefenseArea().negXPosYCorner()}));
+    setEnemyGoalie(0);
+    setAIPlay(TYPENAME(StopPlay));
+    setRefereeCommand(RefereeCommand::STOP, RefereeCommand::STOP);
+
+    std::vector<ValidationFunction> terminating_validation_functions = {};
+
+    std::vector<ValidationFunction> non_terminating_validation_functions = {
+            [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
+                // this waits 2 seconds to allow robots that are initially too close to the ball to move away from it
+                while (world_ptr->getMostRecentTimestamp() < Timestamp::fromSeconds(2))
+                {
+                    yield();
+                }
+                // TODO: #1882 implement robots slow down when responding to stop command
+                /*robotSlowsDown(0, world_ptr, yield);
+                        robotSlowsDown(1, world_ptr, yield);
                 robotSlowsDown(2, world_ptr, yield);
                 robotSlowsDown(3, world_ptr, yield);
                 robotSlowsDown(4, world_ptr, yield);
                 robotSlowsDown(5, world_ptr, yield);*/
 
-                robotAvoidsBall(0, world_ptr, yield);
+                    robotAvoidsBall(0, world_ptr, yield);
                 robotAvoidsBall(1, world_ptr, yield);
                 robotAvoidsBall(2, world_ptr, yield);
                 robotAvoidsBall(3, world_ptr, yield);
