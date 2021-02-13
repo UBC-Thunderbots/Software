f27212ab73b0bb04c7000ae3e9eef6f98ec4e92d
jonathanlew <31377876+jonathanlew@users.noreply.github.com>
2021-02-13 10:34:02 -0800

    ChipTactic and KickTactic using FSMs (#1924)
    * GetBehindBallTactic
    
    * ChipTactic hierarchical FSM works
    
    * Add get behind ball fsm transition test
    
    * Add chip tactic fsm test
    
    * Add kick tactic

---
+++ src/software/ai/hl/stp/tactic/chip_fsm.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "software/ai/hl/stp/tactic/get_behind_ball_fsm.h"
+#include "software/ai/hl/stp/tactic/tactic.h"
+#include "software/ai/intent/chip_intent.h"
+
+struct ChipFSM
+{
+    class chip_state;
+
+    struct ControlParams
+    {
+        // The location where the chip will be taken from
+        Point chip_origin;
+        // The direction the Robot will chip in
+        Angle chip_direction;
+        // The distance the robot will chip to
+        double chip_distance_meters;
+    };
+
+    DEFINE_UPDATE_STRUCT_WITH_CONTROL_AND_COMMON_PARAMS
+
+    auto operator()()
+    {
+        using namespace boost::sml;
+
+        const auto get_behind_ball_s = state<GetBehindBallFSM>;
+        const auto chip_s            = state<chip_state>;
+        const auto update_e          = event<Update>;
+
+        const auto update_chip = [](auto event) {
+            event.common.set_intent(std::make_unique<ChipIntent>(
+                event.common.robot.id(), event.control_params.chip_origin,
+                event.control_params.chip_direction,
+                event.control_params.chip_distance_meters));
+        };
+
+        const auto update_get_behind_ball =
+            [](auto event, back::process<GetBehindBallFSM::Update> processEvent) {
+                GetBehindBallFSM::ControlParams control_params{
+                    .ball_location   = event.control_params.chip_origin,
+                    .chick_direction = event.control_params.chip_direction};
+
+                // Update the get behind ball fsm
+                processEvent(GetBehindBallFSM::Update(control_params, event.common));
+            };
+
+        const auto ball_chicked = [](auto event) {
+            return event.common.world.ball().hasBallBeenKicked(
+                event.control_params.chip_direction);
+        };
+
+        return make_transition_table(
+            // src_state + event [guard] / action = dest state
+            *get_behind_ball_s + update_e / update_get_behind_ball,
+            get_behind_ball_s                              = chip_s,
+            chip_s + update_e[!ball_chicked] / update_chip = chip_s,
+            chip_s + update_e[ball_chicked]                = X);
+    }
+};
---
+++ src/software/ai/hl/stp/tactic/chip_fsm_test.cpp
@@ -0,0 +1,41 @@
+#include "software/ai/hl/stp/tactic/chip_fsm.h"
+
+#include <gtest/gtest.h>
+
+#include "software/test_util/test_util.h"
+
+TEST(ChipFSMTest, test_transitions)
+{
+    World world = ::TestUtil::createBlankTestingWorld();
+    Robot robot = ::TestUtil::createRobotAtPos(Point(-2, -3));
+    ChipFSM::ControlParams control_params{.chip_origin          = Point(-2, 1.5),
+                                          .chip_direction       = Angle::threeQuarter(),
+                                          .chip_distance_meters = 1.2};
+
+    HFSM<ChipFSM> fsm;
+    // Start in GetBehindBallFSM state's idle_state
+    EXPECT_TRUE(fsm.is(boost::sml::state<GetBehindBallFSM>));
+    EXPECT_TRUE(fsm.is<decltype(boost::sml::state<GetBehindBallFSM>)>(
+        boost::sml::state<GetBehindBallFSM::idle_state>));
+    fsm.process_event(ChipFSM::Update(
+        control_params, TacticUpdate(robot, world, [](std::unique_ptr<Intent>) {})));
+    // Transition to GetBehindBallFSM state's get_behind_ball_state
+    EXPECT_TRUE(fsm.is(boost::sml::state<GetBehindBallFSM>));
+    EXPECT_TRUE(fsm.is<decltype(boost::sml::state<GetBehindBallFSM>)>(
+        boost::sml::state<GetBehindBallFSM::get_behind_ball_state>));
+    // Robot is now behind ball
+    robot = ::TestUtil::createRobotAtPos(Point(-2, 1.8));
+    fsm.process_event(ChipFSM::Update(
+        control_params, TacticUpdate(robot, world, [](std::unique_ptr<Intent>) {})));
+    // Transition to chip_state
+    EXPECT_TRUE(fsm.is(boost::sml::state<ChipFSM::chip_state>));
+    robot = ::TestUtil::createRobotAtPos(Point(-2, 1.8));
+    // Ball is now chipped
+    world =
+        ::TestUtil::setBallVelocity(world, Vector(0, -2.1), Timestamp::fromSeconds(123));
+    EXPECT_TRUE(world.ball().hasBallBeenKicked(Angle::threeQuarter()));
+    // Tactic is done
+    fsm.process_event(ChipFSM::Update(
+        control_params, TacticUpdate(robot, world, [](std::unique_ptr<Intent>) {})));
+    EXPECT_TRUE(fsm.is(boost::sml::X));
+}
