e06e4163d6c0863e12b950d043f25de3fa60d8ad
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-02-13 19:50:48 -0800

    Fix dependencies

--- src/software/ai/hl/stp/play/penalty_kick_enemy_play.cpp
+++ src/software/ai/hl/stp/play/penalty_kick_enemy_play.cpp
@@ -2,7 +2,7 @@
 
 #include "shared/constants.h"
 #include "software/ai/hl/stp/tactic/goalie_tactic.h"
-#include "software/ai/hl/stp/tactic/move_tactic.h"
+#include "software/ai/hl/stp/tactic/move/move_tactic.h"
 #include "software/util/design_patterns/generic_factory.h"
 
 bool PenaltyKickEnemyPlay::isApplicable(const World &world) const
