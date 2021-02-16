b674f1cd6ef457c30a71cadbad838e90d17ce656
Jordan Liu <jordan.zliu@gmail.com>
2021-02-16 13:48:59 -0800

    notebooks as .py files with jupytext extension

--- src/python_tools/jupyter.py
+++ src/python_tools/jupyter.py
@@ -1,8 +1,21 @@
 from notebook.notebookapp import main
 from notebook.nbextensions import install_nbextension_python, enable_nbextension_python
+from notebook.serverextensions import toggle_serverextension_python
 import sys
+import subprocess
 
 if __name__ == "__main__":
+    # a very nasty hack to install the jupytext server extension into the hermetic interpreter
+    # we CANNOT do this with bazel because adding jupytext to the requirements.txt and then
+    # adding it as a dependency causes a bazel dependency cycle
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "jupytext", "--user"])
+    toggle_serverextension_python("jupytext")
+
+    # install and enable the jupytext notebook extension
+    install_nbextension_python("jupytext", user=True)
+    enable_nbextension_python("jupytext")
+
+
     # we have to manually enable the "widgetsnbextension" notebook extension to use ipywidgets inside our hermetic
     # python interpreter
     install_nbextension_python("widgetsnbextension", user=True)
