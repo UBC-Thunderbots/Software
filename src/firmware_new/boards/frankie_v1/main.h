bb999ba0305e6f44c2fd3ea5fc1e88bdaf431005
akhil <akhil.veeraghanta@outlook.com>
2021-02-19 19:50:17 -0800

    fix formatting

--- src/firmware_new/boards/frankie_v1/main.h
+++ src/firmware_new/boards/frankie_v1/main.h
@@ -54,7 +54,7 @@ extern "C"
     /* Exported functions prototypes ---------------------------------------------*/
     void Error_Handler(void);
 
-/* USER CODE BEGIN EFP */
+    /* USER CODE BEGIN EFP */
 
     void initIoDrivetrain(void);
     void initIoNetworking(void);
