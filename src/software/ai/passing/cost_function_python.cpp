2050741c940e92c7e16b8979e2e0b08d0557d000
Jordan Liu <jordan.zliu@gmail.com>
2021-02-21 13:39:18 -0800

    fix for hermetic interpreter

--- src/software/ai/passing/cost_function_python.cpp
+++ src/software/ai/passing/cost_function_python.cpp
@@ -9,8 +9,8 @@
 
 namespace py = pybind11;
 
-double ratePassWrapper(const std::string& ssl_wrapper_world_string,
-                       py::dict pass_dict, bool receive_and_dribble)
+double ratePassWrapper(const std::string& ssl_wrapper_world_string, py::dict pass_dict,
+                       bool receive_and_dribble)
 {
     SSLProto::SSL_WrapperPacket wrapper_packet;
     wrapper_packet.ParseFromString(ssl_wrapper_world_string);
@@ -23,7 +23,8 @@ double ratePassWrapper(const std::string& ssl_wrapper_world_string,
     SensorFusion sensor_fusion(sensor_fusion_config);
     sensor_fusion.processSensorProto(sensor_proto);
     auto world_or_null = sensor_fusion.getWorld();
-    if (!world_or_null) {
+    if (!world_or_null)
+    {
         throw std::invalid_argument("SensorFusion does not have a world!");
     }
 
@@ -32,16 +33,17 @@ double ratePassWrapper(const std::string& ssl_wrapper_world_string,
     Point receiver_point(pass_dict["receiver_point_x"].cast<double>(),
                          pass_dict["receiver_point_y"].cast<double>());
     auto pass_speed = pass_dict["pass_speed"].cast<double>();
-    PassType pass_type = receive_and_dribble ?
-        PassType::RECEIVE_AND_DRIBBLE : PassType::ONE_TOUCH_SHOT;
+    PassType pass_type =
+        receive_and_dribble ? PassType::RECEIVE_AND_DRIBBLE : PassType::ONE_TOUCH_SHOT;
 
     Pass pass(passer_point, receiver_point, pass_speed,
-        world_or_null->getMostRecentTimestamp());
+              world_or_null->getMostRecentTimestamp());
 
     return ratePass(*world_or_null, pass, std::nullopt, std::nullopt, pass_type);
 }
 
-PYBIND11_MODULE(cost_function_python, m) {
+PYBIND11_MODULE(cost_function_python, m)
+{
     m.def("ratePass", &ratePassWrapper, py::arg("ssl_wrapper_world_string"),
-        py::arg("pass_dict"), py::arg("receive_and_dribble"));
+          py::arg("pass_dict"), py::arg("receive_and_dribble"));
 }
