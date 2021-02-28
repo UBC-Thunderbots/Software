42153653670a674fb6c4efab2782b9ff0edd3acc
Arun <arun.sfc@outlook.com>
2021-02-27 16:21:11 -0800

    fix validation build

--- src/software/simulated_tests/terminating_validation_functions/robots_away_from_ball_validation.cpp
+++ src/software/simulated_tests/terminating_validation_functions/robots_away_from_ball_validation.cpp
@@ -5,7 +5,7 @@ void robotsAwayFromBall(double min_distance, std::vector<RobotId> excluded_robot
                         ValidationCoroutine::push_type& yield)
 {
     auto all_robots_except_excluded_away_from_ball =
-        [excluded_robots](std::shared_ptr<World> world_ptr) {
+        [excluded_robots,  min_distance](std::shared_ptr<World> world_ptr) {
             Point ball_position = world_ptr->ball().position();
             for (const auto& robot : world_ptr->friendlyTeam().getAllRobots())
             {
