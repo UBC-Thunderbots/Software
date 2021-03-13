8d854e1b0ddeae9a4eec5928d60c737c92488f05
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-03-12 16:43:44 -0800

    Fix STP tests

--- src/software/ai/hl/stp/stp_test.cpp
+++ src/software/ai/hl/stp/stp_test.cpp
@@ -6,7 +6,6 @@
 #include <exception>
 
 #include "shared/parameter/cpp_dynamic_parameters.h"
-#include "software/ai/hl/stp/play/halt_play.h"
 #include "software/ai/hl/stp/play/test_plays/halt_test_play.h"
 #include "software/ai/hl/stp/play/test_plays/move_test_play.h"
 #include "software/test_util/test_util.h"
@@ -48,22 +47,20 @@ class STPTest : public ::testing::Test
 TEST_F(STPTest, test_only_test_plays_are_registered_in_play_factory)
 {
     auto play_names = GenericFactory<std::string, Play, PlayConfig>::getRegisteredNames();
-    EXPECT_EQ(3, play_names.size());
+    EXPECT_EQ(2, play_names.size());
     EXPECT_EQ(std::count(play_names.begin(), play_names.end(), TYPENAME(MoveTestPlay)),
               1);
     EXPECT_EQ(std::count(play_names.begin(), play_names.end(), TYPENAME(HaltTestPlay)),
               1);
-    EXPECT_EQ(std::count(play_names.begin(), play_names.end(), TYPENAME(HaltPlay)), 1);
 }
 
 TEST_F(STPTest, test_exception_thrown_when_no_play_applicable)
 {
     // Put the ball where both its x and y coordinates are negative. Neither test Play
-    // is applicable in this case so we should see the HaltPlay being set
+    // is applicable in this case
     world = world =
         ::TestUtil::setBallPosition(world, Point(-1, -1), Timestamp::fromSeconds(0));
-
-    EXPECT_EQ(TYPENAME(*stp.calculateNewPlay(world)), TYPENAME(HaltPlay));
+    EXPECT_THROW(stp.calculateNewPlay(world), std::runtime_error);
 }
 
 TEST_F(STPTest, test_calculate_new_play_when_one_play_valid)
@@ -93,10 +90,10 @@ TEST_F(STPTest, test_calculate_new_play_when_multiple_plays_valid)
     }
 
     std::vector<std::string> expected_play_names = {
-        TYPENAME(HaltTestPlay), TYPENAME(HaltTestPlay), TYPENAME(HaltPlay),
-        TYPENAME(MoveTestPlay), TYPENAME(HaltPlay),     TYPENAME(HaltTestPlay),
-        TYPENAME(MoveTestPlay), TYPENAME(HaltTestPlay), TYPENAME(HaltTestPlay),
-        TYPENAME(MoveTestPlay),
+        TYPENAME(MoveTestPlay), TYPENAME(MoveTestPlay), TYPENAME(HaltTestPlay),
+        TYPENAME(HaltTestPlay), TYPENAME(HaltTestPlay), TYPENAME(MoveTestPlay),
+        TYPENAME(MoveTestPlay), TYPENAME(MoveTestPlay), TYPENAME(MoveTestPlay),
+        TYPENAME(HaltTestPlay),
     };
 
     EXPECT_EQ(expected_play_names, actual_play_names);
