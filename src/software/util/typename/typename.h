bb644531c35e851526658b07686f6de30cf0f650
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-03-14 14:12:28 -0700

    objectTypeName function and CLASS_TYPENAME macro

--- src/software/util/typename/typename.h
+++ src/software/util/typename/typename.h
@@ -11,7 +11,20 @@
  *
  * @return the demangled string representation
  */
-std::string demangle_typeid(const char* mangled_name);
+std::string demangleTypeId(const char* mangled_name);
+
+/**
+ * Gets the demangled typeid name of an object reference
+ *
+ * @param obj_ref The reference to the object
+ *
+ * @return the demangled string representation
+ */
+template <typename T>
+std::string objectTypeName(const T& obj_ref)
+{
+return demangleTypeId(typeid(obj_ref).name());
+}
 
 /**
  * MACRO to get typename of the object, i.e.
@@ -24,4 +37,4 @@ std::string demangle_typeid(const char* mangled_name);
  *
  * @return the string representation of the object
  */
-#define TYPENAME(object) (demangle_typeid(typeid(object).name()))
+#define CLASS_TYPENAME(object) (demangle_typeid(typeid(object).name()))
