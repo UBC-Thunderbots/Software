5795de2b47d8e318c5755952b008de3298e2878a
phil <phillipdumitru71@gmail.com>
2021-04-07 21:39:37 -0700

    Cleaned up Dockerfile, changed setup_repo script

--- docker/getRepo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Script to pull Thunderbots/Software 
-#repository from github
-
-echo "Creating Directory"
-mkdir Thunderbots || echo "Directory Exists"
-cd Thunderbots
-git clone https://github.com/UBC-Thunderbots/Software.git || echo "Failed Clone"
