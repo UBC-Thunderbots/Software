5be2e4ee34ed389125e93982b42b3b6773810cbb
Akhil Veeraghanta <31548846+akhilveeraghanta@users.noreply.github.com>
2021-04-07 21:15:08 -0700

    Zone based Passing (#1980)
    * introduce pitch division abstraction
    
    * wip pass evaluation
    
    * finish pass evaluation
    
    * fix cost function tests
    
    * cost function tests pass
    
    * pass generator test starting to get patched up
    
    * everything builds and runs
    
    * corner kick play test works
    
    * finalize cost function
    
    * undo cherry pick change
    
    * shoot or pass play fix
    
    * fix pr comments and do very minor tuning

--- src/software/ai/hl/stp/tactic/passer_tactic.cpp
+++ src/software/ai/hl/stp/tactic/passer_tactic.cpp
@@ -6,7 +6,6 @@
 #include "software/ai/hl/stp/action/move_action.h"
 #include "software/logger/logger.h"
 #include "software/world/ball.h"
-
 PasserTactic::PasserTactic(Pass pass, const Ball& ball, const Field& field,
                            bool loop_forever)
     : Tactic(loop_forever, {RobotCapability::Kick, RobotCapability::Move}),
@@ -32,43 +31,17 @@ double PasserTactic::calculateRobotCost(const Robot& robot, const World& world) 
     // Prefer robots closer to the pass start position
     // We normalize with the total field length so that robots that are within the field
     // have a cost less than 1
-    double cost =
-        (robot.position() - pass.passerPoint()).length() / world.field().totalXLength();
+    double cost = (robot.position() - world.ball().position()).length() /
+                  world.field().totalXLength();
     return std::clamp<double>(cost, 0, 1);
 }
 
 void PasserTactic::calculateNextAction(ActionCoroutine::push_type& yield)
 {
-    // If the ball is moving, we are likely already in a live game scenario and
-    // so we need to collect the ball before we can pass. If the ball is not moving,
-    // we are likely in a set play and so we don't need to initially collect the ball
-    if (ball.velocity().length() > INTERCEPT_BALL_SPEED_THRESHOLD)
-    {
-        auto intercept_action = std::make_shared<InterceptBallAction>(field, ball);
-        do
-        {
-            intercept_action->updateControlParams(*robot_);
-            yield(intercept_action);
-        } while (!intercept_action->done());
-    }
-
     // Move to a position just behind the ball (in the direction of the pass)
     // until it's time to perform the pass
     auto move_action = std::make_shared<MoveAction>(
         true, MoveAction::ROBOT_CLOSE_TO_DEST_THRESHOLD, Angle());
-    while (ball.timestamp() < pass.startTime())
-    {
-        // We want to wait just behind where the pass is supposed to start, so that the
-        // ball is *almost* touching the kicker
-        Vector ball_offset =
-            Vector::createFromAngle(pass.passerOrientation())
-                .normalize(DIST_TO_FRONT_OF_ROBOT_METERS + BALL_MAX_RADIUS_METERS * 2);
-        Point wait_position = pass.passerPoint() - ball_offset;
-
-        move_action->updateControlParams(*robot_, wait_position, pass.passerOrientation(),
-                                         0, DribblerMode::OFF, BallCollisionType::ALLOW);
-        yield(move_action);
-    }
 
     Angle kick_direction;
 
@@ -83,7 +56,7 @@ void PasserTactic::calculateNextAction(ActionCoroutine::push_type& yield)
 
         // We want to keep trying to kick until the ball is moving along the pass
         // vector with sufficient velocity
-        kick_direction = (pass.receiverPoint() - ball.position()).orientation();
+        kick_direction = pass.passerOrientation();
 
     } while (!ball.hasBallBeenKicked(kick_direction));
 }
@@ -91,9 +64,4 @@ void PasserTactic::calculateNextAction(ActionCoroutine::push_type& yield)
 void PasserTactic::accept(TacticVisitor& visitor) const
 {
     visitor.visit(*this);
-}
-
-Ball PasserTactic::getBall() const
-{
-    return this->ball;
 }
