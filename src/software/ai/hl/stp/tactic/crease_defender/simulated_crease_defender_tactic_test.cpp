899682e6eee98eb712b6739dc7747b6b81729eac
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-05-01 01:20:03 -0700

    Address pr comments

--- src/software/ai/hl/stp/tactic/crease_defender/simulated_crease_defender_tactic_test.cpp
+++ src/software/ai/hl/stp/tactic/crease_defender/simulated_crease_defender_tactic_test.cpp
@@ -16,7 +16,7 @@
 class SimulatedCreaseDefenderTacticPositionTest
     : public SimulatedTacticTestFixture,
       public ::testing::WithParamInterface<
-          std::tuple<Point, CreaseDefenderAlignment, Rectangle>>
+          std::tuple<Point, CreaseDefenderAlignment, unsigned int>>
 {
 };
 
@@ -68,7 +68,8 @@ TEST_P(SimulatedCreaseDefenderTacticPositionTest, crease_defender_test)
 {
     Point enemy_threat_point          = std::get<0>(GetParam());
     CreaseDefenderAlignment alignment = std::get<1>(GetParam());
-    Rectangle target_defend_location  = std::get<2>(GetParam());
+    unsigned int target_defend_region = std::get<2>(GetParam());
+    ASSERT_LE(target_defend_region, 5);
 
     Point initial_position = Point(-3, 1.5);
     setBallState(BallState(Point(4.5, -3), Vector(0, 0)));
@@ -89,14 +90,37 @@ TEST_P(SimulatedCreaseDefenderTacticPositionTest, crease_defender_test)
     setMotionConstraints({MotionConstraint::ENEMY_ROBOTS_COLLISION,
                           MotionConstraint::FRIENDLY_DEFENSE_AREA});
 
+    Rectangle defense_area         = field().friendlyDefenseArea();
+    Rectangle field_lines          = field().fieldLines();
+    Vector defense_area_half_width = Vector(defense_area.xLength() / 2, 0);
+
+    std::vector<Rectangle> defender_regions = {
+        Rectangle(field().friendlyCornerPos(),
+                  defense_area.negXPosYCorner() + defense_area_half_width),
+        Rectangle(field().friendlyCornerPos() + defense_area_half_width,
+                  defense_area.posXPosYCorner()),
+        Rectangle(Point(defense_area.xMax(), field_lines.yMax()), field().centerPoint()),
+        Rectangle(Point(defense_area.xMax(), field_lines.yMin()), field().centerPoint()),
+        Rectangle(field().friendlyCornerNeg() + defense_area_half_width,
+                  defense_area.posXNegYCorner()),
+        Rectangle(field().friendlyCornerNeg(),
+                  defense_area.negXNegYCorner() + defense_area_half_width),
+    };
+
     std::vector<ValidationFunction> terminating_validation_functions = {
-        [target_defend_location, tactic](std::shared_ptr<World> world_ptr,
-                                         ValidationCoroutine::push_type& yield) {
+        [target_defend_region, defender_regions, tactic](
+            std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
+            // Check that tactic is done
+            while (!tactic->done())
+            {
+                yield("Tactic not done");
+            }
             // Check that conditions hold for 1000 ticks
             unsigned num_ticks = 1000;
             for (unsigned i = 0; i < num_ticks; i++)
             {
-                robotInPolygon(0, target_defend_location, world_ptr, yield);
+                robotInPolygon(0, defender_regions[target_defend_region], world_ptr,
+                               yield);
             }
         }};
 
@@ -106,27 +130,41 @@ TEST_P(SimulatedCreaseDefenderTacticPositionTest, crease_defender_test)
             Duration::fromSeconds(10));
 }
 
+// We check if the robot is in one of the following regions
+//  ┌───┬───┬────────────────┐
+//  │   │   │                │
+//  │ 0 │ 1 │                C
+//  │   │   │                E
+//  │   │   │                N
+//  ├───┴───┤       2        T
+//  E       │                R
+//  N  Def. │                E
+//  D       │                │
+//  L       ├────────────────┤
+//  I  Area │                │
+//  N       │                │
+//  E       │                L
+//  ├───┬───┤                I
+//  │   │   │       3        N
+//  │ 5 │ 4 │                E
+//  │   │   │                │
+//  │   │   │                │
+//  └───┴───┴────────────────┘
+
 INSTANTIATE_TEST_CASE_P(
     CreaseDefenderEnvironment, SimulatedCreaseDefenderTacticPositionTest,
     ::testing::Values(
         // Enemy threat in front of crease, LEFT
-        std::make_tuple(Point(1, 2.5), CreaseDefenderAlignment::LEFT,
-                        Rectangle(Point(-3.5, 1), Point(-3.25, 0.8))),
+        std::make_tuple(Point(1, 2.5), CreaseDefenderAlignment::LEFT, 2),
         // Enemy threat in front of crease, CENTRE
-        std::make_tuple(Point(1, -2.5), CreaseDefenderAlignment::CENTRE,
-                        Rectangle(Point(-3.5, -0.5), Point(-3.25, -0.7))),
+        std::make_tuple(Point(1, -2.5), CreaseDefenderAlignment::CENTRE, 3),
         // Enemy threat in front of crease, RIGHT
-        std::make_tuple(Point(1.5, 2), CreaseDefenderAlignment::RIGHT,
-                        Rectangle(Point(-3.25, 0.7), Point(-3, 0.5))),
+        std::make_tuple(Point(1.5, 2), CreaseDefenderAlignment::RIGHT, 2),
         // Enemy threat left side of crease, RIGHT
-        std::make_tuple(Point(-4.25, 2.5), CreaseDefenderAlignment::RIGHT,
-                        Rectangle(Point(-4.3, 1), Point(-4.5, 1.2))),
+        std::make_tuple(Point(-3.5, 2.5), CreaseDefenderAlignment::RIGHT, 1),
         // Enemy threat left side of crease, CENTRE
-        std::make_tuple(Point(-4, 2.5), CreaseDefenderAlignment::CENTRE,
-                        Rectangle(Point(-4.2, 1), Point(-4.3, 1.2))),
+        std::make_tuple(Point(-4, 2.5), CreaseDefenderAlignment::CENTRE, 0),
         // Enemy threat right side of crease, RIGHT
-        std::make_tuple(Point(-4, -2), CreaseDefenderAlignment::RIGHT,
-                        Rectangle(Point(-4.3, -1), Point(-4.1, -1.2))),
+        std::make_tuple(Point(-4, -2), CreaseDefenderAlignment::RIGHT, 5),
         // Enemy threat right side of crease, LEFT
-        std::make_tuple(Point(-4.25, -2), CreaseDefenderAlignment::LEFT,
-                        Rectangle(Point(-4.3, -1), Point(-4.1, -1.2)))));
+        std::make_tuple(Point(-4.25, -2), CreaseDefenderAlignment::LEFT, 5)));
