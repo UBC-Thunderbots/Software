b887503c89c448eecfd2413f7dc46ae65461fccb
Resocram <marcoser2000@gmail.com>
2021-05-06 16:29:15 -0700

    Used robotInPolygon to detect if robot is blocking shot

--- src/software/ai/hl/stp/tactic/shadow_enemy/simulated_shadow_enemy_tactic_test.cpp
+++ src/software/ai/hl/stp/tactic/shadow_enemy/simulated_shadow_enemy_tactic_test.cpp
@@ -3,10 +3,10 @@
 #include <utility>
 
 #include "software/ai/hl/stp/tactic/shadow_enemy/shadow_enemy_tactic.h"
-#include "software/geom/algorithms/contains.h"
+#include "software/geom/triangle.h"
 #include "software/simulated_tests/simulated_tactic_test_fixture.h"
 #include "software/simulated_tests/terminating_validation_functions/ball_kicked_validation.h"
-#include "software/simulated_tests/terminating_validation_functions/robot_blocking_shot.h"
+#include "software/simulated_tests/terminating_validation_functions/robot_in_polygon_validation.h"
 #include "software/simulated_tests/terminating_validation_functions/robot_state_validation.h"
 #include "software/test_util/test_util.h"
 #include "software/time/duration.h"
@@ -139,7 +139,10 @@ TEST_F(SimulatedShadowEnemyTacticTest, test_block_net_then_steal_and_chip)
             // As the shadowee is located at (0,-2), we first find the shot
             // vector to our net and then normalize this vector to a distance
             // of 2 away from the shadowee
-            robotBlockingShot(0, world_ptr, yield);
+            Triangle shotTriangle{world_ptr->field().friendlyGoalpostPos(),
+                                  world_ptr->field().friendlyGoalpostNeg(),
+                                  world_ptr->ball().position()};
+            robotInPolygon(0, shotTriangle, world_ptr, yield);
         },
         [this, tactic, shadower](std::shared_ptr<World> world_ptr,
                                  ValidationCoroutine::push_type& yield) {
@@ -185,7 +188,10 @@ TEST_F(SimulatedShadowEnemyTacticTest, test_block_net_if_enemy_threat_is_null)
             // As the shadowee is located at (0,-2), we first find the shot
             // vector to our net and then normalize this vector to a distance
             // of 2 away from the shadowee
-            robotBlockingShot(0, world_ptr, yield);
+            Triangle shotTriangle{world_ptr->field().friendlyGoalpostPos(),
+                                  world_ptr->field().friendlyGoalpostNeg(),
+                                  world_ptr->ball().position()};
+            robotInPolygon(0, shotTriangle, world_ptr, yield);
         }};
 
     std::vector<ValidationFunction> non_terminating_validation_functions = {};
