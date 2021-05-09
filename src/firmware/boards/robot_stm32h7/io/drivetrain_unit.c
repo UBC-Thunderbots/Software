693242c5874ca9b7890bc548d485f272f4839793
akhil <akhil.veeraghanta@outlook.com>
2021-05-09 14:52:52 -0700

    cleanup

--- src/firmware/boards/robot_stm32h7/io/drivetrain_unit.c
+++ src/firmware/boards/robot_stm32h7/io/drivetrain_unit.c
@@ -24,6 +24,9 @@ void io_drivetrain_unit_applyForce(DrivetrainUnit_t* drive_train_unit,
     // NOTE: This is a placeholder implementation. With the new controller we will not
     //       control each wheel by applying "force" to it, but rather by directly
     //       applying voltage
+
+    float pwm_percentage = fminf(1.0f, fabsf(force_newtons) / 255.0f);
+
     if (force_newtons > 0)
     {
         io_allegro_a3931_motor_driver_setDirection(drive_train_unit->motor_driver,
@@ -36,5 +39,5 @@ void io_drivetrain_unit_applyForce(DrivetrainUnit_t* drive_train_unit,
     }
 
     io_allegro_a3931_motor_setPwmPercentage(drive_train_unit->motor_driver,
-                                            force_newtons);
+                                            pwm_percentage);
 }
