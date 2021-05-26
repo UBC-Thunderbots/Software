020bc4651c722c31f703181c5b4adc5431d35fe8
Jordan Liu <jordan.zliu@gmail.com>
2021-05-25 23:23:10 -0700

    make the evaluation function

---
+++ src/software/ai/evaluation/keep_away.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "shared/parameter/cpp_dynamic_parameters.h"
+#include "software/ai/passing/pass.h"
+#include "software/world/world.h"
+
+Point findKeepAwayTargetPoint(const Point& ball_possessor_point,
+                              const Pass& best_pass_so_far, const World& world,
+                              std::shared_ptr<const PassingConfig> passing_config);
---
+++ src/software/python_bindings/keep_away.cpp
@@ -0,0 +1,20 @@
+#include <pybind11/pybind11.h>
+
+#include "software/ai/evaluation/keep_away.h"
+#include "software/python_bindings/pass_utilities.h"
+#include "software/python_bindings/python_binding_utilities.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(keep_away, m)
+{
+    m.def("findKeepAwayTargetPosition",
+          [](const World& world, const Point& possessor_position,
+             py::dict best_pass_so_far_dict, py::dict passing_config_dict) {
+              Pass best_pass_so_far = createPassFromDict(best_pass_so_far_dict);
+              auto passing_config   = std::make_shared<PassingConfig>();
+              updateDynamicParametersConfigFromDict(passing_config, passing_config_dict);
+              return findKeepAwayTargetPoint(possessor_position,
+                                                best_pass_so_far, world, passing_config);
+          });
+}
