7cc82612bae1100242b063f45fef35996341e6ae
akhil <akhil.veeraghanta@outlook.com>
2021-05-26 16:59:54 -0700

    fix comment

--- src/firmware/boards/robot_stm32h7/unused.h
+++ src/firmware/boards/robot_stm32h7/unused.h
@@ -3,9 +3,6 @@
 /**
  * Marks a function parameter as unused.
  *
- * This macro should be wrapped around the parameter name in the function definition’s
- * parameter list.
- *
  * @param x the name of the parameter
  */
 #define UNUSED(x) (void)(x)
