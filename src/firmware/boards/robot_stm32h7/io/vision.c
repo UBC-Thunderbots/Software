7beddb1e551b3457e9b819cfb96730e70c9ab726
akhil <akhil.veeraghanta@outlook.com>
2021-05-25 20:28:46 -0700

    fix pr comments

--- src/firmware/boards/robot_stm32h7/io/vision.c
+++ src/firmware/boards/robot_stm32h7/io/vision.c
@@ -2,52 +2,52 @@
 
 float io_vision_getBallPositionX(void)
 {
-    // TODO (#2083) implement this stuff
+    // TODO (#2083) implement
     return 0.0f;
 }
 float io_vision_getBallPositionY(void)
 {
-    // TODO (#2083) implement this stuff
+    // TODO (#2083) implement
     return 0.0f;
 }
 float io_vision_getBallVelocityX(void)
 {
-    // TODO (#2083) implement this stuff
+    // TODO (#2083) implement
     return 0.0f;
 }
 float io_vision_getBallVelocityY(void)
 {
-    // TODO (#2083) implement this stuff
+    // TODO (#2083) implement
     return 0.0f;
 }
 float io_vision_getRobotPositionX(void)
 {
-    // TODO (#2083) implement this stuff
+    // TODO (#2083) implement
     return 0.0f;
 }
 float io_vision_getRobotPositionY(void)
 {
-    // TODO (#2083) implement this stuff
+    // TODO (#2083) implement
     return 0.0f;
 }
 float io_vision_getRobotOrientation(void)
 {
-    // TODO (#2083) implement this stuff
+    // TODO (#2083) implement
     return 0.0f;
 }
 
 float io_vision_getRobotVelocityX(void)
 {
-    // TODO (#2083) implement this stuff
+    // TODO (#2083) implement
     return 0.0f;
 }
 float io_vision_getRobotVelocityY(void)
 {
-    // TODO (#2083) implement this stuff
+    // TODO (#2083) implement
     return 0.0f;
 }
 float io_vision_getRobotAngularVelocity(void)
 {
-    // TODO (#2083) implement this stuff
+    // TODO (#2083) implement
     return 0.0f;
 }
--- src/firmware/boards/robot_stm32h7/io/vision.h
+++ src/firmware/boards/robot_stm32h7/io/vision.h
@@ -1,15 +1,35 @@
 #pragma once
 
-// TODO (#2083) implement this stuff
+/*
+ * @returns the x,y positions of the ball
+ */
 float io_vision_getBallPositionX(void);
 float io_vision_getBallPositionY(void);
+
+/*
+ * @returns the x,y components of the ball's velocity
+ */
 float io_vision_getBallVelocityX(void);
 float io_vision_getBallVelocityY(void);
 
+/*
+ * @returns the x,y positions of this robot
+ */
 float io_vision_getRobotPositionX(void);
 float io_vision_getRobotPositionY(void);
-float io_vision_getRobotOrientation(void);
 
+/*
+ * @returns the x,y components of this robots velocity
+ */
 float io_vision_getRobotVelocityX(void);
 float io_vision_getRobotVelocityY(void);
+
+/*
+ * @returns the orientation of this robot
+ */
+float io_vision_getRobotOrientation(void);
+
+/*
+ * @returns the angular velocity of this robot
+ */
 float io_vision_getRobotAngularVelocity(void);
