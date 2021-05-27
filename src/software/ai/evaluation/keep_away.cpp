3b0f3b1afb29eb83ab021085c89a91a540230ce4
Jordan Liu <jordan.zliu@gmail.com>
2021-05-26 23:52:06 -0700

    some more cleanup

--- src/software/ai/evaluation/keep_away.cpp
+++ src/software/ai/evaluation/keep_away.cpp
@@ -6,11 +6,16 @@
 
 
 Point findKeepAwayTargetPoint(const Point& ball_possessor_position,
-                              const Pass& best_pass_so_far, const World& world,
-                              std::shared_ptr<const PassingConfig> passing_config)
+                              const Pass& best_pass_so_far, const World& world)
 {
     static constexpr auto KEEPAWAY_SEARCH_CIRCLE_RADIUS = 0.3;
     static constexpr auto CIRCLE_SIGMOID_WIDTH          = 0.1;
+
+    // the default values for these passing parameters
+    // TODO: cleanup passing parameters as part of #1987
+    static constexpr double ENEMY_PROXIMITY_IMPORTANCE = 0.5;
+    static const auto ENEMY_REACTION_TIME              = Duration::fromSeconds(0.4);
+    static constexpr auto GRADIENT_STEPS_PER_ITER      = 2;
 
     // the region to which the optimization is (effectively) constrained to
     Circle keepaway_search_region(ball_possessor_position, KEEPAWAY_SEARCH_CIRCLE_RADIUS);
@@ -20,13 +25,14 @@ Point findKeepAwayTargetPoint(const Point& ball_possessor_position,
     const auto keepaway_point_cost = [&](const std::array<double, 2>& passer_pt_array) {
         Point passer_pt(std::get<0>(passer_pt_array), std::get<1>(passer_pt_array));
         Pass pass(passer_pt, best_pass_so_far.receiverPoint(), best_pass_so_far.speed());
-        return ratePassEnemyRisk(world.enemyTeam(), pass, passing_config) *
+        return ratePassEnemyRisk(world.enemyTeam(), pass, ENEMY_REACTION_TIME,
+                                 ENEMY_PROXIMITY_IMPORTANCE) *
                circleSigmoid(keepaway_search_region, passer_pt, CIRCLE_SIGMOID_WIDTH);
     };
     GradientDescentOptimizer<2> optimizer;
     auto passer_pt_array = optimizer.maximize(
         keepaway_point_cost,
         std::array<double, 2>{ball_possessor_position.x(), ball_possessor_position.y()},
-        passing_config->getNumberOfGradientDescentStepsPerIter()->value());
+        GRADIENT_STEPS_PER_ITER);
     return Point(std::get<0>(passer_pt_array), std::get<1>(passer_pt_array));
 }
