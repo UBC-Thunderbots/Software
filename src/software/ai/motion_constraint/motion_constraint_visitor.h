9d1bfb586c2b43f9d2c9478866f5901e9771e799
Tiger <980401@gmail.com>
2021-06-03 20:21:43 -0700

    fixed minor bugs, all software tests pass

--- src/software/ai/motion_constraint/motion_constraint_visitor.h
+++ src/software/ai/motion_constraint/motion_constraint_visitor.h
@@ -38,7 +38,6 @@ class MotionConstraintVisitor : public TacticVisitor
     void visit(const GoalieTestTactic &tactic) override;
     void visit(const DribbleTactic &tactic) override;
     void visit(const GetBehindBallTactic &tactic) override;
-    void visit(const MoveGoalieToGoalLineTactic &tactic) override;
     void visit(const PivotKickTactic &tactic) override;
     void visit(const MoveGoalieToGoalLineTactic &tactic) override;
 
