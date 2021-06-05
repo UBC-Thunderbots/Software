49c0e0eafde0f9666c3a660fc03b171422e08448
nima <nimazareian80@gmail.com>
2021-06-05 10:35:27 -0700

    Removed extra comments

--- src/software/geom/algorithms/rasterize_test.cpp
+++ src/software/geom/algorithms/rasterize_test.cpp
@@ -25,12 +25,6 @@ namespace TestUtil
                                   compare_based_on_distance_to_p);
             EXPECT_LE(distance(p, closest_p), max_dist);
         }
-        //        LOG(INFO) << "[TEST ROBOT " << robot_log.robot_id << "]["
-        //                  << TbotsProto::LogLevel_Name(
-        //                          static_cast<TbotsProto::LogLevel>(robot_log.log_level))
-        //                  << "]"
-        //                  << "[" << robot_log.file_name << ":" << robot_log.line_number
-        //                  << "]: " << robot_log.log_msg;
     }
 };  // namespace TestUtil
 
