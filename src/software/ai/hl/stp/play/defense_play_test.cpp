c4a5d42285edc95d48392213ba3c1cb9ac814777
LiCody <licody2000@gmail.com>
2021-06-08 22:07:36 -0700

    update defense play

--- src/software/ai/hl/stp/play/defense_play_test.cpp
+++ src/software/ai/hl/stp/play/defense_play_test.cpp
@@ -46,9 +46,46 @@ TEST_F(DefensePlayTest, test_defense_play)
         }};
 
     std::vector<ValidationFunction> non_terminating_validation_functions = {
-        // TODO: Implement proper validation
-        // https://github.com/UBC-Thunderbots/Software/issues/1971
         [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {}};
+
+    runTest(field, ball_state, friendly_robots, enemy_robots,
+            terminating_validation_functions, non_terminating_validation_functions,
+            Duration::fromSeconds(10));
+}
+
+TEST_F(DefensePlayTest, test_defense_play_one_threat)
+{
+    BallState ball_state(Point(0.9, 2.85), Vector(0, 0));
+    auto friendly_robots = TestUtil::createStationaryRobotStatesWithId(
+            {Point(-4.5, 0), Point(-3, 1.5), Point(-3, 0.5), Point(-3, -0.5), Point(-3, -1.5),
+             Point(-3, -3.0)});
+    setFriendlyGoalie(0);
+    auto enemy_robots = TestUtil::createStationaryRobotStatesWithId({
+                                                                            field.enemyGoalCenter(),
+                                                                            field.enemyDefenseArea().negXNegYCorner(),
+                                                                            field.enemyDefenseArea().negXPosYCorner(),
+                                                                            field.enemyDefenseArea().posXNegYCorner(),
+                                                                            Point(1, 3),
+                                                                            Point(-1, -0.25),
+                                                                    });
+    setEnemyGoalie(0);
+    setAIPlay(TYPENAME(DefensePlay));
+    setRefereeCommand(RefereeCommand::FORCE_START, RefereeCommand::NORMAL_START);
+
+    std::vector<ValidationFunction> terminating_validation_functions = {
+            // This will keep the test running for 9.5 seconds to give everything enough
+            // time to settle into position and be observed with the Visualizer
+            // TODO: Implement proper validation
+            // https://github.com/UBC-Thunderbots/Software/issues/1971
+            [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
+                while (world_ptr->getMostRecentTimestamp() < Timestamp::fromSeconds(9.5))
+                {
+                    yield("Timestamp not at 9.5s");
+                }
+            }};
+
+    std::vector<ValidationFunction> non_terminating_validation_functions = {
+            [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {}};
 
     runTest(field, ball_state, friendly_robots, enemy_robots,
             terminating_validation_functions, non_terminating_validation_functions,
