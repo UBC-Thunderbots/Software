616c71254a75edc0cf1d52095ad02c06fae8bcc2
Jordan Liu <jordan.zliu@gmail.com>
2021-06-12 14:59:17 -0700

    keep away field boundaries improvement

--- src/software/ai/evaluation/keep_away.cpp
+++ src/software/ai/evaluation/keep_away.cpp
@@ -14,9 +14,9 @@ double ratePasserPointForKeepAway(const Pass& pass, const Team& enemy_team)
 
 
     return ratePassEnemyRisk(enemy_team, pass, ENEMY_REACTION_TIME,
-                             RECEIVER_ENEMY_PROXIMITY_IMPORTANCE) *
-           (1 - calculateProximityRisk(pass.passerPoint(), enemy_team,
-                                       PASSER_ENEMY_PROXIMITY_IMPORTANCE));
+                                 RECEIVER_ENEMY_PROXIMITY_IMPORTANCE) *
+           (1 - std::max(0., calculateProximityRisk(pass.passerPoint(), enemy_team,
+                                                    PASSER_ENEMY_PROXIMITY_IMPORTANCE)));
 }
 
 
@@ -25,7 +25,9 @@ Point findKeepAwayTargetPoint(const World& world, const Pass& best_pass_so_far)
     static constexpr auto KEEPAWAY_SEARCH_CIRCLE_RADIUS = 0.75;
 
     // the width of both the field boundary sigmoid and the circular search region sigmoid
-    static constexpr auto SIGMOID_WIDTH = 0.05;
+    static constexpr auto SIGMOID_WIDTH = 0.1;
+    // the inward offset of the field boundaries to use for the field lines sigmoid
+    static constexpr auto FIELD_SIZE_REDUCTION_M = 0.1;
 
     static constexpr auto GRADIENT_STEPS_PER_ITER = 2;
 
@@ -38,6 +40,15 @@ Point findKeepAwayTargetPoint(const World& world, const Pass& best_pass_so_far)
     // the region to which the optimization is (effectively) constrained to
     Circle keepaway_search_region(world.ball().position(), KEEPAWAY_SEARCH_CIRCLE_RADIUS);
 
+    // a reduced field rectangle to effectively constrain the optimization to field
+    // boundaries
+    const auto& field_bounds       = world.field().fieldLines();
+    Point reduced_bottom_left      = Point(field_bounds.xMin() + FIELD_SIZE_REDUCTION_M,
+                                      field_bounds.yMin() + FIELD_SIZE_REDUCTION_M);
+    Point reduced_top_right        = Point(field_bounds.xMax() - FIELD_SIZE_REDUCTION_M,
+                                    field_bounds.yMax() - FIELD_SIZE_REDUCTION_M);
+    Rectangle reduced_field_bounds = Rectangle(reduced_bottom_left, reduced_top_right);
+
     // the position rating function we want to maximize
     const auto keepaway_point_cost = [&](const std::array<double, 2>& passer_pt_array) {
         Point passer_pt(std::get<0>(passer_pt_array), std::get<1>(passer_pt_array));
@@ -46,7 +57,7 @@ Point findKeepAwayTargetPoint(const World& world, const Pass& best_pass_so_far)
                // constrain the optimization to a circular area around the ball
                circleSigmoid(keepaway_search_region, passer_pt, SIGMOID_WIDTH) *
                // don't try to dribble the ball off the field
-               rectangleSigmoid(world.field().fieldLines(), passer_pt, SIGMOID_WIDTH);
+               rectangleSigmoid(reduced_field_bounds, passer_pt, SIGMOID_WIDTH);
     };
     GradientDescentOptimizer<2> optimizer{PARAM_WEIGHTS};
     auto passer_pt_array = optimizer.maximize(
