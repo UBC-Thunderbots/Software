6586559e4f84432d4abbe3b8bb68ea27b2747697
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-06-14 11:39:14 -0700

    update comment

--- src/software/ai/hl/stp/tactic/goalie/goalie_fsm.h
+++ src/software/ai/hl/stp/tactic/goalie/goalie_fsm.h
@@ -173,7 +173,7 @@ struct GoalieFSM
 
         /**
          * Guard that checks if the ball is moving slower than the panic threshold and is
-         * inside the no-chip rectangle, if true then the goalie should dribble the ball
+         * inside the defense area, if true then the goalie should dribble and chip the ball
          *
          * @param event GoalieFSM::Update
          *
