a2733b6cb9e6db768b5e4a4431fc7f047e7391ab
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-06-15 01:08:29 -0700

    test ci catches failure

--- src/software/ai/hl/stp/play/corner_kick_play_test.cpp
+++ src/software/ai/hl/stp/play/corner_kick_play_test.cpp
@@ -64,7 +64,7 @@ TEST_F(CornerKickPlayTest, test_corner_kick_play_top_right)
 
     std::vector<ValidationFunction> terminating_validation_functions = {
         [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
-            robotReceivedBall(5, world_ptr, yield);
+            robotReceivedBall(4, world_ptr, yield);
             friendlyScored(world_ptr, yield);
         }};
 
