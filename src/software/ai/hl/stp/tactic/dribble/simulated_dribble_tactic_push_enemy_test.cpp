4ae5015b46d184f893a25e0643e3d720027e692e
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-06-15 01:04:06 -0700

    intial sim test

---
+++ src/software/ai/hl/stp/tactic/dribble/simulated_dribble_tactic_push_enemy_test.cpp
@@ -0,0 +1,75 @@
+#include <gtest/gtest.h>
+
+#include <utility>
+
+#include "software/ai/hl/stp/tactic/dribble/dribble_tactic.h"
+#include "software/geom/algorithms/contains.h"
+#include "software/simulated_tests/non_terminating_validation_functions/robot_not_excessively_dribbling_validation.h"
+#include "software/simulated_tests/simulated_tactic_test_fixture.h"
+#include "software/simulated_tests/terminating_validation_functions/ball_at_point_validation.h"
+#include "software/simulated_tests/terminating_validation_functions/robot_received_ball_validation.h"
+#include "software/simulated_tests/terminating_validation_functions/robot_state_validation.h"
+#include "software/simulated_tests/validation/validation_function.h"
+#include "software/test_util/test_util.h"
+#include "software/time/duration.h"
+#include "software/world/world.h"
+
+class SimulatedDribbleTacticTest : public SimulatedTacticTestFixture
+{
+   protected:
+    void checkPossession(std::shared_ptr<DribbleTactic> tactic,
+                         std::shared_ptr<World> world_ptr,
+                         ValidationCoroutine::push_type& yield)
+    {
+        while (!tactic->done())
+        {
+            yield("Tactic not done");
+        }
+        robotReceivedBall(1, world_ptr, yield);
+        auto received_ball_time = world_ptr->getMostRecentTimestamp();
+        while (world_ptr->getMostRecentTimestamp() <
+               received_ball_time + Duration::fromSeconds(1))
+        {
+            yield("Waiting 1 second to see if possession is maintained");
+        }
+        robotReceivedBall(1, world_ptr, yield);
+    }
+
+    void SetUp() override
+    {
+        SimulatedTacticTestFixture::SetUp();
+        setMotionConstraints({MotionConstraint::ENEMY_ROBOTS_COLLISION,
+                              MotionConstraint::ENEMY_DEFENSE_AREA});
+    }
+    Field field = Field::createSSLDivisionBField();
+    std::vector<RobotStateWithId> enemy_robots =
+        TestUtil::createStationaryRobotStatesWithId(
+            {Point(1, 0), Point(1, 2.5), Point(1, -2.5), field.enemyGoalCenter(),
+             field.enemyDefenseArea().negXNegYCorner(),
+             field.enemyDefenseArea().negXPosYCorner()});
+};
+
+TEST_F(SimulatedDribbleTacticTest, test_intercept_ball_behind_enemy_robot)
+{
+    Point initial_position = Point(-3, 1.5);
+    BallState ball_state(Point(3, -2), Vector(-0.5, 1));
+    auto friendly_robots =
+        TestUtil::createStationaryRobotStatesWithId({Point(-3, 2.5), initial_position});
+
+    auto tactic = std::make_shared<DribbleTactic>();
+    setTactic(tactic);
+    setRobotId(1);
+    setMotionConstraints({MotionConstraint::ENEMY_ROBOTS_COLLISION});
+
+    std::vector<ValidationFunction> terminating_validation_functions = {
+        [this, tactic](std::shared_ptr<World> world_ptr,
+                       ValidationCoroutine::push_type& yield) {
+            checkPossession(tactic, world_ptr, yield);
+        }};
+
+    std::vector<ValidationFunction> non_terminating_validation_functions = {};
+
+    runTest(field, ball_state, friendly_robots, enemy_robots,
+            terminating_validation_functions, non_terminating_validation_functions,
+            Duration::fromSeconds(10));
+}
