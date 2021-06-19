600f292d563392eb267f0a36d7e120c3b473ef71
Simon <simon.zheng@alumni.ubc.ca>
2021-06-18 19:54:09 -0700

    Integration testing of Chicker code

--- src/firmware/boards/robot_stm32h7/io/chicker.c
+++ src/firmware/boards/robot_stm32h7/io/chicker.c
@@ -3,16 +3,19 @@
 #include "firmware/app/logger/logger.h"
 #include "firmware/boards/robot_stm32h7/tim.h"
 
-// This is a hard cap on the pulse_width to prevent the fuse from blowing.
-// TODO(new): figure out what this should be (currently set to be very small)
-#define KICKER_MAX_PULSE 1U
+// Identify timers used for either KICK or CHIP
+#define KICK_TIM &htim16
+#define CHIP_TIM &htim17
 
-// This is a hard cap on the pulse_width to prevent the fuse from blowing.
-// TODO (new): figure out what this should be (currently set to be very large)
-#define COLLIDE_TIMEOUT 1000000U
+// This is a hard cap on the pulse_width to minimize capacitor bank discharging
+// This value is experimentally deduced from testing.
+#define KICKER_MAX_PULSE 32000U
 
 // The number of ticks for which no device should be activated because it would interfere
 // with an ongoing fire.
+//
+#define COLLIDE_TIMEOUT 5000000U
+
 // TODO (new): make sure there's no datarace on this variable
 static unsigned int collide_timeout;
 
@@ -35,6 +38,8 @@ static void chicker_fire(ChickerDevice_t device, unsigned int width);
 static void kicker_fire(unsigned int width);
 static unsigned int kicker_speedToPulseWidth(float speed_m_per_s);
 static unsigned int chipper_distanceToPulseWidth(float distance_meters);
+//static unsigned int kicker_timeToPulseWidth(float kick_pulse_time);
+//static unsigned int chipper_timeToPulseWidth(float chip_pulse_time);
 
 void io_chicker_tick()
 {
@@ -75,39 +80,46 @@ void io_chicker_disable_auto_chip()
     // TODO (#2080) implement this
 }
 
+static void kicker_fire(unsigned int width)
+{
+    // TODO(new): test that this actually works
+
+    TIM16->CCR1 = 1;
+    TIM16->ARR = width < KICKER_MAX_PULSE ? width : KICKER_MAX_PULSE;
+    HAL_TIM_OnePulse_Start(KICK_TIM, TIM_CHANNEL_1);
+    // Trigger the timer directly in one pulse mode via the control register CR1
+    // CEN : Counter Enable
+    // OPM : One Pulse Mode
+    TIM16->CR1 = TIM_CR1_CEN | TIM_CR1_OPM;
+
+    /* Testing for one pulse mode
+    //HAL_TIM_OnePulse_DeInit(KICK_TIM);
+    //HAL_TIM_OnePulse_Stop(KICK_TIM, TIM_CHANNEL_1);
+    TIM16->CCR1 = width < KICKER_MAX_PULSE ? width : KICKER_MAX_PULSE;
+    //HAL_TIM_OnePulse_Init(KICK_TIM, TIM_OPMODE_SINGLE);
+    HAL_TIM_OnePulse_Start(KICK_TIM, TIM_CHANNEL_1);
+    TIM16->CR1 = TIM_CR1_CEN | TIM_CR1_OPM;
+    //HAL_TIM_OnePulse_Stop(KICK_TIM, TIM_CHANNEL_1);
+    //HAL_TIM_OnePulse_DeInit(KICK_TIM);
+    */
+}
+
 static void chipper_fire(unsigned int width)
 {
     // TODO (new): implement this
 }
 
-static void kicker_fire(unsigned int width)
-{
-    // TODO(new): test that this actually works
-    HAL_TIM_OnePulse_DeInit(&htim16);
-    TIM16->CCR1 = width < KICKER_MAX_PULSE ? width : KICKER_MAX_PULSE;
-    HAL_TIM_OnePulse_Init(&htim16, TIM_OPMODE_SINGLE);
-    HAL_TIM_OnePulse_Start(&htim16, TIM_CHANNEL_1);
-    HAL_TIM_OnePulse_Stop(&htim16, TIM_CHANNEL_1);
-    HAL_TIM_OnePulse_DeInit(&htim16);
-
-    /* copied from timer branch
-    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
-    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
-    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
-    // Trigger one pulse
-    HAL_TIM_OnePulse_Start(&htim16, TIM_CHANNEL_1);
-    TIM16->CR1 = TIM_CR1_CEN | TIM_CR1_OPM;
-    HAL_TIM_OnePulse_Stop(&htim16, TIM_CHANNEL_1);
-    */
-}
-
 static unsigned int kicker_speedToPulseWidth(float speed_m_per_s)
 {
-    // TODO (new): implement this
-    return 0;
+/*    // TODO (new): implement conversion from speed to pulse duration (time)
+    float kick_time = 0.006f; //expect single digit ms range
+    // TODO (new): programatically calculate pulse duration from RCC registers
+    unsigned int pulse_width =
+            __LL_TIM_CALC_PULSE(120000000,
+                                __HAL_TIM_GET_ICPRESCALER(KICK_TIM, TIM_CHANNEL_1),
+
+            )*/
+    return 30000;
 }
 
 static unsigned int chipper_distanceToPulseWidth(float distance_meters)
