4017f57fea58d5c836e00e6d39b743330c270770
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-06-19 10:20:10 -0700

    gc defending side, command line vision port, ball occlusion

--- src/software/sensor_fusion/sensor_fusion.cpp
+++ src/software/sensor_fusion/sensor_fusion.cpp
@@ -14,6 +14,7 @@ SensorFusion::SensorFusion(std::shared_ptr<const SensorFusionConfig> sensor_fusi
       friendly_team_filter(),
       enemy_team_filter(),
       team_with_possession(TeamSide::ENEMY),
+gc_defending_positive_side(false),
       friendly_goalie_id(0),
       enemy_goalie_id(0),
       reset_time_vision_packets_detected(0),
@@ -131,12 +132,20 @@ void SensorFusion::updateWorld(const SSLProto::Referee &packet)
         game_state.updateRefereeCommand(createRefereeCommand(packet, TeamColour::YELLOW));
         friendly_goalie_id = packet.yellow().goalkeeper();
         enemy_goalie_id    = packet.blue().goalkeeper();
+        if(packet.has_blue_team_on_positive_half())
+        {
+            gc_defending_positive_side = !packet.blue_team_on_positive_half();
+        }
     }
     else
     {
         game_state.updateRefereeCommand(createRefereeCommand(packet, TeamColour::BLUE));
         friendly_goalie_id = packet.blue().goalkeeper();
         enemy_goalie_id    = packet.yellow().goalkeeper();
+        if(packet.has_blue_team_on_positive_half())
+        {
+            gc_defending_positive_side = packet.blue_team_on_positive_half();
+        }
     }
 
     if (game_state.isOurBallPlacement())
@@ -204,7 +213,9 @@ void SensorFusion::updateWorld(const SSLProto::SSL_DetectionFrame &ssl_detection
     const bool defending_positive_side =
         sensor_fusion_config->getDefendingPositiveSide()->value();
     const bool should_invert_field =
-        override_game_controller_defending_side && defending_positive_side;
+        (override_game_controller_defending_side && defending_positive_side)
+        ||(!override_game_controller_defending_side && gc_defending_positive_side)
+        ;
 
     bool friendly_team_is_yellow =
         sensor_fusion_config->getFriendlyColorYellow()->value();
@@ -251,9 +262,9 @@ void SensorFusion::updateWorld(const SSLProto::SSL_DetectionFrame &ssl_detection
     {
         updateBall(*new_ball);
     }
-    // If we already have a ball, but miss the ball in the next frame
     else if (ball)
     {
+        // If we already have a ball, but miss the ball in the next frame
         std::optional<Robot> closest_enemy = enemy_team.getNearestRobot(ball->position());
         std::optional<Robot> closest_friendly =
             friendly_team.getNearestRobot(ball->position());
@@ -261,16 +272,19 @@ void SensorFusion::updateWorld(const SSLProto::SSL_DetectionFrame &ssl_detection
 
         closest_robot = closest_enemy;
 
-        if ((closest_enemy->position() - ball->position()).length() >
-            (closest_friendly->position() - ball->position()).length())
+        if(closest_enemy && closest_friendly)
         {
-            closest_robot = closest_friendly;
-        }
+            if ((closest_enemy->position() - ball->position()).length() >
+                (closest_friendly->position() - ball->position()).length())
+            {
+                closest_robot = closest_friendly;
+            }
 
-        ball = Ball(closest_robot->position() +
-                        ROBOT_MAX_RADIUS_METERS *
-                            Vector::createFromAngle(closest_robot->orientation()),
-                    Vector(0, 0), closest_robot->timestamp());
+            ball = Ball(closest_robot->position() +
+                            DIST_TO_FRONT_OF_ROBOT_METERS *
+                                Vector::createFromAngle(closest_robot->orientation()),
+                        Vector(0, 0), closest_robot->timestamp());
+        }
     }
 
     if (ball)
