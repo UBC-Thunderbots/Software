ce24f13b52f9a471755361eb45349af409eaa8e8
Jordan Liu <41462969+jordanzliu@users.noreply.github.com>
2021-06-20 23:18:57 -0700

    Keepaway on robocup 2021 (#2173)
    * prototype optimization based keepaway
    
    * make the evaluation function
    
    * small cost function refactor
    
    * some more cleanup
    
    * always pass the best pass to attacker_fsm
    
    * fix the sigfpe test fails
    
    * it runs now and does things that make sense
    
    * make a test
    
    * tweaks and more test improvmeents
    
    * revert ratepass changes
    
    * move proximity risk out of ratePassEnemyRisk
    
    * add another test case
    
    * more cost function changes
    
    * fix dribble orientation
    
    * make the tests fail (?)
    
    * make the tests pass
    
    * aight finally out of wip
    
    * fix arguments order and MM_PER_M
    
    * PR comments
    
    * oops accidentally commented out a test
    
    * default pass in attacker fsm keep away state
    
    * keep away field boundaries improvement
    
    * formatting
    
    * add a test for field bounds
    
    * update
    
    * hard constrain keep away point to reduced field line bounds
    
    * fix the test

--- src/software/ai/hl/stp/play/corner_kick_play.cpp
+++ src/software/ai/hl/stp/play/corner_kick_play.cpp
@@ -68,7 +68,7 @@ void CornerKickPlay::getNextTactics(TacticCoroutine::push_type &yield, const Wor
 
     do
     {
-        attacker->updateControlParams(pass);
+        attacker->updateControlParams(pass, true);
         receiver->updateControlParams(pass);
 
         if (!attacker->done())
