52f6ddbd497c8f6063d861c962971ae21144433d
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-07-07 21:16:29 -0700

    output both header and source from dynamic param

--- src/shared/parameter/generation_scripts/generate_dynamic_parameters.py
+++ src/shared/parameter/generation_scripts/generate_dynamic_parameters.py
@@ -20,32 +20,43 @@ PARAMETER_TEST_CONFIG_PATH = Path(
 )
 
 
-def generate_dynamic_parameters(output_file, include_headers, generate_for_cpp):
+def generate_dynamic_parameters(output_header, output_source, include_headers, generate_for_cpp):
     if generate_for_cpp:
 
         yamls = list(PARAMETER_CONFIG_PATH.glob("*.yaml"))
         config_metadata = ConfigYamlLoader.get_config_metadata(yamls)
 
         CppWriter.write_config_metadata(
-            output_file, include_headers, "ThunderbotsConfig", config_metadata
+            output_header, include_headers, "ThunderbotsConfig", config_metadata
+        )
+        CppWriter.write_config_metadata(
+            output_source, include_headers, "ThunderbotsConfig", config_metadata
         )
     else:
         # Genernate this properly as part of https://github.com/UBC-Thunderbots/Software/issues/1731
         test_yamls = list(PARAMETER_TEST_CONFIG_PATH.glob("*.yaml"))
         test_config_metadata = ConfigYamlLoader.get_config_metadata(test_yamls)
         CWriter.write_config_metadata(
-            output_file, "ThunderbotsConfig", test_config_metadata
+            output_header, "ThunderbotsConfig", test_config_metadata
+        )
+        CWriter.write_config_metadata(
+            output_source, "ThunderbotsConfig", test_config_metadata
         )
 
 
 def main():
     parser = argparse.ArgumentParser(description="Generate DynamicParameters")
     parser.add_argument(
-        "-o",
-        "--output_file",
+        "--output_header",
         type=str,
         required=True,
-        help="The file that will be generated",
+        help="The header file that will be generated",
+    )
+    parser.add_argument(
+        "--output_source",
+        type=str,
+        required=True,
+        help="The source file that will be generated",
     )
     output_type = parser.add_mutually_exclusive_group(required=True)
     output_type.add_argument(
@@ -72,7 +83,7 @@ def main():
 
     args = parser.parse_args()
     generate_dynamic_parameters(
-        args.output_file, args.include_headers, args.generate_for_cpp
+        args.output_header, args.output_source, args.include_headers, args.generate_for_cpp
     )
 
 
