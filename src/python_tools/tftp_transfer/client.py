6aa9dbf850a00fb92e8283c6513481eaf2698e12
Arun <arun.sfc@outlook.com>
2021-08-21 11:41:00 -0700

    formatting fixes

--- src/python_tools/tftp_transfer/client.py
+++ src/python_tools/tftp_transfer/client.py
@@ -1,17 +1,23 @@
 import tftpy, argparse
 
 # parse command line arguments
-ap      = argparse.ArgumentParser()
-ap.add_argument("-p", "--port", type=int, default=42000,
-               help="inbound port")
-ap.add_argument("-r", "--root_dir", required=True, type=str,
-               help="root directory for the TFTP server")
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--port", type=int, default=42000, help="inbound port")
+ap.add_argument(
+    "-r",
+    "--root_dir",
+    required=True,
+    type=str,
+    help="root directory for the TFTP server",
+)
 
-args    = vars(ap.parse_args())
-port    = args['port']
+args = vars(ap.parse_args())
+port = args["port"]
 
-server = tftpy.TftpServer(args['root_dir'])
-try :
-    server.listen('0.0.0.0', port)
+server = tftpy.TftpServer(args["root_dir"])
+try:
+    server.listen("0.0.0.0", port)
 except PermissionError:
-    print("Elevated privilege is required to use a port less than 1024. Retry with another port.")
+    print(
+        "Elevated privilege is required to use a port less than 1024. Retry with another port."
+    )
--- src/python_tools/tftp_transfer/host.py
+++ src/python_tools/tftp_transfer/host.py
@@ -1,9 +1,9 @@
 import tftpy, hashlib, os, argparse, pathlib, shutil
 
-TEMP = os.path.join(os.getcwd(), '.temp')
+TEMP = os.path.join(os.getcwd(), ".temp")
 
-def verify_checksum(file_path: str, 
-                    checksum: str) -> bool :
+
+def verify_checksum(file_path: str, checksum: str) -> bool:
     """
     Compare the SHA-256 checksum of a given file and verify that it is the same as a given checksum.
     
@@ -13,12 +13,13 @@ def verify_checksum(file_path: str,
     :param checksum: the target SHA-256 checksum to compare against
     :return true if both checksums are correct, false otherwise
     """
-    try :
+    try:
         file_hash = encode_sha256_checksum(file_path)
-    except Exception :
+    except Exception:
         return False
-    
+
     return file_hash == checksum
+
 
 def encode_sha256_checksum(file_path: str) -> str:
     """
@@ -30,59 +31,69 @@ def encode_sha256_checksum(file_path: str) -> str:
     # make hash by reading consecutive parts of data
     hash = hashlib.sha256()
     with open(file_path, "rb") as f:
-        for byte_block in iter(lambda: f.read(4096), b"") :
+        for byte_block in iter(lambda: f.read(4096), b""):
             hash.update(byte_block)
 
-    return hash.hexdigest()    
+    return hash.hexdigest()
+
 
 # get command line arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--file", required=True, type=pathlib.Path,
-               help="path to the file to be transferred")
+ap.add_argument(
+    "-f",
+    "--file",
+    required=True,
+    type=pathlib.Path,
+    help="path to the file to be transferred",
+)
 
-# TODO: the following two arguments won't be necessary when the announcement system is 
+# TODO: the following two arguments won't be necessary when the announcement system is
 #       in place
-ap.add_argument("-p", "--port", type=int,
-               help="the port to be used", default=42000)
-ap.add_argument("-ip", "--ip_address", required=True, type=str,
-               help="ip address of outbound connection")
+ap.add_argument("-p", "--port", type=int, help="the port to be used", default=42000)
+ap.add_argument(
+    "-ip",
+    "--ip_address",
+    required=True,
+    type=str,
+    help="ip address of outbound connection",
+)
 
 args = vars(ap.parse_args())
 
-file_path           = args['file']
-file_name           = os.path.basename(args['file'])
-transfer_file_hash  = encode_sha256_checksum(file_path)
-port                = args['port']
-ip_address          = args['ip_address']
-client              = tftpy.TftpClient(ip_address, port)
+file_path = args["file"]
+file_name = os.path.basename(args["file"])
+transfer_file_hash = encode_sha256_checksum(file_path)
+port = args["port"]
+ip_address = args["ip_address"]
+client = tftpy.TftpClient(ip_address, port)
 
-try :
+try:
     os.mkdir(TEMP)
-except FileExistsError :
+except FileExistsError:
     pass
 
 # send file to the robot
 client.upload(file_name, file_path)
-while True :
+while True:
     # verify the checksum
     print("Verifying checksum...")
-    # TODO: currently, the checksum is verified by redownloading the file that we just 
-    #       transferred from the robot to a temp directory & making sure the checksum 
-    #       of the redownloaded file is the same. When we have announcements, the 
+    # TODO: currently, the checksum is verified by redownloading the file that we just
+    #       transferred from the robot to a temp directory & making sure the checksum
+    #       of the redownloaded file is the same. When we have announcements, the
     #       checksum will be reported by the robot and we just compare the hash from the
     #       announcement
     transferred_file_path = os.path.join(TEMP, file_name)
     client.download(file_name, transferred_file_path)
 
-    if verify_checksum(transferred_file_path, transfer_file_hash) :
+    if verify_checksum(transferred_file_path, transfer_file_hash):
         print("Checksum verified")
         break
-    else :
+    else:
         # if we can't validate the checksum, re-upload the file.
         print("Checksum verification failed")
         print("Retrying...")
         client.upload(file_name, file_path)
-        
+
 # delete the temp directory we made to re-download the file
 # TODO: we won't need this when announcements go in
 print("Cleaning up temp directory")
