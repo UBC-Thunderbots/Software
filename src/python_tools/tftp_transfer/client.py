4bc73fd0f3b0c3d94a670b97b2d1cfa096278eb5
Arun <arun.sfc@outlook.com>
2021-08-21 11:25:45 -0700

    documentation

--- src/python_tools/tftp_transfer/client.py
+++ src/python_tools/tftp_transfer/client.py
@@ -1,10 +1,8 @@
 import tftpy, argparse
 
-PORT = 21
-
 # parse command line arguments
 ap      = argparse.ArgumentParser()
-ap.add_argument("-p", "--port", required=True, type=int,
+ap.add_argument("-p", "--port", type=int, default=42000,
                help="inbound port")
 ap.add_argument("-r", "--root_dir", required=True, type=str,
                help="root directory for the TFTP server")
@@ -13,4 +11,7 @@ args    = vars(ap.parse_args())
 port    = args['port']
 
 server = tftpy.TftpServer(args['root_dir'])
-server.listen('0.0.0.0', PORT)
+try :
+    server.listen('0.0.0.0', port)
+except PermissionError:
+    print("Elevated privilege is required to use a port less than 1024. Retry with another port.")
--- src/python_tools/tftp_transfer/host.py
+++ src/python_tools/tftp_transfer/host.py
@@ -4,6 +4,15 @@ TEMP = os.path.join(os.getcwd(), '.temp')
 
 def verify_checksum(file_path: str, 
                     checksum: str) -> bool :
+    """
+    Compare the SHA-256 checksum of a given file and verify that it is the same as a given checksum.
+    
+    Any file-reading errors will return false.
+    
+    :param file_path: the path to a file
+    :param checksum: the target SHA-256 checksum to compare against
+    :return true if both checksums are correct, false otherwise
+    """
     try :
         file_hash = encode_sha256_checksum(file_path)
     except Exception :
@@ -12,7 +21,13 @@ def verify_checksum(file_path: str,
     return file_hash == checksum
 
 def encode_sha256_checksum(file_path: str) -> str:
-    # make hash
+    """
+    Determines the SHA-256 checksum for a given file.
+    
+    :param file_path: the path to a file
+    :return: the SHA-256 checksum for the given file
+    """
+    # make hash by reading consecutive parts of data
     hash = hashlib.sha256()
     with open(file_path, "rb") as f:
         for byte_block in iter(lambda: f.read(4096), b"") :
@@ -22,20 +37,22 @@ def encode_sha256_checksum(file_path: str) -> str:
 
 # get command line arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", required=True, type=pathlib.Path,
+ap.add_argument("-f", "--file", required=True, type=pathlib.Path,
                help="path to the file to be transferred")
-ap.add_argument("-p", required=True, type=int,
-               help="outbound tftp port to be used", default=69)
 
+# TODO: the following two arguments won't be necessary when the announcement system is 
+#       in place
+ap.add_argument("-p", "--port", type=int,
+               help="the port to be used", default=42000)
 ap.add_argument("-ip", "--ip_address", required=True, type=str,
                help="ip address of outbound connection")
 
 args = vars(ap.parse_args())
 
-file_path           = args['f']
-file_name           = os.path.basename(args['f'])
+file_path           = args['file']
+file_name           = os.path.basename(args['file'])
 transfer_file_hash  = encode_sha256_checksum(file_path)
-port                = args['p']
+port                = args['port']
 ip_address          = args['ip_address']
 client              = tftpy.TftpClient(ip_address, port)
 
@@ -44,15 +61,16 @@ try :
 except FileExistsError :
     pass
 
-# listen to announcements; for each robot, get info about ip address and reported sha_checksum
-# get binaries and calculate check_sum
-# compare checksums and upload if necessary
-
-# for each robot reported in the announcement, compare readable_hash to reported hash
-# if has isn't the same, upload new:
+# send file to the robot
 client.upload(file_name, file_path)
 while True :
+    # verify the checksum
     print("Verifying checksum...")
+    # TODO: currently, the checksum is verified by redownloading the file that we just 
+    #       transferred from the robot to a temp directory & making sure the checksum 
+    #       of the redownloaded file is the same. When we have announcements, the 
+    #       checksum will be reported by the robot and we just compare the hash from the
+    #       announcement
     transferred_file_path = os.path.join(TEMP, file_name)
     client.download(file_name, transferred_file_path)
 
@@ -60,9 +78,12 @@ while True :
         print("Checksum verified")
         break
     else :
+        # if we can't validate the checksum, re-upload the file.
         print("Checksum verification failed")
         print("Retrying...")
         client.upload(file_name, file_path)
         
+# delete the temp directory we made to re-download the file
+# TODO: we won't need this when announcements go in
 print("Cleaning up temp directory")
 shutil.rmtree(TEMP)
