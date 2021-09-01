85de2b9d20ca8958ed1bc91f7bf3739f450b89cb
LiCody <licody2000@gmail.com>
2021-08-31 17:51:29 -0700

    refactor and document

--- src/python_tools/robot_broadcast_receiver.py
+++ src/python_tools/robot_broadcast_receiver.py
@@ -1,31 +1,53 @@
-from software.proto.announcement_pb2 import Announcement
+from shared.proto.announcement_pb2 import Announcement
 import socket
-import errno
-from time import time, sleep
+import argparse
+from time import time
 
-receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-receiver.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-receiver.setblocking(False)
-receiver.bind(("", 42020))
+RECEIVE_TIMEOUT = 0.2
 
-announcements = []
-timeout = time() + 4  # 4s
 
-while time() < timeout:
-    try:
-        data = receiver.recv(1024)
-    except socket.error as e:
-        err = e.args[0]
-        if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
-            sleep(0.2)
+def receive_announcements(port: int) -> [Announcement]:
+    """
+    Returns a list of Announcements, without duplicates received within a time window of 4s on a specified port
+    :param port: the port to listen for announcements on
+    :return: a list of Announcements, without duplicates
+    """
+    receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    receiver.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    receiver.settimeout(RECEIVE_TIMEOUT)
+    receiver.bind(("", port))
+
+    announcements = []
+    timeout = time() + 4  # 4s
+    while time() < timeout:
+        try:
+            data = receiver.recv(1024)
+        except socket.timeout:  # Ignore timeout errors
             continue
-    else:
-        announcement = Announcement()
-        announcement.ParseFromString(data)
-        if announcement not in announcements:
-            announcements.append(announcement)
+        else:
+            # parse announcement protobuf
+            announcement = Announcement()
+            announcement.ParseFromString(data)
+            # filter out duplicates
+            if announcement not in announcements:
+                announcements.append(announcement)
+    return announcements
 
-for announcement in announcements:
-    print(
-        f"robot_id: {announcement.robot_id} \nip_addr: {announcement.ip_addr} \nmac_addr: {announcement.mac_addr} \n"
-    )
+
+def main():
+    # get command line args
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--port", required=True, type=int, help="port to listen on")
+    args = vars(ap.parse_args())
+
+    port = args["port"]
+
+    announcements = receive_announcements(port)
+    for announcement in announcements:
+        print(
+            f"robot_id: {announcement.robot_id} \nip_addr: {announcement.ip_addr} \nmac_addr: {announcement.mac_addr} \n"
+        )
+
+
+if __name__ == "__main__":
+    main()
--- src/python_tools/robot_broadcast_sender.py
+++ src/python_tools/robot_broadcast_sender.py
@@ -1,11 +1,18 @@
-from software.proto.announcement_pb2 import Announcement
-import socket
+from shared.proto.announcement_pb2 import Announcement
+import argparse
 import fcntl
+import socket
 import struct
-import time
+from time import sleep
 
 
-def get_ip_address(ifname):
+def get_ip_address(ifname: str) -> str:
+    """
+    Uses the Linux SIOCGIFADDR ioctl to find the IP address associated with a network interface, given the name of that
+    interface
+    :param ifname: the interface to find the IP address associated with
+    :return: the IP address associated with the given interface
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(
         fcntl.ioctl(
@@ -16,26 +23,48 @@ def get_ip_address(ifname):
     )
 
 
-def get_mac_address(ifname):
+def get_mac_address(ifname: str) -> str:
+    """
+    Uses the Linux SIOCGIFHWADDR ioctl to find the HW/mac address associated with a network interface, given the name of
+    that interface
+    :param ifname: the interface to find the HW/max address associated with
+    :return: the HW/mac address associated with the given interface
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     info = fcntl.ioctl(
-        s.fileno(), 0x8927, struct.pack("256s", bytes(ifname, "utf-8")[:15])
+        s.fileno(), 
+        0x8927,  # SIOCGIFHWADDR
+        struct.pack("256s", bytes(ifname, "utf-8")[:15])
     )
     return ":".join("%02x" % b for b in info[18:24])
 
 
-interface = "eth0"
+def main():
+    # get command line args
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--port", required=True, type=int, help="port to send on")
+    ap.add_argument("-if", "--ifname", required=True, type=str, help="interface to use to get ip and mac addr")
+    args = vars(ap.parse_args())
 
-announcement = Announcement()
-announcement.robot_id = 1
-announcement.ip_addr = get_ip_address(interface)
-announcement.mac_addr = get_mac_address(interface)
+    interface = args["ifname"]
+    port = args["port"]
 
-sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sender.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sender.bind(("", 42021))
+    # construct a announcement protobuf
+    announcement = Announcement()
+    announcement.robot_id = 1
+    announcement.ip_addr = get_ip_address(interface)
+    announcement.mac_addr = get_mac_address(interface)
 
-while True:
-    sender.sendto(announcement.SerializeToString(), ("<broadcast>", 42020))
-    print(announcement)
-    time.sleep(2)
+    # send the announcement protobuf on the broadcast ip and specified port
+    sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sender.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    print("Starting broadcast..")
+    while True:
+        sender.sendto(announcement.SerializeToString(), ("<broadcast>", port))
+        print(announcement)
+        sleep(2)
+
+
+if __name__ == "__main__":
+    main()
