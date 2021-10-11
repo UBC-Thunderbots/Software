0ccb9e6a858010f727e6212d1b3bfdd128ed7468
Josh Wong <joshwong.ca@gmail.com>
2021-10-11 13:42:15 -0700

    Added play name to warning log

--- src/software/ai/hl/stp/stp.cpp
+++ src/software/ai/hl/stp/stp.cpp
@@ -123,8 +123,9 @@ std::unique_ptr<Play> STP::calculateNewPlay(const World& world)
         auto play = play_constructor(play_config);
         if (play->isApplicable(world))
         {
-            if (!play->invariantHolds(world)){
-                LOG(WARNING) << "Play::isApplicable() is true and Play::invariantHolds() is false"
+            if (!play->invariantHolds(world))
+            {
+                LOG(WARNING) << "Unexpected behaviour from " << objectTypeName(*play) << ". Play::isApplicable() is true and Play::invariantHolds() is false"
                              << std::endl;
             }
             applicable_plays.emplace_back(std::move(play));
