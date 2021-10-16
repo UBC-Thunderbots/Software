ce541b1db1888078d4a5430899bae82b75d07e43
phil <phillipdumitru71@gmail.com>
2021-10-16 12:26:45 -0700

    fixed comment

--- src/software/simulated_tests/non_terminating_validation_functions/robots_violating_motion_constraint.h
+++ src/software/simulated_tests/non_terminating_validation_functions/robots_violating_motion_constraint.h
@@ -6,10 +6,13 @@
 #include "software/world/world.h"
 
 /**
- * Checks if the friendly robots are in the friendly half of the field, else assertion
- * fails.
+ * Checks if the friendly robots do not enter specified motion constraint during the test.
+ * If they do, assertion fails. 
+ * 
  * @param world_ptr the world pointer given by the simulator. Gets updated every tick
  * @param yield yields control to the next routine (coroutines) with error message
+ * @param obstacle_factory the obstacle factory object to be passed in
+ * @param constraint the motion constraint to be specified
  */
 void robotsViolatingMotionConstraint(
     std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield,
