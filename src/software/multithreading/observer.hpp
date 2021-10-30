578e15883c2f196048ed488c10eaacb3bb76e55f
Hubert <hubertotheo@gmail.com>
2021-10-30 12:57:40 -0700

    combined tpp and h files into hpp

--- src/software/multithreading/observer.hpp
+++ src/software/multithreading/observer.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "shared/constants.h"
-#include "software/multithreading/thread_safe_buffer.h"
+#include "software/multithreading/thread_safe_buffer.hpp"
 
 /**
  * This class observes an "Subject<T>". That is, it can be registered with an
@@ -79,4 +79,50 @@ class Observer
     boost::circular_buffer<std::chrono::milliseconds> receive_time_buffer;
 };
 
-#include "software/multithreading/observer.tpp"
+template <typename T>
+Observer<T>::Observer(size_t buffer_size, bool log_buffer_full)
+    : buffer(buffer_size, log_buffer_full), receive_time_buffer(TIME_BUFFER_SIZE)
+{
+}
+
+template <typename T>
+void Observer<T>::receiveValue(T val)
+{
+    receive_time_buffer.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::steady_clock::now().time_since_epoch()));
+    buffer.push(std::move(val));
+}
+
+template <typename T>
+std::optional<T> Observer<T>::popMostRecentlyReceivedValue(Duration max_wait_time)
+{
+    return buffer.popMostRecentlyAddedValue(max_wait_time);
+}
+
+template <typename T>
+std::optional<T> Observer<T>::popLeastRecentlyReceivedValue(Duration max_wait_time)
+{
+    return buffer.popLeastRecentlyAddedValue(max_wait_time);
+}
+
+template <typename T>
+double Observer<T>::getDataReceivedPerSecond()
+{
+    if (receive_time_buffer.empty())
+    {
+        return 0;
+    }
+    else
+    {
+        double time_s =
+            static_cast<double>(
+                (receive_time_buffer.back() - receive_time_buffer.front()).count()) *
+            SECONDS_PER_MILLISECOND;
+
+        double rate = static_cast<double>(receive_time_buffer.size() - 1) / time_s;
+        return std::max(0.0, rate);
+    }
+}
+
+
+
--- src/software/multithreading/threaded_observer.hpp
+++ src/software/multithreading/threaded_observer.hpp
@@ -3,7 +3,7 @@
 #include <boost/bind.hpp>
 #include <thread>
 
-#include "software/multithreading/observer.h"
+#include "software/multithreading/observer.hpp"
 
 /**
  * The general usage of this class should be to extend it, then override
@@ -82,4 +82,66 @@ class ThreadedObserver : public Observer<T>
     std::thread pull_from_buffer_thread;
 };
 
-#include "software/multithreading/threaded_observer.tpp"
+template <typename T>
+ThreadedObserver<T>::ThreadedObserver(size_t buffer_size, bool log_buffer_full)
+    : Observer<T>(buffer_size, log_buffer_full),
+      in_destructor(false),
+      IN_DESTRUCTOR_CHECK_PERIOD(Duration::fromSeconds(0.1))
+{
+    pull_from_buffer_thread = std::thread(
+        boost::bind(&ThreadedObserver::continuouslyPullValuesFromBuffer, this));
+}
+
+template <typename T>
+void ThreadedObserver<T>::onValueReceived(T val)
+{
+    // Do nothing, this function should be overridden to enable custom behavior on
+    // message reception.
+}
+
+template <typename T>
+void ThreadedObserver<T>::continuouslyPullValuesFromBuffer()
+{
+    do
+    {
+        in_destructor_mutex.unlock();
+        std::optional<T> new_val;
+
+        new_val = this->getNextValue(IN_DESTRUCTOR_CHECK_PERIOD);
+
+        if (new_val)
+        {
+            onValueReceived(*new_val);
+        }
+
+        in_destructor_mutex.lock();
+    } while (!in_destructor);
+}
+
+
+template <typename T>
+ThreadedObserver<T>::~ThreadedObserver()
+{
+    in_destructor_mutex.lock();
+    in_destructor = true;
+    in_destructor_mutex.unlock();
+
+    // We must wait for the thread to stop, as if we destroy it while it's still
+    // running we will segfault
+    pull_from_buffer_thread.join();
+}
+
+template <typename T>
+std::optional<T> ThreadedObserver<T>::getNextValue(const Duration& max_wait_time)
+{
+    // Do nothing, this function should be overridden to enable custom behavior on
+    // message reception.
+    // We *must* provide an implementation here instead of making it pure virtual because
+    // this function may be called from the `pull_from_buffer_thread` *before* the
+    // subclass constructor has completed (ie. before it has "finished overriding" all the
+    // methods). If this functions is pure virtual then this case would cause a crash.
+    return std::nullopt;
+}
+
+
+
