cbea41e9d56e4930edd4fe00bd669ba2d97f19cc
Ubuntu <ubuntu@ip-172-31-9-125.ec2.internal>
2021-11-06 06:28:45 +0000

    obstacle hpp merged

--- src/software/ai/navigator/obstacle/obstacle.hpp
+++ src/software/ai/navigator/obstacle/obstacle.hpp
@@ -114,5 +114,54 @@ inline std::ostream& operator<<(std::ostream& os, const ObstaclePtr& obstacle_pt
     os << obstacle_ptr->toString();
     return os;
 }
+#include "software/geom/algorithms/contains.h"
+#include "software/geom/algorithms/rasterize.h"
 
-#include "software/ai/navigator/obstacle/obstacle.tpp"
+template <typename GEOM_TYPE>
+GeomObstacle<GEOM_TYPE>::GeomObstacle(const GEOM_TYPE& geom) : geom_(geom)
+{
+}
+
+template <typename GEOM_TYPE>
+bool GeomObstacle<GEOM_TYPE>::contains(const Point& p) const
+{
+    return ::contains(geom_, p);
+}
+
+template <typename GEOM_TYPE>
+double GeomObstacle<GEOM_TYPE>::distance(const Point& p) const
+{
+    return ::distance(geom_, p);
+}
+
+template <typename GEOM_TYPE>
+bool GeomObstacle<GEOM_TYPE>::intersects(const Segment& segment) const
+{
+    return ::intersects(geom_, segment);
+}
+
+template <typename GEOM_TYPE>
+std::vector<Point> GeomObstacle<GEOM_TYPE>::rasterize(const double resolution_size) const
+{
+    return ::rasterize(geom_, resolution_size);
+}
+
+template <typename GEOM_TYPE>
+std::string GeomObstacle<GEOM_TYPE>::toString(void) const
+{
+    std::ostringstream ss;
+    ss << "Obstacle with shape " << geom_;
+    return ss.str();
+}
+
+template <typename GEOM_TYPE>
+const GEOM_TYPE GeomObstacle<GEOM_TYPE>::getGeom(void) const
+{
+    return geom_;
+}
+
+template <typename GEOM_TYPE>
+void GeomObstacle<GEOM_TYPE>::accept(ObstacleVisitor& visitor) const
+{
+    visitor.visit(*this);
+}
