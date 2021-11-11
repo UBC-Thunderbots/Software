ece4ec5adfc4c5f7af2f08e441ee74d1486308b1
Hubert <hubertotheo@gmail.com>
2021-11-10 23:33:35 -0800

    combined tpp and h files into hpp

--- src/software/networking/proto_udp_sender.hpp
+++ src/software/networking/proto_udp_sender.hpp
@@ -4,6 +4,8 @@
 #include <boost/bind.hpp>
 #include <string>
 
+#include "software/logger/logger.h"
+
 template <class SendProto>
 class ProtoUdpSender
 {
@@ -47,4 +49,36 @@ class ProtoUdpSender
     std::string data_buffer;
 };
 
-#include "software/networking/proto_udp_sender.tpp"
+template <class SendProto>
+ProtoUdpSender<SendProto>::ProtoUdpSender(boost::asio::io_service& io_service,
+                                          const std::string& ip_address,
+                                          const unsigned short port, bool multicast)
+    : socket_(io_service)
+{
+    boost::asio::ip::address addr = boost::asio::ip::make_address(ip_address);
+
+    // The receiver endpoint identifies where this UdpSender will send data to
+    receiver_endpoint = boost::asio::ip::udp::endpoint(addr, port);
+
+    socket_.open(receiver_endpoint.protocol());
+
+    if (multicast)
+    {
+        socket_.set_option(boost::asio::ip::multicast::join_group(addr));
+    }
+}
+
+template <class SendProto>
+void ProtoUdpSender<SendProto>::sendProto(const SendProto& message)
+{
+    message.SerializeToString(&data_buffer);
+    socket_.send_to(boost::asio::buffer(data_buffer), receiver_endpoint);
+}
+
+template <class SendProto>
+ProtoUdpSender<SendProto>::~ProtoUdpSender()
+{
+    socket_.close();
+}
+
+#include "software/networking/proto_udp_sender.hpp"
--- src/software/networking/threaded_proto_udp_sender.hpp
+++ src/software/networking/threaded_proto_udp_sender.hpp
@@ -4,7 +4,7 @@
 #include <boost/bind.hpp>
 #include <string>
 
-#include "software/networking/proto_udp_sender.h"
+#include "software/networking/proto_udp_sender.hpp"
 
 template <class SendProto>
 class ThreadedProtoUdpSender
@@ -43,4 +43,37 @@ class ThreadedProtoUdpSender
     std::thread io_service_thread;
 };
 
-#include "software/networking/threaded_proto_udp_sender.tpp"
+template <class SendProtoT>
+ThreadedProtoUdpSender<SendProtoT>::ThreadedProtoUdpSender(const std::string& ip_address,
+                                                           const unsigned short port,
+                                                           bool multicast)
+    : io_service(),
+      udp_sender(io_service, ip_address, port, multicast),
+      io_service_thread([this]() { io_service.run(); })
+{
+}
+
+template <class SendProtoT>
+ThreadedProtoUdpSender<SendProtoT>::~ThreadedProtoUdpSender()
+{
+    // Stop the io_service. This is safe to call from another thread.
+    // https://stackoverflow.com/questions/4808848/boost-asio-stopping-io-service
+    // This MUST be done before attempting to join the thread because otherwise the
+    // io_service will not stop and the thread will not join
+    io_service.stop();
+
+    // Join the io_service_thread so that we wait for it to exit before destructing the
+    // thread object. If we do not wait for the thread to finish executing, it will call
+    // `std::terminate` when we deallocate the thread object and kill our whole program
+    io_service_thread.join();
+}
+
+template <class SendProtoT>
+void ThreadedProtoUdpSender<SendProtoT>::sendProto(const SendProtoT& message)
+{
+    udp_sender.sendProto(message);
+}
+
+
+#include "software/networking/threaded_proto_udp_sender.hpp"
+
