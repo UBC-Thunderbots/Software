eae7a352b2bdd7c38ccb92e93e7537450371f5ae
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2021-11-10 21:18:34 -0800

    clean up

--- src/software/ai/hl/stp/play/shoot_or_pass_play.cpp
+++ src/software/ai/hl/stp/play/shoot_or_pass_play.cpp
@@ -43,7 +43,6 @@ bool ShootOrPassPlay::invariantHolds(const World &world) const
            (world.getTeamWithPossession() == TeamSide::FRIENDLY);
 }
 
-
 void ShootOrPassPlay::getNextTactics(TacticCoroutine::push_type &yield,
                                      const World &world)
 {
@@ -75,100 +74,18 @@ void ShootOrPassPlay::getNextTactics(TacticCoroutine::push_type &yield,
             ->updateControlParams(world.ball().position(),
                                   CreaseDefenderAlignment::RIGHT);
 
-        auto offensive_tactics = std::make_shared<TacticVector>();
+        TacticVector offensive_tactics;
 
         fsm.process_event(OffensivePlayFSM::Update(
             OffensivePlayFSM::ControlParams(),
-            PlayUpdate(world, [offensive_tactics](TacticVector new_tactics) {
-                *offensive_tactics = new_tactics;
+            PlayUpdate(world, [&offensive_tactics](TacticVector new_tactics) {
+                offensive_tactics = new_tactics;
             })));
 
-        yield({*offensive_tactics,
+        yield({offensive_tactics,
                {std::get<0>(crease_defender_tactics),
                 std::get<1>(crease_defender_tactics)}});
     } while (!fsm.is(boost::sml::X));
-}
-
-PassWithRating ShootOrPassPlay::attemptToShootWhileLookingForAPass(
-    TacticCoroutine::push_type &yield,
-    std::array<std::shared_ptr<CreaseDefenderTactic>, 2> crease_defender_tactics,
-    std::shared_ptr<AttackerTactic> attacker_tactic, const World &world)
-
-{
-    auto pitch_division =
-        std::make_shared<const EighteenZonePitchDivision>(world.field());
-
-    PassGenerator<EighteenZoneId> pass_generator(pitch_division,
-                                                 play_config->getPassingConfig());
-
-    auto pass_eval    = pass_generator.generatePassEvaluation(world);
-    auto ranked_zones = pass_eval.rankZonesForReceiving(world, world.ball().position());
-    Zones cherry_pick_region_1 = {ranked_zones[0]};
-    Zones cherry_pick_region_2 = {ranked_zones[1]};
-
-    PassWithRating best_pass_and_score_so_far = pass_eval.getBestPassOnField();
-
-    // These two tactics will set robots to roam around the field, trying to put
-    // themselves into a good position to receive a pass
-    auto cherry_pick_tactic_1 = std::make_shared<MoveTactic>(false);
-    auto cherry_pick_tactic_2 = std::make_shared<MoveTactic>(false);
-
-    // Wait for a good pass by starting out only looking for "perfect" passes (with a
-    // score of 1) and decreasing this threshold over time
-    double min_pass_score_threshold        = 1.0;
-    Timestamp pass_optimization_start_time = world.getMostRecentTimestamp();
-    // This boolean indicates if we're ready to perform a pass
-    bool ready_to_pass = false;
-
-    double abs_min_pass_score =
-        play_config->getShootOrPassPlayConfig()->getAbsMinPassScore()->value();
-    double pass_score_ramp_down_duration =
-        play_config->getShootOrPassPlayConfig()->getPassScoreRampDownDuration()->value();
-    do
-    {
-        LOG(DEBUG) << "Best pass so far is: " << best_pass_and_score_so_far.pass;
-        LOG(DEBUG) << "      with score of: " << best_pass_and_score_so_far.rating;
-
-        std::get<0>(crease_defender_tactics)
-            ->updateControlParams(world.ball().position(), CreaseDefenderAlignment::LEFT);
-        std::get<1>(crease_defender_tactics)
-            ->updateControlParams(world.ball().position(),
-                                  CreaseDefenderAlignment::RIGHT);
-        pass_eval                  = pass_generator.generatePassEvaluation(world);
-        best_pass_and_score_so_far = pass_eval.getBestPassOnField();
-
-        auto pass1 = pass_eval.getBestPassInZones(cherry_pick_region_1).pass;
-        auto pass2 = pass_eval.getBestPassInZones(cherry_pick_region_2).pass;
-
-        cherry_pick_tactic_1->updateControlParams(pass1.receiverPoint(),
-                                                  pass1.receiverOrientation(), 0.0,
-                                                  MaxAllowedSpeedMode::PHYSICAL_LIMIT);
-        cherry_pick_tactic_2->updateControlParams(pass2.receiverPoint(),
-                                                  pass2.receiverOrientation(), 0.0,
-                                                  MaxAllowedSpeedMode::PHYSICAL_LIMIT);
-
-        // update the best pass in the attacker tactic
-        attacker_tactic->updateControlParams(best_pass_and_score_so_far.pass, false);
-
-        yield({{attacker_tactic, cherry_pick_tactic_1, cherry_pick_tactic_2,
-                std::get<0>(crease_defender_tactics),
-                std::get<1>(crease_defender_tactics)}});
-
-        // We're ready to pass if we have a robot assigned in the PassGenerator as the
-        // passer and the PassGenerator has found a pass above our current threshold
-        ready_to_pass = best_pass_and_score_so_far.rating > min_pass_score_threshold;
-
-        // If we've assigned a robot as the passer in the PassGenerator, we lower
-        // our threshold based on how long the PassGenerator as been running since
-        // we set it
-        Duration time_since_commit_stage_start =
-            world.getMostRecentTimestamp() - pass_optimization_start_time;
-        min_pass_score_threshold =
-            1 - std::min(time_since_commit_stage_start.toSeconds() /
-                             pass_score_ramp_down_duration,
-                         1.0 - abs_min_pass_score);
-    } while (!ready_to_pass);
-    return best_pass_and_score_so_far;
 }
 
 // Register this play in the genericFactory
--- src/software/ai/hl/stp/play/shoot_or_pass_play.h
+++ src/software/ai/hl/stp/play/shoot_or_pass_play.h
@@ -23,28 +23,5 @@ class ShootOrPassPlay : public Play
     void getNextTactics(TacticCoroutine::push_type &yield, const World &world) override;
 
    private:
-    // How close each of our patrol tactics must be to each of the points in their
-    // route before progressing on the next one
-    static constexpr double AT_PATROL_POINT_TOLERANCE = 0.3;
-
-    // The speed each patrolling robot should be moving through its control point
-    static constexpr double SPEED_AT_PATROL_POINTS = 0.0;
-
-    /**
-     * Sets up the pass for the corner kick: aligns the passer and positions the cherry
-     * pickers
-     *
-     * @param yield The coroutine to yield
-     * @param crease_defender_tactics The crease defender tactics
-     * @param attacker_tactic The attacker tactic
-     * @param world The current state of the world
-     *
-     * @return the best pass found
-     */
-    PassWithRating attemptToShootWhileLookingForAPass(
-        TacticCoroutine::push_type &yield,
-        std::array<std::shared_ptr<CreaseDefenderTactic>, 2> crease_defender_tactics,
-        std::shared_ptr<AttackerTactic> attacker_tactic, const World &world);
-
     FSM<OffensivePlayFSM> fsm;
 };
