4de5a82d2cae199f2e535e9db23cdd2b5f83d09c
akhil <akhil.veeraghanta@outlook.com>
2021-11-10 17:49:05 -0800

    add a test

--- src/proto/message_translation/ssl_simulation_robot_control_test.cpp
+++ src/proto/message_translation/ssl_simulation_robot_control_test.cpp
@@ -38,7 +38,21 @@ TEST_F(SSLSimulationProtoTest, test_create_robot_move_command_forward)
 
 TEST_F(SSLSimulationProtoTest, test_create_robot_move_command_forward_from_primitive)
 {
-    // TODO IMPL ME
+    TbotsProto::DirectControlPrimitive test;
+
+    test.mutable_direct_velocity_control()->mutable_velocity()->set_x_component_meters(
+        10);
+    test.mutable_direct_velocity_control()->mutable_velocity()->set_y_component_meters(5);
+    test.mutable_direct_velocity_control()
+        ->mutable_angular_velocity()
+        ->set_radians_per_second(2);
+
+    auto move_command =
+        createRobotMoveCommand(test, 35, 45, wheel_constants.wheel_radius_meters);
+
+    EXPECT_EQ(move_command->local_velocity().left(), 5);
+    EXPECT_EQ(move_command->local_velocity().forward(), 10);
+    EXPECT_EQ(move_command->local_velocity().angular(), 2);
 }
 
 TEST_F(SSLSimulationProtoTest, test_create_robot_move_command_backward)
