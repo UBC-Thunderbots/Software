ac6e0cf0bcb1fa5e76afbd6153913b100ecf5eea
Ubuntu <ubuntu@ip-172-31-9-125.ec2.internal>
2021-11-13 01:39:58 +0000

    upd createBall, createRobot for filter

--- src/proto/message_translation/er_force_world.cpp
+++ src/proto/message_translation/er_force_world.cpp
@@ -1,20 +1,24 @@
 #include <btBulletinDynamicsCommon.h>
 
-#include "software/world/ball_state.h"
-#include "software/world/robot_state.h"
 #include "software/time/timestamp.h"
+#include "software/world/robot_state.h"
+#include "software/world/ball_state.h"
 #include "proto/message_translation/er_force_world.h"
+#include "extlibs/er_force_sim/src/protobuf/robot.pb.h"
 
-Ball createBall(world::SimBall sim_ball){
-    const btVector3 position = sim_ball.position();
-    const btVector3 velocity = sim_ball.speed();
-    return Ball(
-        {position[0], position[1]}, 
-        {velocity[0], velocity[1]}, 
-        TimeStamp::fromSeconds(0)
-    );
+Ball createBall(world::SimBall sim_ball, TimeStamp timestamp){
+    Point position(sim_ball.p_x(), sim_ball.p_y());
+    Vector velocity(sim_ball.v_x(), sim_ball.v_y());
+    BallState state(position, velocity);
+    return Ball(state, timestamp);
 }
 
-Robot createRobot(world::SimRobot sim_robot){
-
+Robot createRobot(world::SimRobot sim_robot, TimeStamp timestamp){
+    RobotId id(sim_robot.id());
+    Point position(sim_robot.p_x(), sim_robot.p_y());
+    Vector velocity(sim_robot.v_x(), sim_robot.v_y());
+    AngularVelocity omega(sim_robot.r_x(), sim_robot.r_y());
+    Angle angle(2 * acos(sim_robot.rotation().real()));
+    RobotState state(position, velocity, angle, omega);
+    return Robot(id, state, timestamp);
 }
