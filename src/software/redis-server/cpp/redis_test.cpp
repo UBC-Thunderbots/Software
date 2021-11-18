444dfeb44cb1b185d31cf8b8db278ce287792e6b
Akhil Veeraghanta <31548846+akhilveeraghanta@users.noreply.github.com>
2021-11-18 00:12:53 -0800

    redis cpp builds (#2347)

--- src/software/redis-server/cpp/redis_test.cpp
+++ src/software/redis-server/cpp/redis_test.cpp
@@ -1,12 +1,35 @@
 #include <gtest/gtest.h>
-#include <sw/redis++/redis++.h>
+
 #include <iostream>
 #include <string>
+
+#include "cpp_redis/core/client.hpp"
 
 
 TEST(ServerTest, Get)
 {
-    auto redis = sw::redis::Redis("tcp://127.0.0.1:6379");
-    // redis.set("1","2");
-    EXPECT_EQ(3, 3);
+    cpp_redis::client client;
+    client.connect(
+        "127.0.0.1", 6379,
+        [](const std::string &host, std::size_t port, cpp_redis::connect_state status) {
+            if (status == cpp_redis::connect_state::dropped)
+            {
+                std::cout << "client disconnected from " << host << ":" << port
+                          << std::endl;
+            }
+        });
+
+    client.set("hello", "42", [](cpp_redis::reply &reply) {
+        std::cout << "set hello 42: " << reply << std::endl;
+    });
+
+    client.decrby("hello", 12, [](cpp_redis::reply &reply) {
+        std::cout << "decrby hello 12: " << reply << std::endl;
+    });
+
+    client.get("hello", [](cpp_redis::reply &reply) {
+        std::cout << "get hello: " << reply << std::endl;
+    });
+
+    client.sync_commit();
 }
