848d5f0ae7b071a8fcc038c08ce55e758e4716ed
alinas2000 <alinashabanova581@gmail.com>
2021-11-18 20:08:22 -0800

    ratepass has connected buttons and dropdown

--- src/python_tools/notebooks/ratepass_plot.py
+++ src/python_tools/notebooks/ratepass_plot.py
@@ -8,7 +8,7 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.11.2
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
@@ -16,6 +16,7 @@
 # +
 from proto.messages_robocup_ssl_wrapper_pb2 import SSL_WrapperPacket
 from python_tools.proto_log import ProtoLog
+from proto.ai_config_pb2 import AiControlConfig
 import ipywidgets
 from IPython.display import display
 from software.python_bindings import world, passing, pass_generator
@@ -31,7 +32,8 @@ world.getDefaultSensorFusionConfig()
 
 # +
 from bokeh.plotting import figure
-from bokeh.io import output_notebook, show, push_notebook
+from bokeh.io import output_notebook, show, push_notebook, curdoc
+from bokeh.models import Button, CustomJS, Select
 from python_tools.plotting.plot_ssl_wrapper import SSLWrapperPlotter, MM_PER_M
 from python_tools.plotting.plot_heatmap import HeatmapPlotter
 from python_tools.plotting.plot_pass_generator import PassGeneratorPlotter
@@ -123,3 +125,73 @@ show(fig, notebook_handle=True)
 
 slider = ipywidgets.IntSlider(min=0, max=len(wrapper_proto_log) - 1)
 ipywidgets.interact(plot_ssl_wrapper_at_idx, idx=slider)
+
+
+#      START AI BUTTON 
+buttonStart = ipywidgets.Button(description="Start AI")
+output = ipywidgets.Output()
+
+display(buttonStart, output)
+
+def start_clicked(b):
+    with output:
+        AiControlConfig.run_ai = 'true'
+        print(AiControlConfig.run_ai)
+
+buttonStart.on_click(start_clicked)
+
+
+#      STOP AI BUTTON
+buttonStop = ipywidgets.Button(description="Stop AI")
+output = ipywidgets.Output()
+
+display(buttonStop, output)
+
+def stop_clicked(b):
+    with output:
+        AiControlConfig.run_ai = 'false'
+        print(AiControlConfig.run_ai)
+
+buttonStop.on_click(stop_clicked)
+
+
+#       GAMESTATE OVERRIDE
+def gamestate(b):
+    with output:
+        AiControlConfig.override_ai_play = b.new 
+        print(AiControlConfig.override_ai_play)
+        
+gamestateOver = ipywidgets.Dropdown(
+    options=[(''), ('Use GameController'), ('HALT'), ('STOP'), ('NORMAL_START'), ('FORCE_START'), ('PREPARE_KICKOFF_US'), ('PREPARE_KICKOFF_THEM'), ('PREPARE_PENALTY_US'), ('PREPARE_PENALTY_THEM'), ('DIRECT_FREE_US'), ('DIRECT_FREE_THEM'), ('INDIRECT_FREE_US'), ('INDIRECT_FREE_THEM'), ('TIMEOUT_US'), ('TIMEOUT_THEM'), ('GOAL_US'), ('GOAL_THEM'), ('BALL_PLACEMENT_US'), ('BALL_PLACEMENT_THEM')],
+    value= '',
+    description='Gamestate Override:',
+)
+output = ipywidgets.Output()
+
+gamestateOver.observe(gamestate, names = 'value')
+display(gamestateOver, output)
+
+
+#        PLAY OVERRIDE 
+def play(b):
+    with output:
+        AiControlConfig.current_ai_play = b.new
+        print(AiControlConfig.current_ai_play)
+
+
+playOver = ipywidgets.Dropdown(
+    options=[(''), ('Use AI Selection'), ('HaltPlay')],
+    value= '',
+    description='Play Override:',
+)
+
+output = ipywidgets.Output()
+
+playOver.observe(play, names = 'value')
+display(playOver, output)
+
+
+
+# -
+
+
