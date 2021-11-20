69a390678f760c02ed3bf538c5d50dede36af9e0
Ubuntu <ubuntu@ip-172-31-9-125.ec2.internal>
2021-11-20 20:51:54 +0000

    formatting

--- src/proto/message_translation/er_force_world.cpp
+++ src/proto/message_translation/er_force_world.cpp
@@ -1,18 +1,21 @@
 #include "proto/message_translation/er_force_world.h"
 
-Ball createBall(world::SimBall sim_ball, Timestamp timestamp){
+Ball createBall(world::SimBall sim_ball, Timestamp timestamp)
+{
     const Point position(sim_ball.p_x(), sim_ball.p_y());
     const Vector velocity(sim_ball.v_x(), sim_ball.v_y());
     const BallState state(position, velocity);
     const Ball ball(state, timestamp);
     return ball;
 }
-Robot createRobot(world::SimRobot sim_robot, Timestamp timestamp){
+Robot createRobot(world::SimRobot sim_robot, Timestamp timestamp)
+{
     const RobotId id(sim_robot.id());
     const Point position(sim_robot.p_x(), sim_robot.p_y());
     const Vector velocity(sim_robot.v_x(), sim_robot.v_y());
     double angle = 2 * Angle::acos(sim_robot.rotation().real()).toRadians();
-    const RobotState state(position, velocity, Angle::fromRadians(angle), Angle::atan(sim_robot.r_y()/sim_robot.r_x()));
+    const RobotState state(position, velocity, Angle::fromRadians(angle),
+                           Angle::atan(sim_robot.r_y() / sim_robot.r_x()));
     const Robot robot(id, state, timestamp);
     return robot;
 }
--- src/proto/message_translation/er_force_world.h
+++ src/proto/message_translation/er_force_world.h
@@ -1,27 +1,27 @@
 #pragma once
 
-#include "software/world/ball.h"
-#include "software/world/robot.h"
-#include "software/time/timestamp.h"
 #include "extlibs/er_force_sim/src/protobuf/world.pb.h"
+#include "software/time/timestamp.h"
+#include "software/world/ball.h"
 #include "software/world/ball_state.h"
+#include "software/world/robot.h"
 #include "software/world/robot_state.h"
 
 /**
  * convert a er force simulator ball to a ball object
- * 
+ *
  * @param sim_ball: the ball information retrieved from the er force simulator
- * 
- * @return the corresponding ball object to the er simulator ball 
+ *
+ * @return the corresponding ball object to the er simulator ball
  */
 
 Ball createBall(world::SimBall sim_ball, Timestamp timestamp);
 
 /**
  * Convert a er force simulator robot to a robot object
- * 
- * @param sim_robot: the robot information retrieved from the er force simulator 
- * 
- * @return the corresponding robot object to the er simulator robot 
+ *
+ * @param sim_robot: the robot information retrieved from the er force simulator
+ *
+ * @return the corresponding robot object to the er simulator robot
  */
 Robot createRobot(world::SimRobot sim_robot, Timestamp timestamp);
