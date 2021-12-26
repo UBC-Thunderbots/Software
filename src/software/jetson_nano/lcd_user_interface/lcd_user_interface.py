1c7726f74ec2d453a90631e90359c869a719c691
Mason Wong <masonwong19@gmail.com>
2021-12-26 14:43:27 -0800

    Add libraries to BUILD and requirements.txt, add comments

--- src/software/jetson_nano/lcd_user_interface/lcd_user_interface.py
+++ src/software/jetson_nano/lcd_user_interface/lcd_user_interface.py
@@ -4,9 +4,9 @@ from PIL import Image, ImageDraw, ImageOps
 import adafruit_rgb_display.st7735 as st7735
 
 # Configuration for CS and DC pins (these are PiTFT defaults):
-CS_PIN = digitalio.DigitalInOut(board.CE0)
-DC_PIN = digitalio.DigitalInOut(board.D25)
-RESET_PIN = digitalio.DigitalInOut(board.D24)
+CS_PIN = digitalio.DigitalInOut(board.CE0)      # Pin 24
+DC_PIN = digitalio.DigitalInOut(board.D25)      # Pin 22
+RESET_PIN = digitalio.DigitalInOut(board.D24)   # Pin 18
 
 # Config for display baudrate (default max is 24mhz):
 BAUDRATE = 24000000
@@ -21,18 +21,19 @@ COLOUR_MODEL = 'RGB'
 
 class lcd_display:
     def __init__(self):
-        
+        """ Create a lcd_dislpay object """
         # Create the display for 1.8" ST7735R:
         self.disp = st7735.ST7735R(SPI, rotation=ROTATION, cs=CS_PIN, dc=DC_PIN, rst=RESET_PIN, baudrate=BAUDRATE)
         
-        # we swap height/width to rotate it to landscape!
+        # We swap height/width to rotate it to landscape
         self.width = self.disp.height
         self.height = self.disp.width
 
-        # initalize to an empty black screen
+        # Initalize to an empty black screen
         self.clear_screen()
 
     def clear_screen(self):
+        """ Clear this LCD dislpay, make it black """
         # Create blank image for drawing.
         image = Image.new(COLOUR_MODEL, (self.width, self.height))
 
@@ -44,9 +45,10 @@ class lcd_display:
         self.disp.image(image)
 
     def draw_logo(self):
+        """ Draw Thunderbots Logo on this LCD display """
         self.clear_screen()
 
-        image = Image.open("tbots.jpg")
+        image = Image.open("./imgs/tbots.jpg")
         image = ImageOps.invert(image)  # Image has inverted color
 
         # Scale the image to the smaller screen dimension
@@ -67,4 +69,3 @@ class lcd_display:
 
         # Display image
         self.disp.image(image)
-
