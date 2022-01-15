bda285e6643c6bee4b77831aad41da9750fe2bd1
boris <borisv.ca@gmail.com>
2022-01-14 18:35:14 -0800

    added deps to build file

--- src/software/jetson_nano/services/redis.cpp
+++ src/software/jetson_nano/services/redis.cpp
@@ -1,16 +1,17 @@
 #include "redis.h"
 
-#include "service.h"
 #include "software/logger/logger.h"
 
-RedisService::RedisService(std::string host, size_t port) : subscriber(), host(host), port(port) {}
+RedisService::RedisService(std::string host, size_t port)
+    : subscriber(), host(host), port(port)
+{
+}
 
 void RedisService::start()
 {
     subscriber.connect(
         host, port,
-        [](const std::string &host, std::size_t port, cpp_redis::connect_state status)
-        {
+        [](const std::string &host, std::size_t port, cpp_redis::connect_state status) {
             if (status == cpp_redis::connect_state::dropped)
             {
                 LOG(WARNING) << "Connection dropped";
@@ -32,7 +33,7 @@ void RedisService::stop()
 }
 
 void RedisService::subscribe(const std::string &channel,
-                      void (*subscribe_callback)(std::string, std::string))
+                             void (*subscribe_callback)(std::string, std::string))
 {
     subscriber.subscribe(channel, subscribe_callback);
 }
--- src/software/jetson_nano/services/redis.h
+++ src/software/jetson_nano/services/redis.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "cpp_redis/cpp_redis"
+#include "software/jetson_nano/services/service.h"
 #include "string"
 
 
-class RedisService : public Service {
-public:
 
+class RedisService : public Service
+{
+   public:
     explicit RedisService(std::string host, size_t port);
 
     virtual void start() override;
@@ -17,7 +19,7 @@ public:
                    void (*subscribe_callback)(std::string, std::string));
 
 
-private:
+   private:
     cpp_redis::subscriber subscriber;
     std::string host;
     size_t port;
