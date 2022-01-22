4931d2fbb4575aa979bd27c13ec44770463c59de
pj-programming <pscholtens2001@gmail.com>
2022-01-22 11:35:44 -0800

    Cleaning up

--- src/software/jetson_nano/services/network.cpp
+++ src/software/jetson_nano/services/network.cpp
@@ -1,47 +1,48 @@
-#include "software/networking/threaded_proto_udp_sender.hpp"
-#include "software/networking/threaded_proto_udp_listener.hpp"
 #include "software/jetson_nano/services/network.h"
 
+#include "software/networking/threaded_proto_udp_listener.hpp"
+#include "software/networking/threaded_proto_udp_sender.hpp"
 
 
-NetworkService::NetworkService(const std::string& ip_address, 
-                            unsigned short vision_listener_port,
-                            unsigned short primitive_listener_port,
-                            unsigned short robot_status_sender_port,
-                            bool multicast)
+
+NetworkService::NetworkService(const std::string& ip_address,
+                               unsigned short vision_listener_port,
+                               unsigned short primitive_listener_port,
+                               unsigned short robot_status_sender_port, bool multicast)
 {
-    sender = std::make_unique<ThreadedProtoUdpSender<TbotsProto::RobotStatus>>(ip_address, robot_status_sender_port, multicast);
-    listenerPrimitiveSet = std::make_unique<ThreadedProtoUdpListener<TbotsProto::PrimitiveSet>>(ip_address, primitive_listener_port, boost::bind(&NetworkService::primitiveSetCallback, this, _1), multicast);
-    listenerVision = std::make_unique<ThreadedProtoUdpListener<TbotsProto::Vision>>(ip_address, vision_listener_port, boost::bind(&NetworkService::visionCallback, this, _1), multicast);
-
+    sender = std::make_unique<ThreadedProtoUdpSender<TbotsProto::RobotStatus>>(
+        ip_address, robot_status_sender_port, multicast);
+    listener_primitive_set =
+        std::make_unique<ThreadedProtoUdpListener<TbotsProto::PrimitiveSet>>(
+            ip_address, primitive_listener_port,
+            boost::bind(&NetworkService::primitiveSetCallback, this, _1), multicast);
+    listener_vision = std::make_unique<ThreadedProtoUdpListener<TbotsProto::Vision>>(
+        ip_address, vision_listener_port,
+        boost::bind(&NetworkService::visionCallback, this, _1), multicast);
 }
 
-void NetworkService::start(){
+void NetworkService::start() {}
+NetworkService::~NetworkService() {}
 
-}
-NetworkService::~NetworkService(){}
-
-std::tuple<TbotsProto::PrimitiveSet, TbotsProto::Vision> NetworkService::poll(const TbotsProto::RobotStatus & robot_status){
-
+std::tuple<TbotsProto::PrimitiveSet, TbotsProto::Vision> NetworkService::poll(
+    const TbotsProto::RobotStatus& robot_status)
+{
     sender->sendProto(robot_status);
-    return std::make_tuple<TbotsProto::PrimitiveSet, TbotsProto::Vision>(std::move(PrimitiveSetInfo), std::move(VisionInfo));
-
-       
+    return std::make_tuple<TbotsProto::PrimitiveSet, TbotsProto::Vision>(
+        std::move(primitive_set_info), std::move(vision_info));
 }
 
 
-void NetworkService::stop(){
-    //Todo:
-
+void NetworkService::stop()
+{
+    // Todo:
 }
 
-void NetworkService::primitiveSetCallback(TbotsProto::PrimitiveSet input){
-    PrimitiveSetInfo = input;
-    std::cout<<"Received PrimitiveSet\n";
-
+void NetworkService::primitiveSetCallback(TbotsProto::PrimitiveSet input)
+{
+    primitive_set_info = input;
 }
-void NetworkService::visionCallback(TbotsProto::Vision input){
-    VisionInfo = input;
-    std::cout<<"Received vision\n";
+void NetworkService::visionCallback(TbotsProto::Vision input)
+{
+    vision_info = input;
 }
-
--- src/software/jetson_nano/services/network.h
+++ src/software/jetson_nano/services/network.h
@@ -1,35 +1,58 @@
 #pragma once
 
+#include "proto/robot_status_msg.pb.h"
+#include "proto/tbots_software_msgs.pb.h"
 #include "shared/robot_constants.h"
 #include "software/jetson_nano/services/service.h"
-#include "proto/tbots_software_msgs.pb.h"
-#include "proto/robot_status_msg.pb.h"
-#include "software/networking/threaded_proto_udp_sender.hpp"
 #include "software/networking/threaded_proto_udp_listener.hpp"
+#include "software/networking/threaded_proto_udp_sender.hpp"
 
-class NetworkService: public Service
+class NetworkService : public Service
 {
-    public:
-        NetworkService(const std::string& ip_address, 
-                            unsigned short vision_listener_port,
-                            unsigned short primitive_listener_port,
-                            unsigned short robot_status_sender_port,
-                            bool multicast);
-    
-        virtual ~NetworkService();
+   public:
+    /**
+     * Service that runs the Network
+     * Opens all the required ports and maintains them until destroyed.
+     *
+     * @param ip_address
+     * @param vision_listener_port
+     * @param primitive_listener_port
+     * @param robot_status_sender_port
+     * @param multicast
+     */
+    NetworkService(const std::string& ip_address, unsigned short vision_listener_port,
+                   unsigned short primitive_listener_port,
+                   unsigned short robot_status_sender_port, bool multicast);
 
-        std::tuple<TbotsProto::PrimitiveSet, TbotsProto::Vision> poll(const TbotsProto::RobotStatus & robot_status);
+    virtual ~NetworkService();
 
-        void start() override;
+    /**
+     * Starts the network service
+     */
+    void start() override;
 
-        void stop() override;
-    private:
-        std::unique_ptr<ThreadedProtoUdpSender<TbotsProto::RobotStatus>> sender;
-        std::unique_ptr<ThreadedProtoUdpListener<TbotsProto::PrimitiveSet>> listenerPrimitiveSet; 
-        std::unique_ptr<ThreadedProtoUdpListener<TbotsProto::Vision>> listenerVision;
-        void primitiveSetCallback(TbotsProto::PrimitiveSet input);
-        void visionCallback(TbotsProto::Vision input);
-        TbotsProto::PrimitiveSet PrimitiveSetInfo;
-        TbotsProto::Vision VisionInfo;
+    /**
+     * Disable the network service
+     */
+    void stop() override;
 
+    /**
+     * When the network service is polled, it senders the robot_status
+     * RobotConstants_t and WheelConstants_t.
+     * @returns a tuple of the stored primitive_set and vision
+     */
+    std::tuple<TbotsProto::PrimitiveSet, TbotsProto::Vision> poll(
+        const TbotsProto::RobotStatus& robot_status);
+
+   private:
+    // Variables
+    TbotsProto::PrimitiveSet primitive_set_info;
+    TbotsProto::Vision vision_info;
+    std::unique_ptr<ThreadedProtoUdpSender<TbotsProto::RobotStatus>> sender;
+    std::unique_ptr<ThreadedProtoUdpListener<TbotsProto::PrimitiveSet>>
+        listener_primitive_set;
+    std::unique_ptr<ThreadedProtoUdpListener<TbotsProto::Vision>> listener_vision;
+    // Functions to callback primitiveSet and vision and stores them in a variable
+    void primitiveSetCallback(TbotsProto::PrimitiveSet input);
+    void visionCallback(TbotsProto::Vision input);
 };
