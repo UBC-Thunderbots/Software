8b057563492267c7420b30d0a944c0ed1643775d
LiCody <Licody2000@gmail.com>
2022-01-27 21:27:01 -0800

    fix formatting

--- src/shared/uart_framing/uart_framing.hpp
+++ src/shared/uart_framing/uart_framing.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <cstdint>
-#include <vector>
 #include <optional>
+#include <vector>
 
 const uint8_t START_END_FLAG_BYTE = 0x00;
 
@@ -14,13 +14,15 @@ const uint8_t START_END_FLAG_BYTE = 0x00;
  * @tparam T the type of message being framed
  */
 template <typename T>
-struct UartMessageFrame {
+struct UartMessageFrame
+{
     uint16_t length;
     uint16_t crc;
     T message;
 };
 
-namespace {
+namespace
+{
     /**
      * Generates a CRC-16 adhering to the CRC-16-CCITT standard
      *
@@ -33,11 +35,12 @@ namespace {
     {
         uint8_t x;
         uint16_t crc = 0xFFFF;
-        auto ptr = reinterpret_cast<const uint8_t*>(&data);
-        auto bytes = std::vector<uint8_t>(ptr, ptr + sizeof(data));
-        int i = 0;
+        auto ptr     = reinterpret_cast<const uint8_t*>(&data);
+        auto bytes   = std::vector<uint8_t>(ptr, ptr + sizeof(data));
+        int i        = 0;
 
-        while(length--) {
+        while (length--)
+        {
             x = static_cast<uint8_t>(crc >> 8u) ^ bytes[i++];
             x ^= static_cast<uint8_t>(x >> 4u);
             crc = static_cast<uint16_t>((crc << 8u) ^ (x << 12u) ^ (x << 5u) ^ x);
@@ -57,8 +60,8 @@ namespace {
     UartMessageFrame<T> addLengthAndCrc(const T& data)
     {
         uint16_t length = sizeof(data);
-        uint16_t crc = crc16(data, length);
-        UartMessageFrame<T> frame {length, crc, data};
+        uint16_t crc    = crc16(data, length);
+        UartMessageFrame<T> frame{length, crc, data};
         return frame;
     }
 
@@ -73,7 +76,7 @@ namespace {
     bool verifyLengthAndCrc(UartMessageFrame<T> data)
     {
         uint16_t expected_length = sizeof(data.message);
-        uint16_t expected_crc = crc16(data.message, data.length);
+        uint16_t expected_crc    = crc16(data.message, data.length);
         return data.crc == expected_crc && data.length == expected_length;
     }
     /**
@@ -91,19 +94,24 @@ namespace {
         encoded.emplace_back(START_END_FLAG_BYTE);
 
         size_t overhead_location = 1;
-        uint8_t overhead = 0x01;
+        uint8_t overhead         = 0x01;
 
-        for (auto byte: to_encode) {
-            if (byte == START_END_FLAG_BYTE) {
+        for (auto byte : to_encode)
+        {
+            if (byte == START_END_FLAG_BYTE)
+            {
                 encoded.insert(encoded.begin() + overhead_location, overhead);
                 overhead_location = encoded.size();
-                overhead = 0x01;
-            } else {
+                overhead          = 0x01;
+            }
+            else
+            {
                 encoded.emplace_back(byte);
-                if (++overhead == 0xFF) {
+                if (++overhead == 0xFF)
+                {
                     encoded.insert(encoded.begin() + overhead_location, overhead);
                     overhead_location = encoded.size();
-                    overhead = 0x01;
+                    overhead          = 0x01;
                 }
             }
         }
@@ -122,30 +130,38 @@ namespace {
      * @param to_decode the vector of bytes to decode
      * @return a vector of bytes decoded with COBS
      */
-    std::optional<std::vector<uint8_t>> cobsDecoding(const std::vector<uint8_t>& to_decode)
+    std::optional<std::vector<uint8_t>> cobsDecoding(
+        const std::vector<uint8_t>& to_decode)
     {
-        if (to_decode.front() != START_END_FLAG_BYTE || to_decode.back() != START_END_FLAG_BYTE) {
+        if (to_decode.front() != START_END_FLAG_BYTE ||
+            to_decode.back() != START_END_FLAG_BYTE)
+        {
             return std::nullopt;
         }
         auto decoded = std::vector<uint8_t>();
 
         uint16_t i = 1;
-        while (i < to_decode.size()) {
+        while (i < to_decode.size())
+        {
             uint8_t overhead = to_decode[i++];
-            for (uint16_t j = 1; i < to_decode.size() && j < overhead; j++) {
+            for (uint16_t j = 1; i < to_decode.size() && j < overhead; j++)
+            {
                 decoded.emplace_back(to_decode[i++]);
             }
-            if (overhead < 0xFF && i != to_decode.size() - 1 && overhead != START_END_FLAG_BYTE) {
+            if (overhead < 0xFF && i != to_decode.size() - 1 &&
+                overhead != START_END_FLAG_BYTE)
+            {
                 decoded.emplace_back(START_END_FLAG_BYTE);
             }
         }
         // Make sure decoded message is within the upper bound of bytes
-        if (decoded.size() + 3 + decoded.size() / 254 < to_decode.size()) {
+        if (decoded.size() + 3 + decoded.size() / 254 < to_decode.size())
+        {
             return std::nullopt;
         }
         return decoded;
     }
-} // anonymous namespace
+}  // anonymous namespace
 
 /**
  * Prepares a struct for being sent over uart.
@@ -159,14 +175,16 @@ template <typename T>
 std::vector<uint8_t> marshallUartPacket(const T& data)
 {
     auto framed_packet_struct = addLengthAndCrc(data);
-    auto framed_packet_ptr = reinterpret_cast<const char*>(&framed_packet_struct);
-    std::vector<uint8_t> framed_packet (framed_packet_ptr, framed_packet_ptr + sizeof(framed_packet_struct));
+    auto framed_packet_ptr    = reinterpret_cast<const char*>(&framed_packet_struct);
+    std::vector<uint8_t> framed_packet(framed_packet_ptr,
+                                       framed_packet_ptr + sizeof(framed_packet_struct));
     return cobsEncoding(framed_packet);
 }
 
 /**
  * Converts a vector of bytes to its corresponding UartMessageFrame if it exists.
- * Returns std::nullopt if bit or length errors are detected while decoding or through the crc.
+ * Returns std::nullopt if bit or length errors are detected while decoding or through the
+ * crc.
  *
  * @tparam T type of UartMessageFrame to unmarshal to
  * @param data vector of bytes to unmarshal
@@ -177,17 +195,23 @@ template <typename T>
 std::optional<UartMessageFrame<T>> unmarshalUartPacket(const std::vector<uint8_t>& data)
 {
     auto decoded_data = cobsDecoding(data);
-    if (!decoded_data.has_value()) {
+    if (!decoded_data.has_value())
+    {
         return std::nullopt;
     }
     UartMessageFrame<T> message_frame;
-    if (decoded_data->size() != sizeof(message_frame)) {
+    if (decoded_data->size() != sizeof(message_frame))
+    {
         return std::nullopt;
     }
-    std::copy(decoded_data->begin(), decoded_data->end(), reinterpret_cast<uint8_t*>(&message_frame));
-    if (verifyLengthAndCrc(message_frame)) {
+    std::copy(decoded_data->begin(), decoded_data->end(),
+              reinterpret_cast<uint8_t*>(&message_frame));
+    if (verifyLengthAndCrc(message_frame))
+    {
         return message_frame;
-    } else {
+    }
+    else
+    {
         return std::nullopt;
     }
 }
--- src/shared/uart_framing/uart_framing_test.cpp
+++ src/shared/uart_framing/uart_framing_test.cpp
@@ -2,15 +2,17 @@
 
 #include <gtest/gtest.h>
 
-class CobsEncodingTest
-    : public ::testing::TestWithParam<std::tuple<std::vector<uint8_t>, std::vector<uint8_t>>>
+class CobsEncodingTest : public ::testing::TestWithParam<
+                             std::tuple<std::vector<uint8_t>, std::vector<uint8_t>>>
 {
 };
 
 // Returns 0x00 0x01 0x02 0x03 0x04 ... 0xFB 0xFC 0xFD 0xFE 0xFF
-static std::vector<uint8_t> generateBigByteVectorToEncode() {
+static std::vector<uint8_t> generateBigByteVectorToEncode()
+{
     std::vector<uint8_t> data;
-    for (uint8_t j = 0x00; j < 0xFF; j++) {
+    for (uint8_t j = 0x00; j < 0xFF; j++)
+    {
         data.emplace_back(j);
     }
     data.emplace_back(0xFF);
@@ -18,7 +20,8 @@ static std::vector<uint8_t> generateBigByteVectorToEncode() {
 }
 
 // Returns 0x00 0x01 0xFF 0x01 0x02 ... 0xFD 0xFE 0x02 0xFF 0x00
-static std::vector<uint8_t> generateBigByteVectorToDecode() {
+static std::vector<uint8_t> generateBigByteVectorToDecode()
+{
     std::vector<uint8_t> data = generateBigByteVectorToEncode();
     std::replace(data.begin(), data.end(), 0x00, 0xFF);
     // insert start and end bytes
@@ -45,20 +48,26 @@ TEST_P(CobsEncodingTest, encode_decode_test)
     EXPECT_EQ(*decoded, std::get<0>(GetParam()));
 }
 
-INSTANTIATE_TEST_CASE_P(encode_decode_test, CobsEncodingTest,
+INSTANTIATE_TEST_CASE_P(
+    encode_decode_test, CobsEncodingTest,
     ::testing::Values(
-        std::make_tuple(
-                std::vector<uint8_t>({0x01}),
-                std::vector<uint8_t>({START_END_FLAG_BYTE, 0x02, 0x01, START_END_FLAG_BYTE})),
-        std::make_tuple(
-                std::vector<uint8_t>({START_END_FLAG_BYTE}), std::vector<uint8_t>({START_END_FLAG_BYTE, 0x01, 0x01, START_END_FLAG_BYTE})),
-        std::make_tuple(std::vector<uint8_t>({0x01, 0x02, 0x03, 0x04}), std::vector<uint8_t>({START_END_FLAG_BYTE, 0x05, 0x01, 0x02, 0x03, 0x04, START_END_FLAG_BYTE})),
-        std::make_tuple(std::vector<uint8_t>({START_END_FLAG_BYTE, START_END_FLAG_BYTE, START_END_FLAG_BYTE, START_END_FLAG_BYTE}), std::vector<uint8_t>({START_END_FLAG_BYTE, 0x01, 0x01, 0x01, 0x01, 0x01, START_END_FLAG_BYTE})),
-        std::make_tuple(generateBigByteVectorToEncode(), generateBigByteVectorToDecode())
-    ));
+        std::make_tuple(std::vector<uint8_t>({0x01}),
+                        std::vector<uint8_t>({START_END_FLAG_BYTE, 0x02, 0x01,
+                                              START_END_FLAG_BYTE})),
+        std::make_tuple(std::vector<uint8_t>({START_END_FLAG_BYTE}),
+                        std::vector<uint8_t>({START_END_FLAG_BYTE, 0x01, 0x01,
+                                              START_END_FLAG_BYTE})),
+        std::make_tuple(std::vector<uint8_t>({0x01, 0x02, 0x03, 0x04}),
+                        std::vector<uint8_t>({START_END_FLAG_BYTE, 0x05, 0x01, 0x02, 0x03,
+                                              0x04, START_END_FLAG_BYTE})),
+        std::make_tuple(std::vector<uint8_t>({START_END_FLAG_BYTE, START_END_FLAG_BYTE,
+                                              START_END_FLAG_BYTE, START_END_FLAG_BYTE}),
+                        std::vector<uint8_t>({START_END_FLAG_BYTE, 0x01, 0x01, 0x01, 0x01,
+                                              0x01, START_END_FLAG_BYTE})),
+        std::make_tuple(generateBigByteVectorToEncode(),
+                        generateBigByteVectorToDecode())));
 
-class CobsDecodingErrorTest
-        : public ::testing::TestWithParam<std::vector<uint8_t>>
+class CobsDecodingErrorTest : public ::testing::TestWithParam<std::vector<uint8_t>>
 {
 };
 
@@ -69,41 +78,41 @@ TEST_P(CobsDecodingErrorTest, decode_error_tests)
 }
 
 INSTANTIATE_TEST_CASE_P(decode_error_tests, CobsDecodingErrorTest,
-    ::testing::Values(
-        std::vector<uint8_t>({0x01}),
-        std::vector<uint8_t>({0x01, START_END_FLAG_BYTE}),
-        std::vector<uint8_t>(100, START_END_FLAG_BYTE),
-        std::vector<uint8_t>(100, 0xFF)
-    ));
+                        ::testing::Values(std::vector<uint8_t>({0x01}),
+                                          std::vector<uint8_t>({0x01,
+                                                                START_END_FLAG_BYTE}),
+                                          std::vector<uint8_t>(100, START_END_FLAG_BYTE),
+                                          std::vector<uint8_t>(100, 0xFF)));
 
-class UartFramingTest : public ::testing::Test {
-protected:
-    struct __attribute__((__packed__)) TestMessage {
-        uint8_t  a;
+class UartFramingTest : public ::testing::Test
+{
+   protected:
+    struct __attribute__((__packed__)) TestMessage
+    {
+        uint8_t a;
         uint16_t b;
         uint32_t c;
         uint64_t d;
         char e;
-    public:
+
+       public:
         bool operator==(const TestMessage& rhs) const
         {
-            return a == rhs.a &&
-                b == rhs.b &&
-                c == rhs.c &&
-                d == rhs.d &&
-                e == rhs.e;
+            return a == rhs.a && b == rhs.b && c == rhs.c && d == rhs.d && e == rhs.e;
         }
     };
 
-    void SetUp() override {
+    void SetUp() override
+    {
         test_message = {1, 2, 3, 4, 'e'};
     }
 
     TestMessage test_message;
-    const uint16_t TEST_MESSAGE_CRC = 57534; // From online calculator
+    const uint16_t TEST_MESSAGE_CRC = 57534;  // From online calculator
 };
 
-TEST_F(UartFramingTest, length_and_crc_test) {
+TEST_F(UartFramingTest, length_and_crc_test)
+{
     auto test_frame = addLengthAndCrc(test_message);
     // Check size of frame is the size of the original struct + length and crc fields
     EXPECT_EQ(sizeof(test_frame), sizeof(TestMessage) + 2 * sizeof(uint16_t));
@@ -114,30 +123,37 @@ TEST_F(UartFramingTest, length_and_crc_test) {
     EXPECT_TRUE(verifyLengthAndCrc(test_frame));
 }
 
-TEST_F(UartFramingTest, verify_length_and_crc_detect_wrong_length_and_crc_test) {
-    UartMessageFrame<TestMessage> test_frame = {sizeof(test_message), TEST_MESSAGE_CRC, test_message};
+TEST_F(UartFramingTest, verify_length_and_crc_detect_wrong_length_and_crc_test)
+{
+    UartMessageFrame<TestMessage> test_frame = {sizeof(test_message), TEST_MESSAGE_CRC,
+                                                test_message};
     EXPECT_TRUE(verifyLengthAndCrc(test_frame));
     test_frame.length = 0;
     EXPECT_FALSE(verifyLengthAndCrc(test_frame));
     test_frame.length = sizeof(test_message);
-    test_frame.crc = 0;
+    test_frame.crc    = 0;
     EXPECT_FALSE(verifyLengthAndCrc(test_frame));
 }
 
-TEST_F(UartFramingTest, verify_length_and_crc_detect_byte_error_test) {
+TEST_F(UartFramingTest, verify_length_and_crc_detect_byte_error_test)
+{
     // Convert original message to vector of bytes
     auto test_message_ptr = reinterpret_cast<uint8_t*>(&test_message);
-    std::vector<uint8_t> bytes (test_message_ptr, test_message_ptr + sizeof(test_message_ptr));
+    std::vector<uint8_t> bytes(test_message_ptr,
+                               test_message_ptr + sizeof(test_message_ptr));
     // Add byte error
     bytes[2] = 0xFF;
-    //Convert back to TestMessage
+    // Convert back to TestMessage
     TestMessage test_message_byte_error;
-    std::copy(bytes.begin(), bytes.end(), reinterpret_cast<uint8_t*>(&test_message_byte_error));
-    UartMessageFrame<TestMessage> test_frame = {sizeof(test_message), TEST_MESSAGE_CRC, test_message_byte_error};
+    std::copy(bytes.begin(), bytes.end(),
+              reinterpret_cast<uint8_t*>(&test_message_byte_error));
+    UartMessageFrame<TestMessage> test_frame = {sizeof(test_message), TEST_MESSAGE_CRC,
+                                                test_message_byte_error};
     EXPECT_FALSE(verifyLengthAndCrc(test_frame));
 }
 
-TEST_F(UartFramingTest, marshalling_test) {
+TEST_F(UartFramingTest, marshalling_test)
+{
     // bytes is expected to be of form 0x00 0x02 0x10 0x05 ... 0x65 0x00
     auto bytes = marshallUartPacket(test_message);
     EXPECT_EQ(bytes.front(), START_END_FLAG_BYTE);
@@ -147,7 +163,8 @@ TEST_F(UartFramingTest, marshalling_test) {
     // Check second last byte 'e'
     EXPECT_EQ(bytes.end()[-2], 0x65);
     // Check that size makes sense
-    EXPECT_EQ(bytes.size(), sizeof(test_message) + 2 * sizeof(uint16_t) + 3 * sizeof(uint8_t));
+    EXPECT_EQ(bytes.size(),
+              sizeof(test_message) + 2 * sizeof(uint16_t) + 3 * sizeof(uint8_t));
 
     auto test_frame = unmarshalUartPacket<TestMessage>(bytes);
     // Check size of frame is the size of the original struct + length and crc fields
