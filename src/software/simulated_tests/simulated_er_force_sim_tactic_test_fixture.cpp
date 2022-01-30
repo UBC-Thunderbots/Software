e099e83e6b04927ad4da9de671bcf93ec0385d45
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2022-01-29 23:23:59 -0800

    wip

---
+++ src/software/simulated_tests/simulated_er_force_sim_tactic_test_fixture.cpp
@@ -0,0 +1,187 @@
+#include "software/simulated_tests/simulated_er_force_sim_tactic_test_fixture.h"
+
+#include "proto/message_translation/primitive_google_to_nanopb_converter.h"
+#include "proto/message_translation/tbots_protobuf.h"
+#include "software/ai/navigator/path_manager/velocity_obstacle_path_manager.h"
+#include "software/ai/navigator/path_planner/theta_star_path_planner.h"
+#include "software/gui/drawing/navigator.h"
+#include "software/simulation/er_force_simulator.h"
+#include "software/test_util/test_util.h"
+
+SimulatedErForceSimTacticTestFixture::SimulatedErForceSimTacticTestFixture()
+    : friendly_motion_constraints(),
+      enemy_motion_constraints(),
+      friendly_navigator(std::make_shared<Navigator>(
+          std::make_unique<VelocityObstaclePathManager>(
+              std::make_unique<ThetaStarPathPlanner>(),
+              RobotNavigationObstacleFactory(
+                  friendly_thunderbots_config->getRobotNavigationObstacleConfig())),
+          RobotNavigationObstacleFactory(
+              friendly_thunderbots_config->getRobotNavigationObstacleConfig()),
+          friendly_thunderbots_config->getNavigatorConfig())),
+      enemy_navigator(std::make_shared<Navigator>(
+          std::make_unique<VelocityObstaclePathManager>(
+              std::make_unique<ThetaStarPathPlanner>(),
+              RobotNavigationObstacleFactory(
+                  enemy_thunderbots_config->getRobotNavigationObstacleConfig())),
+          RobotNavigationObstacleFactory(
+              enemy_thunderbots_config->getRobotNavigationObstacleConfig()),
+          enemy_thunderbots_config->getNavigatorConfig()))
+{
+}
+
+void SimulatedErForceSimTacticTestFixture::SetUp()
+{
+    SimulatedErForceSimTestFixture::SetUp();
+    friendly_navigator = std::make_shared<Navigator>(
+        std::make_unique<VelocityObstaclePathManager>(
+            std::make_unique<ThetaStarPathPlanner>(),
+            RobotNavigationObstacleFactory(
+                friendly_thunderbots_config->getRobotNavigationObstacleConfig())),
+        RobotNavigationObstacleFactory(
+            friendly_thunderbots_config->getRobotNavigationObstacleConfig()),
+        friendly_thunderbots_config->getNavigatorConfig());
+    enemy_navigator = std::make_shared<Navigator>(
+        std::make_unique<VelocityObstaclePathManager>(
+            std::make_unique<ThetaStarPathPlanner>(),
+            RobotNavigationObstacleFactory(
+                enemy_thunderbots_config->getRobotNavigationObstacleConfig())),
+        RobotNavigationObstacleFactory(
+            enemy_thunderbots_config->getRobotNavigationObstacleConfig()),
+        enemy_thunderbots_config->getNavigatorConfig());
+}
+
+void SimulatedErForceSimTacticTestFixture::setTactic(std::shared_ptr<Tactic> friendly_tactic,
+                                           std::shared_ptr<Tactic> enemy_tactic)
+{
+    setFriendlyTactic(friendly_tactic);
+    setEnemyTactic(enemy_tactic);
+}
+
+void SimulatedErForceSimTacticTestFixture::setFriendlyTactic(std::shared_ptr<Tactic> tactic)
+{
+    if (tactic)
+    {
+        this->friendly_tactic = tactic;
+    }
+    else
+    {
+        LOG(FATAL) << "Friendly tactic is invalid" << std::endl;
+    }
+}
+
+void SimulatedErForceSimTacticTestFixture::setEnemyTactic(std::shared_ptr<Tactic> tactic)
+{
+    if (tactic)
+    {
+        this->enemy_tactic = tactic;
+    }
+    else
+    {
+        LOG(FATAL) << "Enemy tactic is invalid" << std::endl;
+    }
+}
+
+void SimulatedErForceSimTacticTestFixture::setFriendlyRobotId(RobotId friendly_robot_id)
+{
+    this->friendly_robot_id = friendly_robot_id;
+}
+
+void SimulatedErForceSimTacticTestFixture::setBothRobotId(RobotId friendly_robot_id,
+                                                RobotId enemy_robot_id)
+{
+    this->friendly_robot_id = friendly_robot_id;
+    this->enemy_robot_id    = enemy_robot_id;
+}
+
+void SimulatedErForceSimTacticTestFixture::setMotionConstraints(
+    const std::set<MotionConstraint>& friendly_motion_constraints,
+    const std::set<MotionConstraint>& enemy_motion_constraints)
+{
+    this->friendly_motion_constraints = friendly_motion_constraints;
+    this->enemy_motion_constraints    = enemy_motion_constraints;
+}
+
+void SimulatedErForceSimTacticTestFixture::updatePrimitives(
+    const World& friendly_world, const World& enemy_world,
+    std::shared_ptr<ErForceSimulator> simulator_to_update)
+{
+    updateFriendlyPrimitives(friendly_world, simulator_to_update);
+    if (this->enemy_robot_id)
+    {
+        updateEnemyPrimitives(enemy_world, simulator_to_update);
+    }
+}
+
+void SimulatedErForceSimTacticTestFixture::updateFriendlyPrimitives(
+    const World& world, std::shared_ptr<ErForceSimulator> simulator_to_update)
+{
+    std::vector<std::unique_ptr<Intent>> intents;
+    auto start_tick_time = std::chrono::system_clock::now();
+
+    if (!friendly_robot_id)
+    {
+        LOG(FATAL) << "No friendly robot id set" << std::endl;
+    }
+    else if (auto new_robot = world.friendlyTeam().getRobotById(*friendly_robot_id))
+    {
+        auto intent = friendly_tactic->get(
+            *world.friendlyTeam().getRobotById(*friendly_robot_id), world);
+        intent->setMotionConstraints(friendly_motion_constraints);
+        intents.push_back(std::move(intent));
+    }
+    else
+    {
+        LOG(FATAL) << "No friendly robot with robot id " << *friendly_robot_id
+                   << std::endl;
+    }
+
+    auto primitive_set_msg = friendly_navigator->getAssignedPrimitives(world, intents);
+    double duration_ms     = ::TestUtil::millisecondsSince(start_tick_time);
+    registerFriendlyTickTime(duration_ms);
+    simulator_to_update->setYellowRobotPrimitiveSet(
+        createNanoPbPrimitiveSet(*primitive_set_msg));
+}
+
+void SimulatedErForceSimTacticTestFixture::updateEnemyPrimitives(
+    const World& world, std::shared_ptr<ErForceSimulator> simulator_to_update)
+{
+    std::vector<std::unique_ptr<Intent>> intents;
+    auto start_tick_time = std::chrono::system_clock::now();
+
+    if (!enemy_robot_id)
+    {
+        LOG(FATAL) << "No enemy robot id set" << std::endl;
+    }
+    else if (auto new_robot = world.friendlyTeam().getRobotById(*enemy_robot_id))
+    {
+        auto intent =
+            enemy_tactic->get(*world.friendlyTeam().getRobotById(*enemy_robot_id), world);
+        intent->setMotionConstraints(enemy_motion_constraints);
+        intents.push_back(std::move(intent));
+    }
+    else
+    {
+        LOG(FATAL) << "No enemy robot with robot id " << *enemy_robot_id << std::endl;
+    }
+
+    auto primitive_set_msg = enemy_navigator->getAssignedPrimitives(world, intents);
+    double duration_ms     = ::TestUtil::millisecondsSince(start_tick_time);
+    registerEnemyTickTime(duration_ms);
+    auto defending_side = DefendingSideProto();
+    defending_side.set_defending_side(
+        DefendingSideProto::FieldSide::DefendingSideProto_FieldSide_POS_X);
+    simulator_to_update->setBlueTeamDefendingSide(defending_side);
+    simulator_to_update->setBlueRobotPrimitiveSet(
+        createNanoPbPrimitiveSet(*primitive_set_msg));
+}
+
+std::optional<TbotsProto::PlayInfo> SimulatedErForceSimTacticTestFixture::getPlayInfo()
+{
+    return std::nullopt;
+}
+
+AIDrawFunction SimulatedErForceSimTacticTestFixture::getDrawFunctions()
+{
+    return drawNavigator(friendly_navigator);
+}
--- src/software/simulated_tests/simulated_er_force_sim_test_fixture.h
+++ src/software/simulated_tests/simulated_er_force_sim_test_fixture.h
@@ -83,15 +83,24 @@ class SimulatedErForceSimTestFixture : public ::testing::Test
         const Duration &timeout);
 
     /**
-     * Registers a new tick time for calculating tick time statistics
+     * Registers a new tick time for calculating friendly tick time statistics
      *
      * @param tick_time_ms The tick time in milliseconds
      */
-    void registerTickTime(double tick_time_ms);
+    void registerFriendlyTickTime(double tick_time_ms);
+
+    /**
+     * Registers a new tick time for calculating enemy tick time statistics
+     *
+     * @param tick_time_ms The tick time in milliseconds
+     */
+    void registerEnemyTickTime(double tick_time_ms);
 
     // The dynamic params being used in the tests
-    std::shared_ptr<ThunderbotsConfig> mutable_thunderbots_config;
-    std::shared_ptr<const ThunderbotsConfig> thunderbots_config;
+    std::shared_ptr<ThunderbotsConfig> friendly_mutable_thunderbots_config;
+    std::shared_ptr<ThunderbotsConfig> enemy_mutable_thunderbots_config;
+    std::shared_ptr<const ThunderbotsConfig> friendly_thunderbots_config;
+    std::shared_ptr<const ThunderbotsConfig> enemy_thunderbots_config;
 
    private:
     /**
@@ -105,7 +114,8 @@ class SimulatedErForceSimTestFixture : public ::testing::Test
      * @return if validation functions are done
      */
     bool tickTest(Duration simulation_time_step, Duration ai_time_step,
-                  std::shared_ptr<World> world,
+                  std::shared_ptr<World> friendly_world,
+                  std::shared_ptr<World> enemy_world,
                   std::shared_ptr<ErForceSimulator> simulator);
 
     /**
@@ -119,10 +129,11 @@ class SimulatedErForceSimTestFixture : public ::testing::Test
     /**
      * Updates primitives in the simulator based on the new world
      *
-     * @param world to update primitives with
+     * @param friendly_world to update primitives with
+     * @param enemy_world to update primitives with
      * @param simulator_to_update The simulator to update
      */
-    virtual void updatePrimitives(
+    virtual void updatePrimitives(const World &friendly_world, const World &enemy_world,
         const World &world, std::shared_ptr<ErForceSimulator> simulator_to_update) = 0;
 
     /**
@@ -174,7 +185,8 @@ class SimulatedErForceSimTestFixture : public ::testing::Test
     // object so we can assign new instances to this variable
     std::shared_ptr<ErForceSimulator> simulator;
     // The SensorFusion being tested and used in simulation
-    SensorFusion sensor_fusion;
+    SensorFusion friendly_sensor_fusion;
+    SensorFusion enemy_sensor_fusion;
 
     // whether we should log the filtered and unfiltered world states as replay logs
     // this will only be set to true if the environment variable
@@ -195,13 +207,17 @@ class SimulatedErForceSimTestFixture : public ::testing::Test
 
     // These variables track tick time statistics
     // Total duration of all ticks registered
-    double total_tick_duration;
+    double total_friendly_tick_duration;
+    double total_enemy_tick_duration;
     // The max tick duration registered
-    double max_tick_duration;
+    double max_friendly_tick_duration;
+    double max_enemy_tick_duration;
     // The min tick duration registered
-    double min_tick_duration;
+    double min_friendly_tick_duration;
+    double min_enemy_tick_duration;
     // Total number of ticks registered
-    unsigned int tick_count;
+    unsigned int friendly_tick_count;
+    unsigned int enemy_tick_count;
 
     // The rate at which camera data will be simulated and given to SensorFusion.
     // Each sequential "camera frame" will be 1 / SIMULATED_CAMERA_FPS time step
