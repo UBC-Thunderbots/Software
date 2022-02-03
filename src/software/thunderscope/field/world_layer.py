21270616f0d33494ebbd2321d32ca86b1583f124
divy-07 <divy07.com@gmail.com>
2022-02-02 20:36:51 -0800

    id showing, but upside down

--- src/software/thunderscope/field/world_layer.py
+++ src/software/thunderscope/field/world_layer.py
@@ -81,6 +81,42 @@ class WorldLayer(FieldLayer):
 
         for robot in team.team_robots:
 
+            painter.setPen(pg.mkPen(colors.ROBOT_ID_COLOR))
+
+            robot_id_font = painter.font()
+            robot_id_font.setPointSize(200)
+            painter.setFont(robot_id_font)
+
+            robot_id_bounding_rect = QtCore.QRectF(robot.current_state.global_position.x_meters * MM_PER_M - (1 * ROBOT_MAX_RADIUS),
+                                                   robot.current_state.global_position.y_meters * MM_PER_M + (2 * ROBOT_MAX_RADIUS),
+                                                   ROBOT_MAX_RADIUS * MM_PER_M,
+                                                   ROBOT_MAX_RADIUS * MM_PER_M)
+
+            scaling_factor = 1.0 / (robot_id_bounding_rect.width() / (ROBOT_MAX_RADIUS * 2))
+
+            # unsuccessful scaling attempts:
+            # QtGui.QTransform(scaling_factor, 0, 0, -scaling_factor, 0, 0)
+            # QtGui.QTransform.scale(scaling_factor, scaling_factor)
+
+            #painter.scale(1.0, -1.0)
+            painter.drawText(robot_id_bounding_rect, str(robot.id))
+
+            """
+            Working code:
+            painter.setPen(pg.mkPen(colors.ROBOT_ID_COLOR))
+
+            robot_id_font = painter.font()
+            robot_id_font.setPointSize(200)
+            painter.setFont(robot_id_font)
+
+            robot_id_bounding_rect = QtCore.QRectF(robot.current_state.global_position.x_meters * MM_PER_M - (1 * ROBOT_MAX_RADIUS),
+                                                   robot.current_state.global_position.y_meters * MM_PER_M + (2 * ROBOT_MAX_RADIUS),
+                                                   ROBOT_MAX_RADIUS * MM_PER_M,
+                                                   ROBOT_MAX_RADIUS * MM_PER_M)
+                                                   
+            painter.drawText(robot_id_bounding_rect, str(robot.id))
+            """
+
             painter.setPen(pg.mkPen(color))
             painter.setBrush(pg.mkBrush(color))
 
