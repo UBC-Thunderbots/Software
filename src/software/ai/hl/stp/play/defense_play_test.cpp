bfdd474defbacddcb3630872126ec3984c01e58f
Resocram <marcoser2000@gmail.com>
2022-02-06 21:52:45 -0800

    Added back in defense play test

--- src/software/ai/hl/stp/play/defense_play_test.cpp
+++ src/software/ai/hl/stp/play/defense_play_test.cpp
@@ -17,122 +17,122 @@ class DefensePlayTest : public SimulatedPlayTestFixture
     Field field = Field::createSSLDivisionBField();
 };
 
-// TEST_F(DefensePlayTest, test_defense_play)
-// {
-//     BallState ball_state(Point(0.9, 2.85), Vector(0, 0));
-//     auto friendly_robots = TestUtil::createStationaryRobotStatesWithId(
-//         {Point(-4.5, 0), Point(-3, 1.5), Point(-3, 0.5), Point(-3, -0.5), Point(-3,
-//         -1.5),
-//          Point(-3, -3.0)});
-//     setFriendlyGoalie(0);
-//     auto enemy_robots = TestUtil::createStationaryRobotStatesWithId({
-//         field.enemyGoalCenter(),
-//         field.enemyDefenseArea().negXNegYCorner(),
-//         field.enemyDefenseArea().negXPosYCorner(),
-//         Point(1, 3),
-//         Point(-1, -0.25),
-//         Point(-2, -1.25),
-//     });
-//     setEnemyGoalie(0);
-//     setAIPlay(TYPENAME(DefensePlay));
-//     // We set the referee command to stop so that the robots do not kick/shoot during
-//     the
-//     // test
-//     setRefereeCommand(RefereeCommand::STOP, RefereeCommand::STOP);
+TEST_F(DefensePlayTest, test_defense_play)
+{
+    BallState ball_state(Point(0.9, 2.85), Vector(0, 0));
+    auto friendly_robots = TestUtil::createStationaryRobotStatesWithId(
+        {Point(-4.5, 0), Point(-3, 1.5), Point(-3, 0.5), Point(-3, -0.5), Point(-3,
+        -1.5),
+         Point(-3, -3.0)});
+    setFriendlyGoalie(0);
+    auto enemy_robots = TestUtil::createStationaryRobotStatesWithId({
+        field.enemyGoalCenter(),
+        field.enemyDefenseArea().negXNegYCorner(),
+        field.enemyDefenseArea().negXPosYCorner(),
+        Point(1, 3),
+        Point(-1, -0.25),
+        Point(-2, -1.25),
+    });
+    setEnemyGoalie(0);
+    setAIPlay(TYPENAME(DefensePlay));
+    // We set the referee command to stop so that the robots do not kick/shoot during
+    the
+    // test
+    setRefereeCommand(RefereeCommand::STOP, RefereeCommand::STOP);
 
-//     std::vector<ValidationFunction> terminating_validation_functions = {
-//         [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
-//             // Wait for all robots to come to a halt
-//             robotHalt(world_ptr, yield);
-//             // Attacker in front of enemy with the ball
-//             Rectangle attacker_rect(Point(0, 2.85), Point(0.9, 2));
-//             robotInPolygon(attacker_rect, 1, world_ptr, yield);
+    std::vector<ValidationFunction> terminating_validation_functions = {
+        [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
+            // Wait for all robots to come to a halt
+            robotHalt(world_ptr, yield);
+            // Attacker in front of enemy with the ball
+            Rectangle attacker_rect(Point(0, 2.85), Point(0.9, 2));
+            robotInPolygon(attacker_rect, 1, world_ptr, yield);
 
-//             // Two friendly robots in position to shadow enemy robots. One is on the
-//             enemy
-//             // with the ball and the other is on the next highest threat
-//             Rectangle shadowing_rect_1(Point(0.5, 2.5), Point(1, 2));
-//             Rectangle shadowing_rect_2(Point(-2, -0.75), Point(-1.5, -1.25));
-//             robotInPolygon(shadowing_rect_1, 1, world_ptr, yield);
-//             robotInPolygon(shadowing_rect_2, 1, world_ptr, yield);
+            // Two friendly robots in position to shadow enemy robots. One is on the
+            enemy
+            // with the ball and the other is on the next highest threat
+            Rectangle shadowing_rect_1(Point(0.5, 2.5), Point(1, 2));
+            Rectangle shadowing_rect_2(Point(-2, -0.75), Point(-1.5, -1.25));
+            robotInPolygon(shadowing_rect_1, 1, world_ptr, yield);
+            robotInPolygon(shadowing_rect_2, 1, world_ptr, yield);
 
-//             // Two friendly crease defenders should be close to the goalie
-//             Point goalie_position = world_ptr->friendlyTeam().goalie()->position();
-//             Rectangle left_crease_defender_rect(
-//                 Point(goalie_position.x(), goalie_position.y() + 0.4),
-//                 Point(goalie_position.x() + 0.3, goalie_position.y()));
-//             Rectangle right_crease_defender_rect(
-//                 Point(goalie_position.x(), goalie_position.y()),
-//                 Point(goalie_position.x() + 0.3, goalie_position.y() - 0.3));
-//             robotInPolygon(left_crease_defender_rect, 1, world_ptr, yield);
-//             robotInPolygon(right_crease_defender_rect, 1, world_ptr, yield);
-//         }};
+            // Two friendly crease defenders should be close to the goalie
+            Point goalie_position = world_ptr->friendlyTeam().goalie()->position();
+            Rectangle left_crease_defender_rect(
+                Point(goalie_position.x(), goalie_position.y() + 0.4),
+                Point(goalie_position.x() + 0.3, goalie_position.y()));
+            Rectangle right_crease_defender_rect(
+                Point(goalie_position.x(), goalie_position.y()),
+                Point(goalie_position.x() + 0.3, goalie_position.y() - 0.3));
+            robotInPolygon(left_crease_defender_rect, 1, world_ptr, yield);
+            robotInPolygon(right_crease_defender_rect, 1, world_ptr, yield);
+        }};
 
-//     std::vector<ValidationFunction> non_terminating_validation_functions = {
-//         [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield)
-//         {}};
+    std::vector<ValidationFunction> non_terminating_validation_functions = {
+        [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield)
+        {}};
 
-//     runTest(field, ball_state, friendly_robots, enemy_robots,
-//             terminating_validation_functions, non_terminating_validation_functions,
-//             Duration::fromSeconds(10));
-// }
+    runTest(field, ball_state, friendly_robots, enemy_robots,
+            terminating_validation_functions, non_terminating_validation_functions,
+            Duration::fromSeconds(10));
+}
 
-// TEST_F(DefensePlayTest, test_defense_play_one_immediate_threat)
-// {
-//     BallState ball_state(Point(-1.2, 0), Vector(0, 0));
-//     auto friendly_robots = TestUtil::createStationaryRobotStatesWithId(
-//         {Point(-4.5, 0), Point(-3, 1.5), Point(-3, 0.5), Point(-3, -0.5), Point(-3,
-//         -1.5),
-//          Point(-3, -3.0)});
-//     setFriendlyGoalie(0);
-//     auto enemy_robots = TestUtil::createStationaryRobotStatesWithId({
-//         field.enemyGoalCenter(),
-//         field.enemyDefenseArea().negXNegYCorner(),
-//         field.enemyDefenseArea().negXPosYCorner(),
-//         field.enemyDefenseArea().posXNegYCorner(),
-//         field.enemyDefenseArea().posXPosYCorner(),
-//         Point(-1, 0),
-//     });
-//     setEnemyGoalie(0);
-//     setAIPlay(TYPENAME(DefensePlay));
-//     // We set the referee command to stop so that the robots do not kick/shoot during
-//     the
-//     // test
-//     setRefereeCommand(RefereeCommand::STOP, RefereeCommand::STOP);
+TEST_F(DefensePlayTest, test_defense_play_one_immediate_threat)
+{
+    BallState ball_state(Point(-1.2, 0), Vector(0, 0));
+    auto friendly_robots = TestUtil::createStationaryRobotStatesWithId(
+        {Point(-4.5, 0), Point(-3, 1.5), Point(-3, 0.5), Point(-3, -0.5), Point(-3,
+        -1.5),
+         Point(-3, -3.0)});
+    setFriendlyGoalie(0);
+    auto enemy_robots = TestUtil::createStationaryRobotStatesWithId({
+        field.enemyGoalCenter(),
+        field.enemyDefenseArea().negXNegYCorner(),
+        field.enemyDefenseArea().negXPosYCorner(),
+        field.enemyDefenseArea().posXNegYCorner(),
+        field.enemyDefenseArea().posXPosYCorner(),
+        Point(-1, 0),
+    });
+    setEnemyGoalie(0);
+    setAIPlay(TYPENAME(DefensePlay));
+    // We set the referee command to stop so that the robots do not kick/shoot during
+    the
+    // test
+    setRefereeCommand(RefereeCommand::STOP, RefereeCommand::STOP);
 
-//     std::vector<ValidationFunction> terminating_validation_functions = {
-//         [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
-//             // Wait for all robots to come to a halt
-//             robotHalt(world_ptr, yield);
-//             // Attacker in front of enemy with the ball
-//             Rectangle attacker_rect(Point(-2.5, 0.5), Point(-1, -0.5));
-//             robotInPolygon(attacker_rect, 1, world_ptr, yield);
+    std::vector<ValidationFunction> terminating_validation_functions = {
+        [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
+            // Wait for all robots to come to a halt
+            robotHalt(world_ptr, yield);
+            // Attacker in front of enemy with the ball
+            Rectangle attacker_rect(Point(-2.5, 0.5), Point(-1, -0.5));
+            robotInPolygon(attacker_rect, 1, world_ptr, yield);
 
-//             // Two friendly robots swarming around the immediate threat
-//             Rectangle swarming_rect(Point(-2, 1), Point(0, -1));
-//             robotInPolygon(swarming_rect, 2, world_ptr, yield);
+            // Two friendly robots swarming around the immediate threat
+            Rectangle swarming_rect(Point(-2, 1), Point(0, -1));
+            robotInPolygon(swarming_rect, 2, world_ptr, yield);
 
-//             // Two friendly crease defenders should be close to the goalie
-//             Point goalie_position = world_ptr->friendlyTeam().goalie()->position();
-//             Rectangle left_crease_defender_rect(
-//                 Point(goalie_position.x(), goalie_position.y() + 0.3),
-//                 Point(goalie_position.x() + 0.3, goalie_position.y()));
-//             Rectangle right_crease_defender_rect(
-//                 Point(goalie_position.x(), goalie_position.y()),
-//                 Point(goalie_position.x() + 0.3, goalie_position.y() - 0.3));
-//             robotInPolygon(left_crease_defender_rect, 1, world_ptr, yield);
-//             robotInPolygon(right_crease_defender_rect, 1, world_ptr, yield);
-//         }};
+            // Two friendly crease defenders should be close to the goalie
+            Point goalie_position = world_ptr->friendlyTeam().goalie()->position();
+            Rectangle left_crease_defender_rect(
+                Point(goalie_position.x(), goalie_position.y() + 0.3),
+                Point(goalie_position.x() + 0.3, goalie_position.y()));
+            Rectangle right_crease_defender_rect(
+                Point(goalie_position.x(), goalie_position.y()),
+                Point(goalie_position.x() + 0.3, goalie_position.y() - 0.3));
+            robotInPolygon(left_crease_defender_rect, 1, world_ptr, yield);
+            robotInPolygon(right_crease_defender_rect, 1, world_ptr, yield);
+        }};
 
-//     std::vector<ValidationFunction> non_terminating_validation_functions = {
-//         [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
-//             ballInPlay(world_ptr, yield);
-//         }};
+    std::vector<ValidationFunction> non_terminating_validation_functions = {
+        [](std::shared_ptr<World> world_ptr, ValidationCoroutine::push_type& yield) {
+            ballInPlay(world_ptr, yield);
+        }};
 
-//     runTest(field, ball_state, friendly_robots, enemy_robots,
-//             terminating_validation_functions, non_terminating_validation_functions,
-//             Duration::fromSeconds(10));
-// }
+    runTest(field, ball_state, friendly_robots, enemy_robots,
+            terminating_validation_functions, non_terminating_validation_functions,
+            Duration::fromSeconds(10));
+}
 
 TEST_F(DefensePlayTest, test_defense_play_close_to_net)
 {
@@ -192,54 +192,54 @@ TEST_F(DefensePlayTest, test_defense_play_close_to_net)
             Duration::fromSeconds(10));
 }
 
-// TEST(DefensePlayIsApplicableInvariantHoldsTest,
-//      test_invariant_and_is_applicable_enemy_possession)
-// {
-//     auto play_config = std::make_shared<ThunderbotsConfig>()->getPlayConfig();
-//     auto world       = ::TestUtil::createBlankTestingWorld();
-//     world.setTeamWithPossession(TeamSide::ENEMY);
+TEST(DefensePlayIsApplicableInvariantHoldsTest,
+     test_invariant_and_is_applicable_enemy_possession)
+{
+    auto play_config = std::make_shared<ThunderbotsConfig>()->getPlayConfig();
+    auto world       = ::TestUtil::createBlankTestingWorld();
+    world.setTeamWithPossession(TeamSide::ENEMY);
 
-//     DefensePlay defense_play = DefensePlay(play_config);
+    DefensePlay defense_play = DefensePlay(play_config);
 
-//     // Gamestate isPlaying
-//     world.updateGameState(
-//         ::TestUtil::createGameState(RefereeCommand::FORCE_START,
-//         RefereeCommand::HALT));
+    // Gamestate isPlaying
+    world.updateGameState(
+        ::TestUtil::createGameState(RefereeCommand::FORCE_START,
+        RefereeCommand::HALT));
 
-//     EXPECT_TRUE(defense_play.isApplicable(world));
-//     EXPECT_TRUE(defense_play.invariantHolds(world));
+    EXPECT_TRUE(defense_play.isApplicable(world));
+    EXPECT_TRUE(defense_play.invariantHolds(world));
 
-//     // Gamestate isHalted
-//     world.updateGameState(
-//         ::TestUtil::createGameState(RefereeCommand::HALT,
-//         RefereeCommand::FORCE_START));
+    // Gamestate isHalted
+    world.updateGameState(
+        ::TestUtil::createGameState(RefereeCommand::HALT,
+        RefereeCommand::FORCE_START));
 
-//     EXPECT_FALSE(defense_play.isApplicable(world));
-//     EXPECT_FALSE(defense_play.invariantHolds(world));
-// }
+    EXPECT_FALSE(defense_play.isApplicable(world));
+    EXPECT_FALSE(defense_play.invariantHolds(world));
+}
 
-// TEST(DefensePlayIsApplicableInvarantHoldsTest,
-//      test_invariant_and_is_applicable_friendly_possession)
-// {
-//     auto play_config = std::make_shared<ThunderbotsConfig>()->getPlayConfig();
-//     auto world       = ::TestUtil::createBlankTestingWorld();
-//     world.setTeamWithPossession(TeamSide::FRIENDLY);
+TEST(DefensePlayIsApplicableInvarantHoldsTest,
+     test_invariant_and_is_applicable_friendly_possession)
+{
+    auto play_config = std::make_shared<ThunderbotsConfig>()->getPlayConfig();
+    auto world       = ::TestUtil::createBlankTestingWorld();
+    world.setTeamWithPossession(TeamSide::FRIENDLY);
 
-//     DefensePlay defense_play = DefensePlay(play_config);
+    DefensePlay defense_play = DefensePlay(play_config);
 
-//     // Gamestate isPlaying
-//     world.updateGameState(
-//         ::TestUtil::createGameState(RefereeCommand::FORCE_START,
-//         RefereeCommand::HALT));
+    // Gamestate isPlaying
+    world.updateGameState(
+        ::TestUtil::createGameState(RefereeCommand::FORCE_START,
+        RefereeCommand::HALT));
 
-//     EXPECT_FALSE(defense_play.isApplicable(world));
-//     EXPECT_FALSE(defense_play.invariantHolds(world));
+    EXPECT_FALSE(defense_play.isApplicable(world));
+    EXPECT_FALSE(defense_play.invariantHolds(world));
 
-//     // Gamestate isHalted
-//     world.updateGameState(
-//         ::TestUtil::createGameState(RefereeCommand::HALT,
-//         RefereeCommand::FORCE_START));
+    // Gamestate isHalted
+    world.updateGameState(
+        ::TestUtil::createGameState(RefereeCommand::HALT,
+        RefereeCommand::FORCE_START));
 
-//     EXPECT_FALSE(defense_play.isApplicable(world));
-//     EXPECT_FALSE(defense_play.invariantHolds(world));
-// }
+    EXPECT_FALSE(defense_play.isApplicable(world));
+    EXPECT_FALSE(defense_play.invariantHolds(world));
+}
