e299a2b99c964d11f398059a109cb2e8f16a6a7b
akhil <akhil.veeraghanta@outlook.com>
2022-02-07 11:32:30 -0800

    --------  nuke ---------

--- src/firmware/boards/legacy_dongle_stm32f4/crc.h
+++
@@ -1,8 +0,0 @@
-#ifndef CRC_H
-#define CRC_H
-
-#include <stdint.h>
-
-uint16_t crc_update(uint16_t crc, uint8_t byte);
-
-#endif
--- src/firmware/boards/legacy_robot_stm32f4/unit_tests/shoot_test.c
+++
@@ -1,7 +0,0 @@
-#include "main/primitives/shoot.h"
-
-#include "check.h"
-#include "test.h"
-
-
-void run_shoot_test(void) {}
