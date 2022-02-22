13c4a7c56757217a9550bae6dd315f7fa8df74fe
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2022-02-21 23:10:05 -0800

    update tactic update

--- src/software/ai/hl/stp/tactic/tactic_fsm.h
+++ src/software/ai/hl/stp/tactic/tactic_fsm.h
@@ -9,26 +9,36 @@
 #include "software/world/world.h"
 
 // This callback is used to return an intent from the fsm
-using SetIntentCallback = std::function<void(std::unique_ptr<Intent>)>;
+using SetIntentCallback    = std::function<void(std::unique_ptr<Intent>)>;
+using SetPrimitiveCallback = std::function<void(std::unique_ptr<TbotsProto::Primitive>)>;
 
 // The tactic update struct is used to update tactics and set the new intent
 struct TacticUpdate
 {
+    // TODO: remove this function
     TacticUpdate(const Robot &robot, const World &world,
-                 const SetIntentCallback &set_intent_fun,
-                 std::shared_ptr<const PathPlanner> path_planner = nullptr)
+                 const SetIntentCallback &set_intent_fun)
+        : robot(robot), world(world), set_intent(set_intent_fun)
+    {
+    }
+
+    TacticUpdate(const Robot &robot, const World &world,
+                 const SetPrimitiveCallback &set_primitive_fun,
+                 std::shared_ptr<const PathPlanner> path_planner)
         : robot(robot),
           world(world),
-          set_intent(set_intent_fun),
+          set_primitive(set_primitive_fun),
           path_planner(path_planner)
     {
     }
+
     // updated robot that tactic is assigned to
     Robot robot;
     // updated world
     World world;
     // callback to return the next intent
     SetIntentCallback set_intent;
+    SetPrimitiveCallback set_primitive;
 
     std::shared_ptr<const PathPlanner> path_planner;
 };
