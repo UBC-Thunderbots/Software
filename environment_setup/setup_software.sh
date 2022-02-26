c9da0c4cf0d48a79db10fb0e7526273a474f57b1
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2022-02-26 11:54:03 -0800

    update bazel set up software

--- environment_setup/setup_software.sh
+++ environment_setup/setup_software.sh
@@ -149,8 +149,9 @@ echo "Installing Bazel"
 echo "================================================================"
 
 # Adapted from https://docs.bazel.build/versions/master/install-ubuntu.html#install-on-ubuntu
-sudo apt-get install curl gnupg
-curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+sudo apt install apt-transport-https curl gnupg
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
+sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 sudo apt-get update
 if ! sudo apt-get install bazel-5.0.0 -y ; then
