3744343bddf0f372b4b05a83d59cb27cd37f5f74
divy-07 <divy07.com@gmail.com>
2022-02-26 14:40:28 -0700

    added radio buttons, re-designed colors functionality

--- src/software/thunderscope/chicker/chicker.py
+++ src/software/thunderscope/chicker/chicker.py
@@ -71,38 +71,88 @@ class ChickerWidget(QWidget):
         return groupBox
 
     def refresh(self):
-        # set text for charge/discharge button
-
-        # iterate over all items in grid
-        """
+        # set 'Charge'/'Discharge' text based on self.charged value
         for button in self.grid.parentWidget().findChildren(QPushButton):
-            if button.text() == "Charge" and button.isChecked():
-                button.toggle()
-                button.setCheckable(False)
-                print("Charge clicked")
-                for otherButton in self.grid.parentWidget().findChildren(QPushButton):
-                    if otherButton.text() != "Charge":
-                        otherButton.setCheckable(True)
-                        otherButton.setStyleSheet("background-color: White")
-                    else:
-                        otherButton.setStyleSheet("background-color: Grey")
+            if self.charged:
+                if button.text() == "Charge":
+                    button.setText("Discharge")
+            elif not self.charged:
+                if button.text() == "Discharge":
+                    button.setText("Charge")
 
-            elif button.text() != "Charge" and button.isChecked():
-                button.toggle()
-                print(button.text(), "button clicked")
-                for otherButton in self.grid.parentWidget().findChildren(QPushButton):
-                    if otherButton.text() == "Charge":
-                        otherButton.setCheckable(True)
-                        otherButton.setStyleSheet("background-color: White")
-                    else:
-                        otherButton.setCheckable(False)
-                        otherButton.setStyleSheet("background-color: Grey")
-        """
-        """
+        # iterate over all boxes - possibly the only way to access the widgets
+        # grid --> groupBox --> button/slider
         for item in self.grid.parentWidget().findChildren(QGroupBox):
+
+            # check all push buttons
             for button in item.findChildren(QPushButton):
-                print(button)
+                if not self.charged:
+                    # charge button white; text: "Charge"; setCheckable(True)
+                    if button.text() == "Charge":
+                        button.setStyleSheet("background-color: White")
+                        button.setCheckable(True)
+                    # kick/chip button grey; setCheckable(False)
+                    else:
+                        button.setStyleSheet("background-color: Grey")
+                        button.setCheckable(False)
+                    # behaviour upon clicking charge; change text to 'Discharge'; print; charged = True; toggle
+                    if button.text() == "Charge" and button.isChecked():
+                        button.toggle()
+                        self.charged = True
+                        print("Charge clicked")
+
+                elif self.charged:
+                    # discharge button white; text: "Discharge"; setCheckable(True)
+                    if button.text() == "Discharge":
+                        button.setStyleSheet("background-color: White")
+                        button.setCheckable(True)
+                    # kick/chip button white; setCheckable(True)
+                    else:
+                        button.setStyleSheet("background-color: White")
+                        button.setCheckable(True)
+
+                    if button.isChecked():
+                        # discharge clicked; change text to 'Charge'; print; set 'No Auto' on; charged = False
+                        if button.text() == "Discharge":
+                            button.toggle()
+                            self.charged = False
+                            print("Discharge clicked")
+                        # kick/chip button clicked; change text to 'Charge';
+                        else:
+                            button.toggle()
+                            self.charged = False
+                            if button.text() == "Kick":
+                                print("Kick clicked")
+                            elif button.text() == "Chip":
+                                print("Chip clicked")
+
+            # check all radio buttons, only if we are charged
+            if self.charged:
+                for radio in item.findChildren(QRadioButton):
+                    # behaviour upon click; change to discharge; print; charged = False
+                    pass
         """
-        # iterate over all boxes
+        # iterate over all boxes - possibly the only way to access the widgets
+        # grid --> groupBox --> button/slider
         for item in self.grid.parentWidget().findChildren(QGroupBox):
-            pass
+
+            # check all push buttons
+            for button in item.findChildren(QPushButton):
+                if not self.charged:
+                    # charge button white; text: "Charge"; setCheckable(True)
+                    # kick/chip button grey; setCheckable(False)
+                    # behaviour upon click; change text to 'Discharge'; print; charged = True
+                    pass
+
+                elif self.charged:
+                    # charge button grey; text: "Discharge"; setCheckable(False)
+                    # kick/chip button white; setCheckable(True)
+                    # behaviour upon click; change text to 'Charge'; print; set 'No Auto' on; charged = False
+                    pass
+
+            # check all radio buttons, only if we are charged
+            if self.charged:
+                for radio in item.findChildren(QRadioButton):
+                    # behaviour upon click; change to discharge; print; charged = False
+                    pass
+        """
