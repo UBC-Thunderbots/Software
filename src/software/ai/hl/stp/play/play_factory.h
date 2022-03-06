f25be766dd7a69340c0ff833539cd961dabb0b10
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2022-03-05 20:23:43 -0800

    fix comments

--- src/software/ai/hl/stp/play/play_factory.h
+++ src/software/ai/hl/stp/play/play_factory.h
@@ -3,5 +3,13 @@
 #include "proto/play.pb.h"
 #include "software/ai/hl/stp/play/play.h"
 
+/**
+ * Creates a play given a play proto
+ *
+ * @param play_proto the play proto
+ * @param ai_config The AI config
+ *
+ * @return a pointer to the play
+ */
 std::unique_ptr<Play> createPlay(const TbotsProto::Play& play_proto,
                                  std::shared_ptr<const AiConfig> ai_config);
