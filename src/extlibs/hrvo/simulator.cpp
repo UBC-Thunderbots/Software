e70a9e93add3652c73b8a7042cdbe3f05db897bf
nima <nimazareian80@gmail.com>
2022-03-18 16:32:33 -0700

    Revert HRVO visualization in thunderscope

--- src/extlibs/hrvo/simulator.cpp
+++ src/extlibs/hrvo/simulator.cpp
@@ -411,7 +411,7 @@ void HRVOSimulator::visualize(unsigned int robot_id) const
         *(obstacle_proto.add_circle()) =
             *createCircleProto(Circle(position, agent->getRadius()));
     }
-    LOG(VISUALIZE) << obstacle_proto;
+    // LOG(VISUALIZE) << obstacle_proto;
 }
 
 std::optional<std::shared_ptr<HRVOAgent>> HRVOSimulator::getFriendlyAgentFromRobotId(
