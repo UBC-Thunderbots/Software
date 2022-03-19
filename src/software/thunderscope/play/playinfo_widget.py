fb7aa82ba63bc25bab8d46ecca24c5700f66f804
alinas2000 <alinashabanova581@gmail.com>
2022-03-18 20:47:13 -0700

    ignore

--- src/software/thunderscope/play/playinfo_widget.py
+++ src/software/thunderscope/play/playinfo_widget.py
@@ -3,72 +3,63 @@ import pyqtgraph.console as pg_console
 from proto.play_info_msg_pb2 import PlayInfo
 from software.networking.threaded_unix_listener import ThreadedUnixListener
 import software.thunderscope.constants as constants
-from google.protobuf.json_format import MessageToDict
-from PyQt5.QtWidgets import QApplication, QTableWidget, QTableWidgetItem
-from PyQt5 import QtWidgets
 import queue
 
 from proto.robot_log_msg_pb2 import RobotLog
 
 
-class playInfoWidget(QTableWidget):
-
-    NUM_ROWS = 11
-    NUM_COLS = 3
-
+class playInfoWidget(pg.DataTreeWidget):
     def __init__(self, buffer_size=10):
-
-        QTableWidget.__init__(self, playInfoWidget.NUM_ROWS, playInfoWidget.NUM_COLS)
+        pg.DataTreeWidget.__init__(self)
 
         self.log_buffer = queue.Queue(buffer_size)
-        self.verticalHeader().setVisible(False)
-
-    def set_data(self, data):
-        """Data to set in the table
-
-        :param data: dict containing {"column_name": [column_items]}
-
-        """
-        horizontal_headers = []
-
-        for n, key in enumerate(sorted(data.keys())):
-            horizontal_headers.append(key)
-
-            for m, item in enumerate(data[key]):
-                newitem = QTableWidgetItem(item)
-                self.setItem(m, n, newitem)
-
-        self.setHorizontalHeaderLabels(horizontal_headers)
 
     def refresh(self):
-        """Update the play info widget with new play information
+        """Update the play info widget with new robot information
         """
         try:
             playinfo = self.log_buffer.get_nowait()
         except queue.Empty as empty:
             return
 
-        play_info_dict = MessageToDict(playinfo)
+        d = {
+            "play name": playinfo.play.play_name,
+            "robot 0": {
+                "tactic name": playinfo.robot_tactic_assignment[0].tactic_name,
+                "tactic fsm state": playinfo.robot_tactic_assignment[
+                    0
+                ].tactic_fsm_state,
+            },
+            "robot 1": {
+                "tactic name": playinfo.robot_tactic_assignment[1].tactic_name,
+                "tactic fsm state": playinfo.robot_tactic_assignment[
+                    1
+                ].tactic_fsm_state,
+            },
+            "robot 2": {
+                "tactic name": playinfo.robot_tactic_assignment[2].tactic_name,
+                "tactic fsm state": playinfo.robot_tactic_assignment[
+                    2
+                ].tactic_fsm_state,
+            },
+            "robot 3": {
+                "tactic name": playinfo.robot_tactic_assignment[3].tactic_name,
+                "tactic fsm state": playinfo.robot_tactic_assignment[
+                    3
+                ].tactic_fsm_state,
+            },
+            "robot 4": {
+                "tactic name": playinfo.robot_tactic_assignment[4].tactic_name,
+                "tactic fsm state": playinfo.robot_tactic_assignment[
+                    4
+                ].tactic_fsm_state,
+            },
+            "robot 5": {
+                "tactic name": playinfo.robot_tactic_assignment[5].tactic_name,
+                "tactic fsm state": playinfo.robot_tactic_assignment[
+                    5
+                ].tactic_fsm_state,
+            },
+        }
 
-        robot_ids = []
-        tactic_fsm_states = []
-        tactic_names = []
-
-        for robot_id in sorted(play_info_dict["robotTacticAssignment"]):
-            robot_ids.append(robot_id)
-            tactic_fsm_states.append(
-                play_info_dict["robotTacticAssignment"][robot_id]["tacticFsmState"]
-            )
-            tactic_names.append(
-                play_info_dict["robotTacticAssignment"][robot_id]["tacticName"]
-            )
-
-        self.set_data(
-            {
-                "Robot ID": robot_ids,
-                "Tactic Name": tactic_names,
-                "Tactic FSM State": tactic_fsm_states,
-            }
-        )
-        self.resizeColumnsToContents()
-        self.resizeRowsToContents()
+        self.setData(d)
