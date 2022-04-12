8d3acbb4ac153e3756fd26f63e9471ef8d4b6ff3
nima <nimazareian80@gmail.com>
2022-04-12 14:36:55 -0700

    testing with 2021 constants

--- src/software/world/robot.h
+++ src/software/world/robot.h
@@ -34,7 +34,7 @@ class Robot
                    const std::set<RobotCapability> &unavailable_capabilities =
                        std::set<RobotCapability>(),
                    // TODO: Maybe not have default??
-                   const RobotConstants_t &robot_constants = create2015RobotConstants());
+                   const RobotConstants_t &robot_constants = create2021RobotConstants());
 
     /**
      * Creates a new robot with the given initial state
