0dc7a62a2c7c86f3e9707cc363eb2c68ec735d4c
Nima Zareian <nimazareian80@gmail.com>
2022-05-03 15:44:05 -0700

    Remvoed numpy

--- src/software/thunderscope/arbitrary_plot/named_value_plotter.py
+++ src/software/thunderscope/arbitrary_plot/named_value_plotter.py
@@ -1,23 +1,20 @@
 import random
 import time
 from collections import deque
-import numpy
 
 import pyqtgraph as pg
 from proto.visualization_pb2 import NamedValue
 from pyqtgraph.Qt import QtGui
 
-from software.networking.threaded_unix_listener import ThreadedUnixListener
 from software.thunderscope.thread_safe_buffer import ThreadSafeBuffer
 
-DEQUE_SIZE = 100000
-MIN_Y_RANGE = 0
-MAX_Y_RANGE = 100
+DEQUE_SIZE = 1000
+INITIAL_Y_MIN = 0
+INITIAL_Y_MAX = 100
 TIME_WINDOW_TO_DISPLAY_S = 20
 
 
 class NamedValuePlotter(object):
-
     """ Plot named values in real time with a scrolling plot """
 
     def __init__(self, buffer_size=1000):
@@ -27,6 +24,8 @@ class NamedValuePlotter(object):
 
         """
         self.win = pg.plot()
+        self.win.disableAutoRange()
+        self.win.setYRange(INITIAL_Y_MIN, INITIAL_Y_MAX)
         self.plots = {}
         self.data_x = {}
         self.data_y = {}
@@ -34,9 +33,6 @@ class NamedValuePlotter(object):
         self.legend.setParentItem(self.win.graphicsItem())
         self.time = time.time()
         self.named_value_buffer = ThreadSafeBuffer(buffer_size, NamedValue)
-        self.total_time = 0
-        self.times_visualized = 0
-        self.win.disableAutoRange()
 
     def refresh(self):
         """Refreshes NamedValuePlotter and updates data in the respective
@@ -45,7 +41,6 @@ class NamedValuePlotter(object):
         """
         self.times_visualized += 1
         start_time = time.time()
-
         # Dump the entire buffer into a deque. This operation is fast because
         # its just consuming data from the buffer and appending it to a deque.
         for _ in range(self.named_value_buffer.queue.qsize()):
@@ -66,47 +61,23 @@ class NamedValuePlotter(object):
                 )
 
                 self.plots[named_value.name].setDownsampling(method="peak")
-                self.data_x[named_value.name] = numpy.zeros(DEQUE_SIZE)
-                self.data_y[named_value.name] = numpy.zeros(DEQUE_SIZE)
+                self.data_x[named_value.name] = deque([], DEQUE_SIZE)
+                self.data_y[named_value.name] = deque([], DEQUE_SIZE)
                 self.legend.addItem(self.plots[named_value.name], named_value.name)
 
             # Add incoming data to existing deques of data
-            self.data_x[named_value.name][0:-1] = self.data_x[named_value.name][1:]
-            self.data_x[named_value.name][-1] = (time.time() - self.time)
+            self.data_x[named_value.name].append(time.time() - self.time)
+            self.data_y[named_value.name].append(named_value.value)
 
-            self.data_y[named_value.name][0:-1] = self.data_y[named_value.name][1:]
-            self.data_y[named_value.name][-1] = named_value.value
-
-        t1 = time.time()
         for named_value, plot in self.plots.items():
             # Update the data
             plot.setData(
                 self.data_x[named_value], self.data_y[named_value]
             )
 
-        t2 = time.time()
-        print("Plotting time: {}".format(t2 - t1))
-
-        t1 = time.time()
         self.win.setRange(
             xRange=[
                 time.time() - self.time - TIME_WINDOW_TO_DISPLAY_S,
                 time.time() - self.time,
             ],
         )
-        # self.win.autoRange()
-        t2 = time.time()
-        print("Setting range time: {}".format(t2 - t1))
-
-        end_time = time.time()
-        self.total_time += end_time - start_time
-        print("Avg Refresh time: {}".format(self.total_time / self.times_visualized))
-        print("================")
-
-# 0.045 No disable autorange, and no autorange, and no setrange
-# 0.016    disable autorange, and no autorange, and no setrange
-# 0.020    disable autorange, and no autorange, and    setrange
-# 0.029    disable autorange, and    autorange, and    setrange
-# 0.039 No disable autorange, and no autorange, and    setrange
-
-# Can auto range, it will automatically get updated by new data, but will also show all data instead of a certain window
