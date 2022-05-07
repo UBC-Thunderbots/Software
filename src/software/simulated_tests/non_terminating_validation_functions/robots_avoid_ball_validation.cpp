59ef2bb68715e50268c0dffd87c316f43c85edf8
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2022-05-06 22:43:36 -0700

    fixes

--- src/software/simulated_tests/non_terminating_validation_functions/robots_avoid_ball_validation.cpp
+++ src/software/simulated_tests/non_terminating_validation_functions/robots_avoid_ball_validation.cpp
@@ -7,17 +7,20 @@ void robotsAvoidBall(double min_distance, std::vector<RobotId> excluded_robots,
                      std::shared_ptr<World> world_ptr,
                      ValidationCoroutine::push_type& yield)
 {
-    for (auto robot : world_ptr->friendlyTeam().getAllRobots())
+    for (size_t i = 0; i < world_ptr->friendlyTeam().getAllRobots().size(); i++)
     {
+        RobotId id = world_ptr->friendlyTeam().getAllRobots().at(i).id();
         double current_distance =
-            (robot.position() - world_ptr->ball().position()).length() -
+            (world_ptr->friendlyTeam().getAllRobots().at(i).position() -
+             world_ptr->ball().position())
+                .length() -
             ROBOT_MAX_RADIUS_METERS;
 
         if (current_distance < min_distance &&
-            std::find(excluded_robots.begin(), excluded_robots.end(), robot.id()) ==
+            std::find(excluded_robots.begin(), excluded_robots.end(), id) ==
                 excluded_robots.end())
         {
-            yield("Robot " + std::to_string(robot.id()) + " is less than " +
+            yield("Robot " + std::to_string(id) + " is less than " +
                   std::to_string(min_distance) + " m away from the ball!");
         }
     }
