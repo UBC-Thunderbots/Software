ee5c7b67974fca30332c2f4d82bd7b7d3e9710dc
akhil <akhil.veeraghanta@outlook.com>
2022-05-12 00:00:31 -0700

    fix some stuff

--- src/software/thunderscope/arbitrary_plot/named_value_plotter.py
+++ src/software/thunderscope/arbitrary_plot/named_value_plotter.py
@@ -1,7 +1,6 @@
 import random
 import time
 import numpy as np
-from collections import deque
 
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
@@ -40,27 +39,24 @@ class NamedValuePlotter(QWidget):
         self.time = time.time()
         self.named_value_buffer = ThreadSafeBuffer(buffer_size, NamedValue)
 
-        self.phase = 0
-        self.lines = 50
-        self.points = 1000
-        self.y = np.linspace(-10, 10, self.lines)
-        self.x = np.linspace(-10, 10, self.points)
-
-        self.traces = dict()
+        self.traces = {}
         self.win = gl.GLViewWidget()
-        self.win.setMinimumSize(640, 480)
+        self.win.setMinimumSize(100, 100)
         self.win.setCameraPosition(distance=100, elevation=90, azimuth=0)
 
-        self.w_gl_axis = gl.GLAxisItem(
-            size=None, antialias=True, glOptions='translucent')
-        self.win.addItem(self.w_gl_axis)
+        # Do NOT allow orbit
+        self.win.orbit = lambda x, y: False
 
         self.g = gl.GLGridItem()
-        self.g.scale(10, 10, 1)
+        self.g.setSize(x=20, y=20, z=10)
+        self.g.scale(50, 50, 10)
         self.win.addItem(self.g)
 
         self.layout.addWidget(self.win)
         self.setLayout(self.layout)
+
+        self.last_update_time = time.time()
+        self.last_incoming_value = {}
 
     def refresh(self):
         """Refreshes NamedValuePlotter and updates data in the respective
@@ -72,41 +68,39 @@ class NamedValuePlotter(QWidget):
             named_value = self.named_value_buffer.get(block=False)
 
             if named_value.name not in self.traces:
-                self.data_x[named_value.name] = np.zeros(DEQUE_SIZE)
+                self.last_incoming_value[named_value.name] = named_value.value
                 self.data_y[named_value.name] = np.zeros(DEQUE_SIZE)
 
                 self.traces[named_value.name] = gl.GLLinePlotItem(
-                    pos=np.vstack(
-                        [
-                            self.data_y[named_value.name],
-                            self.data_x[named_value.name],
-                            np.zeros(
-                                len(self.data_y[named_value.name])
-                            )
-                        ]
-                    ).transpose(),
-                    color=pg.glColor((random.randint(0,10), self.lines * 1.3)),
+                    color=pg.glColor(
+                        random.randint(0, 255),
+                        random.randint(0, 255),
+                        random.randint(0, 255),
+                    ),
                     width=1,
-                    antialias=True
+                    antialias=True,
                 )
                 self.win.addItem(self.traces[named_value.name])
 
-
             # Add incoming data to existing deques of data
-            self.data_x[named_value.name][0:-1] = self.data_x[named_value.name][1:]
-            self.data_y[named_value.name][0:-1] = self.data_y[named_value.name][1:]
+            self.last_incoming_value[named_value.name] = named_value.value
 
-            self.data_x[named_value.name][-1] = (time.time() - self.time) * 10
-            self.data_y[named_value.name][-1] = named_value.value
+        if self.last_update_time + 0.01 > time.time():
+            return
+
+        self.last_update_time = time.time()
 
         for name, trace in self.traces.items():
+
+            self.data_y[name][0:-1] = self.data_y[name][1:]
+            self.data_y[name][-1] = self.last_incoming_value[name]
+
             trace.setData(
-                    pos=np.vstack(
-                        [
-                            self.data_y[name],
-                            self.data_x[name],
-                            np.zeros(
-                                len(self.data_y[name])
-                            )
-                        ]
-                    ).transpose())
+                pos=np.vstack(
+                    [
+                        self.data_y[name],
+                        np.array(range(int(-DEQUE_SIZE / 2), int(DEQUE_SIZE / 2))),
+                        np.zeros(len(self.data_y[name])),
+                    ]
+                ).transpose()
+            )
