eafbb6856006e1a38284182655a970c2d5dbe885
akhil <akhil.veeraghanta@outlook.com>
2022-05-24 10:26:54 -0700

    missed a spot

--- environment_setup/setup_software.sh
+++ environment_setup/setup_software.sh
@@ -160,9 +160,10 @@ echo "================================================================"
 
 # Adapted from https://docs.bazel.build/versions/master/install-ubuntu.html#install-on-ubuntu
 sudo apt install apt-transport-https curl gnupg -y
-curl -fsSL https://storage.googleapis.com/www.bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
-sudo mv bazel-archive-keyring.gpg /usr/share/keyrings/
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
 sudo apt-get update
 if ! sudo apt-get install bazel-5.0.0 -y ; then
     echo "##############################################################"
