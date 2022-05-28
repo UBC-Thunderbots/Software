5ef590f042622734b5a32e48c1dd49ac443c3c45
akhil <akhil.veeraghanta@outlook.com>
2022-05-28 09:38:16 -0700

    wip

--- src/software/thunderscope/robot_diagnostics/robot_view.py
+++ src/software/thunderscope/robot_diagnostics/robot_view.py
@@ -24,33 +24,41 @@ class RobotView(QWidget):
         self.pink = QtGui.QColor(255, 0, 255)
         self.green = QtGui.QColor(0, 255, 0)
 
+        self.vision_pattern = QLabel()
+
+        self.vision_pattern.setSizePolicy(QSizePolicy(
+            QSizePolicy.Policy.Ignored,
+            QSizePolicy.Policy.MinimumExpanding)
+        )
+        self.vision_pattern.setGeometry(QtCore.QRect(0, 0, 10, 10))
 
         # There is no pattern to this so we just have to create
-        # a label to display the robot view.
+        # mapping from robot id to the four corners of the vision pattern
+        #
+        # robot-id: top-right, top-left, bottom-left, bottom-right
         #
         # https://robocup-ssl.github.io/ssl-rules/sslrules.html
         self.vision_pattern_lookup = {
-            0 : (self.pink, self.pink, self.green, self.pink),
-            1 : (self.pink, self.green, self.green, self.pink),
-            2 : (self.green, self.green, self.green, self.pink),
-            3 : (self.green, self.pink, self.green, self.pink),
-            4 : (self.pink, self.pink, self.pink, self.green),
-            5 : (self.pink, self.green, self.pink, self.green),
-            6 : (self.green, self.green, self.pink, self.green),
-            7 : (self.green, self.pink, self.pink, self.green),
-            8 : (self.green, self.green, self.green, self.green),
-            9 : (self.pink, self.pink, self.pink, self.pink),
-            10 : (self.pink, self.pink, self.green, self.green),
-            11 : (self.green, self.green, self.pink, self.pink),
-            12 : (self.pink, self.green, self.green, self.green),
-            13 : (self.pink, self.green, self.pink, self.pink),
-            14 : (self.green, self.pink, self.green, self.green),
-            15 : (self.green, self.pink, self.pink, self.pink),
+            0: (self.pink, self.pink, self.green, self.pink),
+            1: (self.pink, self.green, self.green, self.pink),
+            2: (self.green, self.green, self.green, self.pink),
+            3: (self.green, self.pink, self.green, self.pink),
+            4: (self.pink, self.pink, self.pink, self.green),
+            5: (self.pink, self.green, self.pink, self.green),
+            6: (self.green, self.green, self.pink, self.green),
+            7: (self.green, self.pink, self.pink, self.green),
+            8: (self.green, self.green, self.green, self.green),
+            9: (self.pink, self.pink, self.pink, self.pink),
+            10: (self.pink, self.pink, self.green, self.green),
+            11: (self.green, self.green, self.pink, self.pink),
+            12: (self.pink, self.green, self.green, self.green),
+            13: (self.pink, self.green, self.pink, self.pink),
+            14: (self.green, self.pink, self.green, self.green),
+            15: (self.green, self.pink, self.pink, self.pink),
         }
 
-        self.label = QLabel()
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.vision_pattern)
         self.setLayout(self.layout)
 
     def draw_robot_view(self, robot_status):
@@ -60,14 +68,44 @@ class RobotView(QWidget):
 
         """
 
-        pixmap = QtGui.QPixmap(self.label.size())
+        pixmap = QtGui.QPixmap(self.size())
         pixmap.fill(QtCore.Qt.GlobalColor.transparent)
 
         painter = QtGui.QPainter(pixmap)
-        self.draw_robot(painter, 1, "blue", 100, 100, 50)
+        self.draw_robot(painter, 1, "blue", 50, 50, 50)
+        self.draw_voltage(painter, x=150, y=50, width=100, voltage=19.2, min_voltage=19.2, max_voltage=25.2)
         painter.end()
 
-        self.label.setPixmap(pixmap)
+        self.vision_pattern.setPixmap(pixmap)
+
+    def draw_voltage(self, painter, x, y, width, voltage, min_voltage, max_voltage):
+        """Draw the given voltage on the given painter.
+
+        :param painter: The painter to draw on
+        :param x: The x coordinate of the voltage bar
+        :param y: The y coordinate of the voltage bar
+        :param width: The height of the voltage bar
+        :param voltage: The voltage to draw
+        :param min_voltage: The minimum voltage
+        :param max_voltage: The maximum voltage
+
+        """
+        painter.setPen(pg.mkPen("black", size=2))
+        painter.setBrush(pg.mkBrush(None))
+
+        painter.drawRect(
+            QtCore.QRectF(
+                x, y, width, 20
+            ),
+        )
+
+        painter.setBrush(pg.mkBrush(self.green))
+
+        painter.drawRect(
+            QtCore.QRectF(
+                x, y, (voltage-min_voltage), 20
+            ),
+        )
 
     def draw_robot(self, painter, id, team_colour, x, y, radius):
         """Draw a robot with the given painter. The vision pattern
@@ -80,6 +118,14 @@ class RobotView(QWidget):
         :param radius: The radius of the robot
 
         """
+        painter.setPen(pg.mkPen("white"))
+        painter.setBrush(pg.mkBrush("white"))
+
+        painter.drawText(
+            QtCore.QPointF(0, 10),
+            "Robot str(id)",
+        )
+
         painter.setPen(pg.mkPen("black"))
         painter.setBrush(pg.mkBrush("black"))
 
@@ -94,18 +140,27 @@ class RobotView(QWidget):
         )
 
         painter.setBrush(pg.mkBrush(team_colour))
-        painter.drawEllipse(QtCore.QPointF(x, y), radius/4, radius/4)
+        painter.drawEllipse(QtCore.QPointF(x, y), radius / 4, radius / 4)
+
 
         top_right, top_left, bottom_left, bottom_right = self.vision_pattern_lookup[id]
 
         painter.setBrush(pg.mkBrush(top_right))
-        painter.drawEllipse(QtCore.QPointF(x + radius/2 + 5, y - radius/2), radius/5, radius/5)
+        painter.drawEllipse(
+            QtCore.QPointF(x + radius / 2 + 5, y - radius / 2), radius / 5, radius / 5
+        )
 
         painter.setBrush(pg.mkBrush(top_left))
-        painter.drawEllipse(QtCore.QPointF(x - radius/2 - 5, y - radius/2), radius/5, radius/5)
+        painter.drawEllipse(
+            QtCore.QPointF(x - radius / 2 - 5, y - radius / 2), radius / 5, radius / 5
+        )
 
         painter.setBrush(pg.mkBrush(bottom_left))
-        painter.drawEllipse(QtCore.QPointF(x - radius/2, y + radius/2 + 5), radius/5, radius/5)
+        painter.drawEllipse(
+            QtCore.QPointF(x - radius / 2, y + radius / 2 + 5), radius / 5, radius / 5
+        )
 
         painter.setBrush(pg.mkBrush(bottom_right))
-        painter.drawEllipse(QtCore.QPointF(x + radius/2, y + radius/2 + 5), radius/5, radius/5)
+        painter.drawEllipse(
+            QtCore.QPointF(x + radius / 2, y + radius / 2 + 5), radius / 5, radius / 5
+        )
