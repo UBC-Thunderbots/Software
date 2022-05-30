b8350a0700f9f43b39bbe41c5f5e05cb041ee0bc
akhil <akhil.veeraghanta@outlook.com>
2022-05-29 20:34:27 -0700

    formatting

--- src/software/ai/hl/stp/play/ball_placement_play_test.py
+++ src/software/ai/hl/stp/play/ball_placement_play_test.py
@@ -3,7 +3,10 @@ import pytest
 import software.python_bindings as tbots
 from proto.play_pb2 import Play, PlayName
 from software.simulated_tests.ball_enters_region import *
-from software.simulated_tests.simulated_test_fixture import simulated_test_runner
+from software.simulated_tests.simulated_test_fixture import (
+    simulated_test_runner,
+    pytest_main,
+)
 from proto.message_translation.tbots_protobuf import create_world_state
 from proto.ssl_gc_common_pb2 import Team
 from proto.ssl_gc_geometry_pb2 import Vector2
