054eef7e9c39f3f97a9c9c287e7b0a26f70b61f0
akhil <akhil.veeraghanta@outlook.com>
2022-06-05 23:12:00 -0700

    remove offending package

--- environment_setup/setup_software.sh
+++ environment_setup/setup_software.sh
@@ -134,14 +134,6 @@ if [[ $(lsb_release -rs) == "20.04" ]]; then
     sudo /opt/tbotspython/bin/pip3 install -r ubuntu20_requirements.txt
 fi
 
-
-if ! sudo /opt/tbotspython/bin/pip3 install --upgrade protobuf  ; then
-    echo "##############################################################"
-    echo "Error: Installing protobuf failed"
-    echo "##############################################################"
-    exit 1
-fi
-
 echo "================================================================"
 echo "Done Setting Up Virtual Python Environment"
 echo "================================================================"
