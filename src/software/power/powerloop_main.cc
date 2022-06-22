d6b8c64b1b0e12532e6e6a53adc93f3b9e03eff7
akhil <akhil.veeraghanta@outlook.com>
2022-06-22 11:21:28 -0700

    fix bad merge

--- src/software/power/powerloop_main.cc
+++ src/software/power/powerloop_main.cc
@@ -1,14 +1,4 @@
 #ifdef PLATFORMIO_BUILD
-<<<<<<< HEAD:src/software/power_board/powerloop_main.cc
-#include <constants_platformio.h>  // PlatformIO sees and includes the library based on the bazel rule name ONLY
-#include <pb_decode.h>
-#include <pb_encode.h>
-#include <power_frame_msg_platformio.h>
-#include <proto/power_frame_msg.nanopb.h>
-#include <proto/primitive.nanopb.h>
-#include <proto/robot_status_msg.nanopb.h>
-#include <uart_framing_platformio.h>
-=======
 #include "charger.h"
 #include "chicker.h"
 #include "constants_platformio.h"  // PlatformIO sees and includes the library based on the bazel rule name ONLY
@@ -20,7 +10,6 @@
 #include "proto/primitive.nanopb.h"
 #include "proto/robot_status_msg.nanopb.h"
 #include "uart_framing_platformio.h"
->>>>>>> 87bcb4d311addd2d0995cd8d796f30464ab9a6a3:src/software/power/powerloop_main.cc
 #else
 #include <constants_platformio.h>
 
@@ -29,15 +18,12 @@
 #include "proto/tbots_nanopb_proto_nanopb_gen/proto/robot_status_msg.nanopb.h"
 #include "shared/constants.h"
 #include "shared/uart_framing/uart_framing.hpp"
-<<<<<<< HEAD:src/software/power_board/powerloop_main.cc
-=======
 #include "software/power/charger.h"
 #include "software/power/chicker.h"
 #include "software/power/control_executor.h"
 #include "software/power/geneva.h"
 #include "software/power/power_monitor.h"
 
->>>>>>> 87bcb4d311addd2d0995cd8d796f30464ab9a6a3:src/software/power/powerloop_main.cc
 #endif
 
 #include <Arduino.h>
@@ -58,61 +44,33 @@ void setup()
     Serial.begin(115200, SERIAL_8N1, RXD2, TXD2);
     read_buffer_size =
         getMarshalledSize(TbotsProto_PowerControl TbotsProto_PowerControl_init_default);
-<<<<<<< HEAD:src/software/power_board/powerloop_main.cc
-=======
     charger  = std::make_shared<Charger>();
     chicker  = std::make_shared<Chicker>();
     monitor  = std::make_shared<PowerMonitor>();
     geneva   = std::make_shared<Geneva>();
     executor = std::make_shared<ControlExecutor>(charger, chicker, geneva);
->>>>>>> 87bcb4d311addd2d0995cd8d796f30464ab9a6a3:src/software/power/powerloop_main.cc
 }
 
 void loop()
 {
-<<<<<<< HEAD:src/software/power_board/powerloop_main.cc
-    if (Serial.available() > 0)
-=======
     // Read in bytes from Serial and put them into a buffer to read
     while (Serial.available() > 0 && buffer.size() < read_buffer_size)
->>>>>>> 87bcb4d311addd2d0995cd8d796f30464ab9a6a3:src/software/power/powerloop_main.cc
     {
         incomingByte = Serial.read();
         buffer.emplace_back(static_cast<uint8_t>(incomingByte));
     }
-<<<<<<< HEAD:src/software/power_board/powerloop_main.cc
-=======
     // Once there is enough data attempt to decode
->>>>>>> 87bcb4d311addd2d0995cd8d796f30464ab9a6a3:src/software/power/powerloop_main.cc
     if (buffer.size() == read_buffer_size)
     {
         TbotsProto_PowerFrame frame = TbotsProto_PowerFrame_init_default;
         if (unmarshalUartPacket(buffer, frame))
         {
-<<<<<<< HEAD:src/software/power_board/powerloop_main.cc
-            // TODO(#2597): Perform command here
-
-            auto status = createNanoPbPowerStatus(1.2f, 2.3f, 3.4f, 4.5f, true, false);
-            auto status_frame = createUartFrame(status);
-            packet            = marshallUartPacket(status_frame);
-=======
             // On successful decoding execute the given command
             TbotsProto_PowerControl control = frame.power_msg.power_control;
             executor->execute(control);
->>>>>>> 87bcb4d311addd2d0995cd8d796f30464ab9a6a3:src/software/power/powerloop_main.cc
         }
         buffer.clear();
     }
-<<<<<<< HEAD:src/software/power_board/powerloop_main.cc
-    if (!packet.empty())
-    {
-        for (auto byte : packet)
-        {
-            if (Serial.availableForWrite() > 0)
-            {
-                Serial.write(byte);
-            }
-=======
     // Read sensor values. These are all instantaneous
     auto status = createNanoPbPowerStatus(
         monitor->getBatteryVoltage(), charger->getCapacitorVoltage(),
@@ -125,7 +83,6 @@ void loop()
         if (Serial.availableForWrite() > 0)
         {
             Serial.write(byte);
->>>>>>> 87bcb4d311addd2d0995cd8d796f30464ab9a6a3:src/software/power/powerloop_main.cc
         }
     }
     delay(25);
