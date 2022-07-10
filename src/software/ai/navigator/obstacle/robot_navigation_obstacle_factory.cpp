8f9b1fffd37449567c0c348ae030a2b8a9619ee4
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2022-07-11 06:20:51 +0700

    fix obstacle

--- src/software/ai/navigator/obstacle/robot_navigation_obstacle_factory.cpp
+++ src/software/ai/navigator/obstacle/robot_navigation_obstacle_factory.cpp
@@ -45,13 +45,13 @@ RobotNavigationObstacleFactory::createStaticObstaclesFromMotionConstraint(
             break;
         case TbotsProto::MotionConstraint::ENEMY_HALF_WITHOUT_CENTRE_CIRCLE:
         {
-            double larger_radius = field.centerCircleRadius() + 0.2;
-            Polygon centre_circle_and_enemy_half =
-                Polygon({Point(0, field.yLength() / 2), Point(0, larger_radius),
-                         Point(larger_radius, larger_radius),
-                         Point(larger_radius, -larger_radius), Point(0, -larger_radius),
-                         Point(0, -field.yLength() / 2), field.enemyCornerNeg(),
-                         field.enemyCornerPos()});
+            double larger_radius                 = field.centerCircleRadius() + 0.2;
+            Polygon centre_circle_and_enemy_half = Polygon(
+                {Point(0, field.yLength() / 2), Point(0, larger_radius),
+                 Point(larger_radius, larger_radius), Point(larger_radius + 0.2, 0),
+                 Point(larger_radius, -larger_radius), Point(0, -larger_radius),
+                 Point(0, -field.yLength() / 2), field.fieldBoundary().posXNegYCorner(),
+                 field.fieldBoundary().posXPosYCorner()});
             obstacles.push_back(createFromShape(centre_circle_and_enemy_half));
             break;
         }
