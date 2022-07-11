0bf4677d2541b5e965464c3ff9813567f54ba638
akhil <akhil.veeraghanta@outlook.com>
2022-07-10 18:14:07 -0700

    wip

--- src/software/jetson_nano/primitive_executor.cpp
+++ src/software/jetson_nano/primitive_executor.cpp
@@ -108,55 +108,6 @@ double PrimitiveExecutor::getTargetLinearSpeed(
     return target_linear_speed;
 }
 
-Vector PrimitiveExecutor::getTargetLinearVelocity(
-    const TbotsProto::MovePrimitive& move_primitive, const RobotState& robot_state)
-{
-    const float LOCAL_EPSILON = 1e-6f;  // Avoid dividing by zero
-
-    // Unpack current move primitive
-    const float dest_linear_speed = move_primitive.final_speed_m_per_s();
-    const float max_speed_m_per_s = move_primitive.max_speed_m_per_s();
-    const Point final_position =
-        createPoint(move_primitive.motion_control().path().points().at(1));
-    std::clamp(max_speed_m_per_s, 0.0f, robot_constants_.robot_max_speed_m_per_s);
-
-    const float max_target_linear_speed = fmaxf(max_speed_m_per_s, dest_linear_speed);
-
-    // Compute distance to destination
-    const float norm_dist_delta =
-        static_cast<float>((robot_state.position() - final_position).length());
-
-    // Compute at what linear distance we should start decelerating
-    // d = (Vf^2 - Vi^2) / (2a + LOCAL_EPSILON)
-    const float start_linear_deceleration_distance =
-        (max_target_linear_speed * max_target_linear_speed -
-         dest_linear_speed * dest_linear_speed) /
-        (2 * robot_constants_.robot_max_acceleration_m_per_s_2 + LOCAL_EPSILON);
-
-    // When we are close enough to start decelerating, we reduce the max speed
-    // by 60%. Once we get closer than 0.6 meters, we start to linearly decrease
-    // speed proportional to the distance to the destination. 0.6 was determined
-    // experimentally.
-    float target_linear_speed = max_target_linear_speed;
-    if (norm_dist_delta < start_linear_deceleration_distance)
-    {
-        target_linear_speed = max_target_linear_speed * fminf(norm_dist_delta, 0.6f);
-    }
-
-    Vector target_global_velocity = final_position - robot_state.position();
-
-    double local_x_velocity =
-        robot_state.orientation().cos() * target_global_velocity.x() +
-        robot_state.orientation().sin() * target_global_velocity.y();
-
-    double local_y_velocity =
-        -robot_state.orientation().sin() * target_global_velocity.x() +
-        robot_state.orientation().cos() * target_global_velocity.y();
-
-    return Vector(local_x_velocity, local_y_velocity).normalize(target_linear_speed);
-}
-
-
 std::unique_ptr<TbotsProto::DirectControlPrimitive> PrimitiveExecutor::stepPrimitive(
     const unsigned int robot_id, const RobotState& robot_state)
 {
