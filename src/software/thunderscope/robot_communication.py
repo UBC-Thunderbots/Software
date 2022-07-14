8200d4723377ad393d5decd324e037490e1ea307
Jonathan Lew <31377876+jonathanlew@users.noreply.github.com>
2022-07-15 00:36:08 +0700

    little fixes

--- src/software/thunderscope/robot_communication.py
+++ src/software/thunderscope/robot_communication.py
@@ -220,7 +220,7 @@ class RobotCommunication(object):
         )
 
         self.connect_fullsystem_to_robots()
-        self.disconnect_fullsystem_from_robots()
+        #self.disconnect_fullsystem_from_robots()
         # self.connect_robot_to_diagnostics(0)
         # self.connect_robot_to_diagnostics(1)
         # self.connect_robot_to_diagnostics(1)
