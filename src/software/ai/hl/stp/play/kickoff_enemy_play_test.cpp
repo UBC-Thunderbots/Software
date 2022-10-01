366923ca631c796f36a8c950ef58c228a9abf309
williamckha <williamha@outlook.com>
2022-10-01 16:35:28 -0700

    Changed setAIPlay to setAiPlay

--- src/software/ai/hl/stp/play/kickoff_enemy_play_test.cpp
+++ src/software/ai/hl/stp/play/kickoff_enemy_play_test.cpp
@@ -32,7 +32,7 @@ TEST_F(KickoffEnemyPlayTest, test_kickoff_enemy_play)
          field.enemyDefenseArea().negXNegYCorner(),
          field.enemyDefenseArea().negXPosYCorner()});
     setEnemyGoalie(0);
-    setAIPlay(TbotsProto::PlayName::KickoffEnemyPlay);
+    setAiPlay(TbotsProto::PlayName::KickoffEnemyPlay);
     setRefereeCommand(RefereeCommand::NORMAL_START, RefereeCommand::PREPARE_KICKOFF_THEM);
 
     std::vector<ValidationFunction> terminating_validation_functions = {
