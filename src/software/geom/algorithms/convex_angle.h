833568ba59fc1072de3cb2cb8eaa5e7feefafb9f
AdenC123 <adenrudy23@gmail.com>
2022-10-15 15:38:27 -0700

    use radians

--- src/software/geom/algorithms/convex_angle.h
+++ src/software/geom/algorithms/convex_angle.h
@@ -4,25 +4,25 @@
 
 /**
  * Calculates the convex angle formed by the two given vectors
- * A convex angle is between 0 and 180 degrees inclusive-
+ * A convex angle is between 0 and π radians inclusive-
  * https://proofwiki.org/wiki/Definition:Convex_Angle
  *
  * @param v1
  * @param v2
  *
- * @return The convex angle formed by v1 and v2, in the range [0, 180]
+ * @return The convex angle formed by v1 and v2, in the range [0, π]
  */
 Angle convexAngle(const Vector& v1, const Vector& v2);
 
 /**
  * Calculates the convex angle formed by the vector p2->p1 and p2->p3
- * A convex angle is between 0 and 180 degrees inclusive-
+ * A convex angle is between 0 and π radians inclusive-
  * https://proofwiki.org/wiki/Definition:Convex_Angle
  *
  * @param p1
  * @param p2
  * @param p3
  *
- * @return The convex angle formed by the vector p2->p1 and p2->p3, in the range [0, 180]
+ * @return The convex angle formed by the vector p2->p1 and p2->p3, in the range [0, π]
  */
 Angle convexAngle(const Point& p1, const Point& p2, const Point& p3);
