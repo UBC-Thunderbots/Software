651b169293ee57674a5819e82386e11ffd973ac0
sauravbanna <sauravbanna@gmail.com>
2022-10-17 12:33:22 -0700

    Added kick chip button functionality in robot diagnostics in Thunderscope

--- src/software/thunderscope/robot_diagnostics/chicker.py
+++ src/software/thunderscope/robot_diagnostics/chicker.py
@@ -1,7 +1,8 @@
 import pyqtgraph as pg
-from pyqtgraph.Qt.QtCore import Qt
+from pyqtgraph.Qt.QtCore import *
 from pyqtgraph.Qt.QtWidgets import *
 from software.py_constants import *
+from threading import Timer
 from proto.import_all_protos import *
 import software.thunderscope.common.common_widgets as common_widgets
 
@@ -36,6 +37,11 @@ class ChickerWidget(QWidget):
         self.kick_button = self.push_buttons[0]
         self.chip_button = self.push_buttons[1]
 
+        # QObject.connect(self.kick_button, SIGNAL ('clicked()'), self.kick_clicked())
+
+        self.kick_button.clicked.connect(self.kick_clicked)
+        self.chip_button.clicked.connect(self.chip_clicked)
+
         vbox_layout.addWidget(self.push_button_box)
 
         # radio button group box
@@ -46,8 +52,15 @@ class ChickerWidget(QWidget):
         self.auto_kick_button = self.radio_buttons[1]
         self.auto_chip_button = self.radio_buttons[2]
 
+        self.no_auto_button.toggled.connect(self.buttons_enable)
+        self.auto_kick_button.toggled.connect(self.buttons_disable)
+        self.auto_chip_button.toggled.connect(self.buttons_disable)
+
         vbox_layout.addWidget(self.radio_button_box)
         self.no_auto_button.setChecked(True)
+
+        self.kick_button_enable = True
+        self.chip_button_enable = True
 
         # sliders
         (
@@ -73,6 +86,53 @@ class ChickerWidget(QWidget):
         self.geneva_value = 3
         self.power_value = 1
 
+    def kick_clicked(self):
+        if self.kick_button_enable:
+            self.send_kick_or_chip(True)
+            self.toggle_kick_button()
+            t = Timer(3, lambda s: s.toggle_kick_button(), [self])
+            t.start()
+
+    def chip_clicked(self):
+        if self.chip_button_enable:
+            self.send_kick_or_chip(False)
+            self.toggle_chip_button()
+            t = Timer(3, lambda s: s.toggle_chip_button(), [self])
+            t.start()
+
+    def toggle_kick_button(self):
+        self.kick_button_enable = not self.kick_button_enable
+
+    def toggle_chip_button(self):
+        self.chip_button_enable = not self.chip_button_enable
+
+    def buttons_disable(self):
+        self.kick_button_enable = False
+        self.chip_button_enable = False
+
+    def buttons_enable(self):
+        self.kick_button_enable = True
+        self.chip_button_enable = True
+
+    def send_kick_or_chip(self, kick):
+        geneva_value = self.geneva_slider.value()
+        self.geneva_label.setText(Slot.Name(geneva_value))
+
+        power_value = self.power_slider.value()
+        self.power_label.setText(str(power_value))
+
+        power_control = PowerControl()
+        power_control.geneva_slot = geneva_value
+
+        if kick:
+            power_control.chicker.auto_chip_or_kick.autokick_speed_m_per_s = power_value
+        else:
+            power_control.chicker.auto_chip_or_kick.autochip_distance_meters = (
+                power_value
+            )
+
+        self.proto_unix_io.send_proto(PowerControl, power_control)
+
     def change_button_state(self, button, enable):
         """Change button color and clickable state.
 
@@ -91,31 +151,13 @@ class ChickerWidget(QWidget):
     def refresh(self):
 
         # slider values
-        geneva_value = self.geneva_slider.value()
-        self.geneva_label.setText(Slot.Name(geneva_value))
-
-        power_value = self.power_slider.value()
-        self.power_label.setText(str(power_value))
-
-        # if autokick is enabled, we don't want to allow kick/chip
-        auto_kick_enabled = (
-            self.auto_kick_button.isChecked() or self.auto_chip_button.isChecked()
-        )
-        self.change_button_state(self.kick_button, not auto_kick_enabled)
-        self.change_button_state(self.chip_button, not auto_kick_enabled)
-
-        power_control = PowerControl()
-        power_control.geneva_slot = geneva_value
+        self.change_button_state(self.kick_button, self.kick_button_enable)
+        self.change_button_state(self.chip_button, self.chip_button_enable)
 
         # If auto is enabled, we want to populate the autochip or kick message
         if self.auto_kick_button.isChecked():
-            power_control.chicker.auto_chip_or_kick.autokick_speed_m_per_s = power_value
+            self.send_kick_or_chip(True)
         elif self.auto_chip_button.isChecked():
-            power_control.chicker.auto_chip_or_kick.autochip_distance_meters = (
-                power_value
-            )
+            self.send_kick_or_chip(False)
         elif self.no_auto_button.isChecked():
             pass
-            # TODO (#2715): ATTACH A CALLBACK AND SENDPROTO/KICK IMMEDIATELY
-
-        self.proto_unix_io.send_proto(PowerControl, power_control)
