0f0c01a48033f2572e66bc764c1eb78ed3b52056
JoshuaWu7 <joshua.wu2009@gmail.com>
2022-11-18 23:10:07 -0800

    add documentation

--- src/software/ai/navigator/path_planner/hrvo/brute_force_nearest_neighbor_search.hpp
+++ src/software/ai/navigator/path_planner/hrvo/brute_force_nearest_neighbor_search.hpp
@@ -2,18 +2,29 @@
 
 #include <vector>
 
+/**
+ * Finds the neighbors of a candidate based on a comparator and radius
+ *
+ * @tparam T generic type for the candidate and its neighbors
+ * @tparam Func type for the comparator
+ * @param candidate cannot be null
+ * @param neighbors a list of T objects neighbors, which may contain the candidate object itself
+ * @param radius the maximum distance from a candidate object
+ * @param comparator function that measures the square distance between a candidate and a neighbor
+ * @return a list representing a subset of neighbors
+ */
 template <class T, typename Func>
 std::vector<T> nearestNeighbours(const T& candidate, const std::vector<T>& neighbors,
                                  double radius, Func comparator)
 {
-    std::vector<T> robot_subset;
+    std::vector<T> subset;
     for (const T& neighbor : neighbors)
     {
         if (comparator(candidate, neighbor) < radius * radius &&
             candidate != neighbor)
         {
-            robot_subset.push_back(neighbor);
+            subset.push_back(neighbor);
         }
     }
-    return robot_subset;
+    return subset;
 }
