1cf7940dacaf7a39cd1c49b31d469e6ad51312ca
JoshuaWu7 <joshua.wu2009@gmail.com>
2022-11-19 16:00:46 -0800

    formatting

--- src/software/ai/navigator/path_planner/hrvo/brute_force_nearest_neighbor_search.hpp
+++ src/software/ai/navigator/path_planner/hrvo/brute_force_nearest_neighbor_search.hpp
@@ -8,9 +8,11 @@
  * @tparam T generic type for the candidate and its neighbors
  * @tparam Func type for the comparator
  * @param candidate cannot be null
- * @param neighbors a list of T objects neighbors, which may contain the candidate object itself
+ * @param neighbors a list of T objects neighbors, which may contain the candidate object
+ * itself
  * @param radius the maximum distance from a candidate object
- * @param comparator function that measures the square distance between a candidate and a neighbor
+ * @param comparator function that measures the square distance between a candidate and a
+ * neighbor
  * @return a list representing a subset of neighbors
  */
 template <class T, typename Func>
@@ -20,8 +22,7 @@ std::vector<T> nearestNeighbours(const T& candidate, const std::vector<T>& neigh
     std::vector<T> subset;
     for (const T& neighbor : neighbors)
     {
-        if (comparator(candidate, neighbor) < radius * radius &&
-            candidate != neighbor)
+        if (comparator(candidate, neighbor) < radius * radius && candidate != neighbor)
         {
             subset.push_back(neighbor);
         }
--- src/software/ai/navigator/path_planner/hrvo/nearest_neighbor_test.cpp
+++ src/software/ai/navigator/path_planner/hrvo/nearest_neighbor_test.cpp
@@ -1,15 +1,18 @@
 #include <gtest/gtest.h>
+#include <include/gmock/gmock-matchers.h>
+
+#include <chrono>
 
 #include "software/ai/navigator/path_planner/hrvo/brute_force_nearest_neighbor_search.hpp"
 #include "software/geom/algorithms/distance.h"
 #include "software/test_util/test_util.h"
 #include "software/world/robot.h"
-#include <chrono>
-#include <include/gmock/gmock-matchers.h>
 
-class NearestNeighborSearchTest : public ::testing::TestWithParam<double> {
-protected:
-    NearestNeighborSearchTest() {
+class NearestNeighborSearchTest : public ::testing::TestWithParam<double>
+{
+   protected:
+    NearestNeighborSearchTest()
+    {
         robot_locations.push_back(Point(0, 0));
         robot_locations.push_back(Point(1, 1));
         robot_locations.push_back(Point(-1, -1));
@@ -19,8 +22,10 @@ protected:
         robot_locations.push_back(Point(4, 3));
     }
 
-    void SetUp() override {
-        for (Point point : robot_locations) {
+    void SetUp() override
+    {
+        for (Point point : robot_locations)
+        {
             Robot agent = TestUtil::createRobotAtPos(point);
             agents.push_back(agent);
         }
@@ -30,39 +35,48 @@ protected:
     std::vector<Robot> agents;
 };
 
-double compare(const Robot &r1, const Robot &r2) {
+double compare(const Robot &r1, const Robot &r2)
+{
     return distanceSquared(r1.position(), r2.position());
 }
 
-TEST_F(NearestNeighborSearchTest, no_robot_within_radius_test) {
+TEST_F(NearestNeighborSearchTest, no_robot_within_radius_test)
+{
     double radius = 1.0;
     std::vector<Robot> expected{};
-    std::vector<Robot> agent_subset = nearestNeighbours(agents[0], agents, radius, compare);
+    std::vector<Robot> agent_subset =
+        nearestNeighbours(agents[0], agents, radius, compare);
     EXPECT_THAT(expected, agent_subset);
 }
 
-TEST_F(NearestNeighborSearchTest, two_robots_within_radius_test) {
+TEST_F(NearestNeighborSearchTest, two_robots_within_radius_test)
+{
     double radius = 2.0;
     std::vector<Robot> expected{agents[1], agents[2]};
-    std::vector<Robot> agent_subset = nearestNeighbours(agents[0], agents, radius, compare);
+    std::vector<Robot> agent_subset =
+        nearestNeighbours(agents[0], agents, radius, compare);
     std::sort(expected.begin(), expected.end(), Robot::cmpRobotByID());
     std::sort(agent_subset.begin(), agent_subset.end(), Robot::cmpRobotByID());
     EXPECT_THAT(expected, ::testing::ContainerEq(agent_subset));
 }
 
-TEST_F(NearestNeighborSearchTest, four_robots_within_radius_test) {
+TEST_F(NearestNeighborSearchTest, four_robots_within_radius_test)
+{
     double radius = 3.0;
     std::vector<Robot> expected{agents[1], agents[2], agents[3], agents[4]};
-    std::vector<Robot> agent_subset = nearestNeighbours(agents[0], agents, radius, compare);
+    std::vector<Robot> agent_subset =
+        nearestNeighbours(agents[0], agents, radius, compare);
     std::sort(expected.begin(), expected.end(), Robot::cmpRobotByID());
     std::sort(agent_subset.begin(), agent_subset.end(), Robot::cmpRobotByID());
     EXPECT_THAT(expected, ::testing::ContainerEq(agent_subset));
 }
 
-TEST_F(NearestNeighborSearchTest, robot_on_edge_of_radius_test) {
+TEST_F(NearestNeighborSearchTest, robot_on_edge_of_radius_test)
+{
     double radius = 5.0;
     std::vector<Robot> expected{agents[1], agents[2], agents[3], agents[4]};
-    std::vector<Robot> agent_subset = nearestNeighbours(agents[0], agents, radius, compare);
+    std::vector<Robot> agent_subset =
+        nearestNeighbours(agents[0], agents, radius, compare);
     std::sort(expected.begin(), expected.end(), Robot::cmpRobotByID());
     std::sort(agent_subset.begin(), agent_subset.end(), Robot::cmpRobotByID());
     EXPECT_THAT(expected, ::testing::ContainerEq(agent_subset));
