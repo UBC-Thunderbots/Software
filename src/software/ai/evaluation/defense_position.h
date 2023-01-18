d2720b017b5500f650c11589770682f113a9765b
williamckha <williamha@outlook.com>
2023-01-18 02:12:25 -0800

    forgot to stage

--- src/software/ai/evaluation/defense_position.h
+++ src/software/ai/evaluation/defense_position.h
@@ -5,22 +5,6 @@
 #include "software/geom/segment.h"
 #include "software/world/field.h"
 
-// This struct stores the concept of a defense lane, which describes
-// a potential passing or shooting lane an enemy robot could shoot the
-// ball along
-struct DefenseLane
-{
-    // The segment representing the lane on the field, starting from
-    // the position of the enemy robot and ending at the intended point
-    // where the ball will be received or enter the net
-    Segment lane;
-
-    // The "expected threat" of the lane, which scores the dangerousness
-    // of the lane relative to other lanes (i.e. how likely a pass or shot 
-    // along the lane will eventually result in the enemy team scoring)
-    unsigned int expected_threat;
-};
-
 // This struct stores the concept of a defense position, which describes
 // the location on the field that a defender could move towards to block 
 // a potential enemy pass or shot on net
@@ -42,9 +26,21 @@ struct DefensePosition
     bool is_crease_defense;
 };
 
-struct CreaseDefenderInfo
+// This struct stores the concept of a defense lane, which describes
+// a potential passing or shooting lane an enemy robot could shoot the
+// ball along
+struct DefenseLane
+{
+    // The segment representing the lane on the field, starting from
+    // the position of the enemy robot and ending at the intended point
+    // where the ball will be received or enter the net
+    Segment lane;
 
-static constexpr unsigned int SHOOTING_LANE_MULTIPLIER = 4;
+    // The "expected threat" of the lane, which scores the dangerousness
+    // of the lane relative to other lanes (i.e. how likely a pass or shot 
+    // along the lane will eventually result in the enemy team scoring)
+    unsigned int expected_threat;
+};
 
 /**
  * Determines all possible defense positions where a defender could be placed
@@ -52,7 +48,8 @@ static constexpr unsigned int SHOOTING_LANE_MULTIPLIER = 4;
  * score which quantifies a position's ability to block high-danger enemy  
  * scoring chances or passes.
  * 
- * @param threats all enemy threats to consider in determining defense positions
+ * @param threats all enemy threats to consider in determining defense positions,
+ * in order of decreasing threat
  * @param field the field being played on
  * 
  * @return a list of all possible defense positions in order of decreasing
