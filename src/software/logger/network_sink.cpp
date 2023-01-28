bbc22ca2d844bea4f6742ea0629bbfa3c9ea78e0
AdenC123 <adenrudy23@gmail.com>
2023-01-28 14:43:04 -0800

    notes

--- src/software/logger/network_sink.cpp
+++ src/software/logger/network_sink.cpp
@@ -13,6 +13,13 @@ NetworkSink::NetworkSink(unsigned int channel, const std::string& interface, int
 
 void NetworkSink::sendToNetwork(g3::LogMessageMover log_entry)
 {
+    // Spam reduction notes TODO delete
+    // Different from last message: log it, save timestamp and message
+    // Same as last message: check timestamp, if within interval of saved timestamp, repeats++
+    //         if outside interval, log it with repeats added, reset saved repeats and timestamp
+    
+    // problem?: drops number of repeats on new message
+
     auto log_msg_proto = std::make_unique<TbotsProto::RobotLog>();
     TbotsProto::LogLevel log_level_proto;
 
@@ -34,7 +41,7 @@ void NetworkSink::sendToNetwork(g3::LogMessageMover log_entry)
             static_cast<uint32_t>(std::stoul(log_entry.get().line())));
 
         log_output->sendProto(*log_msg_proto);
-    }
 
-    last_msg = log_entry.get().message();
+        last_msg = log_entry.get().message();
+    }
 }
