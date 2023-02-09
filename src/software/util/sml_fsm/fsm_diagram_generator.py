3fcd7e527d8117ef7e95b802fe70cf7d5ef5b0c4
williamckha <williamha@outlook.com>
2023-02-09 01:21:43 -0800

    Rename script

--- src/software/util/sml_fsm/fsm_diagram_generator.py
+++ src/software/util/sml_fsm/fsm_diagram_generator.py
@@ -1,9 +1,13 @@
+#!/opt/tbotspython/bin/python3.8
+
 import re
 import os
+from pathlib import Path
 
-OUTPUT_FILEPATH = "docs/fsm-diagrams.md"
+OUTPUT_FILE_PATH = "docs/fsm-diagrams.md"
 
-def generateDiagram(fsm):
+
+def generate_diagram(fsm):
     """
     Generates a mermaid.js text definition for a state diagram 
     representing the FSM.
@@ -16,20 +20,20 @@ def generateDiagram(fsm):
     """
 
     # Regex match to extract transition table from the FSM
-    transition_table_match = re.search(r'make_transition_table\(([\s\S]*?)\);', fsm);
-    if not transition_table_match: 
+    transition_table_match = re.search(r"make_transition_table\(([\s\S]*?)\);", fsm)
+    if not transition_table_match:
         return
     transition_table = transition_table_match.group(1)
 
     # Remove comment lines
-    transition_table = re.sub(r'//.*$', '', transition_table, flags=re.MULTILINE)
-    
+    transition_table = re.sub(r"//.*$", "", transition_table, flags=re.MULTILINE)
+
     # Remove all whitespace
-    transition_table = re.sub(r'\s+', '', transition_table, flags=re.MULTILINE)
+    transition_table = re.sub(r"\s+", "", transition_table, flags=re.MULTILINE)
 
-    transitions = transition_table.split(',')
+    transitions = transition_table.split(",")
 
-    diagram_uml = ''
+    diagram_uml = ""
 
     for transition in transitions:
 
@@ -37,39 +41,40 @@ def generateDiagram(fsm):
         # src_state + event [guard] / action = dest_state
 
         # Extract dest_state from transition, which is after '=' sign
-        transition, *rest = transition.split('=')
-        dest_state = rest[0] if rest else ''
+        transition, *rest = transition.split("=")
+        dest_state = rest[0] if rest else ""
 
         # Extract action from transition, which is after '/' sign
-        transition, *rest = transition.split('/')
-        action = rest[0] if rest else ''
+        transition, *rest = transition.split("/")
+        action = rest[0] if rest else ""
 
         # Extract guard contained between square brackets
-        guard = next(iter(re.findall(r'\[(.*?)\]', transition)), '')
+        guard = next(iter(re.findall(r"\[(.*?)\]", transition)), "")
 
         # Extract src_state from transition, which is before '+' sign
-        src_state = transition.split('+')[0]
+        src_state = transition.split("+")[0]
 
         # Remove suffixes from states, guard, action
         def remove_suffix(str, suffix):
-            return str[:-len(suffix)] if str.endswith(suffix) else str
-        src_state = remove_suffix(src_state, '_S')
-        dest_state = remove_suffix(dest_state, '_S')
-        guard = remove_suffix(guard, '_G')
-        action = remove_suffix(action, '_A')
-        
-        if dest_state == 'X':
-            dest_state = '[*]'
+            return str[: -len(suffix)] if str.endswith(suffix) else str
 
-        if src_state.startswith('*'):
+        src_state = remove_suffix(src_state, "_S")
+        dest_state = remove_suffix(dest_state, "_S")
+        guard = remove_suffix(guard, "_G")
+        action = remove_suffix(action, "_A")
+
+        if dest_state == "X":
+            dest_state = "[*]"
+
+        if src_state.startswith("*"):
             src_state = src_state[1:]
             diagram_uml += f"[*] --> {src_state}\n"
-        
+
         if not dest_state:
             dest_state = src_state
 
-        if src_state == 'X':
-            src_state = 'Terminate:::terminate'
+        if src_state == "X":
+            src_state = "Terminate:::terminate"
 
         diagram_uml += f"{src_state} --> {dest_state}"
 
@@ -78,28 +83,35 @@ def generateDiagram(fsm):
 
             if action:
                 diagram_uml += f"\\n<i>{action}</i>"
-        
+
         elif action:
             diagram_uml += f" : <i>{action}</i>"
-        
+
         diagram_uml += "\n"
 
     return (
-        "stateDiagram-v2\n" +
-        "classDef terminate fill:white,color:black,font-weight:bold\n" + 
-        "direction LR\n" +
-        diagram_uml
+        "stateDiagram-v2\n"
+        + "classDef terminate fill:white,color:black,font-weight:bold\n"
+        + "direction LR\n"
+        + diagram_uml
     )
+
 
 if __name__ == "__main__":
 
-    with open(OUTPUT_FILEPATH, "w") as output_file:
+    root_dir = Path(os.path.abspath(__file__)).parents[4]
+
+    output_file_path = os.path.join(root_dir, OUTPUT_FILE_PATH)
+    with open(output_file_path, "w") as output_file:
 
         print("# Play and Tactic FSM Diagrams\n", file=output_file)
-        
+
         diagrams_generated = 0
-        for root, dirs, files in os.walk("src/software/ai"):
+        
+        ai_dir = os.path.join(root_dir, "src/software/ai")
+        for root, dirs, files in os.walk(ai_dir):
             for file in files:
+
                 if not file.endswith("fsm.h"):
                     continue
 
@@ -108,19 +120,23 @@ if __name__ == "__main__":
                     fsm = f.read()
 
                 # Regex match to extract FSM struct name
-                fsm_name = re.findall(r'struct\s+(\w+)\s*\{', fsm)[0]
+                fsm_name = re.findall(r"struct\s+(\w+)\s*\{", fsm)[0]
 
-                diagram = generateDiagram(fsm)
-                
-                # Format diagram in Markdown syntax and write to output file
+                diagram = generate_diagram(fsm)
+
                 if diagram:
+
+                    # Get file path to FSM header file relative to project root
+                    filepath = filepath[filepath.find("src/"):]
+                    
+                    # Format diagram in Markdown syntax and write to output file
                     print(f"## [{fsm_name}](/{filepath})\n", file=output_file)
                     print(f"```mermaid\n\n{diagram}\n```\n", file=output_file)
+
                     diagrams_generated += 1
-    
+
         # Print out confirmation message to console
         print(
-            f"Generated {diagrams_generated} FSM diagrams. " +
-            f"View output in {OUTPUT_FILEPATH}"
+            f"Generated {diagrams_generated} FSM diagrams. "
+            + f"View output in {OUTPUT_FILE_PATH}"
         )
-                    
