c69d997313a0d9f1bb4943c28096aa9159569ca7
Arun <arun.sfc@outlook.com>
2023-02-17 02:48:38 -0800

    autoref script documentation

--- src/software/autoref/run_autoref.sh
+++ src/software/autoref/run_autoref.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Runs the Tigers Autoref binary that is set up by "setup_software.sh". By default, wrapper runs the autoreferee in headless, active mode. If an additional argument is passed in to the script, it is forwarded to the binary.
+
 cd "/opt/tbotspython/autoReferee/"
 export JAVA_HOME="/usr/lib/jvm/jdk-17"
 
