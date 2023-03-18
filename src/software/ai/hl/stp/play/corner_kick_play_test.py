da04d56cf3267347d5a3a4d78ae4fadad0d398f5
sauravbanna <sauravbanna@gmail.com>
2023-03-18 16:50:19 -0700

    fixing tests

--- src/software/ai/hl/stp/play/corner_kick_play_test.py
+++ src/software/ai/hl/stp/play/corner_kick_play_test.py
@@ -11,20 +11,21 @@ from proto.message_translation.tbots_protobuf import create_world_state
 from proto.ssl_gc_common_pb2 import Team
 
 
-def test_corner_kick_play_bottom_left(simulated_test_runner):
-
+def corner_kick_test_setup(ball_and_bots_pos, simulated_test_runner):
+    """
+        Setup for Corner Kick Play Test
+        :param ball_and_bots_pos: object
+            {
+                "ball_initial_pos": initial position of the ball,
+                "blue_bots": list of positions of blue robots
+            }
+        :param simulated_test_runner: the simulated test runner currently being used
+        """
     # starting point must be Point
-    ball_initial_pos = tbots.Point(4.5, -3)
+    ball_initial_pos = ball_and_bots_pos["ball_initial_pos"]
 
     # Setup Bots
-    blue_bots = [
-        tbots.Point(-3, 2.5),
-        tbots.Point(-3, 1.5),
-        tbots.Point(-3, 0.5),
-        tbots.Point(-3, -0.5),
-        tbots.Point(-3, -1.5),
-        tbots.Point(4.6, -3.1),
-    ]
+    blue_bots = ball_and_bots_pos["blue_bots"]
 
     yellow_bots = [
         tbots.Point(1, 0),
@@ -67,89 +68,41 @@ def test_corner_kick_play_bottom_left(simulated_test_runner):
         ),
     )
 
-    # Always Validation
-    # TODO- #2781 Validation
-    always_validation_sequence_set = [[]]
 
-    # Eventually Validation
+def test_corner_kick_play(simulated_test_runner):
     # TODO- #2781 Validation
-    eventually_validation_sequence_set = [[]]
-
+    # TODO- #2781 Validation
     simulated_test_runner.run_test(
-        eventually_validation_sequence_set=eventually_validation_sequence_set,
-        always_validation_sequence_set=always_validation_sequence_set,
+        setup=lambda runner=simulated_test_runner: corner_kick_test_setup(simulated_test_runner),
+        params=[
+            {
+                "ball_initial_pos": tbots.Point(4.5, -3),
+                "blue_bots": [
+                    tbots.Point(-3, 2.5),
+                    tbots.Point(-3, 1.5),
+                    tbots.Point(-3, 0.5),
+                    tbots.Point(-3, -0.5),
+                    tbots.Point(-3, -1.5),
+                    tbots.Point(4.6, -3.1),
+                ]
+            },
+            {
+                "ball_initial_pos": tbots.Point(4.5, 3),
+                "blue_bots": [
+                    tbots.Point(-3, 2.5),
+                    tbots.Point(0, 1.5),
+                    tbots.Point(0, 0.5),
+                    tbots.Point(0, -0.5),
+                    tbots.Point(0, -1.5),
+                    tbots.Point(4.6, 3.1),
+                ]
+            }
+        ],
+        inv_always_validation_sequence_set=[[]],
+        inv_eventually_validation_sequence_set=[[]],
+        ag_always_validation_sequence_set=[[]],
+        ag_eventually_validation_sequence_set=[[]],
         test_timeout_s=10,
-    )
-
-
-def test_corner_kick_play_top_right(simulated_test_runner):
-
-    # starting point must be Point
-    ball_initial_pos = tbots.Point(4.5, 3)
-
-    # Setup Bots
-    blue_bots = [
-        tbots.Point(-3, 2.5),
-        tbots.Point(0, 1.5),
-        tbots.Point(0, 0.5),
-        tbots.Point(0, -0.5),
-        tbots.Point(0, -1.5),
-        tbots.Point(4.6, 3.1),
-    ]
-
-    yellow_bots = [
-        tbots.Point(1, 0),
-        tbots.Point(1, 2.5),
-        tbots.Point(1, -2.5),
-        tbots.Field.createSSLDivisionBField().enemyGoalCenter(),
-        tbots.Field.createSSLDivisionBField().enemyDefenseArea().negXNegYCorner(),
-        tbots.Field.createSSLDivisionBField().enemyDefenseArea().negXPosYCorner(),
-    ]
-
-    # Game Controller Setup
-    simulated_test_runner.gamecontroller.send_ci_input(
-        gc_command=Command.Type.STOP, team=Team.UNKNOWN
-    )
-    simulated_test_runner.gamecontroller.send_ci_input(
-        gc_command=Command.Type.NORMAL_START, team=Team.BLUE
-    )
-    simulated_test_runner.gamecontroller.send_ci_input(
-        gc_command=Command.Type.DIRECT, team=Team.BLUE
-    )
-
-    # Force play override here
-    blue_play = Play()
-    blue_play.name = PlayName.CornerKickPlay
-
-    yellow_play = Play()
-    yellow_play.name = PlayName.HaltPlay
-
-    simulated_test_runner.blue_full_system_proto_unix_io.send_proto(Play, blue_play)
-    simulated_test_runner.yellow_full_system_proto_unix_io.send_proto(Play, yellow_play)
-
-    # Create world state
-    simulated_test_runner.simulator_proto_unix_io.send_proto(
-        WorldState,
-        create_world_state(
-            yellow_robot_locations=yellow_bots,
-            blue_robot_locations=blue_bots,
-            ball_location=ball_initial_pos,
-            ball_velocity=tbots.Vector(0, 0),
-        ),
-    )
-
-    # Always Validation
-    # TODO- #2781 Validation
-    always_validation_sequence_set = [[]]
-
-    # Eventually Validation
-    # TODO- #2781 Validation
-    eventually_validation_sequence_set = [[]]
-
-    simulated_test_runner.run_test(
-        eventually_validation_sequence_set=eventually_validation_sequence_set,
-        always_validation_sequence_set=always_validation_sequence_set,
-        test_timeout_s=7.5,
     )
 
 
