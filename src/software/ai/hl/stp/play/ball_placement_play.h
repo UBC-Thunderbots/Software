2aa6ee568642a18f6fe7cc33f602f820ec6507cf
Arun <arun.sfc@outlook.com>
2023-04-25 21:51:11 -0700

    sample formatting error

--- src/software/ai/hl/stp/play/ball_placement_play.h
+++ src/software/ai/hl/stp/play/ball_placement_play.h
@@ -4,7 +4,7 @@
 #include "software/ai/hl/stp/play/play.h"
 
 /**
- * A Play tat perfurms ball placement, i.e. placing the ball in a defined location
+ * A Play that performs ball placement, i.e. placing the ball in a defined location
  * determined by the referee. This is used to obey the referee "Ball Placement Us" command
  */
 class BallPlacementPlay : public Play
