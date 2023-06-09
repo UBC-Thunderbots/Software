d03173a15536a4922d83c8cc0eac70ac5ae64d0b
williamckha <williamha@outlook.com>
2023-06-08 19:45:25 -0700

    Forgot

--- src/software/ai/hl/stp/tactic/goalie/goalie_fsm.h
+++ src/software/ai/hl/stp/tactic/goalie/goalie_fsm.h
@@ -85,7 +85,7 @@ struct GoalieFSM
      * @return a point on the field that is a good place to chip to
      */
     static Point findGoodChipTarget(const World &world,
-                                    TbotsProto::GoalieTacticConfig goalie_tactic_config);
+                                    const TbotsProto::GoalieTacticConfig &goalie_tactic_config);
 
     /**
      * Guard that checks if the ball is moving faster than the time_to_panic threshold
