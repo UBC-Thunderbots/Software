be0c307be2f7ecf7517c488c4414f6017e98035b
Raymond Wang <ruiwenhw@gmail.com>
2023-07-06 07:28:39 -0700

    Improve enemy free kick tactic assignment

--- src/software/ai/hl/stp/play/enemy_free_kick_play.cpp
+++ src/software/ai/hl/stp/play/enemy_free_kick_play.cpp
@@ -14,117 +14,70 @@ EnemyFreekickPlay::EnemyFreekickPlay(TbotsProto::AiConfig config) : Play(config,
 void EnemyFreekickPlay::getNextTactics(TacticCoroutine::push_type &yield,
                                        const World &world)
 {
-    // Init a Crease Defender Tactic
-    auto crease_defender_tactic = std::make_shared<CreaseDefenderTactic>(
-        ai_config.robot_navigation_obstacle_config());
+    // Free kicks are usually taken near the edge of the field. We assign one robot to
+    // shadow the enemy robot taking the free kick
+    auto shadow_free_kicker = std::make_shared<ShadowEnemyTactic>();
 
-    // These robots will both block the enemy robot taking a free kick
-    std::array<std::shared_ptr<ShadowEnemyTactic>, 2> shadow_free_kicker = {
-        std::make_shared<ShadowEnemyTactic>(), std::make_shared<ShadowEnemyTactic>()};
+    // Assign up to two crease defenders
+    std::vector<std::shared_ptr<CreaseDefenderTactic>> crease_defenders = {
+            std::make_shared<CreaseDefenderTactic>(
+                    ai_config.robot_navigation_obstacle_config()),
+            std::make_shared<CreaseDefenderTactic>(
+                    ai_config.robot_navigation_obstacle_config())};
 
-    // Init Shadow Enemy Tactics for extra robots
-    std::array<std::shared_ptr<ShadowEnemyTactic>, 2> shadow_potential_receivers = {
-        std::make_shared<ShadowEnemyTactic>(), std::make_shared<ShadowEnemyTactic>()};
-
-    // Init Move Tactics for extra robots (These will be used if there are no robots to
-    // shadow)
-    auto move_tactic_main      = std::make_shared<MoveTactic>();
-    auto move_tactic_secondary = std::make_shared<MoveTactic>();
+    // Try to shadow as many enemy robots as possible
+    std::vector<std::shared_ptr<ShadowEnemyTactic>> shadow_potential_receivers;
 
     do
     {
-        // Create tactic vector (starting with Goalie)
+        // Create tactic vector
         PriorityTacticVector tactics_to_run = {{}};
 
         // Get all enemy threats
         auto enemy_threats = getAllEnemyThreats(world.field(), world.friendlyTeam(),
                                                 world.enemyTeam(), world.ball(), false);
 
-        // shadow free kicker should shadow the robot with the ball and if no such enemy
-        // exists, then it will default to positioning between the ball and the friendly
-        // defense area
-        if (enemy_threats.size() >= 1)
+        if (enemy_threats.empty())
         {
-            std::get<0>(shadow_free_kicker)
-                ->updateControlParams(enemy_threats.at(0), ROBOT_MAX_RADIUS_METERS * 3);
-            std::get<1>(shadow_free_kicker)
-                ->updateControlParams(enemy_threats.at(0), ROBOT_MAX_RADIUS_METERS * 3);
+            return;
         }
 
-        // Add Freekick shadower tactics
-        tactics_to_run[0].emplace_back(std::get<0>(shadow_free_kicker));
-        tactics_to_run[0].emplace_back(std::get<1>(shadow_free_kicker));
-        // Add Crease defender tactic on side of open enemy threats
-        if (enemy_threats.size() >= 4)
+        // Assign crease defenders
+        auto num_crease_defenders = world.friendlyTeam().numRobots() > 3 ? 2 : 1;
+        if (num_crease_defenders == 2)
         {
-            if (enemy_threats.at(3).robot.position().y() > 0)
-            {
-                crease_defender_tactic->updateControlParams(
+            crease_defenders[0]->updateControlParams(
                     world.ball().position(), TbotsProto::CreaseDefenderAlignment::LEFT);
-            }
-            else
-            {
-                crease_defender_tactic->updateControlParams(
+            crease_defenders[1]->updateControlParams(
                     world.ball().position(), TbotsProto::CreaseDefenderAlignment::RIGHT);
-            }
+
+            tactics_to_run[0].emplace_back(crease_defenders[0]);
+            tactics_to_run[0].emplace_back(crease_defenders[1]);
         }
         else
         {
-            crease_defender_tactic->updateControlParams(
-                world.ball().position(), TbotsProto::CreaseDefenderAlignment::CENTRE);
+            crease_defenders[0]->updateControlParams(
+                    world.ball().position(), TbotsProto::CreaseDefenderAlignment::CENTRE);
+
+            tactics_to_run[0].emplace_back(crease_defenders[0]);
         }
 
-        tactics_to_run[0].emplace_back(crease_defender_tactic);
-
-        // Assign ShadowEnemy tactics until we have every enemy covered. If there are not
-        // enough threats to shadow, move our robots to block the friendly net
-        if (enemy_threats.size() <= 1)
+        auto num_unassigned_robots = 4 - num_crease_defenders;
+        for (int i = 0;
+             i < std::min(num_unassigned_robots, (int)enemy_threats.size() - 1); i++)
         {
-            // Since the first enemy threat is covered by the shadow_free_kicker, just
-            // move to block the net
-            move_tactic_main->updateControlParams(
-                world.field().friendlyGoalCenter() +
-                    Vector(0, 2 * ROBOT_MAX_RADIUS_METERS),
-                (world.ball().position() - world.field().friendlyGoalCenter())
-                    .orientation(),
-                0);
-            move_tactic_main->updateControlParams(
-                world.field().friendlyGoalCenter() +
-                    Vector(0, -2 * ROBOT_MAX_RADIUS_METERS),
-                (world.ball().position() - world.field().friendlyGoalCenter())
-                    .orientation(),
-                0);
+            shadow_potential_receivers.emplace_back(
+                    std::make_shared<ShadowEnemyTactic>());
+            shadow_potential_receivers[i]->updateControlParams(
+                    enemy_threats[i + 1], ROBOT_MAX_RADIUS_METERS * 3);
 
-            tactics_to_run[0].emplace_back(move_tactic_main);
-            tactics_to_run[0].emplace_back(move_tactic_secondary);
+            tactics_to_run[0].emplace_back(shadow_potential_receivers[i]);
         }
-        if (enemy_threats.size() == 2)
-        {
-            // Shadow the second most threatening enemy threat and move one robot to block
-            // the net
-            move_tactic_main->updateControlParams(
-                world.field().friendlyGoalCenter() +
-                    Vector(0, 2 * ROBOT_MAX_RADIUS_METERS),
-                (world.ball().position() - world.field().friendlyGoalCenter())
-                    .orientation(),
-                0);
-            std::get<0>(shadow_potential_receivers)
-                ->updateControlParams(enemy_threats.at(1), ROBOT_MAX_RADIUS_METERS * 3);
 
-            tactics_to_run[0].emplace_back(std::get<0>(shadow_potential_receivers));
-            tactics_to_run[0].emplace_back(move_tactic_main);
-        }
-        if (enemy_threats.size() >= 3)
-        {
-            // Shadow the second and third most threatening enemy threats
-            std::get<0>(shadow_potential_receivers)
-                ->updateControlParams(enemy_threats.at(1), ROBOT_MAX_RADIUS_METERS * 3);
-            std::get<1>(shadow_potential_receivers)
-                ->updateControlParams(enemy_threats.at(2), ROBOT_MAX_RADIUS_METERS * 3);
-
-            tactics_to_run[0].emplace_back(std::get<0>(shadow_potential_receivers));
-            tactics_to_run[0].emplace_back(std::get<1>(shadow_potential_receivers));
-        }
+        // Shadow free kicker should shadow the enemy robot with the ball
+        shadow_free_kicker->updateControlParams(enemy_threats[0],
+                                                ROBOT_MAX_RADIUS_METERS * 3);
+        tactics_to_run[0].emplace_back(shadow_free_kicker);
 
         // yield the Tactics this Play wants to run, in order of priority
         yield(tactics_to_run);
