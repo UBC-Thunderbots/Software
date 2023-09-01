e1bde8f4f0e5889ee55ed32103cb7c64b96b4788
nima <nimazareian80@gmail.com>
2023-08-31 21:04:55 -0700

    Add pose class

---
+++ src/software/geom/pose.cpp
@@ -0,0 +1,32 @@
+#include "software/geom/pose.h"
+
+Pose::Pose() : position_(Point()), orientation_(Angle::zero()) {}
+
+Pose::Pose(const Point &position, const Angle orientation) : position_(position), orientation_(orientation) {}
+
+const Point &Pose::position() const
+{
+    return position_;
+}
+
+const Angle &Pose::orientation() const
+{
+    return orientation_;
+}
+
+bool Pose::operator==(const Pose &rhs) const
+{
+    return position_ == rhs.position_ &&
+           orientation_ == rhs.orientation_;
+}
+
+bool Pose::operator!=(const Pose &rhs) const
+{
+    return !(rhs == *this);
+}
+
+std::ostream &operator<<(std::ostream &os, const Pose &p)
+{
+    os << p.position() << " @ " << p.orientation();
+    return os;
+}
