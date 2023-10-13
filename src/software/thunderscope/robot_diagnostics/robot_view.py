b77f436e9d882bf7c9aa466e260a5638f25ad9e8
Mai <luongxuanmai2109@gmail.com>
2023-10-12 19:41:49 -0700

    add hinting to new class

--- src/software/thunderscope/robot_diagnostics/robot_view.py
+++ src/software/thunderscope/robot_diagnostics/robot_view.py
@@ -19,7 +19,7 @@ class RobotCrashDialog(QDialog):
 
     """
 
-    def __init__(self, message, robot_status, parent=None):
+    def __init__(self, message: str, robot_status: RobotStatusView, parent: QWidget=None) -> None:
         super().__init__(parent)
 
         self.setWindowTitle("Robot Crash")
