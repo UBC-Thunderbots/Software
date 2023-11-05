8a9f5ad3c8e8548bfa9db6a6d1d54763614f053e
sauravbanna <sauravbanna@gmail.com>
2023-11-04 17:59:24 -0700

    changes

--- src/software/simulated_tests/robot_enters_region_and_stops.py
+++ src/software/simulated_tests/robot_enters_region_and_stops.py
@@ -11,17 +11,16 @@ from software.simulated_tests.robot_enters_region import RobotEntersRegion
 
 
 class RobotEntersRegionAndStops(RobotEntersRegion):
-    """Checks if a specific robot enters the provided regions and stops there for the provided time"""
+    """Checks if a robot enters the provided regions and stops there for the provided time"""
 
     ROBOT_MAX_STATIONARY_SPEED_M_PER_S = 0.01
 
     def __init__(self, regions=None, num_ticks=1):
         """
-        Constructs the base validation with the given region
+        Constructs the base validation with the given regions
         Sets boolean indicating validation stage to default
 
-        :param robot_id: the id of the robot being validated
-        :param region: the region the robot has to enter
+        :param regions: the regions the robot has to enter
         :param num_ticks: the time the robot has to stay stationary for in the regions
         """
         super().__init__(regions)
@@ -33,10 +32,13 @@ class RobotEntersRegionAndStops(RobotEntersRegion):
 
     def get_validation_status(self, world) -> ValidationStatus:
         """
-        Checks if a specific robot id is in the provided region
+        Checks if a robot is in the provided region
         Then checks if that robot is stationary within a threshold for the provided number of ticks
 
         Sets booleans about the state of the validation for logging
+
+        ASSUMES 1 robot per region. If there's more than 1 robot in the region the given regions can
+        probably be made more specific
 
         :param world: the world message to validate
         :return: FAILING until the specific robot enters the provided region
@@ -69,7 +71,7 @@ class RobotEntersRegionAndStops(RobotEntersRegion):
                 self.is_stationary = False
                 return ValidationStatus.FAILING
         else:
-            # reset the num ticks since robot is
+            # reset the num ticks since robot is no longer in region
             self.ticks_so_far = 0
             self.is_stationary = False
             return robot_in_region_validation
@@ -78,7 +80,7 @@ class RobotEntersRegionAndStops(RobotEntersRegion):
         """
         (override) shows region to enter
         """
-        return create_validation_geometry([self.region])
+        return create_validation_geometry(self.regions)
 
     def __repr__(self):
         """
@@ -86,12 +88,10 @@ class RobotEntersRegionAndStops(RobotEntersRegion):
         Either the robot has not entered the region yet, or it has but is not stationary
         """
         if not self.is_stationary:
-            return f"Check that robot {self.robot_id} in correct region is stationary"
+            return f"Check that robot in correct region is stationary"
 
-        return (
-            f"Check for stationary robot {self.robot_id} in region "
-            + ","
-            + repr(self.region)
+        return f"Check for stationary robot in region " + ",".join(
+            repr(region) for region in self.regions
         )
 
 
