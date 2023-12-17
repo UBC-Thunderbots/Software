299125cac047ff36f74097ba9cdb3a8c88764591
nima <nimazareian80@gmail.com>
2023-12-17 12:52:33 -0800

    remove comments

--- src/software/geom/angular_acceleration.h
+++ src/software/geom/angular_acceleration.h
@@ -3,8 +3,8 @@
  * We also use variables of type 'Angle' to represent angular acceleration, since they
  * are essentially represented the same. This typedef allows us to refer to Angles as
  * AngularAcceleration, which makes the interfaces more intuitive.
- * @note Not all methods of Angle class make sense for AngularAcceleration!
- * E.g. Angle::clamp does not make sense in the context of AngularAcceleration, as
- * 360 deg/s^2 is different from 0 deg/s^2, but 360 deg is the same as 0 deg.
+ * TODO (#3093): Not all methods of Angle class make sense for AngularAcceleration!
+ *  E.g. Angle::clamp does not make sense in the context of AngularAcceleration, as
+ *  360 deg/s^2 is different from 0 deg/s^2, but 360 deg is the same as 0 deg.
  */
 using AngularAcceleration = Angle;
--- src/software/geom/angular_velocity.h
+++ src/software/geom/angular_velocity.h
@@ -4,8 +4,8 @@
  * We also use variables of type 'Angle' to represent angular velocities, since they
  * are essentially represented the same. This typedef allows us to refer to Angles as
  * AngularVelocities, which makes the interfaces more intuitive.
- * @note Not all methods of Angle class make sense for AngularVelocity!
- * E.g. Angle::clamp does not make sense in the context of AngularVelocity, as 360 deg/s
- * is different from 0 deg/s, but 360 deg is the same as 0 deg.
+ * TODO (#3093): Not all methods of Angle class make sense for AngularVelocity!
+ *  E.g. Angle::clamp does not make sense in the context of AngularVelocity, as 360 deg/s
+ *  is different from 0 deg/s, but 360 deg is the same as 0 deg.
  */
 using AngularVelocity = Angle;
