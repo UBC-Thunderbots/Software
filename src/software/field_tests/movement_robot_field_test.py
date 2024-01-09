d34ea35b0c70e4573e16e2cb907efee8d7f3bd91
nima <nimazareian80@gmail.com>
2024-01-09 15:38:31 -0800

    Add movement robot field test

--- src/software/field_tests/movement_robot_field_test.py
+++ src/software/field_tests/movement_robot_field_test.py
@@ -76,55 +76,58 @@ logger = createLogger(__name__)
 
 
 # this test can only be run on the field
-# def test_basic_rotation(field_test_runner):
-#     world = field_test_runner.world_buffer.get(block=True, timeout=WORLD_BUFFER_TIMEOUT)
-#     if len(world.friendly_team.team_robots) == 0:
-#         raise Exception("The first world received had no robots in it!")
-#
-#     print("Here are the robots:")
-#     print([robot.current_state.global_position for robot in world.friendly_team.team_robots])
-#
-#     id = world.friendly_team.team_robots[0].id
-#     print(f"Running test on robot {id}")
-#
-#     rob_pos_p = robot.current_state.global_position
-#     logger.info("staying in pos {rob_pos_p}")
-#
-#     for angle in test_angles:
-#         move_tactic = MoveTactic()
-#         move_tactic.destination.CopyFrom(rob_pos_p)
-#         move_tactic.final_speed = 0.0
-#         move_tactic.dribbler_mode = DribblerMode.OFF
-#         move_tactic.final_orientation.CopyFrom(Angle(radians=angle))
-#         move_tactic.ball_collision_type = BallCollisionType.AVOID
-#         move_tactic.auto_chip_or_kick.CopyFrom(
-#             AutoChipOrKick(autokick_speed_m_per_s=0.0)
-#         )
-#         move_tactic.max_allowed_speed_mode = MaxAllowedSpeedMode.PHYSICAL_LIMIT
-#         move_tactic.target_spin_rev_per_s = 0.0
-#
-#         # Setup Tactic
-#         params = AssignedTacticPlayControlParams()
-#
-#         params.assigned_tactics[id].move.CopyFrom(move_tactic)
-#
-#         field_test_runner.set_tactics(params, True)
-#         field_test_runner.run_test(
-#             always_validation_sequence_set=[[]],
-#             eventually_validation_sequence_set=[[]],
-#             test_timeout_s=5,
-#         )
-#         # Send a stop tactic after the test finishes
-#         stop_tactic = StopTactic()
-#         params = AssignedTacticPlayControlParams()
-#         params.assigned_tactics[id].stop.CopyFrom(stop_tactic)
-#         # send the stop tactic
-#         field_test_runner.set_tactics(params, True)
-#
-#         # validate by eye
-#         logger.info(f"robot set to {angle} orientation")
-#
-#         time.sleep(2)
+def test_basic_rotation(field_test_runner):
+    test_angles = [0, 45, 90, 180, 270, 0]
+
+    world = field_test_runner.world_buffer.get(block=True, timeout=WORLD_BUFFER_TIMEOUT)
+    if len(world.friendly_team.team_robots) == 0:
+        raise Exception("The first world received had no robots in it!")
+
+    print("Here are the robots:")
+    print([robot.current_state.global_position for robot in world.friendly_team.team_robots])
+
+    id = world.friendly_team.team_robots[0].id
+    print(f"Running test on robot {id}")
+
+    robot = world.friendly_team.team_robots[0]
+    rob_pos_p = robot.current_state.global_position
+    logger.info("staying in pos {rob_pos_p}")
+
+    for angle in test_angles:
+        move_tactic = MoveTactic()
+        move_tactic.destination.CopyFrom(rob_pos_p)
+        move_tactic.final_speed = 0.0
+        move_tactic.dribbler_mode = DribblerMode.OFF
+        move_tactic.final_orientation.CopyFrom(Angle(radians=angle))
+        move_tactic.ball_collision_type = BallCollisionType.AVOID
+        move_tactic.auto_chip_or_kick.CopyFrom(
+            AutoChipOrKick(autokick_speed_m_per_s=0.0)
+        )
+        move_tactic.max_allowed_speed_mode = MaxAllowedSpeedMode.PHYSICAL_LIMIT
+        move_tactic.target_spin_rev_per_s = 0.0
+
+        # Setup Tactic
+        params = AssignedTacticPlayControlParams()
+
+        params.assigned_tactics[id].move.CopyFrom(move_tactic)
+
+        field_test_runner.set_tactics(params, True)
+        field_test_runner.run_test(
+            always_validation_sequence_set=[[]],
+            eventually_validation_sequence_set=[[]],
+            test_timeout_s=5,
+        )
+        # Send a stop tactic after the test finishes
+        stop_tactic = StopTactic()
+        params = AssignedTacticPlayControlParams()
+        params.assigned_tactics[id].stop.CopyFrom(stop_tactic)
+        # send the stop tactic
+        field_test_runner.set_tactics(params, True)
+
+        # validate by eye
+        logger.info(f"robot set to {angle} orientation")
+
+        time.sleep(2)
 
 
 def test_one_robots_square(field_test_runner):
