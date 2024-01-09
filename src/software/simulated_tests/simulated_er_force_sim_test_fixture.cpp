508ab78118e17b388b1b5f25b38a5ed268d5f061
nima <nimazareian80@gmail.com>
2024-01-08 21:56:29 -0800

    Revert simulated_er_force_sim_test_fixture.cpp

--- src/software/simulated_tests/simulated_er_force_sim_test_fixture.cpp
+++ src/software/simulated_tests/simulated_er_force_sim_test_fixture.cpp
@@ -529,11 +529,6 @@ bool SimulatedErForceSimTestFixture::tickTest(
         // TODO (#2419): remove this to re-enable sigfpe checks
         feenableexcept(FE_INVALID | FE_OVERFLOW);
         updateSensorFusion(simulator);
-        //        if (run_simulation_in_realtime)
-        //        {
-        //            sleep(wall_start_time,
-        //            Duration::fromMilliseconds(ai_time_step.toMilliseconds() / 2.0));
-        //        }
     }
 
     if (friendly_sensor_fusion.getWorld().has_value() &&
@@ -555,8 +550,7 @@ bool SimulatedErForceSimTestFixture::tickTest(
 
         if (run_simulation_in_realtime)
         {
-            sleep(wall_start_time,
-                  Duration::fromMilliseconds(ai_time_step.toMilliseconds() /* / 2.0*/));
+            sleep(wall_start_time, ai_time_step);
         }
     }
     else
