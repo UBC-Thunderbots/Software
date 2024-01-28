29d684cdd8a6c9d311e5c69b902179be9f554325
Mikhael <michael.benzvi01@gmail.com>
2024-01-27 23:10:14 -0800

    Implemented functions

--- src/software/geom/algorithms/signed_distance.cpp
+++ src/software/geom/algorithms/signed_distance.cpp
@@ -1,2 +1,61 @@
 #include "software/geom/algorithms/signed_distance.h"
 
+#include "software/geom/algorithms/distance.h"
+
+double signedDistance(const Circle &first, const Point &second)
+{
+    return distance(first.origin(), second) - first.radius();
+}
+double signedDistance(const Point &first, const Circle &second)
+{
+    return signedDistance(second, first);
+}
+
+double signedDistance(const Rectangle &first, const Point &second)
+{
+    Vector centre_to_point = second - first.centre();
+    Vector abs_centre_to_point = Vector(std::abs(centre_to_point.x()), std::abs(centre_to_point.y()));
+    Vector corner_to_point =  abs_centre_to_point - (first.posXPosYCorner() - first.centre());
+    Vector clamped_corner_to_point = Vector(std::max(corner_to_point.x(), 0.0), std::max(corner_to_point.y(), 0.0));
+
+    return clamped_corner_to_point.length() + std::min(std::max(corner_to_point.x(), corner_to_point.y()), 0.0);
+}
+double signedDistance(const Point &first, const Rectangle &second)
+{
+    return signedDistance(second, first);
+}
+
+double signedDistance(const Polygon &first, const Point &second)
+{
+    std::vector<Point> points = first.getPoints();
+
+    double min_length = (second-points[0]).lengthSquared();
+    double s = 1.0;
+    for(auto it = points.begin(), j=--points.end(); it != points.end(); j=it, ++it)
+    {
+        Vector e = j->toVector() - it->toVector();
+        Vector w = second.toVector() - it->toVector();
+        Vector b = w - e*std::clamp(w.dot(e) / e.dot(e), 0.0, 1.0);
+        min_length = std::min(min_length, b.lengthSquared());
+        std::vector<bool> c = {second.y() >= it->y(), second.y() < j->y(), e.x()*w.y()>e.y()*w.x()};
+        if(std::all_of(c.begin(), c.end(), [](bool v) {return v; }) ||
+           std::none_of(c.begin(), c.end(), [](bool v) {return v; }))
+        {
+            s *= -1;
+        }
+    }
+    return s*sqrt(min_length);
+}
+double signedDistance(const Point &first, const Polygon &second)
+{
+    return signedDistance(second, first);
+}
+
+double signedDistance(const Stadium &first, const Point &second)
+{
+    return distance(first.segment(), second) - first.radius();
+}
+double signedDistance(const Point &first, const Stadium &second)
+{
+    return signedDistance(second, first);
+}
