36eb41e434becb6349466d2176210185066f03e3
arun <arun.sfc@outlook.com>
2024-01-27 16:17:47 -0800

    integration in progress

--- src/software/ai/ai.cpp
+++ src/software/ai/ai.cpp
@@ -68,6 +68,8 @@ void Ai::checkAiConfig()
 
 std::unique_ptr<TbotsProto::PrimitiveSet> Ai::getPrimitives(const World& world)
 {
+    ZoneNamedN(tracy::ai, "Ai::getPrimitives", true);
+
     checkAiConfig();
 
     fsm->process_event(PlaySelectionFSM::Update(
