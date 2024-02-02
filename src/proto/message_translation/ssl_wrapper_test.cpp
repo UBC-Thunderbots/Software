514631dc683dbc62b0092ab330ce0943d1986fc1
Mikhael <michael.benzvi01@gmail.com>
2024-02-01 19:08:06 -0800

    Fixed failing wrapper test

--- src/proto/message_translation/ssl_wrapper_test.cpp
+++ src/proto/message_translation/ssl_wrapper_test.cpp
@@ -59,7 +59,7 @@ TEST(SSLWrapperTest, test_create_wrapper_with_world_friendly_yellow)
 
     TeamColour friendly_team_colour = TeamColour::YELLOW;
 
-    World world = TestUtil::createBlankTestingWorld();
+    World world = *TestUtil::createBlankTestingWorld();
     world.updateFriendlyTeamState(Team(yellow_robots));
     world.updateEnemyTeamState(Team(blue_robots));
 
