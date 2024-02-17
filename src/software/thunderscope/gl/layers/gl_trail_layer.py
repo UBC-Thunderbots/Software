0f921572e859afa67b91653be3471cade6235834
Andrew Mao <andrewyxmao@gmail.com>
2024-02-16 23:20:39 -0800

    pr changes

--- src/software/thunderscope/gl/layers/gl_trail_layer.py
+++ src/software/thunderscope/gl/layers/gl_trail_layer.py
@@ -1,6 +1,6 @@
 from pyqtgraph.opengl import *
 
-from collections import deque
+from collections import deque, defaultdict
 from proto.world_pb2 import World
 from proto.import_all_protos import Team, Robot
 
@@ -28,26 +28,22 @@ class GLTrailLayer(GLLayer):
 
         self.world_buffer = ThreadSafeBuffer(buffer_size, World)
         self.trail_graphics_head = ObservableList(self._graphics_changed)
-        self.robot_trail_queues = {}
+        self.robot_trail_queues = defaultdict(lambda: deque([], self.max_trail_length))
         self.cached_world = World()
 
-        self.maxTrailLength = TrailValues.DEFAULT_TRAIL_LENGTH
+        self.max_trail_length = TrailValues.DEFAULT_TRAIL_LENGTH
         self.refresh_interval = TrailValues.DEFAULT_TRAIL_SAMPLING_RATE
         self.refresh_count = self.refresh_interval
 
     def refresh_graphics(self) -> None:
         """Update graphics in this layer"""
 
-        self.cached_world = self.world_buffer.get(block=False)
+        self.cached_world = self.world_buffer.get(block=False, return_cached=False)
 
-        if self.refresh_count <= 0:
-
+        if self.refresh_count <= 0 and self.cached_world is not None:
             self.refresh_count = self.refresh_interval
             for robot in self.cached_world.friendly_team.team_robots:
-                if robot.id in self.robot_trail_queues:
-                    self.__update_trail_points(self.robot_trail_queues[robot.id], robot)
-                else:
-                    self.robot_trail_queues[robot.id] = deque([], self.maxTrailLength)
+                self.__update_trail_points(robot)
 
             self.__update_trail_graphics(
                 self.cached_world.friendly_team,
@@ -57,20 +53,30 @@ class GLTrailLayer(GLLayer):
 
         self.refresh_count -= 1
 
-    def __update_trail_points(self, robot_queue: deque, robot: Robot):
+    def __update_trail_points(self, robot: Robot) -> None:
+        """Stores the robot's current global position in its trail history in the world
 
-        robot_queue.append(robot.current_state.global_position)
+        :param robot: the given robot whose position is to be stored
+        """
+        self.robot_trail_queues[robot.id].append(robot.current_state.global_position)
 
     def __update_trail_graphics(
-        self, team: Team, queues_dict: dict, color: Colors
+            self, team: Team, queues_dict: dict, color: Colors
     ) -> None:
+        """Updates the onscreen visualizations of the past robot positions correlating to the
+           given team.
+
+        :param team: Team proto containing the current friendly team
+        :param queues_dict: dictionary with the stored robot position histories
+        :param color: color to render the trails in
+        """
 
         self.trail_graphics_head.resize(
-            len(team.team_robots), lambda: GLPolygon(outline_color=color,),
+            len(team.team_robots), lambda: GLPolygon(outline_color=color, ),
         )
 
         for trail_graphics_head, trail_queue in zip(
-            self.trail_graphics_head, queues_dict,
+                self.trail_graphics_head, queues_dict,
         ):
             trail_graphics_head.set_points(
                 [[point.x_meters, point.y_meters] for point in queues_dict[trail_queue]]
