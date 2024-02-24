040a00c70a66eb9ff0300af1011c9ad9eeb3589c
Michael Ben-Zvi <michael.benzvi01@gmail.com>
2024-02-24 13:07:59 -0800

    Store World in shared_ptr to Avoid Unnecessary Copying (#3129)
    * move qt resources to software
    
    * updating qt deps
    
    * no need for clang in base setup_software install
    
    * python3.11?
    
    * fixing python 3.11 install
    
    * python install
    
    * use python3.11 bin
    
    * setup_software.sh fix
    
    * support newer eigen code / revert to qt5 base
    
    * working
    
    * remove whitespace
    
    * fix failing jetson nano build test
    
    * Minor unnecessary change
    
    * add 22.04 to docs + updated bazel boost rules git
    
    * forgot to push everything
    
    * downgrade boost to 1.78.0
    
    * downgrade to boost 1.76.0
    
    * revert thunderscope changes
    
    * settle on boost 1.71.0
    
    * cleaned up class data names in world
    
    * Update setup_software.sh
    
    * Update setup_software.sh
    
    * fixed team test
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * retrigger checks
    
    * removed all the copying of world
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Fixed many tests
    
    * Fixed remaining tests
    
    * Fixed failing wrapper test
    
    * Moved setFriendlyRobotPositions calls to test class definition in primitive_test.cpp
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Fixed failing wrapper test for real this time
    
    * Fixed failing test from merge
    
    ---------
    
    Co-authored-by: Arun <arun.sfc@outlook.com>
    Co-authored-by: itsarune <42703774+itsarune@users.noreply.github.com>
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- src/software/ai/hl/stp/play/test_plays/move_test_play.cpp
+++ src/software/ai/hl/stp/play/test_plays/move_test_play.cpp
@@ -5,7 +5,8 @@
 
 MoveTestPlay::MoveTestPlay(TbotsProto::AiConfig config) : Play(config, false) {}
 
-void MoveTestPlay::getNextTactics(TacticCoroutine::push_type &yield, const World &world)
+void MoveTestPlay::getNextTactics(TacticCoroutine::push_type &yield,
+                                  const WorldPtr &world_ptr)
 {
     auto move_test_tactic_friendly_goal = std::make_shared<MoveTactic>();
     auto move_test_tactic_enemy_goal    = std::make_shared<MoveTactic>();
@@ -14,10 +15,10 @@ void MoveTestPlay::getNextTactics(TacticCoroutine::push_type &yield, const World
     do
     {
         move_test_tactic_friendly_goal->updateControlParams(
-            world.field().friendlyGoalCenter(), Angle::zero(), 0,
+            world_ptr->field().friendlyGoalCenter(), Angle::zero(), 0,
             TbotsProto::MaxAllowedSpeedMode::PHYSICAL_LIMIT);
         move_test_tactic_enemy_goal->updateControlParams(
-            world.field().enemyGoalCenter(), Angle::zero(), 0,
+            world_ptr->field().enemyGoalCenter(), Angle::zero(), 0,
             TbotsProto::MaxAllowedSpeedMode::PHYSICAL_LIMIT);
         move_test_tactic_center_field->updateControlParams(
             Point(0, 0), Angle::zero(), 0,
