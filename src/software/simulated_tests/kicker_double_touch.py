e603fdbc34e4708259a1d3fe732dc701554dbcb3
Joey Xiang <joey.xiang426@gmail.com>
2024-03-29 12:22:30 -0700

    requested changes: kicker_double_touch validation geometry

--- src/software/simulated_tests/kicker_double_touch.py
+++ src/software/simulated_tests/kicker_double_touch.py
@@ -14,7 +14,7 @@ class KickerDoubleTouch(Validation):
 
     KICKOFF_DOUBLE_TOUCH_M = 0.05
 
-    def __init__(self, threshold=0.01):
+    def __init__(self, threshold=0.1):
         """
         :param threshold: The distance from the ball to consider a robot touching it
         """
@@ -31,16 +31,15 @@ class KickerDoubleTouch(Validation):
                   PASSING when the robot kicking the ball does not touch the ball again before another robot touches it
         """
         ball_position = tbots_cpp.createPoint(world.ball.current_state.global_position)
+        if self.kick_position is None:
+            self.kick_position = ball_position
         # quick passing check
         if self.other_robot is not None:
             return ValidationStatus.PASSING
         # main check loop
         for robot in world.friendly_team.team_robots:
             if tbots_cpp.Robot(robot).isNearDribbler(ball_position, self.threshold):
-                if self.kicker_robot is None:
-                    self.kicker_robot = robot
-                    self.kick_position = ball_position
-                elif (
+                if (
                     self.kicker_robot == robot
                     and self.other_robot is None
                     and (ball_position - self.kick_position).length()
@@ -59,17 +58,28 @@ class KickerDoubleTouch(Validation):
         :returns: ValidationGeometry containing geometry to visualize
 
         """
+        # assigns closest robot to ball as kicker if not already assigned
+        if self.kicker_robot is None:
+            ball_position = tbots_cpp.createPoint(world.ball.current_state.global_position)
+            dist = 1000
+            for robot in world.friendly_team.team_robots:
+                robot_position = tbots_cpp.Point(
+                    robot.current_state.global_position.x_meters,
+                    robot.current_state.global_position.y_meters,
+                )
+                if (ball_position - robot_position).length() < dist:
+                    dist = (ball_position - robot_position).length()
+                    self.kicker_robot = robot
+
         return create_validation_geometry(
-            # [tbots_cpp.Field.createSSLDivisionBField().fieldLines()]
             [
                 tbots_cpp.Circle(
                     tbots_cpp.Point(
-                        robot.current_state.global_position.x_meters,
-                        robot.current_state.global_position.y_meters,
+                        self.kicker_robot.current_state.global_position.x_meters,
+                        self.kicker_robot.current_state.global_position.y_meters,
                     ),
                     0.5,
                 )
-                for robot in list(world.friendly_team.team_robots)
             ]
         )
 
