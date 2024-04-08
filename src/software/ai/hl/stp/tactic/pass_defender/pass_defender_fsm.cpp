0dd25b02e01de80e8056c181c1a779f4742f4ef4
itsarune <42703774+itsarune@users.noreply.github.com>
2024-04-07 21:33:02 -0700

    Fix build issue on master (#3185)
    * fix build issue
    
    * Update src/software/ai/hl/stp/tactic/pass_defender/pass_defender_fsm.cpp
    
    Co-authored-by: Nima Zareian <nimazareian80@gmail.com>
    
    ---------
    
    Co-authored-by: Nima Zareian <nimazareian80@gmail.com>

--- src/software/ai/hl/stp/tactic/pass_defender/pass_defender_fsm.cpp
+++ src/software/ai/hl/stp/tactic/pass_defender/pass_defender_fsm.cpp
@@ -98,9 +98,10 @@ void PassDefenderFSM::interceptBall(const Update& event)
     // backup by the length of the ball
     Point backup_position =
         robot_position + ball.velocity().normalize(ROBOT_MAX_RADIUS_METERS);
-    event.common.set_primitive(std::make_unique<MovePrimitive>(
+    event.common.set_primitive(std::make_shared<MovePrimitive>(
         event.common.robot, backup_position, face_ball_orientation,
         TbotsProto::MaxAllowedSpeedMode::PHYSICAL_LIMIT,
+        TbotsProto::ObstacleAvoidanceMode::AGGRESSIVE,
         TbotsProto::DribblerMode::MAX_FORCE, TbotsProto::BallCollisionType::ALLOW,
         AutoChipOrKick{AutoChipOrKickMode::OFF, 0}));
 }
