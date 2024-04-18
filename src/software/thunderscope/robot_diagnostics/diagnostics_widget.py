96ecf2ae822fed387ed4c7a8308f09398e8dbfd3
boris <borisv.ca@gmail.com>
2024-04-17 21:51:03 -0700

    refactored logic - added more signals to propagate ui changes, added handlers, simplified some other methods

--- src/software/thunderscope/robot_diagnostics/diagnostics_widget.py
+++ src/software/thunderscope/robot_diagnostics/diagnostics_widget.py
@@ -2,7 +2,7 @@ from pyqtgraph.Qt import QtCore, QtGui
 from pyqtgraph.Qt.QtWidgets import *
 from pyqtgraph.Qt.QtCore import *
 from proto.import_all_protos import *
-from software.logger.logger import createLogger
+from software.logger.logger import create_logger
 
 from software.thunderscope.proto_unix_io import ProtoUnixIO
 from software.thunderscope.robot_diagnostics.chicker_widget import ChickerWidget
@@ -28,21 +28,27 @@ class DiagnosticsWidget(QWidget):
     diagnostics_input_mode_signal = pyqtSignal(ControlMode)
 
     # signal to indicate that controller should be reinitialized
-    reinitialize_controller_signal = pyqtSignal()
+    reinitialize_handheld_device_signal = pyqtSignal()
+
+    # signal to indicate that controller was disconnected
+    handheld_device_connection_status_signal = pyqtSignal(HandheldDeviceConnectionStatus)
 
     def __init__(self, proto_unix_io: ProtoUnixIO) -> None:
         super(DiagnosticsWidget, self).__init__()
 
-        self.logger = createLogger("RobotDiagnostics")
+        self.logger = create_logger("RobotDiagnostics")
 
         self.proto_unix_io = proto_unix_io
 
         # default start with diagnostics input
         self.__control_mode = ControlMode.DIAGNOSTICS
 
+        # default start with disconnected controller
+        self.__handheld_device_status = HandheldDeviceConnectionStatus.DISCONNECTED
+
         # initialize widgets
-        self.controller_status = HandheldDeviceStatusView(
-            self.reinitialize_controller_signal
+        self.handheld_device_status_widget = HandheldDeviceStatusView(
+            self.reinitialize_handheld_device_signal
         )
         self.drive_dribbler_widget = DriveAndDribblerWidget()
         self.chicker_widget = ChickerWidget(proto_unix_io)
@@ -51,47 +57,53 @@ class DiagnosticsWidget(QWidget):
         )
 
         # connect input mode signal with handler
-        self.diagnostics_input_mode_signal.connect(self.__toggle_control_and_refresh)
+        self.diagnostics_input_mode_signal.connect(self.__toggle_control)
 
-        # connect controller reinitialization signal with handler
-        self.reinitialize_controller_signal.connect(self.__reinitialize_controller)
+        # connect handheld device reinitialization signal with handler
+        self.reinitialize_handheld_device_signal.connect(self.__reinitialize_controller)
+
+        # connect handheld device connection status toggle signal with handler
+        self.handheld_device_connection_status_signal.connect(self.__toggle_handheld_device_connection_status)
 
         # initialize controller
-        self.controller_handler = HandheldDeviceManager(proto_unix_io, self.logger)
+        self.controller_handler = HandheldDeviceManager(
+            self.proto_unix_io,  # TODO: proto shouldn't be passed down
+            self.logger,
+            self.handheld_device_connection_status_signal
+        )
 
         # layout for the entire diagnostics tab
         vbox_layout = QVBoxLayout()
 
-        vbox_layout.addWidget(self.controller_status)
+        vbox_layout.addWidget(self.handheld_device_status_widget)
         vbox_layout.addWidget(self.diagnostics_control_input_widget)
         vbox_layout.addWidget(self.drive_dribbler_widget)
         vbox_layout.addWidget(self.chicker_widget)
 
         self.setLayout(vbox_layout)
 
-    def __toggle_control_and_refresh(self, mode: ControlMode):
+    def __toggle_control(self, mode: ControlMode):
         self.__control_mode = mode
+        self.controller_handler.refresh(self.__control_mode)
 
-        self.drive_dribbler_widget.refresh(mode)
-        self.chicker_widget.refresh(mode)
-        self.controller_handler.refresh(mode)
+    def __toggle_handheld_device_connection_status(self, status: HandheldDeviceConnectionStatus):
+        self.__handheld_device_status = status
 
     def refresh(self):
         """
         Refreshes sub-widgets so that they display the most recent values, as well as
         the most recent values are available for use.
         """
-        # get the most recent state on whether controller is connected from controller handler
-        controller_status = self.controller_handler.get_controller_connection_status()
 
         # update controller status view with most recent controller status
-        self.controller_status.refresh(controller_status)
+        self.handheld_device_status_widget.refresh(self.__handheld_device_status)
 
         # update diagnostic ui with most recent recent controller status
-        self.diagnostics_control_input_widget.refresh(controller_status)
+        self.diagnostics_control_input_widget.refresh(self.__handheld_device_status)
 
-        if controller_status == HandheldDeviceConnectionStatus.DISCONNECTED:
-            self.__toggle_control_and_refresh(ControlMode.DIAGNOSTICS)
+        # update all widgets based on current control mode
+        self.drive_dribbler_widget.refresh(self.__control_mode)
+        self.chicker_widget.refresh(self.__control_mode)
 
         if self.__control_mode == ControlMode.DIAGNOSTICS:
             diagnostics_primitive = Primitive(
@@ -100,32 +112,38 @@ class DiagnosticsWidget(QWidget):
                     power_control=self.chicker_widget.power_control,
                 )
             )
-
+            # self.logger.debug(self.drive_dribbler_widget.motor_control)
             self.proto_unix_io.send_proto(Primitive, diagnostics_primitive)
 
         elif self.__control_mode == ControlMode.HANDHELD:
-            # diagnostics_primitive = (
-            #     self.controller_handler.get_latest_primitive_controls()
-            # )
-
-            self.drive_dribbler_widget.set_slider(
-                SliderType.XVelocitySlider, self.controller_handler.move_x
-            )
-            self.drive_dribbler_widget.set_slider(
-                SliderType.YVelocitySlider, self.controller_handler.move_y
-            )
-            self.drive_dribbler_widget.set_slider(
-                SliderType.DribblerVelocitySlider, self.controller_handler.ang_vel
+            # get latest diagnostics primitive
+            diagnostics_primitive = (
+                self.controller_handler.get_latest_primitive_controls()
             )
 
-            # if diagnostics_primitive is not None:
-            #     self.proto_unix_io.send_proto(Primitive, diagnostics_primitive)
-            # else:
-            #     self.proto_unix_io.send_proto(Primitive, StopPrimitive())
+            # send it
+
+            # update ui visually
+            # TODO update dribbler speed and kick power, flash kick and chip buttons
+            self.drive_dribbler_widget.set_x_velocity_slider(
+                diagnostics_primitive.direct_control.motor_control.direct_velocity_control.velocity.x_component_meters
+            )
+            self.drive_dribbler_widget.set_y_velocity_slider(
+                diagnostics_primitive.direct_control.motor_control.direct_velocity_control.velocity.y_component_meters
+            )
+            self.drive_dribbler_widget.set_angular_velocity_slider(
+                diagnostics_primitive.direct_control.motor_control.direct_velocity_control.angular_velocity.
+                radians_per_second
+            )
+
+            self.drive_dribbler_widget.set_dribbler_velocity_slider(
+                diagnostics_primitive.direct_control.motor_control.dribbler_speed_rpm
+            )
+
+            self.proto_unix_io.send_proto(Primitive, diagnostics_primitive)
 
     def __reinitialize_controller(self) -> None:
-        self.controller_handler.clear_controller()
-        self.controller_handler.initialize_controller()
+        self.controller_handler.reinitialize_controller()
 
     # TODO: investigate why these methods aren't called on user hitting close button
     def closeEvent(self, event):
