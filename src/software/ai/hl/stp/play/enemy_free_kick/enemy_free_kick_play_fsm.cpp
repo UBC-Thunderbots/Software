a1774e5e395e5cc7cb19f710b07bd8cc7b9ab936
Andrew Mao <andrewyxmao@gmail.com>
2024-05-29 23:44:13 -0700

    refactored

--- src/software/ai/hl/stp/play/enemy_free_kick/enemy_free_kick_play_fsm.cpp
+++ src/software/ai/hl/stp/play/enemy_free_kick/enemy_free_kick_play_fsm.cpp
@@ -11,7 +11,7 @@
 #include "software/util/generic_factory/generic_factory.h"
 
 EnemyFreeKickPlayFSM::EnemyFreeKickPlayFSM(TbotsProto::AiConfig ai_config)
-    : ai_config(ai_config), crease_defenders({}), pass_defenders({})
+        : DefensePlayBase::DefensePlayBase(ai_config)
 {
 }
 
@@ -129,52 +129,11 @@ void EnemyFreeKickPlayFSM::setTactics(const Update& event, unsigned int num_tact
 
     // Reset tactics if the number of crease defenders or pass defenders
     // we intend to assign has changed
-    setUpCreaseDefenders(static_cast<unsigned int>(crease_defender_assignments.size()));
-    setUpPassDefenders(static_cast<unsigned int>(pass_defender_assignments.size()));
+    DefensePlayBase::setUpCreaseDefenders(static_cast<unsigned int>(crease_defender_assignments.size()));
+    DefensePlayBase::setUpPassDefenders(static_cast<unsigned int>(pass_defender_assignments.size()));
 
-    for (unsigned int i = 0; i < crease_defenders.size(); i++)
-    {
-        auto target = crease_defender_assignments.at(i).target;
-
-        // Determine the number of crease defenders already assigned to the target
-        auto defenders_with_target_count = std::count_if(
-            crease_defender_assignments.begin(), crease_defender_assignments.begin() + i,
-            [&target](const auto& assignment) { return assignment.target == target; });
-
-        // Pick alignment based on how many crease defenders are already assigned to the
-        // target
-        auto alignment = TbotsProto::CreaseDefenderAlignment::CENTRE;
-        if (defenders_with_target_count == 1)
-        {
-            if (event.common.world_ptr->ball().position().y() > 0)
-            {
-                alignment = TbotsProto::CreaseDefenderAlignment::LEFT;
-            }
-            else
-            {
-                alignment = TbotsProto::CreaseDefenderAlignment::RIGHT;
-            }
-        }
-        else if (defenders_with_target_count == 2)
-        {
-            if (event.common.world_ptr->ball().position().y() > 0)
-            {
-                alignment = TbotsProto::CreaseDefenderAlignment::RIGHT;
-            }
-            else
-            {
-                alignment = TbotsProto::CreaseDefenderAlignment::LEFT;
-            }
-        }
-
-        crease_defenders.at(i)->updateControlParams(
-            target, alignment, event.control_params.max_allowed_speed_mode);
-    }
-
-    for (unsigned int i = 0; i < pass_defenders.size(); i++)
-    {
-        pass_defenders.at(i)->updateControlParams(pass_defender_assignments.at(i).target);
-    }
+    DefensePlayBase::setAlignment(event, crease_defender_assignments);
+    DefensePlayBase::updatePassDefenderControlParams(pass_defender_assignments);
 
 
     tactics_to_return[1].insert(tactics_to_return[1].end(), crease_defenders.begin(),
@@ -184,29 +143,3 @@ void EnemyFreeKickPlayFSM::setTactics(const Update& event, unsigned int num_tact
     event.common.set_tactics(tactics_to_return);
 }
 
-void EnemyFreeKickPlayFSM::setUpCreaseDefenders(unsigned int num_crease_defenders)
-{
-    if (num_crease_defenders == crease_defenders.size())
-    {
-        return;
-    }
-
-    crease_defenders =
-        std::vector<std::shared_ptr<CreaseDefenderTactic>>(num_crease_defenders);
-    std::generate(crease_defenders.begin(), crease_defenders.end(), [this]() {
-        return std::make_shared<CreaseDefenderTactic>(
-            ai_config.robot_navigation_obstacle_config());
-    });
-}
-
-void EnemyFreeKickPlayFSM::setUpPassDefenders(unsigned int num_pass_defenders)
-{
-    if (num_pass_defenders == pass_defenders.size())
-    {
-        return;
-    }
-
-    pass_defenders = std::vector<std::shared_ptr<PassDefenderTactic>>(num_pass_defenders);
-    std::generate(pass_defenders.begin(), pass_defenders.end(),
-                  [this]() { return std::make_shared<PassDefenderTactic>(); });
-}
