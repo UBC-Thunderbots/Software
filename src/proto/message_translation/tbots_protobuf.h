076df417bbebf69dd2a53b086501516ffd669ccf
Nima Zareian <nimazareian80@gmail.com>
2024-06-15 14:14:02 -0700

    Various Thunderscope visualization improvements (#3198)
    * Add attacker tactic visualizations, thunderscope sim speed control, update debug shapes layer
    
    * Add function docs
    
    * Move attacker tactic visualization to attacker_tactic.cpp
    
    * address comments
    
    * Add new autokick/chip graphic and address comment
    
    * Remove extra TODO
    
    * fix cpp test viz
    
    * update debug shapes

--- src/proto/message_translation/tbots_protobuf.h
+++ src/proto/message_translation/tbots_protobuf.h
@@ -127,23 +127,47 @@ std::unique_ptr<TbotsProto::PlotJugglerValue> createPlotJugglerValue(
     const std::map<std::string, double>& values);
 
 /**
- * Returns a TbotsProto::DebugShapesMap proto containing the name
- * shape pairs.
+ * Returns a TbotsProto::DebugShapes::DebugShape proto for a given shape,
+ * unique id, and an optional debug text.
+ *
+ * @tparam Shape A shape which has a matching createShapeProto function
+ *
+ * @param shape The shape to create the protobuf for
+ * @param unique_id A unique string used to differentiate this shape
+ * from others. Note that this ID should remain the same for the same shape
+ * to avoid it from being plotted multiple times.
+ * @param debug_text An optional string to display on the shape
+ *
+ * @return The unique_ptr to a TbotsProto::DebugShapes::DebugShape proto
+ */
+template <class Shape>
+std::unique_ptr<TbotsProto::DebugShapes::DebugShape> createDebugShape(
+    const Shape& shape, const std::string& unique_id, const std::string& debug_text = "")
+{
+    auto debug_shape = std::make_unique<TbotsProto::DebugShapes::DebugShape>();
+    (*debug_shape->mutable_shape()) = *createShapeProto(shape);
+    debug_shape->set_unique_id(unique_id);
+    debug_shape->set_debug_text(debug_text);
+    return debug_shape;
+};
+
+/**
+ * Returns a TbotsProto::DebugShapes proto containing the debug shapes
  *
  * Could use LOG(VISUALIZE) to plot these values. Example:
- *  LOG(VISUALIZE) << *createDebugShapesMap({
- *      {"circle_name", *createShapeProto(circle_object)},
- *      {"stadium_name", *createShapeProto(stadium_object)},
- *      {"polygon_name", *createShapeProto(polygon_object)}
+ *  LOG(VISUALIZE) << *createDebugShapes({
+ *       *createDebugShape(circle, unique_id1, optional_text),
+ *       *createDebugShape(polygon, unique_id2, optional_text),
+ *       *createDebugShape(stadium, unique_id3, optional_text)
  *  });
  *
- * @param named_shapes The map of name shape proto pairs to plot
+ * @param debug_shapes A list of debug shapes proto to plot
  *
- * @return The unique_ptr to a TbotsProto::DebugShapesMap proto containing data with
+ * @return The unique_ptr to a TbotsProto::DebugShapes proto containing data with
  *        specified names and shapes
  */
-std::unique_ptr<TbotsProto::DebugShapesMap> createDebugShapesMap(
-    const std::map<std::string, TbotsProto::Shape>& named_shapes);
+std::unique_ptr<TbotsProto::DebugShapes> createDebugShapes(
+    const std::vector<TbotsProto::DebugShapes::DebugShape>& debug_shapes);
 
 /**
  * Returns a TbotsProto::Shape proto given a shape.
