34ef99096d3421bac9bb8f94df4f888902387edb
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2024-06-26 15:27:10 +0800

    made some changes to the replay_file_debugging_scripts

--- src/software/thunderscope/replay/replay_file_debugging_script.py
+++ src/software/thunderscope/replay/replay_file_debugging_script.py
@@ -1,3 +1,4 @@
+import traceback
 import base64
 import glob
 import gzip
@@ -7,10 +8,60 @@ import argparse
 from proto.import_all_protos import *
 from software.thunderscope.replay.replay_constants import *
 
+
+def read_one_chunk(replay_file_name):
+    """
+    read one chunk of the replay file:
+
+    :param replay_file_name: the name of the replay file that is going to be read
+    :return: the number of lines that have been read
+    """
+
+    line_num = 0
+    with gzip.open(replay_file_name, "rb") as replay_file:
+        line = replay_file.readline()
+
+        for line in replay_file.readlines():
+            # do not parse empty line
+            if line == b"":
+                continue
+
+            try:
+                timestamp, protobuf_type, data = line.split(
+                    bytes(REPLAY_METADATA_DELIMETER, encoding="utf-8")
+                )
+            except Exception as e:
+                print("Exception ignored. Please see below for more!")
+                print(e)
+                continue
+
+            try:
+                # The format of the protobuf type is:
+                # package.proto_class (e.g. TbotsProto.Primitive)
+                proto_class = eval(str(protobuf_type.split(b".")[-1], encoding="utf-8"))
+            except Exception as e:
+                print("Error ignored with Exception")
+                print(e)
+                continue
+
+            # Deserialize protobuf
+            proto = proto_class.FromString(
+                base64.b64decode(data[len("b") : -len("\n")])
+            )
+
+            #######################################
+            # Do something with the protobuf here #
+            #######################################
+            print(f"{line_num}: {float(timestamp)}: {protobuf_type} - {proto=}")
+
+            line = replay_file.readline()
+            line_num += 1
+
+    return line_num
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Script for debugging replay files"
-    )
+    parser = argparse.ArgumentParser(description="Script for debugging replay files")
     parser.add_argument(
         "--replay_dir",
         action="store",
@@ -29,47 +80,17 @@ if __name__ == "__main__":
         replay_index, _ = tail.split(".")
         return int(replay_index)
 
-
     sorted_chunks = sorted(replay_files, key=__sort_replay_chunks)
 
     print(f"Found {len(sorted_chunks)} replay files in {args.replay_dir}")
 
     total_line_num = 0
     for replay_file_name in sorted_chunks:
-        print(f"Replaying {replay_file_name}...")
-        with gzip.open(replay_file_name, "rb") as replay_file:
-            line = replay_file.readline()
-            line_num = 0
-            while line is not None:
-                # do not parse empty line
-                if line == b"":
-                    continue 
+        try:
+            total_line_num += read_one_chunk(replay_file_name)
+        except Exception as e:
+            print(f"Cannot read {replay_file_name} wiht exception")
+            print("Please note that the following traceback is ignored. The program is still running")
+            print(traceback.format_exc())
 
-                try:
-                    timestamp, protobuf_type, data = line.split(
-                        bytes(REPLAY_METADATA_DELIMETER, encoding="utf-8")
-                    )
-                except ValueError:
-                    raise ValueError(f"Unable to split line {line_num} of replay file {replay_file_name}. Line: {line}")
-
-                try:
-                    # The format of the protobuf type is:
-                    # package.proto_class (e.g. TbotsProto.Primitive)
-                    proto_class = eval(str(protobuf_type.split(b".")[-1], encoding="utf-8"))
-                except NameError:
-                    raise TypeError(f"Unknown proto type in replay: '{protobuf_type}'")
-
-                # Deserialize protobuf
-                proto = proto_class.FromString(base64.b64decode(data[len("b"): -len("\n")]))
-
-                #######################################
-                # Do something with the protobuf here #
-                #######################################
-                print(f"{line_num}: {float(timestamp)}: {protobuf_type} - {proto=}")
-
-                line = replay_file.readline()
-                line_num += 1
-
-            total_line_num += line_num
-
-    print(f"Replayed {total_line_num} log lines and found no errors.")
+    print(f"Replayed {total_line_num} log lines")
