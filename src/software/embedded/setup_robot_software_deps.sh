d2048327af5bc778a0f60eb42994cf2e5b738fea
arun <arun.sfc@outlook.com>
2024-07-02 00:38:34 -0700

    partial refactor

--- src/software/embedded/setup_robot_software_deps.sh
+++ src/software/embedded/setup_robot_software_deps.sh
@@ -4,13 +4,10 @@ set -ex
 
 host_software_packages=(
     redis
-    libjpeg8-dev
-    libjpeg-dev
     device-tree-compiler
     python3.8      # Python 3
     python3.8-venv # Virtual Environment
     python3.8-dev  # Python 3 Headers
-    zlib1g-dev
     curl
 )
 
@@ -29,13 +26,6 @@ sudo /opt/tbotspython/bin/python3 -m pip install --upgrade pip
 
 pip_libaries=(
     setuptools==60.5.0
-    Adafruit-Blinka==6.15.0
-    adafruit-circuitpython-busdevice==5.1.1
-    adafruit-circuitpython-rgb-display==3.10.9
-    Adafruit-PlatformDetect==3.18.0
-    Adafruit-PureIO==1.1.9
-    Jetson.GPIO==2.0.17
-    Pillow==9.1.0
     redis==4.1.4
     platformio==6.0.2
 )
@@ -45,4 +35,3 @@ sudo /opt/tbotspython/bin/pip3.8 install "${pip_libaries[@]}"
 
 # Install platformio udev rules
 curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/develop/platformio/assets/system/99-platformio-udev.rules | sudo tee /etc/udev/rules.d/99-platformio-udev.rules
-
