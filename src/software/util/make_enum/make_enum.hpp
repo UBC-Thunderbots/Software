f06776e992e377155ea7fafd5298f9875429723d
williamckha <williamha@outlook.com>
2024-07-03 09:01:06 -0700

    Implement reflective enum

---
+++ src/software/util/make_enum/make_enum.hpp
@@ -0,0 +1,142 @@
+#pragma once
+
+#include "software/util/make_enum/make_enum_helpers.hpp"
+
+/**
+ * Creates a "smart" enum with compile-time, template-friendly reflection.
+ *
+ * This enum will have support for:
+ * 1. Getting the number of enum values (reflective_enum::size)
+ * 2. Getting the string representation of each enum value (reflective_enum::valueNames)
+ * 3. Getting a list of all enum values (reflective_enum::values)
+ * 4. Getting an enum value from its string representation (reflective_enum::fromName)
+ *
+ * IMPORTANT: This macro does NOT support enums that have manually specified values.
+ * e.g. MAKE_ENUM(MyEnum, foo=1, bar) is not valid
+ *
+ * @param name The name of the enum
+ * @param ... The values of the enum
+ */
+#define MAKE_ENUM(name, ...)                                                               \
+    constexpr std::string_view _enum_string_args_##name = #__VA_ARGS__;                    \
+    static_assert(                                                                         \
+        isEnumArgsValid(_enum_string_args_##name),                                         \
+        "Error: Enums created with the MAKE_ENUM macro may not manually specify enum "     \
+        "values. Please remove any enum assignments with the '=' operator");               \
+    enum class name                                                                      \
+    {                                                                                    \
+        __VA_ARGS__                                                                      \
+        /* Although common convention, we don't use a COUNT value as the last item in */ \
+        /* our enum in order to figure out how many values the enum contains. The */     \
+        /* reason for this is that the compiler includes helpful warnings like */        \
+        /* "enum value not included in switch" to help avoid missing a case in a */      \
+        /* switch statement. Adding an extra COUNT value that will not be handled in */  \
+        /* these cases means we will get warnings (or errors). See this stackoverflow */ \
+        /* answer for a similar explanation: https://stackoverflow.com/a/2102673 */      \
+    }; \
+    constexpr size_t _enum_size_##name = countSubstrings(_enum_string_args_##name, ',');   \
+    constexpr auto _enum_values_##name = getEnumValues<name, _enum_size_##name>();         \
+    constexpr auto _enum_value_names_##name = splitString(                                 \
+        _enum_string_args_##name, ',', std::make_index_sequence<_enum_size_##name>());     \
+    template <>                                                                            \
+    struct reflective_enum::is_reflective_enum<name> : std::true_type                      \
+    {                                                                                      \
+    };                                                                                     \
+    template <>                                                                            \
+    constexpr size_t reflective_enum::size<name>()                                         \
+    {                                                                                      \
+        return _enum_size_##name;                                                          \
+    }                                                                                      \
+    template <>                                                                            \
+    constexpr auto reflective_enum::values<name>()                                         \
+    {                                                                                      \
+        return _enum_values_##name;                                                        \
+    }                                                                                      \
+    template <>                                                                            \
+    constexpr auto reflective_enum::valueNames<name>()                                     \
+    {                                                                                      \
+        return _enum_value_names_##name;                                                   \
+    }
+
+namespace reflective_enum
+{
+    /**
+     * Type trait that checks whether T is a reflective enum, i.e. whether it
+     * was created using the MAKE_ENUM macro.
+     */
+    template <typename T>
+    struct is_reflective_enum : std::false_type
+    {
+    };
+
+    /**
+     * Concept that is satisfied by any type E that is a reflective enum.
+     */
+    template <typename E>
+    concept ReflectiveEnum = is_reflective_enum<E>::value;
+
+    /**
+     * Returns the number of values in the reflective enum E.
+     *
+     * @return the number of values in the enum
+     */
+    template <ReflectiveEnum E>
+    constexpr size_t size();
+
+    /**
+     * Returns an array containing the values of the reflective enum E.
+     *
+     * @return an array with the values of the enum
+     */
+    template <ReflectiveEnum E>
+    constexpr auto values();
+
+    /**
+     * Returns an array contain the string representations of each value
+     * in the reflective enum E.
+     *
+     * @return an array with the names of the values in the enum
+     */
+    template <ReflectiveEnum E>
+    constexpr auto valueNames();
+
+    /**
+     * Returns the enum value with the given string representation.
+     *
+     * @param value_name the string representation of the enum value
+     *
+     * @return the enum value
+     */
+    template <ReflectiveEnum E>
+    E fromName(const std::string value_name)
+    {
+        constexpr size_t enum_size      = size<E>();
+        constexpr auto enum_value_names = valueNames<E>();
+        for (size_t i = 0; i < enum_size; ++i)
+        {
+            if (enum_value_names.at(i) == value_name)
+            {
+                return static_cast<E>(i);
+            }
+        }
+        throw std::invalid_argument(value_name + " cannot be converted to enum value");
+    }
+
+    /**
+     * Implements the << operator for printing.
+     *
+     * @param os the output stream to print to
+     * @param value the enum value to print
+     *
+     * @return the output stream with the string representation of the enum value appended
+     */
+    template <ReflectiveEnum E>
+    std::ostream& operator<<(std::ostream& os, E value)
+    {
+        // This index lookup relies on the assumption that the enum does not manually
+        // specify any values. If it did, the underlying integer of the given value
+        // may be out of range of the vector of strings
+        os << valueNames<E>().at(static_cast<int>(value));
+        return os;
+    }
+}  // namespace reflective_enum
--- src/software/util/make_enum/make_enum_test.cpp
+++ src/software/util/make_enum/make_enum_test.cpp
@@ -1,71 +1,94 @@
-#include "software/util/make_enum/make_enum.h"
+#include "software/util/make_enum/make_enum.hpp"
 
 #include <gtest/gtest.h>
 
-TEST(EnumMacroTest, validate_valid_enum_args_without_trailing_comma)
+TEST(ReflectiveEnumTest, validate_valid_enum_args_without_trailing_comma)
 {
     std::string enum_string = "foo, bar, baz";
     bool result             = isEnumArgsValid(enum_string);
     EXPECT_TRUE(result);
 }
 
-TEST(EnumMacroTest, validate_valid_enum_args_with_trailing_comma)
+TEST(ReflectiveEnumTest, validate_valid_enum_args_with_trailing_comma)
 {
     std::string enum_string = "foo, bar, baz,";
     bool result             = isEnumArgsValid(enum_string);
     EXPECT_TRUE(result);
 }
 
-TEST(EnumMacroTest, validate_invalid_enum_args)
+TEST(ReflectiveEnumTest, validate_invalid_enum_args)
 {
     std::string enum_string = "foo, bar=3, baz,";
     bool result             = isEnumArgsValid(enum_string);
     EXPECT_FALSE(result);
 }
 
-TEST(EnumMacroTest, separate_enum_strings_empty_string)
+TEST(ReflectiveEnumTest, separate_enum_strings_empty_string)
 {
-    std::string enum_string = "";
-    auto result             = separateEnumStrings(enum_string);
+    constexpr std::string_view enum_string = "";
+
+    constexpr size_t enum_size = countSubstrings(enum_string, ',');
+    constexpr auto result =
+        splitString(enum_string, ',', std::make_index_sequence<enum_size>());
+
     EXPECT_TRUE(result.empty());
 }
 
-TEST(EnumMacroTest, separate_enum_strings_without_trailing_comma)
+TEST(ReflectiveEnumTest, separate_enum_strings_without_trailing_comma)
 {
-    std::string enum_string                  = "foo, bar, baz";
-    auto result                              = separateEnumStrings(enum_string);
-    std::vector<std::string> expected_result = {"foo", "bar", "baz"};
+    constexpr std::string_view enum_string = "foo, bar, baz";
+
+    constexpr size_t enum_size = countSubstrings(enum_string, ',');
+    constexpr auto result =
+        splitString(enum_string, ',', std::make_index_sequence<enum_size>());
+
+    constexpr std::array<std::string_view, 3> expected_result = {"foo", "bar", "baz"};
     EXPECT_EQ(expected_result, result);
 }
 
-TEST(EnumMacroTest, separate_enum_strings_with_extra_spaces_tabs_and_newlines)
+TEST(ReflectiveEnumTest, separate_enum_strings_with_extra_spaces_tabs_and_newlines)
 {
-    std::string enum_string                  = "foo   \n, \tbar, \nbaz \t ";
-    auto result                              = separateEnumStrings(enum_string);
-    std::vector<std::string> expected_result = {"foo", "bar", "baz"};
+    constexpr std::string_view enum_string = "foo   \n, \tbar, \nbaz \t ";
+
+    constexpr size_t enum_size = countSubstrings(enum_string, ',');
+    constexpr auto result =
+        splitString(enum_string, ',', std::make_index_sequence<enum_size>());
+
+    constexpr std::array<std::string_view, 3> expected_result = {"foo", "bar", "baz"};
     EXPECT_EQ(expected_result, result);
 }
 
-TEST(EnumMacroTest, separate_enum_strings_with_trailing_comma)
+TEST(ReflectiveEnumTest, separate_enum_strings_with_trailing_comma)
 {
-    std::string enum_string                  = "foo, bar, baz,";
-    auto result                              = separateEnumStrings(enum_string);
-    std::vector<std::string> expected_result = {"foo", "bar", "baz"};
+    constexpr std::string_view enum_string = "foo, bar, baz,";
+
+    constexpr size_t enum_size = countSubstrings(enum_string, ',');
+    constexpr auto result =
+        splitString(enum_string, ',', std::make_index_sequence<enum_size>());
+
+    constexpr std::array<std::string_view, 3> expected_result = {"foo", "bar", "baz"};
     EXPECT_EQ(expected_result, result);
 }
 
-MAKE_ENUM(TestEnum, Var1, Var2)
-TEST(EnumMacroTest, test_macro_generated_functions)
+MAKE_ENUM(FooEnum, FOO, BAR, BAZ)
+
+TEST(ReflectiveEnumTest, test_reflective_enum_functions)
 {
-    EXPECT_EQ(2, sizeTestEnum());
-    EXPECT_EQ(std::vector<TestEnum>({TestEnum::Var1, TestEnum::Var2}),
-              allValuesTestEnum());
+    EXPECT_TRUE(reflective_enum::is_reflective_enum<FooEnum>::value);
+
+    EXPECT_EQ(reflective_enum::size<FooEnum>(), 3);
+
+    std::array<FooEnum, 3> foo_values = {FooEnum::FOO, FooEnum::BAR, FooEnum::BAZ};
+    EXPECT_EQ(reflective_enum::values<FooEnum>(), foo_values);
+
+    std::array<std::string_view, 3> foo_value_names = {"FOO", "BAR", "BAZ"};
+    EXPECT_EQ(reflective_enum::valueNames<FooEnum>(), foo_value_names);
 }
 
-TEST(EnumMacroTest, test_macro_from_string)
+TEST(ReflectiveEnumTest, get_reflective_enum_values_from_names)
 {
-    EXPECT_EQ(2, sizeTestEnum());
-    EXPECT_EQ(TestEnum::Var1, fromStringToTestEnum("Var1"));
-    EXPECT_EQ(TestEnum::Var2, fromStringToTestEnum("Var2"));
-    EXPECT_THROW(fromStringToTestEnum("Var3"), std::invalid_argument);
+    EXPECT_EQ(reflective_enum::fromName<FooEnum>("FOO"), FooEnum::FOO);
+    EXPECT_EQ(reflective_enum::fromName<FooEnum>("BAR"), FooEnum::BAR);
+    EXPECT_EQ(reflective_enum::fromName<FooEnum>("BAZ"), FooEnum::BAZ);
+    EXPECT_THROW(reflective_enum::fromName<FooEnum>("QUX"), std::invalid_argument);
 }
