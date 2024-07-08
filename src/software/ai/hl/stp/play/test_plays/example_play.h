526b7d27d50804647a4dd783d7cf29e63d0a2803
williamckha <williamha@outlook.com>
2024-07-08 00:36:20 -0700

    Revive ExamplePlay

---
+++ src/software/ai/hl/stp/play/test_plays/example_play.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "proto/parameters.pb.h"
+#include "software/ai/hl/stp/play/play.h"
+
+/**
+ * An example Play that moves the robots in a circle around the ball
+ */
+class ExamplePlay : public Play
+{
+   public:
+    explicit ExamplePlay(std::shared_ptr<Strategy> strategy);
+
+    void getNextTactics(TacticCoroutine::push_type &yield,
+                        const WorldPtr &world_ptr) override;
+};
