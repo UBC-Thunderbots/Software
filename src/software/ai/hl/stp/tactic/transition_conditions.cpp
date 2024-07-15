ee33fe83ec54ebe79e5ea90161590dcbc8a54fdc
Nima Zareian <nimazareian80@gmail.com>
2024-07-15 10:06:27 +0200

    Merge #2953 - Fix Freekick

--- src/software/ai/hl/stp/tactic/transition_conditions.cpp
+++ src/software/ai/hl/stp/tactic/transition_conditions.cpp
@@ -1,5 +1,7 @@
 #include "software/ai/hl/stp/tactic/transition_conditions.h"
 
+#include "software/geom/algorithms/contains.h"
+
 bool robotReachedDestination(const Robot& robot, const Point& destination,
                              const Angle& final_orientation, double DISTANCE_THRESHOLD,
                              const Angle& ANGLE_THRESHOLD)
@@ -21,4 +23,41 @@ bool comparePoints(const Point& pt1, const Point& pt2, double DISTANCE_THRESHOLD
 bool compareAngles(const Angle& angle1, const Angle& angle2, const Angle& ANGLE_THRESHOLD)
 {
     return angle1.minDiff(angle2) <= ANGLE_THRESHOLD;
+}
+
+bool isRobotReadyToChick(const Robot& robot, const Point& ball_position,
+                         const Angle& chick_direction, const Angle& ANGLE_THRESHOLD)
+{
+    /*
+     * Region behind ball where the robot should be to chick the ball.
+     * Diagram not to scale!
+     *    ┌─────────────┐
+     *    │             │
+     *    │             │
+     *    │             │ behind_ball_region
+     *    │             │
+     *    │             │
+     *    └──────┬──────┘
+     *           ╎
+     *          ─┼─ ball_position
+     *           ╎
+     *           ▼
+     *    chick_direction
+     */
+    Vector behind_ball_direction =
+        Vector::createFromAngle(chick_direction + Angle::half());
+    Segment behind_ball_segment(
+        ball_position + behind_ball_direction.normalize(DIST_TO_FRONT_OF_ROBOT_METERS),
+        ball_position + behind_ball_direction.normalize(2.5 * ROBOT_MAX_RADIUS_METERS));
+
+    // The width of the kicker/chipper that the ball can be within to not hit the
+    // dribbler's side panels
+    double chicker_width =
+        robot.robotConstants().dribbler_width_meters - 2 * BALL_MAX_RADIUS_METERS;
+
+    Polygon behind_ball_region =
+        Polygon::fromSegment(behind_ball_segment, 0.0, chicker_width / 2);
+
+    return contains(behind_ball_region, robot.position()) &&
+           compareAngles(robot.orientation(), chick_direction, ANGLE_THRESHOLD);
 }
