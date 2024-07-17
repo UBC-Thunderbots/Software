58e9156097c204225810717c2fc2a1e0f2efae9c
Andrew Mao <andrewyxmao@gmail.com>
2024-07-18 01:04:29 +0200

    tests fixed

--- src/proto/message_translation/power_frame_msg_test.cpp
+++ src/proto/message_translation/power_frame_msg_test.cpp
@@ -7,13 +7,13 @@ TEST(PowerFrameMsgTest, google_to_nanopb)
 {
     auto google_control = TbotsProto::PowerControl();
     google_control.mutable_chicker()->set_chip_distance_meters(3);
-    auto nanopb_control = createNanoPbPowerPulseControl(google_control, 0, 0, 100);
+    auto nanopb_control = createNanoPbPowerPulseControl(google_control, 0, 0, 0, 100);
     EXPECT_EQ(nanopb_control.chicker.which_chicker_command,
               TbotsProto_PowerPulseControl_ChickerControl_chip_pulse_width_tag);
     EXPECT_EQ(nanopb_control.chicker.chicker_command.chip_pulse_width, 100);
 
     google_control.mutable_chicker()->set_kick_speed_m_per_s(5);
-    nanopb_control = createNanoPbPowerPulseControl(google_control, 0.3, 300, 0);
+    nanopb_control = createNanoPbPowerPulseControl(google_control, 0.3, 300, 0, 0);
     EXPECT_EQ(nanopb_control.chicker.which_chicker_command,
               TbotsProto_PowerPulseControl_ChickerControl_kick_pulse_width_tag);
     EXPECT_EQ(nanopb_control.chicker.chicker_command.kick_pulse_width,
@@ -22,7 +22,7 @@ TEST(PowerFrameMsgTest, google_to_nanopb)
     google_control.mutable_chicker()
         ->mutable_auto_chip_or_kick()
         ->set_autokick_speed_m_per_s(4);
-    nanopb_control = createNanoPbPowerPulseControl(google_control, 0.1, 3, 0);
+    nanopb_control = createNanoPbPowerPulseControl(google_control, 0.1, 3, 0, 0);
     EXPECT_EQ(nanopb_control.chicker.which_chicker_command,
               TbotsProto_PowerPulseControl_ChickerControl_auto_chip_or_kick_tag);
     EXPECT_EQ(
@@ -35,7 +35,7 @@ TEST(PowerFrameMsgTest, google_to_nanopb)
     google_control.mutable_chicker()
         ->mutable_auto_chip_or_kick()
         ->set_autokick_speed_m_per_s(6);
-    nanopb_control = createNanoPbPowerPulseControl(google_control, 10000, 10000, 0);
+    nanopb_control = createNanoPbPowerPulseControl(google_control, 10000, 10000, 0, 0);
     EXPECT_EQ(nanopb_control.chicker.which_chicker_command,
               TbotsProto_PowerPulseControl_ChickerControl_auto_chip_or_kick_tag);
     EXPECT_EQ(
@@ -48,7 +48,7 @@ TEST(PowerFrameMsgTest, google_to_nanopb)
     google_control.mutable_chicker()
         ->mutable_auto_chip_or_kick()
         ->set_autochip_distance_meters(2);
-    nanopb_control = createNanoPbPowerPulseControl(google_control, 0, 0, 200);
+    nanopb_control = createNanoPbPowerPulseControl(google_control, 0, 0, 0, 200);
     EXPECT_EQ(nanopb_control.chicker.which_chicker_command,
               TbotsProto_PowerPulseControl_ChickerControl_auto_chip_or_kick_tag);
     EXPECT_EQ(
