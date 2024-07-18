d26fb24a759253d30e097f064917ac024d1bf3ad
williamckha <williamha@outlook.com>
2024-07-18 18:07:06 +0200

    Kickoff enemy play changes

--- src/software/ai/hl/stp/play/kickoff_enemy_play.cpp
+++ src/software/ai/hl/stp/play/kickoff_enemy_play.cpp
@@ -52,10 +52,10 @@ void KickoffEnemyPlay::getNextTactics(TacticCoroutine::push_type &yield,
     std::vector<Point> defense_positions = {
         Point(world_ptr->field().friendlyGoalpostNeg().x() +
                   world_ptr->field().defenseAreaXLength() + 2 * ROBOT_MAX_RADIUS_METERS,
-              -world_ptr->field().defenseAreaYLength() / 2.0),
+              -world_ptr->field().defenseAreaYLength() / 2.0 + 0.5),
         Point(world_ptr->field().friendlyGoalpostPos().x() +
                   world_ptr->field().defenseAreaXLength() + 2 * ROBOT_MAX_RADIUS_METERS,
-              world_ptr->field().defenseAreaYLength() / 2.0),
+              world_ptr->field().defenseAreaYLength() / 2.0 - 0.5),
         Point(world_ptr->field().friendlyGoalCenter().x() +
                   world_ptr->field().defenseAreaXLength() + 2 * ROBOT_MAX_RADIUS_METERS,
               world_ptr->field().friendlyGoalCenter().y()),
@@ -94,9 +94,18 @@ void KickoffEnemyPlay::getNextTactics(TacticCoroutine::push_type &yield,
             LOG(WARNING) << "No Robot on the Field!";
         }
 
-        auto enemy_threats =
+        std::vector<EnemyThreat> all_enemy_threats =
             getAllEnemyThreats(world_ptr->field(), world_ptr->friendlyTeam(),
                                world_ptr->enemyTeam(), world_ptr->ball(), false);
+
+        // We can only shadow enemy threats on our side of the field        
+        std::vector<EnemyThreat> filtered_enemy_threats;
+        std::copy_if(all_enemy_threats.begin(), all_enemy_threats.end(),
+                     std::back_inserter(filtered_enemy_threats),
+                     [&](const EnemyThreat &enemy_threat) {
+                         return contains(world_ptr->field().friendlyHalf(),
+                                         enemy_threat.robot.position());
+                     });
 
         PriorityTacticVector result = {{}};
 
@@ -104,10 +113,10 @@ void KickoffEnemyPlay::getNextTactics(TacticCoroutine::push_type &yield,
         int defense_position_index = 0;
         for (unsigned i = 0; i < defense_positions.size() - 1; ++i)
         {
-            if (i < 2 && i < enemy_threats.size())
+            if (i < 2 && i < filtered_enemy_threats.size())
             {
                 // Assign the first 2 robots to shadow enemies, if the enemies exist
-                auto enemy_threat = enemy_threats.at(i);
+                auto enemy_threat = filtered_enemy_threats.at(i);
                 // Shadow with a distance slightly more than the distance from the enemy
                 // robot to the center line, so we are always just on our side of the
                 // center line
