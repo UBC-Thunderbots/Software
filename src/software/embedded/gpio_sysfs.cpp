67e3c23572eb6e77da9e005c2e70dc4f771a9b19
Michael Ben-Zvi <michael.benzvi01@gmail.com>
2024-10-04 15:36:10 -0700

    Raspberry Pi 5 Support (#3253)
    * update gpio pin #s
    
    * update GPIO to use new kernel interface
    
    * wip
    
    * wip
    
    * new linux configs
    
    * spi config
    
    * partial refactor
    
    * wip
    
    * cleaning up ansible script
    
    * cleaning up ansible script
    
    * fixup pi setup ansible script
    
    * wip
    
    * basic stuff
    
    * wip
    
    * update to use new config.txt in ansible setup
    
    * transfer with gusto!
    
    * fixed up perms
    
    * fix build errors and add power_service back
    
    * fix logger build issue and run formatting
    
    * Split new GPIO into new class
    
    * Refactored motor for variable platform
    
    * Added flag to tbots py
    
    * Fixed bazel sync
    
    * Restructured constants
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * tracy on pi fix
    
    * update const-ness of constants -- wip not yet complete
    
    * Move raspberry pi build and config selection to //software/embedded
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * add some documentation and fixup failing tests
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * fix c++ test
    
    * cleanup docs and tests
    
    * remove underflow in gpio char dev implementation
    
    * update use of Jetson word in docs
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * fix tbots.py for flashing pi
    
    * fix tbots.py for flashing pi
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Address PR comments, clean up docs and update temperature handling
    
    * address PR comments and cleanup comments
    
    * fixup docs
    
    * fix mermaid diagram
    
    * fix mermaid v2
    
    * update mermaid to use tbots network for Pis
    
    * fix mermaid broken
    
    * Update useful-robot-commands.md
    
    * Update useful-robot-commands.md
    
    * remove unintentional changes
    
    * fix ansible requirements build failure
    
    * Merge in rpi IP changes from robocup
    
    * Update useful-robot-commands.md
    
    * Fix wrong cpu temp path in jetson_constants.h
    
    * Add network connection test from robocup
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Fix failing build
    
    * update thunderloop build issue
    
    * [pre-commit.ci lite] apply automatic fixes
    
    ---------
    
    Co-authored-by: arun <arun.sfc@outlook.com>
    Co-authored-by: arun <arun@zaber.com>
    Co-authored-by: Nima Zareian <nimazareian80@gmail.com>
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>
    Co-authored-by: itsarune <42703774+itsarune@users.noreply.github.com>
    Co-authored-by: williamckha <williamha@outlook.com>

---
+++ src/software/embedded/gpio_sysfs.cpp
@@ -0,0 +1,88 @@
+#include "software/embedded/gpio_sysfs.h"
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "software/logger/logger.h"
+
+GpioSysfs::GpioSysfs(const std::string& gpio_number, GpioDirection direction,
+                     GpioState initial_state)
+{
+    // Setup the provided GPIO pin
+    gpio_number_ = gpio_number;
+
+    auto export_gpio_fs = std::ofstream("/sys/class/gpio/export");
+    export_gpio_fs << gpio_number;
+    export_gpio_fs.close();
+
+    auto set_direction_fs =
+        std::ofstream("/sys/class/gpio/gpio" + gpio_number + "/direction");
+
+    switch (direction)
+    {
+        case GpioDirection::OUTPUT:
+        {
+            set_direction_fs << "out";
+            break;
+        }
+        case GpioDirection::INPUT:
+        {
+            set_direction_fs << "in";
+            break;
+        }
+    }
+
+    set_direction_fs.close();
+    setValue(initial_state);
+
+    LOG(DEBUG) << "GPIO " << gpio_number_ << " online";
+}
+
+void GpioSysfs::setValue(GpioState state)
+{
+    std::ofstream gpio_fs("/sys/class/gpio/gpio" + gpio_number_ + "/value");
+
+    CHECK(gpio_fs.is_open()) << "Could not set GPIO pin";
+
+    switch (state)
+    {
+        case GpioState::HIGH:
+        {
+            gpio_fs << "1";
+            break;
+        }
+        case GpioState::LOW:
+        {
+            gpio_fs << "0";
+            break;
+        }
+    }
+
+    if (gpio_fs.is_open())
+    {
+        gpio_fs.close();
+    }
+}
+
+GpioState GpioSysfs::getValue()
+{
+    std::ifstream gpio_fs("/sys/class/gpio/gpio" + gpio_number_ + "/value");
+    std::string level;
+
+    CHECK(gpio_fs.is_open()) << "Could not read GPIO pin";
+    std::getline(gpio_fs, level);
+
+    if (level.compare("0") == 0)
+    {
+        return GpioState::LOW;
+    }
+    else
+    {
+        return GpioState::HIGH;
+    }
+}
