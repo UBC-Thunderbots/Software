c470919143cbc5fb110169822f6900ca394db9b0
Daniel Liang <dawgswirly@gmail.com>
2024-10-26 11:57:05 -0700

    Copy relevant code from other fsm plays to `example_play` files
    Still giving errors, as `example_play_fsm.cpp` has not been implemented

--- src/software/ai/hl/stp/play/example/example_play_fsm.h
+++ src/software/ai/hl/stp/play/example/example_play_fsm.h
@@ -1,4 +1,35 @@
-#ifndef UBC_THUNDERBOTS_SOFTWARE_EXAMPLE_PLAY_FSM_H
-#define UBC_THUNDERBOTS_SOFTWARE_EXAMPLE_PLAY_FSM_H
+#pragma once
 
-#endif //UBC_THUNDERBOTS_SOFTWARE_EXAMPLE_PLAY_FSM_H
+#include "proto/parameters.pb.h"
+#include "shared/constants.h"
+#include "software/ai/hl/stp/play/play.h"
+#include "software/ai/hl/stp/tactic/move/move_tactic.h"
+#include "software/util/generic_factory/generic_factory.h"
+
+/**
+ * An example Play that moves the robots in a circle around the ball
+ * */
+
+struct ExamplePlayFSM
+{
+    struct ControlParams
+    {
+        // TODO
+    };
+
+    DEFINE_PLAY_UPDATE_STRUCT_WITH_CONTROL_AND_COMMON_PARAMS
+
+    auto operator()()
+    {
+        using namespace boost::sml;
+
+        DEFINE_SML_EVENT(Update)
+        // TODO
+
+        return make_transition_table(
+                // src_state + event [guard] / action = dest_state
+
+                // X + Update_E = X);
+                ); // TODO
+    }
+};
