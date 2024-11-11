ad79923581603c459b29d237f0fb0982abc514d5
Daniel Liang <dawgswirly@gmail.com>
2024-11-11 12:58:08 -0800

    Create `example_play_fsm_test.cpp`
    Copied from #3398 by @potatoisagender

---
+++ src/software/ai/hl/stp/play/example/example_play_fsm_test.cpp
@@ -0,0 +1,27 @@
+#include "software/ai/hl/stp/play/example/example_play_fsm.h"
+
+#include <gtest/gtest.h>
+
+#include "proto/parameters.pb.h"
+#include "software/test_util/equal_within_tolerance.h"
+#include "software/test_util/test_util.h"
+
+TEST(ExamplePlayFSMTest, test_transitions)
+{
+std::shared_ptr<World> world = ::TestUtil::createBlankTestingWorld();
+TbotsProto::AiConfig ai_config;
+
+FSM<ExamplePlayFSM> fsm(ExamplePlayFSM{});
+
+EXPECT_TRUE(fsm.is(boost::sml::state<ExamplePlayFSM::MoveState>));
+
+int num_tactics = 6;
+
+fsm.process_event(ExamplePlayFSM::Update(
+        ExamplePlayFSM::ControlParams{},
+        PlayUpdate(
+                world, num_tactics, [](PriorityTacticVector new_tactics) {},
+                InterPlayCommunication{}, [](InterPlayCommunication comm) {})));
+
+EXPECT_TRUE(fsm.is(boost::sml::state<ExamplePlayFSM::MoveState>));
+}
