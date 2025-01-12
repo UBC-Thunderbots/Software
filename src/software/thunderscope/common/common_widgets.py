bd78f7ea9ea66bb0d4bbbca9a113b148eb8ccf54
muk <muk.chunpongtong@gmail.com>
2025-01-11 16:01:11 -0800

    Need testing

--- src/software/thunderscope/common/common_widgets.py
+++ src/software/thunderscope/common/common_widgets.py
@@ -2,7 +2,7 @@ from pyqtgraph.Qt import QtCore
 from pyqtgraph.Qt.QtWidgets import *
 from pyqtgraph.Qt.QtCore import *
 from software.py_constants import *
-from software.util import color_from_gradient
+from software.thunderscope.util import color_from_gradient
 
 
 class FloatSlider(QSlider):
@@ -155,15 +155,12 @@ class ColorProgressBar(QProgressBar):
             [255, 255, 0],
             [0, 255, 255],
             [0, 0, 0],
-            [255, 255, 255],
+            [255, 255, 255]
         )
 
         # Extract color into CSS form.
         super(ColorProgressBar, self).setStyleSheet(
-            "QProgressBar::chunk"
-            "{"
-            f"background: rgb({color.red}, {color.green}, {color.blue})"
-            "}"
+            "QProgressBar::chunk" "{" f"background: rgb({color.red}, {color.green}, {color.blue})" "}"
         )
 
     def getPercentage(self):
