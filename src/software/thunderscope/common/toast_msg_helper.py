9a4a6861a01ac3a028319785caad6b905fd1a66b
Minghao Li <lmh1230612306@163.com>
2025-01-13 20:17:36 -0800

    add _ms suffix

--- src/software/thunderscope/common/toast_msg_helper.py
+++ src/software/thunderscope/common/toast_msg_helper.py
@@ -3,17 +3,17 @@ from pyqttoast import Toast, ToastPreset
 
 
 def show_toast(
-    parent: QWidget, title: str, text: str, timeout: int, preset: ToastPreset
+    parent: QWidget, title: str, text: str, timeout_ms: int, preset: ToastPreset
 ) -> None:
     """Display a toast message with the given properties
     :param parent: parent widget to inject toast message component
     :param title: title of toast message
     :param text: text of toast message
-    :param timeout: duration of the message (in ms)
+    :param timeout_ms: duration of the message (in ms)
     :param preset: preset of the message box
     """
     toast = Toast(parent)
-    toast.setDuration(timeout)
+    toast.setDuration(timeout_ms)
     toast.setTitle(title)
     toast.setText(text)
     toast.applyPreset(preset)
@@ -23,37 +23,37 @@ def show_toast(
     toast.show()
 
 
-def success_toast(parent: QWidget, text: str, timeout: int = 1000) -> None:
+def success_toast(parent: QWidget, text: str, timeout_ms: int = 1000) -> None:
     """Display a success toast message
     :param parent: parent widget to inject toast message component
     :param text: text of toast message
-    :param timeout: duration of the message (in ms)
+    :param timeout_ms: duration of the message (in ms)
     """
-    show_toast(parent, "", text, timeout, ToastPreset.SUCCESS_DARK)
+    show_toast(parent, "", text, timeout_ms, ToastPreset.SUCCESS_DARK)
 
 
-def failure_toast(parent: QWidget, text: str, timeout: int = 1000) -> None:
+def failure_toast(parent: QWidget, text: str, timeout_ms: int = 1000) -> None:
     """Display a failure toast message
     :param parent: parent widget to inject toast message component
     :param text: text of toast message
-    :param timeout: duration of the message (in ms)
+    :param timeout_ms: duration of the message (in ms)
     """
-    show_toast(parent, "", text, timeout, ToastPreset.ERROR_DARK)
+    show_toast(parent, "", text, timeout_ms, ToastPreset.ERROR_DARK)
 
 
-def info_toast(parent: QWidget, text: str, timeout: int = 1000) -> None:
+def info_toast(parent: QWidget, text: str, timeout_ms: int = 1000) -> None:
     """Display an information toast message
     :param parent: parent widget to inject toast message component
     :param text: text of toast message
-    :param timeout: duration of the message (in ms)
+    :param timeout_ms: duration of the message (in ms)
     """
-    show_toast(parent, "", text, timeout, ToastPreset.INFORMATION_DARK)
+    show_toast(parent, "", text, timeout_ms, ToastPreset.INFORMATION_DARK)
 
 
-def warning_toast(parent: QWidget, text: str, timeout: int = 1000) -> None:
+def warning_toast(parent: QWidget, text: str, timeout_ms: int = 1000) -> None:
     """Display a warning toast message
     :param parent: parent widget to inject toast message component
     :param text: text of toast message
-    :param timeout: duration of the message (in ms)
+    :param timeout_ms: duration of the message (in ms)
     """
-    show_toast(parent, "", text, timeout, ToastPreset.WARNING_DARK)
+    show_toast(parent, "", text, timeout_ms, ToastPreset.WARNING_DARK)
