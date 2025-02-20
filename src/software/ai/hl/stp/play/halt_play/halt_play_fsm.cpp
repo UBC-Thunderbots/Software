98afbb2eb4aaef9fcef8567510fbe06964a9b3a5
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2025-02-20 12:42:07 -0800

    t_formation

--- src/software/ai/hl/stp/play/halt_play/halt_play_fsm.cpp
+++ src/software/ai/hl/stp/play/halt_play/halt_play_fsm.cpp
@@ -4,13 +4,66 @@
 #include <algorithm>
 #include <iterator>
 
+#include "software/ai/hl/stp/tactic/move/move_tactic.h"
+#include "software/world/game_state.h"
+
+static PriorityTacticVector makeTimeoutFormation(WorldPtr world)
+{
+    TacticVector tactic = {};
+
+    double half_field_length_x = world->field().xLength() / 2.0;
+    double x_grain             = half_field_length_x / 4;
+
+    double half_field_legnth_y = world->field().yLength() / 2.0;
+    double y_grain             = half_field_legnth_y / 4;
+
+    std::vector<Point> timeoout_formation_points = {Point(-x_grain, 0),
+                                                    Point(-x_grain * 2, 0),
+                                                    Point(-x_grain * 3, 0),
+                                                    Point(-x_grain * 2, -y_grain),
+                                                    Point(-x_grain * 2, -y_grain * 2),
+                                                    Point(-x_grain * 2, -y_grain * 3)};
+
+    // making a t like formation
+    std::for_each(timeoout_formation_points.begin(), timeoout_formation_points.end(),
+                  [&](Point point)
+                  {
+                      std::shared_ptr<MoveTactic> move_tactic =
+                          std::make_shared<MoveTactic>();
+                      move_tactic->updateControlParams(point, Angle::zero());
+                      tactic.push_back(move_tactic);
+                  });
+
+    return {tactic};
+}
+
 HaltPlayFSM::HaltPlayFSM(TbotsProto::AiConfig ai_config) : halt_tactics({{}})
 {
     std::generate_n(std::back_inserter(halt_tactics.front()), MAX_ROBOT_IDS_PER_SIDE,
                     []() { return std::make_shared<HaltTactic>(); });
 }
 
+
+void HaltPlayFSM::updateTimeout(const Update& event)
+{
+    event.common.set_tactics(makeTimeoutFormation(event.common.world_ptr));
+}
+
 void HaltPlayFSM::updateStop(const Update& event)
 {
     event.common.set_tactics(halt_tactics);
+}
+
+
+bool HaltPlayFSM::shouldEnterHalt(const Update& update)
+{
+    RefereeCommand command = update.common.world_ptr->gameState().getRefereeCommand();
+    return command == RefereeCommand::HALT;
+}
+
+bool HaltPlayFSM::shouldEnterTimeout(const Update& update)
+{
+    RefereeCommand command = update.common.world_ptr->gameState().getRefereeCommand();
+    return command == RefereeCommand::TIMEOUT_US ||
+           command == RefereeCommand::TIMEOUT_THEM;
 }
--- src/software/ai/hl/stp/play/halt_play/halt_play_fsm.h
+++ src/software/ai/hl/stp/play/halt_play/halt_play_fsm.h
@@ -11,7 +11,9 @@ struct HaltPlayFSM
     struct ControlParams
     {
     };
+
     class HaltState;
+    class TimeoutState;
 
     DEFINE_PLAY_UPDATE_STRUCT_WITH_CONTROL_AND_COMMON_PARAMS
 
@@ -29,20 +31,53 @@ struct HaltPlayFSM
      */
     void updateStop(const Update& event);
 
+    /**
+     *Action to make the robot make a T like formation in half time
+     * @param event the HaltPlayFSM event
+     */
+    void updateTimeout(const Update& event);
+
+    /**
+     *  Guards on whether the FSM should enter timeout
+     *
+     * @param event the HaltPlayFSM event
+     */
+    bool shouldEnterTimeout(const Update& event);
+
+    /**
+     *  Guards on whether the FSM should enter default halt state
+     *
+     * @param event the HaltPlayFSM event
+     */
+    bool shouldEnterHalt(const Update& event);
 
     auto operator()()
     {
         using namespace boost::sml;
 
         DEFINE_SML_STATE(HaltState)
+        DEFINE_SML_STATE(TimeoutState)
+
+        DEFINE_SML_GUARD(shouldEnterHalt);
+        DEFINE_SML_GUARD(shouldEnterTimeout);
 
         DEFINE_SML_EVENT(Update)
 
         DEFINE_SML_ACTION(updateStop)
+        DEFINE_SML_ACTION(updateTimeout)
 
         return make_transition_table(
             // src_state + event [guard] / action = dest_state
-            *HaltState_S + Update_E / updateStop_A = HaltState_S,
+            *HaltState_S + Update_E[shouldEnterHalt_G] / updateStop_A = HaltState_S,
+            HaltState_S + Update_E[shouldEnterTimeout_G] / updateTimeout_A =
+                TimeoutState_S,
+
+            TimeoutState_S + Update_E[shouldEnterHalt_G] / updateStop_A = HaltState_S,
+            TimeoutState_S + Update_E[shouldEnterTimeout_G] / updateTimeout_A =
+                TimeoutState_S,
+
+
+            // HaltState_S + Update_E / updateStop_A = HaltState_S,
 
             X + Update_E / updateStop_A = X);
     }
