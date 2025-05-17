6c1276e9f4db9ff02c489d27e9e5b2121e60f385
Andrew Mao <andrewyxmao@gmail.com>
2025-05-16 21:01:08 -0700

    changes to namespace

--- src/proto/message_translation/ssl_referee.cpp
+++ src/proto/message_translation/ssl_referee.cpp
@@ -2,35 +2,29 @@
 
 #include "shared/constants.h"
 
-RefereeCommand ssl_referee::createRefereeCommand(const SSLProto::Referee &packet,
-                                    TeamColour team_colour)
-{
-    if (team_colour == TeamColour::YELLOW)
-    {
+namespace ssl_referee {
+
+RefereeCommand createRefereeCommand(const SSLProto::Referee &packet,
+                                                 TeamColour team_colour) {
+    if (team_colour == TeamColour::YELLOW) {
         return yellow_team_command_map.at(packet.command());
-    }
-    else
-    {
+    } else {
         return blue_team_command_map.at(packet.command());
     }
 }
 
-RefereeStage ssl_referee::createRefereeStage(const SSLProto::Referee &packet)
-{
+RefereeStage createRefereeStage(const SSLProto::Referee &packet) {
     return referee_stage_map.at(packet.stage());
 }
 
-std::optional<Point> getBallPlacementPoint(const SSLProto::Referee &packet)
-{
-    if (packet.has_designated_position())
-    {
+std::optional<Point> getBallPlacementPoint(const SSLProto::Referee &packet) {
+    if (packet.has_designated_position()) {
         return Point(
-            static_cast<double>(packet.designated_position().x() * METERS_PER_MILLIMETER),
-            static_cast<double>(packet.designated_position().y() *
-                                METERS_PER_MILLIMETER));
-    }
-    else
-    {
+                static_cast<double>(packet.designated_position().x() * METERS_PER_MILLIMETER),
+                static_cast<double>(packet.designated_position().y() *
+                                    METERS_PER_MILLIMETER));
+    } else {
         return std::nullopt;
     }
+}
 }
