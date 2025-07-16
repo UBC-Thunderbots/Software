caf3295541823675d092dacb74b40a20e8aa6c5c
Grayson Hoang <grayhoang@gmail.com>
2025-07-16 15:56:04 -0700

    penalty kick tactic test

--- src/software/ai/hl/stp/tactic/penalty_kick/penalty_kick_tactic_test.py
+++ src/software/ai/hl/stp/tactic/penalty_kick/penalty_kick_tactic_test.py
@@ -5,7 +5,8 @@ from proto.message_translation.tbots_protobuf import create_world_state
 from software.simulated_tests.ball_enters_region import BallEventuallyEntersRegion, BallAlwaysStaysInRegion
 from software.simulated_tests.ball_moves_forward import BallAlwaysMovesForward
 from software.simulated_tests.friendly_has_ball_possession import FriendlyAlwaysHasBallPossession
-
+from software.simulated_tests.simulated_test_fixture import (pytest_main,)
+from proto.import_all_protos import *
 
 def test_penalty_kick_no_goalie(simulated_test_runner):
     # Setup Robot
@@ -15,7 +16,7 @@ def test_penalty_kick_no_goalie(simulated_test_runner):
             yellow_robot_locations=[],
             blue_robot_locations=[tbots_cpp.Point(-3, 0)],
             ball_location=tbots_cpp.Point(0,0),
-            ball_velocity=0
+            ball_velocity=tbots_cpp.Vector(0, 0),
         )
     )
 
@@ -32,15 +33,39 @@ def test_penalty_kick_no_goalie(simulated_test_runner):
     # Setup no tactics on the enemy side
     params = AssignedTacticPlayControlParams()
     simulated_test_runner.yellow_full_system_proto_unix_io.send_proto(
-        AssignedTacticPlayControlPArams, params
+        AssignedTacticPlayControlParams, params
     )
 
     # Validation
+
+    always_validation_sequence_set = [
+        [
+            BallAlwaysStaysInRegion(
+                regions=[
+                    tbots_cpp.Field.createSSLDivisionBField().fieldLines(),
+                    tbots_cpp.Field.createSSLDivisionBField().enemyGoal()
+                ]
+            ),
+
+        ]
+    ]
+    eventually_validation_sequence_set = [
+        [
+            BallEventuallyEntersRegion(
+                regions=[
+                    tbots_cpp.Field.createSSLDivisionBField().enemyGoal()
+                ]
+            )
+        ]
+    ]
     simulated_test_runner.run_test(
-        inv_eventually_validation_sequence_set=[[BallEventuallyEntersRegion(regions=[tbots_cpp.Field.createSSLDivisionBField().enemyGoal()])]],
-        inv_always_validation_sequence_set=[[BallAlwaysStaysInRegion(regions=[tbots_cpp.Field.createSSLDivisionBField().fieldLines()]),
-                                             FriendlyAlwaysHasBallPossession(), BallAlwaysMovesForward(tbots_cpp.Point(0,0))]],
-        ag_eventually_validation_sequence_set=[[]],
-        ag_always_validation_sequence_set=[[]],
-        test_timeout_s=10
+        inv_eventually_validation_sequence_set=eventually_validation_sequence_set,
+        inv_always_validation_sequence_set=always_validation_sequence_set,
+        ag_eventually_validation_sequence_set=eventually_validation_sequence_set,
+        ag_always_validation_sequence_set=[[FriendlyAlwaysHasBallPossession(),
+                                            BallAlwaysMovesForward(tbots_cpp.Point(0,0))]],
+        test_timeout_s=15
     )
+
+if __name__ == "__main__":
+    pytest_main(__file__)
