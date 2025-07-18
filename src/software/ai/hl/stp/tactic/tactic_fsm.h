62c9c22bddb6e48b6d2b6e3ba300de4bc2392695
Grayson Hoang <grayhoang@gmail.com>
2025-07-18 16:30:28 -0700

    Add a new TacticFSM Template class

--- src/software/ai/hl/stp/tactic/tactic_fsm.h
+++ src/software/ai/hl/stp/tactic/tactic_fsm.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <functional>
+#include <memory>
 
 #include "proto/primitive/primitive_msg_factory.h"
+#include "proto/parameters.pb.h"
 #include "proto/tbots_software_msgs.pb.h"
 #include "software/ai/hl/stp/tactic/primitive.h"
 #include "software/ai/hl/stp/tactic/stop_primitive.h"
@@ -26,6 +28,41 @@ struct TacticUpdate
     WorldPtr world_ptr;
     // callback to return the next primitive
     SetPrimitiveCallback set_primitive;
+};
+
+/*
+ * A general FSM class with some utilities.
+ * T_FSM_CTP should be a structure containing the control parameters for the FSM being built.
+ */
+template<class T_FSM_CTP>
+class TacticFSM
+{
+public:
+    /**
+     * The Update struct is the only event that a tactic fsm should respond to and it is
+     * composed of the following structs:
+     *
+     * T_FSM_CTP - uniquely defined by each tactic to control the FSM
+     * TacticUpdate - common struct that contains Robot, World, and SetPrimitiveCallback
+     */
+    struct Update
+    {
+        Update(const T_FSM_CTP &control_params, const TacticUpdate &common)
+                : control_params(control_params), common(common)
+        {
+        }
+        T_FSM_CTP control_params;
+        TacticUpdate common;
+    };
+
+    explicit TacticFSM(std::shared_ptr<TbotsProto::AiConfig> ai_config_ptr): ai_config_ptr(ai_config_ptr){}
+
+    virtual auto operator()() = 0;
+
+private:
+    // Former constructors took what they needed from ai_config and stored it locally.
+    // Now, we store ai_config as a pointer and use it to update as needed.
+    std::shared_ptr<TbotsProto::AiConfig> ai_config_ptr;
 };
 
 /**
