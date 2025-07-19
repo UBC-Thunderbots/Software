da152fa10729ae4aac2023c577c7e68205e290d9
zuperzane <107359619+zuperzane@users.noreply.github.com>
2025-07-19 11:10:27 -0600

    Shadow Defense Base (#3450)

--- src/software/ai/hl/stp/play/defense/defense_play_fsm_test.cpp
+++ src/software/ai/hl/stp/play/defense/defense_play_fsm_test.cpp
@@ -15,6 +15,10 @@ TEST(DefensePlayFSMTest, test_transitions)
     FSM<DefensePlayFSM> fsm(DefensePlayFSM{ai_config});
     EXPECT_TRUE(fsm.is(boost::sml::state<DefensePlayFSM::DefenseState>));
 
+    // Place enemy robots behind the ball implying there is a large attack
+    ::TestUtil::setEnemyRobotPositions(world, {Point(0, 0), Point(-1, 0), Point(-2, 0)},
+                                       Timestamp::fromSeconds(0));
+
     fsm.process_event(DefensePlayFSM::Update(
         DefensePlayFSM::ControlParams{
             .max_allowed_speed_mode = TbotsProto::MaxAllowedSpeedMode::PHYSICAL_LIMIT},
@@ -22,6 +26,20 @@ TEST(DefensePlayFSMTest, test_transitions)
             world, 3, [](PriorityTacticVector new_tactics) {}, InterPlayCommunication{},
             [](InterPlayCommunication comm) {})));
 
-    // DefensePlayFSM always stays in the DefenseState
     EXPECT_TRUE(fsm.is(boost::sml::state<DefensePlayFSM::DefenseState>));
+
+
+    // Place more friendly robots behind the ball implying this is a safe time
+    // to aggressively defense
+    ::TestUtil::setFriendlyRobotPositions(
+        world, {Point(-1, 0), Point(-3, 0), Point(-2, 0)}, Timestamp::fromSeconds(0));
+
+    fsm.process_event(DefensePlayFSM::Update(
+        DefensePlayFSM::ControlParams{
+            .max_allowed_speed_mode = TbotsProto::MaxAllowedSpeedMode::PHYSICAL_LIMIT},
+        PlayUpdate(
+            world, 3, [](PriorityTacticVector new_tactics) {}, InterPlayCommunication{},
+            [](InterPlayCommunication comm) {})));
+
+    EXPECT_TRUE(fsm.is(boost::sml::state<DefensePlayFSM::AggressiveDefenseState>));
 }
