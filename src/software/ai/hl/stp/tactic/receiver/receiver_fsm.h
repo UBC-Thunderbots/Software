f32a3405745f74e9d5854917a0e9b3642f466b4f
Grayson Hoang <grayhoang@gmail.com>
2025-07-20 10:53:00 -0700

    refactor ReceiverFSM

--- src/software/ai/hl/stp/tactic/receiver/receiver_fsm.h
+++ src/software/ai/hl/stp/tactic/receiver/receiver_fsm.h
@@ -10,32 +10,29 @@
 #include "software/geom/algorithms/closest_point.h"
 #include "software/logger/logger.h"
 
-struct ReceiverFSM
+struct ReceiverFSMControlParams
 {
+    // The pass to receive
+    std::optional<Pass> pass = std::nullopt;
+
+    // If set to true, we will only receive and dribble
+    bool disable_one_touch_shot = false;
+};
+
+struct ReceiverFSM : TacticFSM<ReceiverFSMControlParams>
+{
+    using Update = TacticFSM<ReceiverFSMControlParams>::Update;
+
     /**
      * Constructor for ReceiverFSM
      *
-     * @param receiver_tactic_config The config to fetch parameters from
+     * @param ai_config_ptr shared pointer to ai_config
      */
-    explicit ReceiverFSM(TbotsProto::ReceiverTacticConfig receiver_tactic_config)
-        : receiver_tactic_config(receiver_tactic_config)
-    {
-    }
+     explicit ReceiverFSM(std::shared_ptr<TbotsProto::AiConfig> ai_config_ptr) : TacticFSM<ReceiverFSMControlParams>(ai_config_ptr) {}
 
     class OneTouchShotState;
     class ReceiveAndDribbleState;
     class WaitingForPassState;
-
-    struct ControlParams
-    {
-        // The pass to receive
-        std::optional<Pass> pass = std::nullopt;
-
-        // If set to true, we will only receive and dribble
-        bool disable_one_touch_shot = false;
-    };
-
-    DEFINE_TACTIC_UPDATE_STRUCT_WITH_CONTROL_AND_COMMON_PARAMS
 
     static constexpr double MIN_PASS_START_SPEED_M_PER_SEC    = 0.02;
     static constexpr double BALL_MIN_MOVEMENT_SPEED_M_PER_SEC = 0.04;
@@ -175,8 +172,4 @@ struct ReceiverFSM
             OneTouchShotState_S + Update_E[passFinished_G] / updateOnetouch_A     = X,
             X + Update_E / SET_STOP_PRIMITIVE_ACTION                              = X);
     }
-
-   private:
-    // the receiver tactic config
-    TbotsProto::ReceiverTacticConfig receiver_tactic_config;
 };
