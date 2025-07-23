57d67c574f0ab5955806c67f7a504c4fc2af1393
Grayson Hoang <grayhoang@gmail.com>
2025-07-23 15:40:09 -0700

    Revert "pivot_kick"
    This reverts commit c329476b

--- src/software/ai/hl/stp/tactic/pivot_kick/pivot_kick_fsm.h
+++ src/software/ai/hl/stp/tactic/pivot_kick/pivot_kick_fsm.h
@@ -61,16 +61,6 @@ struct PivotKickFSM : TacticFSM<PivotKickFSMControlParams>
      */
     bool ballKicked(const Update& event);
 
-    /**
-     * Update control params for PivotKickFSM
-     *
-     * @param kick_origin The location where the kick will be taken
-     * @param kick_direction The direction the Robot will kick in
-     * @param auto_chip_or_kick The command to autochip or autokick
-     */
-    void updateControlParams(const Point& kick_origin, const Angle& kick_direction,
-                             AutoChipOrKick auto_chip_or_kick);
-
     auto operator()()
     {
         using namespace boost::sml;
--- src/software/ai/hl/stp/tactic/pivot_kick/pivot_kick_tactic.h
+++ src/software/ai/hl/stp/tactic/pivot_kick/pivot_kick_tactic.h
@@ -18,10 +18,25 @@ class PivotKickTactic : public Tactic<PivotKickFSM>
 
     PivotKickTactic() = delete;
 
+    /**
+     * Update control params for this tactic
+     *
+     * @param kick_origin The location where the kick will be taken
+     * @param kick_direction The direction the Robot will kick in
+     * @param auto_chip_or_kick The command to autochip or autokick
+     */
+    void updateControlParams(const Point& kick_origin, const Angle& kick_direction,
+                             AutoChipOrKick auto_chip_or_kick);
+
     void accept(TacticVisitor& visitor) const override;
 
    private:
     std::unique_ptr<FSM<PivotKickFSM>> fsm_init() override;
+
+    void updatePrimitive(const TacticUpdate& tactic_update, bool reset_fsm) override;
+
+    PivotKickFSMControlParams control_params;
+
 };
 
 COPY_TACTIC(WallKickoffTactic, PivotKickTactic)
