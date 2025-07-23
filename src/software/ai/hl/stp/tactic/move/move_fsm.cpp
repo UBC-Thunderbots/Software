9e46d975d1805a38e8475434b6e03aeb695cde0f
Grayson Hoang <grayhoang@gmail.com>
2025-07-23 16:12:54 -0700

    Revert "move"
    This reverts commit 358995a2

--- src/software/ai/hl/stp/tactic/move/move_fsm.cpp
+++ src/software/ai/hl/stp/tactic/move/move_fsm.cpp
@@ -18,36 +18,3 @@ bool MoveFSM::moveDone(const Update &event)
     return robotReachedDestination(event.common.robot, event.control_params.destination,
                                    event.control_params.final_orientation);
 }
-
-
-void MoveFSM::updateControlParams(
-        Point destination, Angle final_orientation, TbotsProto::DribblerMode dribbler_mode,
-        TbotsProto::BallCollisionType ball_collision_type, AutoChipOrKick auto_chip_or_kick,
-        TbotsProto::MaxAllowedSpeedMode max_allowed_speed_mode,
-        TbotsProto::ObstacleAvoidanceMode obstacle_avoidance_mode)
-{
-    // Update the control parameters stored by this Tactic
-    control_params.destination             = destination;
-    control_params.final_orientation       = final_orientation;
-    control_params.dribbler_mode           = dribbler_mode;
-    control_params.ball_collision_type     = ball_collision_type;
-    control_params.auto_chip_or_kick       = auto_chip_or_kick;
-    control_params.max_allowed_speed_mode  = max_allowed_speed_mode;
-    control_params.obstacle_avoidance_mode = obstacle_avoidance_mode;
-}
-
-void MoveFSM::updateControlParams(
-        Point destination, Angle final_orientation,
-        TbotsProto::MaxAllowedSpeedMode max_allowed_speed_mode,
-        TbotsProto::ObstacleAvoidanceMode obstacle_avoidance_mode)
-{
-    // Update the control parameters stored by this Tactic
-    control_params.destination             = destination;
-    control_params.final_orientation       = final_orientation;
-    control_params.dribbler_mode           = TbotsProto::DribblerMode::OFF;
-    control_params.ball_collision_type     = TbotsProto::BallCollisionType::AVOID;
-    control_params.auto_chip_or_kick       = {AutoChipOrKickMode::OFF, 0};
-    control_params.max_allowed_speed_mode  = max_allowed_speed_mode;
-    control_params.obstacle_avoidance_mode = obstacle_avoidance_mode;
-}
-
--- src/software/ai/hl/stp/tactic/move/move_tactic.cpp
+++ src/software/ai/hl/stp/tactic/move/move_tactic.cpp
@@ -3,8 +3,57 @@
 #include <algorithm>
 
 MoveTactic::MoveTactic(std::shared_ptr<TbotsProto::AiConfig> ai_config_ptr)
-    : Tactic<MoveFSM>({RobotCapability::Move}, ai_config_ptr)
+    : Tactic<MoveFSM>({RobotCapability::Move}, ai_config_ptr),
+      control_params{
+          .destination             = Point(),
+          .final_orientation       = Angle::zero(),
+          .dribbler_mode           = TbotsProto::DribblerMode::OFF,
+          .ball_collision_type     = TbotsProto::BallCollisionType::AVOID,
+          .auto_chip_or_kick       = {AutoChipOrKickMode::OFF, 0},
+          .max_allowed_speed_mode  = TbotsProto::MaxAllowedSpeedMode::PHYSICAL_LIMIT,
+          .obstacle_avoidance_mode = TbotsProto::ObstacleAvoidanceMode::AGGRESSIVE}
 {
+}
+
+void MoveTactic::updateControlParams(
+    Point destination, Angle final_orientation, TbotsProto::DribblerMode dribbler_mode,
+    TbotsProto::BallCollisionType ball_collision_type, AutoChipOrKick auto_chip_or_kick,
+    TbotsProto::MaxAllowedSpeedMode max_allowed_speed_mode,
+    TbotsProto::ObstacleAvoidanceMode obstacle_avoidance_mode)
+{
+    // Update the control parameters stored by this Tactic
+    control_params.destination             = destination;
+    control_params.final_orientation       = final_orientation;
+    control_params.dribbler_mode           = dribbler_mode;
+    control_params.ball_collision_type     = ball_collision_type;
+    control_params.auto_chip_or_kick       = auto_chip_or_kick;
+    control_params.max_allowed_speed_mode  = max_allowed_speed_mode;
+    control_params.obstacle_avoidance_mode = obstacle_avoidance_mode;
+}
+
+void MoveTactic::updateControlParams(
+    Point destination, Angle final_orientation,
+    TbotsProto::MaxAllowedSpeedMode max_allowed_speed_mode,
+    TbotsProto::ObstacleAvoidanceMode obstacle_avoidance_mode)
+{
+    // Update the control parameters stored by this Tactic
+    control_params.destination             = destination;
+    control_params.final_orientation       = final_orientation;
+    control_params.dribbler_mode           = TbotsProto::DribblerMode::OFF;
+    control_params.ball_collision_type     = TbotsProto::BallCollisionType::AVOID;
+    control_params.auto_chip_or_kick       = {AutoChipOrKickMode::OFF, 0};
+    control_params.max_allowed_speed_mode  = max_allowed_speed_mode;
+    control_params.obstacle_avoidance_mode = obstacle_avoidance_mode;
+}
+
+void MoveTactic::updatePrimitive(const TacticUpdate &tactic_update, bool reset_fsm)
+{
+    if (reset_fsm)
+    {
+        fsm_map[tactic_update.robot.id()] = fsm_init();
+    }
+    fsm_map.at(tactic_update.robot.id())
+        ->process_event(MoveFSM::Update(control_params, tactic_update));
 }
 
 void MoveTactic::accept(TacticVisitor &visitor) const
