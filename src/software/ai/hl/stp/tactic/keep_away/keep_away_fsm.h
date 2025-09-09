99412f67a36cbf0567602ea0fef42d01a64b1400
Grayson Hoang <grayhoang@gmail.com>
2025-09-09 12:24:09 -0700

    Revert "refactor KeepAwayFSM"
    This reverts commit 8419b396

--- src/software/ai/hl/stp/tactic/keep_away/keep_away_fsm.h
+++ src/software/ai/hl/stp/tactic/keep_away/keep_away_fsm.h
@@ -4,18 +4,13 @@
 #include "software/ai/evaluation/keep_away.h"
 #include "software/ai/hl/stp/tactic/dribble/dribble_fsm.h"
 
-/**
- * The control parameters for updating KeepAwayFSM
- */
-struct KeepAwayFSMControlParams
+struct KeepAwayFSM : TacticFSM<KeepAwayFSM>
 {
-    std::optional<Pass> best_pass_so_far;
-};
-
-struct KeepAwayFSM : TacticFSM<KeepAwayFSMControlParams>
-{
-    using Update = TacticFSM<KeepAwayFSMControlParams>::Update;
-
+    using Update = TacticFSM<KeepAwayFSM>::Update;
+    struct ControlParams
+    {
+        std::optional<Pass> best_pass_so_far;
+    };
     /**
      * Constructor for KeepAwayFSM
      *
