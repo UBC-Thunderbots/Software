5c97980c8beb5829c98129acf6146ed147df1488
Grayson Hoang <grayhoang@gmail.com>
2025-09-09 13:58:06 -0700

    fixes

--- src/software/ai/hl/stp/tactic/pass_defender/pass_defender_fsm.h
+++ src/software/ai/hl/stp/tactic/pass_defender/pass_defender_fsm.h
@@ -30,7 +30,7 @@ struct PassDefenderFSM : public DefenderFSMBase, TacticFSM<PassDefenderFSM>
      * @param ai_config_ptr shared pointer to ai_config
      */
     explicit PassDefenderFSM(std::shared_ptr<const TbotsProto::AiConfig> ai_config_ptr)
-        : DefenderFSMBase(), TacticFSM<PassDefenderFSMControlParams>(ai_config_ptr)
+        : DefenderFSMBase(), TacticFSM<PassDefenderFSM>(ai_config_ptr)
     {
     }
 
