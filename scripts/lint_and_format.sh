d59c4a0a0f0ea009c98b494df10da4287ceb1060
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2025-10-03 16:28:45 -0700

    Use pre-push hook instead of pre-commit hook

--- scripts/lint_and_format.sh
+++ scripts/lint_and_format.sh
@@ -149,7 +149,10 @@ run_eof_new_line
 run_git_diff_check
 run_ansible_lint
 
-# Update time marker so pre-commit git hook knows that code has been formatted
-touch "$CURR_DIR/.last_format_time"
+# Update markers, telling Git hooks that formatting has been done
+# (Per-branch, so switching branches doesn't confuse the hooks)
+branch="$(git rev-parse --abbrev-ref HEAD)"
+mkdir -p "$CURR_DIR/.format_markers/$(dirname "$branch")"
+touch "$CURR_DIR/.format_markers/${branch}"
 
 exit 0
