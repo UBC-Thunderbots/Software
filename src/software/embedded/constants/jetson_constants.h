e281eebbb1e38258cbdda127300bff39004c7119
annieisawesome2 <anniesun608@gmail.com>
2025-10-15 18:14:22 -0700

    made constants for magic numbers

--- src/software/embedded/constants/jetson_constants.h
+++ src/software/embedded/constants/jetson_constants.h
@@ -10,4 +10,9 @@ constexpr const char DRIVER_CONTROL_ENABLE_GPIO[]             = "194";
 // Path to the CPU thermal zone temperature file
 constexpr const char CPU_TEMP_FILE_PATH[] = "/sys/class/thermal/thermal_zone1/temp";
 
+// Robot diagnostics constants
+constexpr double AUTO_CHIP_DISTANCE_DEFAULT_M = 1.5;
+constexpr double AUTO_KICK_SPEED_DEFAULT_M_PER_S = 1.5;
+constexpr double WHEEL_ROTATION_MAX_SPEED_M_PER_S = 2.0;
+
 constexpr Platform PLATFORM = Platform::JETSON_NANO;
--- src/software/embedded/constants/pi_constants.h
+++ src/software/embedded/constants/pi_constants.h
@@ -10,4 +10,9 @@ constexpr int DRIVER_CONTROL_ENABLE_GPIO             = 22;
 // Path to the CPU thermal zone temperature file
 constexpr const char CPU_TEMP_FILE_PATH[] = "/sys/class/thermal/thermal_zone0/temp";
 
+// Robot diagnostics constants
+constexpr double AUTO_CHIP_DISTANCE_DEFAULT_M = 1.5;
+constexpr double AUTO_KICK_SPEED_DEFAULT_M_PER_S = 1.5;
+constexpr double WHEEL_ROTATION_MAX_SPEED_M_PER_S = 15.2;
+
 constexpr Platform PLATFORM = Platform::RASP_PI;
