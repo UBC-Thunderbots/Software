544bc55b312ea56c95da80eb91607112247b4849
adrianchan787 <castlevictory1@gmail.com>
2025-11-30 02:57:18 -0800

    Added excessive dribbling functionality and testing

--- src/software/ai/hl/stp/tactic/dribble/excessive_dribble_test.py
+++ src/software/ai/hl/stp/tactic/dribble/excessive_dribble_test.py
@@ -15,14 +15,27 @@ from proto.message_translation.tbots_protobuf import create_world_state
 from proto.ssl_gc_common_pb2 import Team
 
 @pytest.mark.parametrize(
-    "dribble_destination,final_dribble_orientation,allow_excessive_dribbling",
+    "initial_location,dribble_destination,final_dribble_orientation,allow_excessive_dribbling",
     [
-        # Dribble Destination for the ball > 1.0 from its starting position
-        (tbots_cpp.Point(1.03, 0), tbots_cpp.Angle(), True),
+        # Dribble Destination for the ball < 1.0 from its starting position
+        (tbots_cpp.Point(0.5, 0), tbots_cpp.Point(1.02, 0), tbots_cpp.Angle(), True),
+
+        # Dribble Testing diagonally
+        (tbots_cpp.Point(0.25, 0.25), tbots_cpp.Point(0.85, 0.65), tbots_cpp.Angle.fromRadians(50), True),
+
+        # Boundary Testing, because of the autoref implementation (initial of position Bot to final of Ball),
+        # a dribble distance a tiny bit over 1m should pass
+        (tbots_cpp.Point(0.01, 0), tbots_cpp.Point(1.025, 0), tbots_cpp.Angle(), True),
+        (tbots_cpp.Point(1, 1.5), tbots_cpp.Point(2, 1.5), tbots_cpp.Angle(), True),
+
+        # When the robot is directly on the ball, the orientation at which the bot begins to
+        # dribble is different; there appears to be a bit more leeway
+        (tbots_cpp.Point(0, 1), tbots_cpp.Point(0, 2.025), tbots_cpp.Angle(), True),
     ],
 )
 
-def test_excessive_dribbling(
+def test_not_excessive_dribbling(
+        initial_location,
         dribble_destination,
         final_dribble_orientation,
         allow_excessive_dribbling,
@@ -32,11 +45,12 @@ def test_excessive_dribbling(
         WorldState,
         create_world_state(
             [],
-            blue_robot_locations=[tbots_cpp.Point(1, 0)],
-            ball_location=tbots_cpp.Point(0, 0),
+            blue_robot_locations=[tbots_cpp.Point(0, 1)],
+            ball_location=initial_location,
             ball_velocity=tbots_cpp.Vector(0, 0),
         ),
     )
+
 
     # Setup Tactic
     params = AssignedTacticPlayControlParams()
@@ -59,14 +73,81 @@ def test_excessive_dribbling(
 
     # Always Validation
     always_validation_sequence_set = [
-        [BallAlwaysStaysInRegion(
-        regions=[tbots_cpp.Circle(tbots_cpp.Point(0, 0), 1)]
-        )],
         [NeverExcessivelyDribbles()]
     ]
 
     # Eventually Validation
     eventually_validation_sequence_set = [[]]
+
+    simulated_test_runner.run_test(
+        inv_eventually_validation_sequence_set=eventually_validation_sequence_set,
+        inv_always_validation_sequence_set=always_validation_sequence_set,
+        ag_eventually_validation_sequence_set=eventually_validation_sequence_set,
+        ag_always_validation_sequence_set=always_validation_sequence_set,
+    )
+
+
+@pytest.mark.parametrize(
+    "initial_location,dribble_destination,final_dribble_orientation,allow_excessive_dribbling",
+    [
+        # Dribble Destination for the ball > 1.0 from its starting position
+        (tbots_cpp.Point(0, 2), tbots_cpp.Point(0, 0.5), tbots_cpp.Angle(), True),
+        # Dribble Testing diagonally
+        (tbots_cpp.Point(0.1, 1.1), tbots_cpp.Point(1.1, 0.1), tbots_cpp.Angle.fromRadians(50), True),
+        # Boundary Testing, because of the autoref implementation (initial position of Bot to final of Ball),
+        # a dribble distance a tiny bit over 1m should pass, but too much (> 1.015 from testing) fails
+        (tbots_cpp.Point(0, 1), tbots_cpp.Point(0, 2.02), tbots_cpp.Angle(), True),
+
+        # When the robot is directly on the ball, the orientation at which the bot begins to
+        # dribble is different; there appears to be a bit more leeway on the distance that passes
+        # Furthermore, that leeway appears to be different depending on the position of the bot and ball
+        # (e.g. here the point is 0, 0, and the leeway is 1 cm more than at 0, 1)
+        (tbots_cpp.Point(0, 0), tbots_cpp.Point(0, 1.035), tbots_cpp.Angle(), True),
+    ],
+)
+
+def test_excessive_dribbling(
+        initial_location,
+        dribble_destination,
+        final_dribble_orientation,
+        allow_excessive_dribbling,
+        simulated_test_runner,
+):
+    simulated_test_runner.simulator_proto_unix_io.send_proto(
+        WorldState,
+        create_world_state(
+            [],
+            blue_robot_locations=[tbots_cpp.Point(0, 0.0)],
+            ball_location=initial_location,
+            ball_velocity=tbots_cpp.Vector(0, 0),
+        ),
+    )
+
+
+    # Setup Tactic
+    params = AssignedTacticPlayControlParams()
+    params.assigned_tactics[0].dribble.CopyFrom(
+        DribbleTactic(
+            dribble_destination=tbots_cpp.createPointProto(dribble_destination),
+            final_dribble_orientation=tbots_cpp.createAngleProto(final_dribble_orientation),
+            allow_excessive_dribbling=allow_excessive_dribbling,
+        )
+    )
+    simulated_test_runner.blue_full_system_proto_unix_io.send_proto(
+        AssignedTacticPlayControlParams, params
+    )
+
+    # Setup no tactics on the enemy side
+    params = AssignedTacticPlayControlParams()
+    simulated_test_runner.yellow_full_system_proto_unix_io.send_proto(
+        AssignedTacticPlayControlParams, params
+    )
+
+    # Always Validation
+    always_validation_sequence_set = [[]]
+
+    # Eventually Validation
+    eventually_validation_sequence_set = [[EventuallyStartsExcessivelyDribbling()]]
 
     simulated_test_runner.run_test(
         inv_eventually_validation_sequence_set=eventually_validation_sequence_set,
--- src/software/simulated_tests/excessive_dribbling.py
+++ src/software/simulated_tests/excessive_dribbling.py
@@ -1,6 +1,5 @@
 import software.python_bindings as tbots_cpp
 from proto.import_all_protos import *
-from software.thunderscope.thread_safe_buffer import ThreadSafeBuffer
 
 from software.simulated_tests.validation import (
     Validation,
@@ -12,12 +11,10 @@ from software.simulated_tests.validation import (
 class ExcessivelyDribbling(Validation):
     """Checks if any friendly robot is excessively dribbling the ball, i.e. for over 1m."""
 
-    def __init__(self, buffer_size: int = 5):
+    def __init__(self):
         self.continous_dribbling_start_point = None
 
-        self.world_buffer = ThreadSafeBuffer(buffer_size, World)
-
-    def get_validation_status(self, world, max_dribble_length: int = 1) -> ValidationStatus:
+    def get_validation_status(self, world, max_dribble_length: float = 1.00) -> ValidationStatus:
         """Checks if any friendly robot is excessively dribbling the ball, i.e. for over 1m.
 
         :param world: The world msg to validate
@@ -25,7 +22,8 @@ class ExcessivelyDribbling(Validation):
         :return: FAILING when the robot is excessively dribbling
                  PASSING when the robot is not excessively dribbling
         """
-
+        # Use world calculation of dribbling distance, which uses implementation
+        # of initial position of BOT to final position of BALL
         if world.HasField("dribble_displacement"):
             dribble_disp = world.dribble_displacement
             dist = tbots_cpp.createSegment(dribble_disp).length()
@@ -33,23 +31,6 @@ class ExcessivelyDribbling(Validation):
                 return ValidationStatus.FAILING
 
         return ValidationStatus.PASSING
-
-
-
-        ball_position = tbots_cpp.createPoint(world.ball.current_state.global_position)
-        ball = world.friendly_team.team_robots
-        for robot in world.friendly_team.team_robots:
-            if not tbots_cpp.Robot(robot).isNearDribbler(ball_position, 0.02):
-                # if ball is not near dribbler then de-activate this validation
-                self.continous_dribbling_start_point = None
-            else:
-                if self.continous_dribbling_start_point is None:
-                    self.continous_dribbling_start_point = ball_position
-                print(ball_position)
-                if (ball_position - self.continous_dribbling_start_point).length() > 1:
-                    return ValidationStatus.FAILING
-        return ValidationStatus.PASSING
-
 
     def get_validation_geometry(self, world) -> ValidationGeometry:
         """(override) Shows the max allowed dribbling circle"""
