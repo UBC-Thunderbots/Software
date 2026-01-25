c951fd4e2f36635ebd47f0561eb9ecb97e1bcc42
Eric Xiao <ericxiao0000@gmail.com>
2026-01-24 22:26:18 -0800

    Add distinct versions for each installed runtime

--- src/software/thunderscope/binary_context_managers/runtime_installer.py
+++ src/software/thunderscope/binary_context_managers/runtime_installer.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import zipfile
 import tarfile
 import shutil
-import logging
 from software.thunderscope.constants import RuntimeManagerConstants
 
 
@@ -46,7 +45,9 @@ class RuntimeInstaller:
 
         filename = Path(url).name
         target_dir = Path(RuntimeManagerConstants.EXTERNAL_RUNTIMES_PATH)
-        tmp_path = Path("/tmp") / filename
+        tmp_dir = Path("/tmp")
+        tmp_path = tmp_dir / filename
+        extracted_binary_name = "unix_full_system"
 
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
@@ -55,13 +56,18 @@ class RuntimeInstaller:
                     if chunk:
                         f.write(chunk)
 
+        # TODO: make this not reliant on release assets name formatting
+        # The split + join gets rid of file extension and os in filename
+        dest = target_dir / "_".join(filename.split("_")[:-1])
+
         if filename.endswith((".tar.gz", ".tgz")):
             with tarfile.open(tmp_path, "r:*") as tar:
-                tar.extractall(path=target_dir)
+                tar.extractall(tmp_dir)
+                shutil.move(tmp_dir / extracted_binary_name, dest)
         elif filename.endswith(".zip"):
             with zipfile.ZipFile(tmp_path, "r") as zipf:
-                zipf.extractall(path=target_dir)
+                zipf.extractall(tmp_dir)
+                shutil.move(tmp_dir / extracted_binary_name, dest)
         else:
-            dest = target_dir / filename
             shutil.copy2(tmp_path, dest)
             dest.chmod(0o755)  # make executable (common for runtimes)
