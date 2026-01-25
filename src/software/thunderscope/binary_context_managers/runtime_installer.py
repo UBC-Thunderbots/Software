5d68cfe2a84807285ee871a6aeddb4a0adc2f524
Eric Xiao <ericxiao0000@gmail.com>
2026-01-24 22:02:07 -0800

    Refactor runtime installer backend

--- src/software/thunderscope/binary_context_managers/runtime_installer.py
+++ src/software/thunderscope/binary_context_managers/runtime_installer.py
@@ -1,38 +1,28 @@
 import requests
 from pathlib import Path
-from software.logger.logger import create_logger
 import zipfile
 import tarfile
 import shutil
+import logging
 from software.thunderscope.constants import RuntimeManagerConstants
-
-logger = create_logger(__name__)
 
 
 class RuntimeInstaller:
     """Delegate class for handling runtime installation and remote interfacing"""
 
     def __init__(self):
-        self.download_urls = []
+        self.runtime_install_targets = {}
 
     def fetch_remote_runtimes(self) -> list[str]:
         """Requests a list of available runtimes from the remote. This is an expensive operation
         and should only be called when necessary.
         :return: A unique list of names for available runtimes
         """
-        url = RuntimeManagerConstants.INSTALL_URL
-        headers = {"Accept": "application/vnd.github+json"}
+        releases = requests.get(
+            RuntimeManagerConstants.INSTALL_URL,
+            headers={"Accept": "application/vnd.github+json"},
+        ).json()
 
-        response = requests.get(url, headers=headers)
-        logger.warning(response)
-
-        releases = response.json()
-
-        PREFIX = RuntimeManagerConstants.DOWNLOAD_PREFIX
-
-        # I'm going to assume you are trying to reload the assets so reset the download_urls
-        if len(self.download_urls) != 0:
-            self.download_urls = []
         download_names = []
 
         for release in releases:
@@ -40,9 +30,10 @@ class RuntimeInstaller:
                 url = asset["browser_download_url"]
                 if "unix_full_system" not in url:
                     continue
-                self.download_urls.append(url)
-                trimmed = url.removeprefix(PREFIX)
-                download_names.append(trimmed)
+
+                version = url.removeprefix(RuntimeManagerConstants.DOWNLOAD_PREFIX)
+                download_names.append(version)
+                self.runtime_install_targets[version] = url
 
         return download_names[:5]
 
@@ -51,48 +42,26 @@ class RuntimeInstaller:
         Ensures that the runtime is compatible with the current platform
         :param version: Version of the runtime hosted on the remote to install
         """
-        url = RuntimeManagerConstants.INSTALL_URL
-        headers = {"Accept": "application/vnd.github+json"}
+        url = self.runtime_install_targets[version]
 
-        response = requests.get(url, headers=headers)
-        logger.warning(response)
+        filename = Path(url).name
+        target_dir = Path(RuntimeManagerConstants.EXTERNAL_RUNTIMES_PATH)
+        tmp_path = Path("/tmp") / filename
 
-        releases = response.json()
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(tmp_path, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
 
-        TARGET_SUFFIX = version
-
-        selected_asset = None
-
-        for download_url in self.download_urls:
-            if download_url.endswith(TARGET_SUFFIX):
-                selected_asset = download_url
-
-        if selected_asset:
-            url = selected_asset
-            filename = Path(url).name
-
-            target_dir = Path(RuntimeManagerConstants.EXTERNAL_RUNTIMES_PATH)
-            tmp_path = Path("/tmp") / filename
-
-            with requests.get(url, stream=True) as r:
-                r.raise_for_status()
-                with open(tmp_path, "wb") as f:
-                    for chunk in r.iter_content(chunk_size=8192):
-                        if chunk:
-                            f.write(chunk)
-
-            if filename.endswith((".tar.gz", ".tgz")):
-                with tarfile.open(tmp_path, "r:*") as tar:
-                    tar.extractall(path=target_dir)
-
-            elif filename.endswith(".zip"):
-                with zipfile.ZipFile(tmp_path, "r") as zipf:
-                    zipf.extractall(path=target_dir)
-
-            else:
-                dest = target_dir / filename
-                shutil.copy2(tmp_path, dest)
-                dest.chmod(0o755)  # make executable (common for runtimes)
-
-        if not selected_asset:
-            logger.warning("Can't find binary")
+        if filename.endswith((".tar.gz", ".tgz")):
+            with tarfile.open(tmp_path, "r:*") as tar:
+                tar.extractall(path=target_dir)
+        elif filename.endswith(".zip"):
+            with zipfile.ZipFile(tmp_path, "r") as zipf:
+                zipf.extractall(path=target_dir)
+        else:
+            dest = target_dir / filename
+            shutil.copy2(tmp_path, dest)
+            dest.chmod(0o755)  # make executable (common for runtimes)
