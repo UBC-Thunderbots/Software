6e8b73dcc6235f59e0a5471df8123541aa2c0d4c
StarrryNight <lausingsamuel@gmail.com>
2026-02-01 11:48:49 -0800

    add traj calculations in collision evaluator

--- src/software/ai/navigator/trajectory/collision_evaluator.cpp
+++ src/software/ai/navigator/trajectory/collision_evaluator.cpp
@@ -8,12 +8,19 @@ CollisionEvaluator::CollisionEvaluator(const std::vector<ObstaclePtr> &obstacles
 TrajectoryPathWithCost CollisionEvaluator::evaluate(
     const TrajectoryPath &trajectory,
     const std::optional<TrajectoryPathWithCost> &sub_traj_with_cost,
-    const std::optional<double> sub_traj_duration_s)
+    const std::optional<double> sub_traj_duration_s,
+	const std::optional<double> max_cost)
 {
-    TrajectoryPathWithCost traj_with_cost(trajectory);
+    TrajectoryPathWithCost traj_with_cost(trajectory);	
 
+	const double traj_time = trajectory.getTotalTime;
     const double search_end_time_s =
         std::min(trajectory.getTotalTime(), MAX_FUTURE_COLLISION_CHECK_SEC);
+    const Point destination = traj_with_cost.traj_path.getDestination();
+
+	// 1. Initialize total cost as trajectory time
+	double total_cost = traj_time;
+
 
     /**
      * Find the start duration before the trajectory leaves all obstacles
@@ -33,6 +40,15 @@ TrajectoryPathWithCost CollisionEvaluator::evaluate(
     }
     traj_with_cost.collision_duration_front_s = first_non_collision_time;
 
+	// 2 .Add first front collision to total cost
+	total_cost += 3 * first_non_collision_time;
+
+    // Return early if current cost already higher than max cost
+	if (total_cost >= max_cost){
+		traj_with_cost.cost = total_cost;
+		return traj_with_cost;
+	}
+
     /**
      * Find the duration we're within an obstacle before search_end_time_s
      */
@@ -40,6 +56,16 @@ TrajectoryPathWithCost CollisionEvaluator::evaluate(
         getLastNonCollisionTime(trajectory, search_end_time_s);
     traj_with_cost.collision_duration_back_s =
         search_end_time_s - last_non_collision_time;
+
+	// 3. Add back collision to total cost
+	total_cost += 1 * traj_with_cost.collision_duration_back_s;
+
+    // Return early if current cost already higher than max cost
+	if (total_cost >= max_cost){
+		traj_with_cost.cost = total_cost;
+		return traj_with_cost;
+	}
+
 
     /**
      * Get the first collision time, excluding the time at the start and end of path
@@ -59,7 +85,20 @@ TrajectoryPathWithCost CollisionEvaluator::evaluate(
         traj_with_cost.first_collision_time_s = collision.first;
         traj_with_cost.colliding_obstacle     = collision.second;
     }
+	
+    // 4. Add 6.0 to collision if mid-trajectory collision exist
+	if (traj_with_cost.colliding_obstacle != std::nullptr){
+		total_cost += 6.0;
+	}
 
+    // 5: Add distance from first collision to destination                                                                                                                                   │
+    Point first_collision_position = trajectory.getPosition(traj_with_cost.first_collision_time_s);                                                                                               │
+    total_cost += (first_collision_position - destination).length();                                                                                                                            │
+                                                                                                                                                                                                  │
+    // 6: Add early collision penalty                                                                                                                                                        │
+    total_cost += std::max(0.0, MAX_FUTURE_COLLISION_CHECK_SEC - traj_with_cost.first_collision_time_s);                                                                                        │
+
+	traj_with_cost.cost = total_cost;
     return traj_with_cost;
 }
 
