e648a345b9d43af6cd95656fc1e3a7980680228f
Thunderbots <ubcrobocup@gmail.com>
2026-01-31 19:57:10 -0800

    adding gl generate test toolbar

---
+++ src/software/thunderscope/gl/widgets/toolbars/generate_test/gl_generate_test_toolbar.py
@@ -0,0 +1,8 @@
+from software.thunderscope.gl.gl_widget import GLWidget
+from software.thunderscope.gl.widgets.gl_toolbar import GLToolbar
+
+
+class GLGenerateTestToolbar():
+
+    def __init__(self, toolbar: GLToolbar):
+        self.__toolbar = toolbar
