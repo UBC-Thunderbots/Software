f17451f6b0b7011406f5974fc4931e0b4dc6a4f1
saurav <sauravbanna@gmail.com>
2026-02-13 19:45:35 -0800

    added referee tracker

---
+++ src/software/thunderscope/log/trackers/referee_tracker.py
@@ -0,0 +1,45 @@
+from typing import override, Callable
+
+
+class RefereeTracker(Tracker):
+  def __init__(self, friendly_color_yellow: bool, callback: Callable[[int, int, int], None], buffer_size: int = 5):
+    super().__init__(callback, buffer_size)
+    
+    self.referee_buffer = ThreadSafeBuffer(buffer_size, Referee)
+    
+    self.friendly_color_yellow = friendly_color_yellow
+
+
+  @override
+  def set_proto_unix_io(self, proto_unix_io: ProtoUnixIO) -> None:
+    super().set_proto_unix_io(proto_unix_io, [
+        (Referee, self.referee_buffer),
+    ])
+    
+  @override
+  def refresh(self):
+    """Refresh and update the callback with the latest referee information"""
+    
+    refree_msg = self.referee_buffer.get(block=False, return_cached=True)
+    
+    if not refree_msg:
+      return
+
+    if refree_msg.HasField("yellow" if self.friendly_colour_yellow else "blue"):
+      team_info = refree_msg.yellow if self.friendly_colour_yellow else refree_msg.blue
+      
+      num_goals = 0
+      num_yellow_cards = 0
+      num_red_cards = 0
+      
+      if team_info.HasField("score"):
+          num_goals = team_info.score
+
+      if team_info.HasField("yellow_cards"):
+          num_yellow_cards = team_info.yellow_cards
+
+      if team_info.HasField("red_cards"):
+          num_red_cards = team_info.red_cards
+          
+      if self.callback:
+        self.callback(num_goals, num_yellow_cards, num_red_cards)
