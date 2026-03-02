3e87e5f456ba99ab2da57bbf2878ef2fdf8d6c3a
Eric Xiao <ericxiao0000@gmail.com>
2026-03-02 13:01:24 -0800

    Implement robot received ball (has ball) validation

---
+++ src/software/simulated_tests/pytest_validations/robot_received_ball.py
@@ -0,0 +1,60 @@
+import software.python_bindings as tbots_cpp
+from proto.import_all_protos import *
+
+from software.simulated_tests.pytest_validations.validation import (
+    Validation,
+    create_validation_geometry,
+    create_validation_types,
+)
+from typing import override
+
+
+class RobotReceivedBall(Validation):
+    """Checks if a specific robot has received the ball (ball is near dribbler)"""
+
+    def __init__(self, robot_id, tolerance=0.02):
+        """Constructs the validation object
+
+        :param robot_id: The ID of the robot to check
+        :param tolerance: The tolerance for when we check if the robot has the ball
+        """
+        self.robot_id = robot_id
+        self.tolerance = tolerance
+
+    @override
+    def get_validation_status(self, world) -> ValidationStatus:
+        """Checks if the specific robot has received the ball
+
+        :param world: The world msg to validate
+        :return: FAILING when the robot does not have the ball
+                 PASSING when the robot has the ball
+        """
+        ball_position = tbots_cpp.createPoint(world.ball.current_state.global_position)
+        for robot in world.friendly_team.team_robots:
+            if robot.id == self.robot_id:
+                if tbots_cpp.Robot(robot).isNearDribbler(ball_position, self.tolerance):
+                    return ValidationStatus.PASSING
+        return ValidationStatus.FAILING
+
+    @override
+    def get_validation_geometry(self, world) -> ValidationGeometry:
+        """Highlights the dribbler area of the robot"""
+        # TODO (#2558): create better validation geometry
+        for robot in world.friendly_team.team_robots:
+            if robot.id == self.robot_id:
+                return create_validation_geometry(
+                    [tbots_cpp.Robot(robot).dribblerArea()]
+                )
+        return create_validation_geometry()
+
+    @override
+    def __repr__(self):
+        return f"Check that robot {self.robot_id} has received the ball"
+
+
+(
+    RobotEventuallyReceivedBall,
+    RobotEventuallyDidNotReceiveBall,
+    RobotAlwaysHasBall,
+    RobotNeverHasBall,
+) = create_validation_types(RobotReceivedBall)
