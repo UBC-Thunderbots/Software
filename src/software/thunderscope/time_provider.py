41f15dc5d959aec054f2483f74f667e496526766
Thunderbots <ubcrobocup@gmail.com>
2026-03-21 13:39:38 -0700

    Get rid of mutex cause it doesn't matter

--- src/software/thunderscope/time_provider.py
+++ src/software/thunderscope/time_provider.py
@@ -1,23 +1,17 @@
-import threading
-
-
 class TimeProvider:
     """A singleton class that can provide time information"""
 
     def __init__(self) -> None:
         """Constructs time provider with no elapsed time"""
         self._elapsed_time = 0.0
-        self._mutex = threading.Lock()
 
     def elapsed_time_ns(self) -> float:
         """Returns elapsed time in nanoseconds since time provider was constructed"""
-        with self._mutex:
-            return self._elapsed_time
+        return self._elapsed_time
 
     def tick_ns(self, delta_time_ns: float) -> None:
         """Advances elapsed time by a delta time in nanoseconds"""
-        with self._mutex:
-            self._elapsed_time += delta_time_ns
+        self._elapsed_time += delta_time_ns
 
 
 time_provider_instance = TimeProvider()
