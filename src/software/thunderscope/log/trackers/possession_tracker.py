e42bf21a052f1db0f3d98e721e1b180d7f696b84
saurav <sauravbanna@gmail.com>
2026-03-28 13:11:09 -0700

    refactored possession tracker

--- src/software/thunderscope/log/trackers/possession_tracker.py
+++ src/software/thunderscope/log/trackers/possession_tracker.py
@@ -4,72 +4,81 @@ from software.thunderscope.thread_safe_buffer import ThreadSafeBuffer
 from software.thunderscope.proto_unix_io import ProtoUnixIO
 from proto.import_all_protos import *
 import software.python_bindings as tbots_cpp
+from software.thunderscope.log.trackers.tracked_event import EventType
 
 
 class PossessionTracker(Tracker):
-    """Tracker to track when ball possession changes"""
+    """Tracker to track and log when ball possession changes"""
 
-    def __init__(self, callback: Callable[[bool | None], None], buffer_size: int = 5):
+    def __init__(self, proto_unix_io: ProtoUnixIO, out_file_path: str, buffer_size: int = 5):
         """Initializes the Possession tracker
-
-        :param callback: function to call when possession changes
-                         called with an optional bool value:
-                            - True: friendly possession
-                            - False: enemy possession
-                            - None: neither
+             
+        :param proto_unix_io: the proto unix io to get the game state from
+        :param out_file_path: the file to write tracked events to               
         :param buffer_size: buffer size for the tracker's io
         """
-        super().__init__(callback=callback, buffer_size=buffer_size)
-
-        self.world_buffer = ThreadSafeBuffer(self.buffer_size, World)
-
-    @override
-    def set_proto_unix_io(self, proto_unix_io: ProtoUnixIO) -> None:
-        super().set_proto_unix_io(
-            proto_unix_io,
-            [
-                (World, self.world_buffer),
-            ],
-        )
-
+        super().__init__(proto_unix_io, out_file_path, buffer_size)
+        
+        # start with no team having possession
+        self.curr_possession = None
+        
     @override
     def refresh(self):
-        """Refresh and update the callback with the latest ball possession"""
-        world_msg = self.world_buffer.get(block=False, return_cached=True)
-
-        if world_msg is None:
+        """Refresh and logs any changes in ball possession"""
+        super().refresh()
+        
+        if self.cached_world is None:
             return
+        
+        self._log_posession_for_friendly(
+            self.cached_world.friendlyTeam(), self.cached_world.enemyTeam(), self.cached_world.ball().position()
+        )
 
-        world = tbots_cpp.World(world_msg)
-
-        if self.callback:
-            self.callback(
-                self._check_posession_for_friendly(
-                    world.friendlyTeam(), world.enemyTeam(), world.ball().position()
-                )
-            )
-
-    def _check_posession_for_friendly(
+    def _log_posession_for_friendly(
         self,
         friendly_team: tbots_cpp.Team,
         enemy_team: tbots_cpp.Team,
         ball_position: tbots_cpp.Point,
-    ) -> bool | None:
-        """Check for if the friendly team has possession of the ball
-        True if they do, False if enemy team has possession, None if neither
-
-        :param friendly_team: the friendly team
-        :param enemy_team: the enemy team
-        :param ball_position: the current ball position
-        :return: True / False / None depending on which team has possession
+    ) -> None:
         """
+        Detects possession changes and logs the corresponding start/end events
+    
+        Checks the current world state against the last known possession. 
+        True if friendly possession, False is enemy possession, None if neither
+        
+        If a transition occurs (e.g., Friendly -> None, None -> Enemy), it triggers 
+        a write_event with the appropriate EventType and updates the internal 
+        possession state.
+
+        :param friendly_team: the friendly team object
+        :param enemy_team: the enemy team object
+        :param ball_position: the current position of the ball
+        :return: None
+        """
+        new_possession = None
+        
         if self._check_posession_for_team(friendly_team, ball_position):
-            return True
-
-        if self._check_posession_for_team(enemy_team, ball_position):
-            return False
-
-        return None
+            new_possession = True
+        elif self._check_posession_for_team(enemy_team, ball_position):
+            new_possession = False
+    
+        # if possession didn't change, no need to log
+        if new_possession == self.curr_possession:
+            return
+        
+        event_type = None
+        if new_possession:
+            event_type = EventType.FRIENDLY_POSSESSION_START
+        elif self.curr_possession:
+            event_type = EventType.FRIENDLY_POSSESSION_END
+        elif new_possession == False:
+            event_type = EventType.ENEMY_POSSESSION_START
+        else:
+            event_type = EventType.ENEMY_POSSESSION_END
+            
+        self.write_event(event_type=event_type)
+        
+        self.curr_possession = new_possession
 
     def _check_posession_for_team(
         self, team: tbots_cpp.Team, ball_position: tbots_cpp.Point
--- src/software/thunderscope/log/trackers/tracker.py
+++ src/software/thunderscope/log/trackers/tracker.py
@@ -2,35 +2,53 @@ from software.thunderscope.proto_unix_io import ProtoUnixIO
 from typing import Callable, Optional, Tuple, Type
 from software.thunderscope.thread_safe_buffer import ThreadSafeBuffer
 from google.protobuf.message import Message
-
+from proto.import_all_protos import *
+import software.python_bindings as tbots_cpp
+import os
+from software.thunderscope.log.trackers.tracked_event import EventType, get_event_from_world, event_to_csv_row
 
 class Tracker:
     """Generic tracker base class."""
 
     def __init__(
-        self, callback: Optional[Callable[..., None]] = None, buffer_size: int = 5
+        self, proto_unix_io: ProtoUnixIO, out_file_path: str, buffer_size: int = 5
     ):
         """Initializes the tracker with the given callback and buffer size
-
-        :param callback: the function to call when the tracker tracks an event
+             
+        :param proto_unix_io: the proto unix io to get the game state from
+        :param out_file_path: the file to write tracked events to               
         :param buffer_size: buffer size for the tracker's io
         """
-        self.callback = callback
         self.buffer_size = buffer_size
+        
+        # create path directories if they doesn't exist
+        os.makedirs(os.path.dirname(out_file_path), exist_ok=True)
 
-    def set_proto_unix_io(
-        self,
-        proto_unix_io: ProtoUnixIO,
-        type_buffers: list[Tuple[Type[Message], ThreadSafeBuffer]],
-    ) -> None:
-        """Registers the given message types and buffers to the given proto unix io connection
+        self.out_file = open(out_file_path, "w")
 
-        :param proto_unix_io: the io connection to listen on
-        :param type_buffers: a list of (Message Type, Buffer) tuples.
-                             messages of each type will be placed into their corresponding buffer
-        """
-        for message_type, buffer in type_buffers:
-            proto_unix_io.register_observer(message_type, buffer)
+        self.proto_unix_io = proto_unix_io
+        self.world_buffer = ThreadSafeBuffer(self.buffer_size, World)
+        self.proto_unix_io.register_observer(World, self.world_buffer)
+        
+        self.cached_world = None
 
     def refresh(self) -> None:
-        raise Exception("Not Implemented, please use the appropriate subclass!")
+        world_msg = self.world_buffer.get(block=False, return_cached=True)
+
+        if world_msg is None:
+            return
+
+        self.cached_world = tbots_cpp.World(world_msg)
+    
+    def write_event(self, event_type: EventType) -> None:
+        if not self.cached_world:
+            return
+        
+        event = get_event_from_world(
+            world=self.cached_world, event_type=event_type
+        )
+        
+        csv_row = event_to_csv_row(event)
+        
+        self.out_file.write(csv_row + "\n")
+        self.out_file.flush()
