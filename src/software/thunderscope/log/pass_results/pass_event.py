76f983719408336535cdd579ff7a32765da5a5d5
saurav <sauravbanna@gmail.com>
2026-03-31 01:17:14 -0700

    format

--- src/software/thunderscope/log/pass_results/pass_event.py
+++ src/software/thunderscope/log/pass_results/pass_event.py
@@ -1,7 +1,9 @@
 from software.thunderscope.log.trackers.tracked_event import (
-    TrackedEvent, EventType,
+    TrackedEvent,
+    EventType,
     event_to_csv_row as base_event_to_csv_row,
-    get_event_from_world, Team
+    get_event_from_world,
+    Team,
 )
 from dataclasses import dataclass
 from enum import auto, StrEnum
@@ -10,7 +12,9 @@ from proto.import_all_protos import *
 
 class PassResultType(StrEnum):
     """Enum for the different types of pass results we want to log"""
+
     RESULT_PRE = auto()
+
 
 @dataclass
 class TrackedPassResult:
@@ -24,23 +28,24 @@ class TrackedPassResult:
     pass_: Pass
     score: float
 
+
 def pass_vis_to_csv_row(pass_features: PassFeatures, team: Team) -> str:
     pass_event = get_event_from_world(
         world_msg=pass_features.world_state,
         event_type=EventType.PASS,
         from_team=team,
-        for_team=team
+        for_team=team,
     )
-    
+
     pass_result = TrackedPassResult(
         pass_event=pass_event,
         pass_result_type=PassResultType.RESULT_PRE,
         pass_=pass_features.pass_,
-        score=pass_features.score
+        score=pass_features.score,
     )
-    
+
     return result_to_csv_row(pass_result)
-    
+
 
 def result_to_csv_row(pass_result: TrackedPassResult) -> str:
     base_csv_str = base_event_to_csv_row(pass_result.pass_event)
