a1d308326231f8bb71ac48fd03711d30946eb36d
Eric Xiao <ericxiao0000@gmail.com>
2026-04-02 10:08:11 -0700

    Port stop play test

--- src/software/ai/hl/stp/play/stop_play_test.py
+++ src/software/ai/hl/stp/play/stop_play_test.py
@@ -1,75 +1,173 @@
-import software.python_bindings as tbots_cpp
-from proto.play_pb2 import PlayName
+import pytest
 
+import software.python_bindings as tbots_cpp
 from proto.import_all_protos import *
+from proto.play_pb2 import Play, PlayName
 from proto.ssl_gc_common_pb2 import Team
 from proto.message_translation.tbots_protobuf import create_world_state
 from software.simulated_tests.validation.robot_speed_threshold import (
-    RobotSpeedEventuallyBelowThreshold,
+    RobotSpeedAlwaysBelowThreshold,
 )
-from software.simulated_tests.simulated_test_fixture import (
-    pytest_main,
+from software.simulated_tests.validation.robot_enters_region import (
+    RobotNeverEntersRegion,
 )
+from software.simulated_tests.validation.delay_validation import DelayValidation
+from software.simulated_tests.simulated_test_fixture import pytest_main
 
 
-def test_stop_play(simulated_test_runner):
+@pytest.mark.parametrize(
+    "ball_position, blue_robot_positions",
+    [
+        # test_stop_play_ball_at_centre_robots_spread_out
+        (
+            tbots_cpp.Point(0, 0),
+            [
+                tbots_cpp.Point(-4, 0),
+                tbots_cpp.Point(-0.3, 0),
+                tbots_cpp.Point(0.3, 0),
+                tbots_cpp.Point(0, 0.3),
+                tbots_cpp.Point(-3, -1.5),
+                tbots_cpp.Point(4.6, -3.1),
+            ],
+        ),
+        # test_stop_play_friendly_half_robots_spread_out
+        (
+            tbots_cpp.Point(-1, 0),
+            [
+                tbots_cpp.Point(-4, 0),
+                tbots_cpp.Point(-2.3, 0),
+                tbots_cpp.Point(-1.7, 0),
+                tbots_cpp.Point(-2, 0.3),
+                tbots_cpp.Point(-3, -1.5),
+                tbots_cpp.Point(4.6, -3.1),
+            ],
+        ),
+        # test_stop_play_friendly_half_corner_robots_close_together
+        (
+            tbots_cpp.Point(-4, -2.5),
+            [
+                tbots_cpp.Point(-3, -2.5),
+                tbots_cpp.Point(-4, -2),
+                tbots_cpp.Point(-2, -2.5),
+                tbots_cpp.Point(-3, -2),
+                tbots_cpp.Point(-3.5, -2),
+                tbots_cpp.Point(-3, -1),
+            ],
+        ),
+        # test_stop_play_enemy_half_robots_spread_out
+        (
+            tbots_cpp.Point(2, 0),
+            [
+                tbots_cpp.Point(-4, 0),
+                tbots_cpp.Point(1.7, 0),
+                tbots_cpp.Point(2.3, 0),
+                tbots_cpp.Point(2, 0.3),
+                tbots_cpp.Point(-3, -1.5),
+                tbots_cpp.Point(3, -3),
+            ],
+        ),
+        # test_stop_play_enemy_half_corner_robots_close_together
+        (
+            tbots_cpp.Point(4, -2.5),
+            [
+                tbots_cpp.Point(2, -2.5),
+                tbots_cpp.Point(4, -1),
+                tbots_cpp.Point(3, -2.5),
+                tbots_cpp.Point(3, -2),
+                tbots_cpp.Point(3.5, -2),
+                tbots_cpp.Point(3, -1),
+            ],
+        ),
+        # test_stop_play_centre_robots_close_together
+        (
+            tbots_cpp.Point(0, 0),
+            [
+                tbots_cpp.Point(-2, 0),
+                tbots_cpp.Point(0, 0.3),
+                tbots_cpp.Point(0.3, 0),
+                tbots_cpp.Point(0, -0.3),
+                tbots_cpp.Point(-0.3, 0),
+                tbots_cpp.Point(0.2, 0.2),
+            ],
+        ),
+        # test_stop_play_ball_in_front_of_enemy_defense_area
+        (
+            tbots_cpp.Point(3, 0),
+            [
+                tbots_cpp.Point(-4.5, 2),
+                tbots_cpp.Point(0, 0.3),
+                tbots_cpp.Point(0.3, 0),
+                tbots_cpp.Point(0, -0.3),
+                tbots_cpp.Point(-0.3, 0),
+                tbots_cpp.Point(0.2, 0.2),
+            ],
+        ),
+        # test_stop_play_ball_in_front_of_friendly_defense_area
+        # TODO (#3630): robots stop within 0.5m of ball
+        # (
+        #     tbots_cpp.Point(-3, 0),
+        #     [
+        #         tbots_cpp.Point(-4.5, 2),
+        #         tbots_cpp.Point(0, 3),
+        #         tbots_cpp.Point(-1, -1),
+        #         tbots_cpp.Point(0, 0),
+        #         tbots_cpp.Point(-1, 0),
+        #         tbots_cpp.Point(2, 2),
+        #     ],
+        # ),
+    ],
+)
+def test_stop_play(ball_position, blue_robot_positions, simulated_test_runner):
+    field = tbots_cpp.Field.createSSLDivisionBField()
+
     def setup(*args):
-        # Ball at centre (matches C++ test_stop_play_ball_at_centre_robots_spread_out)
-        ball_initial_pos = tbots_cpp.Point(0, 0)
-
-        field = tbots_cpp.Field.createSSLDivisionBField()
-
-        blue_bots = [
-            tbots_cpp.Point(-4, 0),
-            tbots_cpp.Point(-0.3, 0),
-            tbots_cpp.Point(0.3, 0),
-            tbots_cpp.Point(0, 0.3),
-            tbots_cpp.Point(-3, -1.5),
-            tbots_cpp.Point(4.6, -3.1),
-        ]
-
-        yellow_bots = [
-            tbots_cpp.Point(1, 0),
-            tbots_cpp.Point(1, 2.5),
-            tbots_cpp.Point(1, -2.5),
-            field.enemyGoalCenter(),
-            field.enemyDefenseArea().negXNegYCorner(),
-            field.enemyDefenseArea().negXPosYCorner(),
-        ]
-
         simulated_test_runner.set_world_state(
             create_world_state(
-                yellow_robot_locations=yellow_bots,
-                blue_robot_locations=blue_bots,
-                ball_location=ball_initial_pos,
+                blue_robot_locations=blue_robot_positions,
+                yellow_robot_locations=[
+                    tbots_cpp.Point(1, 0),
+                    tbots_cpp.Point(1, 2.5),
+                    tbots_cpp.Point(1, -2.5),
+                    field.enemyGoalCenter(),
+                    field.enemyDefenseArea().negXNegYCorner(),
+                    field.enemyDefenseArea().negXPosYCorner(),
+                ],
+                ball_location=ball_position,
                 ball_velocity=tbots_cpp.Vector(0, 0),
-            )
+            ),
         )
 
-        # Game controller: STOP (both teams) - stop play behaviour
         simulated_test_runner.send_gamecontroller_command(
             gc_command=Command.Type.STOP, team=Team.UNKNOWN
-        )
-        simulated_test_runner.send_gamecontroller_command(
-            gc_command=Command.Type.FORCE_START, team=Team.UNKNOWN
         )
 
         simulated_test_runner.set_plays(
             blue_play=PlayName.StopPlay, yellow_play=PlayName.HaltPlay
         )
 
-    # C++ test waits 8s before checking; use 15s timeout so robots have time to slow.
-    # Threshold 1.4 m/s: expect robots to eventually slow below the 1.5 m/s STOP limit.
-    # TODO (#3638): add an eventually-validation that friendly robots stay at least 0.5 m away
-    # from the ball once pytest fixtures support delaying validations (to mirror the
-    # C++ robotsAvoidBall(0.5, ...) check).
-    eventually_validations = [[RobotSpeedEventuallyBelowThreshold(1.4)]]
+    # Wait 3 seconds for robots that start too close to the ball to move away
+    # Then validate robots slow down below 1.5 m/s
+    # And validate robots avoid ball (stay at least 0.5m away)
+    always_stop_play_rules = [
+        [
+            DelayValidation(
+                delay_s=3,
+                validation=RobotSpeedAlwaysBelowThreshold(speed_threshold=1.5),
+            ),
+            DelayValidation(
+                delay_s=3,
+                validation=RobotNeverEntersRegion(
+                    regions=[tbots_cpp.Circle(ball_position, 0.5)]
+                ),
+            ),
+        ]
+    ]
 
     simulated_test_runner.run_test(
         setup=setup,
-        inv_eventually_validation_sequence_set=eventually_validations,
-        ag_eventually_validation_sequence_set=eventually_validations,
-        test_timeout_s=15,
+        inv_always_validation_sequence_set=always_stop_play_rules,
+        ag_always_validation_sequence_set=always_stop_play_rules,
+        test_timeout_s=6,
     )
 
 
