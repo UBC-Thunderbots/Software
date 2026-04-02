e2497fb064395696ccfeb4411fba41d2b0cfafc1
Eric Xiao <ericxiao0000@gmail.com>
2026-04-02 09:41:13 -0700

    Add delay validation

---
+++ src/software/simulated_tests/validation/delay_validation.py
@@ -0,0 +1,42 @@
+from typing import override
+
+from proto.validation_pb2 import ValidationStatus, ValidationType
+
+from software.simulated_tests.validation.validation import (
+    Validation,
+)
+
+
+class DelayValidation(Validation):
+    def __init__(self, delay_s, validation):
+        """A validation wrapper that adds a delay to given validation before being evaluated"""
+        tick_duration_s = 0.0166
+
+        self.delay_s = delay_s
+        self.ticks_so_far = 0
+        self.delay_ticks = int(delay_s / tick_duration_s)
+        self.validation = validation
+
+    @override
+    def get_validation_status(self, world):
+        self.ticks_so_far += 1
+        if self.ticks_so_far > self.delay_ticks:
+            return self.validation.get_validation_status(world)
+        else:
+            return (
+                ValidationStatus.PASSING
+                if self.validation.get_validation_type() == ValidationType.ALWAYS
+                else ValidationStatus.FAILING
+            )
+
+    @override
+    def get_validation_geometry(self, world):
+        return self.validation.get_validation_geometry(world)
+
+    @override
+    def __repr__(self):
+        return f"Delayed validation after {self.delay_s} seconds for {self.validation.__repr__()}"
+
+    @override
+    def get_validation_type(self, world):
+        return self.validation.get_validation_type()
