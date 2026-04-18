92ceb8d914a6cbd4f97b70d15d1a572d3dabcadb
williamckha <williamha@outlook.com>
2026-04-17 20:34:41 -0700

    Lower max acceleration

--- src/shared/robot_constants.cpp
+++ src/shared/robot_constants.cpp
@@ -17,10 +17,10 @@ RobotConstants create2026RobotConstants()
         .max_force_dribbler_speed_rpm  = -12000,
 
         // Motor constant
-        .motor_max_acceleration_m_per_s_2 = 4.5f,
+        .motor_max_acceleration_m_per_s_2 = 2.0f,
 
         // Robot's linear movement constants
-        .robot_max_speed_m_per_s          = 3.000f,
+        .robot_max_speed_m_per_s          = 3.0f,
         .robot_max_acceleration_m_per_s_2 = 3.0f,
         .robot_max_deceleration_m_per_s_2 = 3.0f,
 
