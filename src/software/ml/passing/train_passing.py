f6eb940e19376681edf85493e2a03168f0df538b
saurav <sauravbanna@gmail.com>
2026-05-02 12:23:11 -0700

    back to normal weights

--- src/software/ml/passing/train_passing.py
+++ src/software/ml/passing/train_passing.py
@@ -408,4 +408,4 @@ if __name__ == "__main__":
 
     print("Dataset generated!")
 
-    train_and_export_models(graphs, labels, interval_scaled_weights)
+    train_and_export_models(graphs, labels, label_weights)
