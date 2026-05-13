d80dacc4aeb2abd70f8832b00f3beeffe4048873
saurav <sauravbanna@gmail.com>
2026-05-12 22:34:01 -0700

    remove ml related changes

--- src/software/ml/data_cleanup/result_interface.py
+++
@@ -1,8 +0,0 @@
-from abc import abstractmethod
-from software.evaluation.logs.log_interface import TimestampedEvalLog
-
-
-class IResult:
-    @abstractmethod
-    def update_result(self, eval_log: TimestampedEvalLog) -> None:
-        raise NotImplementedError("Please use the appropriate subclass!")
--- src/software/thunderscope/constants.py
+++ src/software/thunderscope/constants.py
@@ -403,15 +403,10 @@ class RuntimeManagerConstants:
     RUNTIME_CONFIG_PATH = f"{EXTERNAL_RUNTIMES_PATH}/runtime_config.toml"
 
     RUNTIME_EVENTS_DIRECTORY_PATH = (
-        "/home/thunderbots/Software/src/software/ml/datasets"
+        "/tmp/tbots/stats"
     )
     RUNTIME_EVENTS_FILE = "game_events.csv"
 
     RELEASES_URL = "https://api.github.com/repos/UBC-Thunderbots/Software/releases"
     DOWNLOAD_URL = "https://github.com/UBC-Thunderbots/Software/releases/download/"
     MAX_RELEASES_FETCHED = 5
-
-
-class PassResultsConstants:
-    PASS_RESULTS_DIRECTORY_PATH = "/home/thunderbots/Software/src/software/ml/datasets"
-    PASS_RESULTS_FILE_NAME = "pass_results.csv"
