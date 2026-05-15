e27a65ebc4269056b7ddb5a1274c9c5d96109c55
Samuel Ubuntu Laptop <lausingsamuel@gmail.com>
2026-05-14 19:08:18 -0700

    cleanup

--- src/software/ai/hl/stp/tactic/goalie/goalie_tactic_test.py
+++ src/software/ai/hl/stp/tactic/goalie/goalie_tactic_test.py
@@ -1,18 +1,18 @@
 import pytest
-import software.python_bindings as tbots_cpp
 
+import software.python_bindings as tbots_cpp
 from proto.import_all_protos import *
-from proto.message_translation.tbots_protobuf import create_world_state
+from software.simulated_tests.validation.robot_enters_region import *
+from software.simulated_tests.validation.ball_enters_region import *
+from software.simulated_tests.validation.ball_moves_in_direction import *
+from software.simulated_tests.validation.friendly_has_ball_possession import *
+from software.simulated_tests.validation.ball_speed_threshold import *
+from software.simulated_tests.validation.robot_speed_threshold import *
+from software.simulated_tests.validation.excessive_dribbling import *
 from software.simulated_tests.simulated_test_fixture import (
     pytest_main,
 )
-from software.simulated_tests.validation.ball_enters_region import *
-from software.simulated_tests.validation.ball_moves_in_direction import *
-from software.simulated_tests.validation.ball_speed_threshold import *
-from software.simulated_tests.validation.excessive_dribbling import *
-from software.simulated_tests.validation.friendly_has_ball_possession import *
-from software.simulated_tests.validation.robot_enters_region import *
-from software.simulated_tests.validation.robot_speed_threshold import *
+from proto.message_translation.tbots_protobuf import create_world_state
 
 
 @pytest.mark.parametrize(
