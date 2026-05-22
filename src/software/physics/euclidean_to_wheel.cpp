e541f5d1118bbf71d0c40f75eb19244e80268835
sunghyuneun <gregoryeun@gmail.com>
2026-05-22 12:29:10 -0700

    Delete silly comment

--- src/software/physics/euclidean_to_wheel.cpp
+++ src/software/physics/euclidean_to_wheel.cpp
@@ -8,9 +8,6 @@
 #include "software/geom/angular_velocity.h"
 #include "software/geom/vector.h"
 
-// get rid of this stupid ifdef, and then look at the issue
-
-// Turn this to
 #if CHECK_VERSION(2026)
 EuclideanToWheel::EuclideanToWheel(const robot_constants::RobotConstants& robot_constants)
     : robot_constants_(robot_constants)
