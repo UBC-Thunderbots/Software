c292a1dcd2fc1acd63d0651a339fdb3c6012d481
Eric Xiao <ericxiao0000@gmail.com>
2026-05-22 13:03:48 -0700

    Add position controller basic test cases

--- src/software/embedded/position_controller/position_controller_test.cpp
+++ src/software/embedded/position_controller/position_controller_test.cpp
@@ -2,6 +2,24 @@
 
 #include <gtest/gtest.h>
 
-TEST(PositionControllerTest, TestConstructor) {
-	PositionController controller{};
+TEST(PositionControllerTest, TestConstructor)
+{
+    PositionController controller{};
+
+    Vector error{-3.0, 1.0};
+    Vector control_effort = controller.step(error);
+
+    EXPECT_DOUBLE_EQ(error.x() * 0.8, control_effort.x());
+    EXPECT_DOUBLE_EQ(error.y() * 0.8, control_effort.y());
+}
+
+TEST(PositionControllerTest, TestClosePid)
+{
+    PositionController controller{};
+
+    Vector error{0.005, 0.05};
+    Vector control_effort = controller.step(error);
+
+    EXPECT_DOUBLE_EQ(error.x() * 2.0, control_effort.x());
+    EXPECT_DOUBLE_EQ(error.y() * 2.0, control_effort.y());
 }
