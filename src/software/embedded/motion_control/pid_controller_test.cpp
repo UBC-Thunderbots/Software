2a39569e785bb4f0f81ad646828e00942be3cfc2
Eric Xiao <ericxiao0000@gmail.com>
2026-05-27 08:05:40 -0700

    Implement PID/Position/Orientation controllers (#3730)
    * Add PID controller class and basic test cases
    
    * Rename directory to position_controller
    
    * Create position controller class specs
    
    * Add missing BUILD file
    
    * Fix BUILD file
    
    * Add position controller basic test cases
    
    * Implement position controller
    
    * Re-write position controller for feedforward
    
    * Rename directory to motion_control
    
    * Add specification for OrientationController
    
    * Implement OrientationController
    
    * Remove orientation and position controller tests
    
    * Add motion_control BUILD file
    
    * Add reset() method
    
    * Update parameter names and doc comments
    
    * Remove old tests
    
    * Tag issue in TODO
    
    * Formatting
    
    * Remove default argument for delta_time
    
    * Remove integrator reset on error sign swap
    
    * Add doc comment
    
    * Use length instead of lengthSquared checking for pure pid
    
    * Add generic motion controller interface
    
    * Fix include
    
    * Add test placeholders for position and orientation controller
    
    * Use assertion instead of throwing exception
    
    * Remove exception tests
    
    * Make pid controller gain variables private

---
+++ src/software/embedded/motion_control/pid_controller_test.cpp
@@ -0,0 +1,75 @@
+#include "software/embedded/motion_control/pid_controller.h"
+
+#include <gtest/gtest.h>
+
+TEST(PidControllerTest, OnlyProportionTermNonZero)
+{
+    PidController pid{1.0, 0.0, 0.0, 0.0};
+
+    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), 0.0);
+    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), 0.0);
+    EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), 1.0);
+    EXPECT_DOUBLE_EQ(pid.step(8.0, 1.0), 8.0);
+    EXPECT_DOUBLE_EQ(pid.step(-5.0, 1.0), -5.0);
+    EXPECT_DOUBLE_EQ(pid.step(-1.0, 1.0), -1.0);
+
+    pid = PidController{5.0, 0.0, 0.0, 0.0};
+
+    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), 0.0);
+    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), 0.0);
+    EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), 5.0);
+    EXPECT_DOUBLE_EQ(pid.step(8.0, 1.0), 40.0);
+    EXPECT_DOUBLE_EQ(pid.step(-5.0, 1.0), -25.0);
+    EXPECT_DOUBLE_EQ(pid.step(-1.0, 1.0), -5.0);
+}
+
+TEST(PidControllerTest, OnlyIntegralTermNonZero)
+{
+    constexpr double k_i = 2.0;
+    PidController pid{0.0, k_i, 0.0, 10.0};
+
+    EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), k_i * 1.0);
+    EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), k_i * 2.0);
+    EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), k_i * 3.0);
+    EXPECT_DOUBLE_EQ(pid.step(0.5, 1.0), k_i * 3.5);
+
+    EXPECT_DOUBLE_EQ(pid.step(-0.2, 1.0), k_i * 3.3);
+    EXPECT_DOUBLE_EQ(pid.step(-1.0, 1.0), k_i * 2.3);
+    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), k_i * 2.3);
+
+    // should not accumulate integral term above max_integral
+    EXPECT_DOUBLE_EQ(pid.step(6.7, 1.0), k_i * 9.0);
+    EXPECT_DOUBLE_EQ(pid.step(5.0, 1.0), k_i * 10.0);
+    EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), k_i * 10.0);
+}
+
+TEST(PidControllerTest, OnlyDerivativeTermNonZero)
+{
+    constexpr double k_d = 3.0;
+    PidController pid{0.0, 0.0, k_d, 0.0};
+
+    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), k_d * 0.0);
+    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), k_d * 0.0);
+    EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), k_d * 1.0);
+    EXPECT_DOUBLE_EQ(pid.step(8.0, 1.0), k_d * 7.0);
+    EXPECT_DOUBLE_EQ(pid.step(-5.0, 1.0), k_d * -13.0);
+    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), k_d * 5.0);
+    EXPECT_DOUBLE_EQ(pid.step(5.0, 0.5), k_d * 5.0 / 0.5);
+    EXPECT_DOUBLE_EQ(pid.step(8.0, 2.0), k_d * 3.0 / 2.0);
+}
+
+TEST(PidControllerTest, GeneralApplication)
+{
+    constexpr double k_p          = 10.0;
+    constexpr double k_i          = 3.0;
+    constexpr double k_d          = -1.0;
+    constexpr double max_integral = 10.0;
+    PidController pid{k_p, k_i, k_d, max_integral};
+
+    EXPECT_DOUBLE_EQ(pid.step(12.0, 0.75), k_p * 12.0 + k_i * 9.0 + k_d * 0.0);
+    EXPECT_DOUBLE_EQ(pid.step(24.0, 0.75), k_p * 24.0 + k_i * 10.0 + k_d * 12.0 / 0.75);
+    EXPECT_DOUBLE_EQ(pid.step(4.0, 1.0), k_p * 4.0 + k_i * 10.0 + k_d * -20.0);
+    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), k_p * 0.0 + k_i * 10.0 + k_d * -4.0);
+    EXPECT_DOUBLE_EQ(pid.step(2.0, 1.0), k_p * 2.0 + k_i * 10.0 + k_d * 2.0);
+    EXPECT_DOUBLE_EQ(pid.step(-2.0, 1.0), k_p * -2.0 + k_i * 8.0 + k_d * -4.0);
+}
