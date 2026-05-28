294a02841524aaa7bd1003b60377e83d79206b83
Grayson <grayhoang@gmail.com>
2026-05-28 16:39:01 -0700

    link comments to issues

--- src/software/embedded/motor_controller/stspin_motor_controller.h
+++ src/software/embedded/motor_controller/stspin_motor_controller.h
@@ -32,6 +32,7 @@ class StSpinMotorController : public MotorController
     void immediatelyDisable() override;
 
    private:
+   // TODO: #3750 Use a template function instead of std::variant.
     using OutgoingFrame =
         std::variant<NoOpFrame, SetResponseTypeFrame, SetTargetSpeedFrame,
                      SetTargetTorqueFrame, SetPidTorqueKpKiFrame, SetPidFluxKpKiFrame,
@@ -62,6 +63,8 @@ class StSpinMotorController : public MotorController
 
     static constexpr int MAX_SPEED_FEED_FORWARD_STATIC_GAIN = 750;
     static constexpr int MIN_SPEED_FEED_FORWARD_STATIC_GAIN = 300;
+    static constexpr double MINIMUM_SPEED_FOR_FEED_FORWARD = 0.01;
+
     robot_constants::RobotConstants robot_constants_;
 
     // SPI file descriptors
