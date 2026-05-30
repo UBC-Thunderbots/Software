2808eccbbe39af8ed306db8c957584ba84a38c6e
Eric Xiao <ericxiao0000@gmail.com>
2026-05-30 14:00:11 -0700

    Add field test runner to gameplay test fixture

---
+++ src/software/gameplay_tests/field_test_runner.py
@@ -0,0 +1,191 @@
+import queue
+import time
+import threading
+
+import pytest
+from proto.import_all_protos import *
+
+from software.gameplay_tests.validation import validation
+from software.logger.logger import create_logger
+
+
+from software.gameplay_tests.tbots_test_runner import TbotsTestRunner
+from software.py_constants import *
+from typing import override
+
+logger = create_logger(__name__)
+
+WORLD_BUFFER_TIMEOUT = 5.0
+PROCESS_BUFFER_DELAY_S = 0.01
+PAUSE_AFTER_FAIL_DELAY_S = 3
+LAUNCH_DELAY_S = 0.1
+TEST_END_DELAY = 0.3
+
+
+class FieldTestRunner(TbotsTestRunner):
+    """Run a field test"""
+
+    def __init__(
+        self,
+        test_name,
+        thunderscope,
+        blue_full_system_proto_unix_io,
+        yellow_full_system_proto_unix_io,
+        gamecontroller,
+        publish_validation_protos=True,
+        is_yellow_friendly=False,
+    ):
+        """Initialize the FieldTestRunner
+
+        :param test_name: The name of the test to run
+        :param thunderscope: The Thunderscope to use, None if not used
+        :param blue_full_system_proto_unix_io: The blue full system proto unix io to use
+        :param yellow_full_system_proto_unix_io: The yellow full system proto unix io to use
+        :param gamecontroller: The gamecontroller context managed instance
+        :param publish_validation_protos: whether to publish validation protos
+        :param: is_yellow_friendly: if yellow is the friendly team
+        """
+        super(FieldTestRunner, self).__init__(
+            test_name,
+            thunderscope,
+            blue_full_system_proto_unix_io,
+            yellow_full_system_proto_unix_io,
+            gamecontroller,
+            is_yellow_friendly,
+        )
+        self.publish_validation_protos = publish_validation_protos
+        self.is_yellow_friendly = is_yellow_friendly
+
+        logger.info("determining robots on field")
+        # survey field for available robot ids
+        try:
+            world = self.world_buffer.get(block=True, timeout=WORLD_BUFFER_TIMEOUT)
+            self.initial_world = world
+            self.friendly_robot_ids_field = [
+                robot.id for robot in world.friendly_team.team_robots
+            ]
+
+            logger.info(f"friendly team ids {self.friendly_robot_ids_field}")
+
+            if len(self.friendly_robot_ids_field) == 0:
+                raise Exception("no friendly robots found on field")
+
+        except queue.Empty:
+            raise Exception(
+                f"No Worlds were received with in {WORLD_BUFFER_TIMEOUT} seconds. Please make sure atleast 1 robot and 1 ball is present on the field."
+            )
+
+    @override
+    def send_gamecontroller_command(
+        self,
+        gc_command: proto.ssl_gc_state_pb2.Command,
+        team: proto.ssl_gc_common_pb2.Team,
+        final_ball_placement_point=None,
+    ):
+        """Send a command to the gamecontroller
+
+        :param gc_command: The command to send
+        :param team: The team which the command as attributed to
+        :param final_ball_placement_point: The ball placement point
+        """
+        self.gamecontroller.send_ci_input(
+            gc_command=gc_command,
+            team=team,
+            final_ball_placement_point=final_ball_placement_point,
+        )
+
+    @override
+    def run_test(
+        self,
+        always_validation_sequence_set=[[]],
+        eventually_validation_sequence_set=[[]],
+        test_timeout_s=3,
+    ):
+        """Run a test. In a field test this means beginning validation.
+
+        :param always_validation_sequence_set: Validation functions that should
+                                hold on every tick
+        :param eventually_validation_sequence_set: Validation that should
+                                eventually be true, before the test ends
+        :param test_timeout_s: The timeout for the test, if any eventually_validations
+                                remain after the timeout, the test fails.
+        """
+
+        def stop_test(delay):
+            time.sleep(delay)
+            if self.thunderscope:
+                self.thunderscope.close()
+
+        def runner():
+            time.sleep(LAUNCH_DELAY_S)
+
+            test_end_time = time.time() + test_timeout_s
+
+            while time.time() < test_end_time:
+                while True:
+                    try:
+                        world = self.world_buffer.get(
+                            block=True, timeout=WORLD_BUFFER_TIMEOUT
+                        )
+                        break
+                    except queue.Empty:
+                        # If we timeout, that means full_system missed the last
+                        # wrapper and robot status, lets resend it.
+                        logger.warning(
+                            f"No World was received for {WORLD_BUFFER_TIMEOUT} seconds. Ending test early."
+                        )
+
+                # Validate
+                (
+                    eventually_validation_proto_set,
+                    always_validation_proto_set,
+                ) = validation.run_validation_sequence_sets(
+                    world,
+                    eventually_validation_sequence_set,
+                    always_validation_sequence_set,
+                )
+
+                if self.publish_validation_protos:
+                    # Set the test name
+                    eventually_validation_proto_set.test_name = self.test_name
+                    always_validation_proto_set.test_name = self.test_name
+
+                    # Send out the validation proto to thunderscope
+                    self.blue_full_system_proto_unix_io.send_proto(
+                        ValidationProtoSet, eventually_validation_proto_set
+                    )
+                    self.blue_full_system_proto_unix_io.send_proto(
+                        ValidationProtoSet, always_validation_proto_set
+                    )
+
+                # Check that all always validations are always valid
+                validation.check_validation(always_validation_proto_set)
+
+            # Check that all eventually validations are eventually valid
+            validation.check_validation(eventually_validation_proto_set)
+            stop_test(TEST_END_DELAY)
+
+        def excepthook(args):
+            """This function is _critical_ for show_thunderscope to work.
+            If the test Thread will raises an exception we won't be able to close
+            the window from the main thread.
+
+            :param args: The args passed in from the hook
+            """
+            stop_test(delay=PAUSE_AFTER_FAIL_DELAY_S)
+            self.last_exception = args.exc_value
+            raise self.last_exception
+
+        threading.excepthook = excepthook
+
+        if self.thunderscope:
+            run_test_thread = threading.Thread(target=runner, daemon=True)
+            run_test_thread.start()
+            self.thunderscope.show()
+            run_test_thread.join()
+
+            if self.last_exception:
+                pytest.fail(str(ex.last_exception))
+
+        else:
+            runner()
--- src/software/gameplay_tests/fixture.py
+++ src/software/gameplay_tests/fixture.py
@@ -1,48 +1,163 @@
-import os
 import time
 
 import pytest
-
-from software.gameplay_tests.util import load_command_line_arguments
-from software.gameplay_tests.simulated_test_runner import (
-    InvariantTestRunner,
-    AggregateTestRunner,
+from software.py_constants import (
+    MAX_ROBOT_IDS_PER_SIDE,
+    SSL_REFEREE_PORT,
+    getRobotMulticastChannel,
 )
+
+from proto.import_all_protos import World
+from software.gameplay_tests.field_test_runner import FieldTestRunner
+from software.gameplay_tests.simulated_test_runner import (
+    AggregateTestRunner,
+    InvariantTestRunner,
+)
+from software.gameplay_tests.util import get_current_pytest, load_command_line_arguments
+from software.logger.logger import create_logger
 from software.thunderscope.binary_context_managers.full_system import FullSystem
 from software.thunderscope.binary_context_managers.game_controller import Gamecontroller
 from software.thunderscope.binary_context_managers.simulator import Simulator
+from software.thunderscope.constants import EstopMode, IndividualRobotMode
+from software.thunderscope.estop_helpers import get_estop_config
 from software.thunderscope.proto_unix_io import ProtoUnixIO
+from software.thunderscope.robot_communication import RobotCommunication
 from software.thunderscope.thunderscope import Thunderscope
-from software.thunderscope.thunderscope_config import configure_simulated_test_view
+from software.thunderscope.thunderscope_config import (
+    configure_field_test_view,
+    configure_simulated_test_view,
+)
+from software.thunderscope.wifi_communication_manager import WifiCommunicationManager
+
+logger = create_logger(__name__)
 
 LAUNCH_DELAY_S = 0.1
 
 
 @pytest.fixture
 def gameplay_test_runner():
-    args = load_command_line_arguments()
-    tscope = None
+    args = load_command_line_arguments(allow_unrecognized=True)
 
-    aggregate = args.aggregate
+    if args.run_field_test:
+        yield from create_field_test_runner(args)
+    else:
+        yield from create_simulated_test_runner(args)
+
+
+def create_field_test_runner(args):
+    # Grab the current test name to store the proto log for the test case
+    current_test = get_current_pytest()
+    test_name = get_current_pytest().split("-")[0]
 
     simulator_proto_unix_io = ProtoUnixIO()
     yellow_full_system_proto_unix_io = ProtoUnixIO()
     blue_full_system_proto_unix_io = ProtoUnixIO()
 
-    # Grab the current test name to store the proto log for the test case
-    current_test = os.environ.get("PYTEST_CURRENT_TEST").split(":")[-1].split(" ")[0]
-    current_test = current_test.replace("]", "")
-    current_test = current_test.replace("[", "-")
+    runtime_dir = f"{args.blue_full_system_runtime_dir}/test/{test_name}"
+    friendly_proto_unix_io = blue_full_system_proto_unix_io
 
-    test_name = current_test.split("-")[0]
+    if args.run_yellow:
+        debug_full_sys = args.debug_yellow_full_system
+        runtime_dir = f"{args.yellow_full_system_runtime_dir}/test/{test_name}"
+        friendly_proto_unix_io = yellow_full_system_proto_unix_io
+
+    estop_mode, estop_path = get_estop_config(
+        args.keyboard_estop, args.disable_communication
+    )
 
     # Launch all binaries
     with (
-        Simulator(
-            f"{args.simulator_runtime_dir}/test/{test_name}",
-            args.debug_simulator,
-            args.enable_realism,
-        ) as simulator,
+        FullSystem(
+            "software/unix_full_system",
+            full_system_runtime_dir=runtime_dir,
+            debug_full_system=debug_full_sys,
+            friendly_colour_yellow=args.run_yellow,
+            should_restart_on_crash=False,
+        ) as friendly_fs,
+        Gamecontroller(
+            # we would be using conventional port if and only if we are playing in robocup.
+            suppress_logs=(not args.show_gamecontroller_logs),
+            use_conventional_port=False,
+        ) as gamecontroller,
+        WifiCommunicationManager(
+            current_proto_unix_io=friendly_proto_unix_io,
+            multicast_channel=getRobotMulticastChannel(args.channel),
+            should_setup_full_system=True,
+            interface=args.interface,
+            referee_port=gamecontroller.get_referee_port()
+            if gamecontroller
+            else SSL_REFEREE_PORT,
+        ) as wifi_communication_manager,
+        RobotCommunication(
+            current_proto_unix_io=friendly_proto_unix_io,
+            communication_manager=wifi_communication_manager,
+            estop_mode=estop_mode,
+            estop_path=estop_path,
+        ) as rc_friendly,
+    ):
+        friendly_fs.setup_proto_unix_io(friendly_proto_unix_io)
+
+        gamecontroller.setup_proto_unix_io(
+            blue_full_system_proto_unix_io=blue_full_system_proto_unix_io,
+            yellow_full_system_proto_unix_io=yellow_full_system_proto_unix_io,
+            simulator_proto_unix_io=simulator_proto_unix_io,
+        )
+        # Inject the proto unix ios into thunderscope and start the test
+        tscope = Thunderscope(
+            configure_field_test_view(
+                simulator_proto_unix_io=simulator_proto_unix_io,
+                blue_full_system_proto_unix_io=blue_full_system_proto_unix_io,
+                yellow_full_system_proto_unix_io=yellow_full_system_proto_unix_io,
+                yellow_is_friendly=args.run_yellow,
+            ),
+            layout_path=None,
+        )
+
+        # Set control mode for all robots to AI so that packets are sent to the robots
+        for robot_id in range(MAX_ROBOT_IDS_PER_SIDE):
+            rc_friendly.toggle_individual_robot_control_mode(
+                robot_id,
+                IndividualRobotMode.AI,
+            )
+
+        # connect the keyboard estop toggle to the key event if needed
+        if estop_mode == EstopMode.KEYBOARD_ESTOP:
+            tscope.keyboard_estop_shortcut.activated.connect(
+                rc_friendly.toggle_keyboard_estop
+            )
+            # we call this method to enable estop automatically when a field test starts
+            rc_friendly.toggle_keyboard_estop()
+            logger.warning(
+                "\x1b[31;20m"
+                + "Keyboard Estop Enabled, robots will start moving automatically when test starts!"
+                + "\x1b[0m"
+            )
+
+        time.sleep(LAUNCH_DELAY_S)
+        runner = FieldTestRunner(
+            test_name=current_test,
+            blue_full_system_proto_unix_io=blue_full_system_proto_unix_io,
+            yellow_full_system_proto_unix_io=yellow_full_system_proto_unix_io,
+            gamecontroller=gamecontroller,
+            thunderscope=tscope,
+            is_yellow_friendly=args.run_yellow,
+        )
+
+        friendly_proto_unix_io.register_observer(World, runner.world_buffer)
+
+        yield runner
+
+
+def create_simulated_test_runner(args):
+    # Grab the current test name to store the proto log for the test case
+    current_test = get_current_pytest()
+    test_name = get_current_pytest().split("-")[0]
+
+    simulator_proto_unix_io = ProtoUnixIO()
+    yellow_full_system_proto_unix_io = ProtoUnixIO()
+    blue_full_system_proto_unix_io = ProtoUnixIO()
+
+    with (
         FullSystem(
             "software/unix_full_system",
             f"{args.blue_full_system_runtime_dir}/test/{test_name}",
@@ -59,6 +174,11 @@ def gameplay_test_runner():
             should_restart_on_crash=False,
             running_in_realtime=args.enable_thunderscope,
         ) as yellow_fs,
+        Simulator(
+            f"{args.simulator_runtime_dir}/test/{test_name}",
+            args.debug_simulator,
+            args.enable_realism,
+        ) as simulator,
     ):
         with Gamecontroller(
             suppress_logs=(not args.show_gamecontroller_logs)
@@ -79,6 +199,7 @@ def gameplay_test_runner():
 
             # If we want to run thunderscope, inject the proto unix ios
             # and start the test
+            tscope = None
             if args.enable_thunderscope:
                 tscope = Thunderscope(
                     configure_simulated_test_view(
@@ -94,7 +215,7 @@ def gameplay_test_runner():
             runner = None
 
             # Initialise the right runner based on which testing mode is selected
-            if aggregate:
+            if args.aggregate:
                 runner = AggregateTestRunner(
                     current_test,
                     tscope,
