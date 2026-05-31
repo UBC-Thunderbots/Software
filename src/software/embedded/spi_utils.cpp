d1818d3aedfc340ac6ed285e2ecfa8881c365eb6
adrianchan787 <castlevictory1@gmail.com>
2026-05-30 18:40:42 -0700

    Further changes that didn't save

--- src/software/embedded/spi_utils.cpp
+++ src/software/embedded/spi_utils.cpp
@@ -30,23 +30,23 @@ void spiTransfer(int fd, const std::array<uint8_t, len>& tx, std::array<uint8_t,
 
 template <uint32_t read_len, uint32_t write_len>
 void readThenWriteSpiTransfer(int fd, const std::array<uint8_t, read_len>& read_tx,
-                              std::array<uint8_t, write_len>& write_tx,
-                              const std::array<uint8_t, read_len>& read_rx,
-                              int32_t spi_speed)
+                              const std::array<uint8_t, write_len>& write_tx,
+                              std::array<uint8_t, read_len>& read_rx,
+                              uint32_t spi_speed)
 {
     uint8_t write_rx[5] = {};
 
     struct spi_ioc_transfer tr[2];
     memset(tr, 0, sizeof(tr));
 
-    tr[0].tx_buf        = reinterpret_cast<uintptr_t>(read_tx);
-    tr[0].rx_buf        = reinterpret_cast<uintptr_t>(read_rx);
+    tr[0].tx_buf        = reinterpret_cast<uintptr_t>(read_tx.data());
+    tr[0].rx_buf        = reinterpret_cast<uintptr_t>(read_rx.data());
     tr[0].len           = read_len;
     tr[0].delay_usecs   = 0;
     tr[0].speed_hz      = spi_speed;
     tr[0].bits_per_word = 8;
     tr[0].cs_change     = 0;
-    tr[1].tx_buf        = reinterpret_cast<uintptr_t>(write_tx);
+    tr[1].tx_buf        = reinterpret_cast<uintptr_t>(write_tx.data());
     tr[1].rx_buf        = reinterpret_cast<uintptr_t>(write_rx);
     tr[1].len           = write_len;
     tr[1].delay_usecs   = 0;
