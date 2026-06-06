6b400ef70d06952e0aa06cd0b6c1f32585d398b6
Eric Xiao <ericxiao0000@gmail.com>
2026-06-06 15:05:25 -0700

    Tune more

--- src/software/embedded/motion_control/position_controller.h
+++ src/software/embedded/motion_control/position_controller.h
@@ -38,8 +38,8 @@ class PositionController : public MotionController<Point, TrajectoryPath, Vector
     PidController x_pid_{2.0, 0.0, 0.0, 0.0};
     PidController y_pid_{2.0, 0.0, 0.0, 0.0};
 
-    PidController x_pid_close_{2.0, 0.0, 0.0, 0.0};
-    PidController y_pid_close_{2.0, 0.0, 0.0, 0.0};
+    PidController x_pid_close_{2.0, 1.0, 0.0, 5.0};
+    PidController y_pid_close_{2.0, 1.0, 0.0, 5.0};
 
     static constexpr double LINEAR_PURE_PID_THRESHOLD_METERS = 0.1;
 };
