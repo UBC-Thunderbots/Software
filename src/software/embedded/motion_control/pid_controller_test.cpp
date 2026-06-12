c13928fe19e530f0f3d8dd2e6468c1968ca96d03
Andrew Mao <andrewyxmao@gmail.com>
2026-06-12 14:43:30 -0700

    pid

--- src/software/embedded/motion_control/pid_controller_test.cpp
+++ src/software/embedded/motion_control/pid_controller_test.cpp
@@ -4,7 +4,7 @@
 
 TEST(PidControllerTest, OnlyProportionTermNonZero)
 {
-    PidController pid{1.0, 0.0, 0.0, 0.0};
+    PidController<double> pid{1.0, 0.0, 0.0, 0.0};
 
     EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), 0.0);
     EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), 0.0);
@@ -13,7 +13,7 @@ TEST(PidControllerTest, OnlyProportionTermNonZero)
     EXPECT_DOUBLE_EQ(pid.step(-5.0, 1.0), -5.0);
     EXPECT_DOUBLE_EQ(pid.step(-1.0, 1.0), -1.0);
 
-    pid = PidController{5.0, 0.0, 0.0, 0.0};
+    pid = PidController<double>{5.0, 0.0, 0.0, 0.0};
 
     EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), 0.0);
     EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), 0.0);
@@ -26,27 +26,32 @@ TEST(PidControllerTest, OnlyProportionTermNonZero)
 TEST(PidControllerTest, OnlyIntegralTermNonZero)
 {
     constexpr double k_i = 2.0;
-    PidController pid{0.0, k_i, 0.0, 10.0};
+    PidController<double> pid{0.0, k_i, 0.0, 10.0};
 
     EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), k_i * 1.0);
     EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), k_i * 2.0);
     EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), k_i * 3.0);
     EXPECT_DOUBLE_EQ(pid.step(0.5, 1.0), k_i * 3.5);
 
-    EXPECT_DOUBLE_EQ(pid.step(-0.2, 1.0), k_i * 3.3);
-    EXPECT_DOUBLE_EQ(pid.step(-1.0, 1.0), k_i * 2.3);
-    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), k_i * 2.3);
+    // Sign swap should reset integrator to 0, then add -0.2
+    EXPECT_DOUBLE_EQ(pid.step(-0.2, 1.0), k_i * -0.2);
+    EXPECT_DOUBLE_EQ(pid.step(-1.0, 1.0), k_i * -1.2);
+    // Sign swap back to 0.0? No, 0.0 doesn't swap sign usually (0*X is 0, not < 0)
+    // My implementation: last_error_.value() * error < T(0.0)
+    // If error is 0, it's not < 0.
+    EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), k_i * -1.2);
 
     // should not accumulate integral term above max_integral
-    EXPECT_DOUBLE_EQ(pid.step(6.7, 1.0), k_i * 9.0);
-    EXPECT_DOUBLE_EQ(pid.step(5.0, 1.0), k_i * 10.0);
+    EXPECT_DOUBLE_EQ(pid.step(6.7, 1.0), k_i * 5.5);  // Sign not swapped from 0.0 to 6.7
+    EXPECT_DOUBLE_EQ(pid.step(5.0, 1.0),
+                     k_i * 10.0);  // Clamped (5.5 + 5.0 = 10.5 -> 10.0)
     EXPECT_DOUBLE_EQ(pid.step(1.0, 1.0), k_i * 10.0);
 }
 
 TEST(PidControllerTest, OnlyDerivativeTermNonZero)
 {
     constexpr double k_d = 3.0;
-    PidController pid{0.0, 0.0, k_d, 0.0};
+    PidController<double> pid{0.0, 0.0, k_d, 0.0};
 
     EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), k_d * 0.0);
     EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), k_d * 0.0);
@@ -64,12 +69,13 @@ TEST(PidControllerTest, GeneralApplication)
     constexpr double k_i          = 3.0;
     constexpr double k_d          = -1.0;
     constexpr double max_integral = 10.0;
-    PidController pid{k_p, k_i, k_d, max_integral};
+    PidController<double> pid{k_p, k_i, k_d, max_integral};
 
     EXPECT_DOUBLE_EQ(pid.step(12.0, 0.75), k_p * 12.0 + k_i * 9.0 + k_d * 0.0);
     EXPECT_DOUBLE_EQ(pid.step(24.0, 0.75), k_p * 24.0 + k_i * 10.0 + k_d * 12.0 / 0.75);
     EXPECT_DOUBLE_EQ(pid.step(4.0, 1.0), k_p * 4.0 + k_i * 10.0 + k_d * -20.0);
     EXPECT_DOUBLE_EQ(pid.step(0.0, 1.0), k_p * 0.0 + k_i * 10.0 + k_d * -4.0);
     EXPECT_DOUBLE_EQ(pid.step(2.0, 1.0), k_p * 2.0 + k_i * 10.0 + k_d * 2.0);
-    EXPECT_DOUBLE_EQ(pid.step(-2.0, 1.0), k_p * -2.0 + k_i * 8.0 + k_d * -4.0);
+    // Sign swap reset: integral becomes 0 + -2.0*1.0 = -2.0
+    EXPECT_DOUBLE_EQ(pid.step(-2.0, 1.0), k_p * -2.0 + k_i * -2.0 + k_d * -4.0);
 }
