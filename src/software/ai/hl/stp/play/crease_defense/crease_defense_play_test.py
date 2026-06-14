916cdf4752f34680f59f5caab3ccb0aa46b76f59
Eric Xiao <ericxiao0000@gmail.com>
2026-06-13 23:13:39 -0700

    Unite simulated_tests and field_tests package in gameplay_tests (#3773)
    * Rename simulated_tests package to gameplay_tests
    
    * Move field_tests contents to gameplay_tests
    
    * Add missing BUILD file
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Add other missing BUILD file
    
    * Disable tests in gameplay_tests because field tests aren't supported
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Revert "[pre-commit.ci lite] apply automatic fixes"
    
    This reverts commit e21feb9e43d0de57d18aa972506cbc9e48c0e72f.
    
    * Fix github action

--- src/software/ai/hl/stp/play/crease_defense/crease_defense_play_test.py
+++ src/software/ai/hl/stp/play/crease_defense/crease_defense_play_test.py
@@ -3,16 +3,16 @@ from proto.play_pb2 import PlayName
 from proto.import_all_protos import *
 from proto.message_translation.tbots_protobuf import create_world_state
 from proto.ssl_gc_common_pb2 import Team
-from software.simulated_tests.simulated_test_fixture import (
+from software.gameplay_tests.simulated_test_fixture import (
     pytest_main,
 )
-from software.simulated_tests.validation.robot_speed_threshold import (
+from software.gameplay_tests.validation.robot_speed_threshold import (
     RobotSpeedEventuallyBelowThreshold,
 )
-from software.simulated_tests.validation.robot_enters_region import (
+from software.gameplay_tests.validation.robot_enters_region import (
     NumberOfRobotsEventuallyEntersRegion,
 )
-from software.simulated_tests.validation.delay_validation import DelayValidation
+from software.gameplay_tests.validation.delay_validation import DelayValidation
 
 
 def test_crease_defense_play(simulated_test_runner):
