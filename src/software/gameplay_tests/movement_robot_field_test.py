3edd9ed8a6e31c3eab8a6026355bb5401c06690a
Eric Xiao <ericxiao0000@gmail.com>
2026-06-17 13:15:36 -0700

    Rewrite field test in sim test style

--- src/software/gameplay_tests/movement_robot_field_test.py
+++ src/software/gameplay_tests/movement_robot_field_test.py
@@ -1,192 +1,138 @@
 import math
-import threading
+
+import pytest
+import software.python_bindings as tbots_cpp
 
 from proto.import_all_protos import *
-from software.gameplay_tests.fixture import *
-from software.gameplay_tests.gameplay_test_runner import WORLD_BUFFER_TIMEOUT
-
+from proto.message_translation.tbots_protobuf import create_world_state
 from software.gameplay_tests.util import pytest_main
-from software.logger.logger import create_logger
+from software.gameplay_tests.validation.delay_validation import DelayValidation
+from software.gameplay_tests.validation.duration_validation import DurationValidation
+from software.gameplay_tests.validation.robot_at_orientation import (
+    RobotEventuallyAtOrientation,
+)
+from software.gameplay_tests.validation.robot_enters_region import (
+    RobotEventuallyEntersRegion,
+)
 
-logger = create_logger(__name__)
 
+@pytest.mark.parametrize(
+    "angle",
+    [0, 45, 90, 180, 270, 0],
+)
+def test_basic_rotation(angle, gameplay_test_runner):
+    target_angle = tbots_cpp.Angle.fromDegrees(angle)
+    start_position = tbots_cpp.Point(-1.5, 0.6)
 
-# this test can only be run on the field
-def test_basic_rotation(gameplay_test_runner):
-    test_angles = [0, 45, 90, 180, 270, 0]
-
-    world = gameplay_test_runner.world_buffer.get(block=True, timeout=WORLD_BUFFER_TIMEOUT)
-    if len(world.friendly_team.team_robots) == 0:
-        raise Exception("The first world received had no robots in it!")
-
-    print("Here are the robots:")
-    print(
-        [
-            robot.current_state.global_position
-            for robot in world.friendly_team.team_robots
-        ]
-    )
-
-    id = world.friendly_team.team_robots[0].id
-    print(f"Running test on robot {id}")
-
-    robot = world.friendly_team.team_robots[0]
-    rob_pos_p = robot.current_state.global_position
-
-    def execute_test():
-        # Wait for the user to flip the estop to PLAY
-        field_test_runner.wait_for_estop_play()
-
-        # Force start the game automatically
-        field_test_runner.gamecontroller.send_gc_command(
-            proto.ssl_gc_state_pb2.Command.FORCE_START,
-            proto.ssl_gc_common_pb2.Team.UNKNOWN,
-            final_ball_placement_point=None,
+    def setup():
+        gameplay_test_runner.set_world_state(
+            create_world_state(
+                blue_robot_locations=[
+                    start_position,
+                ],
+                yellow_robot_locations=[],
+                ball_location=tbots_cpp.Point(0, 0),
+                ball_velocity=tbots_cpp.Vector(0, 0),
+            ),
         )
 
-        for angle in test_angles:
-            print(f"Rotating to {angle} degrees")
-            move_tactic = MoveTactic()
-            move_tactic.destination.CopyFrom(rob_pos_p)
-            move_tactic.dribbler_mode = DribblerMode.OFF
-            move_tactic.final_orientation.CopyFrom(
-                Angle(radians=angle * math.pi / 180.0)
-            )
-            move_tactic.ball_collision_type = BallCollisionType.AVOID
-            move_tactic.auto_chip_or_kick.CopyFrom(
-                AutoChipOrKick(autokick_speed_m_per_s=0.0)
-            )
-            move_tactic.max_allowed_speed_mode = MaxAllowedSpeedMode.PHYSICAL_LIMIT
-            move_tactic.obstacle_avoidance_mode = ObstacleAvoidanceMode.SAFE
+        move_tactic = MoveTactic(
+            destination=tbots_cpp.createPointProto(start_position),
+            dribbler_mode=DribblerMode.OFF,
+            final_orientation=tbots_cpp.createAngleProto(target_angle),
+            ball_collision_type=BallCollisionType.AVOID,
+            auto_chip_or_kick=AutoChipOrKick(autokick_speed_m_per_s=0.0),
+            max_allowed_speed_mode=MaxAllowedSpeedMode.PHYSICAL_LIMIT,
+            obstacle_avoidance_mode=ObstacleAvoidanceMode.SAFE,
+        )
 
-            # Setup Tactic
-            field_test_runner.set_tactics(
-                blue_tactics={id: move_tactic}, yellow_tactics=None
-            )
-            field_test_runner.run_test(
-                always_validation_sequence_set=[[]],
-                eventually_validation_sequence_set=[[]],
-                test_timeout_s=5,
-            )
-
-            # validate by eye
-            logger.info(f"robot set to {angle} orientation")
-            time.sleep(2)
-
-        # Send a halt tactic after the test finishes
         gameplay_test_runner.set_tactics(
-            blue_tactics={id: HaltTactic()}, yellow_tactics=None
+            blue_tactics={
+                0: move_tactic,
+            },
         )
 
-    test_thread = threading.Thread(target=execute_test, daemon=True)
-    test_thread.start()
-
-    # If thunderscope is enabled, show it in the main thread (blocking)
-    if field_test_runner.thunderscope:
-        field_test_runner.thunderscope.show()
-
-    test_thread.join()
-
-
-def test_one_robots_square(gameplay_test_runner):
-    world = gameplay_test_runner.world_buffer.get(block=True, timeout=WORLD_BUFFER_TIMEOUT)
-    if len(world.friendly_team.team_robots) == 0:
-        raise Exception("The first world received had no robots in it!")
-
-    print("Here are the robots:")
-    print(
-        [
-            robot.current_state.global_position
-            for robot in world.friendly_team.team_robots
-        ]
+    gameplay_test_runner.run_test(
+        setup=setup,
+        test_timeout_s=5,
+        eventually_validation_sequence_set=[
+            [
+                DurationValidation(
+                    duration_s=1,
+                    validation=RobotEventuallyAtOrientation(0, target_angle),
+                ),
+            ]
+        ],
     )
 
-    id = world.friendly_team.team_robots[0].id
-    print(f"Running test on robot {id}")
 
-    point1 = Point(x_meters=0.4, y_meters=0.4)
-    point2 = Point(x_meters=0.4, y_meters=-0.4)
-    point3 = Point(x_meters=-0.4, y_meters=-0.4)
-    point4 = Point(x_meters=-0.4, y_meters=0.4)
-
-    tactic_0 = MoveTactic(
-        destination=point1,
-        dribbler_mode=DribblerMode.OFF,
-        final_orientation=Angle(radians=-math.pi / 2),
-        ball_collision_type=BallCollisionType.AVOID,
-        auto_chip_or_kick=AutoChipOrKick(autokick_speed_m_per_s=0.0),
-        max_allowed_speed_mode=MaxAllowedSpeedMode.PHYSICAL_LIMIT,
-        obstacle_avoidance_mode=ObstacleAvoidanceMode.SAFE,
-    )
-    tactic_1 = MoveTactic(
-        destination=point2,
-        dribbler_mode=DribblerMode.OFF,
-        final_orientation=Angle(radians=-math.pi / 2),
-        ball_collision_type=BallCollisionType.AVOID,
-        auto_chip_or_kick=AutoChipOrKick(autokick_speed_m_per_s=0.0),
-        max_allowed_speed_mode=MaxAllowedSpeedMode.PHYSICAL_LIMIT,
-        obstacle_avoidance_mode=ObstacleAvoidanceMode.SAFE,
-    )
-    tactic_2 = MoveTactic(
-        destination=point3,
-        dribbler_mode=DribblerMode.OFF,
-        final_orientation=Angle(radians=-math.pi / 2),
-        ball_collision_type=BallCollisionType.AVOID,
-        auto_chip_or_kick=AutoChipOrKick(autokick_speed_m_per_s=0.0),
-        max_allowed_speed_mode=MaxAllowedSpeedMode.PHYSICAL_LIMIT,
-        obstacle_avoidance_mode=ObstacleAvoidanceMode.SAFE,
-    )
-    tactic_3 = MoveTactic(
-        destination=point4,
-        dribbler_mode=DribblerMode.OFF,
-        final_orientation=Angle(radians=-math.pi / 2),
-        ball_collision_type=BallCollisionType.AVOID,
-        auto_chip_or_kick=AutoChipOrKick(autokick_speed_m_per_s=0.0),
-        max_allowed_speed_mode=MaxAllowedSpeedMode.PHYSICAL_LIMIT,
-        obstacle_avoidance_mode=ObstacleAvoidanceMode.SAFE,
-    )
-
-    tactics = [tactic_0, tactic_1, tactic_2, tactic_3, tactic_0]
-
-    def execute_test():
-        # Wait for the user to flip the estop to PLAY
-        field_test_runner.wait_for_estop_play()
-
-        # Force start the game automatically
-        field_test_runner.gamecontroller.send_gc_command(
-            proto.ssl_gc_state_pb2.Command.FORCE_START,
-            proto.ssl_gc_common_pb2.Team.UNKNOWN,
-            final_ball_placement_point=None,
+@pytest.mark.parametrize(
+    "start_position, end_position",
+    [
+        (
+            tbots_cpp.Point(-1.5, 0.6),
+            tbots_cpp.Point(-0.3, 0.6),
+        ),
+        (
+            tbots_cpp.Point(-0.3, 0.6),
+            tbots_cpp.Point(-0.3, -0.6),
+        ),
+        (
+            tbots_cpp.Point(-0.3, -0.6),
+            tbots_cpp.Point(-1.5, -0.6),
+        ),
+        (
+            tbots_cpp.Point(-1.5, -0.6),
+            tbots_cpp.Point(-1.5, 0.6),
+        ),
+    ],
+)
+def test_one_robots_square(start_position, end_position, gameplay_test_runner):
+    def setup():
+        gameplay_test_runner.set_world_state(
+            create_world_state(
+                blue_robot_locations=[
+                    start_position,
+                ],
+                yellow_robot_locations=[],
+                ball_location=tbots_cpp.Point(0, 0),
+                ball_velocity=tbots_cpp.Vector(0, 0),
+            ),
         )
 
-        for tactic in tactics:
-            print(f"Going to {tactic.destination}")
-
-            field_test_runner.set_tactics(
-                blue_tactics={
-                    id: tactic,
-                },
-                yellow_tactics=None,
-            )
-            field_test_runner.run_test(
-                always_validation_sequence_set=[[]],
-                eventually_validation_sequence_set=[[]],
-                test_timeout_s=8,
-            )
-
-        # Send a halt tactic after the test finishes
-        field_test_runner.set_tactics(
-            blue_tactics={id: HaltTactic()}, yellow_tactics=None
+        tactic = MoveTactic(
+            destination=tbots_cpp.createPointProto(end_position),
+            dribbler_mode=DribblerMode.OFF,
+            final_orientation=Angle(radians=-math.pi / 2),
+            ball_collision_type=BallCollisionType.AVOID,
+            auto_chip_or_kick=AutoChipOrKick(autokick_speed_m_per_s=0.0),
+            max_allowed_speed_mode=MaxAllowedSpeedMode.PHYSICAL_LIMIT,
+            obstacle_avoidance_mode=ObstacleAvoidanceMode.SAFE,
         )
 
-    test_thread = threading.Thread(target=execute_test, daemon=True)
-    test_thread.start()
+        gameplay_test_runner.set_tactics(
+            blue_tactics={
+                0: tactic,
+            },
+        )
 
-    # If thunderscope is enabled, show it in the main thread (blocking)
-    if field_test_runner.thunderscope:
-        field_test_runner.thunderscope.show()
-
-    test_thread.join()
+    gameplay_test_runner.run_test(
+        setup=setup,
+        test_timeout_s=4,
+        eventually_validation_sequence_set=[
+            [
+                RobotEventuallyEntersRegion(
+                    regions=[tbots_cpp.Circle(end_position, 0.05)]
+                ),
+                DelayValidation(
+                    delay_s=1,
+                    validation=RobotEventuallyEntersRegion(
+                        regions=[tbots_cpp.Circle(end_position, 0.05)]
+                    ),
+                ),
+            ]
+        ],
+    )
 
 
 if __name__ == "__main__":
