02acb95fe3ff08a02bb58763a0038df8e2bedcae
Andrew Mao <andrewyxmao@gmail.com>
2026-06-16 20:14:17 -0700

    linting

--- src/software/power/charger.h
+++ src/software/power/charger.h
@@ -7,15 +7,15 @@
  */
 class Charger
 {
-public:
+   public:
     /**
      * Creates a Charger setting up pins and attaching interrupts.
      */
     Charger();
     /**
      * Sets the state of the capacitors and whether we should charge them.
-     * @param should_charge Sets the charge pin to HIGH if should_charge is true to begin charging the capacitors.
-     * Otherwise, sets the charge pin to LOW.
+     * @param should_charge Sets the charge pin to HIGH if should_charge is true to begin
+     * charging the capacitors. Otherwise, sets the charge pin to LOW.
      */
     static void chargeCapacitors(bool should_charge);
     /**
@@ -31,7 +31,7 @@ public:
      */
     bool getFlybackFault();
 
-private:
+   private:
     static constexpr float VOLTAGE_DIVIDER = 1003.0 / 13.0;
     static constexpr float RESOLUTION      = 4096.0;
     static constexpr float SCALE_VOLTAGE   = 3.3;
