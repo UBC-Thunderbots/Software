5cbed4920e39e82272d54920a6e7d752eb7c0d9e
sauravbanna <sauravbanna@gmail.com>
2026-06-29 20:01:17 -0700

    move to utils file

--- src/software/evaluation/logs/log_interface.py
+++ src/software/evaluation/logs/log_interface.py
@@ -3,30 +3,7 @@ from abc import abstractmethod, ABC
 from dataclasses import dataclass, field
 from proto.import_all_protos import *
 from typing import Iterator, Any, override
-from google.protobuf.descriptor import Descriptor, FieldDescriptor
 from software.thunderscope.time_provider import time_provider_instance
-
-
-def count_primitive_fields(descriptor: Descriptor):
-    """Recursively counts the number of primitive fields in a Protobuf message
-    using its descriptor.
-
-    :param message: the message descriptor to count all leaf-level primitive fields for
-    :return: the count of primitive fields
-    """
-    count = 0
-
-    for field in descriptor.fields:
-        # Check if the field is a nested message
-        if field.type == FieldDescriptor.TYPE_MESSAGE:
-            # Get the nested message class to recurse into its descriptor
-            nested_message = field.message_type
-            # Recurse using the nested message's descriptor
-            count += count_primitive_fields(nested_message)
-        else:
-            # It's a primitive type (double, float, int, bool, string, etc.)
-            count += 1
-    return count
 
 
 class IEvalLog(ABC):
