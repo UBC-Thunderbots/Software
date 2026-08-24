7d18b68ed63c700c573266e5e73632e409427737
Avah Xiao <avahxiao@gmail.com>
2026-08-24 15:19:36 -0700

    Link to linter rule docs in comments

--- scripts/lint_and_format.sh
+++ scripts/lint_and_format.sh
@@ -71,7 +71,8 @@ function run_ruff() {
         exit 1
     fi
 
-    # F403 flags wildcard imports, F405 flags names resolved through wildcard imports.
+    # F403: https://docs.astral.sh/ruff/rules/undefined-local-with-import-star/
+    # F405: https://docs.astral.sh/ruff/rules/undefined-local-with-import-star-usage/
     /opt/tbotspython/bin/python3 -m ruff check $BAZEL_ROOT_DIR --select F403,F405
 
     if [[ "$?" != 0 ]]; then
