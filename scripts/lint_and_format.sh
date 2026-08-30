1f4ace5ea2a767d8da536a2d533f87f25bac1fc1
Avah Xiao <avahxiao@gmail.com>
2026-08-29 22:47:29 -0700

    Add ruff config with all linter rules

--- scripts/lint_and_format.sh
+++ scripts/lint_and_format.sh
@@ -63,20 +63,11 @@ function run_bazel_formatting () {
 function run_ruff() {
     printf "Running ruff to lint and format Python files...\n\n"
 
-    /opt/tbotspython/bin/python3 -m ruff check $BAZEL_ROOT_DIR --fix-only --extend-select D
+    /opt/tbotspython/bin/python3 -m ruff check $BAZEL_ROOT_DIR --fix
     /opt/tbotspython/bin/python3 -m ruff format $BAZEL_ROOT_DIR
 
     if [[ "$?" != 0 ]]; then
         printf "\n***Failed to lint/format Python files!***\n\n"
-        exit 1
-    fi
-
-    # F403: https://docs.astral.sh/ruff/rules/undefined-local-with-import-star/
-    # F405: https://docs.astral.sh/ruff/rules/undefined-local-with-import-star-usage/
-    /opt/tbotspython/bin/python3 -m ruff check $BAZEL_ROOT_DIR --select F403,F405
-
-    if [[ "$?" != 0 ]]; then
-        printf "\n***Wildcard imports should not be used (F403/F405)!***\n\n"
         exit 1
     fi
 }
