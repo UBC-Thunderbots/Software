58170e5657ece934ac749fef934ba7001a75c70a
Avah Xiao <avahxiao@gmail.com>
2026-09-03 13:40:45 -0700

    Fix Qt bug with pytest thunderscope

--- src/software/conftest.py
+++ src/software/conftest.py
@@ -1,6 +1,14 @@
+from software.thunderscope.qt_dependency_bootstrap import preload_bundled_qt_libs
+
+# Must run before importing any Qt bindings (see qt_dependency_bootstrap).
+preload_bundled_qt_libs()
+
 import pytest  # noqa: F401
-from software.gameplay_tests.simulated_test_fixture import simulated_test_runner  # noqa: F401
+
 from software.gameplay_tests.field_test_fixture import field_test_runner  # noqa: F401
+from software.gameplay_tests.simulated_test_fixture import (
+    simulated_test_runner,  # noqa: F401
+)
 
 # Pytest requires that all tests fixtures shared across a package be defined
 # in a single conftest.py file in the parent directory of the package.
