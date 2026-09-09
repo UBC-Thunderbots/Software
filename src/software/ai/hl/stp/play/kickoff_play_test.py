4f16d76164c3db9629ecb9277ab95c1cd59e2eaf
annieisawesome2 <anniesun608@gmail.com>
2026-09-08 20:10:43 -0700

    fix merge conflict

--- src/software/ai/hl/stp/play/kickoff_play_test.py
+++ src/software/ai/hl/stp/play/kickoff_play_test.py
@@ -14,25 +14,24 @@
 
 import threading
 
-import pytest
-
-import software.python_bindings as tbots_cpp
-from software.gameplay_tests.validation.robot_enters_region import (
-    NumberOfRobotsAlwaysStaysInRegion,
-    NumberOfRobotsNeverEntersRegion,
-    RobotNeverEntersRegion,
-)
-from software.gameplay_tests.validation.ball_enters_region import (
-    BallEventuallyExitsRegion,
-    BallNeverEntersRegion,
-)
 import proto.import_all_protos as protos
+import pytest
+import software.python_bindings as tbots_cpp
 from proto.message_translation.tbots_protobuf import create_world_state
 from proto.ssl_gc_common_pb2 import Team as SslTeam
 from software.gameplay_tests.simulated_test_fixture import (
     pytest_main,
 )
+from software.gameplay_tests.validation.ball_enters_region import (
+    BallEventuallyExitsRegion,
+    BallNeverEntersRegion,
+)
 from software.gameplay_tests.validation.or_validation import OrValidation
+from software.gameplay_tests.validation.robot_enters_region import (
+    NumberOfRobotsAlwaysStaysInRegion,
+    NumberOfRobotsNeverEntersRegion,
+    RobotNeverEntersRegion,
+)
 
 
 @pytest.mark.parametrize("is_friendly_test", [True, False])
