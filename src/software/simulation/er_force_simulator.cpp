1e9da2187d4d4feee438885ce488d0171e465c07
Samuel Ubuntu Laptop <lausingsamuel@gmail.com>
2026-09-13 14:06:50 -0700

    x

--- src/software/simulation/er_force_simulator.cpp
+++ src/software/simulation/er_force_simulator.cpp
@@ -65,7 +65,7 @@ double sampleCorrelatedNoise(std::mt19937& rng, double& bias, double dt_seconds,
 }
 }  // namespace
 
-const std::string ErForceSimulator::CSV_OUTPUT_PATH = "/tmp/master_test_new.csv";
+const std::string ErForceSimulator::CSV_OUTPUT_PATH = "/tmp/sim_test_new.csv";
 
 ErForceSimulator::ErForceSimulator(const TbotsProto::FieldType& field_type,
                                    const robot_constants::RobotConstants& robot_constants,
