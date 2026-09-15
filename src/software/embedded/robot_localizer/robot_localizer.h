e1ec566bc5c1b1bb246f6361d5b3fa73b16deaaa
Thunderbots <ubcrobocup@gmail.com>
2026-09-14 22:27:16 -0700

    move folder

--- src/software/embedded/robot_localizer/robot_localizer.h
+++ src/software/embedded/robot_localizer/robot_localizer.h
@@ -14,14 +14,9 @@
 #include "software/util/make_enum/make_enum.hpp"
 #include "software/world/robot_state.h"
 
-// X_POSITION/Y_POSITION are in world space; X_VELOCITY/Y_VELOCITY are in the robot's
-// local frame (see velocity_conversion_util.h), matching what the motor sensors report
-// directly and avoiding a lossy conversion through the orientation estimate.
 MAKE_ENUM(StateIndex, X_POSITION, Y_POSITION, ORIENTATION, X_VELOCITY, Y_VELOCITY,
           ANGULAR_VELOCITY);
 
-// MOTOR_X_VELOCITY/MOTOR_Y_VELOCITY are in the robot's local frame, matching
-// StateIndex::X_VELOCITY/Y_VELOCITY.
 MAKE_ENUM(MeasurementIndex, VISION_X_POSITION, VISION_Y_POSITION, VISION_ORIENTATION,
           MOTOR_X_VELOCITY, MOTOR_Y_VELOCITY, MOTOR_ANGULAR_VELOCITY,
           IMU_ANGULAR_VELOCITY);
@@ -30,6 +25,14 @@ MAKE_ENUM(ControlIndex, X_VELOCITY_TARGET, Y_VELOCITY_TARGET);
 
 MAKE_ENUM(FilterStepType, PREDICT, MOTOR_DATA, IMU_DATA, VISION_DATA);
 
+static constexpr VISION_X_INITIAL_VARIANCE_M = 0.00001;
+static constexpr VISION_Y_INITIAL_VARIANCE_M = 0.00001;
+static constexpr VISION_THETA_INITIAL_VARIANCE_RAD = 0.00001;
+static constexpr MOTOR_THETA_INITIAL_VARIANCE_M_S = 0.5;
+static constexpr MOTOR_THETA_INITIAL_VARIANCE_M_S = 0.5;
+static constexpr MOTOR_THETA_INITIAL_VARIANCE_M_S = 0.5;
+
+static constexpr PROCESS_MODEL_INITIAL_VARIANCE = 0.5;
 /**
  * Estimates robot position, orientation, velocity, and angular velocity using an
  * extended Kalman filter.
