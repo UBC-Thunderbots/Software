edb5a179435f696c9355c66ddb12adba5fa90f28
Thunderbots <ubcrobocup@gmail.com>
2026-09-14 22:35:51 -0700

    localizer constants

--- src/software/embedded/robot_localizer/robot_localizer.h
+++ src/software/embedded/robot_localizer/robot_localizer.h
@@ -14,25 +14,6 @@
 #include "software/util/make_enum/make_enum.hpp"
 #include "software/world/robot_state.h"
 
-MAKE_ENUM(StateIndex, X_POSITION, Y_POSITION, ORIENTATION, X_VELOCITY, Y_VELOCITY,
-          ANGULAR_VELOCITY);
-
-MAKE_ENUM(MeasurementIndex, VISION_X_POSITION, VISION_Y_POSITION, VISION_ORIENTATION,
-          MOTOR_X_VELOCITY, MOTOR_Y_VELOCITY, MOTOR_ANGULAR_VELOCITY,
-          IMU_ANGULAR_VELOCITY);
-
-MAKE_ENUM(ControlIndex, X_VELOCITY_TARGET, Y_VELOCITY_TARGET);
-
-MAKE_ENUM(FilterStepType, PREDICT, MOTOR_DATA, IMU_DATA, VISION_DATA);
-
-static constexpr VISION_X_INITIAL_VARIANCE_M = 0.00001;
-static constexpr VISION_Y_INITIAL_VARIANCE_M = 0.00001;
-static constexpr VISION_THETA_INITIAL_VARIANCE_RAD = 0.00001;
-static constexpr MOTOR_THETA_INITIAL_VARIANCE_M_S = 0.5;
-static constexpr MOTOR_THETA_INITIAL_VARIANCE_M_S = 0.5;
-static constexpr MOTOR_THETA_INITIAL_VARIANCE_M_S = 0.5;
-
-static constexpr PROCESS_MODEL_INITIAL_VARIANCE = 0.5;
 /**
  * Estimates robot position, orientation, velocity, and angular velocity using an
  * extended Kalman filter.
---
+++ src/software/embedded/robot_localizer/robot_localizer_constants.h
@@ -0,0 +1,53 @@
+#include <Eigen/Dense>
+MAKE_ENUM(StateIndex, X_POSITION, Y_POSITION, ORIENTATION, X_VELOCITY, Y_VELOCITY,
+          ANGULAR_VELOCITY);
+
+MAKE_ENUM(MeasurementIndex, VISION_X_POSITION, VISION_Y_POSITION, VISION_ORIENTATION,
+          MOTOR_X_VELOCITY, MOTOR_Y_VELOCITY, MOTOR_ANGULAR_VELOCITY,
+          IMU_ANGULAR_VELOCITY);
+
+MAKE_ENUM(ControlIndex, X_VELOCITY_TARGET, Y_VELOCITY_TARGET);
+
+MAKE_ENUM(FilterStepType, PREDICT, MOTOR_DATA, IMU_DATA, VISION_DATA);
+
+
+static constexpr size_t STATE_SIZE       = reflective_enum::size<StateIndex>();
+static constexpr size_t MEASUREMENT_SIZE = reflective_enum::size<MeasurementIndex>();
+static constexpr size_t CONTROL_SIZE     = reflective_enum::size<ControlIndex>();
+
+// Initial Covariances
+static constexpr double VISION_X_INITIAL_VARIANCE_M = 0.00001;
+static constexpr double VISION_Y_INITIAL_VARIANCE_M = 0.00001;
+static constexpr double VISION_THETA_INITIAL_VARIANCE_RAD = 0.00001;
+
+static constexpr double MOTOR_THETA_INITIAL_VARIANCE_M_S = 0.5;
+static constexpr double MOTOR_THETA_INITIAL_VARIANCE_M_S = 0.5;
+static constexpr double MOTOR_THETA_INITIAL_VARIANCE_M_S = 0.5;
+
+static constexpr double PROCESS_MODEL_INITIAL_VARIANCE = 0.5;
+
+
+// Measurement models
+static constexpr Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> VISION_MEASUREMENT_COVARIANCE << 
+1,0,0,0,0,0,0,
+0,1,0,0,0,0,0,
+0,0,1,0,0,0,0,
+0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,
+0,0,0,0,0,0,0;
+
+static constexpr Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> MOTOR_MEASUREMENT_COVARIANCE << 
+0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,
+0,0,0,0,0,0,0;
+0,0,0,1,0,0,0,
+0,0,0,0,1,0,0,
+0,0,0,0,0,1,0,
+
+static constexpr Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> IMU_MEASUREMENT_COVARIANCE << 
+0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,
+0,0,0,0,0,0,0;
+0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,
+0,0,0,0,0,0,1,
